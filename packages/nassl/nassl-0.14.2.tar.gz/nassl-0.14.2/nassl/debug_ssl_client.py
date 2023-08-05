042088382a1c86d2f590e496b042e7793421deff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:52 +0000

    Add package nassl/0.14.2/nassl-0.14.2.tar.gz

---
+++ packages/nassl/nassl-0.14.2.tar.gz/nassl-0.14.2/nassl/debug_ssl_client.py
@@ -0,0 +1,117 @@
+#!/usr/bin/python2.7
+from nassl._nassl import SSL
+from ssl_client import SslClient
+
+
+
+class DebugSslClient(SslClient):
+    """An SSL client with additional debug methods that no one should ever use (insecure renegotiation, etc.).
+    """
+
+    def get_secure_renegotiation_support(self):
+        # type: () -> bool
+        return self._ssl.get_secure_renegotiation_support()
+
+
+    def get_current_compression_method(self):
+        # type: () -> Optional[str]
+        return self._ssl.get_current_compression_method()
+
+
+    @staticmethod
+    def get_available_compression_methods():
+        # type: () -> List[str]
+        """Returns the list of SSL compression methods supported by SslClient.
+        """
+        return SSL.get_available_compression_methods()
+
+
+    def do_renegotiate(self):
+        # type: () -> None
+        """Initiate an SSL renegotiation.
+        """
+        if not self._is_handshake_completed:
+            raise IOError('SSL Handshake was not completed; cannot renegotiate.')
+
+        self._ssl.renegotiate()
+        self.do_handshake()
+
+
+    def get_session(self):
+        # type: () -> nassl._nassl.SSL_SESSION
+        """Get the SSL connection's Session object.
+        """
+        return self._ssl.get_session()
+
+
+    def set_session(self, ssl_session):
+        # type: (nassl._nassl.SSL_SESSION) -> None
+        """Set the SSL connection's Session object.
+        """
+        self._ssl.set_session(ssl_session)
+
+
+    def set_options(self, options):
+        # type: (int) -> int
+        return self._ssl.set_options(options)
+
+
+    def get_dh_param(self):
+        # type: () -> Dict[str, str]
+        """Retrieve the negotiated Ephemeral Diffie Helmann parameters.
+        """
+        d = self._openssl_str_to_dic(self._ssl.get_dh_param())
+        d['GroupSize'] = d.pop('DH_Parameters').strip('( bit)')
+        d['Type'] = "DH"
+        d['Generator'] = d.pop('generator').split(' ')[0]
+        return d
+
+
+    def get_ecdh_param(self):
+        # type: () -> Dict[str, str]
+        """Retrieve the negotiated Ephemeral EC Diffie Helmann parameters.
+        """
+        d = self._openssl_str_to_dic(self._ssl.get_ecdh_param(), '        ')
+        d['GroupSize'] = d.pop('ECDSA_Parameters').strip('( bit)')
+        d['Type'] = "ECDH"
+        if 'Cofactor' in d :
+            d['Cofactor'] = d['Cofactor'].split(' ')[0]
+
+        for k in d.keys() :
+            if k.startswith('Generator') :
+                d['Generator'] = d.pop(k)
+                d['GeneratorType'] = k.split('_')[1].strip('()')
+                break
+        else :
+            d['GeneratorType'] = 'Unknown'
+        return d
+
+
+    @staticmethod
+    def _openssl_str_to_dic(s, param_tab='            '):
+        # type: (str, str) -> Dict[str, str]
+        """EDH and ECDH parameters pretty-printing.
+        """
+        d = {}
+        to_XML = lambda x : "_".join(m for m in x.replace('-', ' ').split(' '))
+        current_arg = None
+        for l in s.splitlines() :
+            if not l.startswith(param_tab) :
+                if current_arg :
+                    d[current_arg] = "0x"+d[current_arg].replace(':', '')
+                    current_arg = None
+                args = tuple(arg.strip() for arg in l.split(':') if arg.strip())
+                if len(args) > 1 :
+                    # one line parameter
+                    d[to_XML(args[0])] = args[1]
+                else :
+                    # multi-line parameter
+                    current_arg = to_XML(args[0])
+                    d[current_arg] = ''
+            else :
+                d[current_arg] += l.strip()
+        if current_arg :
+            d[current_arg] = "0x"+d[current_arg].replace(':', '')
+        return d
+
+
