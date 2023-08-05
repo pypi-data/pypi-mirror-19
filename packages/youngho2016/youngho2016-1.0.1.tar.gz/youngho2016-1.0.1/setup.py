23fd5ffe3e4d3ec675966d4ae67f178fa6331347
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:06 +0000

    Add package youngho2016/1.0.1/youngho2016-1.0.1.tar.gz

---
+++ packages/youngho2016/youngho2016-1.0.1.tar.gz/youngho2016-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+	name = 'youngho2016',
+	version = '1.0.1',
+	py_modules = ['youngho2016'],
+	author = 'youngho kim',
+	author_email = 'luckypenny79@gmail.com',
+	url = 'http://www.appsealing.com',
+	description = 'A simple printer of nested lists',
+)
