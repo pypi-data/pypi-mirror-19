d18c2899fd083efc4415959faca15f8b1f54dd31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:56 +0000

    Add package nico_nester/1.0.0/nico_nester-1.0.0.tar.gz

---
+++ packages/nico_nester/nico_nester-1.0.0.tar.gz/nico_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+         name           = 'nico_nester',
+         version        = '1.0.0',
+         Py_modules     = ['nico_nester'],
+         author         = 'NicoNova',
+         author_email   = 'gdbaygan@gmail.com',
+         url            = '',
+         description    = 'A simple printer of nested lists',
+    )
