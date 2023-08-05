cb41c32666b38818254eda75a88838c48794662b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:48 +0000

    Add package CoolAMQP/0.11/CoolAMQP-0.11.zip

---
+++ packages/CoolAMQP/CoolAMQP-0.11.zip/CoolAMQP-0.11/coolamqp/__init__.py
@@ -0,0 +1,5 @@
+# coding=UTF-8
+from .cluster import ClusterNode, Cluster
+from .events import ConnectionDown, ConnectionUp, MessageReceived, ConsumerCancelled
+from .messages import Message, Exchange, Queue
+
