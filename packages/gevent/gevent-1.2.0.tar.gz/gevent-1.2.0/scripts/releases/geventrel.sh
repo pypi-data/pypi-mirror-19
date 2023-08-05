69f08209e8e0c2cdf1706d2e5c81f59a0626e444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:20 +0000

    Add package gevent/1.2.0/gevent-1.2.0.tar.gz

---
+++ packages/gevent/gevent-1.2.0.tar.gz/gevent-1.2.0/scripts/releases/geventrel.sh
@@ -0,0 +1,35 @@
+#!/opt/local/bin/bash
+#
+# Quick hack script to build a single gevent release in a virtual env. Takes one
+# argument, the path to python to use.
+# Has hardcoded paths, probably only works on my (JAM) machine.
+
+set -e
+export WORKON_HOME=$HOME/Projects/VirtualEnvs
+export VIRTUALENVWRAPPER_LOG_DIR=~/.virtualenvs
+source `which virtualenvwrapper.sh`
+
+# Make sure there are no -march flags set
+# https://github.com/gevent/gevent/issues/791
+unset CFLAGS
+unset CXXFLAGS
+unset CPPFLAGS
+
+BASE=`pwd`/../../
+BASE=`greadlink -f $BASE`
+
+
+cd /tmp/gevent
+virtualenv -p $1 `basename $1`
+cd `basename $1`
+echo "Made tmpenv"
+echo `pwd`
+source bin/activate
+echo cloning $BASE
+git clone $BASE gevent
+cd ./gevent
+pip install -U pip
+pip install -U setuptools cython greenlet cffi
+pip install -U wheel
+python ./setup.py sdist bdist_wheel
+cp dist/*whl /tmp/gevent/
