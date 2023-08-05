38f37afda59a12060512b669eccce0d5d8f97b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:49 +0000

    Add package Optimus/0.8.1/Optimus-0.8.1.tar.gz

---
+++ packages/Optimus/Optimus-0.8.1.tar.gz/Optimus-0.8.1/optimus/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Optimus is a static site builder, it produces HTML from templates (through
+'Jinja2') with assets management (through 'webassets') and i18n support for
+translations (through 'Babel').
+"""
+__version__ = "0.8.1"
