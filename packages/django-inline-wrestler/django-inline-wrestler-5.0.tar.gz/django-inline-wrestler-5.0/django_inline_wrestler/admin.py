682b1f55d3d4ffbf0350dbcdca4a897d24fbd229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:08 +0000

    Add package django-inline-wrestler/5.0/django-inline-wrestler-5.0.tar.gz

---
+++ packages/django-inline-wrestler/django-inline-wrestler-5.0.tar.gz/django-inline-wrestler-5.0/django_inline_wrestler/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+
+class AdminListOrderable(admin.ModelAdmin):
+	class Media:        
+		css = {
+			"all": ('django_inline_wrestler/css/django-inline-wrestler.css',)
+		}
+		js = [
+			'django_inline_wrestler/js/django-inline-wrestler.js' 
+		]
+
+class TabularInlineOrderable(admin.TabularInline):
+	class Media:        
+		css = {
+			"all": ('django_inline_wrestler/css/django-inline-wrestler.css',)
+		}
+		js = [
+			'django_inline_wrestler/js/django-inline-wrestler.js' 
+		]
+
+	classes = ['django-inline-wrestler'] 
+	extra = 0
+
+
+
+class StackedInlineOrderable(admin.StackedInline):
+	class Media:        
+		css = {
+			"all": ('django_inline_wrestler/css/django-inline-wrestler.css',)
+		}
+		js = [
+			'django_inline_wrestler/js/django-inline-wrestler.js' 
+		]
+
+	classes = ['django-inline-wrestler'] 
+	extra = 0
