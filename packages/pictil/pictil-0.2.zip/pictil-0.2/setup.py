ae9270e503009fca9bc6af3706b6d60b6ef99a60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:42 +0000

    Add package pictil/0.2/pictil-0.2.zip

---
+++ packages/pictil/pictil-0.2.zip/pictil-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+    name='pictil',
+    description='Provies renaming utility for Nikon output files.',
+    version='0.2',
+    url='https://github.com/ChrisoftheBoyerClan/pictil',
+    author='Christopher Boyer',
+    author_email='ChrisoftheBoyerClan@gmail.com',
+    entry_points={ 'console_scripts': [ 'pictil=pictil.main:main', ], },
+    packages=['pictil'],
+    license='MIT',
+    keywords='rename nikon pictures'
+)
