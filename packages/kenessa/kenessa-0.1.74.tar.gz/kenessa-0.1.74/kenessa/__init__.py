3734bf80f2c8a4f79c7137750b61fafed248f20c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:56 +0000

    Add package kenessa/0.1.74/kenessa-0.1.74.tar.gz

---
+++ packages/kenessa/kenessa-0.1.74.tar.gz/kenessa-0.1.74/kenessa/__init__.py
@@ -0,0 +1,27 @@
+import json
+import os
+import kenessa
+
+
+class Province:
+    def __init__(self, identifier):
+        self.path = os.path.dirname(kenessa.__file__)
+        self.json_province = json.loads(open(self.path + '/json/province.json').read())
+        self.identifier = str(identifier).replace(" ", "")
+
+    def province(self):
+        if self.identifier == 'all':
+            return self.json_province
+        else:
+            for province in self.json_province:
+                keywords = province['keywords']
+                keywords = [keyword.lower() for keyword in keywords]
+                if self.identifier.lower() in keywords:
+                    return province
+
+            return None
+
+
+class District:
+    def __init__(self):
+        pass
