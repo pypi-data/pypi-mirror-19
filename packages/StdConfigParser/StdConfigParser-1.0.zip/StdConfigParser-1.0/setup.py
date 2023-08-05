739f9dd94ed8fe38f09be45758d5f9702ae4016d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:41 +0000

    Add package StdConfigParser/1.0/StdConfigParser-1.0.zip

---
+++ packages/StdConfigParser/StdConfigParser-1.0.zip/StdConfigParser-1.0/setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import sys
+import os
+import runpy
+from setuptools import setup
+
+
+basepath = os.path.dirname(__file__)
+versiondict = runpy.run_path(os.path.join(basepath, "stdconfigparser.py"))
+version = versiondict["__version__"]
+#with open(os.path.join(basepath, "stdconfigparser.py")) as cfgparser:
+#    exec(cfgparser.read(), versiondict)
+
+with open(os.path.join(basepath, "README.rst")) as readme:
+    long_description = readme.read()
+
+setup(name='StdConfigParser',
+      version=version,
+      description="A standard INI style configuration parser.",
+      long_description=long_description,
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+      ],
+      keywords='configuration config ini parser',
+      author='Wolfgang Langner',
+      author_email='tds333@mailbox.org',
+      url='https://github.com/tds333/stdconfigparser',
+      license='MIT',
+      py_modules=['stdconfigparser'],
+      tests_require=['pytest'],
+      test_suite='pytest',
+      include_package_data=True,
+      zip_safe=True)
