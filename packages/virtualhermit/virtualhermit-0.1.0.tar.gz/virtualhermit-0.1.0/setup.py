181e1c4bdf61128b8112683d77d967bdc7b7255c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:49 +0000

    Add package virtualhermit/0.1.0/virtualhermit-0.1.0.tar.gz

---
+++ packages/virtualhermit/virtualhermit-0.1.0.tar.gz/virtualhermit-0.1.0/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+version = '0.1.0'
+
+setup(
+    name='virtualhermit',
+    version=version,
+    description='virtualhermit checks your newsfeed before you waste time on it.',
+    author='Lakshay Kalbhor',
+    author_email='lakshaykalbhor@gmail.com',
+    license='MIT',
+    keywords=['facebook', 'git', 'github', 'command line', 'cli', 'hermit'],
+    url='http://github.com/lakshaykalbhor/virtualhermit',
+    packages=['src'],
+    install_requires=[
+    'requests',
+    'colorama',
+    'argparse',
+    ],
+    entry_points={
+        'console_scripts': [
+            'virtualhermit=src.virtualhermit:main'
+        ],
+    }
+)
