099ddf967e9d4449906b02e18066e8cf11e8e18c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:05 +0000

    Add package tinker-access-client/2017.1.4.234/tinker-access-client-2017.1.4.234.tar.gz

---
+++ packages/tinker-access-client/tinker-access-client-2017.1.4.234.tar.gz/tinker-access-client-2017.1.4.234/tinker_access_client/Client.py
@@ -0,0 +1,36 @@
+import time
+
+from PackageInfo import PackageInfo
+from ClientLogger import ClientLogger
+from DeviceApi import DeviceApi, Channel
+from ClientOptionParser import ClientOptionParser
+
+
+class Client(object):
+
+    def __init__(self):
+        self.__logger = ClientLogger.setup()
+        self.__opts = ClientOptionParser().parse_args()[0]
+        #self.__deviceApi = DeviceApi(self.__opts)
+
+    def run(self):
+        try:
+            wait = 5
+            counter = 0
+            while counter < 720:
+                counter += 1
+                time.sleep(wait)
+                self.__logger.debug('%s running for %s seconds...', PackageInfo.pip_package_name, counter * wait)
+
+        except KeyboardInterrupt as e:
+            self.__logger.debug('Keyboard-Interrupt')
+            raise e
+
+        except SystemExit as e:
+            self.__logger.debug('System-Exit, process killed')
+            raise e
+
+        except Exception as e:
+            self.__logger.debug('Unable to start the %s daemon.')
+            self.__logger.exception(e)
+            raise e
