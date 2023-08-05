78228286e38854e6e4c828d0f9fda2dfa4f9c7b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:14 +0000

    Add package funni3st/0.2/funni3st-0.2.tar.gz

---
+++ packages/funni3st/funni3st-0.2.tar.gz/funni3st-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funni3st',
+      version='0.2',
+      description='The funniest joke in the world',
+      url='https://github.com/hamadi15/test',
+      author='halim hamadi',
+      author_email='halim.hamadi@mail.ugm.ac.id',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
