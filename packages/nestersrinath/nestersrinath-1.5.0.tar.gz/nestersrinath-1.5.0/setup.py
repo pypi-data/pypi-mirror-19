b3dc331f58da321a40297c4466ea147d17dd8819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:14 +0000

    Add package nestersrinath/1.5.0/nestersrinath-1.5.0.tar.gz

---
+++ packages/nestersrinath/nestersrinath-1.5.0.tar.gz/nestersrinath-1.5.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name		= 'nestersrinath',
+		version		= '1.5.0',
+		py_modules	= ['nestersrinath'],
+		author		= 'srinath',
+		author_email	= 'srinath.anand@gmail.com',
+		url			= 'http://www.headfirstlabs.com',
+		description = 'A simple printer of nested lists'
+	)
