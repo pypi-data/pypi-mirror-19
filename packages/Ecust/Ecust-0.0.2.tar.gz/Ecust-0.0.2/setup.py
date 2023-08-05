9f738f7e072b089dc72e73496e26bf07ac7d27fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:51 +0000

    Add package Ecust/0.0.2/Ecust-0.0.2.tar.gz

---
+++ packages/Ecust/Ecust-0.0.2.tar.gz/Ecust-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='Ecust',
+    version='0.0.2',    
+    description='Ecust Login Module and Some Functions',
+    author='Command',
+    author_email='maoxs2@163.com',    
+    url='http://github.com/maoxs2/Ecust',    
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    scripts=['Ecust_cli.py'],
+    long_description=open('README.rst').read(),
+    entry_points={
+        'console_scripts': [
+            'Ecust_cli = Ecust_cli:main',
+        ],
+    },
+    install_requires=[
+        "lxml",
+        "requests",
+    ],
+)
