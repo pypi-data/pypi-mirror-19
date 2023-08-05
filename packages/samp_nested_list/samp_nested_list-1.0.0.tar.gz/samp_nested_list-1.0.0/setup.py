82526d45af42ef2f43427122fcb24885a5f7735e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:03 +0000

    Add package samp_nested_list/1.0.0/samp_nested_list-1.0.0.tar.gz

---
+++ packages/samp_nested_list/samp_nested_list-1.0.0.tar.gz/samp_nested_list-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'samp_nested_list', 
+        version      = '1.0.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['samp_nested_list'],
+        author       = 'vinod',
+        author_email = 'saivinodpt@gmail.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of nested lists',
+     )
