084b6215bce6731cb04bf4d7d1de7cd9802f0fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:43 +0000

    Add package nesterlixin/1.2.0/nesterlixin-1.2.0.zip

---
+++ packages/nesterlixin/nesterlixin-1.2.0.zip/nesterlixin-1.2.0/nester.py
@@ -0,0 +1,22 @@
+'''这是“nester.py”模块，
+提供了一个名为print_lol()的函数，这个函数的作用是打印列表，
+其中有可能包含（也可能不包含）嵌套列表'''
+import sys
+def print_lol(the_list, indent=False, level=0, fn=sys.stdout):
+
+    '''这个函数取一个位置参数，名为“the_list”，
+    这可以是任何python列表（也可以是包含嵌套列表的列表）。
+    所指定的列表中的每个数据项会（递归的）输出到屏幕上，额数据项各占一行'''
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level+1, fn);
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='',file=fn);
+            print(each_item,file=fn);
+
+            '''with open('output.txt', 'w') as output_file:
+                print(output,file=output_file);'''
+#movies = ["The Holy Grail",1975,"Terry Jones & Terry Gilliam",91,["Graham Chapman",["Michael Palin","John Cheese","Terry Gilliam","Eric Idle","Terry Jones"]]]
+#print_lol(movies,False,0)
---
+++ packages/nesterlixin/nesterlixin-1.2.0.zip/nesterlixin-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nesterlixin',
+    version='1.2.0',
+    py_modules=['nester'],
+    author='someonelixin',
+    author_email='wanglixin112417@pwrd.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+)
