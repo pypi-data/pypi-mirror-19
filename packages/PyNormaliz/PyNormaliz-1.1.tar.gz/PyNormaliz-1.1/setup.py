0c0b684302da3e881db32570112427708ac5e368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:01 +0000

    Add package PyNormaliz/1.1/PyNormaliz-1.1.tar.gz

---
+++ packages/PyNormaliz/PyNormaliz-1.1.tar.gz/PyNormaliz-1.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Extension
+import sys
+
+if sys.version_info < (3,5):
+    macro_list = [ ( "PYTHON_VERSION_OLDER_THREE_FIVE", "1" ) ]
+else:
+    macro_list = [ ]
+
+setup(
+    name = 'PyNormaliz',
+    version = '1.1',
+    description = 'An interface to Normaliz',
+    author = 'Sebastian Gutsche',
+    author_email = 'sebastian.gutsche@gmail.com',
+    url = 'https://github.com/sebasguts/PyNormaliz',
+    ext_modules = [ Extension( "PyNormaliz",
+                              [ "NormalizModule.cpp" ],
+                              extra_link_args=['-lnormaliz', '-lgmp' ],
+                              define_macros = macro_list ) ],
+    package_data = {'': [ "COPYING", "GPLv2" ] },
+)
