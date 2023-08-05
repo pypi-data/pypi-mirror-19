574e0a58db7a04e49a002a38cc9f52f66d8acc71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:41 +0000

    Add package pyirobot/1.0.3/pyirobot-1.0.3.tar.gz

---
+++ packages/pyirobot/pyirobot-1.0.3.tar.gz/pyirobot-1.0.3/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2.7
+
+from setuptools import setup
+import os
+
+setup(
+    name = "pyirobot",
+    version = "1.0.3",
+    author = "Carl Seelye",
+    author_email = "cseelye@gmail.com",
+    description = "Control iRobot cleaning robots",
+    license = "MIT",
+    keywords = "irobot roomba",
+    packages = ["pyirobot"],
+    url = "https://github.com/cseelye/pyirobot",
+    long_description = open(os.path.join(os.path.dirname(__file__), "README.rst")).read(),
+    install_requires = [
+        "enum34>=1.1.6",
+        "requests>=2.12.3",
+    ]
+)
