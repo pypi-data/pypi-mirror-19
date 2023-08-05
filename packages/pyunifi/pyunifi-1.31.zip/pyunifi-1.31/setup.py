c827e8b1f607cb4d4298a17fd887a5f07b6567e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:40 +0000

    Add package pyunifi/1.31/pyunifi-1.31.zip

---
+++ packages/pyunifi/pyunifi-1.31.zip/pyunifi-1.31/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import sys
+
+if sys.version_info[0] == 2:
+    from commands import getoutput
+elif sys.version_info[0] == 3:
+    from subprocess import getoutput
+
+
+setup(name='pyunifi',
+      version='1.31',
+      description='API towards Ubiquity Networks UniFi controller',
+      author='Caleb Dunn',
+      author_email='finish.06@gmail.com',
+      url='https://github.com/finish06/unifi-api',
+      packages=['pyunifi'],
+      scripts=['unifi-low-snr-reconnect', 'unifi-ls-clients', 'unifi-save-statistics', 'unifi-log-roaming'],
+      classifiers=[],
+     )
