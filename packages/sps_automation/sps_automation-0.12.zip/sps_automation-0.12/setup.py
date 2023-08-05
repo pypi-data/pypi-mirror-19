c9259788b5d2b3a2867c18bab233c3c47a67600b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:31 +0000

    Add package sps_automation/0.12/sps_automation-0.12.zip

---
+++ packages/sps_automation/sps_automation-0.12.zip/sps_automation-0.12/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'sps_automation',
+  packages = ['sps_automation'], # this must be the same as the name above
+  version = '0.12',
+  description = 'A simple wrapper for some common automation tasks for school system data departments.',
+  author = 'Patrick Yoho',
+  author_email = 'trickyoho@gmail.com',
+  url = 'https://github.com/SummitPublicSchools/sps-automation', # use the URL to the github repo
+  download_url = 'https://github.com/SummitPublicSchools/sps-automation/tarball/0.12', # I'll explain this in a second
+  keywords = ['automation', 'education', 'illuminate', 'selenium', 'etl'], # arbitrary keywords
+  classifiers = [],
+)
