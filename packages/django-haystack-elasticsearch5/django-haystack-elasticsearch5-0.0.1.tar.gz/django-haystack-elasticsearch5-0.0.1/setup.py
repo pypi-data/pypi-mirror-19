f9f6d87deb3d4fa0c34ef9f2d924e1ad3d212a43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:11 +0000

    Add package django-haystack-elasticsearch5/0.0.1/django-haystack-elasticsearch5-0.0.1.tar.gz

---
+++ packages/django-haystack-elasticsearch5/django-haystack-elasticsearch5-0.0.1.tar.gz/django-haystack-elasticsearch5-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="django-haystack-elasticsearch5",
+    description = "Elasticsearch 5 supporting backend for django-haystack",
+    version="0.0.1",
+    packages=find_packages(),
+    author = "Alexey Kalinin",
+)
