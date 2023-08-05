9a75094132d2d6081e90bf0987bc2875ecedb83a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:48 +0000

    Add package jsonapi-mock-server/0.12/jsonapi_mock_server-0.12-py2-none-any.whl

---
+++ packages/jsonapi-mock-server/jsonapi_mock_server-0.12-py2-none-any.whl/sample_mock_server/resources/question.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+from jsonapi_mock_server.base_models import BaseResource
+
+
+class QuestionResource(BaseResource):
+    resource_type = 'Question'
+    attributes = {
+    }
+    relationships = {
+    }
