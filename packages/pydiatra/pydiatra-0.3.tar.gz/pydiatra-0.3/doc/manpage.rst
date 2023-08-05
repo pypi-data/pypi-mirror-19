30cea49fa9d8ab713f8b880c8aa31726df6748e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:16 +0000

    Add package pydiatra/0.3/pydiatra-0.3.tar.gz

---
+++ packages/pydiatra/pydiatra-0.3.tar.gz/pydiatra-0.3/doc/manpage.rst
@@ -0,0 +1,53 @@
+========
+pydiatra
+========
+
+------------------------------
+static checker for Python code
+------------------------------
+
+:manual section: 1
+:version: pydiatra 0.3
+:date: |date|
+
+.. |date| date:: %Y-%m-%d
+
+Synopsis
+--------
+For Python 2:
+
+| **py2diatra** [*options*] *pyfile* [*pyfile* …]
+| **python2.**\ *X* **-3tt** **-m** **pydiatra** [*options*] *pyfile* [*pyfile* …]
+
+For Python 3:
+
+| **py3diatra** [*options*] *pyfile* [*pyfile* …]
+| **python3.**\ *X* **-m** **pydiatra** [*options*] *pyfile* [*pyfile* …]
+
+Options
+-------
+
+-j n, --jobs n
+   Use *n* processes in parallel.
+   *n* can be a positive integer,
+   or ``auto`` to determine the number automatically.
+   The default is to use only a single process.
+-h, --help
+   Show the help message and exit.
+--version
+   Show the program's version number and exit.
+
+Description
+-----------
+**pydiatra** is a static checker for Python code.
+
+The following checks are implemented:
+
+.. include:: tags.rst
+
+See also
+--------
+**pyflakes**\ (1),
+**pylint**\ (1)
+
+.. vim:ts=3 sts=3 sw=3
---
+++ packages/pydiatra/pydiatra-0.3.tar.gz/pydiatra-0.3/pydiatra/main.py
@@ -0,0 +1,94 @@
+# encoding=UTF-8
+
+# Copyright © 2011-2016 Jakub Wilk <jwilk@jwilk.net>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the “Software”), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+'''
+pydiatra CLI
+'''
+
+from __future__ import print_function
+
+import argparse
+import io
+import multiprocessing
+import sys
+
+try:
+    import concurrent.futures
+except ImportError as concurrent_exc:
+    concurrent = False
+else:
+    concurrent_exc = None  # hi, pyflakes!
+
+from . import __version__
+from . import checks
+
+def check_file(path, file=sys.stdout):
+    for t in checks.check_file(path):
+        print(t, file=file)
+
+def check_file_s(path):
+    if str is bytes:
+        file = io.BytesIO()
+    else:
+        file = io.StringIO()
+    check_file(path, file=file)
+    return file.getvalue()
+
+def parse_jobs(s):
+    if s == 'auto':
+        try:
+            return multiprocessing.cpu_count()
+        except NotImplementedError:
+            return 1
+    n = int(s)
+    if n <= 0:
+        raise ValueError
+    return n
+parse_jobs.__name__ = 'jobs'
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('paths', metavar='<file>', nargs='+')
+    ap.add_argument('--version', action='version', version='%(prog)s {0}'.format(__version__))
+    ap.add_argument('-j', '--jobs', metavar='<n>', type=parse_jobs, default=1,
+        help=('use <n> processes' if concurrent else argparse.SUPPRESS)
+    )
+    options = ap.parse_args()
+    if len(options.paths) <= 1:
+        options.jobs = 1
+    if options.jobs > 1 and not concurrent:
+        print('{prog}: warning: cannot import concurrent.futures: {msg}'.format(prog=ap.prog, msg=concurrent_exc), file=sys.stderr)
+        options.jobs = 1
+    checks.load_data()
+    if options.jobs <= 1:
+        for path in options.paths:
+            check_file(path)
+    else:
+        executor = concurrent.futures.ProcessPoolExecutor(max_workers=options.jobs)
+        with executor:
+            for s in executor.map(check_file_s, options.paths):
+                sys.stdout.write(s)
+    sys.exit(0)
+
+__all__ = ['main']
+
+# vim:ts=4 sts=4 sw=4 et
