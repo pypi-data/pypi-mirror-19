6d94c90a0a16f6087bed3460a51528ee186c42b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:09 +0000

    Add package spotifylyrics/1.0/spotifylyrics-1.0.tar.gz

---
+++ packages/spotifylyrics/spotifylyrics-1.0.tar.gz/spotifylyrics-1.0/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name="spotifylyrics",
+	version="1.0",
+	description="Displays lyrics for spotify songs",
+	url="http://github.com",
+	author="Yask Srivastava",
+	author_email="yask123@gmail.com",
+	license='MIT',
+	packages=["spotifylyrics"],
+	scripts=["bin/spotifylyrics"],
+	zip_safe=False)
