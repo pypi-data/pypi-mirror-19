2847fdc76945db11f1bbdd052f7e47c9c2fc4c52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:15 +0000

    Add package vmhome/2.1.0/vmhome-2.1.0.zip

---
+++ packages/vmhome/vmhome-2.1.0.zip/vmhome-2.1.0/print_list.py
@@ -0,0 +1,11 @@
+import sys
+
+def print_list(list_item, indent = False, level = 0, output = sys.stdout):
+    for item in list_item:
+        if (isinstance(item, list)):
+            print_list(item, indent, level + 1, output)
+        else:
+            if indent:
+                for num in range(level):
+                    print("\t", end='', file = output)
+            print(item, file = output)
