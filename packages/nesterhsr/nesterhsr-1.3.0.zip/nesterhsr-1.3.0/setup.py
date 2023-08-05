e6d506bf88d98760ee14305a14f2a6b6b20429ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:48 +0000

    Add package nesterhsr/1.3.0/nesterhsr-1.3.0.zip

---
+++ packages/nesterhsr/nesterhsr-1.3.0.zip/nesterhsr-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup   #从发布工具导入setup函数
+
+setup(
+       name  = 'nesterhsr',
+	   version = '1.3.0',
+	   py_modules = ['nesterhsr'],
+	   author = 'hsr',
+	   author_email = '644743774@qq.com',
+	   url = 'http://www.headfirstlabs.com',
+	   description = 'a simple printer of nested lists'
+)
