35f46d1e04ea6932a7445553c5bb62667063e080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:38 +0000

    Add package hrs/1.1.5/hrs-1.1.5.tar.gz

---
+++ packages/hrs/hrs-1.1.5.tar.gz/hrs-1.1.5/config.py
@@ -0,0 +1,15 @@
+import os
+
+__version__ = "1.1.0"
+
+PACKAGE_PATH = os.path.dirname(__file__)
+
+WELCOME = """
+Welcome to use **HRS**.
+Author: Bo Zhang (bozhang@nao.cas.cn).
+"""
+
+EXTRA_COMMENTS = """
+# Github url: https://github.com/hypergravity/hrs
+# PYPI url: https://pypi.python.org/pypi/hrs
+"""
