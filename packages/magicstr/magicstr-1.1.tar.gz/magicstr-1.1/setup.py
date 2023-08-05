697d95a034a1e40e903f95c9e913efab8231d548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:45 +0000

    Add package magicstr/1.1/magicstr-1.1.tar.gz

---
+++ packages/magicstr/magicstr-1.1.tar.gz/magicstr-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'magicstr',
+    packages = ['magicstr'],
+    discription = 'Do some magic on your strings files.',
+    version = '1.01',
+    author = 'wombatwen',
+    author_email = 'wombatwen@gmail.com',
+    scripts = ['AutoNameString.py'],
+)
