a6c18648eb22bbbffe8e4cc4c657fd4c85e169d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:14 +0000

    Add package alignak_module_EXAMPLE/0.2.1/alignak_module_EXAMPLE-0.2.1.tar.gz

---
+++ packages/alignak_module_EXAMPLE/alignak_module_EXAMPLE-0.2.1.tar.gz/alignak_module_EXAMPLE-0.2.1/version.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+    Alignak - Broker module for the monitoring logs
+"""
+# Package name
+__pkg_name__ = u"alignak_module_EXAMPLE"
+
+# Module type for PyPI keywords
+# Used for:
+# - PyPI keywords
+__module_types__ = u"example"
+
+# Application manifest
+__version__ = u"0.2.1"
+__author__ = u"Alignak team"
+__author_email__ = u"frederic.mohier@gmail.com"
+__copyright__ = u"(c) 2015-2016 - %s" % __author__
+__license__ = u"GNU Affero General Public License, version 3"
+__url__ = u"https://github.com/Alignak-monitoring/alignak-module-example"
+__description__ = u"Alignak - Alignak module as an example to build other modules"
