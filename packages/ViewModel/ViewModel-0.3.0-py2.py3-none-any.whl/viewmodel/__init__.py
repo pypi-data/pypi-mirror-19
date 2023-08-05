117af7a5707f862b28a9c5198bb83fab904bfb08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:37 +0000

    Add package ViewModel/0.3.0/ViewModel-0.3.0-py2.py3-none-any.whl

---
+++ packages/ViewModel/ViewModel-0.3.0-py2.py3-none-any.whl/viewmodel/__init__.py
@@ -0,0 +1,7 @@
+from . import viewModel, viewFields, viewMongoDB
+from .viewModel import BaseView, ViewRow
+from .viewFields import (
+    Case, IdField, TxtField, IntField, DecField, ObjDictField,
+    DateField, TimeField, EnumField, EnumForeignField,
+    TxtListField, ObjListField
+    )
