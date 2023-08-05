0ef04f0bb3050f93e1323e4035eaf381ca7f74f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:11 +0000

    Add package guizero/0.2.0/GUIzero-0.2.0-py3-none-any.whl

---
+++ packages/guizero/GUIzero-0.2.0-py3-none-any.whl/guizero/App.py
@@ -0,0 +1,31 @@
+try:
+    from tkinter import *
+except ImportError:
+    print("tkinter did not import successfully. Please check your setup.")
+
+from . import utilities as utils
+
+class App(Tk):
+
+    def __init__(self, title="GUIzero", width=500, height=500, layout="auto"):
+
+        try:
+            super().__init__()
+        except:            
+            utils.error_format("Failed to initialise [App] object")
+
+        # Initial setup
+        self.title( str(title) )
+        self.geometry(str(width)+"x"+str(height))
+        self.layout_manager = layout  		# Only behaves differently if equals "grid"
+    
+    
+    # Alias of mainloop with friendlier name
+    def display(self):
+        super().mainloop()
+
+    # Set the title of the window
+    def set_title(self, title):
+        self.title( str(title) )
+
+   
