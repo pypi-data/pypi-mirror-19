bf13333e6d864de9e149629b65477f3f73ecf891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:04 +0000

    Add package filesystems/0.5.0/filesystems-0.5.0.tar.gz

---
+++ packages/filesystems/filesystems-0.5.0.tar.gz/filesystems-0.5.0/filesystems/_path.py
@@ -0,0 +1,42 @@
+import os
+
+from characteristic import Attribute, attributes
+from pyrsistent import pvector
+
+
+@attributes(
+    [Attribute(name="segments", exclude_from_init=True)],
+)
+class Path(object):
+    def __init__(self, *segments):
+        self.segments = pvector(segments)
+
+    def __str__(self):
+        return os.sep + os.sep.join(self.segments)
+
+    @classmethod
+    def cwd(cls):
+        return cls.from_string(os.getcwd())
+
+    @classmethod
+    def root(cls):
+        return cls()
+
+    @classmethod
+    def from_string(cls, path):
+        """
+        Create a path out of an OS-specific string.
+
+        """
+
+        segments = os.path.abspath(path).lstrip(os.sep).split(os.sep)
+        return cls(*segments)
+
+    def basename(self):
+        return (self.segments or [""])[-1]
+
+    def descendant(self, *segments):
+        return self.__class__(*self.segments.extend(segments))
+
+    def parent(self):
+        return self.__class__(*self.segments[:-1])
