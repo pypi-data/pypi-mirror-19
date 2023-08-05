0aa6e111dbc2ea2ec8ef01f930661b5c075cb40e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:25 +0000

    Add package hallgrim/0.3/hallgrim-0.3.tar.gz

---
+++ packages/hallgrim/hallgrim-0.3.tar.gz/hallgrim-0.3/hallgrim/templates.py
@@ -0,0 +1,13 @@
+scaffolding = r'''
+meta = {{
+    'author': '{}',
+    'title': '{}',
+    'type': '{}',
+    'points': {},
+}}
+
+task = """ decription """
+{}
+feedback = """ decription """
+'''
+
