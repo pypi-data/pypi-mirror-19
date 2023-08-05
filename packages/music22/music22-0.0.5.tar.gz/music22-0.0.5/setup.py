99a7d08a472b2127be2a9ca5404853f86ce49e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:25 +0000

    Add package music22/0.0.5/music22-0.0.5.tar.gz

---
+++ packages/music22/music22-0.0.5.tar.gz/music22-0.0.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='music22',
+      version='0.0.5',
+      description='A tool for musicological analysis from audio files. Now it is focused on modal music analysis : Scale analysis, tonic detection',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Multimedia :: Sound/Audio :: Analysis',
+      ],
+      keywords='musicology analysis from non-symbolic data',
+      url='https://gitlab.com/AnasGhrab/music22',
+      author='Anas Ghrab',
+      author_email='anas.ghrab@gmail.com',
+      license='MIT',
+      packages=['music22',
+		],
+      install_requires=[
+          'numpy','matplotlib','scipy','pandas'
+      ],
+      zip_safe=False)
