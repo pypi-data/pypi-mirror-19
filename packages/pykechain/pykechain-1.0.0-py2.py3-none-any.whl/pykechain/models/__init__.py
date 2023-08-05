9fc9343885067d12a285b735801b035b2b80b6c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:35 +0000

    Add package pykechain/1.0.0/pykechain-1.0.0-py2.py3-none-any.whl

---
+++ packages/pykechain/pykechain-1.0.0-py2.py3-none-any.whl/pykechain/models/__init__.py
@@ -0,0 +1,17 @@
+from .base import Base
+from .scope import Scope
+from .activity import Activity
+from .part import Part
+from .property import Property
+from .attachment import AttachmentProperty
+from .partset import PartSet
+
+__all__ = [
+    'Base',
+    'Scope',
+    'Activity',
+    'Part',
+    'Property',
+    'AttachmentProperty',
+    'PartSet'
+]
