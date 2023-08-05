89f3089c66f03dab7e5ae9366e16bf697535ea6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:53 +0000

    Add package ShockCalendar/0.2.1/ShockCalendar-0.2.1.zip

---
+++ packages/ShockCalendar/ShockCalendar-0.2.1.zip/ShockCalendar-0.2.1/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+from setuptools import setup;
+
+
+
+setup(name="ShockCalendar",
+    version="0.2.1",
+    description="Un widget calendario para usar en Tkinter",
+    long_description="Un módulo con un objeto calendario widget dibujado con \
+canvas que permite al usuario seleccionar cualquier fechas e introducir fechas de eventos\
+con información.\n Muy facil de usar y incluye ejemplos.",
+    author="adrian_leonardo_05 at hotmail.com.ar",
+    author_email="adrian_leonardo_05@hotmail.com.ar",
+    url="http://foro.el-hacker.com/miembros/sokoleonardo/",
+    license="MIT",
+    packages=["ShockCalendar"],
+    keywords=["Tkinter", "widget", "calendar", "date", "time", "canvas", "GUI"]
+    );
