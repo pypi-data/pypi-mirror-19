3b5cb95286e4b23fa40759fd2c6817890e5770f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package infi.sysv_service/0.1.13/infi.sysv_service-0.1.13-py2.7.egg

---
+++ packages/infi.sysv_service/infi.sysv_service-0.1.13-py2.7.egg/infi/sysv_service/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.13"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.1.13)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'c60e82f10345b71877ca1a6994164b72e328e555'
+__git_head_subject__ = 'Finished Release v0.1.13'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
