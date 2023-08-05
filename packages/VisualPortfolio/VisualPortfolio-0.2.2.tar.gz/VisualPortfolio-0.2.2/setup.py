0bee150cffb11b853d5657106068b74689936367
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:19 +0000

    Add package VisualPortfolio/0.2.2/VisualPortfolio-0.2.2.tar.gz

---
+++ packages/VisualPortfolio/VisualPortfolio-0.2.2.tar.gz/VisualPortfolio-0.2.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+u"""
+Created on 2015-11-10
+
+@author: cheng.li
+"""
+import sys
+import io
+from setuptools import setup
+
+PACKAGE = "VisualPortfolio"
+NAME = "VisualPortfolio"
+VERSION = "0.2.2"
+DESCRIPTION = "VisualPortfolio " + VERSION
+AUTHOR = "cheng li"
+AUTHOR_EMAIL = "wegamekinglc@hotmail.com"
+URL = 'https://github.com/ChinaQuants/VisualPortfolio'
+
+if sys.version_info > (3, 0, 0):
+    requirements = "requirements/py3.txt"
+else:
+    requirements = "requirements/py2.txt"
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    packages=['VisualPortfolio'],
+    py_modules=['VisualPortfolio.__init__'],
+    install_requires=io.open(requirements, encoding='utf8').read(),
+)
