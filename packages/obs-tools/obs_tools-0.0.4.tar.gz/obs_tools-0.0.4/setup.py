01cbd0eaa008f187c7c778f866fd52d479f63101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:06 +0000

    Add package obs-tools/0.0.4/obs_tools-0.0.4.tar.gz

---
+++ packages/obs-tools/obs_tools-0.0.4.tar.gz/obs_tools-0.0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="obs_tools",
+    version="0.0.4",
+    author="unfoldingWord",
+    author_email="phillip_hopper@wycliffeassociates.org",
+    description="A collection of useful scripts",
+    license="MIT",
+    keywords="unfoldingWord obs tools",
+    url="https://github.org/unfoldingWord-dev/obs_tools",
+    packages=['obs'],
+    long_description='A collection of Python scripts that have proven useful and have been reused.',
+    classifiers=[]
+)
