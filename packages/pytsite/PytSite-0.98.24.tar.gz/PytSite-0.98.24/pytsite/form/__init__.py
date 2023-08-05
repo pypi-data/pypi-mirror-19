fec43516bf75960343c7a2d6ebc73d19a61af56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:02 +0000

    Add package pytsite/0.98.24/PytSite-0.98.24.tar.gz

---
+++ packages/pytsite/PytSite-0.98.24.tar.gz/PytSite-0.98.24/pytsite/form/__init__.py
@@ -0,0 +1,22 @@
+"""Pytsite Form Package.
+"""
+# Public API
+from . import _error as error
+from ._form import Form
+
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+def _init():
+    from pytsite import assetman, tpl, lang, http_api
+
+    lang.register_package(__name__)
+    assetman.register_package(__name__)
+    tpl.register_package(__name__)
+    http_api.register_handler('form', 'pytsite.form.http_api')
+
+
+_init()
