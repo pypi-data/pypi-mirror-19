4723127775fd15ec6032423e4d779ff27538e7cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:54 +0000

    Add package irs/2.6.13/irs-2.6.13.tar.gz

---
+++ packages/irs/irs-2.6.13.tar.gz/irs-2.6.13/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='irs',
+    version='2.6.13',
+    description='A music downloader that just gets metadata.',
+    url='https://github.com/kepoorhampond/irs',
+    author='Kepoor Hampond',
+    author_email='kepoorh@gmail.com',
+    license='GNU',
+    packages =['irs'],
+    install_requires=[
+      'bs4',
+      'mutagen',
+      'youtube-dl',
+      'requests',
+    ],
+    entry_points = {
+      'console_scripts': ['irs = irs.__main__:main'],
+    },
+)
