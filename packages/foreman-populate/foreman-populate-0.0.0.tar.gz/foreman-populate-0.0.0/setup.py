fd37506fb730a5ecbf34d72ba57b2ce326e1bd2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:27 +0000

    Add package foreman-populate/0.0.0/foreman-populate-0.0.0.tar.gz

---
+++ packages/foreman-populate/foreman-populate-0.0.0.tar.gz/foreman-populate-0.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='foreman-populate',
+    version='0.0.0',
+    description='Using nailgun reads YAML description and populate a system',
+    long_description='Reads definition from YAML file to populate the system using nailgun',
+    url='https://github.com/SatelliteQE/foreman-populate',
+    author='Bruno Rocha',
+    author_email='rochacbruno@gmail.com',
+    license='GPLv3',
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        ('License :: OSI Approved :: GNU General Public License v3 or later '
+         '(GPLv3+)'),
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'nailgun',
+        'import_string',
+        'manage'
+    ],
+)
