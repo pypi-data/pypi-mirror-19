99ffcb25a8c78c71f96c7c32c1153b0e61cbc050
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:00 +0000

    Add package begin_v/1.0.0/begin_v-1.0.0.zip

---
+++ packages/begin_v/begin_v-1.0.0.zip/begin_v-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name        = 'begin_v',
+        version     = '1.0.0',
+        py_modules  = ['begin_v'],
+        author      = 'gnsseei',
+        author_email = 'gnsseei@gmail.com',
+        url         = 'http://www.headfirstlabs.com',
+        description = 'module creation',
+    )
