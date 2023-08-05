5fbc2d4f9e01e60becd160afada553420f958bc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:39 +0000

    Add package gbx_nester/1.0.0/gbx_nester-1.0.0.zip

---
+++ packages/gbx_nester/gbx_nester-1.0.0.zip/gbx_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'gbx_nester',
+    version='1.0.0',
+    py_modules = ['gbx_nester'],
+    author='Gbingxue',
+    author_email='Gbingxue1227@163.com',
+    url='http://www.headfirstlabs.com',
+    description='a simple test',
+)
