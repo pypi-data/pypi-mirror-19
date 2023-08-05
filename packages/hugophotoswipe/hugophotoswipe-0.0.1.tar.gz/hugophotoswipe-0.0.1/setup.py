8f5c28b57850c0055b846e0f610e951ae67cc4bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:51 +0000

    Add package hugophotoswipe/0.0.1/hugophotoswipe-0.0.1.tar.gz

---
+++ packages/hugophotoswipe/hugophotoswipe-0.0.1.tar.gz/hugophotoswipe-0.0.1/setup.py
@@ -0,0 +1,32 @@
+
+import re
+from setuptools import setup
+
+version = re.search("__version__ = '([^']+)'", 
+        open('hugophotoswipe/__init__.py').read()).group(1)
+
+setup(
+        name="hugophotoswipe",
+        version=version,
+        description="Tool for creating and managing PhotoSwipe albums in Hugo",
+        url="https://www.github.com/GjjvdBurg/HugoPhotoSwipe",
+        author="Gertjan van den Burg",
+        author_email="gertjanvandenburg@gmail.com",
+        license="GPL v3",
+        packages=["hugophotoswipe"],
+        scripts=['bin/hps'],
+        include_package_data=True,
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3 :: Only',
+            'Programming Language :: Python :: 3.0',
+            'Programming Language :: Python :: 3.1',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            ],
+        )
