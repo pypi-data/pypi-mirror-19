975d8b3d16c0decce12d08b726ffe1c9b1e5c5c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:49 +0000

    Add package singularity/0.47/singularity-0.47.tar.gz

---
+++ packages/singularity/singularity-0.47.tar.gz/singularity-0.47/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+setup(
+    # Application name:
+    name="singularity",
+
+    # Version number:
+    version="0.47",
+
+    # Application author details:
+    author="Vanessa Sochat",
+    author_email="vsochat@stanford.edu",
+
+    # Packages
+    packages=find_packages(),
+ 
+    # Data files
+    include_package_data=True,
+    zip_safe=False,
+
+    # Details
+    url="http://www.github.com/singularityware/singularity-python",
+
+    license="LICENSE",
+    description="Command line tool for working with singularity-hub and packaging singularity containers.",
+    keywords='singularity containers hub reproducibility package science',
+
+    install_requires = ['Flask','gitpython','flask-restful','selenium','simplejson','pygments',
+                        'requests','oauth2client','google-api-python-client'],
+
+    entry_points = {
+        'console_scripts': [
+            'shub=singularity.scripts:main',
+        ],
+    },
+
+)
