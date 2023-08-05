9acfc5d38b9d47d839e25dc5459c7a836666caca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:48 +0000

    Add package hydra-fa/0.6/hydra-fa-0.6.tar.gz

---
+++ packages/hydra-fa/hydra-fa-0.6.tar.gz/hydra-fa-0.6/automationserver/main.py
@@ -0,0 +1,44 @@
+import os
+import automationserver.server as aut
+
+import logging
+import sys
+import argparse
+
+import fieldagent.common as fa_common
+
+def main():
+
+    root = logging.getLogger()
+    root.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    root.addHandler(ch)
+    root.info('Start automation server ...')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", help="Automation server host of field agent.", type=str, default='localhost')
+    parser.add_argument("--visa", help="PyVISA visa backend.", type=str, default='@py')
+    parser.add_argument("--exchange", help="RabbitMQ exchange for RPC.", type=str, default='')
+    parser.add_argument("-v", "--verbose", help="Increase output verbosity.", action="store_true")
+    args = parser.parse_args()
+
+    # Setup logging
+    if args.verbose:
+        root = fa_common.setupLogging(logging.DEBUG)
+    else:
+        root = fa_common.setupLogging(logging.INFO)
+
+    server = aut.VisaRpcServer(
+        host=args.host,
+        visa_library=args.visa,
+        exchange=args.exchange)
+
+    try:
+        server.start()
+    except KeyboardInterrupt:
+        logging.info('Closed by user.')
+
+    server.stop()
