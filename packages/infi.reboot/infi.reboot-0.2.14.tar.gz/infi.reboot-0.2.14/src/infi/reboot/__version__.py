05dd73b69f2ad36fb7d87576aa86307da49e18e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:23 +0000

    Add package infi.reboot/0.2.14/infi.reboot-0.2.14.tar.gz

---
+++ packages/infi.reboot/infi.reboot-0.2.14.tar.gz/infi.reboot-0.2.14/src/infi/reboot/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.2.14"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.2.14)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '5252a8432907c9c69c581b4a0a760a0ecce75a6c'
+__git_head_subject__ = 'Finished Release v0.2.14'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
+__git_commit_date__ = '2016-12-18 14:21:11'
