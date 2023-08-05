0041f1d4cf4c026183ab8e85dbccb75d3c3d970e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:44 +0000

    Add package stance/1.1/stance-1.1.tar.gz

---
+++ packages/stance/stance-1.1.tar.gz/stance-1.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(
+    name='stance',
+    version='1.1',
+    description='stance provides self-instantiating worker processes for Python 3',
+    author='Christoph Jansen',
+    author_email='christoph@gnork.org',
+    url='https://github.com/curious-containers/stance',
+    packages=['stance'],
+    license='Apache License 2.0',
+    platforms=['any'],
+    install_requires=[]
+)
