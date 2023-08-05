f279918b18be6c5a7c79dfc24e0323999d1b2607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:57 +0000

    Add package python-terraform/0.8.0/python-terraform-0.8.0.tar.gz

---
+++ packages/python-terraform/python-terraform-0.8.0.tar.gz/python-terraform-0.8.0/python_terraform/__init__.py
@@ -0,0 +1,259 @@
+# -*- coding: utf-8 -*-
+# above is for compatibility of python2.7.11
+
+import subprocess
+import os
+import json
+import logging
+import tempfile
+
+from python_terraform.tfstate import Tfstate
+
+log = logging.getLogger(__name__)
+
+
+class IsFlagged:
+    pass
+
+
+class IsNotFlagged:
+    pass
+
+
+class Terraform(object):
+    """
+    Wrapper of terraform command line tool
+    https://www.terraform.io/
+    """
+
+    def __init__(self, working_dir=None,
+                 targets=None,
+                 state=None,
+                 variables=None,
+                 parallelism=None,
+                 var_file=None,
+                 terraform_bin_path=None):
+        """
+        :param working_dir: the folder of the working folder, if not given, will be where python
+        :param targets: list of target
+        :param state: path of state file relative to working folder
+        :param variables: variables for apply/destroy/plan command
+        :param parallelism: parallelism for apply/destroy command
+        :param var_file: passed as value of -var-file option, could be string or list
+            list stands for multiple -var-file option
+        :param terraform_bin_path: binary path of terraform
+        """
+        self.working_dir = working_dir
+        self.state = state
+        self.targets = [] if targets is None else targets
+        self.variables = dict() if variables is None else variables
+        self.parallelism = parallelism
+        self.terraform_bin_path = terraform_bin_path \
+            if terraform_bin_path else 'terraform'
+        self.var_file = var_file
+        self.temp_var_files = VaribleFiles()
+
+        # store the tfstate data
+        self.tfstate = None
+        self.read_state_file(self.state)
+
+    def __getattr__(self, item):
+        def wrapper(*args, **kwargs):
+            print('called with %r and %r' % (args, kwargs))
+            return self.cmd(item, *args, **kwargs)
+
+        return wrapper
+
+    def apply(self, dir_or_plan=None, **kwargs):
+        """
+        refer to https://terraform.io/docs/commands/apply.html
+        no-color is flagged by default
+        :param dir_or_plan: folder relative to working folder
+        :param kwargs: same as kwags in method 'cmd'
+        :returns return_code, stdout, stderr
+        """
+        default = dict()
+        args, option_dict = self._create_cmd_args(dir_or_plan, default, kwargs)
+
+        return self.cmd('apply', *args, **option_dict)
+
+    def _create_cmd_args(self, dir_or_plan, default_dict, kwargs):
+        option_dict = default_dict
+        option_dict['state'] = self.state
+        option_dict['target'] = self.targets
+        option_dict['var'] = self.variables
+        option_dict['var_file'] = self.var_file
+        option_dict['parallelism'] = self.parallelism
+        option_dict['no_color'] = IsFlagged
+        option_dict['input'] = False
+        option_dict.update(kwargs)
+        args = [dir_or_plan] if dir_or_plan else []
+        return args, option_dict
+
+    def destroy(self, dir_or_plan=None, **kwargs):
+        """
+        refer to https://www.terraform.io/docs/commands/destroy.html
+        force/no-color option is flagged by default
+        :return: ret_code, stdout, stderr
+        """
+        default = {'force': IsFlagged}
+        args, option_dict = self._create_cmd_args(dir_or_plan, default, kwargs)
+        return self.cmd('destroy', *args, **option_dict)
+
+    def generate_cmd_string(self, cmd, *args, **kwargs):
+        """
+        for any generate_cmd_string doesn't written as public method of terraform
+
+        examples:
+        1. call import command,
+        ref to https://www.terraform.io/docs/commands/import.html
+        --> generate_cmd_string call:
+                terraform import -input=true aws_instance.foo i-abcd1234
+        --> python call:
+                tf.generate_cmd_string('import', 'aws_instance.foo', 'i-abcd1234', input=True)
+
+        2. call apply command,
+        --> generate_cmd_string call:
+                terraform apply -var='a=b' -var='c=d' -no-color the_folder
+        --> python call:
+                tf.generate_cmd_string('apply', the_folder, no_color=IsFlagged, var={'a':'b', 'c':'d'})
+
+        :param cmd: command and sub-command of terraform, seperated with space
+                    refer to https://www.terraform.io/docs/commands/index.html
+        :param args: arguments of a command
+        :param kwargs: same as kwags in method 'cmd'
+        :return: string of valid terraform command
+        """
+        cmds = cmd.split()
+        cmds = [self.terraform_bin_path] + cmds
+
+        for k, v in kwargs.items():
+            if '_' in k:
+                k = k.replace('_', '-')
+
+            if type(v) is list:
+                for sub_v in v:
+                    cmds += ['-{k}={v}'.format(k=k, v=sub_v)]
+                continue
+
+            # right now we assume only variables will be passed as dict
+            # since map type sent in string won't work, create temp var file for
+            # variables, and clean it up later
+            if type(v) is dict:
+                filename = self.temp_var_files.create(v)
+                cmds += ['-var-file={0}'.format(filename)]
+                continue
+
+            # simple flag,
+            if v is IsFlagged:
+                cmds += ['-{k}'.format(k=k)]
+                continue
+
+            if v is IsNotFlagged:
+                continue
+
+            if not v:
+                continue
+
+            if type(v) is bool:
+                v = 'true' if v else 'false'
+
+            cmds += ['-{k}={v}'.format(k=k, v=v)]
+
+        cmds += args
+        cmd = ' '.join(cmds)
+        return cmd
+
+    def cmd(self, cmd, *args, **kwargs):
+        """
+        run a terraform command, if success, will try to read state file
+        :param cmd: command and sub-command of terraform, seperated with space
+                    refer to https://www.terraform.io/docs/commands/index.html
+        :param args: arguments of a command
+        :param kwargs:  any option flag with key value without prefixed dash character
+                if there's a dash in the option name, use under line instead of dash,
+                    ex. -no-color --> no_color
+                if it's a simple flag with no value, value should be IsFlagged
+                    ex. cmd('taint', allow＿missing=IsFlagged)
+                if it's a boolean value flag, assign True or false
+                if it's a flag could be used multiple times, assign list to it's value
+                if it's a "var" variable flag, assign dictionary to it
+                if a value is None, will skip this option
+        :return: ret_code, out, err
+        """
+        cmd_string = self.generate_cmd_string(cmd, *args, **kwargs)
+        log.debug('command: {c}'.format(c=cmd_string))
+
+        working_folder = self.working_dir if self.working_dir else None
+
+        p = subprocess.Popen(cmd_string, stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE, shell=True,
+                             cwd=working_folder)
+        out, err = p.communicate()
+        ret_code = p.returncode
+        log.debug('output: {o}'.format(o=out))
+
+        if ret_code == 0:
+            self.read_state_file()
+        else:
+            log.warn('error: {e}'.format(e=err))
+
+        self.temp_var_files.clean_up()
+        return ret_code, out.decode('utf-8'), err.decode('utf-8')
+
+    def output(self, name):
+        """
+        https://www.terraform.io/docs/commands/output.html
+        :param name: name of output
+        :return: output value
+        """
+        ret, out, err = self.cmd('output', name, json=IsFlagged)
+
+        log.debug('output raw string: {0}'.format(out))
+        if ret != 0:
+            return None
+        out = out.lstrip()
+
+        output_dict = json.loads(out)
+        return output_dict['value']
+
+    def read_state_file(self, file_path=None):
+        """
+        read .tfstate file
+        :param file_path: relative path to working dir
+        :return: states file in dict type
+        """
+
+        if not file_path:
+            file_path = self.state
+
+        if not file_path:
+            file_path = 'terraform.tfstate'
+
+        if self.working_dir:
+            file_path = os.path.join(self.working_dir, file_path)
+
+        self.tfstate = Tfstate.load_file(file_path)
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.temp_var_files.clean_up()
+
+
+class VaribleFiles(object):
+    def __init__(self):
+        self.files = []
+
+    def create(self, variables):
+        with tempfile.NamedTemporaryFile('w+t', delete=False) as temp:
+            logging.debug('{0} is created'.format(temp.name))
+            self.files.append(temp)
+            temp.write(json.dumps(variables))
+            file_name = temp.name
+
+        return file_name
+
+    def clean_up(self):
+        for f in self.files:
+            os.unlink(f.name)
+
+        self.files = []
---
+++ packages/python-terraform/python-terraform-0.8.0.tar.gz/python-terraform-0.8.0/test/test_terraform.py
@@ -0,0 +1,121 @@
+from python_terraform import *
+import pytest
+import os
+import logging
+import re
+
+logging.basicConfig(level=logging.DEBUG)
+current_path = os.path.dirname(os.path.realpath(__file__))
+
+STRING_CASES = [
+     [
+         lambda x: x.generate_cmd_string('apply', 'the_folder',
+                                         no_color=IsFlagged),
+         "terraform apply -no-color the_folder"
+     ],
+     [
+         lambda x: x.generate_cmd_string('push', 'path', vcs=True,
+                                         token='token',
+                                         atlas_address='url'),
+         "terraform push -vcs=true -token=token -atlas-address=url path"
+     ],
+ ]
+
+CMD_CASES = [
+    ['method', 'expected_output'],
+    [
+        [
+            lambda x: x.cmd('plan', 'var_to_output', no_color=IsFlagged, var={'test_var': 'test'}) ,
+            "doesn't need to do anything"
+        ]
+    ]
+]
+
+
+class TestTerraform(object):
+    def teardown_method(self, method):
+        """ teardown any state that was previously setup with a setup_method
+        call.
+        """
+
+        def purge(dir, pattern):
+            for f in os.listdir(dir):
+                if re.search(pattern, f):
+                    if os.path.isfile(f):
+                        os.remove(os.path.join(dir, f))
+
+        purge('.', '.tfstate')
+
+    @pytest.mark.parametrize([
+                 "method", "expected"
+             ], STRING_CASES)
+    def test_generate_cmd_string(self, method, expected):
+        tf = Terraform(working_dir=current_path)
+        result = method(tf)
+
+        strs = expected.split()
+        for s in strs:
+            assert s in result
+
+    @pytest.mark.parametrize(*CMD_CASES)
+    def test_cmd(self, method, expected_output):
+        tf = Terraform(working_dir=current_path)
+        ret, out, err = method(tf)
+        assert expected_output in out
+        assert ret == 0
+
+    @pytest.mark.parametrize(
+        ("folder", "variables", "var_files", "expected_output"),
+        [
+            ("var_to_output", {'test_var': 'test'}, None, "test_output=test"),
+            ("var_to_output", {'test_list_var': ['c', 'd']}, None, "test_list_output=[c,d]"),
+            ("var_to_output", {'test_map_var': {"c": "c", "d": "d"}}, None, "test_map_output={a=ab=bc=cd=d}"),
+            ("var_to_output", {'test_map_var': {"c": "c", "d": "d"}}, 'var_to_output/test_map_var.json', "test_map_output={a=ab=bc=cd=de=ef=f}")
+        ])
+    def test_apply(self, folder, variables, var_files, expected_output):
+        tf = Terraform(working_dir=current_path, variables=variables, var_file=var_files)
+        ret, out, err = tf.apply(folder)
+        assert ret == 0
+        assert expected_output in out.replace('\n', '').replace(' ', '')
+        assert err == ''
+
+
+    def test_state_data(self):
+        cwd = os.path.join(current_path, 'test_tfstate_file')
+        tf = Terraform(working_dir=cwd, state='tfstate.test')
+        tf.read_state_file()
+        assert tf.tfstate.modules[0]['path'] == ['root']
+
+    def test_pre_load_state_data(self):
+        cwd = os.path.join(current_path, 'test_tfstate_file')
+        tf = Terraform(working_dir=cwd, state='tfstate.test')
+        assert tf.tfstate.modules[0]['path'] == ['root']
+
+    @pytest.mark.parametrize(
+        ("folder", 'variables'),
+        [
+            ("var_to_output", {'test_var': 'test'})
+        ]
+    )
+    def test_override_default(self, folder, variables):
+        tf = Terraform(working_dir=current_path, variables=variables)
+        ret, out, err = tf.apply(folder, var={'test_var': 'test2'},
+                                 no_color=IsNotFlagged)
+        out = out.replace('\n', '')
+        assert '\x1b[0m\x1b[1m\x1b[32mApply' in out
+
+    def test_get_output(self):
+        tf = Terraform(working_dir=current_path, variables={'test_var': 'test'})
+        tf.apply('var_to_output')
+        assert tf.output('test_output') == 'test'
+
+    def test_destroy(self):
+        tf = Terraform(working_dir=current_path, variables={'test_var': 'test'})
+        ret, out, err = tf.destroy('var_to_output')
+        assert ret == 0
+        assert 'Destroy complete! Resources: 0 destroyed.' in out
+
+    def test_fmt(self):
+        tf = Terraform(working_dir=current_path, variables={'test_var': 'test'})
+        ret, out, err = tf.fmt(diff=True)
+        assert ret == 0
