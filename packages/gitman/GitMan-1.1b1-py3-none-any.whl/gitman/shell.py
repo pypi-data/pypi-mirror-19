04095e788c3c000cd572a424bfdaaa634ef0f10e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:29 +0000

    Add package gitman/1.1b1/GitMan-1.1b1-py3-none-any.whl

---
+++ packages/gitman/GitMan-1.1b1-py3-none-any.whl/gitman/shell.py
@@ -0,0 +1,75 @@
+"""Utilities to call shell programs."""
+
+import os
+import subprocess
+import logging
+
+from . import common
+from .exceptions import ShellError
+
+CMD_PREFIX = "$ "
+OUT_PREFIX = "> "
+
+log = logging.getLogger(__name__)
+
+
+def call(name, *args, _show=True, _ignore=False):
+    """Call a shell program with arguments.
+
+    :param name: name of program to call
+    :param args: list of command-line arguments
+    :param _show: display the call on stdout
+    :param _ignore: ignore non-zero return codes
+
+    """
+    program = CMD_PREFIX + ' '.join([name, *args])
+    if _show:
+        common.show(program, color='shell')
+    else:
+        log.debug(program)
+
+    if name == 'cd':
+        return os.chdir(args[0])  # 'cd' has no effect in a subprocess
+
+    command = subprocess.run(
+        [name, *args], universal_newlines=True,
+        stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+    )
+
+    for line in command.stdout.splitlines():
+        log.debug(OUT_PREFIX + line.strip())
+
+    if command.returncode == 0:
+        return command.stdout.strip()
+
+    elif _ignore:
+        log.debug("Ignored error from call to '%s'", program)
+
+    else:
+        message = (
+            "An external program call failed." + "\n\n"
+            "In working directory: " + os.getcwd() + "\n\n"
+            "The following command produced a non-zero return code:" + "\n\n" +
+            program + "\n" +
+            command.stdout
+        )
+        raise ShellError(message)
+
+
+def mkdir(path):
+    call('mkdir', '-p', path)
+
+
+def cd(path, _show=True):
+    call('cd', path, _show=_show)
+
+
+def ln(source, target):
+    dirpath = os.path.dirname(target)
+    if not os.path.isdir(dirpath):
+        mkdir(dirpath)
+    call('ln', '-s', source, target)
+
+
+def rm(path):
+    call('rm', '-rf', path)
---
+++ packages/gitman/GitMan-1.1b1-py3-none-any.whl/gitman/tests/test_shell.py
@@ -0,0 +1,69 @@
+# pylint: disable=no-self-use,misplaced-comparison-constant
+
+from unittest.mock import patch, Mock
+
+import pytest
+
+from gitman import shell
+from gitman.exceptions import ShellError
+
+from . import assert_calls
+
+
+class TestCall:
+
+    """Tests for interacting with the shell."""
+
+    @patch('os.chdir')
+    def test_cd(self, mock_chdir):
+        """Verify directories are changed correctly."""
+        shell.call('cd', 'mock/name')
+        mock_chdir.assert_called_once_with('mock/name')
+
+    def test_other_error_uncaught(self):
+        """Verify program errors raise exceptions."""
+        with pytest.raises(ShellError):
+            shell.call('git', '--invalid-git-argument')
+
+    def test_other_error_ignored(self):
+        """Verify program errors can be ignored."""
+        shell.call('git', '--invalid-git-argument', _ignore=True)
+
+    def test_other_capture(self):
+        """Verify a program's output can be captured."""
+        stdout = shell.call('echo', 'Hello, world!\n')
+        assert "Hello, world!" == stdout
+
+
+@patch('gitman.shell.call')
+class TestPrograms:
+
+    """Tests for calls to shell programs."""
+
+    def test_mkdir(self, mock_call):
+        """Verify the commands to create directories."""
+        shell.mkdir('mock/name/path')
+        assert_calls(mock_call, ["mkdir -p mock/name/path"])
+
+    def test_cd(self, mock_call):
+        """Verify the commands to change directories."""
+        shell.cd('mock/name/path')
+        assert_calls(mock_call, ["cd mock/name/path"])
+
+    @patch('os.path.isdir', Mock(return_value=True))
+    def test_ln(self, mock_call):
+        """Verify the commands to create symbolic links."""
+        shell.ln('mock/target', 'mock/source')
+        assert_calls(mock_call, ["ln -s mock/target mock/source"])
+
+    @patch('os.path.isdir', Mock(return_value=False))
+    def test_ln_missing_parent(self, mock_call):
+        """Verify the commands to create symbolic links (missing parent)."""
+        shell.ln('mock/target', 'mock/source')
+        assert_calls(mock_call, ["mkdir -p mock",
+                                 "ln -s mock/target mock/source"])
+
+    def test_rm(self, mock_call):
+        """Verify the commands to delete files/folders."""
+        shell.rm('mock/name/path')
+        assert_calls(mock_call, ["rm -rf mock/name/path"])
