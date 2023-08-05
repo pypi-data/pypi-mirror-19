ab037c224d1bee1da5e22f2a96a3c7b4edea4878
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:45 +0000

    Add package diskutil_parser/1.0.0/diskutil_parser-1.0.0.tar.gz

---
+++ packages/diskutil_parser/diskutil_parser-1.0.0.tar.gz/diskutil_parser-1.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+setup(
+    name="diskutil_parser",
+    version="1.0.0",
+    packages=find_packages(),
+
+    # metadata for upload to PyPI
+    author="Kenzie Togami",
+    author_email="ket1999@gmail.com",
+    description="Parses diskutil output on macOS",
+    license="MIT",
+    keywords="diskutil",
+    url="https://github.com/kenzierocks/diskutil_parser",
+)
