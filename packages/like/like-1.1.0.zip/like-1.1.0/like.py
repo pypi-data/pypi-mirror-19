36acc74d3f92d1f87705ee6c3f767c254569d0ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:49 +0000

    Add package like/1.1.0/like-1.1.0.zip

---
+++ packages/like/like-1.1.0.zip/like-1.1.0/like.py
@@ -0,0 +1,10 @@
+"""hahahaha"""
+def print_lol2(the_list,level):
+    """dasdadadada"""
+    for each_item in the_list:
+        if isinstance (each_item,list):
+                print_lol2(each_item,level+1)
+        else:
+                for tab_stop in range(level):
+                    print("\t",end='')
+                print (each_item)
