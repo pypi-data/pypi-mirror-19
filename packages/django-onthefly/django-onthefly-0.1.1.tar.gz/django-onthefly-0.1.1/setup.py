16dcff872ae012f7622d3a19e2f2747d741a941e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:56 +0000

    Add package django-onthefly/0.1.1/django-onthefly-0.1.1.tar.gz

---
+++ packages/django-onthefly/django-onthefly-0.1.1.tar.gz/django-onthefly-0.1.1/setup.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+from onthefly import __version__
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+packages = find_packages()
+
+setup(
+    name='django-onthefly',
+    version=__version__,
+    description='Change Django Settings On the Fly',
+    long_description=README,
+    url='https://github.com/baranbartu/onthfly',
+    download_url='https://github.com/baranbartu/onthfly/tarball/%s' % (
+        __version__,),
+    author='Baran Bartu Demirci',
+    author_email='bbartu.demirci@gmail.com',
+    license='MIT',
+    keywords='django,settings,change django settings',
+    packages=packages,
+    install_requires=[
+        'Django>=1.7',
+        'redis>=2.10.5'
+    ]
+)
