b556b847bd1d5873ddea6e72d4826a1e9bcd19e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:08 +0000

    Add package os0/0.2.14/os0-0.2.14.tar.gz

---
+++ packages/os0/os0-0.2.14.tar.gz/os0-0.2.14/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(name='os0',
+      version='0.2.14',
+      description='OS indipendent interface',
+      long_description="""
+Manage both URI both local filenames.
+""",
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: GNU Affero General Public License v3',
+          'Programming Language :: Python :: 2.6',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development',
+          'Topic :: Software Development :: Build Tools',
+          'Operating System :: OS Independent',
+      ],
+      keywords='os path linux windows openvms',
+      url='http://wiki.zeroincombenze.org/en/Python/opt/os0',
+      author='Antonio M. Vigliotti',
+      author_email='antoniomaria.vigliotti@gmail.com',
+      license='Affero: GPL',
+      packages=['os0'],
+      zip_safe=False)
