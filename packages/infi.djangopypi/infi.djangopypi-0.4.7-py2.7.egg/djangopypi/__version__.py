da485bc4c2857d24d182c7cff639914ddddf44d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:29 +0000

    Add package infi.djangopypi/0.4.7/infi.djangopypi-0.4.7-py2.7.egg

---
+++ packages/infi.djangopypi/infi.djangopypi-0.4.7-py2.7.egg/djangopypi/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.4.7"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.4.7)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '3e6fc761b2ea3108fb7490d3f0192990580fa060'
+__git_head_subject__ = 'Finished Release v0.4.7'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
+__git_commit_date__ = '2016-12-18 15:21:01'
