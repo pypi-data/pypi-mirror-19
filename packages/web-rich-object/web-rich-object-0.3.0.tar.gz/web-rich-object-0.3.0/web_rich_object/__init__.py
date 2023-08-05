e07e080239de6cd9d28bea338de37f69111dac71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:13 +0000

    Add package web-rich-object/0.3.0/web-rich-object-0.3.0.tar.gz

---
+++ packages/web-rich-object/web-rich-object-0.3.0.tar.gz/web-rich-object-0.3.0/web_rich_object/__init__.py
@@ -0,0 +1,13 @@
+"""Web rich object handler"""
+try:
+    from .api import WebRichObject
+except ImportError:
+    pass
+
+VERSION = (0, 3, 0)
+__version__ = '.'.join([str(i) for i in VERSION])
+__author__ = 'Anthony Monthe (ZuluPro)'
+__email__ = 'anthony.monthe@gmail.com'
+__url__ = 'https://github.com/ZuluPro/web-rich-object'
+__license__ = 'BSD'
+__keywords__ = ['web rich object', 'opengraph', 'facebook', 'web', 'twitter']
