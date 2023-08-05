1526f6b42ed8f702242d44581f46d7a56f3408e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:17 +0000

    Add package nester_li/1.1.0/nester_li-1.1.0.zip

---
+++ packages/nester_li/nester_li-1.1.0.zip/nester_li-1.1.0/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='nester_li',
+    version='1.1.0',
+    py_modules=['nester'],
+    author= 'lw199888464',
+    author_email= '764391557@qq.com',
+    url='http://www.li.com',
+    description= 'A simple printer of nested lists'
+)
