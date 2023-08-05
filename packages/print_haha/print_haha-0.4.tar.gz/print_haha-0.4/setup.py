f75ae7a11f2f7a238b2f43b9f42f8da8c417e109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:43 +0000

    Add package print_haha/0.4/print_haha-0.4.tar.gz

---
+++ packages/print_haha/print_haha-0.4.tar.gz/print_haha-0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'print_haha',
+    package = ['print_haha'],
+    package_data = {'print_haha.data': ['data_file.dat']},
+    discription = 'Just a test.',
+    version = '0.4',
+    author = 'wombatwen',
+    author_email = 'wombatwen@gmail.com',
+    scripts = ['print_haha/print_haha'],
+)
