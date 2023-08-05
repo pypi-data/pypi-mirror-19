f29c9daad5778076636c7ac76d99610a5c30c3a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:59 +0000

    Add package PhotonHomeAssistant/1.0.2/PhotonHomeAssistant-1.0.2.tar.gz

---
+++ packages/PhotonHomeAssistant/PhotonHomeAssistant-1.0.2.tar.gz/PhotonHomeAssistant-1.0.2/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = "PhotonHomeAssistant",
+    version = "1.0.2",
+    author = "Emil Rais",
+    author_email = "kongvman@hotmail.com",
+    url = "https://pypi.python.org/pypi/PhotonHomeAssistant"
+)
