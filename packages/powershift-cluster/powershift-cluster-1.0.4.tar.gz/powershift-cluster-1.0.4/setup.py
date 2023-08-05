809b26c84965957b48e604516abc942196549a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:52 +0000

    Add package powershift-cluster/1.0.4/powershift-cluster-1.0.4.tar.gz

---
+++ packages/powershift-cluster/powershift-cluster-1.0.4.tar.gz/powershift-cluster-1.0.4/setup.py
@@ -0,0 +1,33 @@
+import sys
+import os
+
+from setuptools import setup
+
+long_description = open('README.rst').read()
+
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+]
+
+setup_kwargs = dict(
+    name='powershift-cluster',
+    version='1.0.4',
+    description='PowerShift command plugin for creating OpenShift clusters.',
+    long_description=long_description,
+    url='https://github.com/getwarped/powershift-cluster',
+    author='Graham Dumpleton',
+    author_email='Graham.Dumpleton@gmail.com',
+    license='BSD',
+    classifiers=classifiers,
+    keywords='openshift kubernetes',
+    packages=['powershift', 'powershift.cluster'],
+    package_dir={'powershift': 'src/powershift'},
+    install_requires=['powershift>=1.3.8', 'powershift-cli'],
+    entry_points = {'powershift_cli_plugins': ['cluster = powershift.cluster']},
+)
+
+setup(**setup_kwargs)
