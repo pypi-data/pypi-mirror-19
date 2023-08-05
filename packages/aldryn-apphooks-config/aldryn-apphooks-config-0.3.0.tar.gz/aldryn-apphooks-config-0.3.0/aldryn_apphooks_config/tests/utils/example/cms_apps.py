143f84553c74d24929f04314f5888407f1993853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:57 +0000

    Add package aldryn-apphooks-config/0.3.0/aldryn-apphooks-config-0.3.0.tar.gz

---
+++ packages/aldryn-apphooks-config/aldryn-apphooks-config-0.3.0.tar.gz/aldryn-apphooks-config-0.3.0/aldryn_apphooks_config/tests/utils/example/cms_apps.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+from aldryn_apphooks_config.app_base import CMSConfigApp
+from cms.apphook_pool import apphook_pool
+from django.conf.urls import url
+from django.utils.translation import ugettext_lazy as _
+
+from .models import ExampleConfig
+from .views import ArticleDetail, ArticleList
+
+
+class ExampleApp(CMSConfigApp):
+    name = _('Example')
+    urls = [
+        [
+            url(r'^$', ArticleList.as_view(), name='example_list'),
+            url(r'^(?P<slug>[\w_-]+)/$', ArticleDetail.as_view(), name='example_detail'),
+        ]
+    ]
+    app_name = 'example'
+    app_config = ExampleConfig
+
+
+apphook_pool.register(ExampleApp)
