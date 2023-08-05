397a4ea0e4e37e390a4f33eacf0a0d827bb5fdf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:24 +0000

    Add package wagtailsystemtext/1.1.0/wagtailsystemtext-1.1.0.tar.gz

---
+++ packages/wagtailsystemtext/wagtailsystemtext-1.1.0.tar.gz/wagtailsystemtext-1.1.0/wagtailsystemtext/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+wagtailsystemtext
+----------
+Simplified Wagtail system text management
+"""
+
+__title__ = 'wagtailsystemtext'
+__version__ = '1.1.0'
+__build__ = 110
+__author__ = "Martin Sandström"
+__license__ = "MIT"
+__copyright__ = "Copyright 2016 Martin Sandström"
+
+default_app_config = 'wagtailsystemtext.apps.WagtailSystemTextConfig'
