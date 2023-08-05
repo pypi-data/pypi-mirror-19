02d7e687c5adfeba3c243883fb3e1a1ffe8453f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:17 +0000

    Add package nester_ghsemail/1.0.0/nester_ghsemail-1.0.0.tar.gz

---
+++ packages/nester_ghsemail/nester_ghsemail-1.0.0.tar.gz/nester_ghsemail-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'nester_ghsemail',
+    version='1.0.0',
+    py_modules = ['nester'],
+    author='ghsemail',
+    authoremail = 'ghsemail@163.com',
+    url = 'http://www.hadfirstlabs.com',
+    description='A simple printer of nested lists',
+)
