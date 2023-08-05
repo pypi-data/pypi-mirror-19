201f866787ac4fad49e92015e5fd25df2254099a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:43 +0000

    Add package magicstr/1.73/magicstr-1.73.tar.gz

---
+++ packages/magicstr/magicstr-1.73.tar.gz/magicstr-1.73/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = 'magicstr',
+    packages = ['magicstr'],
+    package_data = {'magicstr': ['config']},
+    discription = 'Do some magic on your strings files.',
+    version = '1.73',
+    author = 'wombatwen',
+    author_email = 'wombatwen@gmail.com',
+    install_requires = [
+        'gspread', 
+        'lxml',
+        'oauth2client'    
+    ],
+    scripts = ['magicstr/magicstr']
+)
