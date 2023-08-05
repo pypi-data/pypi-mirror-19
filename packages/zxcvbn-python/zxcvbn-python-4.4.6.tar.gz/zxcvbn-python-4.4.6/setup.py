5a06015d89945322b6f212fd69c36b1fa25177cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:03 +0000

    Add package zxcvbn-python/4.4.6/zxcvbn-python-4.4.6.tar.gz

---
+++ packages/zxcvbn-python/zxcvbn-python-4.4.6.tar.gz/zxcvbn-python-4.4.6/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='zxcvbn-python',
+    version='4.4.6',
+    packages=['zxcvbn'],
+    url='https://github.com/dwolfhub/zxcvbn-python',
+    download_url='https://github.com/dwolfhub/zxcvbn-python/tarball/4.4.6',
+    license='MIT',
+    author='Daniel Wolf',
+    author_email='danielrwolf5@gmail.com',
+    description='python port of Dropbox\'s zxcvbn library',
+    keywords=['zxcvbn', 'password', 'security'],
+)
