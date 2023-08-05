d057ba9dd683ad396c1bfda43d342316d3037369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:58 +0000

    Add package matrix-bot/0.0.1/matrix-bot-0.0.1.tar.gz

---
+++ packages/matrix-bot/matrix-bot-0.0.1.tar.gz/matrix-bot-0.0.1/setup.py
@@ -0,0 +1,52 @@
+from setuptools import setup, find_packages
+
+version = "0.0.1"
+
+long_description = ""
+try:
+    long_description = file('README.md').read()
+except Exception:
+    pass
+
+license = ""
+try:
+    license = file('LICENSE').read()
+except Exception:
+    pass
+
+
+setup(
+    name='matrix-bot',
+    version=version,
+    description='A matrix.org bot',
+    author='Pablo Saavedra',
+    author_email='saavedra.pablo@gmail.com',
+    url='http://github.com/psaavedra/matrix-bot',
+    packages=find_packages(),
+    package_data={
+    },
+    scripts=[
+        "tools/matrix-bot",
+    ],
+    zip_safe=False,
+    install_requires=[
+        "getconf",
+        "matrix-client",
+        "python-ldap",
+    ],
+    data_files=[
+        ('/usr/share/doc/matrix-bot/',
+            ['cfg/matrix-bot.cfg.example']),
+    ],
+
+    download_url='https://github.com/psaavedra/matrix-bot/zipball/master',
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Communications :: Chat",
+    ],
+    long_description=long_description,
+    license=license,
+    keywords="python matrix bot matrix-python-sdk",
+)
