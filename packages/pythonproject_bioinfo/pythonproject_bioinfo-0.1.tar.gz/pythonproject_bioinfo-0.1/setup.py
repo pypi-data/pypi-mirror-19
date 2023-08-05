81b30d1b91cefedb0658b071827516255eef0432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:11 +0000

    Add package pythonproject_bioinfo/0.1/pythonproject_bioinfo-0.1.tar.gz

---
+++ packages/pythonproject_bioinfo/pythonproject_bioinfo-0.1.tar.gz/pythonproject_bioinfo-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name='pythonproject_bioinfo',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/pnatsi/StarSeeker',
+      author='Flying Circus',
+      autho_email='pnatsidis@hotmail.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False
+)
+
+      
