06bd031b27ffccfc559ccd54cc5513b05fdfd4f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:15 +0000

    Add package wxyAlice/0.0.13/wxyAlice-0.0.13.tar.gz

---
+++ packages/wxyAlice/wxyAlice-0.0.13.tar.gz/wxyAlice-0.0.13/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+setup(
+    name='wxyAlice',
+    packages=['wxyAlice'],
+    version='0.0.13',
+    description='micro service on falcon',
+    author='wangxiaoyu',
+    author_email='wangxiaoyu.wangxiaoyu@@gmail.com',
+    license='MIT',
+    install_requires=[
+        'gevent',
+        'falcon',
+        'requests'
+    ],
+    url='https://github.com/netsmallfish1977/wxyAlice',
+    download_url='https://github.com/netsmallfish1977/wxyAlice/tarball/0.0.1',
+    keywords=['Alice', 'falcon', 'micro_service'],
+    classifiers=[],
+)
