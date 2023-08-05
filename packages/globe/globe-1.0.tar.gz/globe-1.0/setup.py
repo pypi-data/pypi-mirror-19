83b9edb6aa94ad0a76c68ae0da29b7d2895848f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:18 +0000

    Add package globe/1.0/globe-1.0.tar.gz

---
+++ packages/globe/globe-1.0.tar.gz/globe-1.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'globe',
+  packages = ['globe'], # this must be the same as the name above
+  version = '1.0',
+  description = 'Globe Api ',
+  author = 'clark galgo',
+  author_email = 'clark21@dev-engine.net',
+  url = 'https://github.com/globelabs/globe-connect-python', # use the URL to the github repo
+  download_url = 'https://github.com/globelabs/globe-connect-python/archive/master.zip', # I'll explain this in a second
+  keywords = ['Globe', 'Globe Api'], # arbitrary keywords
+  classifiers = [],
+)
