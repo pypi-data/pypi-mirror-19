68371ed8ce9d906ffc001e793fec24b7cb8c319b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:37 +0000

    Add package lowerpines/0.2.2/lowerpines-0.2.2.tar.gz

---
+++ packages/lowerpines/lowerpines-0.2.2.tar.gz/lowerpines-0.2.2/lowerpines/gmi.py
@@ -0,0 +1,31 @@
+import requests
+
+_gmi_objects = []
+
+
+def get_gmi(access_token):
+    for gmi in _gmi_objects:
+        if gmi.access_token == access_token:
+            return gmi
+    gmi = GMI(access_token)
+    _gmi_objects.append(gmi)
+    return gmi
+
+
+class GMI:
+    def __init__(self, access_token):
+        from lowerpines.group import GroupManager
+        from lowerpines.bot import BotManager
+        from lowerpines.chat import ChatManager
+        from lowerpines.user import UserManager
+
+        self.access_token = access_token
+        self.groups = GroupManager(self)
+        self.bots = BotManager(self)
+        self.chats = ChatManager(self)
+        self.user = UserManager(self)
+
+    def convert_image_url(self, url):
+        from lowerpines.endpoints.image import ImageConvertRequest
+        return ImageConvertRequest(self, requests.get(url).content).result
+
