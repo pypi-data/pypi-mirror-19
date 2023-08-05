09f8b4d1639966ac9529bfc9856e9713b97ac132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:51 +0000

    Add package odoo9-addon-web_selenium/9.0.1.0.2/odoo9_addon_web_selenium-9.0.1.0.2-py2-none-any.whl

---
+++ packages/odoo9-addon-web_selenium/odoo9_addon_web_selenium-9.0.1.0.2-py2-none-any.whl/odoo_addons/web_selenium/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2013 brain-tec AG (http://www.brain-tec.ch)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Web Selenium",
+    "summary": "Record your testcases with Selenium",
+    "version": "9.0.1.0.2",
+    "category": "Hidden",
+    "website": "http://www.brain-tec.ch",
+    "author": "Andreas Stauder <andreas.stauder@brain-tec.ch>",
+    "depends": ["web"],
+    "qweb": [
+        'static/src/xml/base_ext.xml',
+    ],
+    "active": False,
+    "installable": True,
+    'bootstrap': True,
+}
