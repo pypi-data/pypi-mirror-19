ad5745a5e5e1c9e264fd178ecd6cf17464d1622b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:06 +0000

    Add package essentialdb/0.3.19/essentialdb-0.3.19-py3-none-any.whl

---
+++ packages/essentialdb/essentialdb-0.3.19-py3-none-any.whl/essentialdb/essential_index.py
@@ -0,0 +1,28 @@
+from essentialdb import SimpleDocument
+
+class EssentialIndex:
+    """HashIndex provides fast lookup indexing for dictionary"""
+
+    def __init__(self, field_key, index_type='hash', index_name=None):
+        self.field_key = field_key
+        self.index_name = index_name if index_name != None else index_type + field_key
+        self.index = {}
+
+
+    def find(self, data, value):
+        results = SimpleDocument()
+        if value in self.index:
+            for _id in self.index[value]:
+                results[_id] = data[_id]
+        return results
+
+    def create_index(self, data):
+        index = {}
+        #start with a simple numeric indexing
+        for item in data:
+            if self.field_key in data[item] and data[item][self.field_key] not in index:
+                index[data[item][self.field_key]] = []
+
+            index[data[item][self.field_key]].append(item)
+        self.index = index
+        return self.index
