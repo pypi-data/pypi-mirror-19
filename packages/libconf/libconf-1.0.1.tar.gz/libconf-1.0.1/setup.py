ab516e3493c8864d883c2c4823e89bd01ce3509e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:03 +0000

    Add package libconf/1.0.1/libconf-1.0.1.tar.gz

---
+++ packages/libconf/libconf-1.0.1.tar.gz/libconf-1.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='libconf',
+    version='1.0.1',
+    description="A pure-Python libconfig reader/writer with permissive license",
+    long_description=open('README.rst').read(),
+    author="Christian Aichinger",
+    author_email="Greek0@gmx.net",
+    url='https://github.com/Grk0/python-libconf',
+    download_url='https://github.com/Grk0/python-libconf/tarball/1.0.1',
+    license="MIT",
+    py_modules=['libconf'],
+    keywords='libconfig configuration parser library',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
