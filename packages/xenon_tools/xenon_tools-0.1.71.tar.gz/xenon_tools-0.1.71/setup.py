ba144bd10db826081e7ea4d53c2881478ee75356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:55 +0000

    Add package xenon_tools/0.1.71/xenon_tools-0.1.71.tar.gz

---
+++ packages/xenon_tools/xenon_tools-0.1.71.tar.gz/xenon_tools-0.1.71/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+'''python setup.py sdist upload -r pypi'''
+
+setup(
+    name='xenon_tools',
+    packages=['xenon_tools'],  # this must be the same as the name above
+    version='0.1.71',
+    description='Tools for Xenon',
+    author='minus79',
+    author_email='gergely06@gmail.com',
+    url='https://github.com/minus79/xenon_tools',  # use the URL to the github repo
+    download_url='https://github.com/minus79/xenon_tools/tarball/0.1',  # I'll explain this in a second
+    keywords=['xenon', 'xenon-tools', 'xenon_tools'],  # arbitrary keywords
+    classifiers=[],
+)
---
+++ packages/xenon_tools/xenon_tools-0.1.71.tar.gz/xenon_tools-0.1.71/xenon_tools/utils.py
@@ -0,0 +1,71 @@
+#! /usr/bin/env python
+import os
+import sys
+
+
+def human_time(seconds):
+    if not seconds:
+        seconds = 0
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+    return "%02d:%02d" % (m, s) if h == 0 else "%d:%02d:%02d" % (h, m, s)
+
+
+def push_in_range(val, minimum, maximum):
+    if val < minimum:
+        return minimum
+    elif val > maximum:
+        return maximum
+    else:
+        return val
+
+
+def print_progress(iteration, total, time=0, mdet=None, prefix='', suffix='', decimals=1, bar_length=100):
+    format_str = "{0:." + str(decimals) + "f}"
+    percents = format_str.format(100 * (iteration / float(total)))
+    filled_length = int(round(bar_length * iteration / float(total)))
+    bar = '=' * filled_length + ' ' * (bar_length - filled_length)
+    if mdet is None:
+        sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percents, '%', suffix))
+    else:
+        sys.stdout.write('\r%s |%s| %s%s %s %s=%s-%s [%shz][%sch][%sbytes/sample]' %
+                         (prefix, bar, percents, '%', suffix,
+                          human_time(time), human_time(mdet.duration_seconds), human_time(mdet.duration_seconds - time),
+                          mdet.frame_rate, mdet.no_channels, mdet.frame_width))
+    sys.stdout.flush()
+    if iteration == total:
+        sys.stdout.write('\n')
+        sys.stdout.flush()
+
+
+def find_file(filename, basedir, recursive=True, halt_on_first=False):
+    found = []
+    for subdir, dirs, files in os.walk(basedir):
+        if filename in files:
+            found.append(str(subdir) + '/' + filename)
+            if halt_on_first:
+                return found
+    return found
+
+
+def is_int(s):
+    if len(s):
+        if s[0] in ('-', '+'):
+            return s[1:].isdigit()
+        return s.isdigit()
+    return False
+
+class MediaDetails:
+    duration_seconds = 0
+    duration_human = ''
+    frame_rate = 0
+    no_channels = 0
+    frame_width = 0
+
+    def __init__(self, duration_seconds=0, frame_rate=0, no_channels=0, frame_width=0):
+        self.duration_seconds = duration_seconds
+        self.duration_human = human_time(duration_seconds)
+        self.frame_width = frame_width
+        self.frame_rate = frame_rate
+        self.no_channels = no_channels
+
