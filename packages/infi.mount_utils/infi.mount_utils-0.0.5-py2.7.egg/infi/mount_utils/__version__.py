2394f2b1fafb4c1ca0077c0eb9c903653e54c5fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:22 +0000

    Add package infi.mount_utils/0.0.5/infi.mount_utils-0.0.5-py2.7.egg

---
+++ packages/infi.mount_utils/infi.mount_utils-0.0.5-py2.7.egg/infi/mount_utils/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.5"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.0.5)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'e5f6060fc77762a3f667cc8ea99b5911293c77a9'
+__git_head_subject__ = 'Finished Release v0.0.5'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
+__git_commit_date__ = '2016-12-18 14:13:57'
