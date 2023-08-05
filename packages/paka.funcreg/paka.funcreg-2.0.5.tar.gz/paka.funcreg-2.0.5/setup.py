5773ca2377b223ac168c14ecb42c351064a2c742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:41 +0000

    Add package paka.funcreg/2.0.5/paka.funcreg-2.0.5.tar.gz

---
+++ packages/paka.funcreg/paka.funcreg-2.0.5.tar.gz/paka.funcreg-2.0.5/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+setuptools.setup(
+    name="paka.funcreg",
+    version="2.0.5",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    namespace_packages=["paka"],
+    zip_safe=False,
+    url="https://github.com/PavloKapyshin/paka.funcreg",
+    keywords="registry",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5"],
+    license="BSD",
+    author="Pavlo Kapyshin",
+    author_email="i@93z.org")
