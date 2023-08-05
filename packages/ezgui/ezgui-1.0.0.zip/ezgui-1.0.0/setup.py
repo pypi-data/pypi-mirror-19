023cc5f0cc58a744ba67f461866fe9544971928f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:42 +0000

    Add package ezgui/1.0.0/ezgui-1.0.0.zip

---
+++ packages/ezgui/ezgui-1.0.0.zip/ezgui-1.0.0/setup.py
@@ -0,0 +1,12 @@
+#coding:utf-8
+from distutils.core import setup
+setup(
+	keywords		=['easygui','gui','ezgui'],
+	name				= 'ezgui',
+	version 		='1.0.0',
+	py_modules	=['ezgui'],
+	author			='kancy',
+	author_email='793272861@qq.com',
+	url					='http://easygui.sourceforge.net/',
+	description ='2009年的easygui简单插件'
+)
