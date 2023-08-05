7284c2d1450cc418ca9ab6fbb408ed94f469cb61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:06 +0000

    Add package pic-dl/0.1.6/pic_dl-0.1.6-py3-none-any.whl

---
+++ packages/pic-dl/pic_dl-0.1.6-py3-none-any.whl/pic_dl/known_sites.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+SITES = {
+        'twitter': 'twitter',
+        'tumblr': 'tumblr',
+        'tuchong': 'tuchong',
+        'lofter': 'lofter',
+        'bcy': 'bcy',
+        '163': '163',
+        'poco': 'poco',
+        }
