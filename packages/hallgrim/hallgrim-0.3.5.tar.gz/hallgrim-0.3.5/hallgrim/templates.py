253f54320d22ba808b4839a393be16412843cb38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:44 +0000

    Add package hallgrim/0.3.5/hallgrim-0.3.5.tar.gz

---
+++ packages/hallgrim/hallgrim-0.3.5.tar.gz/hallgrim-0.3.5/hallgrim/templates.py
@@ -0,0 +1,65 @@
+gap = r'''meta = {{
+    'author': '{}',
+    'title': '{}',
+    'type': 'gap',
+}}
+
+task = """ description
+
+[gap(1.0P)]Answer A, Answer B, Answer C[/gap]
+
+[numeric(1.0P)]<value>,<min>,<max>[/numeric]
+
+[select]
+[ ] Answer A
+[ ] Answer B
+[X] Answer C
+[/select]
+
+"""
+
+feedback = """ feedback """
+'''
+
+order = r'''meta = {{
+    'author': '{}',
+    'title': '{}',
+    'type': 'order',
+    'points': {}, # total number of points
+}}
+
+task = """ decription """
+
+order = """ Answer A -- Answer B -- Answer C """
+
+feedback = """ feedback """
+'''
+
+choice = r'''meta = {{
+    'author': '{}',
+    'title': '{}',
+    'type': '{} choice',
+    'points': {}, # points per correct question
+}}
+
+task = """ decription """
+
+choices = """
+    [ ] Answer A
+    [ ] Answer B
+    [X] Answer C
+"""
+
+feedback = """ feedback """
+'''
+
+config_sample = """[META]
+author = {}
+output = {}
+
+[UPLAODER]
+user = root
+pass = homer
+host = http://localhost:8000/
+rtoken = c13456ec3d71dc657e19fb826750f676
+"""
