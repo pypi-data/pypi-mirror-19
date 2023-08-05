3cb109244a434db84732fb2628548e3cf732ecfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:01 +0000

    Add package django-ohm2-handlers/1.1.9/django-ohm2-handlers-1.1.9.tar.gz

---
+++ packages/django-ohm2-handlers/django-ohm2-handlers-1.1.9.tar.gz/django-ohm2-handlers-1.1.9/ohm2_handlers/countries/decorators.py
@@ -0,0 +1,34 @@
+from functools import wraps
+from .definitions import CountriesInputException
+import inspect
+
+def countries_safe_request(func):
+	
+	@wraps(func)
+	def wrapper(*args, **kwargs):
+		try:
+			result = func(*args, **kwargs)
+		except Exception as e:
+			frm = inspect.trace()[-1]
+			
+			ins_filename = frm[1]
+			ins_lineno = frm[2]
+			ins_function = frm[3]
+
+			if hasattr(e, "code"):
+				code = e.code
+			else:
+				code = -1	
+
+			
+			if hasattr(e, "message"):
+				message = e.message
+			else:
+				message = "Uncaught exception: {0}".format(e)
+
+			return (None, CountriesInputException(code, message, ins_filename, ins_lineno, ins_function))
+		else:
+			return (result, None)
+
+	return wrapper
+	
---
+++ packages/django-ohm2-handlers/django-ohm2-handlers-1.1.9.tar.gz/django-ohm2-handlers-1.1.9/ohm2_handlers/socialstatistics/decorators.py
@@ -0,0 +1,34 @@
+from functools import wraps
+from .definitions import SocialstatisticsInputException
+import inspect
+
+def socialstatistics_safe_request(func):
+	
+	@wraps(func)
+	def wrapper(*args, **kwargs):
+		try:
+			result = func(*args, **kwargs)
+		except Exception as e:
+			frm = inspect.trace()[-1]
+			
+			ins_filename = frm[1]
+			ins_lineno = frm[2]
+			ins_function = frm[3]
+
+			if hasattr(e, "code"):
+				code = e.code
+			else:
+				code = -1	
+
+			
+			if hasattr(e, "message"):
+				message = e.message
+			else:
+				message = "Uncaught exception: {0}".format(e)
+
+			return (None, SocialstatisticsInputException(code, message, ins_filename, ins_lineno, ins_function))
+		else:
+			return (result, None)
+
+	return wrapper
+	
