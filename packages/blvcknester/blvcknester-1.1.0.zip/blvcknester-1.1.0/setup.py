9b3fdadb1c0a3881e1296e29dd17367294023d91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:32 +0000

    Add package blvcknester/1.1.0/blvcknester-1.1.0.zip

---
+++ packages/blvcknester/blvcknester-1.1.0.zip/blvcknester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name                   =  'blvcknester' ,
+    version                =  '1.1.0' ,
+    py_modules         =  ['blvcknester'],
+    author                 =  'Blvckode',
+    author_email       =  'papaacquah17@gmail.com',
+    url                      =  'http://www.hackactivits.blogspot.com',
+    description         =  'A simple printer of nested lists',
+    )
