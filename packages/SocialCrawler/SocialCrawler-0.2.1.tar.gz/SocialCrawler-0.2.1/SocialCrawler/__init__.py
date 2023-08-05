39bd25f2f58155e3796ba8a701b8b63085b136f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:45 +0000

    Add package SocialCrawler/0.2.1/SocialCrawler-0.2.1.tar.gz

---
+++ packages/SocialCrawler/SocialCrawler-0.2.1.tar.gz/SocialCrawler-0.2.1/SocialCrawler/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""SocialCrawler.
+
+This modules import all classes from SocialCrawler package.
+
+ (c) J.Wirlino and A.Adativa
+ Date Sept 1th 2016
+ https://github.com/JosielWirlino/SocialCrawler
+
+"""
+
+
+from . import HistoricalCollector
+from . import ExtractorData
+
+# from  . import ExtractorData
+
+__version__ = "0.2.1"
+
+__all__ = [	
+			"HistoricalCollector",
+			"ExtractorData",
+			"HackFoursquare",
+			"NewExtractorData",
+			"HeadLess"
+		]
