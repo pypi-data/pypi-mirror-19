e738159a49e9ed92d6f52c68286c951455c13b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:47 +0000

    Add package sphinxmix/0.0.3/sphinxmix-0.0.3.tar.gz

---
+++ packages/sphinxmix/sphinxmix-0.0.3.tar.gz/sphinxmix-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+import sphinxmix
+
+setup(name='sphinxmix',
+      version=sphinxmix.VERSION,
+      description='A Python implementation of the Sphinx mix packet format.',
+      author='George Danezis',
+      author_email='g.danezis@ucl.ac.uk',
+      url=r'https://pypi.python.org/pypi/sphinxmix/',
+      packages=['sphinxmix'],
+      license="2-clause BSD",
+      long_description="""A Python implementation of the Sphinx mix packet format.""",
+
+      setup_requires=["pytest >= 2.6.4"],
+      install_requires=[
+            "future >= 0.14.3",
+            "pytest >= 2.5.0",
+            "msgpack-python >= 0.4.6",
+            "petlib >= 0.0.38",
+      ]
+)
