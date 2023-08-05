ae224455407a7f58fcf6a73eebb1be4949282b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:02 +0000

    Add package refifer/0.1.4/refifer-0.1.4.tar.gz

---
+++ packages/refifer/refifer-0.1.4.tar.gz/refifer-0.1.4/refifer/constants.py
@@ -0,0 +1,7 @@
+REGISTRATION_ENDPOINT = 'https://api.fetchr.us/v3/notifications'
+FIRE_ENDPOINT = 'https://api.fetchr.us/v3/events'
+
+#BASE_API_ENDPOINT = 'http://scooterlabs.com/echo'
+#FIRE_ENDPOINT = 'http://scooterlabs.com/echo'
+
+RETRY_COUNT = 10
