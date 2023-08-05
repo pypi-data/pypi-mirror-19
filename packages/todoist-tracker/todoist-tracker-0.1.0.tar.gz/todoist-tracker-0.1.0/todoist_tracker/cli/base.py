db9f8a02144235abf9ce02da6f34bdd0b03eb663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:42 +0000

    Add package todoist-tracker/0.1.0/todoist-tracker-0.1.0.tar.gz

---
+++ packages/todoist-tracker/todoist-tracker-0.1.0.tar.gz/todoist-tracker-0.1.0/todoist_tracker/cli/base.py
@@ -0,0 +1,75 @@
+import argparse
+import json
+
+from todoist import TodoistAPI
+import gspread
+from gspread.exceptions import WorksheetNotFound
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+class BaseCommand(object):
+    help_text = ''
+
+    def __init__(self, subcommand_creator):
+
+        # keep a local copy of the config file which is useful during
+        # autocompletion
+        self.config = None
+
+        # set up the subcommand options
+        self.subcommand_creator = subcommand_creator
+        self.option_parser = self.subcommand_creator.add_parser(
+            self.get_command_name(),
+            help=self.help_text,
+            description=self.help_text,
+        )
+        self.add_command_line_options()
+
+    def get_command_name(self):
+        """The command name defaults to the name of the module."""
+        return self.__module__.rsplit('.', 1)[1]
+
+    def add_command_line_options(self):
+        self.option_parser.add_argument(
+            '--todoist',
+            type=argparse.FileType('r'),
+            metavar='JSONFILE',
+            default='todoist.json',
+            help='todoist credentials file in json format',
+        )
+        self.option_parser.add_argument(
+            '--google',
+            type=argparse.FileType('r'),
+            metavar='JSONFILE',
+            default='google.json',
+            help='google credentials file in json format',
+        )
+        self.option_parser.add_argument(
+            '--debug',
+            action='store_true',
+            help='log output on command line, NOT google spreadsheet'
+        )
+
+    def execute(self, todoist=None, google=None, debug=None, **kwargs):
+        """Common execution workflows are handled here"""
+
+        # create an authenticated instance of the TodoistAPI
+        credentials = json.load(todoist)
+        self.todoist_api = TodoistAPI(**credentials)
+
+        # authenticate to google
+        google_keys = json.load(google)
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            google.name,
+            ['https://spreadsheets.google.com/feeds'],
+        )
+        gdrive = gspread.authorize(credentials)
+        self.gdrive_workbook = gdrive.open_by_url(google_keys['workbook_url'])
+
+    def get_or_create_worksheet(self, title, header):
+        try:
+            worksheet = self.gdrive_workbook.worksheet(title)
+        except WorksheetNotFound:
+            worksheet = self.gdrive_workbook.add_worksheet(title, 1, 26)
+            worksheet.insert_row(header)
+        return worksheet
