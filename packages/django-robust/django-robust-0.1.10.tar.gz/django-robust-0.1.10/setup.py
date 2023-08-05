b1ee503b8ae19673f61d15982fac99dacb580c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:00 +0000

    Add package django-robust/0.1.10/django-robust-0.1.10.tar.gz

---
+++ packages/django-robust/django-robust-0.1.10.tar.gz/django-robust-0.1.10/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-robust',
+    version='0.1.10',
+    description='robust background queue for django',
+    author='Victor Kotseruba',
+    author_email='barbuzaster@gmail.com',
+    url='https://github.com/barbuza/django-robust',
+    include_package_data=True,
+    packages=find_packages(exclude=['django_robust']),
+    install_requires=[
+        'django >= 1.9',
+        'psycopg2 >= 2.5',
+        'django-object-actions',
+        'schedule',
+        'colorlog',
+    ]
+)
