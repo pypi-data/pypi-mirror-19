4474e15a5b42af1b05d22ec0afb41cb19ad65ad9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:05 +0000

    Add package greenswitch/0.0.8/greenswitch-0.0.8.tar.gz

---
+++ packages/greenswitch/greenswitch-0.0.8.tar.gz/greenswitch-0.0.8/greenswitch/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+GreenSWITCH: FreeSWITCH Event Socket Protocol
+---------------------------------------------
+
+Complete documentation at https://github.com/evoluxbr/greenswitch
+
+"""
+
+
+from .esl import InboundESL
