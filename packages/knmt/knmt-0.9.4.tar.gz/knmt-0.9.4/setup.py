8c7c8fcf0ff0cb137b75bf61a8edfd325b6bb60c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:00 +0000

    Add package knmt/0.9.4/knmt-0.9.4.tar.gz

---
+++ packages/knmt/knmt-0.9.4.tar.gz/knmt-0.9.4/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# from distutils.core import setup
+from setuptools import setup, find_packages
+
+CLASSIFIERS = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+setup(name='knmt',
+      version='0.9.4',
+      description='Implementation of RNNSearch and other Neural MT models in Chainer',
+      author='Fabien Cromieres',
+      author_email='fabien.cromieres@gmail.com',
+        #packages=find_packages()
+      packages=['nmt_chainer'],
+      url="https://github.com/fabiencro/knmt",
+#       scripts=["nmt_chainer/knmt.py"],
+
+      scripts=["bin/knmt", "bin/knmt-debug"],
+      
+#       entry_points= {
+#           'console_scripts': [
+#               'knmt = nmt_chainer.__main__:main'
+#           ]
+#                      },
+      
+      install_requires=['chainer', 'bokeh', 'plotly']
+     )
+
+
