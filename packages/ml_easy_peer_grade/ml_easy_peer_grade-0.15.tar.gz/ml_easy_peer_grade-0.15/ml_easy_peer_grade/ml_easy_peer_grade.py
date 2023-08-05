c044d4b9b595f7d22933ef5e458856f94caa21ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:47 +0000

    Add package ml_easy_peer_grade/0.15/ml_easy_peer_grade-0.15.tar.gz

---
+++ packages/ml_easy_peer_grade/ml_easy_peer_grade-0.15.tar.gz/ml_easy_peer_grade-0.15/ml_easy_peer_grade/ml_easy_peer_grade.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import sys
+
+def ez():
+  if len(sys.argv) != 6:
+    print '<usage> ml_easy_peer_grade [YOUR_ID] [YOUR_LAST_NAME] [LAST_NAME_1] [LAST_NAME_2] [LAST_NAME_3]'
+  else:
+    args = sys.argv[1:]
+    f = open('{}.txt'.format(args[0]), 'w')
+    s = "{0[0]} {0[1]}\n{0[2]} 1\n{0[3]} 1\n{0[4]} 1\n".format(args)
+    f.write(s)
+    f.close() 
