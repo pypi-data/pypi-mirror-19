aedcaf7edaf240a5231035f63d1f01898e71d4e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:35 +0000

    Add package app_store_ranking_checker/0.1/app_store_ranking_checker-0.1.tar.gz

---
+++ packages/app_store_ranking_checker/app_store_ranking_checker-0.1.tar.gz/app_store_ranking_checker-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='app_store_ranking_checker',
+      version='0.1',
+      description='Check App Store ranking for the given app',
+      url='https://github.com/yuweizhan/app-store-ranking-checker',
+      author='Yuwei Zhan',
+      author_email='yzhan@coursehero.com',
+      license='MIT',
+      packages=['app_store_ranking_checker'],
+      zip_safe=False)
