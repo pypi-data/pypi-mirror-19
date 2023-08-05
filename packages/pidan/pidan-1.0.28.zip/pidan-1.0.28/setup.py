ae73acc9c01de787de041b166290751352ccb6d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:00 +0000

    Add package pidan/1.0.28/pidan-1.0.28.zip

---
+++ packages/pidan/pidan-1.0.28.zip/pidan-1.0.28/setup.py
@@ -0,0 +1,16 @@
+# install:python setup.py sdist upload
+# -*- coding:utf-8 -*-
+from distutils.core import setup
+ 
+setup (
+    name = 'pidan',
+    version = '1.0.28',
+    packages=["pidan"],
+    platforms=['any'],
+    author = 'chengang',
+    author_email = 'chengang.net@gmail.com',
+    description = u'更新远程下载图片函数',
+    package_data = {
+        '': ['*.data'],
+    },
+)
