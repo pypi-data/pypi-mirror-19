40967025e12ef66f88c143ab223c221436b910de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:00 +0000

    Add package pythainlp/1.0.0/pythainlp-1.0.0.zip

---
+++ packages/pythainlp/pythainlp-1.0.0.zip/pythainlp-1.0.0/pythainlp/corpus/thaiword.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from future import standard_library
+standard_library.install_aliases()
+import os
+from nine import nimport,str
+codecs= nimport('codecs')
+import codecs
+import pythainlp
+templates_dir = os.path.join(os.path.dirname(pythainlp.__file__), 'corpus')
+template_file = os.path.join(templates_dir, 'thaiword.txt')
+def get_data():
+	with codecs.open(template_file, 'r',encoding='utf8') as f:
+		lines = f.read().splitlines()
+	return lines
