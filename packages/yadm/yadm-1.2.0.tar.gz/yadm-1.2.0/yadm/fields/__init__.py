29cad16bcba8ed9b71cd204f1892f60537f60d22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:24 +0000

    Add package yadm/1.2.0/yadm-1.2.0.tar.gz

---
+++ packages/yadm/yadm-1.2.0.tar.gz/yadm-1.2.0/yadm/fields/__init__.py
@@ -0,0 +1,34 @@
+""" This package contain all fields.
+"""
+
+from yadm.fields.base import FieldDescriptor, Field, NotLoadedError  # noqa
+
+from yadm.fields.simple import (  # noqa
+    ObjectIdField,
+    BooleanField,
+    IntegerField,
+    FloatField,
+    StringField,
+)
+
+from yadm.fields.email import EmailField  # noqa
+
+from yadm.fields.datetime import DatetimeField  # noqa
+from yadm.fields.decimal import DecimalField  # noqa
+from yadm.fields.money import Money, MoneyField  # noqa
+
+from yadm.fields.embedded import EmbeddedDocumentField  # noqa
+
+from yadm.fields.list import ListField  # noqa
+from yadm.fields.set import SetField  # noqa
+from yadm.fields.map import MapField, MapCustomKeysField  # noqa
+from yadm.fields.mongo_map import UnmutableMap, MongoMapField  # noqa
+
+from yadm.fields.reference import (  # noqa
+    ReferenceField,
+    BrokenReference,
+    NotBindingToDatabase,
+)
+
+from yadm.fields.geo import Point, PointField  # noqa
+from yadm.fields.geo import MultiPoint, MultiPointField  # noqa
