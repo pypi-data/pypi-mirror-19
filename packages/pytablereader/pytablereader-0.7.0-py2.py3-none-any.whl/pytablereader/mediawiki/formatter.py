e2f154a3d96987b97c53cbe4031e2b4d129795b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:00 +0000

    Add package pytablereader/0.7.0/pytablereader-0.7.0-py2.py3-none-any.whl

---
+++ packages/pytablereader/pytablereader-0.7.0-py2.py3-none-any.whl/pytablereader/mediawiki/formatter.py
@@ -0,0 +1,18 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import absolute_import
+
+from ..html.formatter import HtmlTableFormatter
+
+
+class MediaWikiTableFormatter(HtmlTableFormatter):
+
+    def __init__(self, source_data):
+        import pypandoc
+
+        super(MediaWikiTableFormatter, self).__init__(
+            pypandoc.convert_text(source_data, "html", format="mediawiki"))
