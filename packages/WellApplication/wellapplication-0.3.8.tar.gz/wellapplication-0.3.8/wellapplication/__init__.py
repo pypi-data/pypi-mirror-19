7fb5b71c6bfc4a9e4ed8afa5b03e9512c5217250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:06 +0000

    Add package WellApplication/0.3.8/wellapplication-0.3.8.tar.gz

---
+++ packages/WellApplication/wellapplication-0.3.8.tar.gz/wellapplication-0.3.8/wellapplication/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__version__ = '0.3.8'
+__author__ = 'Paul Inkenbrandt'
+__name__ = 'wellapplication'
+
+from transport import *
+from usgs import *
+from chem import WQP
+from mesopy import *
+from graphs import piper, fdc, gantt
+import MannKendall
