780be26cfb02b4410213ea8af4747a32a9610e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:39 +0000

    Add package nester_wong/1.0.0/nester_wong-1.0.0.tar.gz

---
+++ packages/nester_wong/nester_wong-1.0.0.tar.gz/nester_wong-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name = 'nester_wong',
+        version = '1.0.0',
+        py_modules = ['nester_wong'],
+        author = 'claytonjwong',
+        author_email = 'claytonjwong@gmail.com',
+        url = 'http://www.something.com',
+        description = 'a simple printer of nested lists',
+    )
+    
