cac1ac2acaec03fa5a051c1904b5eb7cbd6a2254
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:41 +0000

    Add package nester001as/1.2.0/nester001as-1.2.0.zip

---
+++ packages/nester001as/nester001as-1.2.0.zip/nester001as-1.2.0/nester001as.py
@@ -0,0 +1,9 @@
+def printListItem(item,indent_status=false,level=0):
+	for nest_item in item:
+		if isinstance(nest_item,list):
+			printListItem(nest_item,indent_status,level+1)
+		else:
+			if indent_status:
+				for tab_stop in range(level):
+					print("\t",end='')	
+			print(nest_item)
