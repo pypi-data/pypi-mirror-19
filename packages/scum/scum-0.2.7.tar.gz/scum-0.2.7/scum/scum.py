0118d1b5b91d3e25435a9a1b24c1d31771e55d02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:47 +0000

    Add package scum/0.2.7/scum-0.2.7.tar.gz

---
+++ packages/scum/scum-0.2.7.tar.gz/scum-0.2.7/scum/scum.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from main import MainGUI, START_PATH, TABS_PATH
+import os
+import signal
+
+def main():
+    os.system('stty -ixon') # disable XOFF to accept Ctrl-S
+    # instantiate it!
+    main = MainGUI()
+    signal.signal(signal.SIGTSTP, signal.SIG_IGN)
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
+    status = main.display()
+    os.system('stty ixon') # re-enable XOFF!
+    if status == 'failure':
+        with open(TABS_PATH, 'w') as f:
+            f.write(START_PATH + '\n')
+            f.write('False')
+
+#if __name__=="__main__":
+#       main()
+
+
