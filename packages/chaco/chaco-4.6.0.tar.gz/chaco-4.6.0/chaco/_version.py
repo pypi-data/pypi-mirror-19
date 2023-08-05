4f21923271fc69abc2b5f99e29bf1614a85a6963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:19 +0000

    Add package chaco/4.6.0/chaco-4.6.0.tar.gz

---
+++ packages/chaco/chaco-4.6.0.tar.gz/chaco-4.6.0/chaco/_version.py
@@ -0,0 +1,8 @@
+# This file was automatically generated from the `setup.py` script.
+version = '4.6.0'
+full_version = '4.6.0'
+git_revision = '66854ea'
+is_released = True
+
+if not is_released:
+    version = full_version
