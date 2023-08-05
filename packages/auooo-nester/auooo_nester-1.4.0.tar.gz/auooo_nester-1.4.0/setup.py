864f6ce940c4169797030d2a6e12da0e8a9afb5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:43 +0000

    Add package auooo-nester/1.4.0/auooo_nester-1.4.0.tar.gz

---
+++ packages/auooo-nester/auooo_nester-1.4.0.tar.gz/auooo_nester-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name            = 'auooo_nester',
+    version         = '1.4.0',
+    py_modules      = ['auooo_nester'],
+    author          = 'auooo',
+    author_email    = 'mh245500@gmail.com',
+    url             = 'http://www.auooo.com',
+    description     = 'A simple printer of nested lists, test for twine update',
+    )
