806b8d718c5bdcd5a82f51231105aa98b2f68db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:40 +0000

    Add package apcaccess/0.0.6/apcaccess-0.0.6.tar.gz

---
+++ packages/apcaccess/apcaccess-0.0.6.tar.gz/apcaccess-0.0.6/apcaccess/__main__.py
@@ -0,0 +1,23 @@
+"""
+apcaccess.__main__
+
+Provides the command-line functionalty similar to the original apcaccess cmd.
+"""
+
+import argparse
+
+from apcaccess import status
+
+
+if __name__ == "__main__":
+    # No need to use "proper" names on such simple code.
+    # pylint: disable=invalid-name
+    p = argparse.ArgumentParser()
+    p.add_argument("--host", default="localhost")
+    p.add_argument("--port", type=int, default=3551)
+    p.add_argument("--strip-units", action="store_true", default=False)
+    args = p.parse_args()
+    status.print_status(
+        status.get(args.host, args.port),
+        strip_units=args.strip_units
+    )
