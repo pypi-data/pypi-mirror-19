4b8f42faf04a9d64f939689e791eb3309eec8ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:36 +0000

    Add package legos/0.1.1.dev2/legos-0.1.1.dev2.tar.gz

---
+++ packages/legos/legos-0.1.1.dev2.tar.gz/legos-0.1.1.dev2/legos/legos.py
@@ -0,0 +1,2 @@
+#http://www.agileinnovation.eu/wordpress/wp-content/uploads/2010/09/KanbanAndScrum_MakingTheMostOfBoth.pdf Legos: A collection of Legobot plugins
+# Copyright (C) 2017 Brenton Briggs
---
+++ packages/legos/legos-0.1.1.dev2.tar.gz/legos-0.1.1.dev2/setup.py
@@ -0,0 +1,44 @@
+# Legos, A namespace package for distribution of Legobot plugins
+# Copyright (C) 2017  Brenton Briggs II
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from setuptools import setup, find_packages
+
+setup(
+    name='legos',
+
+    version='0.1.1dev2',
+
+    namespace_packages=['legos'],
+
+    license='GPLv3',
+
+    py_modules=['legos'],
+
+    description='Collection of Legos (plugins) for the Legobot framework',
+
+    author='Brenton Briggs II',
+
+    url='https://github.com/bbriggs/Legos',
+
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+
+        'Programming Language :: Python :: 3'
+    ],
+
+    packages=find_packages(exclude=['contrib', 'docs']),
+
+)
