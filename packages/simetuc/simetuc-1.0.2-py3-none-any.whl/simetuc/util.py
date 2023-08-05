f9190fa387999f11831a49216909bd4393cd7ee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:14 +0000

    Add package simetuc/1.0.2/simetuc-1.0.2-py3-none-any.whl

---
+++ packages/simetuc/simetuc-1.0.2-py3-none-any.whl/simetuc/util.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 23 16:45:32 2016
+
+@author: Pedro
+"""
+import tempfile
+from contextlib import contextmanager
+import os
+from collections import namedtuple
+
+
+# http://stackoverflow.com/a/11892712
+@contextmanager
+def temp_config_filename(data):
+    '''Creates a temporary file and writes text data to it. It returns its filename.
+        It deletes the file after use in a context manager.
+    '''
+    # file won't be deleted after closing
+    temp = tempfile.NamedTemporaryFile(mode='wt', delete=False)
+    if data:
+        temp.write(data)
+    temp.close()
+    yield temp.name
+    os.unlink(temp.name)  # delete file
+
+
+@contextmanager
+def temp_bin_filename():
+    '''Creates a temporary binary file. It returns its filename.
+        It deletes the file after use in a context manager.
+    '''
+    # file won't be deleted after closing
+    temp = tempfile.NamedTemporaryFile(mode='wb', delete=False)
+    temp.close()
+    yield temp.name
+    os.unlink(temp.name)  # delete file
+
+
+# namedtuple for the concentration of solutions
+Conc = namedtuple('Concentration', ['S_conc', 'A_conc'])
