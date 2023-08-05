24116f7d8748de04d3bac0f541730e3029ecab9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:25 +0000

    Add package infi.tracing/0.2.10/infi.tracing-0.2.10.tar.gz

---
+++ packages/infi.tracing/infi.tracing-0.2.10.tar.gz/infi.tracing-0.2.10/src/infi/tracing/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.2.10"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.2.10)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '70294ac2b24a953829cc1d57a8bff925d992e636'
+__git_head_subject__ = 'Finished Release v0.2.10'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
+__git_commit_date__ = '2016-12-18 14:30:10'
