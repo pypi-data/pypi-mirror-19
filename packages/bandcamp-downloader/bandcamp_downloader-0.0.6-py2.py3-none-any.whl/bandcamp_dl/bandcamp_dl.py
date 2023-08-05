a8cb892b6e953bb04450cd4c829b7c7d32ba3886
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:40 +0000

    Add package bandcamp-downloader/0.0.6/bandcamp_downloader-0.0.6-py2.py3-none-any.whl

---
+++ packages/bandcamp-downloader/bandcamp_downloader-0.0.6-py2.py3-none-any.whl/bandcamp_dl/bandcamp_dl.py
@@ -0,0 +1,75 @@
+"""bandcamp-dl
+
+Usage:
+  bandcamp-dl.py <url>
+  bandcamp-dl.py [--template=<template>] [--base-dir=<dir>]
+                 [--full-album]
+                 (<url> | --artist=<artist> --album=<album>)
+                 [--overwrite]
+                 [--no-art]
+  bandcamp-dl.py (-h | --help)
+  bandcamp-dl.py (--version)
+
+Options:
+  -h --help                 Show this screen.
+  -v --version              Show version.
+  -a --artist=<artist>      The artist's slug (from the URL)
+  -b --album=<album>        The album's slug (from the URL)
+  -t --template=<template>  Output filename template.
+                            [default: %{artist}/%{album}/%{track} - %{title}]
+  -d --base-dir=<dir>       Base location of which all files are downloaded.
+  -f --full-album           Download only if all tracks are available.
+  -o --overwrite            Overwrite tracks that already exist. Default is False.
+  -n --no-art               Skip grabbing album art
+
+Coded by:
+
+Iheanyi Ekechukwu
+    http://twitter.com/kwuchu
+    http://github.com/iheanyi
+
+Simon W. Jackson
+    http://miniarray.com
+    http://twitter.com/miniarray
+    http://github.com/miniarray
+
+Anthony Forsberg:
+    http://evolution0.github.io
+    http://github.com/evolution0
+
+Iheanyi:
+    Feel free to use this in any way you wish. I made this just for fun.
+    Shout out to darkf for writing a helper function for parsing the JavaScript!
+"""
+
+import os
+from docopt import docopt
+from .bandcamp import Bandcamp
+from .bandcampdownloader import BandcampDownloader
+
+
+def main():
+    arguments = docopt(__doc__, version='bandcamp-dl 0.0.6')
+    bandcamp = Bandcamp()
+
+    if arguments['--artist'] and arguments['--album']:
+        url = Bandcamp.generate_album_url(arguments['--artist'], arguments['--album'])
+    else:
+        url = arguments['<url>']
+
+    if arguments['--no-art']:
+        album = bandcamp.parse(url, False)
+    else:
+        album = bandcamp.parse(url)
+    basedir = arguments['--base-dir'] or os.getcwd()
+
+    if not album:
+        print("The url {} is not a valid bandcamp page.".format(url))
+    elif arguments['--full-album'] and not album['full']:
+        print("Full album not available. Skipping...")
+    else:
+        bandcamp_downloader = BandcampDownloader(url, arguments['--template'], basedir, arguments['--overwrite'])
+        bandcamp_downloader.start(album)
+
+if __name__ == '__main__':
+    main()
