a4a982331c51a1c417572ca3fcb79e0bc5f82733
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:13:47 +0000

    Add package logshuttle/0.2.0/logshuttle-0.2.0.tar.gz

---
+++ packages/logshuttle/logshuttle-0.2.0.tar.gz/logshuttle-0.2.0/README.md
@@ -0,0 +1,2 @@
+# logshuttle
+Python 3+ log handler for sending log records to Google Cloud Stackdriver service as custom logs in batches.
---
+++ packages/logshuttle/logshuttle-0.2.0.tar.gz/logshuttle-0.2.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+setup(
+    name="logshuttle",
+    version="0.2.0",
+    url="https://github.com/arunbala/logshuttle/",
+    license=open("LICENSE").read(),
+    author="Arun Bala",
+    author_email="arunprakashb@gmail.com",
+    description="Python 3 log handler for sending log records to Google Cloud Stackdriver service as custom logs in batches.",
+    install_requires=['google-cloud >= 0.22.0'],
+    tests_require=[],
+    extras_require={'test': []},
+    packages=find_packages(exclude=["test"]),
+    platforms=["MacOS X", "Posix"],
+    include_package_data=True,
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
