bb7221a143f208cc8a11ffdfdede5b8914f1c48f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:11 +0000

    Add package comicfetcher/0.1/comicfetcher-0.1.tar.gz

---
+++ packages/comicfetcher/comicfetcher-0.1.tar.gz/comicfetcher-0.1/comicfetcher/command_line.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+import datetime
+import re
+import six
+
+if six.PY2:
+    from urllib2 import urlretrieve
+    from urllib2 import quote
+    input = raw_input
+elif six.PY3:
+    from urllib.parse import quote
+    from urllib.request import urlretrieve
+
+FILE_TYPE = '.png'
+
+def main():
+	explosm()
+
+def explosm():
+	find = re.compile( r'.+?(?=' + re.escape(FILE_TYPE) + r')' )
+	now = datetime.datetime.now()
+	date = str(now.day)+'-'+str(now.month)+'-'+str(now.year) 
+	response = requests.get('http://explosm.net/')
+	soup = BeautifulSoup(response.text, 'html.parser')
+	comic_src = soup.find('img', {'id' : 'featured-comic'}).get('src')
+
+	found = re.match(find,comic_src).group()	
+	comic_url = 'http:'+found+FILE_TYPE
+	file_name = date + ' ' + found.split('/')[-1]
+	urllib.request.urlretrieve(comic_url, file_name + FILE_TYPE)
+	print('Saved %s'%(found.split('/')[-1]))
