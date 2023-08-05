026fa4516cc842eba77bf4bfb0711b382757a677
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:53 +0000

    Add package aetf/0.0.1/aetf-0.0.1.tar.gz

---
+++ packages/aetf/aetf-0.0.1.tar.gz/aetf-0.0.1/aetf/ae.py
@@ -0,0 +1,8 @@
+from markdown import markdown
+
+def dark_humour():
+    """It's a joke, you fool!"""
+    return markdown("HAHA!")
+def german():
+    """WWII Joke"""
+    return markdown("Two peanuts!!")
