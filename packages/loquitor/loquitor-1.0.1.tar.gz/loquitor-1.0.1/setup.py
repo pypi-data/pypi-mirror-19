d1a0f758dbbe77bc22695c33dea3ea95e31eaacd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:37 +0000

    Add package loquitor/1.0.1/loquitor-1.0.1.tar.gz

---
+++ packages/loquitor/loquitor-1.0.1.tar.gz/loquitor-1.0.1/setup.py
@@ -0,0 +1,17 @@
+import os
+
+from setuptools import setup
+config_path = os.path.join(os.path.expanduser("~"), ".loquitor")
+py_modules = ['bot', 'skeleton']
+setup(name='loquitor',
+      version='1.0.1',
+      description='Chatbot for Stack Overflow',
+      author='Ralph Embree',
+      author_email='ralph.embree@brominator.org',
+      url='https://github.com/ralphembree/Loquitor',
+      packages=['Loquitor', 'Loquitor.scripts'],
+      install_requires=['BingTranslator', 'feedparser', 'ChatExchange'],
+      dependency_links=['https://github.com/ByteCommander/ChatExchange6/tarball/master#egg=ChatExchange-1.0'],
+      scripts=['bin/loquitor'],
+      data_files=[(config_path, ['Loquitor/scripts/SUBSTITUTIONS.txt'])],
+)
