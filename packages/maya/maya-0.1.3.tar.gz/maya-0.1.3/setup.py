18e4ace5f03ef06b50a8b84a9486ddb7497500cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:48 +0000

    Add package maya/0.1.3/maya-0.1.3.tar.gz

---
+++ packages/maya/maya-0.1.3.tar.gz/maya-0.1.3/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import codecs
+
+from setuptools import setup
+
+try:
+    # Python 3
+    from os import dirname
+except ImportError:
+    # Python 2
+    from os.path import dirname
+
+here = os.path.abspath(dirname(__file__))
+
+def read(*parts):
+    return codecs.open(os.path.join(here, *parts), 'r').read()
+
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist bdist_wheel upload")
+    sys.exit()
+
+required = [
+    'humanize',
+    'pytz',
+    'dateparser',
+    'iso8601',
+    'python-dateutil',
+    'ruamel.yaml'
+]
+
+setup(
+    name='maya',
+    version='0.1.3',
+    description='Datetimes for Humans.',
+    long_description= '\n' + read('README.rst'),
+    author='Kenneth Reitz',
+    author_email='me@kennethreitz.com',
+    url='https://github.com/kennethreitz/maya',
+    py_modules=['maya'],
+    install_requires=required,
+    license='MIT',
+    classifiers=(
+
+    ),
+)
