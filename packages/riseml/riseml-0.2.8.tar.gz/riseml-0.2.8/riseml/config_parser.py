19c22a07d860c2e807b8b152ac0ffbba4b497f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:39 +0000

    Add package riseml/0.2.8/riseml-0.2.8.tar.gz

---
+++ packages/riseml/riseml-0.2.8.tar.gz/riseml-0.2.8/riseml/config_parser.py
@@ -0,0 +1,59 @@
+import sys
+import os
+import subprocess
+import contextlib
+
+import yaml
+import requests
+
+
+class ConfigException(Exception): pass
+
+
+@contextlib.contextmanager
+def chdir(new_dir):
+    old_dir = os.getcwd()
+    try:
+        os.chdir(new_dir)
+        sys.path.insert(0, new_dir)
+        yield
+    finally:
+        del sys.path[0]
+        os.chdir(old_dir)
+
+
+class Config(object):
+    image = None
+    commands = None
+
+
+def parse_text(text):
+    config = Config()
+    tmp = yaml.load(text)
+    if not 'image' in tmp:
+        raise ConfigException(u'missing key: image')
+    elif not 'script' in tmp:
+        raise ConfigException(u'missing key: script')
+
+    if isinstance(tmp['image'], list):
+        if len(tmp['image']) > 1:
+            raise ConfigException(u"you can only specify a single image")
+        config.image = tmp['image'][0]
+    else:
+        config.image = tmp['image']
+
+    if isinstance(tmp['script'], list):
+        config.commands = tmp['script']
+    else:
+        config.commands = [tmp['script']]
+
+    return config
+
+
+def parse(f):
+    return parse_text(f.read())
+
+
+def parse_file(filename):
+    with open(filename) as f:
+        return parse(f)
