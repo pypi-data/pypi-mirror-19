4c480cdb48722d02d2a26d86a26b9ad2617bb6d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:51 +0000

    Add package djenga/0.3dev/djenga-0.3dev.tar.gz

---
+++ packages/djenga/djenga-0.3dev.tar.gz/djenga-0.3dev/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.3'
+
+setup(name='djenga',
+      version=version,
+      description="Useful building blocks for Django.",
+      long_description="""\
+Useful building blocks for Django.""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='django python',
+      author='Preetam Shingavi',
+      author_email='p.shingavi@yahoo.com',
+      url='https://github.com/2ps/djenga',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'Django>=1.9',
+          'cssutils==1.0',
+          'beautifulsoup4==4.3.2',
+          'pycrypto>=2.6.1',
+          'pytz',
+          'psutil',
+          'python-dateutil',
+          'pyyaml',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
