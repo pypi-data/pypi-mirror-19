513f72a2959f8b8f4f610fe6c058c83d300ae0cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:28 +0000

    Add package django-dummy-thumbnails/0.1.5/django_dummy_thumbnails-0.1.5-py2.py3-none-any.whl

---
+++ packages/django-dummy-thumbnails/django_dummy_thumbnails-0.1.5-py2.py3-none-any.whl/dummy_thumbnails/tests/base.py
@@ -0,0 +1,90 @@
+"""
+Base tests module.
+
+- log_info: Method decorator, logs info about the method (name, return value).
+- LOG_INFO: Flag.
+- app_setup: Setup the app.
+- skip: Method decorator. If used, tests is skipped.
+- is_app_setup_completed: Check if app setup has been completed.
+- mark_app_setup_as_completed: Mark the app setup as completed.
+"""
+
+from __future__ import print_function
+
+import logging
+
+__title__ = 'dummy_thumbnails.tests.base'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2016 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = (
+    'LOG_INFO',
+    'log_info',
+    'app_setup',
+    'skip',
+    'is_app_setup_completed',
+    'mark_app_setup_as_completed',
+)
+
+
+logger = logging.getLogger(__name__)
+
+LOG_INFO = True
+
+
+def log_info(func):
+    """Log some useful info."""
+    if not LOG_INFO:
+        return func
+
+    def inner(self, *args, **kwargs):
+        """Inner."""
+        result = func(self, *args, **kwargs)
+
+        logger.info('\n%s', func.__name__)
+        logger.info('============================')
+        if func.__doc__:
+            logger.info('""" %s """', func.__doc__.strip())
+        logger.info('----------------------------')
+        if result is not None:
+            logger.info(result)
+        logger.info('\n')
+
+        return result
+    return inner
+
+
+SKIP = False
+
+
+def skip(func):
+    """Simply skip the test."""
+    def inner(self, *args, **kwargs):
+        """Inner."""
+        if SKIP:
+            return
+        return func(self, *args, **kwargs)
+    return inner
+
+
+class AppSetup(object):
+    """Basic setup class.
+
+    Created in order to avoid the app test data to be initialised
+    multiple times.
+    """
+    def __init__(self):
+        self.is_done = False
+
+
+app_setup = AppSetup()
+
+
+def is_app_setup_completed():
+    """Check if app setup is completed."""
+    return app_setup.is_done is True
+
+
+def mark_app_setup_as_completed():
+    """Mark app setup as completed."""
+    app_setup.is_done = True
