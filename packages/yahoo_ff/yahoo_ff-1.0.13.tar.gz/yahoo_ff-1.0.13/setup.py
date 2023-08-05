af732687a632d024294309a3881129be031d786c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:34 +0000

    Add package yahoo_ff/1.0.13/yahoo_ff-1.0.13.tar.gz

---
+++ packages/yahoo_ff/yahoo_ff-1.0.13.tar.gz/yahoo_ff-1.0.13/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+import os
+long_description = 'Add a fallback short description here'
+if os.path.exists('README.txt'):
+    long_description = open('README.txt').read()
+
+setup(
+    name = "yahoo_ff",
+    version = "1.0.13",
+    author = "Alexandre Sobolevski",
+    author_email = "sobolevski.a@gmail.com",
+    description = "Quick module to scrape yahoo financial data for stocks.",
+    license = "MIT",
+    url = "http://github.com/alexandresobolevski/yahoo_ff",
+    long_description=long_description
+)
