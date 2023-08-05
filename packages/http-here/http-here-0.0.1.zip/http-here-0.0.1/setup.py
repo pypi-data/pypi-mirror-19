95d089f308e94e4749a3fa49cd88d560b211400b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:50 +0000

    Add package http-here/0.0.1/http-here-0.0.1.zip

---
+++ packages/http-here/http-here-0.0.1.zip/http-here-0.0.1/setup.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import setuptools  # noqa
+from distutils.core import setup
+import io
+import re
+import os
+
+
+def read(*names, **kwargs):
+    return io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ).read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(name='http-here',
+      version=find_version('run.py'),
+      description=('Simple Python HTTP server short for SimpleHTTPServer.'),
+      long_description=open('README.md', 'rt').read(),
+      author='hustcc',
+      author_email='i@hust.cc',
+      url='https://github.com/hustcc',
+      license='MIT',
+      install_requires=[
+        'click'
+      ],
+      classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Natural Language :: Chinese (Simplified)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Utilities'
+      ],
+      keywords='server, http, httpserver, simple, SimpleHTTPServer',
+      py_modules=['run'],
+      entry_points={
+        'console_scripts': ['http-here=run:run']
+      })
