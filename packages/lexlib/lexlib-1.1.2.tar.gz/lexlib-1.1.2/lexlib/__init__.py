edcec2499755fa5f72b91fa21418e552ecb5f58f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:50 +0000

    Add package lexlib/1.1.2/lexlib-1.1.2.tar.gz

---
+++ packages/lexlib/lexlib-1.1.2.tar.gz/lexlib-1.1.2/lexlib/__init__.py
@@ -0,0 +1,4 @@
+from .clusters import *
+from .get_words import *
+from .neighbors import *
+from .syllable_filter import *
---
+++ packages/lexlib/lexlib-1.1.2.tar.gz/lexlib-1.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+ver = "1.1.2"
+repo = "https://github.com/cranndarach/lexlib"
+setup(name="lexlib",
+      version=ver,
+      description="Utilities for research involving words.",
+      keywords="psycholinguistics research words language linguistics \
+                psychology cognitive science cogsci",
+      url=repo,
+      download_url="{}/tarball/{}".format(repo, ver),
+      author="R. J. Steiner",
+      author_email="r.steiner@uconn.edu",
+      license="MIT",
+      packages=["lexlib"],
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Science/Research",
+          "License :: OSI Approved :: MIT License"
+          ],
+      install_requires=["pandas"]
+      )
