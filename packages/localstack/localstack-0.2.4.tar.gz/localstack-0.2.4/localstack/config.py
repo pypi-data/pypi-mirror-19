13844e1ee079aabdf8b92f3c8f7ab86de2713044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:45 +0000

    Add package localstack/0.2.4/localstack-0.2.4.tar.gz

---
+++ packages/localstack/localstack-0.2.4.tar.gz/localstack-0.2.4/localstack/config.py
@@ -0,0 +1,6 @@
+
+# Ratio (range [0,1]) of Kinesis PutRecord and PutRecords API calls
+# that should be rejected with a ProvisionedThroughputExceededException
+# exception. This value defaults to 0, but can be dynamically set to
+# a non-zero value for testing.
+KINESIS_THROTTLE_EXCEPTION_RATIO = 0
