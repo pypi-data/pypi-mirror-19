a2e21969cfedb59b2dd494afb1f43a6cbc3152f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:22 +0000

    Add package markdown-i18n/1.2.0/markdown-i18n-1.2.0.tar.gz

---
+++ packages/markdown-i18n/markdown-i18n-1.2.0.tar.gz/markdown-i18n-1.2.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='markdown-i18n',
+    version='1.2.0',
+    packages=find_packages(),
+    url='https://github.com/gisce/markdown-i18n',
+    license='MIT',
+    install_requires=['Markdown', 'babel'],
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    test_suite="tests",
+    description=''
+)
