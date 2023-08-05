c0690794e8bdfa59134dd201674a903a125c4ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:44 +0000

    Add package abi42/1.4.0/abi42-1.4.0.tar.gz

---
+++ packages/abi42/abi42-1.4.0.tar.gz/abi42-1.4.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+
+    name = 'abi42',
+    version = '1.4.0',
+    py_modules = ['nester'],
+    author = 'abhinav',
+    author_email = 'abhinav8@connect.hku.hk',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+    )
