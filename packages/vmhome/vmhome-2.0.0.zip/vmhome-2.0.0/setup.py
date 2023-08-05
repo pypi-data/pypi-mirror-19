97cf3784ce97e9b2f90d66c280739999ad774ff9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:13 +0000

    Add package vmhome/2.0.0/vmhome-2.0.0.zip

---
+++ packages/vmhome/vmhome-2.0.0.zip/vmhome-2.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'vmhome',
+        version = '2.0.0',
+        py_modules = ['print_list'],
+        author = 'jayhebe',
+        author_email = 'jayhebe1983@sina.com',
+        url = 'http://www.vmhome.me',
+        description = 'A simple printer of nested lists',
+    )
