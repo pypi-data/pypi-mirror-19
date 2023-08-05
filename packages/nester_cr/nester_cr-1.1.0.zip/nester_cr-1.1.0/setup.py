ffdd0200be8a452abb5697a3d7ef04bc189157cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:57 +0000

    Add package nester_cr/1.1.0/nester_cr-1.1.0.zip

---
+++ packages/nester_cr/nester_cr-1.1.0.zip/nester_cr-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup (
+	name           = 'nester_cr',
+	version        = '1.1.0',
+	py_modules = ['nester_cr'],
+	author           = 'clickRun',
+	author_email = 'hfpython@headfirstlabs.com',
+	url              = 'http://www.haedfirstlabs.com',
+	description   = ' A simple printer of nested lists_cr',
+	)
