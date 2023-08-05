6482c2261ca6dfc7fdf34e8ab4c72710ffa3bd0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:50 +0000

    Add package sdklib/1.7/sdklib-1.7-py2.7.egg

---
+++ packages/sdklib/sdklib-1.7-py2.7.egg/sdklib/compat.py
@@ -0,0 +1,38 @@
+import sys
+
+
+_ver = sys.version_info
+
+#: Python 2.x?
+is_py2 = (_ver[0] == 2)
+
+#: Python 3.x?
+is_py3 = (_ver[0] == 3)
+
+if is_py2:
+    import Cookie as cookies
+    from urllib import urlencode, quote_plus
+    from urlparse import urlsplit
+    import SocketServer as socketserver
+    import thread
+    from StringIO import StringIO
+
+    basestring = basestring
+    str = unicode
+elif is_py3:
+    from urllib.parse import urlencode, quote_plus, urlsplit
+    from urllib.parse import urlencode
+    from http import cookies
+    import socketserver
+    import _thread as thread
+    from io import StringIO
+
+    basestring = (str, bytes)
+    str = str
+
+
+try:
+    import lxml
+    html_lxml = True
+except:
+    html_lxml = False
