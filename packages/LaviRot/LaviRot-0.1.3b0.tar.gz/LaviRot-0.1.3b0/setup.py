e693d559091584d634be90b73d24c5b7bb501ceb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:35 +0000

    Add package LaviRot/0.1.3b0/LaviRot-0.1.3b0.tar.gz

---
+++ packages/LaviRot/LaviRot-0.1.3b0.tar.gz/LaviRot-0.1.3b0/setup.py
@@ -0,0 +1,12 @@
+# from distutils.core import setup
+from setuptools import setup
+
+setup(name='LaviRot',
+      version='0.1.3b',
+      description='LaviRot',
+      author='Lavi',
+      author_email='raphaelts@gmail.com',
+      packages=['LaviRot'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+     )
