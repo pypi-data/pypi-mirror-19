b1a047d6a460feb5fcc87774b18a21e6e248cd2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:41 +0000

    Add package discurses/0.2.0/discurses-0.2.0.tar.gz

---
+++ packages/discurses/discurses-0.2.0.tar.gz/discurses-0.2.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md')) as f:
+    readme = f.read()
+
+with open(path.join(here, 'LICENSE')) as f:
+    license = f.read()
+
+setup(
+    name='discurses',
+    version='0.2.0',
+    description='Discord CLI written in python, based on urwid',
+    long_description=readme,
+    author='Topisani',
+    author_email='topisani@hamsterpoison.com',
+    url='https://github.com/topisani/discurses',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=['urwid', 'discord.py', 'pyyaml'],
+    entry_points={
+        'console_scripts': [
+            'discurses=discurses:main',
+        ],
+    },
+)
