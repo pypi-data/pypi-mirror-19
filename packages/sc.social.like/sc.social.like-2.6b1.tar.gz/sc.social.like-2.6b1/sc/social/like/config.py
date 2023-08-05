b4bf8fb21692df3478b0b66bf9ec079cffd985fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:57 +0000

    Add package sc.social.like/2.6b1/sc.social.like-2.6b1.tar.gz

---
+++ packages/sc.social.like/sc.social.like-2.6b1.tar.gz/sc.social.like-2.6b1/sc/social/like/config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+PROJECTNAME = 'sc.social.like'
+
+DEFAULT_ENABLED_CONTENT_TYPES = ('Document', 'Event', 'News Item')
+DEFAULT_PLUGINS_ENABLED = ('Facebook', 'Twitter')
+
+TILES = [
+    u'sc.social.like.facebook',
+    u'sc.social.like.twitter',
+]
