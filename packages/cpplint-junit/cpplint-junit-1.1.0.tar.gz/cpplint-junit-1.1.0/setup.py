64c29a91427806ca0b2153a659763fd3ccc3addc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:36 +0000

    Add package cpplint-junit/1.1.0/cpplint-junit-1.1.0.tar.gz

---
+++ packages/cpplint-junit/cpplint-junit-1.1.0.tar.gz/cpplint-junit-1.1.0/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup
+
+import cpplint_junit
+
+
+setup(
+    name='cpplint-junit',
+    version=cpplint_junit.__version__,
+
+    description='Converts cpplint output to JUnit format.',
+    long_description=open('README.rst').read(),
+    keywords='cpplint C++ Google JUnit',
+
+    author='John Hagen',
+    author_email='johnthagen@gmail.com',
+    url='https://github.com/johnthagen/cpplint-junit',
+    license='MIT',
+
+    py_modules=['cpplint_junit'],
+    install_requires=open('requirements.txt').readlines(),
+    zip_safe=False,
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: C++',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Quality Assurance',
+    ],
+
+    scripts=['cpplint_junit.py'],
+
+    entry_points={
+        'console_scripts': [
+            'cpplint_junit = cpplint_junit:main',
+        ],
+    }
+)
