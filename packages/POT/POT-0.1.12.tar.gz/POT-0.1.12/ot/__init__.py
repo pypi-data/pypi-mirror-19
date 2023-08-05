5a516518ae36c3e202b7d88de0cc5881659956ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:14 +0000

    Add package POT/0.1.12/POT-0.1.12.tar.gz

---
+++ packages/POT/POT-0.1.12.tar.gz/POT-0.1.12/ot/__init__.py
@@ -0,0 +1,24 @@
+"""Python Optimal Transport toolbox"""
+
+
+# All submodules and packages
+from . import lp
+from . import bregman
+from . import optim
+from . import utils
+from . import datasets
+from . import plot
+from . import da
+
+# OT functions
+from .lp import emd
+from .bregman import sinkhorn, barycenter
+from .da import sinkhorn_lpl1_mm
+
+# utils functions
+from .utils import dist, unif, tic, toc, toq
+
+__version__ = "0.1.12"
+
+__all__ = ["emd", "sinkhorn", "utils", 'datasets', 'bregman', 'lp', 'plot',
+           'dist', 'unif', 'barycenter', 'sinkhorn_lpl1_mm', 'da', 'optim']
