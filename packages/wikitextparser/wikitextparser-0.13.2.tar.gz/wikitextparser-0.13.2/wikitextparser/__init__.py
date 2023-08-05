f7630da88581c0080ff0e7c16cfe8adfee969afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:35 +0000

    Add package wikitextparser/0.13.2/wikitextparser-0.13.2.tar.gz

---
+++ packages/wikitextparser/wikitextparser-0.13.2.tar.gz/wikitextparser-0.13.2/wikitextparser/__init__.py
@@ -0,0 +1,35 @@
+"""Initialize the wikitextparser."""
+
+
+import regex as _regex
+
+from .parameter import Parameter
+from .argument import Argument
+from .externallink import ExternalLink
+from .wikilink import WikiLink
+from .section import Section
+from .comment import Comment
+from . import wikitext as _wikitext
+from .table import Table
+from .template import Template
+from .parser_function import ParserFunction
+from .tag import Tag
+from .wikilist import WikiList
+from .wikilist import LIST_PATTERN as _LIST_PATTERN
+
+
+_regex.DEFAULT_VERSION = _regex.VERSION1
+_wikitext.ExternalLink = ExternalLink
+_wikitext.WikiLink = WikiLink
+_wikitext.Template = Template
+_wikitext.Comment = Comment
+_wikitext.ParserFunction = ParserFunction
+_wikitext.Parameter = Parameter
+_wikitext.Table = Table
+_wikitext.Section = Section
+_wikitext.WikiList = WikiList
+_wikitext.LIST_PATTERN = _LIST_PATTERN
+
+
+WikiText = _wikitext.WikiText
+parse = WikiText
