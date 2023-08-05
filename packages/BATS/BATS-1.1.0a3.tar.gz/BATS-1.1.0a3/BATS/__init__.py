9c24b04974c28ee5c8bb6a65a7081395a221afda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:03 +0000

    Add package BATS/1.1.0a3/BATS-1.1.0a3.tar.gz

---
+++ packages/BATS/BATS-1.1.0a3.tar.gz/BATS-1.1.0a3/BATS/__init__.py
@@ -0,0 +1,17 @@
+"""
+A fast python module with friendly GUI for Bayesian Multi-Arm Multi-Stage Design
+"""
+import sys as _sys
+import os as _os
+
+_sys.path.append(_os.path.abspath('../'))
+
+__version__ = '1.1.0a3'
+__author__ = 'Zhenning Yu'
+__email__ = 'yuzhenning.bio@gmail.com'
+__All__=['__init__', 'main', 'TrialSimulation', 'CalPosteriorProbability']
+
+from .BATS import __init__
+from .BATS import main
+from .BATS import FixedTrial as TrialSimulation
+from .BATS import CalPosteriorProbability as CalPosteriorProbability
