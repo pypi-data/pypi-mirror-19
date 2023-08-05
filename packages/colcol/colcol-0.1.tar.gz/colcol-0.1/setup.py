e17a12127fe15b85c85dfcaa0475dfad02da4b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:08 +0000

    Add package colcol/0.1/colcol-0.1.tar.gz

---
+++ packages/colcol/colcol-0.1.tar.gz/colcol-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+ 
+setup(
+	name='colcol',    # This is the name of your PyPI-package.
+	description='A script to deal with color conversions, color transformations, and generating color scales.',       #package description
+	version='0.1',                          # Update the version number for new releases
+	scripts=['colcol'],                  # The name of your scipt, and also the command you'll be using for calling it
+	author='Martin Engqvist',
+	author_email='martin_engqvist@hotmail.com'
+)
+
+
+
+#for dependancies
+#install_requires=[
+#          'markdown',
+#      ],
