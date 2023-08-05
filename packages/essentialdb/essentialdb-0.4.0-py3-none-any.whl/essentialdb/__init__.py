92fc762e07b4e061a081e1c1811cbc8f5ad802ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:10 +0000

    Add package essentialdb/0.4.0/essentialdb-0.4.0-py3-none-any.whl

---
+++ packages/essentialdb/essentialdb-0.4.0-py3-none-any.whl/essentialdb/__init__.py
@@ -0,0 +1,6 @@
+__author__ = 'scmason'
+from .keys import Keys
+from .essential_index import EssentialIndex
+from .query_filter import QueryFilter
+from .simplecollection import SimpleCollection
+from .essentialdb import EssentialDB
