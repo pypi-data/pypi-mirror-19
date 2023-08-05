0a179c4bf75f7630ad98b3b1d7bdd7d6d9ce7396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:27 +0000

    Add package precisely/0.1.0/precisely-0.1.0.tar.gz

---
+++ packages/precisely/precisely-0.1.0.tar.gz/precisely-0.1.0/precisely/__init__.py
@@ -0,0 +1,24 @@
+from .core_matchers import equal_to, anything, all_of
+from .object_matchers import has_attr, has_attrs, instance_of
+from .iterable_matchers import contains_exactly, is_same_sequence
+from .feature_matchers import has_feature
+
+
+__all__ = [
+    "assert_that",
+    "equal_to",
+    "anything",
+    "all_of",
+    "has_attr",
+    "has_attrs",
+    "instance_of",
+    "contains_exactly",
+    "is_same_sequence",
+    "has_feature",
+]
+
+
+def assert_that(value, matcher):
+    result = matcher.match(value)
+    if not result.is_match:
+        raise AssertionError("\nExpected: {0}\nbut: {1}".format(matcher.describe(), result.explanation))
---
+++ packages/precisely/precisely-0.1.0.tar.gz/precisely-0.1.0/precisely/core_matchers.py
@@ -0,0 +1,53 @@
+from .results import matched, unmatched, indented_list
+
+
+def equal_to(value):
+    return EqualToMatcher(value)
+
+
+class EqualToMatcher(object):
+    def __init__(self, value):
+        self._value = value
+    
+    def match(self, actual):
+        if self._value == actual:
+            return matched()
+        else:
+            return unmatched("was {0!r}".format(actual))
+    
+    def describe(self):
+        return repr(self._value)
+
+
+class AnyThingMatcher(object):
+    def match(self, actual):
+        return matched()
+    
+    def describe(self):
+        return "anything"
+
+
+anything = AnyThingMatcher()
+
+
+def all_of(*matchers):
+    return AllOfMatcher(matchers)
+
+class AllOfMatcher(object):
+    def __init__(self, matchers):
+        self._matchers = matchers
+    
+    def match(self, actual):
+        for matcher in self._matchers:
+            result = matcher.match(actual)
+            if not result.is_match:
+                return result
+                
+        return matched()
+    
+    def describe(self):
+        return "all of:{0}".format(indented_list(
+            matcher.describe()
+            for matcher in self._matchers
+        ))
+
