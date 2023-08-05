84170dd3234b835da2fbfe95786a2b4cce778c5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:08 +0000

    Add package component_builder/1.0.1.101/component_builder-1.0.1.101-py2-none-any.whl

---
+++ packages/component_builder/component_builder-1.0.1.101-py2-none-any.whl/component_builder/config.py
@@ -0,0 +1,32 @@
+from collections import OrderedDict
+import os.path
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import ConfigParser
+
+from .component import Component
+
+
+def read_component_configuration(builder_ini_file, root='.'):
+    config = ConfigParser(defaults={
+        'upstream': '',
+        'release-process': ''
+    })
+    config.readfp(builder_ini_file)
+    components = OrderedDict()
+    for component in config.sections():
+        ini_section_dict = dict(config.items(component))
+
+        kwargs = {
+            'title': component,
+            'release_process': config.get(component, 'release-process'),
+            'path': os.path.join(root, config.get(component, 'path')),
+            'ini': ini_section_dict,
+        }
+        upstream = config.get(component, 'upstream')
+        if upstream:
+            kwargs['upstream'] = upstream.split(',')
+        c = Component(**kwargs)
+        components[c.title] = c
+    return components
