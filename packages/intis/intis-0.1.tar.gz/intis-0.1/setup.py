c92543ce8f36225e8796159b6d2a005e3415b33d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:04 +0000

    Add package intis/0.1/intis-0.1.tar.gz

---
+++ packages/intis/intis-0.1.tar.gz/intis-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='intis',
+    packages=['intis'],
+    version='0.1',
+    description='A Python 2/3 client for the Intis',
+    author='Ilya Mukhortov',
+    author_email='ilya.muhortov@gmail.com',
+    url='https://github.com/intistelecom/Intis-Telecom-SDK-Python',
+    download_url='https://github.com/intistelecom/Intis-Telecom-SDK-Python/archive/master.zip',
+    keywords=['api', 'sms'],
+    classifiers=[]
+)
