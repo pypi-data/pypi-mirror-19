17b747aa8872a744f167fb34170aa9eb73d38807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:15 +0000

    Add package plugs-auth/0.1.5/plugs-auth-0.1.5.tar.gz

---
+++ packages/plugs-auth/plugs-auth-0.1.5.tar.gz/plugs-auth-0.1.5/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='plugs-auth',
+    package = 'plugs_auth',
+    version='0.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    description='Reusable Authentication APP',
+    long_description=README,
+    url='https://github.com/yo-solo/plugs-auth',
+    author='Ricardo Lobo',
+    author_email='ricardolobo@soloweb.pt',
+    install_requires = [
+        'plugs-core>=0.1.0',
+        'plugs-mail>=0.1.0'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
