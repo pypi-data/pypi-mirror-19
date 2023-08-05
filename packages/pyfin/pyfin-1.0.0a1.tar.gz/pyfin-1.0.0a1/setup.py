b8cf16bb92c4eba3dbfcefaabfbeea1bc44c73fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:05 +0000

    Add package pyfin/1.0.0a1/pyfin-1.0.0a1.tar.gz

---
+++ packages/pyfin/pyfin-1.0.0a1.tar.gz/pyfin-1.0.0a1/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+
+setuptools.setup(
+    name="pyfin",
+    version='1.0.0a1',
+    author='Leon Zhang',
+    author_email='pku09zl@gmail.com',
+    description='Financial toolkit for Quant',
+    keywords='python finance quant toolkit',
+    url='https://github.com/X0Leon/pyfin',
+    license='MIT',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'scipy',
+        'tabulate',
+        'matplotlib',
+        'scikit-learn',
+        'requests'
+    ],
+    packages=['pyfin'],
+    long_description='Financial toolkit for Quantitative investment of Chinese market built on pandas',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development :: Libraries',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        # 'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+    ]
+)
