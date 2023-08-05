63bb4c1fce2094e69a5074a169446489ca501f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:59 +0000

    Add package Helsonnester/1.0.0/Helsonnester-1.0.0.zip

---
+++ packages/Helsonnester/Helsonnester-1.0.0.zip/Helsonnester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'Helsonnester',
+version = '1.0.0',
+py_modules = ['Helsonnester'],
+author = 'Helsonkumar',
+author_email = 'helsonkumar@gmail.com',
+url = 'http://www.headfirstlabs.com',
+description = 'A simple printer of nested lists',
+)
