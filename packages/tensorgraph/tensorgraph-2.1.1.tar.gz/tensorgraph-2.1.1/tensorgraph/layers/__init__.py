d935560e094fb5718e31c8873bc7139b0a04be9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:53 +0000

    Add package tensorgraph/2.1.1/tensorgraph-2.1.1.tar.gz

---
+++ packages/tensorgraph/tensorgraph-2.1.1.tar.gz/tensorgraph-2.1.1/tensorgraph/layers/__init__.py
@@ -0,0 +1,12 @@
+
+from .activation import *
+from .conv import *
+from .linear import *
+from .merge import *
+from .noise import *
+from .normalization import *
+from .random import *
+from .misc import *
+from .iterative import *
+from .recurrent import *
+from .template import *
---
+++ packages/tensorgraph/tensorgraph-2.1.1.tar.gz/tensorgraph-2.1.1/tensorgraph/layers/merge.py
@@ -0,0 +1,52 @@
+
+import tensorflow as tf
+
+
+class Merge(object):
+    '''
+    Merge layer is used to merge the list of states from layer below into one state
+    '''
+    def _train_fprop(self, state_list):
+        '''state_list (list): list of states to be merged
+        '''
+        raise NotImplementedError()
+
+    def _test_fprop(self, state_list):
+        '''Defines the forward propogation through the layer during testing,
+           defaults to the same as train forward propogation
+        '''
+        return self._train_fprop(state_list)
+
+    @property
+    def _variables(self):
+        '''Defines the trainable parameters in the layer
+        '''
+        return []
+
+
+class Concat(Merge):
+    def _train_fprop(self, state_list):
+        return tf.concat(1, state_list)
+
+
+class Mean(Merge):
+    def _train_fprop(self, state_list):
+        return tf.add_n(state_list) / len(state_list)
+
+
+class Sum(Merge):
+    def _train_fprop(self, state_list):
+        return tf.add_n(state_list)
+
+
+class NoChange(Merge):
+    def _train_fprop(self, state_list):
+        return state_list
+
+
+class Multiply(Merge):
+    def _train_fprop(self, state_list):
+        out = state_list[0]
+        for state in state_list[1:]:
+            out = tf.mul(out, state)
+        return out
