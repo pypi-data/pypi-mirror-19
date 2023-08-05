1e4ce7a076838f1ad31e06280f467ddd52ec5145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:29 +0000

    Add package django-celery-model/0.1.3/django_celery_model-0.1.3-py2-none-any.whl

---
+++ packages/django-celery-model/django_celery_model-0.1.3-py2-none-any.whl/djcelery_model/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+django-celery-model is an extension to Celery which adds support
+for tracking Celery tasks assigned to Django model instances.
+"""
+
+__version_info__ = {
+    'major': 0,
+    'minor': 1,
+    'micro': 3,
+    'releaselevel': 'final',
+}
+
+def get_version():
+    """
+    Return the formatted version information
+    """
+    vers = ["%(major)i.%(minor)i" % __version_info__, ]
+
+    if __version_info__['micro']:
+        vers.append(".%(micro)i" % __version_info__)
+    if __version_info__['releaselevel'] != 'final':
+        vers.append('%(releaselevel)s' % __version_info__)
+    return ''.join(vers)
+
+__version__ = get_version()
