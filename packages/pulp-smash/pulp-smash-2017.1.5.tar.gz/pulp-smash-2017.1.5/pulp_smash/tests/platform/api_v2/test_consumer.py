30290c9979f7bad9c1bdae0462a4f55c8c5c2f1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:21 +0000

    Add package pulp-smash/2017.1.5/pulp-smash-2017.1.5.tar.gz

---
+++ packages/pulp-smash/pulp-smash-2017.1.5.tar.gz/pulp-smash-2017.1.5/pulp_smash/tests/platform/api_v2/test_consumer.py
@@ -0,0 +1,71 @@
+# coding=utf-8
+"""Test the `consumer`_ API endpoints.
+
+.. _consumer:
+    https://docs.pulpproject.org/en/latest/dev-guide/integration/rest-api/consumer/index.html
+"""
+from urllib.parse import urljoin
+
+from pulp_smash import api, utils
+from pulp_smash.constants import CONSUMER_PATH, REPOSITORY_PATH
+from pulp_smash.tests.rpm.api_v2.utils import gen_repo, gen_distributor
+
+
+class BindConsumerTestCase(utils.BaseAPITestCase):
+    """Show that one can `bind a consumer to a repository`_.
+
+    .. _bind a consumer to a repository:
+        https://docs.pulpproject.org/en/latest/dev-guide/integration/rest-api/consumer/bind.html#bind-a-consumer-to-a-repository
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """Bind a consumer to a distributor.
+
+        Do the following:
+
+        1. Add a consumer.
+        2. Add a repository.
+        3. Add a distributor to the repository.
+        4. Bind the consumer to the distributor.
+        """
+        super(BindConsumerTestCase, cls).setUpClass()
+
+        # Steps 1–3
+        client = api.Client(cls.cfg, api.json_handler)
+        cls.consumer = client.post(CONSUMER_PATH, {'id': utils.uuid4()})
+        repository = client.post(REPOSITORY_PATH, gen_repo())
+        distributor = client.post(
+            urljoin(repository['_href'], 'distributors/'),
+            gen_distributor()
+        )
+        cls.resources.add(repository['_href'])
+
+        # Step 4
+        client.response_handler = api.safe_handler
+        path = urljoin(CONSUMER_PATH, cls.consumer['consumer']['id'] + '/')
+        path = urljoin(path, 'bindings/')
+        cls.request = {
+            'binding_config': {'B': 21},
+            'distributor_id': distributor['id'],
+            'notify_agent': False,
+            'repo_id': distributor['repo_id'],
+        }
+        cls.response = client.post(path, cls.request)
+
+    def test_status_code(self):
+        """Assert the "bind" request returned an HTTP 200."""
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_result(self):
+        """Assert the distributor has a correct set of attributes."""
+        result = self.response.json()['result']
+        expect = {
+            'binding_config': result['binding_config'],
+            'consumer_id': self.consumer['consumer']['id'],
+            'distributor_id': result['distributor_id'],
+            'repo_id': result['repo_id'],
+        }
+        for key, value in expect.items():
+            with self.subTest(key=key):
+                self.assertEqual(result[key], value)
---
+++ packages/pulp-smash/pulp-smash-2017.1.5.tar.gz/pulp-smash-2017.1.5/pulp_smash/tests/rpm/api_v2/test_unassociate.py
@@ -0,0 +1,187 @@
+# coding=utf-8
+"""Test `Unassociating Content Units from a Repository`_ for RPM.
+
+This module assumes that the tests in
+:mod:`pulp_smash.tests.platform.api_v2.test_repository` and
+:mod:`pulp_smash.tests.rpm.api_v2.test_sync_publish` hold true.
+
+.. _Unassociating Content Units from a Repository:
+   http://docs.pulpproject.org/en/latest/dev-guide/integration/rest-api/content/associate.html#unassociating-content-units-from-a-repository
+"""
+import random
+import time
+from urllib.parse import urljoin
+
+from dateutil.parser import parse
+
+from pulp_smash import api, utils
+from pulp_smash.constants import REPOSITORY_PATH, RPM_SIGNED_FEED_URL
+from pulp_smash.tests.rpm.api_v2.utils import gen_distributor, gen_repo
+from pulp_smash.tests.rpm.utils import set_up_module as setUpModule  # noqa pylint:disable=unused-import
+
+_RPM_ID_FIELD = 'checksum'
+# RPM units do not have an ``id`` metadata field. This is problematic when
+# trying to select a specific RPM content unit, as the remaining metadata
+# fields may be non-unique. For example, two different RPM content units may
+# both have a name of "walrus" — they just provide different versions. This
+# constant attempts to name a substitute for an ID.
+
+
+def _get_unit_id(unit):
+    """Return the unit's _RPM_ID_FIELD if an RPM. Otherwise, return ID."""
+    if unit['unit_type_id'] == 'rpm':
+        return unit['metadata'][_RPM_ID_FIELD]
+    return unit['metadata']['id']
+
+
+def _get_units_by_type(units, type_id):
+    """Return a list of units having the given unit type ID."""
+    return [unit for unit in units if unit['unit_type_id'] == type_id]
+
+
+def _remove_unit(server_config, repo_href, unit):
+    """Remove unit ``unit`` from the repository at ``repo_href``.
+
+    Return the JSON-decoded response body.
+    """
+    path = urljoin(repo_href, 'actions/unassociate/')
+    type_id = unit['unit_type_id']  # e.g. 'rpm'
+    key = _RPM_ID_FIELD if type_id == 'rpm' else 'id'
+    body = {'criteria': {
+        'filters': {'unit': {key: unit['metadata'][key]}},
+        'type_ids': [type_id],
+    }}
+    return api.Client(server_config).post(path, body).json()
+
+
+def _search_units(server_config, repo_href, type_ids):
+    """Find units of types ``type_ids`` in the repository at ``repo_href``.
+
+    Return the JSON-decoded response body.
+    """
+    return api.Client(server_config).post(
+        urljoin(repo_href, 'search/units/'),
+        {'criteria': {'type_ids': type_ids}},
+    ).json()
+
+
+class RemoveUnitsTestCase(utils.BaseAPITestCase):
+    """Remove units of various types from a synced RPM repository."""
+
+    TYPE_IDS = [
+        'erratum',
+        'package_category',
+        'package_group',
+        'rpm',
+    ]
+    """IDs of unit types that can be removed from an RPM repository."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Create an RPM repository, sync it, and remove some units from it.
+
+        After creating and syncing an RPM repository, we walk through the unit
+        type IDs listed in
+        :data:`pulp_smash.tests.rpm.api_v2.test_unassociate.RemoveUnitsTestCase.TYPE_IDS`
+        and remove on unit of each kind from the repository. We verify Pulp's
+        behaviour by recording repository contents pre and post removal.
+        """
+        super(RemoveUnitsTestCase, cls).setUpClass()
+        client = api.Client(cls.cfg, api.json_handler)
+        body = gen_repo()
+        body['importer_config']['feed'] = RPM_SIGNED_FEED_URL
+        repo = client.post(REPOSITORY_PATH, body)
+        cls.resources.add(repo['_href'])
+        utils.sync_repo(cls.cfg, repo['_href'])
+
+        # Remove one unit of each type.
+        cls.units_before = _search_units(cls.cfg, repo['_href'], cls.TYPE_IDS)
+        cls.units_removed = []
+        for type_id in cls.TYPE_IDS:
+            unit = random.choice(_get_units_by_type(cls.units_before, type_id))
+            cls.units_removed.append(unit)
+            _remove_unit(cls.cfg, repo['_href'], unit)
+        cls.units_after = _search_units(cls.cfg, repo['_href'], cls.TYPE_IDS)
+
+    def test_units_not_in_search(self):
+        """Assert the removed units do not appear in search results."""
+        ids_removed = {_get_unit_id(unit) for unit in self.units_removed}
+        ids_after = {_get_unit_id(unit) for unit in self.units_after}
+        self.assertEqual(ids_removed & ids_after, set())  # s1.isdisjoint(s2)
+
+    def test_one_removal_per_unit_type(self):
+        """Assert, for each unit type, that one unit has been removed."""
+        for type_id in self.TYPE_IDS:
+            with self.subTest(type_id=type_id):
+                before = _get_units_by_type(self.units_before, type_id)
+                after = _get_units_by_type(self.units_after, type_id)
+                self.assertEqual(len(before) - 1, len(after))
+
+
+class RemoveAndRepublishTestCase(utils.BaseAPITestCase):
+    """Publish a repository, remove a unit, and publish it again.
+
+    The removed unit should be inaccessible after the repository is published a
+    second time. In addition, repository and distributor timestamps should be
+    updated accordingly.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """Publish a repository, remove a unit, and publish it again.
+
+        Specifically, do the following:
+
+        1. Create a repository with a feed and distributor.
+        2. Sync and publish the repository.
+        3. Select a content unit at random. Remove it from the repository.
+        4. Publish the repository.
+        """
+        super(RemoveAndRepublishTestCase, cls).setUpClass()
+
+        # Create a repository with a feed and distributor.
+        client = api.Client(cls.cfg, api.json_handler)
+        body = gen_repo()
+        body['importer_config']['feed'] = RPM_SIGNED_FEED_URL
+        body['distributors'] = [gen_distributor()]
+        repo = client.post(REPOSITORY_PATH, body)
+        repo = client.get(repo['_href'], params={'details': True})
+        cls.resources.add(repo['_href'])  # mark for deletion
+
+        # Sync and publish the repository.
+        utils.sync_repo(cls.cfg, repo['_href'])
+        utils.publish_repo(cls.cfg, repo)
+
+        # List RPM content units in the repository. Pick one and remove it.
+        # NOTE: There are two versions of the "walrus" RPM, and this works even
+        # when that name is picked.
+        unit = random.choice(_search_units(cls.cfg, repo['_href'], ('rpm',)))
+        cls.unit_id = _get_unit_id(unit)
+        _remove_unit(cls.cfg, repo['_href'], unit)
+
+        # Re-publish the repository. sleep() for test_compare_timestamps.
+        # Re-read the repository so the test methods have fresh data.
+        time.sleep(2)
+        utils.publish_repo(cls.cfg, repo)
+        cls.repo = client.get(repo['_href'], params={'details': True})
+
+    def test_get_removed_unit(self):
+        """Verify the removed unit cannot be fetched."""
+        units = _search_units(self.cfg, self.repo['_href'], ('rpm',))
+        unit_ids = {_get_unit_id(unit) for unit in units}
+        self.assertNotIn(self.unit_id, unit_ids)
+
+    def test_compare_timestamps(self):
+        """Verify the repository and distributor timestamps are corrects.
+
+        Specifically, the repository's ``last_unit_removed`` time should be
+        before the distributor's ``last_publish`` time. Times returned by Pulp
+        are accurate to the nearest second, and it is possible that the process
+        of publishing a repository could take less than one second. To ensure
+        the times differ, :meth:`setUpClass` waits one second between the
+        removal and second publication.
+        """
+        last_unit_removed = parse(self.repo['last_unit_removed'])
+        self.assertEqual(len(self.repo['distributors']), 1)
+        last_publish = parse(self.repo['distributors'][0]['last_publish'])
+        self.assertLess(last_unit_removed, last_publish)
