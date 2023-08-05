8ac56f345d28f410d10db8eb76c0ca74258421a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package add_months/1.0.0/add_months-1.0.0.zip

---
+++ packages/add_months/add_months-1.0.0.zip/add_months-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+      name = 'add_months',
+      version = '1.0.0',
+      py_modules = ['add_months'],
+      author = 'gowthaman',
+      author_email = 'pearlhessonite@gmail.com',
+      url = '',
+      description = 'Date functions add_months'
+     )
