2279e01430292e243a72dc5a0d8b0b2097c452d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:53 +0000

    Add package liketest/1.1.1/liketest-1.1.1.zip

---
+++ packages/liketest/liketest-1.1.1.zip/liketest-1.1.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name = 'liketest',
+        version = '1.1.1',
+        py_modules = ['liketest'],
+        author = 'hfpython',
+        author_email = 'm18266811928@163.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+        )
