490b6936800982e8153167be3ee3a2bafaf2c22e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:46 +0000

    Add package nester_zhen/1.1.0/nester_zhen-1.1.0.zip

---
+++ packages/nester_zhen/nester_zhen-1.1.0.zip/nester_zhen-1.1.0/nester_zhen.py
@@ -0,0 +1,13 @@
+"""This is "nester.py"module, which provide function print_lol,
+it can print the list, which may include nested list."""
+from __future__ import print_function
+
+def print_lol(the_list,level):
+    """The list's item will be print recursionly"""
+    for each_item in the_list:
+        if isinstance(each_item,list):
+            print_lol(each_item,level+1)
+        else:
+            for tab_stop in range(level):
+                print ("\t",end='')
+            print(each_item)
