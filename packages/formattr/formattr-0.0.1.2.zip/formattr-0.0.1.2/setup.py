d7e619c71848a6e6f99f1fa473df824d199c279f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:01 +0000

    Add package formattr/0.0.1.2/formattr-0.0.1.2.zip

---
+++ packages/formattr/formattr-0.0.1.2.zip/formattr-0.0.1.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'formattr',
+  packages = ['formattr'],
+  version = '0.0.1.2',
+  description = 'An object formatter for string secure outputs',
+  author = 'Vinicius Mesel',
+  author_email = 'me@vmesel.com',
+  url = 'https://github.com/vmesel/formattr/',
+  download_url = 'https://www.github.com/vmesel/formattr/tarball/0.0.1.2',
+  keywords = ['format', 'security', 'string formatting'],
+  classifiers = [],
+  )
