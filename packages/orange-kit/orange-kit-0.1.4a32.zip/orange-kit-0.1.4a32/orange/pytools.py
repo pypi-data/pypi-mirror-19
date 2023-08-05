def6531e743e94b5792dcd45e444497785aad3ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:07 +0000

    Add package orange-kit/0.1.4a32/orange-kit-0.1.4a32.zip

---
+++ packages/orange-kit/orange-kit-0.1.4a32.zip/orange-kit-0.1.4a32/orange/pytools.py
@@ -0,0 +1,36 @@
+# 项目：标准函数库
+# 模块：Python相关实用命令
+# 作者：黄涛
+# License:GPL
+# Email:huangtao.sh@icloud.com
+# 创建：2015-08-09 07:50
+# 修订：2016-03-19
+# 修订：2016-5-12
+# 修订：2016-9-23 新增pyupload功能，使用run_setup来进行安装调用
+
+import sys
+import os
+from orange import Path,exec_shell
+from orange.deploy import *
+import atexit
+
+def pytest():
+    sys.path.insert(0,'.')
+    import unittest
+    unittest.main('testing')
+
+def _clear():
+    for path in Path('.').glob('*.egg-info'):
+        print('Path %s has beed deleted!'%(path))
+        if path.is_dir():
+            path.rmtree()
+            
+def pyupload():
+    atexit.register(_clear)
+    run_setup('sdist','--dist-dir',str(Path('~/OneDrive/pylib')),'upload')
+    # _clear()
+
+def pysdist():
+    run_setup('sdist','--dist-dir',str(Path('~/OneDrive/pylib')))
+    _clear()
+
