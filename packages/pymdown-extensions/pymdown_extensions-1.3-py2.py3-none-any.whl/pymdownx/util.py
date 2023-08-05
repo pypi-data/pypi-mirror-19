c5f39bcf46dd1b88d3659e4fdea4a5652fd99124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:31 +0000

    Add package pymdown-extensions/1.3/pymdown_extensions-1.3-py2.py3-none-any.whl

---
+++ packages/pymdown-extensions/pymdown_extensions-1.3-py2.py3-none-any.whl/pymdownx/util.py
@@ -0,0 +1,12 @@
+"""
+General utilities.
+
+MIT license.
+
+Copyright (c) 2017 Isaac Muse <isaacmuse@gmail.com>
+"""
+from __future__ import unicode_literals
+
+
+class PymdownxDeprecationWarning(UserWarning):
+    """Deprecation warning for Pymdownx that is not hidden."""
