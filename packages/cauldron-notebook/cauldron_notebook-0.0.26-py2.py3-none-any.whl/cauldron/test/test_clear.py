f48834e378c7a4a9cced5d441984d929a6fef953
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:26 +0000

    Add package cauldron-notebook/0.0.26/cauldron_notebook-0.0.26-py2.py3-none-any.whl

---
+++ packages/cauldron-notebook/cauldron_notebook-0.0.26-py2.py3-none-any.whl/cauldron/test/test_clear.py
@@ -0,0 +1,17 @@
+from cauldron.test import support
+from cauldron.test.support import scaffolds
+
+
+class TestClear(scaffolds.ResultsTest):
+    """
+
+    """
+
+    def test_clear(self):
+        """
+        """
+
+        r = support.run_command('open @examples:hello_cauldron')
+        r = support.run_command('clear')
+        self.assertFalse(r.failed, 'should not have failed')
+        support.run_command('close')
