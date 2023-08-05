1f15ef0e7d02d969beafa1cae85c60f999ed7a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:58 +0000

    Add package CodeLibWrapper/0.1.25/CodeLibWrapper-0.1.25.zip

---
+++ packages/CodeLibWrapper/CodeLibWrapper-0.1.25.zip/CodeLibWrapper-0.1.25/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# 更多setuptools信息，请参见http://www.360doc.com/content/14/0306/11/13084517_358166737.shtml
+# from os.path import abspath, dirname
+from CodeLibWrapper import CodeLibWrapper
+from setuptools import setup, find_packages
+
+# CURDIR = dirname (abspath (__file__))
+with open ('requirements.txt') as f:
+    required = f.read ().splitlines ()
+
+setup (
+    name="CodeLibWrapper",  # pip安装时的Package对外名称
+    version=CodeLibWrapper.CodeLibWrapper.Version,  # 版本
+
+    # trying to add files...
+    # package_dir={'': 'CodeLibWrapper'},
+    include_package_data=True,
+
+    packages=find_packages (exclude=['UnConfirmCode', 'PythonStudyCase']),
+    package_data={
+        '': ['README.txt', 'License.txt', 'Upgrade.bat', 'Uninstall.bat', 'requirements.txt'],
+        'Template': ['Sample_EnvironmentConfig.xml'],
+        'RobotFrameworkSample': ['SampleData/*.*', 'SimpleUsageTutorials/*.*', 'UserKeywordTestSuite/*.*'],
+        'UserKeywords': ['*.txt'],
+    },
+    author=CodeLibWrapper.CodeLibWrapper.Author,  # 作者
+    author_email="Benjamin.Zhou@morningstar.com",  # 作者联系方式
+    license='All Data Production/API QA team member use.',  # License
+    long_description=open ('README.txt').read (),  # Readme File
+    platforms="Python & Robot framework",
+    url="https://pypi.python.org/pypi/CodeLibWrapper",
+    maintainer="Benjamin Zhou",
+    maintainer_email="Benjamin.Zhou@morningstar.com",
+    keywords="Data Production, API, Robot Framework",
+    description="Robot Framework util package for MorningStar DataProduct/API team.",
+    install_requires=required
+
+)
