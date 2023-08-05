fdccb194c9fecaa3351189aec5c1e54ac2a9db0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:01 +0000

    Add package nester_fhf/2.0.0/nester_fhf-2.0.0.zip

---
+++ packages/nester_fhf/nester_fhf-2.0.0.zip/nester_fhf-2.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        =   "nester_fhf",
+    version     =   '2.0.0',
+    py_modules  =   ['nester_fhf'],
+    author      =   'fanhaifeng',
+    author_email=   "809930630@qq.com",
+    url         =   "http://www.baidu.com",
+    description =   "a simple printer of nested lists",
+)
