b492fa9359fcc672a0e9c77a074a7d7381baaf15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:14 +0000

    Add package diskimage-builder/1.25.2/diskimage-builder-1.25.2.tar.gz

---
+++ packages/diskimage-builder/diskimage-builder-1.25.2.tar.gz/diskimage-builder-1.25.2/elements/ubuntu-minimal/environment.d/10-ubuntu-distro-name.bash
@@ -0,0 +1,4 @@
+export DISTRO_NAME=ubuntu
+export DIB_RELEASE=${DIB_RELEASE:-xenial}
+export DIB_DEBIAN_COMPONENTS=${DIB_DEBIAN_COMPONENTS:-main,restricted,universe}
+export DIB_DISTRIBUTION_MIRROR=${DIB_DISTRIBUTION_MIRROR:-http://archive.ubuntu.com/ubuntu}
