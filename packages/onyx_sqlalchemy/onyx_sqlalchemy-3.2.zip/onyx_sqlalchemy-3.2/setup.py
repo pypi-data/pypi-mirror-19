5ec74121e1b158f846be3205ef2af3d710497202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:26 +0000

    Add package onyx_sqlalchemy/3.2/onyx_sqlalchemy-3.2.zip

---
+++ packages/onyx_sqlalchemy/onyx_sqlalchemy-3.2.zip/onyx_sqlalchemy-3.2/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""
+Flask-SQLAlchemy
+----------------
+
+Adds SQLAlchemy support to your Flask application.
+
+Links
+`````
+
+* `documentation <http://flask-sqlalchemy.pocoo.org>`_
+* `development version
+  <http://github.com/mitsuhiko/flask-sqlalchemy/zipball/master#egg=Flask-SQLAlchemy-dev>`_
+
+"""
+from setuptools import setup
+
+setup(
+    name='onyx_sqlalchemy',
+    version='3.2',
+    url='http://github.com/mitsuhiko/flask-sqlalchemy',
+    license='BSD',
+    author='Armin Ronacher',
+    author_email='armin.ronacher@active-4.com',
+    maintainer='Phil Howell',
+    maintainer_email='phil@quae.co.uk',
+    description='flask_sqlalchemy for Onyx',
+    long_description=__doc__,
+    packages=['onyx_sqlalchemy'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'Flask>=0.10',
+        'SQLAlchemy>=0.8.0'
+    ],
+    test_suite='test_sqlalchemy.suite',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ]
+)
