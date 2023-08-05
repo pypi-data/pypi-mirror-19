4a8d80129d107f6709fbad10993eb7b645b6743a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:10 +0000

    Add package braintree/3.33.0/braintree-3.33.0.tar.gz

---
+++ packages/braintree/braintree-3.33.0.tar.gz/braintree-3.33.0/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+    name="braintree",
+    version="3.33.0",
+    description="Braintree Python Library",
+    author="Braintree",
+    author_email="support@braintreepayments.com",
+    url="https://developers.braintreepayments.com/python/sdk/server/overview",
+    packages=["braintree", "braintree.exceptions", "braintree.exceptions.http", "braintree.merchant_account", "braintree.util", "braintree.test"],
+    package_data={"braintree": ["ssl/*"]},
+    install_requires=["requests>=0.11.1,<3.0"],
+    zip_safe=False,
+    classifiers = [
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5"
+    ]
+)
