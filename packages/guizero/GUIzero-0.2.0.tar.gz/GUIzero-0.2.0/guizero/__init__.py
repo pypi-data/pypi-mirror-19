96a59daa6347fee17f2a12732aad0a6a029c2097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:11 +0000

    Add package guizero/0.2.0/GUIzero-0.2.0.tar.gz

---
+++ packages/guizero/GUIzero-0.2.0.tar.gz/GUIzero-0.2.0/guizero/__init__.py
@@ -0,0 +1,25 @@
+try:
+    from tkinter import *
+except:
+    from Tkinter import *
+
+# -----------------------------
+__all__ = ['utilities', 'alerts', 'App', 'Box', 'ButtonGroup', 'CheckBox', 'Combo', 'MenuBar', 'Picture', 'PushButton',
+           'RadioButton', 'Slider', 'Text', 'TextBox', 'PushButton']
+
+from . import utilities as utils
+from .alerts import info, warn, error, yesno
+
+from .App import App
+from .Box import Box
+from .ButtonGroup import ButtonGroup
+from .CheckBox import CheckBox
+from .Combo import Combo
+from .MenuBar import MenuBar
+from .Picture import Picture
+from .PushButton import PushButton
+from .RadioButton import RadioButton
+from .Slider import Slider
+from .Text import Text
+from .TextBox import TextBox
+from .PushButton import PushButton
