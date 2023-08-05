a5dd51c8f9f56ef4ca3f60f939dcff8c1b6b12d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:39 +0000

    Add package concourse/0.0.0/concourse-0.0.0.tar.gz

---
+++ packages/concourse/concourse-0.0.0.tar.gz/concourse-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='concourse',
+    packages=['concourse'],
+    version='0.0.0',
+    description='Tool by Enigma Technologies Inc.',
+    author='Enigma Technologies Inc.',
+    author_email='contact@enigma.io',
+    url='https://enigma.io',
+)
