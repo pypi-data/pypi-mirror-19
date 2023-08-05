16265df55485fce7232b8290d3c286a1434535eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:15 +0000

    Add package musicrepair/5.9.3/musicrepair-5.9.3.tar.gz

---
+++ packages/musicrepair/musicrepair-5.9.3.tar.gz/musicrepair-5.9.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='musicrepair',
+    version='5.9.3',
+    description='Lets you repair your music files by adding metadata and album art',
+    url='https://github.com/lakshaykalbhor/musicrepair',
+    author='Lakshay Kalbhor',
+    author_email='lakshaykalbhor@gmail.com',
+    license='MIT',
+    packages=['musicrepair'],
+    install_requires=[
+        'bs4',
+        'colorama',
+        'mutagen',
+        'spotipy',
+        'six',
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': ['musicrepair=musicrepair.command_line:main'],
+    },
+)
