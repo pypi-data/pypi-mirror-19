37ccfe4a6e377141540dc3071e7f73ac18c8b470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:55 +0000

    Add package tag-processor/0.3.1/tag-processor-0.3.1.zip

---
+++ packages/tag-processor/tag-processor-0.3.1.zip/tag-processor-0.3.1/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, setuptools
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='tag-processor',
+    version='0.3.1',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    license='BSD',
+    description='Application for processing tags inside string, like ${tag}',
+    url='https://bitbucket.org/creekmind/tag_processor/',
+    author='Petr Kapsamun',
+    author_email='creekmind@gmail.com',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4'
+    ],
+)
