2ebb3bfe807a3afefc431dc86b2678df8a66b0be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:35 +0000

    Add package nestedviewer/1.2.0/nestedviewer-1.2.0.zip

---
+++ packages/nestedviewer/nestedviewer-1.2.0.zip/nestedviewer-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name			= 'nestedviewer',
+		version			= '1.2.0',
+		py_modules		= ['nester'],
+		author			= 'kradness',
+		author_email	= 'shubhamsharma364@gmail.com',
+		url				= 'http://www.google.com',
+		description		= 'A simple printer of nested lists',
+	)
