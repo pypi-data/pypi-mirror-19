7fdfc2dccc47e07bec5b3aeb177aa8f9d861a540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:10 +0000

    Add package iotile-build/2.2.4/iotile-build-2.2.4.tar.gz

---
+++ packages/iotile-build/iotile-build-2.2.4.tar.gz/iotile-build-2.2.4/iotile/build/dev/resolvers/registry_resolver.py
@@ -0,0 +1,45 @@
+from depresolver import DependencyResolver
+from iotile.core.exceptions import ArgumentError
+
+class ComponentRegistryResolver (DependencyResolver):
+    def __init__(self, settings={}):
+        pass
+
+    def resolve(self, depinfo, destdir):
+        from iotile.core.dev.registry import ComponentRegistry
+
+        reg = ComponentRegistry()
+
+        try:
+            comp = reg.find_component(depinfo['name'])
+        except ArgumentError:
+            return {'found': False}
+
+        #Make sure the tile we found in the registry has the required version
+        reqver = depinfo['required_version']
+        if not reqver.check(comp.parsed_version):
+            return {'found': False}
+
+        self._copy_folder_contents(comp.output_folder, destdir)
+        return {'found': True}
+
+    def check(self, depinfo, deptile, depsettings):
+        from iotile.core.dev.registry import ComponentRegistry
+
+        reg = ComponentRegistry()
+
+        try:
+            comp = reg.find_component(depinfo['name'])
+        except ArgumentError:
+            return True
+
+        #If the component does not have a matching version, we cannot assess up-to-date-ness
+        #FIXME: We should return that we cannot assess up to dateness, not that we are up to date
+        reqver = depinfo['required_version']
+        if not reqver.check(comp.parsed_version):
+            return True
+
+        if comp.release_date is not None and comp.release_date > deptile.release_date:
+            return False
+
+        return True
