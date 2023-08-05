5bba78bbaf0e4c15933c951582c8450ee55e1a7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:05 +0000

    Add package pytsite/0.98.12/PytSite-0.98.12.tar.gz

---
+++ packages/pytsite/PytSite-0.98.12.tar.gz/PytSite-0.98.12/pytsite/plugman/_error.py
@@ -0,0 +1,42 @@
+"""PytSite Plugin Manager Errors.
+"""
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+class UnknownPlugin(Exception):
+    pass
+
+
+class PluginNotInstalled(Exception):
+    pass
+
+
+class PluginAlreadyInstalled(Exception):
+    pass
+
+
+class PluginInstallationInProgress(Exception):
+    pass
+
+
+class PluginUninstallationInProgress(Exception):
+    pass
+
+
+class PluginInstallError(Exception):
+    pass
+
+
+class PluginUninstallError(Exception):
+    pass
+
+
+class PluginStartError(Exception):
+    pass
+
+
+class PluginAlreadyStarted(PluginStartError):
+    pass
