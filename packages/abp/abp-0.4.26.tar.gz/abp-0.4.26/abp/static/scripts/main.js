ff5d6737f2ac53a4915330310a54201c2738165b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:32 +0000

    Add package abp/0.4.26/abp-0.4.26.tar.gz

---
+++ packages/abp/abp-0.4.26.tar.gz/abp-0.4.26/abp/static/scripts/main.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    graph.prepare();
+    materials.prepare();
+    gui.prepare();
+    mouse.prepare();
+    editor.prepare();
+    gui.loop();
+};
---
+++ packages/abp/abp-0.4.26.tar.gz/abp-0.4.26/abp/static/scripts/materials.js
@@ -0,0 +1,40 @@
+var materials = {};
+
+var curveProperties = {
+    splineDensity: 1,
+    curvature: 20
+};
+
+// Is called on boot
+materials.prepare = function() {
+    var ballSprite = new THREE.Texture(document.getElementById("ball"));
+    ballSprite.needsUpdate = true;
+    materials.edge = new THREE.LineBasicMaterial({
+        color: "gray",
+        transparent: false,
+        linewidth: 3
+    });
+    materials.edge.depthTest = false;
+    materials.qubit = new THREE.PointsMaterial({
+        size: 0.3,
+        map: ballSprite,
+        alphaTest: 0.5,
+        transparent: true,
+        vertexColors: THREE.VertexColors
+    });
+    materials.edge.depthTest = false;
+};
+
+console.log("what");
+
+materials.makeCurve = function(a, b) {
+    var length = new THREE.Vector3().subVectors(a, b).length();
+    var bend = new THREE.Vector3(length / curveProperties.curvature, length / curveProperties.curvature, 0);
+    var mid = new THREE.Vector3().add(a).add(b).multiplyScalar(0.5).add(bend);
+    var spline = new THREE.CatmullRomCurve3([a, mid, b]);
+    var geometry = new THREE.Geometry();
+    var splinePoints = spline.getPoints(curveProperties.splineDensity);
+    Array.prototype.push.apply(geometry.vertices, splinePoints);
+    return new THREE.Line(geometry, materials.edge);
+};
+
