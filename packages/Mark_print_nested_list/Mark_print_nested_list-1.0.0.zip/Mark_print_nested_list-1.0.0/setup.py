a3e1cdc18628bf28fa9bf7dda93adf9928def272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:07 +0000

    Add package Mark_print_nested_list/1.0.0/Mark_print_nested_list-1.0.0.zip

---
+++ packages/Mark_print_nested_list/Mark_print_nested_list-1.0.0.zip/Mark_print_nested_list-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name         = 'Mark_print_nested_list',
+    version      = '1.0.0',
+    py_modules   = ['print_nested_list'],
+    author       = 'MarkChern',
+    author_email = '13zxchen3@gmail.com',
+    url          = 'http://www.headfirstlabs.com',
+    description  = '打印嵌套列表元素的函数',
+    )
