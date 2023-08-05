2c00842ce0186d54e84f8f5e172177f1e7434175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:21 +0000

    Add package amino/9.0.1/amino-9.0.1.tar.gz

---
+++ packages/amino/amino-9.0.1.tar.gz/amino-9.0.1/amino/list.py
@@ -0,0 +1,257 @@
+import itertools
+import typing
+import random
+import string
+from functools import reduce
+from typing import TypeVar, Callable, Generic, Iterable, Any
+
+from toolz.itertoolz import cons
+
+from amino import maybe, boolean
+from amino.logging import log
+from amino.tc.base import ImplicitsMeta, Implicits
+from amino.func import curried, I
+from amino.util.string import safe_string
+
+A = TypeVar('A', covariant=True)
+B = TypeVar('B')
+
+
+def flatten(l: Iterable[Iterable[A]]) -> Iterable[A]:
+    return list(itertools.chain.from_iterable(l))  # type: ignore
+
+
+class ListMeta(ImplicitsMeta):
+
+    def __instancecheck__(self, instance):
+        if type(instance) is list:
+            return False
+        else:
+            return super().__instancecheck__(instance)
+
+    # def __subclasscheck__(self, subclass):
+    #     if subclass is list:
+    #         return False
+    #     return super().__subclasscheck__(subclass)
+
+
+def _rand_str(chars, num: int=10):
+    return ''.join(random.choice(chars) for i in range(num))
+
+
+class List(typing.List[A], Generic[A], Implicits, implicits=True,
+           metaclass=ListMeta):
+
+    def __init__(self, *elements):
+        typing.List.__init__(self, elements)
+
+    def __getitem__(self, arg):
+        s = super().__getitem__(arg)
+        return List.wrap(s) if isinstance(arg, slice) else s
+
+    @staticmethod
+    def wrap(l: Iterable[B]) -> 'List[B]':
+        return List(*list(l))
+
+    @staticmethod
+    def range(*a):
+        return List.wrap(range(*a))
+
+    @staticmethod
+    def random_string(num: int=10):
+        chars = string.ascii_letters + string.digits
+        return _rand_str(chars, num)
+
+    @staticmethod
+    def random_alpha(num: int=10):
+        chars = string.ascii_letters
+        return _rand_str(chars, num)
+
+    @staticmethod
+    def gen(num: int, f: Callable[[], A]):
+        return List.range(num) // (lambda a: f())
+
+    @staticmethod
+    def lines(data: str):
+        return List.wrap(data.splitlines())
+
+    def lift(self, index: int) -> 'maybe.Maybe[A]':
+        return (
+            (maybe.Just(self[index]) if len(self) > index else maybe.Empty())
+            if index >= 0 else
+            (maybe.Just(self[index]) if len(self) >= -index else maybe.Empty())
+        )
+
+    def lift_all(self, index, *indexes):
+        from amino.anon import _
+        def folder(z, n):
+            return n.ap(z / _.cat)
+        els = List.wrap(indexes) / self.lift
+        init = self.lift(index) / List
+        return els.fold_left(init)(folder)
+
+    def smap(self, f: Callable[..., B]) -> 'List[B]':
+        return List.wrap(list(itertools.starmap(f, self)))
+
+    def flat_smap(self, f: Callable[..., 'Iterable[B]']) -> 'List[B]':
+        return List.wrap(flatten(list(itertools.starmap(f, self))))
+
+    def foreach(self, f: Callable[[A], B]) -> None:
+        for el in self:
+            f(el)
+
+    def forall(self, f: Callable[[A], bool]) -> boolean.Boolean:
+        return boolean.Boolean(all(f(el) for el in self))
+
+    def contains(self, value):
+        return value in self
+
+    def exists(self, f: Callable[[A], bool]) -> bool:
+        return self.find(f).is_just
+
+    @property
+    def is_empty(self):
+        return boolean.Boolean(self.length == 0)
+
+    empty = is_empty
+
+    @property
+    def length(self):
+        return len(self)
+
+    @property
+    def head(self):
+        return self.lift(0)
+
+    @property
+    def last(self):
+        return self.lift(-1)
+
+    @property
+    def init(self):
+        return maybe.Empty() if self.empty else maybe.Just(self[:-1])
+
+    @property
+    def tail(self):
+        return maybe.Empty() if self.empty else maybe.Just(self[1:])
+
+    @property
+    def detach_head(self):
+        return self.head.product(self.tail)
+
+    @property
+    def detach_last(self):
+        return self.last.product(self.init)
+
+    @property
+    def distinct(self) -> 'List[A]':
+        return self.distinct_by(I)
+
+    def distinct_by(self, f: Callable[[A], bool]) -> 'List[A]':
+        seen = set()  # type: set
+        def pred(a):
+            v = f(a)
+            return v in seen or seen.add(v)
+        return List.wrap(x for x in self if not pred(x))
+
+    def add(self, other: typing.List[A]) -> 'List[A]':
+        return List.wrap(typing.List.__add__(self, other))
+
+    __add__ = add
+
+    def without(self, el) -> 'List[A]':
+        from amino.anon import _
+        return self.filter(_ != el)
+
+    __sub__ = without
+
+    def split(self, f: Callable[[A], bool]):
+        def splitter(z, e):
+            l, r = z
+            return (l + (e,), r) if f(e) else (l, r + (e,))
+        l, r = reduce(splitter, self, ((), (),))
+        return List.wrap(l), List.wrap(r)
+
+    def split_type(self, tpe: type):
+        return self.split(lambda a: isinstance(a, tpe))
+
+    def debug(self, prefix=None):
+        prefix = '' if prefix is None else prefix + ' '
+        log.debug(prefix + str(self))
+        return self
+
+    def index_of(self, target: Any):
+        from amino.anon import _
+        return self.index_where(_ == target)
+
+    @property
+    def reversed(self):
+        return List.wrap(reversed(self))
+
+    def mk_string(self, sep=''):
+        return sep.join(self / str)
+
+    @property
+    def join_lines(self):
+        return self.mk_string('\n')
+
+    @property
+    def join_comma(self):
+        return self.mk_string(', ')
+
+    @property
+    def join_tokens(self):
+        return self.mk_string(' ')
+
+    def cons(self, item):
+        return List.wrap(cons(item, self))
+
+    def cat(self, item):
+        return self + List(item)
+
+    def cat_m(self, item: maybe.Maybe):
+        return item / self.cat | self
+
+    @property
+    def transpose(self):
+        return List.wrap(map(List.wrap, zip(*self)))
+
+    def drop(self, n: int):
+        return self[n:]
+
+    def take(self, n: int):
+        return self[:n]
+
+    def drop_while(self, pred: Callable[[A], bool]):
+        index = self.index_where(lambda a: not pred(a))
+        return index / (lambda a: self[a:]) | self
+
+    def remove_all(self, els: 'List[A]') -> 'List[A]':
+        return self.filter_not(els.contains)
+
+    def __repr__(self):
+        return 'List({})'.format(', '.join(map(repr, self)))
+
+    def __str__(self):
+        return '[{}]'.format(', '.join(map(safe_string, self)))
+
+    def __hash__(self):
+        return hash(tuple(self))
+
+    def sort_by(self, f: Callable[[A], bool], reverse=False):
+        return List.wrap(sorted(self, key=f, reverse=reverse))
+
+    def sort(self, reverse=False):
+        return self.sort_by(I, reverse)
+
+    def replace_item(self, a, b) -> 'List[A]':
+        return self.map(lambda c: b if c == a else c)
+
+    @curried
+    def replace_where(self, a, pred: Callable):
+        return self.map(lambda b: a if pred(b) else b)
+
+    def __mul__(self, other):
+        return List.wrap(super().__mul__(other))
+
+__all__ = ('List',)
---
+++ packages/amino/amino-9.0.1.tar.gz/amino-9.0.1/amino/logging.py
@@ -0,0 +1,139 @@
+import os
+import re
+import logging
+import operator
+from typing import Callable
+import sys
+from pathlib import Path
+
+from amino.lazy import lazy
+
+import amino
+
+VERBOSE = 15
+DDEBUG = 5
+logging.addLevelName(VERBOSE, 'VERBOSE')
+logging.addLevelName(DDEBUG, 'DDEBUG')
+
+
+class Logger(logging.Logger):
+
+    def verbose(self, message, *args, **kws):
+        if self.isEnabledFor(VERBOSE):
+            self._log(VERBOSE, message, args, **kws)
+
+    def ddebug(self, message, *args, **kws):
+        if self.isEnabledFor(DDEBUG):
+            self._log(DDEBUG, message, args, **kws)
+
+    def caught_exception(self, when, exc, *a, **kw):
+        headline = 'caught exception while {}:'.format(when)
+        self.exception(headline, exc_info=(type(exc), exc, exc.__traceback__))
+
+
+logging.Logger.verbose = Logger.verbose  # type: ignore
+logging.Logger.ddebug = Logger.ddebug  # type: ignore
+logging.Logger.caught_exception = Logger.caught_exception  # type: ignore
+
+log = amino_root_logger = logging.getLogger('amino')
+
+
+def install_logger_class():
+    logging.setLoggerClass(Logger)
+
+
+def amino_logger(name: str):
+    return amino_root_logger.getChild(name)
+
+_stdout_logging_initialized = False
+
+_level_env_var = 'AMINO_LOG_LEVEL'
+
+
+def init_loglevel(logger: logging.Logger, level: int=None):
+    if level is not None:
+        logger.setLevel(level)
+    elif _level_env_var in os.environ:
+        logger.setLevel(os.environ[_level_env_var])
+    elif amino.development:
+        logger.setLevel(VERBOSE)
+
+
+def amino_stdout_logging(level: int=None):
+    global _stdout_logging_initialized
+    if not _stdout_logging_initialized:
+        amino_root_logger.addHandler(logging.StreamHandler(stream=sys.stdout))
+        init_loglevel(amino_root_logger, level)
+        _stdout_logging_initialized = True
+
+
+default_logfile = Path.home() / '.python' / 'log'  # type: ignore
+_file_logging_initialized = False
+
+
+def amino_file_logging(level: int=None, logfile=default_logfile,
+                       handler_level: int=logging.INFO):
+    global _file_logging_initialized
+    if not _file_logging_initialized:
+        logfile.parent.mkdir(exist_ok=True)
+        handler = logging.FileHandler(str(logfile))
+        handler.setLevel(handler_level)
+        amino_root_logger.addHandler(handler)
+        init_loglevel(amino_root_logger, level)
+        _file_logging_initialized = True
+
+
+class Logging:
+
+    @property
+    def log(self) -> Logger:
+        return self._log  # type: ignore
+
+    @lazy
+    def _log(self) -> Logger:
+        return amino_logger(self.__class__.__name__)
+
+    def _p(self, a):
+        v = self.log.verbose
+        v(a)
+        return a
+
+
+def sub_loggers(loggers, root):
+    from amino import Map, _, L
+    children = loggers.keyfilter(L(re.match)('{}\.[^.]+$'.format(root), _))
+    sub = (children.k / L(sub_loggers)(loggers, _))\
+        .fold_left(Map())(operator.pow)
+    return Map({loggers[root]: sub})
+
+
+def logger_tree(root):
+    from amino import __, Map
+    m = Map(logging.Logger.manager.loggerDict)
+    all = m.keyfilter(__.startswith(root))
+    return sub_loggers(all, 'amino')
+
+
+def indent(strings, level, width=1):
+    ws = ' ' * level * width
+    return strings.map(ws.__add__)
+
+
+def format_logger_tree(tree, fmt_logger, level=0):
+    from amino import _, L
+    sub_f = L(format_logger_tree)(_, fmt_logger, level=level + 1)
+    formatted = tree.bimap(fmt_logger, sub_f)
+    return '\n'.join(indent(formatted.map2('{}\n{}'.format), level))
+
+
+def print_info(out: Callable[[str], None]):
+    lname = lambda l: logging.getLevelName(l.getEffectiveLevel())
+    hlname = lambda h: logging.getLevelName(h.level)
+    def handler(h):
+        return '{}({})'.format(h.__class__.__name__, hlname(h))
+    def logger(l):
+        handlers = ','.join(list(map(handler, l.handlers)))
+        return '{}: {} {}'.format(l.name, lname(l), handlers)
+    out(format_logger_tree(logger_tree('amino'), logger))
+
+__all__ = ('amino_root_logger', 'amino_stdout_logging', 'amino_file_logging')
