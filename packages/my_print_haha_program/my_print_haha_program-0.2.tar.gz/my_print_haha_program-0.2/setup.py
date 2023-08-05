05d3426c9fa9dcef6f4166bf599ae3676a3e3b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:38 +0000

    Add package my_print_haha_program/0.2/my_print_haha_program-0.2.tar.gz

---
+++ packages/my_print_haha_program/my_print_haha_program-0.2.tar.gz/my_print_haha_program-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = 'my_print_haha_program',
+    version = '0.2',
+    packages = ['my_package'],
+    author = 'wombatwen',
+    author_email = 'wombatwen@gmail.com',
+    scripts = ['print_haha']
+    
+)
