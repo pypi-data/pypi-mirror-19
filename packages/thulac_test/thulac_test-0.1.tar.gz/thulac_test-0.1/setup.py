fce53492f16a63ff491ade1f71689775e1470299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:12:56 +0000

    Add package thulac_test/0.1/thulac_test-0.1.tar.gz

---
+++ packages/thulac_test/thulac_test-0.1.tar.gz/thulac_test-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'thulac_test',
+  packages = ['thulac_test'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A random test lib',
+  author = 'Peter Downs',
+  author_email = 'majunhua133256@gmail.com',
+  url = 'https://github.com/thunlp/THULAC-Python', # use the URL to the github repo
+  download_url = 'https://github.com/thunlp/THULAC-Python/archive/master.zip', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
