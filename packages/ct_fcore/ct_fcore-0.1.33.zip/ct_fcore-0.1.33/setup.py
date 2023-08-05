729f6eb81961791e8f864d61a7c7eddf0ab3d916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:08 +0000

    Add package ct_fcore/0.1.33/ct_fcore-0.1.33.zip

---
+++ packages/ct_fcore/ct_fcore-0.1.33.zip/ct_fcore-0.1.33/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'ct_fcore',
+    version = '0.1.33',
+    keywords = ('django', 'ctcloud', 'portal', 'ct_fcore', 'chinatelecom'),
+    description = 'a portal framework for ctcloud using tornado',
+    license = 'MIT License',
+    install_requires = [],
+
+    author = 'astwyg',
+    author_email = 'i@ysgh.net',
+    
+    packages = find_packages(),
+    platforms = 'any',
+
+    url = "https://github.com/astwyg/ct-fcore",
+)
