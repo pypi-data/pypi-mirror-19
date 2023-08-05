9e247ee5ab3854a96cf245a5292a495464a56ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:00 +0000

    Add package Optlib/0.0.1/optlib-0.0.1-py3-none-any.whl

---
+++ packages/Optlib/optlib-0.0.1-py3-none-any.whl/optlib/option/greeks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+  @Author: Nasy
+  @Date: Dec 22, 2016
+  @email: sy_n@me.com
+  @file: optlib/options/greeks.py
+  @license: MIT
+
+  An Excited Python Script
+"""
+
+
+def main():
+    """main function
+    """
+    pass
+
+
+if __name__ is '__main__':
+    main()
