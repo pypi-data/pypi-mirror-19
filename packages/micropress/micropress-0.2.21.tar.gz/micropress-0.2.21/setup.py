c3e4303f7c0cee2d7db26491e6f153c475240e35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:42 +0000

    Add package micropress/0.2.21/micropress-0.2.21.tar.gz

---
+++ packages/micropress/micropress-0.2.21.tar.gz/micropress-0.2.21/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+from distutils.sysconfig import get_python_lib
+from distutils.version import StrictVersion
+import os, platform
+
+try:
+    long_description = open('README.md').read()
+except:
+    long_description = u"Blog generator for hackers."
+
+prefix = ''
+if StrictVersion('15.0.0') > StrictVersion(platform.release()):
+    prefix = get_python_lib()
+
+platform.release()
+setup(
+    name='micropress',
+    version='0.2.21',
+    author='Oleksandr Glushchenko',
+    author_email='contact@fluder.co',
+    url='https://github.com/glushchenko/micropress',
+    scripts = ['micropress'],
+    data_files= [(prefix + '/micropress/%s' % (x[0]), map(lambda y: x[0]+'/'+y, x[2])) for x in os.walk('init/')],
+    license='LICENSE.txt',
+    description='Blog generator for hackers.',
+    long_description=long_description,
+    install_requires=[
+        'Jinja2',
+        'markdown',
+        'Werkzeug',
+        'watchdog',
+        's3cmd',
+        'Pillow'
+    ],
+)
