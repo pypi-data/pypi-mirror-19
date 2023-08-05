c67a78852e21f6da0764a453ac0f7c6b5cf791cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:49 +0000

    Add package syfr/0.0.1/syfr-0.0.1.tar.gz

---
+++ packages/syfr/syfr-0.0.1.tar.gz/syfr-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+      name='syfr',
+      version='0.0.1',
+      description='Always Open Always Safe',
+      license='MIT',
+      install_requires=[
+            'cryptography'
+        ]
+      )
