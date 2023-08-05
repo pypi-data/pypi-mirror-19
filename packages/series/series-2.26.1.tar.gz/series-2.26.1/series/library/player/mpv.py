b7bc0ba2b5d24557fbd39d5d49ead181290d7b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:06 +0000

    Add package series/2.26.1/series-2.26.1.tar.gz

---
+++ packages/series/series-2.26.1.tar.gz/series-2.26.1/series/library/player/mpv.py
@@ -0,0 +1,57 @@
+from mpv import MPV as Player, _mpv_terminate_destroy
+
+from series.library.player.interface import PlayerInterface
+
+
+class Callback:
+
+    def __init__(self, func):
+        self._func = func
+
+    def call(self):
+        self._func()
+
+
+class MPV(PlayerInterface):
+
+    def __init__(self, args):
+        super().__init__(Player(**args))
+        self._player.event_callbacks.append(Callback(self.event))
+
+    def stop(self):
+        _mpv_terminate_destroy(self._player.handle)
+        self._player = None
+
+    def pause(self):
+        self._player.pause = not self._player.pause.val
+
+    def osd(self, level):
+        self._player.osd_level = level
+
+    def osd_level(self):
+        return self._player.osd_level
+
+    @property
+    def current_volume(self):
+        return self._player.volume
+
+    def volume(self, value):
+        self._player.ao_volume = value
+
+    def __getattr__(self, name):
+        try:
+            return super().__getattr__(name)
+        except TypeError:
+            pass
+
+    def event(self):
+        try:
+            pass
+        except Exception as e:
+            self.log.error(e)
+
+    @property
+    def length(self):
+        return self.duration
+
+__all__ = ['MPV']
