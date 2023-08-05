f6fbafbffd90a880779cd0d900c4e0f369393d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:25 +0000

    Add package tty_radio/1.1.0/tty_radio-1.1.0.tar.gz

---
+++ packages/tty_radio/tty_radio-1.1.0.tar.gz/tty_radio-1.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='tty_radio',
+    packages=['tty_radio'],
+    version='1.1.0',
+    description=(
+        "Linux/OS X RESTful player for online radio streams, " +
+        "like SomaFM and WCPE. Comes with a terminal UI " +
+        "flavored with colors and ASCII art " +
+        "and web UI for remote management."),
+    author='Ryan Farley',
+    author_email='rfarley3@gmu.edu',
+    url='https://github.com/rfarley3/radio',
+    download_url='https://github.com/rfarley3/radio/tarball/1.1.0',
+    keywords=['radio', 'somafm', 'streaming', 'mpg123'],
+    classifiers=[],
+    install_requires=[
+        'beautifulsoup4',
+        'pyfiglet',
+        'bottle',
+        'requests'
+    ],
+    entry_points={
+        'console_scripts': [
+            'radio = tty_radio.__main__:main_ui',
+            'radio_server = tty_radio.__main__:main_serv',
+        ]
+    },
+    include_package_data=True,
+)
