369eaadd73f694a3eb9b607624b480ed78bb53c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:12 +0000

    Add package like/1.5.0/like-1.5.0.zip

---
+++ packages/like/like-1.5.0.zip/like-1.5.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name = 'like',
+        version = '1.5.0',
+        py_modules = ['like'],
+        author = 'likepython',
+        author_email = 'm18266811928@163.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+    )
+        
