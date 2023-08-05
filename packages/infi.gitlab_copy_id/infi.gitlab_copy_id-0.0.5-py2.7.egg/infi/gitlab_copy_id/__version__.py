4367f23df90cd7fd753cbe7397d7d4e0eb08bfe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:21 +0000

    Add package infi.gitlab_copy_id/0.0.5/infi.gitlab_copy_id-0.0.5-py2.7.egg

---
+++ packages/infi.gitlab_copy_id/infi.gitlab_copy_id-0.0.5-py2.7.egg/infi/gitlab_copy_id/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.5"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.0.5)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'e5a953f6ce5579dd4d11cbabb3aad4262ffc6978'
+__git_head_subject__ = 'Finished Release v0.0.5'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
+__git_commit_date__ = '2016-12-18 14:10:21'
