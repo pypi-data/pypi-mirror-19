9d5141b04154105e64460e7a9ad9f5055199d818
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:27 +0000

    Add package clients/0.3/clients-0.3.tar.gz

---
+++ packages/clients/clients-0.3.tar.gz/clients-0.3/docs/reference.rst
@@ -0,0 +1,20 @@
+.. automodule:: clients
+
+Client
+=============
+.. autoclass:: Client
+   :show-inheritance:
+   :members:
+   :special-members:
+   :exclude-members: __truediv__
+
+Resource
+=============
+.. autoclass:: Resource
+   :show-inheritance:
+   :members:
+   :special-members:
+
+singleton
+=============
+.. autofunction:: singleton
---
+++ packages/clients/clients-0.3.tar.gz/clients-0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='clients',
+    version='0.3',
+    description='HTTP for lazy, impatient, hubristic humans.',
+    long_description=open('README.rst').read(),
+    author='Aric Coady',
+    author_email='aric.coady@gmail.com',
+    url='https://bitbucket.org/coady/clients',
+    license='Apache Software License',
+    py_modules=['clients'],
+    install_requires=['requests>=2.4.2'],
+    tests_require=['pytest-cov'],
+    keywords='requests sessions responses resources',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP :: Session',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
