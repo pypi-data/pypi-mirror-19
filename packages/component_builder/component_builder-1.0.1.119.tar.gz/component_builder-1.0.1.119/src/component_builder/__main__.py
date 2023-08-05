6bdaeb173277f368e1eef947d5ec8361cb586f58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:55 +0000

    Add package component_builder/1.0.1.119/component_builder-1.0.1.119.tar.gz

---
+++ packages/component_builder/component_builder-1.0.1.119.tar.gz/component_builder-1.0.1.119/src/component_builder/__main__.py
@@ -0,0 +1,91 @@
+import json
+import sys
+
+from docopt import docopt
+
+from . import build, discover, envs, github, release
+
+
+USAGE = """
+Intelligent builder for working with component-based repositories
+
+Usage:
+  compbuild discover [--with-versions] [--vs-branch=BRANCH]{common}
+  compbuild build [<component>...] {common}
+  compbuild env [<component>...] {common}
+  compbuild release [<component>...] {common}
+  compbuild test [<component>...] {common}
+  compbuild tag [<component>...] {common}
+  compbuild label <label> [<component>...] {common}
+  compbuild <action> [<component>...] {common}
+  compbuild -h | --help
+  compbuild --version
+
+Options:
+  -h --help            Show this screen.
+  --all                Do all the components
+  --filter=SELECTOR    Filter components based on selector
+                       (e.g. --filter=relase-process=docker)
+  --version            Show version.
+  --conf=FILE          Configuration file location [default: builder.ini]
+  --with-versions      Print out all items of interest, with versions
+  --vs-branch=BRANCH   Discover changes made compared to BRANCH. If not set,
+                       comparison will be to latest staging branch for each
+                       component.
+
+""".format(
+    common='[--all] [--filter=SELECTOR] [--conf=FILE]'
+)
+
+
+def cli(out=sys.stdout):
+    arguments = docopt(USAGE, version='1.0')
+
+    b = build.Builder(arguments['--conf'])
+    b.configure()
+
+    selectors = arguments['--filter']
+    if selectors:
+        selectors = selectors.split(',')
+
+    components = discover.run(
+        b.components,
+        arguments.get('<component>', []),
+        arguments['--all'],
+        compare_branch=arguments['--vs-branch'],
+        selectors=selectors,
+    )
+    envs.set_envs(components)
+
+    if arguments['discover']:
+        tmpl = u"{title}"
+        if arguments['--with-versions']:
+            tmpl += u':{version}'
+        for c in components:
+            out.write(
+                tmpl.format(title=c.title, version=c.env['VERSION']) + '\n'
+            )
+    elif arguments['build']:
+        b.pre('build', components)
+        build.run('build', components)
+        b.post('build', components)
+    elif arguments['test']:
+        b.pre('test', components)
+        build.run('test', components)
+        b.post('test', components)
+    elif arguments['tag']:
+        for comp in components:
+            github.create_tag("{0}-{1}".format(
+                comp.title, comp.env['VERSION'])
+            )
+    elif arguments['label']:
+        for comp in components:
+            github.update_branch(comp.branch_name(arguments['<label>']))
+    elif arguments['release']:
+        release.run(components)
+    elif arguments['env']:
+        out.write("\n".join(json.dumps(c.env, indent=4) for c in components))
+
+
+if __name__ == '__main__':
+    cli()
