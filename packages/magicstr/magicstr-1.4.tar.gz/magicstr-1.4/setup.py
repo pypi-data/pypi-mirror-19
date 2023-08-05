133badc783b641658ee7f10935bea92a644b1b09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:48 +0000

    Add package magicstr/1.4/magicstr-1.4.tar.gz

---
+++ packages/magicstr/magicstr-1.4.tar.gz/magicstr-1.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'magicstr',
+    packages = ['magicstr'],
+    discription = 'Do some magic on your strings files.',
+    version = '1.04',
+    author = 'wombatwen',
+    author_email = 'wombatwen@gmail.com',
+)
