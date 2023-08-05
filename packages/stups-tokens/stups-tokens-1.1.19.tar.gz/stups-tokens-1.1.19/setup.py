ab84262c141b15e3cf0b4cc259c889bbccdf6778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:05 +0000

    Add package stups-tokens/1.1.19/stups-tokens-1.1.19.tar.gz

---
+++ packages/stups-tokens/stups-tokens-1.1.19.tar.gz/stups-tokens-1.1.19/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+def read_version(package):
+    with open(os.path.join(package, '__init__.py'), 'r') as fd:
+        for line in fd:
+            if line.startswith('__version__ = '):
+                return line.split()[-1].strip().strip("'")
+
+
+__version__ = read_version('tokens')
+
+
+class PyTest(TestCommand):
+
+    user_options = [('cov-html=', None, 'Generate junit html report')]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.cov = None
+        self.pytest_args = ['--cov', 'tokens', '--cov-report', 'term-missing', '-v']
+        self.cov_html = False
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        if self.cov_html:
+            self.pytest_args.extend(['--cov-report', 'html'])
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+setup(
+    name='stups-tokens',
+    packages=find_packages(),
+    version=__version__,
+    description='Python library to manage OAuth access tokens',
+    long_description=open('README.rst').read(),
+    author='Henning Jacobs',
+    author_email='henning.jacobs@zalando.de',
+    url='https://github.com/zalando-stups/python-tokens',
+    license='Apache License Version 2.0',
+    install_requires=['requests'],
+    tests_require=['pytest-cov', 'pytest', 'mock'],
+    extras_require={
+        'tests': ['flake8'],
+    },
+    cmdclass={'test': PyTest},
+    test_suite='tests',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+)
