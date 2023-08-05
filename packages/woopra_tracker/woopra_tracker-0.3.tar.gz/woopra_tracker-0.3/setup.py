6d6fd67404f713b0a04cc4f4d25682b2f5f5959c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:40 +0000

    Add package woopra_tracker/0.3/woopra_tracker-0.3.tar.gz

---
+++ packages/woopra_tracker/woopra_tracker-0.3.tar.gz/woopra_tracker-0.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='woopra_tracker',
+    version='0.3',
+    author='Woopra',
+    author_email='dev@woopra.com',
+    url='https://github.com/Woopra/woopra-python-sdk',
+    packages=['woopra_tracker'],
+    license='LICENSE.txt',
+    description='Woopra Analytics Tracking API',
+)
