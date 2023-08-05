e20187c4131381a5ccd1f4af9fe516be5b2d137b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:37 +0000

    Add package rst2wiki/0.3.1/rst2wiki-0.3.1.tar.gz

---
+++ packages/rst2wiki/rst2wiki-0.3.1.tar.gz/rst2wiki-0.3.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import os
+# dirty hack for locale bug (for docutils)
+os.environ['LC_CTYPE'] = 'en_US.UTF8'
+
+
+setup(
+    name='rst2wiki',
+    version='0.3.1',
+    description='Tool to push reST docs to confluence',
+    author='Artem Dayneko',
+    author_email='dayneko.ab@gmail.com',
+    url='https://github.com/jamert/rst2wiki',
+    py_modules=['rst2wiki'],
+    install_requires=[
+        'click',
+        'requests',
+        'docutils',
+        'Pygments',
+        'rst2confluence',
+    ],
+    entry_points='''
+        [console_scripts]
+        rst2wiki = rst2wiki:main
+    ''',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+    ]
+)
