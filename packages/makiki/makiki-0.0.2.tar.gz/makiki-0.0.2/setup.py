0931bf928f6765adb97467b2d8cec794bd8658d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:15 +0000

    Add package makiki/0.0.2/makiki-0.0.2.tar.gz

---
+++ packages/makiki/makiki-0.0.2.tar.gz/makiki-0.0.2/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+console_scripts = [
+    'makiki = makiki.cli:main_parser',
+]
+
+requires = [
+    'blinker>=1.4',
+    'gevent>=1.1',
+    'gunicorn>=19.0',
+    'Jinja2>=2.8.1',
+]
+
+setup(
+    name='makiki',
+    version='0.0.2',
+    description='Web service utils and generator.',
+    long_description='',
+    author='Wang Yanqing',
+    author_email='me@oreki.moe',
+    packages=find_packages(),
+    url='http://github.com/faith0811/makiki',
+    include_package_data=True,
+    entry_points={
+        'console_scripts': console_scripts,
+    },
+    zip_safe=False,
+    install_requires=requires,
+)
