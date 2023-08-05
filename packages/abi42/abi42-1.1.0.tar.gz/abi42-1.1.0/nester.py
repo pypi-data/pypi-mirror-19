9176970989ed188d65e32480eeb950dfbe1b5c8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:28 +0000

    Add package abi42/1.1.0/abi42-1.1.0.tar.gz

---
+++ packages/abi42/abi42-1.1.0.tar.gz/abi42-1.1.0/nester.py
@@ -0,0 +1,18 @@
+"""This module contains a function which prints lists which may/may not include nested lists"""
+
+movies = [
+    "The Holy Grail", 1975, "Terry JOnes & Terry Gilliam", 91,
+    ["Graham Chapman",
+     ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]
+]
+
+
+def print_lol(the_list, level):
+    """This function takes a list argument and each item of the list is printed on the screen in its own line """
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level + 1)
+        else:
+            for num in range(level):
+                print('\t', end='')
+            print(each_item)
