2ca282f982554c7ccc94c00ab1aa6b304b18dbca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:40 +0000

    Add package infi.iscsiapi/0.2.1/infi.iscsiapi-0.2.1.tar.gz

---
+++ packages/infi.iscsiapi/infi.iscsiapi-0.2.1.tar.gz/infi.iscsiapi-0.2.1/src/infi/iscsiapi/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.2.1"
+__git_commiter_name__ = "Amir Reshef"
+__git_commiter_email__ = "amirr@infinidat.com"
+__git_branch__ = u'develop'
+__git_remote_tracking_branch__ = u'origin/develop'
+__git_remote_url__ = u'git@git.infinidat.com:host-opensource/infi.iscsiapi.git'
+__git_head_hash__ = '9074c4b3f6a0b0183ee405c3e7e6ab00a0e3ebd9'
+__git_head_subject__ = u'Finished Release v0.2.1'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2016-12-29 17:41:42'
