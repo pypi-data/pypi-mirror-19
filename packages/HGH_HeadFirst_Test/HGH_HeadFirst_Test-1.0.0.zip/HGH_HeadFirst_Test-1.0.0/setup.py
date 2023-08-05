2efe082a0e3272df3855b4019455fb24a8670af1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:14 +0000

    Add package HGH_HeadFirst_Test/1.0.0/HGH_HeadFirst_Test-1.0.0.zip

---
+++ packages/HGH_HeadFirst_Test/HGH_HeadFirst_Test-1.0.0.zip/HGH_HeadFirst_Test-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name = 'HGH_HeadFirst_Test',
+    version = '1.0.0',
+    py_modules = ['nester'],
+    author = 'hgh',
+    author_email = '1223176133@qq.com',
+    url = 'http://blog.csdn.net/juanchihuan1326',
+    description = 'a simple printer of nester lists',
+    )
