e2b8e55152f41afb62ab4e5591b4e8a4be0aac5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:46 +0000

    Add package django-mptt/0.8.7/django-mptt-0.8.7.tar.gz

---
+++ packages/django-mptt/django-mptt-0.8.7.tar.gz/django-mptt-0.8.7/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+
+setup(
+    name='django-mptt',
+    description='''Utilities for implementing Modified Preorder Tree Traversal
+        with your Django Models and working with trees of Model instances.''',
+    version=__import__('mptt').__version__,
+    author='Craig de Stigter',
+    author_email='craig.ds@gmail.com',
+    url='http://github.com/django-mptt/django-mptt',
+    license='MIT License',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        'Topic :: Utilities',
+    ],
+)
