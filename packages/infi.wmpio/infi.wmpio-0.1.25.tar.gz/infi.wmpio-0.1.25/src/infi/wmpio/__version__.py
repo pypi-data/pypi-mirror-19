022488c8ee80257c75bcbb32e0a3ccf220dd1597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:25 +0000

    Add package infi.wmpio/0.1.25/infi.wmpio-0.1.25.tar.gz

---
+++ packages/infi.wmpio/infi.wmpio-0.1.25.tar.gz/infi.wmpio-0.1.25/src/infi/wmpio/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.25"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.1.25)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'e09a33bb8f4a00843bb3e0da1cd2c20ff4ab8a6f'
+__git_head_subject__ = 'Finished Release v0.1.25'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
+__git_commit_date__ = '2016-12-18 14:34:58'
