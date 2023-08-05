fb63472f846276661e068b2fc43ce375fb56d8ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:10 +0000

    Add package pyfhi/1.0.3/pyfhi-1.0.3.tar.gz

---
+++ packages/pyfhi/pyfhi-1.0.3.tar.gz/pyfhi-1.0.3/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+    name='pyfhi',
+    version='1.0.3',
+    packages=['pyfhi'],
+    url='https://github.com/TheOneHyer/PyFHI',
+    download_url='https://github.com/TheOneHyer/PyFHI/tarball/1.0.3',
+    license='GPLv3',
+    author='TheOneHyer',
+    author_email='theonehyer@gmail.com',
+    description='PyFHI is a single script designed to fix issues with Python '
+                'file handling.See README for more details.',
+    classifiers=[
+        'Development Status :: 7 - Inactive',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Software Development'
+    ],
+    keywords='file handling python closer close all'
+)
