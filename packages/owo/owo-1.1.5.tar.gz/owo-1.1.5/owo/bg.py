bb882fcf32784694c1ff5e7d2c9ca8d5f3ca2c68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:43 +0000

    Add package owo/1.1.5/owo-1.1.5.tar.gz

---
+++ packages/owo/owo-1.1.5.tar.gz/owo-1.1.5/owo/bg.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3.5
+
+"""
+owo.py background process
+main use intended for mobile devices
+usage: `$ owo-bg -p path -k API_KEY`
+"""
+
+import argparse
+import owo
+import os
+import sys
+import time
+
+def print_v(text):
+    if args.verbose:
+        print(text)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-k", "--key", help="API Key", required=True)
+
+parser.add_argument("-p", "--path", help="Path to check file updates",
+                    default="/sdcard/pictures/screenshots/")
+
+parser.add_argument("-u", "--url", help="Base vanity url to use",
+                    default="https://owo.whats-th.is/")
+
+parser.add_argument("-v", "--verbose", help="Increase output verbosity",
+                    action="store_true")
+
+args = parser.parse_args()
+
+sent_files = os.listdir(args.path)
+
+
+def main():
+    if not args.path.endswith("/"):
+        args.path += "/"
+
+    print("Starting background process...")
+    while True:
+        time.sleep(2)
+        new_files = [f for f in os.listdir(args.path) if
+                     f not in sent_files and os.path.isfile(args.path+f)]
+        if new_files == []:
+            continue
+        for file in new_files:
+            print_v("Found file: {}".format(file))
+            try:
+                urls = list(owo.upload_files(args.key, args.path+file,
+                                             verbose=True).values())[0]
+
+                url = urls.get(args.url)
+                if url is None:
+                    print("Vanity url base {} was not found, using default"
+                          .format(args.url))
+                    url = urls["https://owo.whats-th.is/"]
+
+            except ValueError as e:
+                print("Upload failed:\n{}".format(e.args[0]))
+
+            except OverflowError:
+                print("File too big: {}".format(file))
+                sent_files.append(file)
+
+            else:
+                print_v("Upload successful.")
+                sent_files.append(file)
+                if (sys.executable ==
+                        "/data/data/com.termux/files/usr/bin/python"):
+                    # Mobile devices
+
+                    try:
+                        os.system("termux-notification -t \""
+                                  "File uploaded\""
+                                  " -c \"{0}\" -u \"{0}\"".format(
+                                      url))
+
+                        os.system("termux-clipboard-set {}".format(url))
+                        print_v("Sent notification")
+                    except:
+                        print("File uploaded: {}, URL: {}".format(
+                                  file, url))
+                else:
+                    print("File uploaded: {}, URL: {}".format(
+                              file, url))
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/owo/owo-1.1.5.tar.gz/owo-1.1.5/owo/owo.py
@@ -0,0 +1,228 @@
+import mimetypes
+import os.path
+import sys
+
+from functools import lru_cache
+
+PY_VERSION = sys.version_info.major
+
+__all__ = ["upload_files", "shorten_urls",
+           "async_upload_files", "async_shorten_urls",
+           "Client"]
+
+BASE_URL = "https://api.awau.moe"
+
+UPLOAD_PATH = "/upload/pomf"
+SHORTEN_PATH = "/shorten/polr"
+
+UPLOAD_STANDARD = "https://owo.whats-th.is/"
+SHORTEN_STANDARD = "https://uwu.whats-th.is/"
+
+UPLOAD_BASES = ("https://owo.whats-th.is/", "https://i.am-a.ninja/",
+                "https://buttsare.sexy/", "https://nyanyanya.moe/",
+                "https://all.foxgirlsare.sexy/", "https://i.stole-a-me.me/",
+                "https://can-i-ask-dean-on-a.date/", "https://this.is-a.dog/",
+                "https://deanis.sexy/")
+
+SHORTEN_BASES = ("https://awau.moe/", "https://uwu.whats-th.is/")
+
+
+def check_size(file):
+    if os.path.getsize(file) > 83886080:
+        raise OverflowError("File exceeds file size limit")
+
+
+@lru_cache()
+def upload_files(key: str, *files: str, verbose=False):
+    try:
+        import requests
+    except ImportError:
+        raise ImportError("Please install the `requests` module "
+                          "to use this function")
+
+    for file in files:
+        check_size(file)
+
+    multipart = [(
+        "files[]",
+        (
+            file,
+            open(file, "rb"),
+            mimetypes.guess_type(file)[0]
+        ))
+        for file in files]
+
+    response = requests.post(BASE_URL+UPLOAD_PATH, files=multipart,
+                             params={"key": key})
+
+    if response.status_code != 200:
+        raise ValueError("Expected 200, got {}\n{}".format(
+            response.status_code, response.text))
+
+    if verbose:
+        results = {
+            item["name"]: {
+                base: base+item["url"]
+                for base in UPLOAD_BASES
+            }
+            for item in response.json()["files"]
+        }
+
+    else:
+        results = {
+            item["name"]: UPLOAD_STANDARD + item["url"]
+            for item in response.json()["files"]
+        }
+
+    return results
+
+
+@lru_cache()
+def shorten_urls(key: str, *urls: str, verbose=False):
+    try:
+        import requests
+    except ImportError:
+        raise ImportError("Please install the `requests` module "
+                          "to use this function")
+
+    # Make the request
+    results = []
+
+    for url in urls:
+        response = requests.get(BASE_URL+SHORTEN_PATH,
+                                params={"action": "shorten",
+                                        "url": url,
+                                        "key": key})
+
+        if response.status_code != 200:
+            raise ValueError("Expected 200, got {}\n{}".format(
+                response.status_code, response.text))
+
+        path = response.text.split("/")[-1]
+        if verbose:
+            results.append({
+                base: base+path
+                for base in SHORTEN_BASES
+            })
+        else:
+            results.append(SHORTEN_STANDARD + path)
+
+    return results
+
+
+if PY_VERSION == 3:
+    import asyncio
+
+    @lru_cache()
+    @asyncio.coroutine
+    def async_upload_files(key: str, *files: str, loop=None, verbose=False):
+        try:
+            from . import aiohttp2
+            import aiohttp
+        except ImportError:
+            raise ImportError("Please install the `aiohttp` module "
+                              "to use this function")
+
+        results = {}
+
+        for file in files:
+            check_size(file)
+
+        with aiohttp2.MultipartWriter('form-data') as mp:
+            for file in files:
+                part = mp.append(open(file, "rb"))
+                part.set_content_disposition(
+                    'form-data',
+                    should_quote=False,
+                    name='files[]',
+                    filename=file
+                )
+
+            with aiohttp.ClientSession(loop=loop) as session:
+                with (
+                    yield from session.post(BASE_URL+UPLOAD_PATH, data=mp,
+                                            params={"key": key})) as response:
+                    if response.status != 200:
+                        raise ValueError("Expected 200, got {}\n{}".format(
+                            response.status, (yield from response.text())))
+
+                    for item in (yield from response.json())["files"]:
+                        if item.get("error") is True:
+                            raise ValueError("Expected 200, got {}\n{}".format(
+                                item["errorcode"], item["description"]))
+
+                        if verbose:
+                            results[item["name"]] = {
+                                base: base+item["url"]
+                                for base in UPLOAD_BASES
+                            }
+
+                        else:
+                            results[item["name"]] = UPLOAD_STANDARD+item["url"]
+
+        return results
+
+    @lru_cache()
+    @asyncio.coroutine
+    def async_shorten_urls(key: str, *urls: str, loop=None, verbose=False):
+        try:
+            import aiohttp
+        except ImportError:
+            raise ImportError("Please install the `aiohttp` module "
+                              "to use this function")
+
+        results = []
+
+        with aiohttp.ClientSession(loop=loop) as session:
+            for url in urls:
+                with (
+                    yield from session.get(BASE_URL+SHORTEN_PATH,
+                                           params={"action": "shorten",
+                                                   "url": url,
+                                                   "key": key})) as response:
+                    if response.status != 200:
+                        raise ValueError("Expected 200, got {}\n{}".format(
+                            response.status, (yield from response.text())))
+
+                path = (yield from response.text()).split("/")[-1]
+                if verbose:
+                    results.append({
+                        base: base+path
+                        for base in SHORTEN_BASES
+                    })
+                else:
+                    results.append(SHORTEN_STANDARD + path)
+
+        return results
+
+
+class Client:
+    """
+    In case you want to make multiple requests
+    without constantly passing `api_key` and/or `loop`
+    """
+
+    def __init__(self, api_key: str, loop=None, verbose=False):
+        self.key = api_key
+        self.loop = loop
+        self.verbose = verbose
+
+    def toggle_verbose(self):
+        self.verbose = not self.verbose
+
+    def upload_files(self, *files: str):
+        return upload_files(self.key, *files, verbose=self.verbose)
+
+    def shorten_urls(self, *urls: str):
+        return shorten_urls(self.key, *urls, verbose=self.verbose)
+
+    if PY_VERSION == 3:
+        @asyncio.coroutine
+        def async_upload_files(self, *files: str):
+            return async_upload_files(self.key, *files,
+                                      loop=self.loop, verbose=self.verbose)
+
+        @asyncio.coroutine
+        def async_shorten_urls(self, *urls: str):
+            return async_shorten_urls(self.key, *urls,
+                                      loop=self.loop, verbose=self.verbose)
