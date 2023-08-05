7757c81440f2cd94ce4f22768c9a702007c2a6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:24 +0000

    Add package keras-tqdm/1.0.1/keras_tqdm-1.0.1.tar.gz

---
+++ packages/keras-tqdm/keras_tqdm-1.0.1.tar.gz/keras_tqdm-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(name='keras_tqdm',
+      version='1.0.1',
+      install_requires=['keras', 'tqdm'],
+      author="Ben Striner",
+      author_email="bstriner@gmail.com",
+      url="https://github.com/bstriner/keras-tqdm",
+      download_url='https://github.com/bstriner/keras-tqdm/tarball/v1.0.1',
+      description="Train Keras models with TQDM progress bars and IPython support",
+      keywords=['keras', 'tqdm', 'progress', 'progressbar', 'ipython', 'jupyter'],
+      license='MIT',
+      classifiers=[
+          # Indicate who your project is intended for
+          'Intended Audience :: Developers',
+          # Pick your license as you wish (should match "license" above)
+          'License :: OSI Approved :: MIT License',
+
+          # Specify the Python versions you support here. In particular, ensure
+          # that you indicate whether you support Python 2, Python 3 or both.
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3'
+      ],
+      packages=find_packages())
