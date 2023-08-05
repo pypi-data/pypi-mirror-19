50b7c2ad5e7417243f1eb210248dd29095e5835e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:04 +0000

    Add package begin_v/1.2.0/begin_v-1.2.0.zip

---
+++ packages/begin_v/begin_v-1.2.0.zip/begin_v-1.2.0/begin_v.py
@@ -0,0 +1,8 @@
+def print_lol(the_list, level=0):
+	for each in the_list:
+		if isinstance(each, list):
+			print_lol(each, level+1)
+		else:
+                        for tab_stop in range(level):
+                                print("\t", end='')
+                        print(each)
