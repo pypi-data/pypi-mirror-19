631a674011f08144cbb43379066948ae18708747
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:26:19 +0000

    Add package imageregistration/0.2/imageregistration-0.2.tar.gz

---
+++ packages/imageregistration/imageregistration-0.2.tar.gz/imageregistration-0.2/imageregistration/__init__.py
@@ -0,0 +1,20 @@
+def ImageRegistration(imagelist, reference):
+    """
+    Estimates the (affine) transformation matrix between two images. This procedure depends on a server with Mathematica 
+    installation. Please note that image transformation might take a while depending on image size. 
+    :param reference: path/name to reference image (e.g. ortho foto of an object or overview image) 
+    :type reference: string
+    :param imagelist: one or more images for transformation procedure
+    :type imagelist: list
+    
+    :returns: only data export
+    
+    :example:
+        >>> 
+        
+    """
+    for x in range(0, len(imagelist)):
+        print('execute script: math -script run_registration.m '+reference[0]+' '+imagelist[x])
+        script = 'math -script run_registration.m  '+reference[0]+' '+imagelist[x]
+        p = sub.Popen(['ssh', 'mathematica@141.20.159.97',script],stdout=sub.PIPE,stderr=sub.PIPE) 
+    return;
