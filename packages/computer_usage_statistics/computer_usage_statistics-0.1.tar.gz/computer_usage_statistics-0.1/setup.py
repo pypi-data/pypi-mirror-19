413808c52d5afe0dec0d818ae5ec12a73db32220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:02 +0000

    Add package computer_usage_statistics/0.1/computer_usage_statistics-0.1.tar.gz

---
+++ packages/computer_usage_statistics/computer_usage_statistics-0.1.tar.gz/computer_usage_statistics-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'computer_usage_statistics',
+  packages = ['computer_usage_statistics'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Logs when the user is logged in.',
+  author = 'Benjamin Lind',
+  author_email = 'benli778@student.liu.se',
+  url = 'https://github.com/benjiyo/computer_usage_statistics',
+  download_url = 'https://github.com/benjiyo/computer_usage_statistics/tarball/0.1',
+  keywords = ['usage', 'statistics'],
+  classifiers = [],
+)
