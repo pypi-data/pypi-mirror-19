330d5426f523609b10a04c129a7456811c1ad4e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:58 +0000

    Add package live_sjc/1.0.0/live_sjc-1.0.0.tar.gz

---
+++ packages/live_sjc/live_sjc-1.0.0.tar.gz/live_sjc-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name		= 'live_sjc',
+	version 	= '1.0.0',
+	author  	= 'Tecooler',
+	author_email    = 'cool.baozijian@gmail.com',
+	url     	= 'https://www.bz1.me/',
+	description     = 'qiniu_shijianchuo',
+     )
