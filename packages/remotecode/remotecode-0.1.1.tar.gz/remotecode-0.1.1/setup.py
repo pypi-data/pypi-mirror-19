536873102c1327ba5b2a87a560321f7975e69c47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:58 +0000

    Add package remotecode/0.1.1/remotecode-0.1.1.tar.gz

---
+++ packages/remotecode/remotecode-0.1.1.tar.gz/remotecode-0.1.1/setup.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+# coding: utf-8
+"""remotecode setup file."""
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='remotecode',
+    version='0.1.1',
+    description='**DEPRECATED** A Sphinx extension that enables ' +
+                'remote code blocks',
+    long_description=long_description,
+    url='https://github.com/kallimachos/remotecode',
+    author='Brian moss',
+    author_email='kallimachos@gmail.com',
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Documentation :: Sphinx',
+        'Framework :: Sphinx :: Extension',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    keywords='sphinx documentation remote code block',
+    packages=['remotecode'],
+    install_requires=['requests'],
+)
