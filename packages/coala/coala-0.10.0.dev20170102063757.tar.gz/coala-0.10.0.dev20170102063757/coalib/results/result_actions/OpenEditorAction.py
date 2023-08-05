da035ca6ec78f394e7fc1b977a271296e69bcb48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:07 +0000

    Add package coala/0.10.0.dev20170102063757/coala-0.10.0.dev20170102063757.tar.gz

---
+++ packages/coala/coala-0.10.0.dev20170102063757.tar.gz/coala-0.10.0.dev20170102063757/coalib/results/result_actions/OpenEditorAction.py
@@ -0,0 +1,75 @@
+import subprocess
+from os.path import exists
+from os import environ
+
+from coalib.results.Diff import Diff
+from coalib.results.Result import Result
+from coalib.results.result_actions.ResultAction import ResultAction
+
+from coala_utils.decorators import enforce_signature
+
+
+EDITOR_ARGS = {
+    'subl': '--wait',
+    'gedit': '-s',
+    'atom': '--wait'
+}
+
+
+GUI_EDITORS = ['kate', 'gedit', 'subl', 'atom']
+
+
+class OpenEditorAction(ResultAction):
+
+    SUCCESS_MESSAGE = 'Changes saved successfully.'
+
+    @staticmethod
+    @enforce_signature
+    def is_applicable(result: Result, original_file_dict, file_diff_dict):
+        """
+        For being applicable, the result has to point to a number of files
+        that have to exist i.e. have not been previously deleted.
+        """
+
+        if not len(result.affected_code) > 0:
+            return 'The result is not associated with any source code.'
+
+        filenames = set(src.renamed_file(file_diff_dict)
+                        for src in result.affected_code)
+        if not all(exists(filename) for filename in filenames):
+            return ("The result is associated with source code that doesn't "
+                    'seem to exist.')
+        return True
+
+    def apply(self, result, original_file_dict, file_diff_dict, editor: str):
+        """
+        Open file(s)
+
+        :param editor: The editor to open the file with.
+        """
+        # Use set to remove duplicates
+        filenames = {src.file: src.renamed_file(file_diff_dict)
+                     for src in result.affected_code}
+
+        editor_args = [editor] + list(filenames.values())
+        arg = EDITOR_ARGS.get(editor.strip(), None)
+        if arg:
+            editor_args.append(arg)
+
+        # Dear user, you wanted an editor, so you get it. But do you really
+        # think you can do better than we?
+        if editor in GUI_EDITORS:
+            subprocess.call(editor_args, stdout=subprocess.PIPE)
+        else:
+            subprocess.call(editor_args)
+
+        for original_name, filename in filenames.items():
+            with open(filename, encoding='utf-8') as file:
+                file_diff_dict[original_name] = Diff.from_string_arrays(
+                    original_file_dict[original_name], file.readlines(),
+                    rename=False if original_name == filename else filename)
+
+        return file_diff_dict
+
+    if 'EDITOR' in environ:
+        apply.__defaults__ = (environ['EDITOR'],)
