2ac7e1b0c920a26d01e605b97ca9debddfccace7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:53 +0000

    Add package therealcleanmac/1.0/therealcleanmac-1.0.tar.gz

---
+++ packages/therealcleanmac/therealcleanmac-1.0.tar.gz/therealcleanmac-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='therealcleanmac',
+    version='1.0',
+    scripts=['free'],
+    author='Thereal',
+    author_email='darryl@thereal-website.com',
+    license='MIT',
+    description='Simple script to free up your Mac\'s ram',
+    url='http://www.thereal-website.com'
+)
