c5dad24023be741997f461a467f5ed208d014374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:53 +0000

    Add package elevationapi/1.0.1/elevationapi-1.0.1.tar.gz

---
+++ packages/elevationapi/elevationapi-1.0.1.tar.gz/elevationapi-1.0.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+long_description = open('README.rst').read()
+
+setup(
+	name = 'elevationapi',
+	packages = ['elevationapi'],
+	version = '1.0.1',
+	description = 'Get the elevation of any land point on Earth using elevationapi.xyz',
+	long_description=long_description,
+	author = 'Frank Villaro-Dixon',
+	author_email = 'frank@villaro-dixon.eu',
+	url = 'https://github.com/Frankkkkk/python-elevationapi',
+#	download_url = 'https://github.com/Frankkkkk/python-elevationapi/tarball/0.1',
+	keywords = 'elevation altitude earth elevationapi.xyz',
+	classifiers = [
+		'Topic :: Scientific/Engineering :: Atmospheric Science',
+		'Topic :: Scientific/Engineering :: GIS',
+
+		'License :: OSI Approved :: MIT License',
+
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.2',
+		'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5'
+		],
+)
