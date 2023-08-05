e806870a160fcc3ec0536ca609a204611c3ec154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:48 +0000

    Add package slam/0.1.0/slam-0.1.0.tar.gz

---
+++ packages/slam/slam-0.1.0.tar.gz/slam-0.1.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+setup(
+    name='slam',
+    version='0.1.0',
+    url='https://github.com/miguelgrinberg/slam/',
+    license='MIT',
+    author='Miguel Grinberg',
+    author_email='miguelgrinberg50@gmail.com',
+    description='Serverless application manager',
+    long_description=__doc__,
+    packages=['slam'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    entry_points={
+        'console_scripts': [
+            'slam = slam.cli:main'
+        ]
+    },
+    install_requires=[
+        'lambda_uploader',
+        'boto',
+        'climax',
+        'jinja2',
+        'pyyaml',
+        'virtualenv'
+    ],
+    tests_require=[],
+    test_suite='test_slam',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
