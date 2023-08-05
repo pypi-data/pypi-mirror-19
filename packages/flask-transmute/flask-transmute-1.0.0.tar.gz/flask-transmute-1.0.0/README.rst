a6c1e802767e6f20fbd66752e559d60781ed4ee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:36 +0000

    Add package flask-transmute/1.0.0/flask-transmute-1.0.0.tar.gz

---
+++ packages/flask-transmute/flask-transmute-1.0.0.tar.gz/flask-transmute-1.0.0/README.rst
@@ -0,0 +1,13 @@
+===============
+flask-transmute
+===============
+
+.. image:: https://travis-ci.org/toumorokoshi/flask-transmute.svg?branch=master
+    :alt: build status
+    :target: https://travis-ci.org/toumorokoshi/flask-transmute
+
+.. image:: https://coveralls.io/repos/github/toumorokoshi/flask-transmute/badge.svg?branch=master
+    :alt: coverage status
+    :target: https://coveralls.io/github/toumorokoshi/flask-transmute?branch=master
+
+A transmute (http://transmute-core.readthedocs.io/) implementation for flask.
---
+++ packages/flask-transmute/flask-transmute-1.0.0.tar.gz/flask-transmute-1.0.0/flask_transmute/route.py
@@ -0,0 +1,23 @@
+from transmute_core import (
+    describe, default_context, TransmuteFunction
+)
+from transmute_core.swagger import SwaggerSpec
+from .handler import create_routes_and_handler
+from .swagger import SWAGGER_ATTR_NAME
+
+
+def transmute_route(app, context=default_context, **kwargs):
+    """ attach a transmute route. """
+    def decorator(fn):
+        fn = describe(**kwargs)(fn)
+        transmute_func = TransmuteFunction(fn)
+        routes, handler = create_routes_and_handler(transmute_func, context)
+        for r in routes:
+            # push swagger info.
+            if not hasattr(app, SWAGGER_ATTR_NAME):
+                setattr(app, SWAGGER_ATTR_NAME, SwaggerSpec())
+            swagger_obj = getattr(app, SWAGGER_ATTR_NAME)
+            swagger_obj.add_func(transmute_func, context)
+            app.route(r)(handler)
+        return handler
+    return decorator
