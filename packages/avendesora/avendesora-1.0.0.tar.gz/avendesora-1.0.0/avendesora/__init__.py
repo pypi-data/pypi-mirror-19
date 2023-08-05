0c39308756f9d6651748c2832acd2981a1b46278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:48 +0000

    Add package avendesora/1.0.0/avendesora-1.0.0.tar.gz

---
+++ packages/avendesora/avendesora-1.0.0.tar.gz/avendesora-1.0.0/avendesora/__init__.py
@@ -0,0 +1,23 @@
+from .charsets import (
+    exclude, LOWERCASE, UPPERCASE, LETTERS, DIGITS, ALPHANUMERIC,
+    HEXDIGITS, PUNCTUATION, SYMBOLS, WHITESPACE, PRINTABLE, DISTINGUISHABLE
+)
+from .account import Account, StealthAccount, Script
+from .secrets import (
+    Password, Passphrase, PIN, Question, MixedPassword, PasswordRecipe,
+    BirthDate,
+)
+from .obscure import Hide, Hidden, GPG
+try:
+    from .obscure import Scrypt
+except ImportError:  # no cover
+    pass
+from .recognize import (
+    RecognizeAll, RecognizeAny, RecognizeTitle, RecognizeURL, RecognizeCWD,
+    RecognizeHost, RecognizeUser, RecognizeEnvVar, RecognizeNetwork,
+)
+from .generator import PasswordGenerator
+from inform import Error as PasswordError
+
+__version__ = '1.0.0'
+__released__ = '2017-01-01'
