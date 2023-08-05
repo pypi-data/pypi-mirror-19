51fced8dcd95ed021e29ef9857a67abdf65aaec1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:55 +0000

    Add package alignak_checks_snmp/0.2.7/alignak_checks_snmp-0.2.7.tar.gz

---
+++ packages/alignak_checks_snmp/alignak_checks_snmp-0.2.7.tar.gz/alignak_checks_snmp-0.2.7/README.rst
@@ -0,0 +1,13 @@
+Alignak checks package for SNMP
+===============================
+
+Checks pack for monitoring hosts with SNMP
+
+--------------------------------------------------------------------
+**Beware**: This checks pack is now deprecated. It has been replaced with `alignak-checks-linux-snmp` that makes its naming more consistent with the used naming conventions.
+
+The current future-master branch in this repository contains the modifications used as a basis for the next repository.
+
+As of now, see: https://github.com/Alignak-monitoring-contrib/alignak-checks-linux-snmp
+--------------------------------------------------------------------
+
---
+++ packages/alignak_checks_snmp/alignak_checks_snmp-0.2.7.tar.gz/alignak_checks_snmp-0.2.7/version.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015:
+#   Frederic Mohier, frederic.mohier@gmail.com
+#
+
+"""
+    Alignak - Checks pack for monitoring hosts with SNMP
+"""
+# Package name
+__pkg_name__ = u"alignak_checks_snmp"
+
+# Checks types for PyPI keywords
+# Used for:
+# - PyPI keywords
+# - directory where to store files in the Alignak configuration (eg. arbiter/packs/checks_type)
+__checks_type__ = u"snmp"
+
+# Application manifest
+__version__ = u"0.2.7"
+__author__ = u"Frédéric MOHIER"
+__author_email__ = u"frederic.mohier@gmail.com"
+__copyright__ = u"(c) 2015 - %s" % __author__
+__license__ = u"GNU Affero General Public License, version 3"
+__url__ = u"https://github.com/Alignak-monitoring-contrib/alignak-checks-snmp"
+__description__ = u"Alignak checks pack for SNMP monitored hosts"
