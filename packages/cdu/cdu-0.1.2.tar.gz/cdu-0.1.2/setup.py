ec0f9629a8fa1cf064a3132c7cc46aa3ee483dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:44 +0000

    Add package cdu/0.1.2/cdu-0.1.2.tar.gz

---
+++ packages/cdu/cdu-0.1.2.tar.gz/cdu-0.1.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='cdu',
+    version='0.1.2',
+    packages=['cdu'],
+    description="Cloud Storage Disk Usage Analyzer",
+    author="Adrian Mester",
+    author_email="mesteradrian@gmail.com",
+    url="https://github.com/ilogik/cdu",
+    keywords=["aws", "s3", "boto"],
+    entry_points={
+        'console_scripts': [
+            'cdu = cdu.__main__:main'
+        ]
+    },
+    install_requires=[
+        'boto3',
+    ]
+)
