6e21c981218279dfdc434a5f970494680a9a9012
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:38 +0000

    Add package flask-r-login/0.0.1/flask-r-login-0.0.1.tar.gz

---
+++ packages/flask-r-login/flask-r-login-0.0.1.tar.gz/flask-r-login-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='flask-r-login',
+    author='ruitian',
+    author_email='ike960705@gmail.com',
+    description='',
+    url='https://git.coding.net/yybaron/flask-r-login.git',
+    version='0.0.1',
+    py_modules=['flask_r_login'],
+    install_requires=[
+        'Flask>=0.11.0'
+    ]
+)
