37b8c011e7fe5ff65bcf23c2e83db5ee59209876
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:50 +0000

    Add package wxyEllen/0.0.1/wxyEllen-0.0.1.tar.gz

---
+++ packages/wxyEllen/wxyEllen-0.0.1.tar.gz/wxyEllen-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+    name='wxyEllen',
+    packages=['wxyEllen'],
+    version='0.0.1',
+    description='some pic ops',
+    author='wangxiaoyu',
+    author_email='wangxiaoyu.wangxiaoyu@@gmail.com',
+    license='MIT',
+    install_requires=[
+        'scikit-image'
+    ],
+    url='https://github.com/netsmallfish1977/wxyEllen',
+    download_url='https://github.com/netsmallfish1977/wxyEllen/tarball/0.0.1',
+    keywords=['Ellen', 'Image'],
+    classifiers=[],
+)
---
+++ packages/wxyEllen/wxyEllen-0.0.1.tar.gz/wxyEllen-0.0.1/wxyEllen/image.py
@@ -0,0 +1,17 @@
+class Image(object):
+    def __init__(self, filename, filetype):
+        self.filename = filename
+        self.filetype = filetype
+        pass
+
+    def Load(self):
+        pass
+
+    def Save(self):
+        pass
+
+    def Thumbnail(self):
+        pass
+
+    def Rotate(self):
+        pass
