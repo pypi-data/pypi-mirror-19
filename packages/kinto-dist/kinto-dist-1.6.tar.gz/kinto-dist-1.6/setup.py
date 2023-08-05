50c17ecd2f1e972ac7b5392f2db1c9d732bbf143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:24 +0000

    Add package kinto-dist/1.6/kinto-dist-1.6.tar.gz

---
+++ packages/kinto-dist/kinto-dist-1.6.tar.gz/kinto-dist-1.6/setup.py
@@ -0,0 +1,56 @@
+import os
+import codecs
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read_file(filename):
+    """Open a related file and return its content."""
+    with codecs.open(os.path.join(here, filename), encoding='utf-8') as f:
+        content = f.read()
+    return content
+
+README = read_file('README.rst')
+CHANGELOG = read_file('CHANGELOG.rst')
+
+REQUIREMENTS = [
+    "kinto[postgresql,monitoring]>=5.1,<5.2",
+    "kinto-attachment>=1.0,<1.1",
+    "kinto-amo>=0.3.0,<0.4",
+    "kinto-changes>=0.4,<0.5",
+    "kinto-signer>=1.0.0,<1.1.0",
+    "kinto-fxa>=2.3,<3.0",
+    "kinto-ldap>=0.3.0,<0.4",
+    "amo2kinto>=1.6,<1.7",
+    "boto>=2.40,<2.41",
+]
+ENTRY_POINTS = {}
+DEPENDENCY_LINKS = []
+
+setup(name='kinto-dist',
+      version='1.6',
+      description='Kinto Distribution',
+      long_description=README + "\n\n" + CHANGELOG,
+      license='Apache License (2.0)',
+      classifiers=[
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: Implementation :: CPython",
+          "Programming Language :: Python :: Implementation :: PyPy",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+          "License :: OSI Approved :: Apache Software License"
+      ],
+      keywords="web services",
+      author='Mozilla Services',
+      author_email='services-dev@mozilla.com',
+      url='https://github.com/mozilla-services/kinto-dist',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=REQUIREMENTS)
