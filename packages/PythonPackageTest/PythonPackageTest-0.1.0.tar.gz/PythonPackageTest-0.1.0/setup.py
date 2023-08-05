9d01243dbb7e80b5445fc880701248b5f52cef02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:57 +0000

    Add package PythonPackageTest/0.1.0/PythonPackageTest-0.1.0.tar.gz

---
+++ packages/PythonPackageTest/PythonPackageTest-0.1.0.tar.gz/PythonPackageTest-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='PythonPackageTest',
+    version='0.1.0',
+    author='Naila Ata',
+    author_email='naila.ata@gmail.com',
+    packages=['python_package_test'],
+    url='http://pypi.python.org/pypi/PythonPackageTest/',
+    license='LICENSE.txt',
+    description='Testing package',
+  
+
+)
