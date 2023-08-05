c61196b8af06046a464ad991d3e7d96a8943d8e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:35 +0000

    Add package nh_embedded_list_printer/1.3.0/nh_embedded_list_printer-1.3.0.zip

---
+++ packages/nh_embedded_list_printer/nh_embedded_list_printer-1.3.0.zip/nh_embedded_list_printer-1.3.0/nh_embedded_list_printer.py
@@ -0,0 +1,20 @@
+"""
+@author:   Nick Handy
+@date:     12/28/16
+@summary:  This module takes in a list and prints out each element. This method can handle
+           nested lists. 
+"""
+from __future__ import print_function
+
+def print_lol(the_list, indent=False, level=0):
+    for each_item in the_list:
+        """This function takes in a list object "the_list" which may or may not be a 
+           nested list. Each item within the list is printed through the recursive 
+           steps below"""
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end =" ")
+            print(each_item)
