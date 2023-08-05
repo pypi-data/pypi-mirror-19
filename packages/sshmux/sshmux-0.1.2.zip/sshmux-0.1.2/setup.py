a89050805f61a23eaf13f1045b4a60c8a62023f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:29:34 +0000

    Add package sshmux/0.1.2/sshmux-0.1.2.zip

---
+++ packages/sshmux/sshmux-0.1.2.zip/sshmux-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+version = '0.1.2'
+
+setup(name='sshmux',
+      version=version,
+      description="Work on multiple machines over ssh at once",
+      long_description="""\
+""",
+      classifiers=[],
+      keywords='ssh',
+      author='Padmakar Ojha',
+      author_email='padmakar.ojha@gmail.com',
+	  url='https://github.com/dvopsway/sshmux',
+      license='GPL-3.0',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=["pexpect"],
+      entry_points={
+          'console_scripts': [
+              'sshmux = sshmux.ssh:sshmux',
+          ]
+      },
+      )
