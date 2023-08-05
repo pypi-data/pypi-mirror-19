91e8e4d2f3708b590a36ac6dc9e5325360830e66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:31:19 +0000

    Add package nester1986/1.0.0/nester1986-1.0.0.zip

---
+++ packages/nester1986/nester1986-1.0.0.zip/nester1986-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name='nester1986',
+version='1.0.0',
+py_modules=['nester1986'],
+author='pythontyro',
+author_email='249760638@qq.com',
+url='http://www.headfirstlabs.com',
+description='A simple printer of nested lists',
+)
