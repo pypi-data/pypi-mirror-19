483f5a8050098a8b6e609dff9cccffd28f2b022b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:56 +0000

    Add package deco/0.5/deco-0.5.zip

---
+++ packages/deco/deco-0.5.zip/deco-0.5/test/testast.py
@@ -0,0 +1,45 @@
+import unittest
+import ast
+import inspect
+from deco import *
+
+@concurrent
+def conc_func(*args, **kwargs):
+    pass
+
+@synchronized
+def body_cases():
+    conc_func()
+    a = True if False else False
+    b = (lambda : True)()
+
+def indented():
+    @synchronized
+    def _indented():
+        conc_func()
+
+    return _indented()
+
+@synchronized
+def subscript_args():
+    d = type('', (object,), {"items": {(0,0): 0}})()
+    conc_func(d.items[0, 0])
+    #Read d to force a synchronization event
+    d = d
+    return conc_func.in_progress
+
+class TestAST(unittest.TestCase):
+
+    #This just shouldn't throw any exceptions
+    def test_body_cases(self):
+        body_cases()
+
+    #This just shouldn't throw any exceptions
+    def test_indent_cases(self):
+        indented()
+
+    def test_subscript_args(self):
+        self.assertFalse(subscript_args())
+
+if __name__ == "__main__":
+    unittest.main()
