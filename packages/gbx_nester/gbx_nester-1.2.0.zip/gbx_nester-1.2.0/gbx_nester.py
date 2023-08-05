cfaf4596bf975e4cf088824f38052c1a2be13512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:39 +0000

    Add package gbx_nester/1.2.0/gbx_nester-1.2.0.zip

---
+++ packages/gbx_nester/gbx_nester-1.2.0.zip/gbx_nester-1.2.0/gbx_nester.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#-*- coding=utf-8 -*-
+
+def print_lol(the_list,indent=False,level=0):
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_lol(each_item,indent,level+1)
+		else:
+			if indent:
+				for tab_stop in range(level):
+					print("\t",end='')
+			print(each_item)
+			
+# movies = ["The Holy Grail",1975,"Terry Jones & Terry Gilliam",91,["Graham Chapman", ["Michael Palin", "Jonn Cleese","Terry Gilliam","Eric Idle"]]]
+#
+# print_lol(movies)
+
+
