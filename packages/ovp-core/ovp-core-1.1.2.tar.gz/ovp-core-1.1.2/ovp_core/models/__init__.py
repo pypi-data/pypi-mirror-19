9d8f2fe64d8f72d752e8282f76ac96f6f3a564bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:55 +0000

    Add package ovp-core/1.1.2/ovp-core-1.1.2.tar.gz

---
+++ packages/ovp-core/ovp-core-1.1.2.tar.gz/ovp-core-1.1.2/ovp_core/models/__init__.py
@@ -0,0 +1,3 @@
+from ovp_core.models.address import GoogleAddress, AddressComponent, AddressComponentType, GoogleRegion
+from ovp_core.models.cause import Cause
+from ovp_core.models.skill import Skill
