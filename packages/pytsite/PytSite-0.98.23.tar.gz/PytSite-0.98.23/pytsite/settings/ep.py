a2ec8b87753df12cb9983ef20d12648b8bc11c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:59 +0000

    Add package pytsite/0.98.23/PytSite-0.98.23.tar.gz

---
+++ packages/pytsite/PytSite-0.98.23.tar.gz/PytSite-0.98.23/pytsite/settings/ep.py
@@ -0,0 +1,72 @@
+"""Settings Plugin Endpoints
+"""
+import re as _re
+from pytsite import auth as _auth, metatag as _metatag, lang as _lang, router as _router, http as _http, \
+    admin as _admin, util as _util
+from . import _api, _frm
+
+__author__ = 'Alexander Shepetko'
+__email__ = 'a@shepetko.com'
+__license__ = 'MIT'
+
+
+def _build_form(setting_uid: str) -> _frm.Form:
+    """Internal helper.
+    """
+    # Load setting definition
+    setting_def = _api.get_definition(setting_uid)
+
+    return setting_def['form']('settings-form', setting_uid=setting_uid)  # type: _frm.Form
+
+
+def form(args: dict, inp: dict) -> str:
+    """Render settings form.
+    """
+    uid = args.get('uid')
+
+    if not _check_permissions(uid):
+        raise _http.error.Forbidden()
+
+    # Load setting definition
+    setting_def = _api.get_definition(uid)
+
+    # Update page's title
+    _metatag.t_set('title', _lang.t(setting_def['title']))
+
+    # Instantiate form
+    frm = _build_form(uid)
+
+    return _admin.render_form(frm)
+
+
+def form_submit(args: dict, inp: dict) -> _http.response.Redirect:
+    """Process settings form submit.
+    """
+    uid = args.get('uid')
+    if not _check_permissions(uid):
+        raise _http.error.Forbidden()
+
+    frm = _build_form(uid).fill(inp)
+
+    value = {}
+    for k, v in inp.items():
+        # Here we process ALL input, not only form's widgets, because widgets can be added by JS on client side
+        if k.startswith('setting_'):
+            k = _re.sub('^setting_', '', k)
+            value[k] = v
+
+    _api.put(uid, _util.dict_merge(_api.get(uid), value))
+
+    frm.submit()
+
+    _router.session().add_success_message(_lang.t('pytsite.settings@settings_has_been_saved'))
+
+    return _http.response.Redirect(_router.ep_url('pytsite.settings@form', {'uid': uid}))
+
+
+def _check_permissions(uid: str) -> bool:
+    section_def = _api.get_definition(uid)
+    if section_def['perm_name'] == '*':
+        return True
+
+    return _auth.get_current_user().has_permission(section_def['perm_name'])
