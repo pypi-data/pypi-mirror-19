9a79280062bd32a1222b9a5612b409b2f991617c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:56 +0000

    Add package Beginner_nester/1.1.0/Beginner_nester-1.1.0.zip

---
+++ packages/Beginner_nester/Beginner_nester-1.1.0.zip/Beginner_nester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name     		='Beginner_nester',
+	version   		='1.1.0',
+	py_moudle 		=['Beginner_nester'],
+	author      	='tjdans',
+	author_email	='tjdans174@naver.com',
+	url				='',
+	description		='a simple is Best',
+	)
