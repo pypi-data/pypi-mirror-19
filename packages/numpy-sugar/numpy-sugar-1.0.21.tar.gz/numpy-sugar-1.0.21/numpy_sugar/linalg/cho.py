1f7be622a87d0942acc3c2b14e6057c598777bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:17 +0000

    Add package numpy-sugar/1.0.21/numpy-sugar-1.0.21.tar.gz

---
+++ packages/numpy-sugar/numpy-sugar-1.0.21.tar.gz/numpy-sugar-1.0.21/numpy_sugar/linalg/cho.py
@@ -0,0 +1,23 @@
+def cho_solve(L, b):
+    r"""Solve for Cholesky decomposition.
+
+    Solve the linear equations :math:`\mathrm A \mathbf x = \mathbf b`,
+    given the Cholesky factorization of :math:`\mathrm A`.
+
+    Args:
+        L (array_like): Lower triangular matrix.
+        b (array_like): Right-hand side.
+
+    Returns:
+        :class:`numpy.ndarray`: The solution to the system
+                                :math:`\mathrm A \mathbf x = \mathbf b`.
+
+    See Also
+    --------
+    numpy.linalg.cholesky : Cholesky decomposition.
+    scipy.linalg.cho_solve : Solve linear equations given Cholesky
+                             factorization.
+    """
+    from scipy.linalg import cho_solve as sp_cho_solve
+
+    return sp_cho_solve((L, True), b, check_finite=False)
