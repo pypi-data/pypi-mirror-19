85249b69bd8b1c8cb665f0b8b1dc3d502f004727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:37 +0000

    Add package hyperwallet-sdk/0.2.0/hyperwallet_sdk-0.2.0-py2-none-any.whl

---
+++ packages/hyperwallet-sdk/hyperwallet_sdk-0.2.0-py2-none-any.whl/hyperwallet/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+'''A library that provides a Python interface to the Hyperwallet API'''
+
+__author__          = 'The Hyperwallet Developers'
+__email__           = 'devsupport@hyperwallet.com'
+__copyright__       = 'Copyright (c) 2016 Hyperwallet'
+__license__         = 'MIT'
+__version__         = '0.2.0'
+__url__             = 'https://github.com/hyperwallet/python-sdk'
+__download_url__    = 'https://pypi.python.org/pypi/hyperwallet-sdk'
+__description__     = 'A Python wrapper around the Hyperwallet API'
+
+
+from .api import Api                                                     # noqa
