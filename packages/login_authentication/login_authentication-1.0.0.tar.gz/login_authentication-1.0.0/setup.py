2544ce0c6f6d31b8c51edf2377317a97a8e05a37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:01 +0000

    Add package login_authentication/1.0.0/login_authentication-1.0.0.tar.gz

---
+++ packages/login_authentication/login_authentication-1.0.0.tar.gz/login_authentication-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+             name  ='login_authentication',
+          version  ='1.0.0',
+       py_modules  =['login_authentication'],
+           author  ='jack.haha',
+       author_email='test@163.com',
+              url  ='http://www.headfirsttlabs.com',
+       description ='a simple example',
+     )
