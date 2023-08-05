29e2ee8384df11fe7bc0071842e4fdc612df21e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:11 +0000

    Add package fuzzy_dempster_shafer/0.1/fuzzy_dempster_shafer-0.1.tar.gz

---
+++ packages/fuzzy_dempster_shafer/fuzzy_dempster_shafer-0.1.tar.gz/fuzzy_dempster_shafer-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name = 'fuzzy_dempster_shafer',
+    version = '0.1',
+    description = 'Prediction algorithm based in Dempster-Shafer Fuzzy Logic and Bayesian principles',
+    url = 'https://github.com/phystistics/lrds',
+    author = 'Austin Powell',
+    author_email = 'powellaus10@gmail.com',
+    license = 'MIT',
+    zip_safe = False)
