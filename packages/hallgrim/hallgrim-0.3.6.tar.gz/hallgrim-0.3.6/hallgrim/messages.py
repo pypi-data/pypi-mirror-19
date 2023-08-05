da4702f70abb3138130491ed06deaa53d77e4265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:18 +0000

    Add package hallgrim/0.3.6/hallgrim-0.3.6.tar.gz

---
+++ packages/hallgrim/hallgrim-0.3.6.tar.gz/hallgrim-0.3.6/hallgrim/messages.py
@@ -0,0 +1,20 @@
+import sys
+
+def warn(msg):
+    print('[WARN]', msg)
+
+def debug(msg):
+    print('[DEBUG]', msg)
+
+def info(msg, notag=False):
+    print('[INFO]' if not notag else '      ', msg)
+
+def error(msg):
+    print('[ERROR]', msg)
+
+def abort(msg):
+    print('[FATAL]', msg)
+    sys.exit('exiting...')
+
+def exit():
+    sys.exit('exiting...')
