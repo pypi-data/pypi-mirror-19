ea2eefc3cd654805b421e96e39127f41c02478ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:44 +0000

    Add package jupyter_highlight_selected_word/0.0.6/jupyter_highlight_selected_word-0.0.6.tar.gz

---
+++ packages/jupyter_highlight_selected_word/jupyter_highlight_selected_word-0.0.6.tar.gz/jupyter_highlight_selected_word-0.0.6/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+"""Setup script for jupyter_highlight_selected_word."""
+
+from setuptools import find_packages, setup
+
+setup(
+    name='jupyter_highlight_selected_word',
+    version='0.0.6',
+    description=('Jupyter notebook extension that enables highighting of every'
+                 ' instance of the current word in the editor.'),
+    author='Joshua Cooke Barnes',
+    author_email='joshuacookebarnes@gmail.com',
+    url='https://github.com/jcb91/jupyter_highlight_selected_word.git',
+    license='BSD',
+    long_description="""
+Jupyter notebook extension that enables the CodeMirror addon
+`Match Highlighter <https://codemirror.net/demo/matchhighlighter.html>`_
+which highlights all instances of the selected word in the current editor.
+    """,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+)
---
+++ packages/jupyter_highlight_selected_word/jupyter_highlight_selected_word-0.0.6.tar.gz/jupyter_highlight_selected_word-0.0.6/src/jupyter_highlight_selected_word/__init__.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+"""Provides magically-named functions for python-package installation."""
+
+__version__ = '0.0.6'
+
+
+def _jupyter_nbextension_paths():
+    return [dict(
+        section='notebook',
+        # src is relative to current module
+        src='static/highlight_selected_word',
+        # dest directory is in the `nbextensions/` namespace
+        dest='highlight_selected_word',
+        # require is also in the `nbextensions/` namespace
+        require='highlight_selected_word/main',
+    )]
