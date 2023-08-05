fa9087a59d3781cdf870fe1094f655938e0d5899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:32:57 +0000

    Add package drf-schema-adapter/0.9.0/drf-schema-adapter-0.9.0.tar.gz

---
+++ packages/drf-schema-adapter/drf-schema-adapter-0.9.0.tar.gz/drf-schema-adapter-0.9.0/drf_auto_endpoint/adapters.py
@@ -0,0 +1,93 @@
+class BaseAdapter(object):
+    """
+    Basic adapter that renders a dict to json with no modifications.
+    """
+
+    def render(self, config):
+        return config['fields']
+
+    def __call__(self, config):
+        return self.render(config)
+
+
+def to_html_tag(widget_type):
+    if widget_type == "checkbox":
+        return "checkbox"
+    if widget_type == "select":
+        return "select"
+    return "input"
+
+
+class AngularFormlyAdapter(BaseAdapter):
+
+    def render(self, config):
+        fields = config['fields'];
+        adapted = []
+        for field in fields:
+            field["validation"].update({
+                "label": field["ui"]["label"],
+                "type": field["type"],
+            })
+
+            new_field = {
+                "key": field["key"],
+                "read_only": field["read_only"],
+                "type": to_html_tag(field["type"]),
+                "templateOptions": field["validation"]
+            }
+
+            if "placeholder" in field["ui"]:
+                new_field["templateOptions"]["placeholder"] = field["ui"]["placeholder"]
+
+            if "default" in field:
+                new_field["defaultValue"] = field["default"]
+
+            adapted.append(new_field)
+
+        return adapted
+
+
+class EmberAdapter(BaseAdapter):
+    """
+    Here is an example of the expected output
+        [{
+            "read_only": False,
+            "extra": {
+                "related_model": "emberadmin/category",
+                "choices": [{"foo": "bar"}]
+            },
+            "name": "category",
+            "label": "Category",
+            "widget": "foreignkey"
+        }, ...]
+    """
+
+    def render(self, config):
+        fields = config['fields'];
+        adapted = []
+        for field in fields:
+            new_field = {
+                "label": field["ui"]["label"],
+                "read_only": field["read_only"],
+                "extra": {},
+                "name": field["key"],
+                "widget": field["type"],
+            }
+
+            if "choices" in field:
+                new_field["extra"]["choices"] = field["choices"]
+
+            if "related_endpoint" in field:
+                new_field["extra"]["related_model"] = field["related_endpoint"]
+
+            if "placeholder" in field["ui"]:
+                new_field["extra"]["placeholder"] = field["ui"]["placeholder"]
+
+            if "default" in field:
+                new_field["extra"]["default"] = field["default"]
+
+            adapted.append(new_field)
+
+        config['fields'] =  adapted
+
+        return config
---
+++ packages/drf-schema-adapter/drf-schema-adapter-0.9.0.tar.gz/drf-schema-adapter-0.9.0/drf_auto_endpoint/metadata.py
@@ -0,0 +1,91 @@
+from django.utils.module_loading import import_string
+
+from rest_framework.fields import empty
+from rest_framework.metadata import SimpleMetadata, BaseMetadata
+
+from .utils import get_validation_attrs
+
+
+class AutoMetadataMixin(object):
+    def determine_metadata(self, request, view):
+        from drf_auto_endpoint.app_settings import settings
+
+        serializer_instance = view.serializer_class()
+        try:
+             metadata = super(AutoMetadataMixin, self).determine_metadata(request, view)
+        except NotImplementedError:
+            metadata = {}
+        except AttributeError:
+            metadata = {}
+
+        if not hasattr(view, 'endpoint'):
+            fields_metadata = []
+
+            for field in view.serializer_class.Meta.fields:
+                if field in {'id', '__str__'}:
+                    continue
+
+                instance_field = serializer_instance.fields[field]
+                type_ = settings.WIDGET_MAPPING.get(instance_field.__class__.__name__)
+
+                if type_ is None:
+                    raise NotImplementedError()
+
+                field_metadata = {
+                    'key': field,
+                    'type': type_,
+                    'read_only': False,
+                    'ui': {
+                        'label': field.title(),
+                    },
+
+                    'validation': {
+                        'required': instance_field.required,
+                    },
+                    'extra': {}
+                }
+
+                default = instance_field.default
+
+                if default and default != empty:
+                    field_metadata['default'] = default
+
+                if getattr(instance_field, 'choices', None):
+                    field_metadata['choices'] = [{
+                        'label': v,
+                        'value': k,
+                    } for k, v in instance_field.choices.items()]
+
+                field_metadata['validation'].update(get_validation_attrs(instance_field))
+
+                if type_ == 'foreignkey':
+                    field_metadata['related_endpoint'] = field
+
+                fields_metadata.append(field_metadata)
+                metadata.update({
+                    'list_display': ['__str__', ],
+                    'filter_fields': [],
+                    'search_fields': [],
+                    'ordering_fields': [],
+                    'fields': fields_metadata,
+                    'fieldsets': [{'title': None, 'fields': [
+                        field
+                        for field in view.serializer_class.Meta.fields
+                        if field != 'id' and field != '__str__'
+                    ]}]
+                })
+        else:
+            for prop in ['fields', 'list_display', 'filter_fields', 'search_enabled', 'ordering_fields',
+                         'needs', 'fieldsets']:
+                metadata[prop] = getattr(view.endpoint, 'get_{}'.format(prop))()
+
+        adapter = import_string(settings.METADATA_ADAPTER)()
+        return adapter(metadata)
+
+
+class AutoMetadata(AutoMetadataMixin, SimpleMetadata):
+    pass
+
+
+class MinimalAutoMetadata(AutoMetadataMixin, BaseMetadata):
+    pass
