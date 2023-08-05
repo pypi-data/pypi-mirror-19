b22fb6e1fd5a9817408fc614ff654b58bd27b3f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:33 +0000

    Add package SoloLearn/0.1dev/SoloLearn-0.1dev.tar.gz

---
+++ packages/SoloLearn/SoloLearn-0.1dev.tar.gz/SoloLearn-0.1dev/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name="SoloLearn",
+    version="0.1dev",
+    packages=["sololearn",],
+    license="MIT",
+    long_description=open("README.txt").read(),
+    author="eckhard",
+    author_email="software@eckhard-weber.de",
+    url="https://pypi.python.org/id/eckhard",
+)
