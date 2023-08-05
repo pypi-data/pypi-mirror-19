f87b7937a90ebeb6fb430e3a8026dde64e7a455a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:12 +0000

    Add package markover/0.3/markover-0.3.tar.gz

---
+++ packages/markover/markover-0.3.tar.gz/markover-0.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'markover',
+  packages = ['markover'], # this must be the same as the name above
+  scripts = ['markover/markover'],
+  version = '0.3',
+  description = 'Text Processing with Markov',
+  author = 'Pedram Pourdavood',
+  author_email = 'pedrampourdavood@gmail.com',
+  url = 'https://github.com/Pedram26/pyMarkov', # use the URL to the github repo
+  download_url = 'https://github.com/pedram26/pyMarkov/tarball/0.1', # I'll explain this in a second
+  keywords = ['file', 'text processing', 'markov'], # arbitrary keywords
+  classifiers = [],
+)
