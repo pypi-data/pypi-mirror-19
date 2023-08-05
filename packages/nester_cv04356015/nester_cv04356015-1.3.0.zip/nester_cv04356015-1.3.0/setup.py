a3c0e46a2fe3a50dd4dedfbb8a2a0bfcfd328628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:03:56 +0000

    Add package nester_cv04356015/1.3.0/nester_cv04356015-1.3.0.zip

---
+++ packages/nester_cv04356015/nester_cv04356015-1.3.0.zip/nester_cv04356015-1.3.0/setup.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+from distutils.core import setup
+
+setup( 
+        name         = 'nester_cv04356015', 
+        version      = '1.3.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['nester'],
+        author       = 'cv04356015',
+        author_email = 'cv04356015@gmail.com',
+        url          = 'https://pypi.python.org/pypi?%3Aaction=pkg_edit&name=nester_cv04356015',
+        description  = 'A simple printer of nested lists',
+     )
