7e4e094546dff70614ce9841d704eb51ecf2948c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:03 +0000

    Add package ducted/1.0/ducted-1.0.tar.gz

---
+++ packages/ducted/ducted-1.0.tar.gz/ducted-1.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="ducted",
+    version='1.0',
+    url='http://github.com/ducted/duct',
+    license='MIT',
+    description="A monitoring agent and event processor",
+    author='Colin Alston',
+    author_email='colin.alston@gmail.com',
+    packages=find_packages() + [
+        "twisted.plugins",
+    ],
+    package_data={
+        'twisted.plugins': ['twisted/plugins/duct_plugin.py']
+    },
+    include_package_data=True,
+    install_requires=[
+        'Twisted',
+        'PyYaml',
+        'protobuf',
+        'construct<2.6',
+        'pysnmp==4.2.5',
+        'cryptography',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Monitoring',
+    ],
+)
