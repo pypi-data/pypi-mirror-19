4ffed921a4007a100681c088503fef3445360e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:55 +0000

    Add package bottle-neck/1.16/bottle-neck-1.16.tar.gz

---
+++ packages/bottle-neck/bottle-neck-1.16.tar.gz/bottle-neck-1.16/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='bottle-neck',
+    version='1.16',
+    packages=['bottle_neck'],
+    url='https://github.com/agile4you/bottle-neck.git',
+    license='GLPv3',
+    author='Papavassiliou Vassilis',
+    author_email='vpapavasil@gmail.com',
+    description='Web services with bottle.py made easy!',
+    extras_require={
+        'test': ['pytest', 'bottle', 'six'],
+    }
+)
