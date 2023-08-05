07144aac12dfd6d46dcd7a18daddf3f2534340f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:24 +0000

    Add package infi.recipe.template.version/0.4.26/infi.recipe.template.version-0.4.26-py2.7.egg

---
+++ packages/infi.recipe.template.version/infi.recipe.template.version-0.4.26-py2.7.egg/infi/recipe/template/version/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.4.26"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = '(HEAD detached at v0.4.26)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '30d034813c41ebbfee2d20da5af5623e267a7f8b'
+__git_head_subject__ = 'Finished Release v0.4.26'
+__git_head_message__ = ''
+__git_dirty_diff__ = ''
+__git_commit_date__ = '2016-12-18 14:23:34'
