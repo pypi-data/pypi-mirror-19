b156068777617e4d9cdb774e9f0656600e5d9d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:33:06 +0000

    Add package begin_v/1.3.0/begin_v-1.3.0.zip

---
+++ packages/begin_v/begin_v-1.3.0.zip/begin_v-1.3.0/begin_v.py
@@ -0,0 +1,10 @@
+def print_lol(the_list,indent=False,level=0):
+	for each in the_list:
+		if isinstance(each, list):
+			print_lol(each,indent,level+1)
+		else:
+                        if indent:
+                                for tab_stop in range(level):
+                                        print("\t", end='')
+                        print(each)
+                              
