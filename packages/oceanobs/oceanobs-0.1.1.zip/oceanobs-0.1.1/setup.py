a63d12739f2f242bca2472c436f327065be97b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:42 +0000

    Add package oceanobs/0.1.1/oceanobs-0.1.1.zip

---
+++ packages/oceanobs/oceanobs-0.1.1.zip/oceanobs-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='oceanobs',
+  packages=['oceanobs'],
+  version='0.1.1',
+  description='Package to analyze ocean observatories data.',
+  author='Raul Bardaji',
+  author_email='rbardaji@gmail.com',
+  url='https://github.com/rbardaji/oceanobs', # use the URL to the github repo
+  download_url='https://github.com/rbardaji/oceanobs/tarball/0.1.1',
+  keywords=['observatory', 'ocean', 'sea', 'GUI', 'OBSEA', 'EMODnet'], # arbitrary keywords
+  classifiers=[],
+)
