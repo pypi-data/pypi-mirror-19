66cb779384162017d27c4601681f8dff08513a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:05:55 +0000

    Add package wasp-eureka/1.1.1/wasp-eureka-1.1.1.tar.gz

---
+++ packages/wasp-eureka/wasp-eureka-1.1.1.tar.gz/wasp-eureka-1.1.1/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+# Version managed by bumpversion, do not change it directly.
+__version__ = '1.1.1'
+
+
+setup(name='wasp-eureka',
+      description='Async Eureka client',
+      version=__version__,
+      author='Matt Rasband',
+      author_email='matt.rasband@gmail.com',
+      license='Apache-2.0',
+      url='https://github.com/wickedasp/eureka',
+      download_url=('https://github.com/wickedasp/eureka'
+                    '/archive/v' + __version__ + '.tar.gz'),
+      keywords=[
+          'microservice',
+          'netflixoss',
+          'asyncio',
+          'springcloud',
+      ],
+      packages=find_packages(exclude=('examples',)),
+      classifiers=[
+          'Programming Language :: Python :: 3.5',
+          'License :: OSI Approved :: Apache Software License',
+          'Intended Audience :: Developers',
+          'Development Status :: 4 - Beta',
+          'Topic :: Software Development',
+      ],
+      setup_requires=[
+          'pytest-runner',
+          'flake8',
+      ],
+      install_requires=[
+          'aiohttp',
+      ],
+      extras_require={
+          'standalone': [
+              'apscheduler>=3.3.0',
+          ]
+      },
+      tests_require=[
+          'pytest-aiohttp',
+          'pytest'
+      ],
+      entry_points={},
+      zip_safe=False)
