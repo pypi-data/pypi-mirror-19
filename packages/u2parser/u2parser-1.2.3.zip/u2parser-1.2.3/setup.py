36fedfa639049fc77901fd64f7d20d2a39d45057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:19 +0000

    Add package u2parser/1.2.3/u2parser-1.2.3.zip

---
+++ packages/u2parser/u2parser-1.2.3.zip/u2parser-1.2.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='u2parser',
+    version='1.2.3',
+    author='Diego Fernandez',
+    author_email='di3g0bs0n@gmail.com',
+    description=('A parser for Unified2 logs (Snort).'),
+    long_description='A unified2 log parser which reads a file, and returns all records.',
+    license='GPLv3',
+    keywords='snort unified2 parser',
+    packages=['u2parser'],
+    url='https://github.com/di3g0bs0n/u2parser',
+    install_requires=['netaddr==0.7.18'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities'
+    ]
+)
