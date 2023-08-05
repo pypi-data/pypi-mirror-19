21ebd393f2cfe7391747fbadd19b3dcd1acb8343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:42 +0000

    Add package django-data-explorer/0.0.7/django-data-explorer-0.0.7.tar.gz

---
+++ packages/django-data-explorer/django-data-explorer-0.0.7.tar.gz/django-data-explorer-0.0.7/gulpfile.js
@@ -0,0 +1,133 @@
+'use strict';
+
+var gulp = require('gulp');
+var Server = require('karma').Server;
+var bower = require('gulp-bower');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var url_adjuster = require('gulp-css-url-adjuster');
+var webpack = require('webpack-stream');
+var webpack_config = require('./webpack.config.js');
+var eslint = require('gulp-eslint');
+
+var config = {
+    paths: {
+        libs: './django_data_explorer/static/django_data_explorer/libs/',
+        static: {
+            src_folder: 'frontend_src',
+            dist_folder: 'frontend_dist',
+            sass: '/sass/**/*.scss',
+            css: '/css',
+            js: '/js/**/*.js',
+            images: '/images/*.*'
+        },
+        js_tests: 'js_tests/public/*.js',
+        sass: function() {
+            return config.paths.static.src_folder + config.paths.static.sass;
+        },
+        js: function() {
+            return config.paths.static.src_folder + config.paths.static.js;
+        },
+        dist: function() {
+            return config.paths.static.dist_folder;
+        },
+        css: function() {
+            return config.paths.static.dist_folder;
+        },
+        django: function() {
+            return './django_data_explorer/static/django_data_explorer/dist/';
+        },
+        images: function() {
+            return config.paths.static.src_folder + config.paths.static.images;
+        }
+    },
+    supported_browsers: [
+        "Android 2.3",
+        "Android >= 4",
+        "Chrome >= 20",
+        "Firefox >= 24",
+        "Explorer >= 8",
+        "iOS >= 6",
+        "Opera >= 12",
+        "Safari >= 6"
+    ]
+};
+
+gulp.task('bower', function() {
+    return bower();
+});
+
+gulp.task('images', function() {
+    gulp.src('node_modules/leaflet/dist/images/*.*').pipe(gulp.dest(config.paths.dist() + '/images'));
+    gulp.src('node_modules/datatables.net-dt/images/*.png').pipe(gulp.dest(config.paths.dist() + '/images'));
+    gulp.src(config.paths.images()).pipe(gulp.dest(config.paths.dist() + '/images'));
+});
+
+gulp.task('build:sass', function () {
+    return gulp.src(config.paths.sass())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(url_adjuster({
+            replace: ['../', '']
+        }))
+        .pipe(postcss([autoprefixer({browsers: config.supported_browsers})]))
+        .pipe(gulp.dest(config.paths.css()));
+});
+
+gulp.task('build:js', function() {
+    return gulp.src(config.paths.js())
+        .pipe(webpack(webpack_config))
+        .pipe(gulp.dest(config.paths.dist()));
+});
+
+/**
+ * Run build on file change
+ */
+gulp.task('watch', function() {
+    gulp.watch([config.paths.sass(), config.paths.js()], ['build']);
+});
+
+/**
+ * Run test once and exit
+ */
+gulp.task('unit_tests', function(done) {
+    new Server({
+        configFile: __dirname + '/karma.conf.js',
+        singleRun: true,
+        logLevel: "info"
+    }, done).start();
+});
+
+/**
+ * Run linter once and exit, tests will fail if there's a warning
+ */
+gulp.task('lint', function() {
+    var tasks = [];
+    tasks.push(
+        gulp.src(config.paths.js())
+            .pipe(eslint())
+            .pipe(eslint.format())
+            .pipe(eslint.failAfterError())
+    );
+    tasks.push(
+        gulp.src(config.paths.js_tests)
+            .pipe(eslint())
+            .pipe(eslint.format())
+            .pipe(eslint.failAfterError())
+    );
+    return tasks;
+});
+
+gulp.task('copy_dist', ['build:js', 'build:sass', 'images'], function() {
+    return gulp.src(config.paths.dist() + '/**/*.*')
+        .pipe(gulp.dest(config.paths.django()));
+});
+
+gulp.task('test', ['lint', 'unit_tests']);
+
+gulp.task('build', ['lint', 'copy_dist']);
+
+/**
+ * The default task (called when you run `gulp` from cli)
+ */
+gulp.task('default', ['watch', 'build']);
---
+++ packages/django-data-explorer/django-data-explorer-0.0.7.tar.gz/django-data-explorer-0.0.7/karma.conf.js
@@ -0,0 +1,136 @@
+var webpack = require('webpack');
+var webpackConfig = require('./webpack.config.js');
+var path = require('path');
+
+webpackConfig.entry = {};
+
+webpackConfig.isparta = {
+  embedSource: true,
+  noAutoWrap: true,
+  babel: {
+    presets: ['es2015']
+  }
+};
+
+webpackConfig.module.preLoaders = [
+  {
+    test: /\.js$/,
+    include: path.resolve('frontend_src/js/'),
+    loader: 'isparta'
+  }
+];
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['wiredep', 'qunit'],
+
+    client: {
+      qunit: {
+        testTimeout: 30000
+      }
+    },
+
+
+    // bower dependencies
+    // https://github.com/lbragaglia/karma-wiredep
+    wiredep: {
+      dependencies: true,
+      devDependencies: true
+    },
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'js_tests/test_index.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      'frontend_src/js/*.js': ['webpack', 'coverage'],
+      'js_tests/*.js': ['webpack']
+    },
+
+
+	webpack: webpackConfig,
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['mocha', 'coverage'],
+
+
+    coverageReporter: {
+      check: {
+        global: {
+          statements: 50,
+          branches: 50,
+          functions: 50,
+          lines: 50,
+          excludes: []
+        },
+        each: {
+          statements: 10,
+          branches: 10,
+          functions: 10,
+          lines: 10,
+          excludes: []
+        }
+      },
+      reporters: [
+        {type : 'html', dir : 'js_tests/coverage/'},
+        {type : 'text'}
+      ]
+    },
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+    browserNoActivityTimeout: 2400,
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: 1
+  })
+};
