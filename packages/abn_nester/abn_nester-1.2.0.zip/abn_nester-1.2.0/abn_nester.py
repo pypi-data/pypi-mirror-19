b78674271e9e8398a4dcc162fbda199f19e8c1de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:55 +0000

    Add package abn_nester/1.2.0/abn_nester-1.2.0.zip

---
+++ packages/abn_nester/abn_nester-1.2.0.zip/abn_nester-1.2.0/abn_nester.py
@@ -0,0 +1,11 @@
+"""this is nester.py  module is used to print lists which have or don't have nested lists"""
+def print_lol(the_list,level=0):
+        """ this function takes the_list (any python list )as argument .when a nested list is encounterd the function recalls itself"""
+        for each_item in the_list:
+                if isinstance(each_item,list):
+                        print_lol(each_item,level+1)
+                else:
+                        for tabs in range(level):
+                                print("\t",end='')
+                        print(each_item)        
+        
