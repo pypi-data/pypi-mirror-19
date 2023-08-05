7f68907abcf1ec26b6ee04297171f16397d440f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:35 +0000

    Add package nh_embedded_list_printer/1.2.0/nh_embedded_list_printer-1.2.0.zip

---
+++ packages/nh_embedded_list_printer/nh_embedded_list_printer-1.2.0.zip/nh_embedded_list_printer-1.2.0/setup.py
@@ -0,0 +1,17 @@
+'''
+@author:   Nick Handy
+@date:     12/28/16
+@summary:  Distribution setup module using nh_embedded_list_printer.py
+'''
+
+from distutils.core import setup
+
+setup( 
+    name            = 'nh_embedded_list_printer',
+    version         = '1.2.0',
+    py_modules      = ['nh_embedded_list_printer'],
+    author          = 'nhandy',
+    author_email    = 'nichandy@gmail.com',
+    url             = 'https://www.youtube.com/channel/UCNfouAHV2SXSI9G2sGNCv8A',
+    description     = 'A simple printer of nested lists with optional indents',
+    )
