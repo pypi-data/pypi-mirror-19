2ede950e723697b2ecbfe4939f6e5d1181e16abb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:11 +0000

    Add package restypy/0.7/restypy-0.7.tar.gz

---
+++ packages/restypy/restypy-0.7.tar.gz/restypy-0.7/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as file:
+    long_description = file.read()
+
+setup(
+  name='restypy',
+  packages=find_packages(),
+  version='0.7',
+  description='A Pythonic Rest API client',
+  long_description=long_description,
+  install_requires=[
+        'requests'
+  ],
+  author='Joji Antony',
+  author_email='simula67@gmail.com',
+  url='https://github.com/simula67/restypy',
+  download_url='https://github.com/simula67/restypy/tarball/master',
+  keywords=['rest', 'pythonic', 'client'],
+  classifiers=[],
+)
