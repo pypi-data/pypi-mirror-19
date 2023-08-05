40a09dcd957ab6eb0f6f9b8daf8d08a4a864b42b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:54 +0000

    Add package orange-kit/0.1.4a35/orange-kit-0.1.4a35.tar.gz

---
+++ packages/orange-kit/orange-kit-0.1.4a35.tar.gz/orange-kit-0.1.4a35/scripts/pytest.py
@@ -0,0 +1,12 @@
+# 项目：标准函数库
+# 模块：业务测试模块
+# 作者：黄涛
+# License:GPL
+# Email:huangtao.sh@icloud.com
+# 创建：2017-01-04 21:34
+
+import sys
+sys.path.insert(0,'.')
+from testing import *
+unittest.main()
+
