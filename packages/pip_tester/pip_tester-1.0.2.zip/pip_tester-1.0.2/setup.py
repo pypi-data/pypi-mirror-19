52a1ca8ec4f1fa16f27e86d8907250ecaf6dd64c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:10 +0000

    Add package pip_tester/1.0.2/pip_tester-1.0.2.zip

---
+++ packages/pip_tester/pip_tester-1.0.2.zip/pip_tester-1.0.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='pip_tester',
+    version='1.0.2',
+    packages=[''],
+    url='https://github.com/IntelRealSense/librealsense',
+    license='',
+    author='eshlomo',
+    author_email='eran.shlomo@intel.com',
+    description='',
+    scripts=['piptester.py']
+)
