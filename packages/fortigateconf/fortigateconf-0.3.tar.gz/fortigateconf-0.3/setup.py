1d5ecdc1d0222b8460836614b0aac9895b084fc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:54 +0000

    Add package fortigateconf/0.3/fortigateconf-0.3.tar.gz

---
+++ packages/fortigateconf/fortigateconf-0.3.tar.gz/fortigateconf-0.3/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+      name='fortigateconf',
+      version='0.3',
+      description='Python modules to interact with Fortigate configuration api',
+      install_requires=['requests'],
+      author='Nicolas Thomas',
+      author_email='nthomas@fortinet.com',
+      url='https://github.com/thomnico/fortigateconf',
+      packages=['fortigateconf'],
+      )
