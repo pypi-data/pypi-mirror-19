a52e59d40b01e7cf840e3075fa0f398b9d3d226a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:49 +0000

    Add package basicprop/0.2/basicprop-0.2.tar.gz

---
+++ packages/basicprop/basicprop-0.2.tar.gz/basicprop-0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'basicprop',
+  packages = ['basicprop'], # this must be the same as the name above
+  version = '0.2',
+  description = 'A synthetic dataset used for generative models',
+  author = 'Andrew Drozdov',
+  author_email = 'andrew@mrdrozdov.com',
+  url = 'https://github.com/mrdrozdov/basicprop', # use the URL to the github repo
+  download_url = 'https://github.com/mrdrozdov/basicprop/tarball/0.1', # I'll explain this in a second
+  keywords = ['deep-learning', 'gan', 'dataset'], # arbitrary keywords
+  classifiers = [],
+)
