4b59181892e6a1a6fd42a7d850e5b80ee5530289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:30:58 +0000

    Add package nester123_321/1.0.0/nester123_321-1.0.0.tar.gz

---
+++ packages/nester123_321/nester123_321-1.0.0.tar.gz/nester123_321-1.0.0/nester123_321.py
@@ -0,0 +1,8 @@
+def print_lol(the_list):
+	for i in the_list :
+	    if isinstance(i, list): 
+                print_lol(i)
+            else:
+                print(i)
+
+
---
+++ packages/nester123_321/nester123_321-1.0.0.tar.gz/nester123_321-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name  ='nester123_321',
+        version = '1.0.0',
+        py_modules = ['nester123_321'],
+        author = 'hfpython',
+        author_email = 'hfpython@headfirstlabs.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+        )
