0f74e3321af5cf3a2e1c777ff7c8c10430f1d7f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:09 +0000

    Add package cloudshare/0.1.1/cloudshare-0.1.1.tar.gz

---
+++ packages/cloudshare/cloudshare-0.1.1.tar.gz/cloudshare-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(
+    name='cloudshare',
+    packages=['cloudshare'],
+    version='0.1.1',
+    description='A python wrapper for the CloudShare REST API.',
+    author='Jonathan Sadan',
+    author_email='jonathan@cloudshare.com',
+    url='https://github.com/cloudshare/cloudshare-py-sdk',
+    keywords=['cloudshare', 'cloud', 'SDK', 'REST', 'API'],
+    classifiers=[],
+)
