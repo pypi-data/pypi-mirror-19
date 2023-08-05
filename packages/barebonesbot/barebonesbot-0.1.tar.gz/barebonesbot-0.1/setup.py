acc57aa02cb8af5fd12e99d023fd62538c9e0fab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:43 +0000

    Add package barebonesbot/0.1/barebonesbot-0.1.tar.gz

---
+++ packages/barebonesbot/barebonesbot-0.1.tar.gz/barebonesbot-0.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='barebonesbot',
+    version='0.1',
+    description='A configurable Twitter-bot',
+    author='Enrique Manjavacas',
+    author_email='enrique.manjavacas@gmail.com',
+    url='https://www.github.com/emanjavacas/barebonesbot/',
+    download_url='https://api.github.com/repos/emanjavacas/barebonesbot/tarball',
+    install_requires=[
+        'birdy>=0.2'
+    ],
+    license='MIT'
+)
