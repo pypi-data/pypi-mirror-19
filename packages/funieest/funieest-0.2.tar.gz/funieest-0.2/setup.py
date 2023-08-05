962facb950ef8a8b501215e6b53a8ed35242f82a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:01 +0000

    Add package funieest/0.2/funieest-0.2.tar.gz

---
+++ packages/funieest/funieest-0.2.tar.gz/funieest-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funieest',
+      version='0.2',
+      description='The funniest joke in the world',
+      url='https://github.com/hamadi15/test',
+      author='halim hamadi',
+      author_email='halim.hamadi@mail.ugm.ac.id',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
