39fd32cc2b7ea9258802a59b16761f15e76b13e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:14 +0000

    Add package pytablewriter/0.16.0/pytablewriter-0.16.0.tar.gz

---
+++ packages/pytablewriter/pytablewriter-0.16.0.tar.gz/pytablewriter-0.16.0/pytablewriter/writer/_md_writer.py
@@ -0,0 +1,89 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+import dataproperty as dp
+from mbstrdecoder import MultiByteStrDecoder
+
+from ._text_writer import IndentationTextTableWriter
+
+
+class MarkdownTableWriter(IndentationTextTableWriter):
+    """
+    Atable writer class for Markdown format.
+
+    :Examples:
+
+        :ref:`example-markdown-table-writer`
+    """
+
+    @property
+    def support_split_write(self):
+        return True
+
+    def __init__(self):
+        super(MarkdownTableWriter, self).__init__()
+
+        self.indent_string = ""
+        self.column_delimiter = "|"
+        self.char_cross_point = "|"
+        self.is_write_opening_row = True
+        self.is_quote_header = False
+        self.quote_flag_table[dp.Typecode.STRING] = False
+        self.quote_flag_table[dp.Typecode.DATETIME] = False
+
+        self._is_remove_line_break = True
+
+        self._prop_extractor.min_padding_len = 3
+
+    def write_table(self):
+        """
+        |write_table| with Markdown table format.
+
+        :raises pytablewriter.EmptyHeaderError: If the |header_list| is empty.
+
+        .. note::
+
+            - |None| values will be written as an empty string.
+        """
+
+        super(MarkdownTableWriter, self).write_table()
+
+    def _verify_header(self):
+        self._validate_empty_header()
+
+    def _get_opening_row_item_list(self):
+        if dp.is_empty_string(self.table_name):
+            return []
+
+        return [
+            "#" * (self._indent_level + 1) + " " +
+            MultiByteStrDecoder(self.table_name).unicode_str
+        ]
+
+    def _get_header_row_separator_item_list(self):
+        header_separator_list = []
+        for col_prop in self._column_prop_list:
+            padding_len = self._get_padding_len(col_prop)
+
+            if col_prop.align == dp.Align.RIGHT:
+                separator_item = "-" * (padding_len - 1) + ":"
+            elif col_prop.align == dp.Align.CENTER:
+                separator_item = ":" + "-" * (padding_len - 2) + ":"
+            else:
+                separator_item = "-" * padding_len
+
+            header_separator_list.append(separator_item)
+
+        return header_separator_list
+
+    def _get_value_row_separator_item_list(self):
+        return []
+
+    def _get_closing_row_item_list(self):
+        return []
---
+++ packages/pytablewriter/pytablewriter-0.16.0.tar.gz/pytablewriter-0.16.0/pytablewriter/writer/_mediawiki_writer.py
@@ -0,0 +1,89 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import absolute_import
+from __future__ import unicode_literals
+import re
+
+import dataproperty as dp
+from mbstrdecoder import MultiByteStrDecoder
+from six.moves import zip
+
+from ._text_writer import TextTableWriter
+
+
+class MediaWikiTableWriter(TextTableWriter):
+    """
+    A table writer class for MediaWiki format.
+
+    :Examples:
+
+        :ref:`example-mediawiki-table-writer`
+    """
+
+    __RE_TABLE_SEQUENCE = re.compile("^[\s]+[*|#]+")
+
+    @property
+    def support_split_write(self):
+        return True
+
+    def __init__(self):
+        super(MediaWikiTableWriter, self).__init__()
+
+        self.column_delimiter = "\n"
+
+        self.is_padding = False
+        self.is_write_header_separator_row = True
+        self.is_write_value_separator_row = True
+        self.is_write_opening_row = True
+        self.is_write_closing_row = True
+        self.is_quote_header = False
+        self.quote_flag_table = {}
+
+    def _write_header(self):
+        if not self.is_write_header:
+            return
+
+        if dp.is_not_empty_string(self.table_name):
+            self._write_line(
+                "|+" + MultiByteStrDecoder(self.table_name).unicode_str)
+
+        super(MediaWikiTableWriter, self)._write_header()
+
+    def _write_value_row(self, value_list, value_prop_list):
+        self._write_row([
+            self.__modify_table_element(value, value_prop)
+            for value, value_prop, in zip(value_list, value_prop_list)
+        ])
+
+    def _get_opening_row_item_list(self):
+        return '{| class="wikitable"'
+
+    def _get_header_row_separator_item_list(self):
+        return ["|-"]
+
+    def _get_value_row_separator_item_list(self):
+        return self._get_header_row_separator_item_list()
+
+    def _get_closing_row_item_list(self):
+        return "|}"
+
+    def _get_header_format_string(self, col_prop, value_prop):
+        return "! {{:{:s}{:s}}}".format(
+            self._get_center_align_formatformat(),
+            str(self._get_padding_len(col_prop, value_prop)))
+
+    def __modify_table_element(self, value, value_prop):
+        if value_prop.align is dp.Align.LEFT:
+            forma_stirng = '| {1:s}'
+        else:
+            forma_stirng = '| style="text-align:{0:s}"| {1:s}'
+
+        if self.__RE_TABLE_SEQUENCE.search(value) is not None:
+            value = "\n" + value.lstrip()
+
+        return forma_stirng.format(
+            value_prop.align.align_string, value)
