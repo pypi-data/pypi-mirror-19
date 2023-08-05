ffedd8a2422dfd4059a7fa079fc6c9f388a59459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:07 +0000

    Add package def_print_list/1.3.0/def_print_list-1.3.0.tar.gz

---
+++ packages/def_print_list/def_print_list-1.3.0.tar.gz/def_print_list-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name		= 'def_print_list',
+	version		= '1.3.0',
+	py_modules	= ['def_print_list'],
+	author		= 'lear',
+	author_email	= 'lear@52li.vip',
+	url		= 'http://52li.me',
+	description	= 'A  simple  printer of nested lists',
+)
