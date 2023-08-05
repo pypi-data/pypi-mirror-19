741c606e671ab0af5b00e60fc521f61a0b43564c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:47 +0000

    Add package pyunsplash/1.0.0a1/pyunsplash-1.0.0a1.zip

---
+++ packages/pyunsplash/pyunsplash-1.0.0a1.zip/pyunsplash-1.0.0a1/pyunsplash/__init__.py
@@ -0,0 +1,13 @@
+###############################################################################
+#
+#      File: pyunsplash.py
+#
+#    Author: Salvatore Ventura <salvoventura@gmail.com>
+#      Date: 08 Dec 2016
+#   Purpose: simplify import statements
+#
+#   Comment:
+#
+###############################################################################
+from pyunsplash import PyUnsplash
+__version__ = '1.0.0a1'
