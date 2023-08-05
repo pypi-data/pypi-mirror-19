62a761d4591de739df833ec2e33c0b1a39247e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:47 +0000

    Add package tudo/0.1.0.dev4/tudo-0.1.0.dev4-py3-none-any.whl

---
+++ packages/tudo/tudo-0.1.0.dev4-py3-none-any.whl/tudo/task.py
@@ -0,0 +1,23 @@
+import datetime
+
+from pytz import timezone, utc
+
+tz_germany = timezone("Europe/Berlin")
+
+
+class Task:
+
+    # Creates a Task
+    def __init__(self, number, description, started, finished=None, important=0, urgent=0):
+        self.number = number
+        self.description = description
+        self.started = utc.localize(started).astimezone(tz_germany)
+        self.finished = utc.localize(finished).astimezone(tz_germany) if finished else None
+        self.important = important
+        self.urgent = urgent
+
+    def is_finished(self):
+        return False if not self.finished else True
+
+    def date_from_timestamp(self, timestamp):
+        return datetime.datetime.fromtimestamp(timestamp, tz_germany) if timestamp else None
