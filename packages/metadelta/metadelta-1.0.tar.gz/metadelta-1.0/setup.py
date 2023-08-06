46b4b1e0c0c4b99fe7719ed76aa1f4a9595c4245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:43 +0000

    Add package metadelta/1.0/metadelta-1.0.tar.gz

---
+++ packages/metadelta/metadelta-1.0.tar.gz/metadelta-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='metadelta',
+      version='1.0',
+      description='A small library for advanced math.',
+      author='Gerald Nash',
+      author_email='g.nash.dev@gmail.com',
+      url='http://aunyks.com/metadelta',
+      keywords= ['cas', 'calculus', 'math', 'symbolic math', 'advanced math'],
+)
