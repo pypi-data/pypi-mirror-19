3006828cdbce3255dccd0c64d3c06522181797f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:04 +0000

    Add package python-manilaclient/1.12.0/python_manilaclient-1.12.0-py2.py3-none-any.whl

---
+++ packages/python-manilaclient/python_manilaclient-1.12.0-py2.py3-none-any.whl/manilaclient/common/constants.py
@@ -0,0 +1,86 @@
+# Copyright 2014 Mirantis, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+"""Common constants that can be used all over the manilaclient."""
+
+# These are used for providing desired sorting params with list requests
+SORT_DIR_VALUES = ('asc', 'desc')
+
+SHARE_SORT_KEY_VALUES = (
+    'id', 'status', 'size', 'host', 'share_proto',
+    'export_location', 'availability_zone',
+    'user_id', 'project_id',
+    'created_at', 'updated_at',
+    'display_name', 'name',
+    'share_type_id', 'share_type',
+    'share_network_id', 'share_network',
+    'snapshot_id', 'snapshot',
+)
+
+SNAPSHOT_SORT_KEY_VALUES = (
+    'id',
+    'status',
+    'size',
+    'share_id',
+    'user_id',
+    'project_id',
+    'progress',
+    'name',
+    'display_name',
+)
+
+CONSISTENCY_GROUP_SORT_KEY_VALUES = (
+    'id',
+    'name',
+    'status',
+    'host',
+    'user_id',
+    'project_id',
+    'created_at',
+    'source_cgsnapshot_id',
+)
+
+CG_SNAPSHOT_SORT_KEY_VALUES = (
+    'id',
+    'name',
+    'status',
+    'host',
+    'user_id',
+    'project_id',
+    'created_at',
+)
+
+CG_SNAPSHOT_MEMBERS_SORT_KEY_VALUES = (
+    'id',
+    'name',
+    'created_at',
+    'size',
+    'share_protocol',
+    'project_id',
+    'share_type_id',
+    'cgsnapshot_id',
+)
+
+TASK_STATE_MIGRATION_SUCCESS = 'migration_success'
+TASK_STATE_MIGRATION_ERROR = 'migration_error'
+TASK_STATE_MIGRATION_CANCELLED = 'migration_cancelled'
+TASK_STATE_MIGRATION_DRIVER_PHASE1_DONE = 'migration_driver_phase1_done'
+TASK_STATE_DATA_COPYING_COMPLETED = 'data_copying_completed'
+
+EXPERIMENTAL_HTTP_HEADER = 'X-OpenStack-Manila-API-Experimental'
+V1_SERVICE_TYPE = 'share'
+V2_SERVICE_TYPE = 'sharev2'
+
+SERVICE_TYPES = {'1': V1_SERVICE_TYPE, '2': V2_SERVICE_TYPE}
