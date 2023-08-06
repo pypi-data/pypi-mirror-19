72d87a44480544f550f9cb82d3939502d3d841de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:27 +0000

    Add package mastercard-parameter-management-services/1.0.3/mastercard_parameter_management_services-1.0.3.tar.gz

---
+++ packages/mastercard-parameter-management-services/mastercard_parameter_management_services-1.0.3.tar.gz/mastercard_parameter_management_services-1.0.3/mastercardparametermanagementservices/__init__.py
@@ -0,0 +1,8 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardparametermanagementservices.resourceconfig import *
+
+
+# import apis into api package
+from mastercardparametermanagementservices.partnerstatus import *
