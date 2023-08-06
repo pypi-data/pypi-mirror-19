50489fba26c364f95659ee3a7a2a19c6fc3ffe93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:40 +0000

    Add package pychatangobot/0.0.6/pychatangobot-0.0.6.tar.gz

---
+++ packages/pychatangobot/pychatangobot-0.0.6.tar.gz/pychatangobot-0.0.6/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+RELEASE_VERSION = '0.0.6'
+
+setup(
+    name='pychatangobot',
+    version=RELEASE_VERSION,
+    url='https://github.com/dozymoe/PyChatangoBot',
+    download_url='https://github.com/dozymoe/PyChatangoBot/tarball/' +\
+            RELEASE_VERSION,
+
+    author='Fahri Reza',
+    author_email='dozymoe@gmail.com',
+    description='asyncio implementation of Chatango bot (ch.py).',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    platforms='any',
+    license='MIT',
+    install_requires=[
+        "aiohttp",
+        "bleach",
+        "pycircularbuffer",
+        "pytz",
+    ],
+    ext_modules=[],
+)
+
+print('Visit https://github.com/dozymoe/PyChatangoBot for example ' +\
+        'run-bot scripts.')
