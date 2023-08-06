dccd127e94dae9919a2a84ee574a541eea506d42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:35 +0000

    Add package target-stitch/0.5.1/target-stitch-0.5.1.tar.gz

---
+++ packages/target-stitch/target-stitch-0.5.1.tar.gz/target-stitch-0.5.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os.path
+
+setup(name='target-stitch',
+      version='0.5.1',
+      description='A target for the Stitch API',
+      author='Stitch',
+      url='https://github.com/stitchstreams/target-stitch',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['target_stitch'],
+      install_requires=['stitchclient>=0.4.1',
+                        'jsonschema',
+                        'strict-rfc3339',
+                        'stitchstream-python>=0.4.1'],
+      entry_points='''
+          [console_scripts]
+          target-stitch=target_stitch:main
+      '''
+)
