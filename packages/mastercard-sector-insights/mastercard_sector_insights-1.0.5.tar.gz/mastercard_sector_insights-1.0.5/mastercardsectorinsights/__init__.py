b8bc31ef38657e9fb8ff70357974fc90664acd55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:25 +0000

    Add package mastercard-sector-insights/1.0.5/mastercard_sector_insights-1.0.5.tar.gz

---
+++ packages/mastercard-sector-insights/mastercard_sector_insights-1.0.5.tar.gz/mastercard_sector_insights-1.0.5/mastercardsectorinsights/__init__.py
@@ -0,0 +1,9 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardsectorinsights.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardsectorinsights.insights import *
+from mastercardsectorinsights.parameters import *
