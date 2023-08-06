13736e45e8d8af83f244488d6096eb4f5edf3d64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:27 +0000

    Add package micescraper/0.0.7/micescraper-0.0.7.tar.gz

---
+++ packages/micescraper/micescraper-0.0.7.tar.gz/micescraper-0.0.7/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name='micescraper',
+    version='0.0.7',
+    description='Applicant pdf scraper for MICE',
+    url='https://github.com/sirrice/micescraper',
+    author='Eugene wu',
+    author_email='ewu@cs.columbia.edu',
+    packages=['micescraper'],
+    package_dir = {'micescraper' : 'micescraper'},
+    scripts=['bin/micescraper'],
+    keywords=['scraper'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python'
+    ],
+    long_description='see http://github.com/sirrice/micescraper',
+    install_requires = [ 'click', 'requests', 'pyquery' ]
+)
