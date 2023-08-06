b27a9c0c4f4bcea4bfd726bd8add54147a2b426d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:48 +0000

    Add package chainerrl/0.0.1/chainerrl-0.0.1.tar.gz

---
+++ packages/chainerrl/chainerrl-0.0.1.tar.gz/chainerrl-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name='chainerrl',
+      version='0.0.1',
+      description='ChainerRL, a deep reinforcement learning library',
+      author='fujita',
+      author_email='fujita@preferred.jp',
+      packages=find_packages(),)
