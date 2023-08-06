a44401f30ab9a8a4731ee7e6101b2e3a5816c532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:43 +0000

    Add package weles/0.3.5/weles-0.3.5.tar.gz

---
+++ packages/weles/weles-0.3.5.tar.gz/weles-0.3.5/weles/sklDTC.py
@@ -0,0 +1,11 @@
+# Adapter for sklearn
+from Dataset import *
+from Classifier import *
+from sklAdapter import *
+
+from sklearn import tree
+
+class sklDTC(sklAdapter):
+    def __init__(self, dataset, configuration):
+        Classifier.__init__(self,dataset)
+        self.clf = tree.DecisionTreeClassifier()
