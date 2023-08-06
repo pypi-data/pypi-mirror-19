114e874d89b3be45789db3a6e042fbc733d62234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:53 +0000

    Add package devcore/0.0.1/devcore-0.0.1.tar.gz

---
+++ packages/devcore/devcore-0.0.1.tar.gz/devcore-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='devcore',
+      version='0.0.1',
+      description='Meta package for a development environment',
+      install_requires=[
+          'log-colorizer-hook',
+          'flask-wdb-hook',
+          'wdb',
+          'isort',
+          'flake8',
+          'importmagic',
+          'jedi',
+          'cutter'
+      ])
