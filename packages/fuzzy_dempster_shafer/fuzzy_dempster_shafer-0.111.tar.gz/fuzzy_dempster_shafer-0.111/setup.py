caf8ab2316908a66fe951310b5f40e9472f0acad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:12 +0000

    Add package fuzzy_dempster_shafer/0.111/fuzzy_dempster_shafer-0.111.tar.gz

---
+++ packages/fuzzy_dempster_shafer/fuzzy_dempster_shafer-0.111.tar.gz/fuzzy_dempster_shafer-0.111/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name = 'fuzzy_dempster_shafer',
+    version = '0.111',
+    description = 'Prediction algorithm based in Dempster-Shafer Fuzzy Logic and Bayesian principles',
+    long_description = 'This is an ongoing project to creating a machine learning classification package. \
+    The highlight of this package will be the utilization of Dempster-Shafer theory to improve on classifications \
+    from other methods such as logistic regression.',
+    classifiers = ['Programming Language :: Python :: 3.5'],
+    url = 'https://github.com/phystistics/fuzzy_dempster_shafer',
+    test_suite = 'nose.collector',
+    tests_require = ['nose'],
+    author = 'Austin Powell',
+    author_email = 'powellaus10@gmail.com',
+    license = 'MIT',
+    install_requires = [
+        'pandas',
+        'numpy',
+        'sklearn'
+    ],
+    zip_safe = False)
