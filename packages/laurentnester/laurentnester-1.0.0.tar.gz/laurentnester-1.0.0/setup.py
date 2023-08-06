342649d1f21fc124d66afdc7a7dd98de81c28674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:19 +0000

    Add package laurentnester/1.0.0/laurentnester-1.0.0.tar.gz

---
+++ packages/laurentnester/laurentnester-1.0.0.tar.gz/laurentnester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name	= 'laurentnester',
+	version = '1.0.0',
+	py_modules=['laurentnester'],
+	author = 'laurent',
+	author_email ='laurent_pinson@hotmail.com',
+	url			='http://www.mmabeijing.com',
+	description= 'prints nested lists'
+	)
