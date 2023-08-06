1d47e739253a958b1b1134a5cff62b74580ded83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:08 +0000

    Add package retinex_for_mri/0.2.5/retinex_for_mri-0.2.5.tar.gz

---
+++ packages/retinex_for_mri/retinex_for_mri-0.2.5.tar.gz/retinex_for_mri-0.2.5/setup.py
@@ -0,0 +1,23 @@
+"""retinex_for_mri setup.
+
+To install, using the commandline do:
+    pip install -e /path/to/retinex_for_mri
+
+"""
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name='retinex_for_mri',
+      version='0.2.5',
+      description='Retinex algorithm for MRI data.',
+      url='https://github.com/ofgulban/retinex_for_mri',
+      download_url='https://github.com/ofgulban/retinex_for_mri/releases/tag/0.2.5',
+      author='Omer Faruk Gulban',
+      author_email='',
+      license='GNU Geneal Public License Version 3',
+      packages=['retinex_for_mri'],
+      install_requires=['numpy', 'scipy'],
+      zip_safe=False)
