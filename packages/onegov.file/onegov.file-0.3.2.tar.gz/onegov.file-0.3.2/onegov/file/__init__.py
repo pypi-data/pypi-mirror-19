f7af78fb33a4d92808a38da5ca85ec05e7eb0a93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:38 +0000

    Add package onegov.file/0.3.2/onegov.file-0.3.2.tar.gz

---
+++ packages/onegov.file/onegov.file-0.3.2.tar.gz/onegov.file-0.3.2/onegov/file/__init__.py
@@ -0,0 +1,11 @@
+from onegov.file.collection import FileCollection, FileSetCollection
+from onegov.file.integration import DepotApp
+from onegov.file.models import File, FileSet
+
+__all__ = [
+    'DepotApp',
+    'File',
+    'FileCollection',
+    'FileSet',
+    'FileSetCollection'
+]
