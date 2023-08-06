d35612f0623f5e9ae23e7b44c2dcf0edd0924689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:53 +0000

    Add package hyfnester/1.2.0/hyfnester-1.2.0.zip

---
+++ packages/hyfnester/hyfnester-1.2.0.zip/hyfnester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         =   'hyfnester',
+        version      =   '1.2.0',
+        py_modules   =   ['nester'],
+        author       =   'hyfpython',
+        author_email =   'hyfpython@headfirstlabs.com',
+        url          =   'http://www.headfirstlabs.com',
+        description  =   'A simple printer of nested lists',
+    )
