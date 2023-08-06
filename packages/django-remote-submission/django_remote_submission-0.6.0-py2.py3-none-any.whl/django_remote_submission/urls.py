b9a1c54e39c1f5914eda4a2743ee318cc29add15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:11 +0000

    Add package django-remote-submission/0.6.0/django_remote_submission-0.6.0-py2.py3-none-any.whl

---
+++ packages/django-remote-submission/django_remote_submission-0.6.0-py2.py3-none-any.whl/django_remote_submission/urls.py
@@ -0,0 +1,14 @@
+"""Provide default route mappings for serializers."""
+# -*- coding: utf-8 -*-
+from rest_framework.routers import DefaultRouter
+
+from .views import ServerViewSet, JobViewSet, LogViewSet
+
+
+router = DefaultRouter()
+router.register(r'servers', ServerViewSet)
+router.register(r'jobs', JobViewSet)
+router.register(r'logs', LogViewSet)
+
+urlpatterns = router.urls
+"""The URL patterns for the defined serializers."""
