f9bbccd67a7d42b0b95e1da5e581edeb44cef589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:08 +0000

    Add package steamfront/0.1.0/steamfront-0.1.00.tar.gz

---
+++ packages/steamfront/steamfront-0.1.00.tar.gz/steamfront-0.1.0/steamfront/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+from .client import Client
+
+__title__ = 'Steamfront'
+__author__ = 'Callum Bartlett'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Callum Bartlett'
+__version__ = '0.1.0'
+
+if __name__ == '__main__':
+	from sys import argv
+
+	if len(argv) < 2:
+		print('Please give the name of a game you want to find the information of.')
+	gameName = ' '.join(argv[1:])
+	c = Client()
+	g = c.getApp(name=gameName)
+	i = '{0.name} :: {0.appid} :: {0.type}'
+	print(i.format(g))
