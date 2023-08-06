4dd7422b55081cdfade8380924adedfd985ef6a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:07 +0000

    Add package googleimagesearch/1.0.1/googleimagesearch-1.0.1-py2.py3-none-any.whl

---
+++ packages/googleimagesearch/googleimagesearch-1.0.1-py2.py3-none-any.whl/googleimagesearch/__init__.py
@@ -0,0 +1,24 @@
+import requests
+import re
+
+url = 'https://images.google.com/searchbyimage'
+session = requests.session()
+session.headers = {'User-Agent': 'Mozilla/5.0 Firefox/50.0'}
+
+
+def patch(response):
+    matches = re.findall('sbiq":"([^"]+)', response.text)
+    response.name = matches[0] if matches else None
+    return response
+
+
+def search_url(url):
+    return patch(session.get(url, params={'image_url': url}))
+
+
+def search_bytes(bytes):
+    return patch(session.post(url + '/upload', files={'encoded_image': bytes}))
+
+
+def search_file(file):
+    return search_bytes(open(file, 'rb').read())
