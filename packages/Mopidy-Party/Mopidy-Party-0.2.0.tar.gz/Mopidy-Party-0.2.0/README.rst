40712874975d16f07052cd5bacb219b156ed196a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:09 +0000

    Add package Mopidy-Party/0.2.0/Mopidy-Party-0.2.0.tar.gz

---
+++ packages/Mopidy-Party/Mopidy-Party-0.2.0.tar.gz/Mopidy-Party-0.2.0/README.rst
@@ -0,0 +1,54 @@
+****************************
+Mopidy-Party
+****************************
+
+Mopidy web extension designed for party! Let your guests manage the sound ;)
+
+- Minimal, fast web interface designed for mobile
+- Search soundtracks and add it to the queue
+- Skip current track (only if at least 1 min played from current track)
+
+See this `blog entry <http://blog.lesterpig.com/2015/09/01/Build-a-connected-jukebox-from-a-Raspberry-Pi/>`_ for some details about this package.
+
+Installation
+============
+
+You must install `mopidy <https://www.mopidy.com/>`_ and some backends (soundcloud, spofity, youtube...).
+
+**PROD:** you just have to install pip and then::
+
+    sudo pip install Mopidy-Party
+
+**DEV:** After cloning the repository, install by running::
+
+    sudo pip install -e .
+
+Usage
+=====
+
+To use the interface, simply point your browser to your Mopidy instance's IP address at port 6680 to see all available web interfaces.
+For example, http://192.168.0.2:6680/
+
+Direct access to Mopidy Party would then be: http://192.168.0.2:6680/party/
+
+Project resources
+=================
+
+- `Source code <https://github.com/Lesterpig/mopidy-party>`_
+- `Issue tracker <https://github.com/Lesterpig/mopidy-party/issues>`_
+- `Development branch tarball <https://github.com/Lesterpig/mopidy-party/archive/master.tar.gz#egg=Mopidy-Party-dev>`_
+
+Changelog
+=========
+
+v0.2.0 (2017-01-08)
+----------------------------------------
+- Added vote to skip (by RealityFork)
+
+v0.1.2 (2016-10-10)
+----------------------------------------
+- Add artists and album names in songs list
+
+v0.1.0 (2015-09-01)
+----------------------------------------
+- Initial release.
---
+++ packages/Mopidy-Party/Mopidy-Party-0.2.0.tar.gz/Mopidy-Party-0.2.0/mopidy_party/static/controller.js
@@ -0,0 +1,165 @@
+'use strict';
+
+// TODO : add a mopidy service designed for angular, to avoid ugly $scope.$apply()...
+angular.module('partyApp', [])
+  .controller('MainController', function($scope) {
+
+  // Scope variables
+
+  $scope.message = [];
+  $scope.tracks  = [];
+  $scope.loading = true;
+  $scope.ready   = false;
+  $scope.currentState = {
+    paused : false,
+    length : 0,
+    track  : {
+      length : 0,
+      name   : 'Nothing playing, add some songs to get the party going!'
+    }
+  };
+
+  // Initialize
+
+  var mopidy = new Mopidy({
+    'callingConvention' : 'by-position-or-by-name'
+  });
+
+  // Adding listenners
+
+  mopidy.on('state:online', function () {
+    mopidy.playback
+    .getCurrentTrack()
+    .then(function(track){
+      if(track)
+        $scope.currentState.track = track;
+      return mopidy.playback.getState();
+    })
+    .then(function(state){
+      $scope.currentState.paused = (state === 'paused');
+      return mopidy.tracklist.getLength();
+    })
+    .then(function(length){
+      $scope.currentState.length = length;
+    })
+    .done(function(){
+      $scope.ready   = true;
+      $scope.loading = false;
+      $scope.$apply();
+    });
+  });
+  mopidy.on('event:playbackStateChanged', function(event){
+    $scope.currentState.paused = (event.new_state === 'paused');
+    $scope.$apply();
+  });
+  mopidy.on('event:trackPlaybackStarted', function(event){
+    $scope.currentState.track = event.tl_track.track;
+    $scope.$apply();
+  });
+  mopidy.on('event:tracklistChanged', function(){
+    mopidy.tracklist.getLength().done(function(length){
+      $scope.currentState.length = length;
+      $scope.$apply();
+    });
+  });
+
+  $scope.printDuration = function(track){
+
+    if(!track.length)
+      return '';
+
+    var _sum = parseInt(track.length / 1000);
+    var _min = parseInt(_sum / 60);
+    var _sec = _sum % 60;
+
+    return '(' + _min + ':' + (_sec < 10 ? '0' + _sec : _sec) + ')' ;
+  };
+
+  $scope.togglePause = function(){
+    var _fn = $scope.currentState.paused ? mopidy.playback.resume : mopidy.playback.pause;
+    _fn().done();
+  };
+
+  $scope.search = function(){
+
+    if(!$scope.searchField)
+      return;
+
+    $scope.message = [];
+    $scope.loading = true;
+
+    mopidy.library.search({
+      'any' : [$scope.searchField]
+    }).done(function(res){
+
+      $scope.loading = false;
+      $scope.tracks  = [];
+
+      var _index = 0;
+      var _found = true;
+      while(_found){
+        _found = false;
+        for(var i = 0; i < res.length; i++){
+          if(res[i].tracks && res[i].tracks[_index]){
+            $scope.tracks.push(res[i].tracks[_index]);
+            _found = true;
+            mopidy.tracklist.filter({'uri': [res[i].tracks[_index].uri]}).done(function(matches){
+    if (matches.length) {
+      for (var i = 0; i < $scope.tracks.length; i++)
+      {
+        if ($scope.tracks[i].uri == matches[0].track.uri)
+          $scope.tracks[i].disabled = true;
+      }
+      $scope.$apply();
+    }
+      });
+          }
+        }
+        _index++;
+      }
+
+      $scope.$apply();
+    });
+  };
+
+  $scope.addTrack = function(track){
+
+    track.disabled = true;
+
+    mopidy.tracklist
+    .index()
+    .then(function(index){
+      return mopidy.tracklist.add({uris: [track.uri]});
+    })
+    .then(function(){
+      // Notify user
+      $scope.message = ['success', 'Next track: ' + track.name];
+      $scope.$apply();
+      return mopidy.tracklist.setConsume([true]);
+    })
+    .then(function(){
+      return mopidy.playback.getState();
+    })
+    .then(function(state){
+      // Get current state
+      if(state !== 'stopped')
+        return;
+      // If stopped, start music NOW!
+      return mopidy.playback.play();
+    })
+    .catch(function(){
+      track.disabled = false;
+      $scope.message = ['error', 'Unable to add track, please try again...'];
+      $scope.$apply();
+    })
+    .done();
+  };
+
+  $scope.nextTrack = function(){
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "/party/vote", false ); // false for synchronous request
+    xmlHttp.send( null );
+    $scope.message = ['success', xmlHttp.responseText];
+    $scope.$apply();
+  };
+});
