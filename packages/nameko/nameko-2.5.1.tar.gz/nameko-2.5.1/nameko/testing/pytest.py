eb9db516ec133d51cd95fbd9606696b5fa39e804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:19 +0000

    Add package nameko/2.5.1/nameko-2.5.1.tar.gz

---
+++ packages/nameko/nameko-2.5.1.tar.gz/nameko-2.5.1/nameko/testing/pytest.py
@@ -0,0 +1,298 @@
+from __future__ import absolute_import
+
+import pytest
+
+
+# all imports are inline to make sure they happen after eventlet.monkey_patch
+# which is called in pytest_load_initial_conftests
+# (calling monkey_patch at import time breaks the pytest capturemanager - see
+#  https://github.com/eventlet/eventlet/pull/239)
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--blocking-detection',
+        action='store_true',
+        dest='blocking_detection',
+        default=False,
+        help='turn on eventlet hub blocking detection')
+
+    parser.addoption(
+        "--log-level", action="store",
+        default='DEBUG',
+        help=("The logging-level for the test run."))
+
+    parser.addoption(
+        "--amqp-uri", action="store", dest='AMQP_URI',
+        default='amqp://guest:guest@localhost:5672/:random:',
+        help=("The AMQP-URI to connect to rabbit with."))
+
+    parser.addoption(
+        "--rabbit-ctl-uri", action="store", dest='RABBIT_CTL_URI',
+        default='http://guest:guest@localhost:15672',
+        help=("The URI for rabbit's management API."))
+
+
+def pytest_load_initial_conftests():
+    # make sure we monkey_patch before local conftests
+    import eventlet
+    eventlet.monkey_patch()
+
+
+def pytest_configure(config):
+    import logging
+    import sys
+
+    if config.option.blocking_detection:  # pragma: no cover
+        from eventlet import debug
+        debug.hub_blocking_detection(True)
+
+    log_level = config.getoption('log_level')
+    if log_level is not None:
+        log_level = getattr(logging, log_level)
+        logging.basicConfig(level=log_level, stream=sys.stderr)
+
+
+@pytest.fixture(autouse=True)
+def always_warn_for_deprecation():
+    import warnings
+    warnings.simplefilter('always', DeprecationWarning)
+
+
+@pytest.fixture
+def empty_config():
+    from nameko.constants import AMQP_URI_CONFIG_KEY
+    return {
+        AMQP_URI_CONFIG_KEY: ""
+    }
+
+
+@pytest.fixture
+def mock_container(request, empty_config):
+    from mock import create_autospec
+    from nameko.constants import SERIALIZER_CONFIG_KEY, DEFAULT_SERIALIZER
+    from nameko.containers import ServiceContainer
+
+    container = create_autospec(ServiceContainer)
+    container.config = empty_config
+    container.config[SERIALIZER_CONFIG_KEY] = DEFAULT_SERIALIZER
+    container.serializer = container.config[SERIALIZER_CONFIG_KEY]
+    container.accept = [DEFAULT_SERIALIZER]
+    return container
+
+
+@pytest.fixture(scope='session')
+def rabbit_manager(request):
+    from nameko.testing import rabbit
+
+    config = request.config
+    return rabbit.Client(config.getoption('RABBIT_CTL_URI'))
+
+
+@pytest.yield_fixture()
+def rabbit_config(request, rabbit_manager):
+    import itertools
+    import random
+    import string
+    import time
+    from kombu import pools
+    from six.moves.urllib.parse import urlparse  # pylint: disable=E0401
+    from nameko.testing.utils import get_rabbit_connections
+
+    amqp_uri = request.config.getoption('AMQP_URI')
+
+    uri = urlparse(amqp_uri)
+    username = uri.username
+    vhost = uri.path[1:]
+
+    use_random_vost = (vhost == ":random:")
+
+    if use_random_vost:
+        vhost = "test_{}".format(
+            "".join(random.choice(string.ascii_lowercase) for _ in range(10))
+        )
+        amqp_uri = "{}://{}/{}".format(uri.scheme, uri.netloc, vhost)
+        rabbit_manager.create_vhost(vhost)
+        rabbit_manager.set_vhost_permissions(vhost, username, '.*', '.*', '.*')
+
+    conf = {
+        'AMQP_URI': amqp_uri,
+        'username': username,
+        'vhost': vhost
+    }
+
+    yield conf
+
+    pools.reset()  # close connections in pools
+
+    def retry(fn):
+        """ Barebones retry decorator
+        """
+        def wrapper():
+            max_retries = 3
+            delay = 1
+            exceptions = RuntimeError
+
+            counter = itertools.count()
+            while True:
+                try:
+                    return fn()
+                except exceptions:
+                    if next(counter) == max_retries:
+                        raise
+                    time.sleep(delay)
+        return wrapper
+
+    @retry
+    def check_connections():
+        """ Raise a runtime error if the test leaves any connections open.
+
+        Allow a few retries because the rabbit api is eventually consistent.
+        """
+        connections = get_rabbit_connections(conf['vhost'], rabbit_manager)
+        open_connections = [
+            conn for conn in connections if conn['state'] != "closed"
+        ]
+        if open_connections:
+            count = len(open_connections)
+            names = ", ".join(conn['name'] for conn in open_connections)
+            raise RuntimeError(
+                "{} rabbit connection(s) left open: {}".format(count, names))
+    try:
+        check_connections()
+    finally:
+        if use_random_vost:
+            rabbit_manager.delete_vhost(vhost)
+
+
+@pytest.fixture
+def ensure_cleanup_order(request):
+    """ Ensure ``rabbit_config`` is invoked early if it's used by any fixture
+    in ``request``.
+    """
+    if "rabbit_config" in request.funcargnames:
+        request.getfuncargvalue("rabbit_config")
+
+
+@pytest.yield_fixture
+def container_factory(ensure_cleanup_order):
+    from nameko.containers import get_container_cls
+    import warnings
+
+    all_containers = []
+
+    def make_container(service_cls, config, worker_ctx_cls=None):
+
+        container_cls = get_container_cls(config)
+
+        if worker_ctx_cls is not None:
+            warnings.warn(
+                "The constructor of `container_factory` has changed. "
+                "The `worker_ctx_cls` kwarg is now deprecated. See CHANGES, "
+                "Version 2.4.0 for more details.", DeprecationWarning
+            )
+
+        container = container_cls(service_cls, config, worker_ctx_cls)
+        all_containers.append(container)
+        return container
+
+    yield make_container
+
+    for c in all_containers:
+        try:
+            c.stop()
+        except:  # pragma: no cover
+            pass
+
+
+@pytest.yield_fixture
+def runner_factory(ensure_cleanup_order):
+    from nameko.runners import ServiceRunner
+
+    all_runners = []
+
+    def make_runner(config, *service_classes):
+        runner = ServiceRunner(config)
+        for service_cls in service_classes:
+            runner.add_service(service_cls)
+        all_runners.append(runner)
+        return runner
+
+    yield make_runner
+
+    for r in all_runners:
+        try:
+            r.stop()
+        except:  # pragma: no cover
+            pass
+
+
+@pytest.yield_fixture
+def predictable_call_ids(request):
+    import itertools
+    from mock import patch
+
+    with patch('nameko.containers.new_call_id', autospec=True) as get_id:
+        get_id.side_effect = (str(i) for i in itertools.count())
+        yield get_id
+
+
+@pytest.fixture()
+def web_config(empty_config):
+    from nameko.constants import WEB_SERVER_CONFIG_KEY
+    from nameko.testing.utils import find_free_port
+
+    port = find_free_port()
+
+    cfg = empty_config
+    cfg[WEB_SERVER_CONFIG_KEY] = str(port)
+    return cfg
+
+
+@pytest.fixture()
+def web_config_port(web_config):
+    from nameko.constants import WEB_SERVER_CONFIG_KEY
+    from nameko.web.server import parse_address
+    return parse_address(web_config[WEB_SERVER_CONFIG_KEY]).port
+
+
+@pytest.yield_fixture()
+def web_session(web_config_port):
+    from requests import Session
+    from werkzeug.urls import url_join
+
+    class WebSession(Session):
+        def request(self, method, url, *args, **kwargs):
+            url = url_join('http://127.0.0.1:%d/' % web_config_port, url)
+            return Session.request(self, method, url, *args, **kwargs)
+
+    sess = WebSession()
+    with sess:
+        yield sess
+
+
+@pytest.yield_fixture()
+def websocket(web_config_port):
+    import eventlet
+    from nameko.testing.websocket import make_virtual_socket
+
+    active_sockets = []
+
+    def socket_creator():
+        ws_app, wait_for_sock = make_virtual_socket(
+            '127.0.0.1', web_config_port)
+        gr = eventlet.spawn(ws_app.run_forever)
+        active_sockets.append((gr, ws_app))
+        socket = wait_for_sock()
+        socket.app = ws_app
+        return socket
+
+    try:
+        yield socket_creator
+    finally:
+        for gr, ws_app in active_sockets:
+            try:
+                ws_app.close()
+            except Exception:  # pragma: no cover
+                pass
+            gr.kill()
---
+++ packages/nameko/nameko-2.5.1.tar.gz/nameko-2.5.1/test/testing/test_utils.py
@@ -0,0 +1,250 @@
+import warnings
+
+import eventlet
+import pytest
+from eventlet.event import Event
+from mock import Mock, call, patch
+from requests import HTTPError, Response
+
+from nameko.constants import DEFAULT_MAX_WORKERS
+from nameko.rpc import Rpc, rpc
+from nameko.testing.rabbit import Client
+from nameko.testing.utils import (
+    AnyInstanceOf, find_free_port, get_container, get_extension,
+    get_rabbit_connections, reset_rabbit_connections, wait_for_call,
+    wait_for_worker_idle)
+
+
+def test_any_instance_of():
+
+    assert "" == AnyInstanceOf(str)
+    assert 99 != AnyInstanceOf(str)
+
+    class Foo(object):
+        pass
+    foo = Foo()
+
+    assert foo == AnyInstanceOf(Foo)
+    assert foo == AnyInstanceOf(object)
+    assert foo != AnyInstanceOf(foo)
+
+    assert repr(AnyInstanceOf(str)) == "<AnyInstanceOf-str>"
+    assert repr(AnyInstanceOf(Foo)) == "<AnyInstanceOf-Foo>"
+
+    assert AnyInstanceOf == AnyInstanceOf(type)
+    assert str == AnyInstanceOf(type)
+    assert int == AnyInstanceOf(type)
+    assert type == AnyInstanceOf(type)
+
+
+def test_wait_for_call():
+    mock = Mock()
+
+    def call_after(seconds):
+        eventlet.sleep(seconds)
+        mock.method()
+
+    # should not raise
+    eventlet.spawn(call_after, 0)
+    with wait_for_call(1, mock.method):
+        pass
+
+    mock.reset_mock()
+
+    with pytest.raises(eventlet.Timeout):
+        eventlet.spawn(call_after, 1)
+        with wait_for_call(0, mock.method):
+            pass  # pragma: no cover
+
+
+def test_get_extension(rabbit_config):
+
+    from nameko.messaging import QueueConsumer
+    from nameko.rpc import Rpc, RpcConsumer
+    from nameko.containers import ServiceContainer
+
+    class Service(object):
+        name = "service"
+
+        @rpc
+        def foo(self):
+            pass  # pragma: no cover
+
+        @rpc
+        def bar(self):
+            pass  # pragma: no cover
+
+    container = ServiceContainer(Service, rabbit_config)
+
+    rpc_consumer = get_extension(container, RpcConsumer)
+    queue_consumer = get_extension(container, QueueConsumer)
+    foo_rpc = get_extension(container, Rpc, method_name="foo")
+    bar_rpc = get_extension(container, Rpc, method_name="bar")
+
+    extensions = container.extensions
+    assert extensions == set([rpc_consumer, queue_consumer, foo_rpc, bar_rpc])
+
+
+def test_get_container(runner_factory, rabbit_config):
+
+    class ServiceX(object):
+        name = "service_x"
+
+    class ServiceY(object):
+        name = "service_y"
+
+    runner = runner_factory(rabbit_config, ServiceX, ServiceY)
+
+    assert get_container(runner, ServiceX).service_cls is ServiceX
+    assert get_container(runner, ServiceY).service_cls is ServiceY
+    assert get_container(runner, object) is None
+
+
+def test_get_rabbit_connections():
+
+    vhost = "vhost_name"
+
+    connections = [{
+        'vhost': vhost,
+        'key': 'value'
+    }, {
+        'vhost': 'unlikely_vhost_name',
+        'key': 'value'
+    }]
+
+    rabbit_manager = Mock()
+
+    rabbit_manager.get_connections.return_value = connections
+    vhost_conns = [connections[0]]
+    assert get_rabbit_connections(vhost, rabbit_manager) == vhost_conns
+
+    rabbit_manager.get_connections.return_value = None
+    assert get_rabbit_connections(vhost, rabbit_manager) == []
+
+
+def test_reset_rabbit_connections():
+
+    with patch('nameko.testing.utils.get_rabbit_connections') as connections:
+        connections.return_value = [{
+            'vhost': 'vhost',
+            'name': 'connection_name'
+        }]
+
+        rabbit_manager = Mock()
+        reset_rabbit_connections('vhost', rabbit_manager)
+
+        assert rabbit_manager.delete_connection.call_args_list == [
+            call("connection_name")]
+
+
+def test_reset_rabbit_connection_errors():
+
+    rabbit_manager = Mock()
+
+    with patch('nameko.testing.utils.get_rabbit_connections') as connections:
+        connections.return_value = [{
+            'vhost': 'vhost_name',
+            'name': 'connection_name'
+        }]
+
+        # 500 error
+        response_500 = Response()
+        response_500.status_code = 500
+        error_500 = HTTPError(response=response_500)
+        rabbit_manager.delete_connection.side_effect = error_500
+
+        with pytest.raises(HTTPError):
+            reset_rabbit_connections("vhost_name", rabbit_manager)
+
+        # 404 error
+        response_404 = Response()
+        response_404.status_code = 404
+        error_404 = HTTPError(response=response_404)
+        rabbit_manager.delete_connection.side_effect = error_404
+
+        # does not raise
+        reset_rabbit_connections("vhost_name", rabbit_manager)
+
+
+def test_wait_for_worker_idle(container_factory, rabbit_config):
+
+    event = Event()
+
+    class Service(object):
+        name = "service"
+
+        @rpc
+        def wait_for_event(self):
+            event.wait()
+
+    container = container_factory(Service, rabbit_config)
+    container.start()
+
+    max_workers = DEFAULT_MAX_WORKERS
+
+    # TODO: pytest.warns is not supported until pytest >= 2.8.0, whose
+    # `testdir` plugin is not compatible with eventlet on python3 --
+    # see https://github.com/mattbennett/eventlet-pytest-bug
+    with warnings.catch_warnings(record=True) as ws:
+        wait_for_worker_idle(container)
+        assert len(ws) == 1
+        assert issubclass(ws[-1].category, DeprecationWarning)
+
+    # verify nothing running
+    assert container._worker_pool.free() == max_workers
+    with eventlet.Timeout(1):
+        wait_for_worker_idle(container)
+
+    # spawn a worker
+    wait_for_event = get_extension(container, Rpc)
+    container.spawn_worker(wait_for_event, [], {})
+
+    # verify that wait_for_worker_idle does not return while worker active
+    assert container._worker_pool.free() == max_workers - 1
+    gt = eventlet.spawn(wait_for_worker_idle, container)
+    assert not gt.dead  # still waiting
+
+    # verify that wait_for_worker_idle raises when it times out
+    with pytest.raises(eventlet.Timeout):
+        wait_for_worker_idle(container, timeout=0)
+
+    # complete the worker, verify previous wait_for_worker_idle completes
+    event.send()
+    with eventlet.Timeout(1):
+        gt.wait()
+    assert container._worker_pool.free() == max_workers
+
+
+def test_rabbit_connection_refused_error():
+
+    # port 4 is an official unassigned port, so no one should be using it
+    bad_port_uri = 'http://localhost:4'
+    with pytest.raises(Exception) as exc_info:
+        Client(bad_port_uri)
+
+    message = str(exc_info.value)
+    assert 'Connection error' in message
+
+
+@patch('nameko.testing.utils.socket')
+class TestFindFreePort(object):
+
+    def test_default_host(self, patched_socket):
+        host = '127.0.0.1'
+        free_port = 9999
+        mock_sock = patched_socket.socket()
+        mock_sock.getsockname.return_value = [host, free_port]
+
+        assert find_free_port() == free_port
+        assert mock_sock.bind.call_args_list == [call((host, 0))]
+        assert mock_sock.close.called
+
+    def test_specified_host(self, patched_socket):
+        host = '10.0.0.1'
+        free_port = 9999
+        mock_sock = patched_socket.socket()
+        mock_sock.getsockname.return_value = [host, free_port]
+
+        assert find_free_port(host) == free_port
+        assert mock_sock.bind.call_args_list == [call((host, 0))]
+        assert mock_sock.close.called
