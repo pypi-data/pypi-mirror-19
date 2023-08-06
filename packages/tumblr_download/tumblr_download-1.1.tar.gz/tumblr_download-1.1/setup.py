3870262ada0153ea66b85e926e66b1d178d7ffb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:56 +0000

    Add package tumblr_download/1.1/tumblr_download-1.1.tar.gz

---
+++ packages/tumblr_download/tumblr_download-1.1.tar.gz/tumblr_download-1.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+longqi 2/Feb/17 10:29
+Description:
+
+
+"""
+
+from setuptools import setup
+
+setup(
+		name='tumblr_download',
+		packages=['tumblr_download'],
+		version='1.1',
+		install_requires=['python-tumblpy>=1.0.5'],
+		description='Download posts from tumblr blog',
+		author='Zhang LongQi',
+		author_email='longqi90@gmail.com',
+		url='https://github.com/zhanglongqi/tumblr_download',
+		download_url='https://github.com/zhanglongqi/tumblr_download/tarball/1.1',
+		keywords=['tumblr', 'download', 'backup', 'copy'],
+		classifiers=[],
+)
