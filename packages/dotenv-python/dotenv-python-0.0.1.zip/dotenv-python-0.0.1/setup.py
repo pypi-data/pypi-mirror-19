a373e5098b99b66fae0725c066323b5fc33121ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:12 +0000

    Add package dotenv-python/0.0.1/dotenv-python-0.0.1.zip

---
+++ packages/dotenv-python/dotenv-python-0.0.1.zip/dotenv-python-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name='dotenv-python',
+    version='0.0.1',
+    author='Cui Jie',
+    author_email='jndion2014@gmail.com',
+    url='https://github.com/TsuiJie/dotenv-python.git',
+    packages=['dotenv'],
+    license='MIT',
+    description='Read .env(-ish) configuration file for python web applications.',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ]
+)
