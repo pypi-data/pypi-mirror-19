545265aee3c58ba7bd31d91da9824faa800f3293
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:33 +0000

    Add package apfelwufirst/1.0.0/apfelwufirst-1.0.0.zip

---
+++ packages/apfelwufirst/apfelwufirst-1.0.0.zip/apfelwufirst-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name='apfelwufirst',
+	version='1.0.0',
+	py_modules=['apfelwufirst'],
+	author='apfelwu',
+	author_email='hfpython@headfirstlabs.com',
+	url='http://www.headfirstlabs.com',
+	description='A simple printer of nested lists',
+	)
