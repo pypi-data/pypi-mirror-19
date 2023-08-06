681d2f8bd0d9934d8043ba8afdb4679e19da5988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:15 +0000

    Add package matchmock/0.7.1/matchmock-0.7.1.tar.gz

---
+++ packages/matchmock/matchmock-0.7.1.tar.gz/matchmock-0.7.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='matchmock',
+    version='0.7.1',
+    description='Hamcrest matchers for mock objects',
+    url='https://github.com/keis/matchmock',
+    py_modules=['matchmock']
+)
