7acc2b10809953055e5bd92e1efc0e1f3f36069e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:22 +0000

    Add package pipenv/3.3.3/pipenv-3.3.3.tar.gz

---
+++ packages/pipenv/pipenv-3.3.3.tar.gz/pipenv-3.3.3/pipenv/environments.py
@@ -0,0 +1,24 @@
+import os
+
+
+# Prevent invalid shebangs with Homebrew-installed Python:
+# https://bugs.python.org/issue22490
+os.environ.pop('__PYVENV_LAUNCHER__', None)
+
+
+# Shell compatibility mode, for mis-configured shells.
+PIPENV_SHELL_COMPAT = os.environ.get('PIPENV_SHELL_COMPAT')
+
+# Create the virtualenv in the project, isntead of with pew.
+PIPENV_VENV_IN_PROJECT = os.environ.get('PIPENV_SHELL_COMPAT')
+
+# No color mode, for unfun people.
+PIPENV_COLORBLIND = os.environ.get('PIPENV_COLORBLIND')
+
+# User-configuraable max-depth for Pipfile searching.
+# Note: +1 because of a bug in Pipenv.
+PIPENV_MAX_DEPTH = int(os.environ.get('PIPENV_MAX_DEPTH', '3')) + 1
+
+# Use shell compatibility mode when using venv in project mode.
+if PIPENV_VENV_IN_PROJECT:
+    PIPENV_SHELL_COMPAT = True
