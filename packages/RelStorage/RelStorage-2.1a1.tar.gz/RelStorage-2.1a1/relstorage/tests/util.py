651a51d25cf6b0ef4f54d9b54801cebb3a0ae47d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:43 +0000

    Add package RelStorage/2.1a1/RelStorage-2.1a1.tar.gz

---
+++ packages/RelStorage/RelStorage-2.1a1.tar.gz/RelStorage-2.1a1/relstorage/tests/util.py
@@ -0,0 +1,51 @@
+import os
+import platform
+import unittest
+
+
+# ZODB >= 3.9.  The blob directory can be a private cache.
+shared_blob_dir_choices = (False, True)
+support_blob_cache = True
+
+RUNNING_ON_TRAVIS = os.environ.get('TRAVIS')
+RUNNING_ON_APPVEYOR = os.environ.get('APPVEYOR')
+RUNNING_ON_CI = RUNNING_ON_TRAVIS or RUNNING_ON_APPVEYOR
+
+def _do_not_skip(reason): # pylint:disable=unused-argument
+    def dec(f):
+        return f
+    return dec
+
+if RUNNING_ON_CI:
+    skipOnCI = unittest.skip
+else:
+    skipOnCI = _do_not_skip
+
+if RUNNING_ON_APPVEYOR:
+    skipOnAppveyor = unittest.skip
+else:
+    skipOnAppveyor = _do_not_skip
+
+CACHE_SERVERS = None
+CACHE_MODULE_NAME = None
+
+if RUNNING_ON_TRAVIS:
+    # We expect to have access to a local memcache server
+    # on travis. Use it if we can import drivers.
+    # pylint:disable=unused-import
+    try:
+        import pylibmc
+        CACHE_SERVERS = ["localhost:11211"]
+        CACHE_MODULE_NAME = 'relstorage.pylibmc_wrapper'
+    except ImportError:
+        try:
+            import memcache
+            CACHE_SERVERS = ["localhost:11211"]
+            CACHE_MODULE_NAME = 'memcache'
+        except ImportError:
+            pass
+
+USE_SMALL_BLOBS = ((RUNNING_ON_CI # slow here
+                    or platform.system() == 'Darwin' # interactive testing
+                    or os.environ.get("RS_SMALL_BLOB")) # define
+                   and not os.environ.get('RS_LARGE_BLOB'))
