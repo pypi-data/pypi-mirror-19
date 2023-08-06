633a7cac3f3971e1f4c6705cc614661beab2acbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:21 +0000

    Add package sql2gee/0.0.2/sql2gee-0.0.2.tar.gz

---
+++ packages/sql2gee/sql2gee-0.0.2.tar.gz/sql2gee-0.0.2/sql2gee/sql2gee.py
@@ -0,0 +1,277 @@
+import sqlparse
+import ee
+from sqlparse.tokens import Keyword, Comparison
+from sqlparse.sql import Identifier, IdentifierList, Function, Parenthesis, Comparison
+
+
+class SQL2GEE:
+    """
+    Converts SQL into calls to Google's Earth Engine via their python (2.7) API
+    """
+    def __init__(self, sql):
+        """Intialize the object and parse sql. Return SQL2GEE object to do the process"""
+        self.parsed = sqlparse.parse(sql)[0]
+        self.filters = {
+            '<': ee.Filter().lt,
+            '<=': ee.Filter().lte,
+            '>': ee.Filter().gt,
+            '>=': ee.Filter().gte,
+            '<>': ee.Filter().neq,
+            '=': ee.Filter().eq,
+            '%LIKE%': ee.Filter().stringContains,
+            '%LIKE': ee.Filter().stringEndsWith,
+            'LIKE%': ee.Filter().stringStartsWith,
+            'LIKE': ee.Filter().eq,
+        }
+        self.comparisons = {
+            'AND': ee.Filter().And,
+            'OR': ee.Filter().Or
+        }
+
+    @property
+    def table_name(self):
+        """Set table_name property using sql tokens, assuming it
+        is the first token of type Identifier after the 'FROM' keyword
+        also of type Identifier. If not found, raise an Exception."""
+        from_seen = False
+        exception_1 = Exception('Table name not found')
+        for item in self.parsed.tokens:
+            if from_seen:
+                if isinstance(item, Identifier):
+                    return self.remove_quotes(str(item))
+                elif item.ttype is Keyword:
+                    raise exception_1
+            elif item.ttype is Keyword and str(item).upper() == 'FROM':
+                from_seen = True
+        raise exception_1
+
+    @property
+    def fields(self):
+        """A list of all fields in SQL query. If the FROM keyword is
+        encountered the list is immediately returned.
+        """
+        field_list = []
+        for t in self.parsed.tokens:
+            is_keyword = t.ttype is Keyword
+            is_from = str(t).upper() == 'FROM'
+            if is_keyword and is_from:
+                return field_list
+            elif isinstance(t, Identifier):
+                field_list.append(str(t))
+            elif isinstance(t, IdentifierList):
+                for identity in t.tokens:
+                    if isinstance(identity, Identifier):
+                        field_list.append(str(identity))
+        return field_list
+
+    @property
+    def group_functions(self):
+        """Returns the group function with column names specified in the query:
+        e.g. from sql input of 'select count(pepe) from mytable', a dictionary of
+        {'function': 'COUNT', 'value': 'pepe'} should be returned by self.group_functions"""
+        group_list = []
+        for t in self.parsed.tokens:
+            if t.ttype is Keyword and t.value.upper() == 'FROM':
+                return group_list
+            elif isinstance(t, Function):
+                group_list.append(self.token_to_dictionary(t))
+            elif isinstance(t, IdentifierList):
+                for identity in t.tokens:
+                    if isinstance(identity, Function):
+                        group_list.append(self.token_to_dictionary(identity))
+        return group_list
+
+    @property
+    def feature_collection(self):
+        """Return the G.E.E. F.C. query object with all filter, groups, functions,
+        etc. specified in the query. Was originally get_query() method."""
+        fc = ee.FeatureCollection(self.table_name)
+        if self.where:
+            fc = fc.filter(self.where)
+        if self.group_functions:
+            for group in self.group_functions:
+                fc = self.apply_group(fc, group)
+        select = self.fields
+        if select and len(select) > 0 and not select[0] == '*':
+            fc = fc.select(select)
+        return fc
+
+    @property
+    def where(self):
+        """Returns filter object obtained from where of the query in GEE format"""
+        val, tmp = self.parsed.token_next_by(i=sqlparse.sql.Where)
+        if tmp:
+            return self.parse_conditions(tmp.tokens)
+        return None
+
+    @staticmethod
+    def apply_group(fc, group):
+        """Given a fc (feature_collection) object and group operation, return a
+        new fc object, extended by a method of the group operation."""
+        if group['function'] == 'COUNT':
+            return fc.aggregate_count(group['value'])
+        elif group['function'] == 'MAX':
+            return fc.aggregate_max(group['value'])
+        elif group['function'] == 'MIN':
+            return fc.aggregate_min(group['value'])
+        elif group['function'] == 'SUM':
+            return fc.aggregate_sum(group['value'])
+        elif group['function'] == 'AVG':
+            return fc.aggregate_mean(group['value'])
+        elif group['function'] == 'FIRST':
+            return fc.aggregate_first(group['value'])
+        elif group['function'] == 'LAST':
+            return fc.aggregate_last(group['value'])
+        else:
+            raise ValueError("Unknown group function attempted: ", group['function'])
+
+    @staticmethod
+    def remove_quotes(input_str):
+        """Checks the first and last characters of an input_str
+        to see if they are quotation marks [' or "], if so
+        the function will strip them and return the string.
+        :type input_str: str"""
+        starts_with_quotation = input_str[0] in ['"', "'"]
+        ends_with_quotation = input_str[-1] in ['"', "'"]
+        if starts_with_quotation and ends_with_quotation:
+            return input_str[1: -1]
+        else:
+            return input_str
+
+    @staticmethod
+    def token_to_dictionary(token_list):
+        """Recieve a token e.g.('count(pepe)') and converts it into a dictionary
+        with key:values for function and value."""
+        assert isinstance(token_list, sqlparse.sql.Function),'unexpected datatype'
+        d = {}
+        for t in token_list:
+            if isinstance(t, Identifier):
+                d['function'] = str(t).upper()
+            elif isinstance(t, Parenthesis):
+                value = t.value.replace('(', '').replace(')', '').strip()
+                if value != '*':
+                    d['value'] = value
+                else:
+                    raise Exception('* not allowed')
+        return d
+
+    def parse_comparison(self, comparison):
+        values = []
+        comparator = None
+        for item in comparison.tokens:
+            if isinstance(item, Identifier):
+                values.append(self.remove_quotes(item.value))
+            elif item.ttype is sqlparse.tokens.Comparison:
+                comparator = item.value
+            elif not item.is_whitespace:
+                if item.ttype is sqlparse.tokens.Number.Integer:
+                    values.append(int(item.value))
+                elif item.ttype is sqlparse.tokens.Number.Float:
+                    values.append(float(item.value))
+                else:
+                    values.append(self.remove_quotes(item.value))
+        if comparator:
+            return self.filters[comparator](values[0], values[1])
+
+    def generate_like(self, left, comp, right, exist_not):
+        if comp.value.upper() == 'LIKE':
+            filter = None
+            if right.strip().startswith('%') and right.strip().endswith('%'):
+                filter = self.filters['%' + comp.value.upper() + '%'](left, right[1:len(right) - 1])
+            elif right.strip().startswith('%'):
+                filter = self.filters['%' + comp.value.upper()](left, right[1:len(right)])
+            elif right.strip().endswith('%'):
+                filter = self.filters[comp.value.upper() + '%'](left, right[0:len(right) - 1])
+            else:
+                filter = self.filters[comp.value.upper()](left, right)
+
+            if exist_not:
+                return filter.Not()
+            return filter
+        else:
+            raise Exception(comp.value + ' not supported')
+
+    def generate_in(self, left, comp, right, exist_not):
+        filter = ee.Filter().inList(left, right)
+        if exist_not:
+            return filter.Not()
+        return filter
+
+    def generate_is(self, left, comp, right, ):
+        if right.upper() == 'NULL':
+            return ee.Filter().eq(left, 'null')
+        elif right.upper().startswith('NOT') and right.upper().endswith('NULL'):
+            return ee.Filter().neq(left, 'null')
+        else:
+            raise Exception('IS only support NULL value')
+
+    def parse_list(self, tokens):
+        values = []
+        for item in tokens:
+            if isinstance(item, Identifier):
+                values.append(self.remove_quotes(item.value))
+            elif not item.is_whitespace and item.value != ',':
+                if item.ttype is sqlparse.tokens.Number.Integer:
+                    values.append(int(item.value))
+                elif item.ttype is sqlparse.tokens.Number.Float:
+                    values.append(float(item.value))
+                else:
+                    values.append(self.remove_quotes(item.value))
+        return values
+
+    def parse_conditions(self, tokens):
+        filters = []
+        comparison = None
+        sub_comparison = None
+        leftValue = None
+        exist_not = False
+        for item in tokens:
+            if isinstance(item, Comparison):
+                filter = self.parse_comparison(item)
+                if exist_not:
+                    filter = filter.Not()
+                filters.append(filter)
+            elif item.ttype is Keyword and (item.value.upper() == 'AND' or item.value.upper() == 'OR'):
+                comparison = self.comparisons[item.value.upper()]
+            elif isinstance(item, Parenthesis):
+                filter = self.parse_conditions(item.tokens)
+                if isinstance(filter, ee.Filter):
+                    filters.append(filter)
+                elif type(filter) is list:
+                    filters.append(self.generate_in(leftValue, sub_comparison, filter, exist_not))
+                    leftValue = None
+                    sub_comparison = None
+                    exist_not = False
+            elif item.ttype is Keyword and (item.value.upper() == 'LIKE' or item.value.upper() == 'IN' or item.value.upper() == 'IS'):
+                sub_comparison = item
+            elif item.ttype is Keyword and item.value.upper() == 'NOT':
+                exist_not = True
+            elif isinstance(item, IdentifierList):
+                return self.parse_list(item.tokens)
+            elif item.ttype is None or (item.ttype is Keyword and (item.value.upper() == 'NULL' or item.value.upper().startswith('NOT'))):
+                if leftValue is None:
+                    leftValue = item.value
+                else:
+                    if sub_comparison.value.upper() == 'LIKE':
+                        filters.append(self.generate_like(leftValue, sub_comparison, self.remove_quotes(item.value), exist_not))
+                    elif sub_comparison.value.upper() == 'IS':
+                        filters.append(self.generate_is(leftValue, sub_comparison, self.remove_quotes(item.value)))
+                    sub_comparison = None
+                    leftValue = None
+                    exist_not = False
+            if comparison and len(filters) == 2:
+                statement = comparison(filters[0], filters[1])
+                if exist_not:
+                    statement = statement.Not()
+                    exist_not = False
+                filters = [statement]
+                comparison = None
+        return filters[0]
+
+
+    def execute(self):
+        """Execute the GEE object in GEE Server. This is the function that, when called, actually sends the SQL
+        request (which was converted to GEE-speak) to Google's servers for execution and returns the result."""
+        if self.feature_collection:
+            return self.feature_collection.getInfo()
+        return None
