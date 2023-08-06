3fad30a2e35f5516aa6190134963d3ee5aea0f34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:48 +0000

    Add package fk_nester/1.2.0/fk_nester-1.2.0.zip

---
+++ packages/fk_nester/fk_nester-1.2.0.zip/fk_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'fk_nester',
+        version = '1.2.0',
+        py_modules = ['fk_nester'],
+        author = 'wade92',
+        author_mail = 'wa_de1992@126.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'a simple printer of nested list',
+    )
