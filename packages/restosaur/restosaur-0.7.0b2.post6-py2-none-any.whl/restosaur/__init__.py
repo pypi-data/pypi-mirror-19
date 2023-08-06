e01f0981e34ef926e8de7f56ba36bc3ecd097243
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:40 +0000

    Add package restosaur/0.7.0b2.post6/restosaur-0.7.0b2.post6-py2-none-any.whl

---
+++ packages/restosaur/restosaur-0.7.0b2.post6-py2-none-any.whl/restosaur/__init__.py
@@ -0,0 +1,16 @@
+"""
+Restosaur - a tiny but real REST library
+
+Author: Marcin Nowak <marcin.j.nowak@gmail.com>
+"""
+
+from __future__ import absolute_import
+
+from . import resource  # NOQA
+from . import responses  # NOQA
+from . import filters  # NOQA
+
+from .api import API  # NOQA
+
+
+default_app_config = 'restosaur.apps.RestosaurAppConfig'
