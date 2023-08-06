4b5954e50115e670fa8470f4e0445ca846f2b5d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:13 +0000

    Add package cs.plone5.tinymcepatch/1.0/cs.plone5.tinymcepatch-1.0.tar.gz

---
+++ packages/cs.plone5.tinymcepatch/cs.plone5.tinymcepatch-1.0.tar.gz/cs.plone5.tinymcepatch-1.0/cs/plone5/tinymcepatch/patches.py
@@ -0,0 +1,25 @@
+from logging import getLogger
+from plone.registry.interfaces import IRegistry
+from Products.CMFPlone.interfaces import ITinyMCESchema
+from Products.CMFPlone.patterns import TinyMCESettingsGenerator
+from Products.CMFPlone.patterns.utils import get_portal_url
+from zope.component.hooks import getSite
+from zope.component import getUtility
+
+
+def __new__init(self, context, request):
+    self.context = context
+    self.request = request
+    self.portal = getSite()
+    registry = getUtility(IRegistry)
+    self.settings = registry.forInterface(
+        ITinyMCESchema, prefix="plone", check=False)
+    self.portal_url = get_portal_url(self.context)
+
+
+TinyMCESettingsGenerator.__old__init__ = TinyMCESettingsGenerator.__old__init__
+TinyMCESettingsGenerator.__init__ = __new__init
+
+
+log = getLogger('cs.plone5.tinymcepatch')
+log.info("TinyMCE patching done")
---
+++ packages/cs.plone5.tinymcepatch/cs.plone5.tinymcepatch-1.0.tar.gz/cs.plone5.tinymcepatch-1.0/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='cs.plone5.tinymcepatch',
+      version=version,
+      description="Patch to make TinyMCE Upload work in Plone <= 5.0.6 with p.a.multilingual",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['cs', 'cs.plone5'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
