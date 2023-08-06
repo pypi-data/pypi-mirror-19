d7fac73066737876bc0b28f25b64b6681bc5fa22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:41 +0000

    Add package awscfncli/0.1/awscfncli-0.1-py2.py3-none-any.whl

---
+++ packages/awscfncli/awscfncli-0.1-py2.py3-none-any.whl/awscfncli/commands/stack/describe.py
@@ -0,0 +1,32 @@
+# -*- encoding: utf-8 -*-
+
+__author__ = 'kotaimen'
+__date__ = '11/01/2017'
+
+import boto3
+import click
+
+from ..utils import boto3_exception_handler, pretty_print_stack
+from ...cli import stack
+from ...config import load_stack_config
+
+
+@stack.command()
+@click.argument('config_file', type=click.Path(exists=True))
+@click.option('--detail', default=1, type=click.IntRange(min=0,max=2))
+@click.pass_context
+@boto3_exception_handler
+def describe(ctx, config_file, detail):
+    """Describe stack status, parameter and output
+
+    \b
+    CONFIG_FILE         Stack configuration file.
+    CHANGESET_NAME      The name of the change set.
+    """
+
+    stack_config = load_stack_config(config_file)
+
+    cfn = boto3.resource('cloudformation', region_name=stack_config['Region'])
+    stack = cfn.Stack(stack_config['StackName'])
+
+    pretty_print_stack(stack, detail=detail)
