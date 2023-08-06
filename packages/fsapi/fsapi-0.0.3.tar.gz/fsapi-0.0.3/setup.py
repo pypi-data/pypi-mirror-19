c1599caa76decb3cf63706f654e76fb01b6adb9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:55 +0000

    Add package fsapi/0.0.3/fsapi-0.0.3.tar.gz

---
+++ packages/fsapi/fsapi-0.0.3.tar.gz/fsapi-0.0.3/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name='fsapi',
+      version='0.0.3',
+      description='Implementation of the Frontier Silicon API for python',
+      #install_requires=['requests','lxml'],
+      #maintainer='Krasimir Zhelev',
+      #maintainer_email='krasimir.zhelev@gmail.com',
+      author='Krasimir Zhelev',
+      author_email='krasimir.zhelev@gmail.com',
+      #zip_safe=False,
+      packages=['fsapi'],
+      #include_package_data=True,
+      keywords='fsapi frontier silicon',
+      license='Apache 2',
+      download_url='https://github.com/zhelev/python-fsapi/archive/0.0.3.zip',
+      url='https://github.com/zhelev/python-fsapi.git',
+)
