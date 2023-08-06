d22688c19313887d332da31df189997eeb9cca45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:02 +0000

    Add package uc_chalk_rip/0.6/uc_chalk_rip-0.6.tar.gz

---
+++ packages/uc_chalk_rip/uc_chalk_rip-0.6.tar.gz/uc_chalk_rip-0.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+version = '0.6'
+
+setup(name='uc_chalk_rip',
+    version = version,
+    description = "Ripper for Uchicago's chalk site",
+    author="Reid McIlroy-Young",
+    author_email = "reidmcy@uchicago.edu",
+    license = 'GPL',
+    url="https://github.com/reidmcy/uc_chalk_rip",
+    classifiers = [
+    "Programming Language :: Python :: 3 :: Only",
+    "Development Status :: 4 - Beta",
+    ],
+    download_url = "https://github.com/reidmcy/uc_chalk_rip/archive/{}.tar.gz".format(version),
+    packages = find_packages(),
+    install_requires= ['bs4', 'requests'],
+    entry_points={'console_scripts': [
+              'chalk_rip = chalk_rip:main',
+              ]},
+)
