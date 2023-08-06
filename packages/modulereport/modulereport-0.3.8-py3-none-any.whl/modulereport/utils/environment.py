496075419f87755ad815996248cc6ed511849ca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:46 +0000

    Add package modulereport/0.3.8/modulereport-0.3.8-py3-none-any.whl

---
+++ packages/modulereport/modulereport-0.3.8-py3-none-any.whl/modulereport/utils/environment.py
@@ -0,0 +1,32 @@
+#
+# Copyright Bertil Kronlund, 2017
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+utils.environment
+-----------------
+
+Utility functions for the executing environment
+
+"""
+
+import sys
+
+
+def python_version(level='patch'):
+    """
+    Default version 'M.m.p' of running Python interpreter
+    """
+    width_dict = {'major': 1, 'minor': 3, 'patch': 5}
+    return sys.version[:width_dict[level]]
