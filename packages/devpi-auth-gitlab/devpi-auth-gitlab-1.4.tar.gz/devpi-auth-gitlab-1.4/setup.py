c1b5ab5069fe3937e0ef4b6ac30603dee35baacc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:25 +0000

    Add package devpi-auth-gitlab/1.4/devpi-auth-gitlab-1.4.tar.gz

---
+++ packages/devpi-auth-gitlab/devpi-auth-gitlab-1.4.tar.gz/devpi-auth-gitlab-1.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="devpi-auth-gitlab",
+    description="An authentication plugin for use with gitlab-ci, utilising the build in registry token authentication scheme",
+    version='1.4',
+    author="Andrew Leech",
+    author_email="andrew@alelec.net",
+    url="https://gitlab.com/corona/devpi-gitlab-auth",
+    keywords="devpi plugin",
+    entry_points={
+        'devpi_server': [
+            "devpi-auth-gitlab = devpi_auth_gitlab.main",
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'devpi-server>=2.0.0',
+    ]
+)
