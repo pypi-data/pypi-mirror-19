0b4372c4d37ea53b6ad75ffec60ab0b12e8d8cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:41 +0000

    Add package pyactors-ng/0.5/pyactors_ng-0.5-py2.py3-none-any.whl

---
+++ packages/pyactors-ng/pyactors_ng-0.5-py2.py3-none-any.whl/actorfactory/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from .factory import make_actor, make_publisher  # noqa
+
+# vim: ts=4 sw=4 sts=4 et:
+
---
+++ packages/pyactors-ng/pyactors_ng-0.5-py2.py3-none-any.whl/processactor/publisher.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from actorfactory import make_publisher
+from .actor import Actor
+
+
+Publisher = make_publisher(Actor)
+# vim: ts=4 sw=4 sts=4 et:
