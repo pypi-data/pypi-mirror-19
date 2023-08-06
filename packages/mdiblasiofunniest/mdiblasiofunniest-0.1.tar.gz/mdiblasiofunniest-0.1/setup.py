e635b01c52be2eb9db1d314c2ab2aba2a2366ce6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:31 +0000

    Add package mdiblasiofunniest/0.1/mdiblasiofunniest-0.1.tar.gz

---
+++ packages/mdiblasiofunniest/mdiblasiofunniest-0.1.tar.gz/mdiblasiofunniest-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mdiblasiofunniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/mdiblasio/funniest',
+      author='Michael DiBlasio',
+      author_email='mdiblasio@google.com',
+      license='dibs',
+      packages=['mdiblasiofunniest'],
+      zip_safe=False)
