dd5a4eba92fe1a844f6d9c4679cb0c553d870235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:01 +0000

    Add package treetojson/0.0.3/treetojson-0.0.3.tar.gz

---
+++ packages/treetojson/treetojson-0.0.3.tar.gz/treetojson-0.0.3/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+version = '0.0.3'
+
+setup(
+    name='treetojson',
+    version=version,
+    description='Converts a tree structure in to a valid JSON',
+    author='Saad Sahibjan',
+    author_email='saad.sahibjan@gmail.com',
+    license='LICENSE',
+    url='https://github.com/saadsahibjan/treetojson',
+    py_modules=['treetojson'],
+    platforms='Cross-platform',
+)
