8af64ddb14bdff141c057a3cd55d0165f7b32ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:16 +0000

    Add package tap-braintree/0.2.0/tap-braintree-0.2.0.tar.gz

---
+++ packages/tap-braintree/tap-braintree-0.2.0.tar.gz/tap-braintree-0.2.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+import os.path
+
+
+setup(name='tap-braintree',
+      version='0.2.0',
+      description='Taps BrainTree data',
+      author='Stitch',
+      url='https://github.com/stitchstreams/tap-braintree',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_braintree'],
+      install_requires=[
+          'stitchstream-python>=0.6.0',
+          'requests==2.12.4',
+          'backoff==1.3.2',
+          'python-dateutil==2.6.0',
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-braintree=tap_braintree:main
+      ''',
+      packages=['tap_braintree'],
+      package_data = {
+          'tap_braintree': [
+              'transactions.json',
+          ],
+      }
+)
