26bc19558d59ad20a8f5c961bcb053b4225b3cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:51 +0000

    Add package ren_nester/1.1.1/ren_nester-1.1.1.tar.gz

---
+++ packages/ren_nester/ren_nester-1.1.1.tar.gz/ren_nester-1.1.1/ren_nester.py
@@ -0,0 +1,16 @@
+"""This is the nester module, and it provide you a function called print_lol()
+which can print the lists that may or may not have nested list.
+Provided in the list of the various data items will be printed to the screen, and a line
+"""
+def print_lol(the_list,level):
+    """ :param custom_list:the list you wanna print
+        :return:each item per line
+    """
+    
+    for each_items in the_list:
+        if isinstance(each_items, list):
+            print_lol(each_items,level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t",end='')
+            print(each_items)
---
+++ packages/ren_nester/ren_nester-1.1.1.tar.gz/ren_nester-1.1.1/setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+
+setup(
+    name='ren_nester',
+    version='1.1.1',
+    py_modules=['ren_nester'],
+    author='rmj',
+    author_email='renmingjun25@gmail.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple print function for nested list.',
+)
