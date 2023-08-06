2c7dffcc8dadd6246dca3a544a9282ef38b19ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:13 +0000

    Add package sdklib/1.8/sdklib-1.8.tar.gz

---
+++ packages/sdklib/sdklib-1.8.tar.gz/sdklib-1.8/tests/test_sdklib.py
@@ -0,0 +1,40 @@
+import unittest
+
+from tests.sample_sdk import SampleHttpSdk
+
+
+class TestSampleSdk(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        # SampleHttpSdk.set_default_proxy("http://localhost:8080")
+        cls.api = SampleHttpSdk()
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_get_items(self):
+        response = self.api.get_items()
+        self.assertEqual(response.status, 200)
+        self.assertTrue(isinstance(response.data, list))
+
+    def test_create_item(self):
+        response = self.api.create_item("mi nombre", "algo")
+        self.assertEqual(response.status, 201)
+
+    def test_update_item(self):
+        response = self.api.update_item(1, "mi nombre", "algo")
+        self.assertEqual(response.status, 200)
+
+    def test_partial_update_item(self):
+        response = self.api.partial_update_item(1, "mi nombre")
+        self.assertEqual(response.status, 405)
+
+    def test_delete_item(self):
+        response = self.api.delete_item(1)
+        self.assertEqual(response.status, 204)
+
+    def test_login(self):
+        response = self.api.login(username="user", password="password")
+        self.assertEqual(response.status, 404)
