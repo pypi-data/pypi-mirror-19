e6dcdcd93b2eab9b10a5a824d03db15631c6bad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:09 +0000

    Add package PyIntruder/0.1.1/PyIntruder-0.1.1.tar.gz

---
+++ packages/PyIntruder/PyIntruder-0.1.1.tar.gz/PyIntruder-0.1.1/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+requirements = []
+
+long_description = ''
+with open('./README.md', 'r') as fp:
+    long_description = fp.read()
+
+setup(
+    name='PyIntruder',
+    version='0.1.1',
+    py_modules=[
+	'PyIntruder',
+    ],
+    install_requires=requirements,
+
+    author='sirpsycho',
+    author_email='',
+    classifiers=[
+	'Development Status :: 3 - Alpha',
+	'License :: OSI Approved :: MIT License',
+	'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    description='Command line URL fuzzer',
+    license='MIT',
+    long_description=long_description,
+    keywords='pyintruder, http, fuzzer, url, scan',
+    url='https://github.com/sirpsycho/PyIntruder',
+)
