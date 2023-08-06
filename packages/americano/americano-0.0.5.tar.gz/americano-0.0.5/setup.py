828013e1b5fd3f07858c5f752e75e0fcfa06b682
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:12 +0000

    Add package americano/0.0.5/americano-0.0.5.tar.gz

---
+++ packages/americano/americano-0.0.5.tar.gz/americano-0.0.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+
+setup(
+    name='americano',
+    version='0.0.5',
+    packages=['americano'],
+    license='MIT',
+    install_requires=['six'],
+    author_email='jungroth@gmail.com',
+    author='Travis Jungroth',
+    url='http://github.com/travisjungroth/americano',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
