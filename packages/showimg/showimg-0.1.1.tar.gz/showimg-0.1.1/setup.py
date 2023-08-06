87bb176673816e611ec96907c53d97577c53dfbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:33 +0000

    Add package showimg/0.1.1/showimg-0.1.1.tar.gz

---
+++ packages/showimg/showimg-0.1.1.tar.gz/showimg-0.1.1/setup.py
@@ -0,0 +1,38 @@
+from os import path
+
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+
+def cwd():
+    return path.abspath(path.dirname(__file__))
+
+
+def readme():
+    with open(path.join(cwd(), 'README.rst')) as fp:
+        return fp.read()
+
+
+setup(
+    name='showimg',
+    version='0.1.1',
+    description='Show image on 256-coloured terminal.',
+    long_description=readme(),
+    url='https://github.com/kjwon15/showimg',
+    author='Kjwon15',
+    license='MIT',
+    py_modules=['showimg'],
+    install_requires=[
+        'Pillow>=4.0.0',
+        'requests>=2.12.5',
+    ],
+    entry_points={
+        'console_scripts': [
+            'showimg=showimg:main'
+        ],
+    }
+)
