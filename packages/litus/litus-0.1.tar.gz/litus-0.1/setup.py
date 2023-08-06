9963e0ca0b2612db87c04a9b73248e1b93831479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:22 +0000

    Add package litus/0.1/litus-0.1.tar.gz

---
+++ packages/litus/litus-0.1.tar.gz/litus-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'litus',
+  packages = ['litus'],
+  version = '0.1',
+  description = 'Utils package, containing among others a class to manage labeled spike/event trains.',
+  author = 'Jacob Huth',
+  author_email = 'jahuth@uos.de',
+  url = 'https://github.com/jahuth/litus',
+  download_url = 'https://github.com/jahuth/litus/tarball/0.1',
+  keywords = ['utils', 'spikes'],
+  classifiers = [],
+)
