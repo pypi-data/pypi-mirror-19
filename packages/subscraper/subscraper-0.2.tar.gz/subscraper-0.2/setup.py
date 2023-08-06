7e9d191b246c8e2a927092642cdf4895d92fa837
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:01 +0000

    Add package subscraper/0.2/subscraper-0.2.tar.gz

---
+++ packages/subscraper/subscraper-0.2.tar.gz/subscraper-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='subscraper',
+	scripts=['bin/subscraper'],
+	version='0.2',
+	description='Movies/Series Subtitle Downloader',
+	url='http://github.com/charmpitz/subscraper',
+	author='Andrei Pit',
+	author_email='charmpitz@gmail.com',
+	license='MIT',
+	packages=['subscraper'],
+	zip_safe=False)
---
+++ packages/subscraper/subscraper-0.2.tar.gz/subscraper-0.2/subscraper/__init__.py
@@ -0,0 +1,1 @@
+from .subscraper import *
