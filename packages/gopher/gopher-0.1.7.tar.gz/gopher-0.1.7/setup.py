58c00297fc2c30d0c0ddef576b85aad000f6eac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:04 +0000

    Add package gopher/0.1.7/gopher-0.1.7.tar.gz

---
+++ packages/gopher/gopher-0.1.7.tar.gz/gopher-0.1.7/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+from setuptools import find_packages
+
+import gopher
+
+with open('README.rst', "r") as f:
+    long_description = f.read()
+
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
+setup(
+    name=gopher.__name__,
+    packages=find_packages(),
+    version=gopher.__version__,
+    author=gopher.__author__,
+    author_email=gopher.__email__,
+    description="Download randomized gopher image (png) from gopherize.me",
+    long_description=long_description,
+    url=gopher.__url__,
+    license=gopher.__license__,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Utilities",
+        "Environment :: Console"
+    ],
+    entry_points="""
+      # -*- Entry points: -*-
+      [console_scripts]
+      gopher = gopher.gopher:main
+    """,
+)
