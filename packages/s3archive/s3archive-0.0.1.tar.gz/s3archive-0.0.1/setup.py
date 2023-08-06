978a9515655e6ae68eb34a44f5ae13ccf4f44877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:29 +0000

    Add package s3archive/0.0.1/s3archive-0.0.1.tar.gz

---
+++ packages/s3archive/s3archive-0.0.1.tar.gz/s3archive-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 's3archive',
+    packages = ['s3archive'],
+    version = '0.0.1',
+    description = 'An easy to use python3 package to archive files on Amazon S3.',
+    author = 'Daniele Maccioni',
+    author_email = 'komradstudios@gmail.com',
+    url = 'https://github.com/GendoIkari/s3archive',
+    download_url = 'https://github.com/GendoIkari/s3archive/tarball/v0.0.1',
+    keywords = ['amazon', 's3', 'archive'],
+    classifiers = [],
+)
