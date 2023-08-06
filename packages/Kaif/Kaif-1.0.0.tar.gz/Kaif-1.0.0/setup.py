5dbfa5c9fc90033aa96b057cf53952b67a4acd0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:13 +0000

    Add package Kaif/1.0.0/Kaif-1.0.0.tar.gz

---
+++ packages/Kaif/Kaif-1.0.0.tar.gz/Kaif-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'Kaif',
+	version = '1.0.0',
+	py_modules = '[Kaif]',
+	author = 'Kaif Jamadar',
+	url='',
+	author_email = 'kaifj99@gmail.com',
+	description = 'A Simple athlete data printer'
+	)
