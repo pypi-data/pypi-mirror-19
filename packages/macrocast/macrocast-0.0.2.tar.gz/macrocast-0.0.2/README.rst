66e674854b0562ae6e6b4b4c6de32422b1b75689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:10 +0000

    Add package macrocast/0.0.2/macrocast-0.0.2.tar.gz

---
+++ packages/macrocast/macrocast-0.0.2.tar.gz/macrocast-0.0.2/README.rst
@@ -0,0 +1,21 @@
+MacroCast
+=========
+
+Description
+-----------
+
+Forecasting package for macroeconomic data
+
+Installation
+------------
+
+pip install --no-cache-dir macrocast
+
+Author: Amir Sani
+^^^^^^^^^^^^^^^^^
+
+Maintainer: `Amir Sani <http://amirsani.com>`__
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+Copyright (c) 2017, Amir Sani
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
---
+++ packages/macrocast/macrocast-0.0.2.tar.gz/macrocast-0.0.2/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='macrocast',
+    version='0.00.02',
+    description='A forecasting package for macroeconomic data',
+    long_description=readme(),
+    author='Amir Sani',
+    url='http://www.amirsani.com',
+    author_email='reachme@amirsani.com',
+    download_url='https://github.com/amirsani/macrocast',
+    packages=['macrocast'],
+    zip_safe=False
+)
