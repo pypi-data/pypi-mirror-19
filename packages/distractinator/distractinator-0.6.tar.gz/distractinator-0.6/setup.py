e71448f4014cd507bd79d7cd1d7c15c5f7e76a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:12 +0000

    Add package distractinator/0.6/distractinator-0.6.tar.gz

---
+++ packages/distractinator/distractinator-0.6.tar.gz/distractinator-0.6/setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+        name="distractinator",
+        version='0.6',
+        description="For use with the Distractinator USB receiver.",
+        author="Joe Dougherty",
+        author_email="joseph.dougherty@gmail.com",
+        packages=['distractinator'],
+        package_data = {'distractinator': ['examples/.distractinator.conf', 'examples/customevents.py']},
+        install_requires=['pyserial>=3.1.1', 'six>=1.10.0'],
+        entry_points={
+            'console_scripts': ['distractd = distractinator:main'],
+            },
+        zip_safe=False,
+        )
+
