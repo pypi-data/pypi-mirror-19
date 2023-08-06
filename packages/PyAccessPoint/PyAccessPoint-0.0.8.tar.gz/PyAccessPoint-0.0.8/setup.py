e267f8bdbda6559d704e9b9085669147045ac41c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:10 +0000

    Add package PyAccessPoint/0.0.8/PyAccessPoint-0.0.8.tar.gz

---
+++ packages/PyAccessPoint/PyAccessPoint-0.0.8.tar.gz/PyAccessPoint-0.0.8/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import os
+
+if os.path.exists('README.md'):
+    try:
+        import pypandoc
+        long_description = pypandoc.convert('README.md', 'rst')
+    except(IOError, ImportError):
+        long_description = open('README.md').read()
+else:
+    long_description = ''
+
+s = setup(
+    install_requires=[
+        'wireless',
+        'netifaces',
+        'psutil'
+    ],
+    name='PyAccessPoint',
+    version='0.0.8',
+    description='Package to manage wifi hotspot on linux',
+    long_description=long_description,
+    license='GNU GPLv3',
+    author='Anton Bautkin',
+    author_email='antonbautkin@gmail.com',
+    url='https://github.com/Goblenus/pyaccesspoint',
+    packages=['PyAccessPoint'],
+    entry_points={
+        'console_scripts': [
+            'pyaccesspoint = PyAccessPoint.main:main',
+        ],
+    }
+)
