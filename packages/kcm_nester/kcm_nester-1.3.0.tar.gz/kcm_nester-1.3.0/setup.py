a31b8d389f047a618ac236d0beee8ea265935171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:07 +0000

    Add package kcm_nester/1.3.0/kcm_nester-1.3.0.tar.gz

---
+++ packages/kcm_nester/kcm_nester-1.3.0.tar.gz/kcm_nester-1.3.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name         = 'kcm_nester',
+        version      = '1.3.0',
+        py_modules   = ['nester'],
+        author       = 'KC Mee',
+        author_email = 'kcmee23@gmail.com',
+        url          = 'http://www.thegoatsports.com',
+        description  = 'A simple printer of nested lists',
+
+    )
