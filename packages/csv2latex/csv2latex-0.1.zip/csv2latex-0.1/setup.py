0886cfab1561e48da6019b7e9e9929a47526b2a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:37 +0000

    Add package csv2latex/0.1/csv2latex-0.1.zip

---
+++ packages/csv2latex/csv2latex-0.1.zip/csv2latex-0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'csv2latex',
+  packages = ['csv2latex'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Python script for csv to latex conversion',
+  long_description='',
+  author = 'Sepand Haghighi & Nima Mohammadi',
+  author_email = 'sepand.haghighi@qpage.ir',
+  url = 'https://github.com/sepandhaghighi/csv2latex', # use the URL to the github repo
+  download_url = 'https://github.com/sepandhaghighi/csv2latex/tarball/v0.1', # I'll explain this in a second
+  keywords = ['free', 'csv', 'latex','python','csv2latex','LaTeX','Table'], # arbitrary keywords
+  classifiers = [],
+  license='MIT',
+)
