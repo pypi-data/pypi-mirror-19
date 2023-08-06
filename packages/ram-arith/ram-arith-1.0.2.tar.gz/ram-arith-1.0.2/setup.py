431e3db179f72fb9004cf37ea338613fc4cdf9e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:12 +0000

    Add package ram-arith/1.0.2/ram-arith-1.0.2.tar.gz

---
+++ packages/ram-arith/ram-arith-1.0.2.tar.gz/ram-arith-1.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    # Application name:
+    name="ram-arith",
+
+    # Version number (initial):
+    version="1.0.2",
+
+    # Application author details:
+    author="ramya",
+    author_email="munja.ramya@gmail.com",
+
+    # Packages
+    packages=["arith","ram"]
+
+   
+)
