46c29ea9962e41f32e1b4f4c57363e25e7b0843e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:45 +0000

    Add package edrn.labcas.ui/0.0.3/edrn.labcas.ui-0.0.3.zip

---
+++ packages/edrn.labcas.ui/edrn.labcas.ui-0.0.3.zip/edrn.labcas.ui-0.0.3/parts/omelette/edrn/labcas/ui/interfaces.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+
+from zope.interface import Interface
+
+
+class IBackend(Interface):
+    u'''What a LabCAS backend looks like.'''
+    def getFileMgr():
+        u'''Retrieves the file manager'''
+    def getWorkflowMgr():
+        u'''Retrieves the workflow manager'''
+    def getStagingDirectory():
+        u'''Retrieves the path to the LabCAS staging directory'''
+    def getArchiveDirectory():
+        u'''Retrieves the path to the LabCAS archive directory'''
+    def getSearchEngine(kind):
+        u'''Retrieves the SOLR search API for the given ``kind`` of data.'''
+
+
+class IVocabularies(Interface):
+    u'''What the vocabulary manager resembles.'''
+    def getPeople():
+        u'''Returns a sequence of people working on EDRN'''
+    def getProtocols():
+        u'''Returns a sequence of EDRN protocol names'''
---
+++ packages/edrn.labcas.ui/edrn.labcas.ui-0.0.3.zip/edrn.labcas.ui-0.0.3/src/edrn/labcas/ui/resources/__init__.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+from ._collection import Collection
+from ._collections import Collections
+from ._dataset import Dataset
+from ._file import File
+from ._root import Root
+from ._upload import Upload
+
+__all__ = (
+    Collection,
+    Collections,
+    Dataset,
+    File,
+    Root,
+    Upload,
+)
