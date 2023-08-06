33e562a4b5ffb393340c3e4da583725e094a8339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:48 +0000

    Add package jagan/1.0.0/jagan-1.0.0.tar.gz

---
+++ packages/jagan/jagan-1.0.0.tar.gz/jagan-1.0.0/setup.py
@@ -0,0 +1,13 @@
+'''
+Created on 09-Jan-2017
+
+@author: jagan
+'''
+from distutils.core import setup
+
+setup(
+    name    = 'jagan',
+    version = '1.0.0',
+    py_modules = ['recur'],
+    author  = 'jagan',
+    )
