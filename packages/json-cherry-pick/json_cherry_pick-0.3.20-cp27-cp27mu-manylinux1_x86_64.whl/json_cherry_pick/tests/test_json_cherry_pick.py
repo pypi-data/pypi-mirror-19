bf3d6097dfd9249c240977b0b71f5c48a30c60c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:57 +0000

    Add package json-cherry-pick/0.3.20/json_cherry_pick-0.3.20-cp27-cp27mu-manylinux1_x86_64.whl

---
+++ packages/json-cherry-pick/json_cherry_pick-0.3.20-cp27-cp27mu-manylinux1_x86_64.whl/json_cherry_pick/tests/test_json_cherry_pick.py
@@ -0,0 +1,56 @@
+import unittest
+import json
+import json_cherry_pick as jcp
+
+ascii_basic = {
+    'test_str': 'hello, world',
+    'test_val': 1234567.89,
+    'test_arr': ['one', 'two', 'three'],
+    'test_obj': {'hello': 'there'},
+    'test_empty_arr': [],
+    'test_empty_obj': {},
+    'test_nest_arr': [[1, 2], [3, 4], [5, 6]],
+    'test_nest_obj': {'outer': {'inner': 'value'}},
+}
+
+class TestBasicExtraction(unittest.TestCase):
+
+    def test_basic_extraction(self):
+        sut = jcp.loads(json.dumps(ascii_basic))
+
+        for k, v in ascii_basic.items():
+            self.assertEqual(sut[k], v)
+
+    def test_get(self):
+        sut = jcp.loads(json.dumps(ascii_basic))
+
+        self.assertEqual(sut.get('test_str'), ascii_basic.get('test_str'))
+        self.assertEqual(sut.get('blah'), None)
+        self.assertEqual(sut.get('blah', 1), 1)
+
+    def test_get_hold_default_ref(self):
+        sut = jcp.loads(json.dumps(ascii_basic))
+        d = 'this is the default'
+
+        x = sut.get('blah', d)
+        del d
+        del x
+        self.assertTrue(True)
+
+
+class TestReification(unittest.TestCase):
+    
+    def test_multiple_reassignment(self):
+        sut = jcp.loads(json.dumps(ascii_basic))
+
+        sut['test_obj']['item1'] = 'Blah'
+        sut['test_obj']['item2'] = 'Bleh'
+
+        self.assertEqual(sut['test_obj']['item1'], 'Blah')
+        self.assertEqual(sut['test_obj']['item2'], 'Bleh')
+
+        sut['test_obj']['item1'] = 'NewBlah'
+        sut['test_obj']['item2'] = 'NewBleh'
+        
+        self.assertEqual(sut['test_obj']['item1'], 'NewBlah')
+        self.assertEqual(sut['test_obj']['item2'], 'NewBleh')
