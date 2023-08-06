1204c4aaba723d99eebfa10dd3b155395e556eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:42 +0000

    Add package iteration-utilities/0.1.0/iteration_utilities-0.1.0.tar.gz

---
+++ packages/iteration-utilities/iteration_utilities-0.1.0.tar.gz/iteration_utilities-0.1.0/src/_globaldocs.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+ * Licensed under Apache License Version 2.0 - see LICENSE.rst
+ *****************************************************************************/
+
+/******************************************************************************
+ *
+ * Documentation for __reduce__
+ *
+ * This section has a differing license because this was copied from the
+ * itertoolsmodule.c file of CPython.
+ *
+ * Licensed under PSF license - see licenses/LICENSE_PYTHON.rst
+ *
+ *****************************************************************************/
+
+PyDoc_STRVAR(PYIU_reduce_doc, "Return state information for pickling.");
+
+PyDoc_STRVAR(PYIU_setstate_doc, "Set state information for unpickling.");
+
+PyDoc_STRVAR(PYIU_lenhint_doc, "Return a length hint if possible.");
