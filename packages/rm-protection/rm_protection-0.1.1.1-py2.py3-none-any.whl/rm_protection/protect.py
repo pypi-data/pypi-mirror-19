f96eb2adbf6fad8ea8ded6f7551d684a5d633d8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:34 +0000

    Add package rm-protection/0.1.1.1/rm_protection-0.1.1.1-py2.py3-none-any.whl

---
+++ packages/rm-protection/rm_protection-0.1.1.1-py2.py3-none-any.whl/rm_protection/protect.py
@@ -0,0 +1,27 @@
+from sys import argv
+from os.path import expanduser as expu, expandvars as expv
+from os.path import basename, dirname, abspath, exists
+from builtins import input
+from rm_protection.config import Config
+
+
+def protect(protect_args=None):
+    c = Config()
+    if not protect_args:
+        protect_args = argv[1:]
+    for arg in protect_args:
+        if arg in c.invalid:
+            print("\".\" and \"..\" may not be protected")
+        else:
+            path = abspath(expv(expu(arg)))
+            evalpath = dirname(path) + "/." + basename(path) + c.suffix
+            if not exists(path):
+                print("Warning: " + path + " does not exist")
+            with open(evalpath, "w") as f:
+                question = input("Question for " + path + ": ")
+                answer = input("Answer: ")
+                f.write(question + "\n" + answer)
+
+
+if __name__ == "__main__":
+    protect()
