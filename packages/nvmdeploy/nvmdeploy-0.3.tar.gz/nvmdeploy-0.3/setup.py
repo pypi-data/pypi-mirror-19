17fa31f59649ae3ce4b1401236115c2f648c3596
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:25 +0000

    Add package nvmdeploy/0.3/nvmdeploy-0.3.tar.gz

---
+++ packages/nvmdeploy/nvmdeploy-0.3.tar.gz/nvmdeploy-0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.3'
+
+setup(name='nvmdeploy',
+      version=VERSION,
+      description='fast release pod tool',
+      long_description='fast release pod tool',
+      classifiers=[],
+      keywords='pod release',
+      author='stephenw',
+      author_email='zhilong.wang@ele.me',
+      url='https://git.elenet.me/zhilong.wang/nvmdeploy',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          'PyYAML',
+          'python-jenkins'
+      ],
+      entry_points={
+          'console_scripts': [
+              'nvmdeploy = nvmdeploy.nevermore:main'
+          ]
+      })
