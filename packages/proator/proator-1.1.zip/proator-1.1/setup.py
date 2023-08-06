975823cbc7405b4fa7d2cbdf3e9a586b9655fcfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:56 +0000

    Add package proator/1.1/proator-1.1.zip

---
+++ packages/proator/proator-1.1.zip/proator-1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='proator',
+      version='1.1',
+      description='Python function prototyping and call time check.',
+      url='https://github.com/mohit-thakur-721/proator',
+      author='Mohit Thakur',
+      author_email='mohit.thakur@nectechnologies.in',
+      license='FREE',
+      packages=['proator'],
+      zip_safe=False)
