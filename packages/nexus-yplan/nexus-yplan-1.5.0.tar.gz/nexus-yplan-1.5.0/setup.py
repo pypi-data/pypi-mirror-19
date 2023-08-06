7acc04f7fcc67ba2126ba86f8178e03771f84b72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:00 +0000

    Add package nexus-yplan/1.5.0/nexus-yplan-1.5.0.tar.gz

---
+++ packages/nexus-yplan/nexus-yplan-1.5.0.tar.gz/nexus-yplan-1.5.0/setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- encoding:utf-8 -*-
+import os
+import re
+import sys
+
+from setuptools import find_packages, setup
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+version = get_version('nexus')
+
+
+if sys.argv[-1] == 'publish':
+    if os.system("pip freeze | grep twine"):
+        print("twine not installed.\nUse `pip install twine`.\nExiting.")
+        sys.exit()
+    os.system("rm -rf .eggs/ build/ dist/")
+    os.system("python setup.py sdist bdist_wheel")
+    os.system("twine upload dist/*")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a v%s -m 'Version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+setup(
+    name='nexus-yplan',
+    version=version,
+    author='Disqus',
+    author_email='opensource@disqus.com',
+    maintainer='YPlan',
+    maintainer_email='adam@yplanapp.com',
+    url='https://github.com/yplan/nexus',
+    description=(
+        'Nexus is a pluggable admin application in Django. '
+        'It\'s designed to give you a simple design and architecture for building admin applications.'
+    ),
+    long_description=readme + '\n\n' + history,
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    zip_safe=False,
+    install_requires=[],
+    license='Apache License 2.0',
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development'
+    ],
+)
