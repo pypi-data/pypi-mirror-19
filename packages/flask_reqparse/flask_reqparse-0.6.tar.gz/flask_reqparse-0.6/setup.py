de7b7202f49b6d49f39831ef279de78b429e617b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:11 +0000

    Add package flask_reqparse/0.6/flask_reqparse-0.6.tar.gz

---
+++ packages/flask_reqparse/flask_reqparse-0.6.tar.gz/flask_reqparse-0.6/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'flask_reqparse',
+  package = ['flask_reqparse'],
+  version = '0.6',
+  description = 'A requestparser for flask apps',
+  author = 'Smit Thakkar',
+  author_email = 'smitthakkar96@gmail.com',
+  url = 'https://github.com/smitthakkar96/flask_reqparse', # use the URL to the github repo
+  keywords = ['flask_reqparse', 'request parsing flask', 'flask request parser'], # arbitrary keywords
+  install_requires=['flask', 'six'],
+  include_package_data=True,
+  classifiers = [],
+)
