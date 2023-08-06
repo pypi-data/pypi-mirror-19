68e9d07fe0192209c5b92448ba1125828a01b02e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:36 +0000

    Add package orionsdk/0.0.6/orionsdk-0.0.6.zip

---
+++ packages/orionsdk/orionsdk-0.0.6.zip/orionsdk-0.0.6/orionsdk/swisclient.py
@@ -0,0 +1,60 @@
+import requests
+import json
+from datetime import datetime
+
+
+def _json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+    if isinstance(obj, datetime):
+        serial = obj.isoformat()
+        return serial
+
+
+class SwisClient:
+    def __init__(self, hostname, username, password, verify=False):
+        self.url = "https://{}:17778/SolarWinds/InformationService/v3/Json/".\
+                format(hostname)
+        self.credentials = (username, password)
+        self.verify = verify
+
+    def query(self, query, **params):
+        return self._req(
+                "POST",
+                "Query",
+                {'query': query, 'parameters': params}).json()
+
+    def invoke(self, entity, verb, *args):
+        return self._req(
+                "POST",
+                "Invoke/{}/{}".format(entity, verb), args).json()
+
+    def create(self, entity, **properties):
+        return self._req(
+                "POST",
+                "Create/" + entity, properties).json()
+
+    def read(self, uri):
+        return self._req("GET", uri).json()
+
+    def update(self, uri, **properties):
+        self._req("POST", uri, properties)
+
+    def delete(self, uri):
+        self._req("DELETE", uri)
+
+    def _req(self, method, frag, data=None):
+        resp = requests.request(method, self.url + frag,
+                                data=json.dumps(data, default=_json_serial),
+                                verify=self.verify,
+                                auth=self.credentials,
+                                headers={'Content-Type': 'application/json'})
+		
+        # try to extract reason from response when request returns error
+        if 400 <= resp.status_code < 600:
+            try:
+                resp.reason = json.loads(resp.text)['Message'];
+            except:
+                pass;
+			 
+        resp.raise_for_status()
+        return resp
