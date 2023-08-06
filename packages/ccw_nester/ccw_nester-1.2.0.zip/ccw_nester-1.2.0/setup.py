89e095e486e08e59daf3230c89457b504696d823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:23 +0000

    Add package ccw_nester/1.2.0/ccw_nester-1.2.0.zip

---
+++ packages/ccw_nester/ccw_nester-1.2.0.zip/ccw_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+#from astroid.__pkginfo__ import author, author_email, description
+
+setup(
+      name = 'ccw_nester',
+      version = '1.2.0',
+      py_modules = ['ccw_nester'],
+      author = 'ccw',
+      author_email = 'ccw3435@naver.com',
+      description = 'a simple printer of nested lists'
+      )
