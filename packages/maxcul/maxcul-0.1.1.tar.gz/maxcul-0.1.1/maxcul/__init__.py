8508ca628dc0a18b33cd329a84e4aa66e5865579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:10 +0000

    Add package maxcul/0.1.1/maxcul-0.1.1.tar.gz

---
+++ packages/maxcul/maxcul-0.1.1.tar.gz/maxcul-0.1.1/maxcul/__init__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+    maxcul
+    ~~~~~~~~~~~~~~
+
+    Implementation of moritz home automation protocol
+
+    :copyright: (c) 2014 by Markus Ullmann.
+    :license: BSD, see LICENSE for more details.
+"""
+
+# environment constants
+
+# python imports
+
+# environment imports
+
+# custom imports
+
+# local constants
+__version__ = "0.1.1"
