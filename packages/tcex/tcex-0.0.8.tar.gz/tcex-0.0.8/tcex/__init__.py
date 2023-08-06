558b1abd20ba44bd7fd1e066ee782e3dae5873fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:46 +0000

    Add package tcex/0.0.8/tcex-0.0.8.tar.gz

---
+++ packages/tcex/tcex-0.0.8.tar.gz/tcex-0.0.8/tcex/__init__.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import inspect
+
+__author__ = 'ThreatConnect (support@threatconnect.com)'
+__version__ = '0.0.8'
+__license__ = 'Apache License, Version 2'
+
+## BCS - This is invalid after lib change for multiple Python versions.
+##       Not sure if it is required now.
+
+## # For Apps Append the '/lib' directory
+## lib_path = os.path.join(os.getcwd(), 'lib')
+##
+## for root, dirs, files in os.walk(lib_path):
+##     while len(dirs) > 0:
+##         sys.path.insert(1, '%s/%s' % (lib_path, dirs.pop()))
+
+try:
+    from tcex import TcEx
+except ImportError as e:
+    print('Error: {}'.format(e))
+    print('Try app.py --lib')
+
+from tcex_local import TcExLocal
+from argparser import ArgParser
