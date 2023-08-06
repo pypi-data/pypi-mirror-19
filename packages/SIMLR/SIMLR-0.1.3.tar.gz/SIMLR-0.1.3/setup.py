f02f488aa5f61cdcdbb2db4dd729ecd7da250f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:55 +0000

    Add package SIMLR/0.1.3/SIMLR-0.1.3.tar.gz

---
+++ packages/SIMLR/SIMLR-0.1.3.tar.gz/SIMLR-0.1.3/setup.py
@@ -0,0 +1,18 @@
+"""
+Copyright (c) 2017, Stanford University.
+All rights reserved.
+
+This source code is a Python implementation of SIMLR for the following paper published in Nature Methods:
+Visualization and analysis of single-cell RNA-seq data by kernel-based similarity learning
+"""
+from distutils.core import setup
+
+setup(
+    name='SIMLR',
+    version='0.1.3',
+    author='Bo Wang',
+    author_email='bowang87@stanford.edu',
+    url='https://github.com/bowang87/SIMLR-PY',
+    description='Visualization and analysis of single-cell RNA-seq data by kernel-based similarity learning',
+    packages=['SIMLR'],
+    classifiers=[])
