169e385947ea6385c86ba23b4fc05a4ae60b0c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:13 +0000

    Add package apply/1.4/apply-1.4.zip

---
+++ packages/apply/apply-1.4.zip/apply-1.4/CHANGES.rst
@@ -0,0 +1,35 @@
+Changelog
+=========
+
+1.4 - 2017-02-05
+----------------
+
+- Support Python 2.6-3.6 without 2to3.
+  [stefan]
+
+- Add a LICENSE file.
+  [stefan]
+
+1.3 - 2014-04-19
+----------------
+
+- Remove setuptools from install_requires because it isn't.
+  [stefan]
+
+1.2 - 2011-11-26
+----------------
+
+- Update README.
+  [stefan]
+
+1.1 - 2011-11-26
+----------------
+
+- Fix function signature.
+  [stefan]
+
+1.0 - 2011-11-25
+----------------
+
+- Initial release.
+  [stefan]
---
+++ packages/apply/apply-1.4.zip/apply-1.4/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+version = '1.4'
+
+setup(name='apply',
+      version=version,
+      description='An apply function for Python 2 and 3',
+      long_description=open('README.rst').read() + '\n' +
+                       open('CHANGES.rst').read(),
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.1',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+      ],
+      keywords='apply read write property properties',
+      author='Stefan H. Holek',
+      author_email='stefan@epy.co.at',
+      url='https://github.com/stefanholek/apply',
+      license='BSD-2-Clause',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=True,
+      test_suite='apply.tests',
+)
