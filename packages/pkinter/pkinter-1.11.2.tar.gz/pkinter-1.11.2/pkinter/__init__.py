1f02007542b369fd2863cd12bc343c38ad7dab2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:41 +0000

    Add package pkinter/1.11.2/pkinter-1.11.2.tar.gz

---
+++ packages/pkinter/pkinter-1.11.2.tar.gz/pkinter-1.11.2/pkinter/__init__.py
@@ -0,0 +1,17 @@
+from .toggledlabelframe import *
+from .labeledseparator import *
+from .roundingscale import *
+from .entrytext import *
+from .limitedentry import *
+from .colourpickerbutton import *
+from .editablelabel import *
+from .collapsiblepane import *
+from .hyperlink import *
+from .pageview import *
+from .toolbar import *
+from .statusbar import *
+from .linenumbers import *
+
+__title__ = "Pkinter"
+__author__ = "DeflatedPickle"
+__version__ = "1.11.2"
---
+++ packages/pkinter/pkinter-1.11.2.tar.gz/pkinter-1.11.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+#from distutils.core import setup
+
+setup (name = "pkinter",
+      version = "1.11.2",
+      description = "A set of useful GUI widgets inherited from TkInter.",
+      author = "DeflatedPickle",
+      author_email = "DeflatedPickle@gmail.com",
+      url = "https://github.com/DeflatedPickle/pkinter",
+      download_url = "https://github.com/DeflatedPickle/pkinter/tarball/0.1",
+      license = "MIT",
+      classifiers = [
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: MIT License",
+      "Programming Language :: Python :: 3.4",
+      "Programming Language :: Python :: 3.5",
+      ],
+      keywords = ["pkinter", "tkinter", "GUI", "widgets"],
+      packages = ["pkinter"])
