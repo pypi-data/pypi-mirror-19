ceaa2d4ac1d1ada2dad184a435cc8317407b692a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:22 +0000

    Add package flake8-SQL/0.1/flake8_SQL-0.1-py2.py3-none-any.whl

---
+++ packages/flake8-SQL/flake8_SQL-0.1-py2.py3-none-any.whl/flake8_sql/keywords.py
@@ -0,0 +1,32 @@
+ABBREVIATED_KEYWORDS = {
+    'ABS',
+    'CHAR',
+    'CHAR_LENGTH',
+    'DEC',
+    'PROC',
+}
+
+ROOT_KEYWORDS = {
+    'AND',
+    'BY',
+    'FROM',
+    'INSERT',
+    'JOIN',
+    'LIMIT',
+    'OR',
+    'RETURNING',
+    'SELECT',
+    'SET',
+    'UPDATE',
+    'VALUES',
+    'WHERE',
+}
+
+ROOT_KEYWORD_DESCRIPTORS = {
+    'GROUP',
+    'INNER',
+    'LEFT',
+    'ORDER',
+    'OUTER',
+    'RIGHT',
+}
