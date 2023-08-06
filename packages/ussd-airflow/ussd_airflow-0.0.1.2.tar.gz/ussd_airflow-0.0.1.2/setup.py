8cf73069741a07a30e083aa41238635706039687
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:33 +0000

    Add package ussd-airflow/0.0.1.2/ussd_airflow-0.0.1.2.tar.gz

---
+++ packages/ussd-airflow/ussd_airflow-0.0.1.2.tar.gz/ussd_airflow-0.0.1.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+import os
+
+__version__ = '0.0.1.2'
+
+setup(
+    name='ussd_airflow',
+    version=__version__,
+    packages=['ussd'],
+    url='https://github.com/mwaaas/ussd_airflow',
+    license='MIT',
+    author='Mwas',
+    author_email='mwasgize@gmail.com',
+    description='Ussd Airflow Library'
+)
