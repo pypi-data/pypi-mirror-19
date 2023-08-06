712839cd6c117603a392f31631e1842ae91d20bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:28 +0000

    Add package lvqnester/1.0.0/lvqnester-1.0.0.tar.gz

---
+++ packages/lvqnester/lvqnester-1.0.0.tar.gz/lvqnester-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup (
+	name		= 'lvqnester',
+	version		= '1.0.0',
+	py_modules	= ['lvqnester'],
+	author		= 'LuCrosTech',
+	author_email	= 'lucrostech@lucrecorp.com',
+	url		= 'http://www.lucrecorp.com',
+	description	= 'A simple printer of nested lists',
+	)
+
