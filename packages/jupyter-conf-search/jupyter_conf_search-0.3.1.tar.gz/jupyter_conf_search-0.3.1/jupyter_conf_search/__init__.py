e374b7302e0e7232e7cda310d217a6fb4d90dd5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:59 +0000

    Add package jupyter-conf-search/0.3.1/jupyter_conf_search-0.3.1.tar.gz

---
+++ packages/jupyter-conf-search/jupyter_conf_search-0.3.1.tar.gz/jupyter_conf_search-0.3.1/jupyter_conf_search/__init__.py
@@ -0,0 +1,6 @@
+"""Utility for searching through jupyter configuration files, using jupyter's path definitions to find their locations dynamically.
+
+"""
+
+__version__="0.3"
+
---
+++ packages/jupyter-conf-search/jupyter_conf_search-0.3.1.tar.gz/jupyter_conf_search-0.3.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='jupyter_conf_search',
+    packages=['jupyter_conf_search'],
+    version='0.3.1',
+    author='Mike Pacer',
+    author_email='mpacer@berkeley.edu',
+    url='https://github.com/michaelpacer/jupyter_conf_search',
+    install_requires=[
+        "jupyter"
+        ]
+)
