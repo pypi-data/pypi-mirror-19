02110ebf75ba2e39a5abd74471dfa1d486fd0f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:26 +0000

    Add package odl/0.5.3.post0/odl-0.5.3.post0-py2.py3-none-any.whl

---
+++ packages/odl/odl-0.5.3.post0-py2.py3-none-any.whl/odl/util/__init__.py
@@ -0,0 +1,42 @@
+﻿# Copyright 2014-2016 The ODL development group
+#
+# This file is part of ODL.
+#
+# ODL is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ODL is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ODL.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Utility library for ODL, only for internal use."""
+
+from __future__ import absolute_import
+
+__all__ = ()
+
+from .testutils import *
+__all__ += testutils.__all__
+
+from .utility import *
+__all__ += utility.__all__
+
+from .normalize import *
+__all__ += normalize.__all__
+
+from .graphics import *
+__all__ += graphics.__all__
+
+from .numerics import *
+__all__ += numerics.__all__
+
+from .vectorization import *
+__all__ += vectorization.__all__
+
+from . import ufuncs
