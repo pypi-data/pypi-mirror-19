ad3c58dcb9581f695f7cd83189dab3f18c999ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:13 +0000

    Add package ptfutils/0.0.4/ptfutils-0.0.4.tar.gz

---
+++ packages/ptfutils/ptfutils-0.0.4.tar.gz/ptfutils-0.0.4/setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='ptfutils',
+    version='0.0.4',
+    packages=['tfutils', 'tfutils.data', 'tfutils.training'],
+    url='https://github.com/PiotrDabkowski/',
+    install_requires = ['tensorflow>=0.12', 'numpy>=1.11'],
+    license='MIT',
+    author='Piotr Dabkowski',
+    author_email='piodrus@gmail.com',
+    description='Useful modules for tensorflow',
+)
