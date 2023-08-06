00d26459c4c9603902f412228d312a9465dbfaa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:56 +0000

    Add package shared_dataverse_information/0.4.91/shared_dataverse_information-0.4.91-py2.py3-none-any.whl

---
+++ packages/shared_dataverse_information/shared_dataverse_information-0.4.91-py2.py3-none-any.whl/shared_dataverse_information/layer_classification/forms_api.py
@@ -0,0 +1,32 @@
+from django import forms
+
+import time
+from .models import ClassifyRequestData
+from shared_dataverse_information.layer_classification.classify_format_helper import format_layer_name_for_classification
+
+
+class ClassifyRequestDataForm(forms.ModelForm):
+
+    def clean_layer_name(self):
+        layer_name = self.cleaned_data.get('layer_name', None)
+        if layer_name is None:
+            raise forms.ValidationError(_('The layer name must be specified'), code='invalid')
+        
+        return format_layer_name_for_classification(layer_name)
+            
+    class Meta:
+        model = ClassifyRequestData
+
+
+class LayerAttributeRequestForm(forms.ModelForm):
+
+    def clean_layer_name(self):
+        layer_name = self.cleaned_data.get('layer_name', None)
+        if layer_name is None:
+            raise forms.ValidationError(_('The layer name must be specified'), code='invalid')
+
+        return format_layer_name_for_classification(layer_name)
+
+    class Meta:
+        model = ClassifyRequestData
+        fields =  ('layer_name', 'datafile_id', 'dataverse_installation_name' )
