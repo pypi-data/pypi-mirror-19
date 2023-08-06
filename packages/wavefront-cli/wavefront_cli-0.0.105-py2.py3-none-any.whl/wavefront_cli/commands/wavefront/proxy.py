8e00872464477cc75dd055c25213847ea3c40470
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:49 +0000

    Add package wavefront-cli/0.0.105/wavefront_cli-0.0.105-py2.py3-none-any.whl

---
+++ packages/wavefront-cli/wavefront_cli-0.0.105-py2.py3-none-any.whl/wavefront_cli/commands/wavefront/proxy.py
@@ -0,0 +1,69 @@
+
+import system
+import api
+import subprocess
+import message
+import sys
+
+proxy_pkg_deb = "https://packagecloud.io/install/repositories/lib/proxy/script.deb.sh"
+proxy_pkg_rpm = "https://packagecloud.io/install/repositories/lib/proxy/script.rpm.sh"
+
+def get_proxy_install_cmd():
+    # dist = self.check_os()
+    dist = system.check_os()
+    print "Detected ", dist
+    if dist == "Amazon Linux AMI" or dist == "Red Hat Enterprise Linux Server":
+        cmd = "sudo curl -s %s | sudo bash" % (proxy_pkg_rpm)
+        cmd += " && sudo yum -y -q install lib-proxy"
+        return cmd
+    elif dist == "Ubuntu":
+        cmd = "sudo curl -s %s | sudo bash" % (proxy_pkg_deb)
+        cmd += " && sudo apt-get -y -q install lib-proxy"
+        return cmd
+    else:
+        print "Error: Unsupported OS version: %s. Please contact support@lib.com." % (dist)
+        return None
+
+
+def install_proxy():
+
+    message.print_bold("Starting Wavefront Proxy Installation!")
+    cmd = get_proxy_install_cmd()
+    ret_code = subprocess.call(cmd, shell=True)
+    if ret_code > 0:
+        message.print_warn("Error installing proxy: " + sys.exc_info()[0])
+        return False
+    else:
+        message.print_success("Finished Wavefront Proxy Installation!")
+        return True
+
+
+def configure_proxy(url, token):
+    message.print_bold("Starting Wavefront Proxy Configuration!")
+    url = api.clean_url(url) + "/api/"
+    print url
+    print token
+
+    # replace token
+    cmd = "sudo sed -i -e '/token=/c\ttoken=%s' /etc/lib/lib-proxy/lib.conf" % (token)
+    ret_code = subprocess.call(cmd, shell=True)
+    if ret_code > 0:
+        message.print_warn("Error configuring proxy: " + sys.exc_info()[0])
+        return False
+
+    # replace server url
+    cmd = "sudo sed -i -e '/server=/c\tserver=%s' /etc/lib/lib-proxy/lib.conf" % (url)
+    ret_code = subprocess.call(cmd, shell=True)
+    if ret_code > 0:
+        message.print_warn("Error configuring proxy: " + sys.exc_info()[0])
+        return False
+
+    # restart proxy
+    ret_code = system.restart_service("lib-proxy")
+    if ret_code > 0:
+        message.print_warn("Error restarting proxy service: " + sys.exc_info()[0])
+        return False
+
+    message.print_success("Finished Wavefront Proxy Installation!")
+
+    return True
