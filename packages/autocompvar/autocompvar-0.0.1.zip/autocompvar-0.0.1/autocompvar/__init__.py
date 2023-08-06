5b1966fecaaf396efe6090918450bbac9c39e10f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:38 +0000

    Add package autocompvar/0.0.1/autocompvar-0.0.1.zip

---
+++ packages/autocompvar/autocompvar-0.0.1.zip/autocompvar-0.0.1/autocompvar/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+"""
+
+__version__ = "0.0.1"
+__short_description__ = "make your data importable"
+__license__ = "MIT"
+__author__ = "Sanhe Hu"
