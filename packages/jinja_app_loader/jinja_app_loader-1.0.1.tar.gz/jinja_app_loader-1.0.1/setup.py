76a791546fbdb2bdd8f46a14220ffacf622bf38b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:35 +0000

    Add package jinja_app_loader/1.0.1/jinja_app_loader-1.0.1.tar.gz

---
+++ packages/jinja_app_loader/jinja_app_loader-1.0.1.tar.gz/jinja_app_loader-1.0.1/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import os
+import sys
+from setuptools import setup
+
+try:
+    import pandoc
+except ImportError:
+    pandoc = None
+
+
+if sys.argv[-1] == 'publish':
+    assert pandoc, 'You have to do: pip install pyandoc'
+    os.system('python setup.py sdist upload')
+    sys.exit(0)
+
+
+def get_description():
+    with open('README.md') as f:
+        desc = f.read()
+    short = desc.split('===\n')[1].strip().split('\n')[0]
+    if pandoc:
+        doc = pandoc.Document()
+        doc.markdown = desc.encode('utf-8')
+        desc = doc.rst.decode('utf-8')
+    return short, desc
+
+
+name = 'jinja_app_loader'
+short, desc = get_description()
+setup(
+    version='1.0.1',
+    name=name,
+    url='https://github.com/imbolc/{}'.format(name),
+    description=short,
+    long_description=desc,
+
+    py_modules=[name],
+
+    author='Imbolc',
+    author_email='imbolc@imbolc.name',
+    license='ISC',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+)
