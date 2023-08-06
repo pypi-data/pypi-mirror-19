5661d93bc75ab6d6151923da018373276eae9586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:05 +0000

    Add package mote-prk/0.2/mote-prk-0.2.tar.gz

---
+++ packages/mote-prk/mote-prk-0.2.tar.gz/mote-prk-0.2/CHANGELOG.rst
@@ -0,0 +1,17 @@
+Changelog
+=========
+
+next
+----
+#. Make it possible to override `iframe.html` for each project.
+#. New UI theme.
+#. Django template and RESTful API.
+
+0.1.1
+-----
+#. Inevitable rename to avoid name collision.
+
+0.1
+----
+#. First release.
+
---
+++ packages/mote-prk/mote-prk-0.2.tar.gz/mote-prk-0.2/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+
+setup(
+    name="mote-prk",
+    version="0.2",
+    description="Mote - the pattern library framework.",
+    long_description = open("README.rst", "r").read() + open("AUTHORS.rst", "r").read() + open("CHANGELOG.rst", "r").read(),
+    author="Praekelt Consulting",
+    author_email="dev@praekelt.com",
+    license="BSD",
+    packages=find_packages(),
+    zip_safe=True,
+    install_requires=[
+        "django",
+        "beautifulsoup4",
+        "ujson",
+        "six",
+        "djangorestframework-jwt",
+        "xmltodict"
+    ],
+    tests_require=[
+        "tox",
+    ],
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+    include_package_data=True
+)
