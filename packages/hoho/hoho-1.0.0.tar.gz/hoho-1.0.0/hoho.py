9e9217568bed326630aab3cb705b5de1af9a7922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:46 +0000

    Add package hoho/1.0.0/hoho-1.0.0.tar.gz

---
+++ packages/hoho/hoho-1.0.0.tar.gz/hoho-1.0.0/hoho.py
@@ -0,0 +1,5 @@
+
+
+
+def hoho():
+    print "Hello hoho!"
---
+++ packages/hoho/hoho-1.0.0.tar.gz/hoho-1.0.0/setup.py
@@ -0,0 +1,14 @@
+
+
+
+from distutils.core import setup
+
+setup(
+    name = 'hoho',
+    version = '1.0.0',
+    py_modules = ['hoho'],
+    author = 'Daniel Kim',
+    author_email = 'elastic7327@gmail.com',
+    url ='http://blog.talchosun.net',
+    description = 'PyPI uploading test!',
+)
