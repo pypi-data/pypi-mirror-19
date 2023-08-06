d13e53a93faaa25ddc44a6c201dc522afba3dd21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:33 +0000

    Add package weles/0.3.3/weles-0.3.3.tar.gz

---
+++ packages/weles/weles-0.3.3.tar.gz/weles-0.3.3/weles/sklSVC.py
@@ -0,0 +1,11 @@
+# Adapter for sklearn
+from Dataset import *
+from Classifier import *
+from sklAdapter import *
+
+from sklearn import svm
+
+class sklSVC(sklAdapter):
+    def __init__(self, dataset, configuration):
+        Classifier.__init__(self,dataset)
+        self.clf = svm.SVC()
