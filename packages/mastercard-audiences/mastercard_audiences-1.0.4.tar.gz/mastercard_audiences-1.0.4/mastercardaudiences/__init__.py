9fe1bfec7b3a933505a6f4171ccc0b569f34d8e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:25 +0000

    Add package mastercard-audiences/1.0.4/mastercard_audiences-1.0.4.tar.gz

---
+++ packages/mastercard-audiences/mastercard_audiences-1.0.4.tar.gz/mastercard_audiences-1.0.4/mastercardaudiences/__init__.py
@@ -0,0 +1,8 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardaudiences.resourceconfig import *
+
+
+# import apis into api package
+from mastercardaudiences.audiencessegment import *
