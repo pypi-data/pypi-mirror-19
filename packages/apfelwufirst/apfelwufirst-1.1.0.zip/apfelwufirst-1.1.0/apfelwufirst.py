1ad4faa26a9b18d4a670a26e5cf5e98b3e8d24a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:34 +0000

    Add package apfelwufirst/1.1.0/apfelwufirst-1.1.0.zip

---
+++ packages/apfelwufirst/apfelwufirst-1.1.0.zip/apfelwufirst-1.1.0/apfelwufirst.py
@@ -0,0 +1,13 @@
+"""这是“nester.py”模块，提供了一个名为print_lol()的函数，这个函数
+的作用是打印列表，其中有可能包含嵌套列表"""
+def print_lol(the_list,level):
+	"""这个函数取一个位置参数，名为“the_list”，这可以是任何python列表。
+	所指定的列表中的每个数据项会输出到屏幕上，各数据项各占一行.
+	第二个参数用来在遇到嵌套列表时插入制表符。"""
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_lol(each_item,level+1)
+		else:
+			for tab_stop in range(level):
+				print("\t",end=")
+			print(each_item)
