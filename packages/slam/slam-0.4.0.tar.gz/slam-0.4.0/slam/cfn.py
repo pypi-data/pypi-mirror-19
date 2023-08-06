bdd96c2eaef08d508995c867db1687f652e232c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:02 +0000

    Add package slam/0.4.0/slam-0.4.0.tar.gz

---
+++ packages/slam/slam-0.4.0.tar.gz/slam-0.4.0/slam/cfn.py
@@ -0,0 +1,100 @@
+import collections
+import json
+
+from . import plugins
+
+
+def _get_cfn_parameters(config):
+    params = collections.OrderedDict()
+    params['LambdaS3Bucket'] = {
+        'Type': 'String',
+        'Description': 'The S3 bucket where the lambda zip file is stored.'
+    }
+    params['LambdaS3Key'] = {
+        'Type': 'String',
+        'Description': 'The S3 key of the lambda zip file.'
+    }
+    for stage in config['stage_environments'].keys():
+        params[stage.title() + 'Version'] = {
+            'Type': 'String',
+            'Description': ('The lambda version number associated with the {} '
+                            'stage.').format(stage),
+            'Default': '$LATEST'
+        }
+    return params
+
+
+def _get_cfn_resources(config):
+    res = collections.OrderedDict()
+    res['FunctionExecutionRole'] = {
+        'Type': 'AWS::IAM::Role',
+        'Properties': {
+            'AssumeRolePolicyDocument': {
+                'Version': '2012-10-17',
+                'Statement': [
+                    {
+                        'Effect': 'Allow',
+                        'Principal': {
+                            'Service': ['lambda.amazonaws.com']
+                        },
+                        'Action': 'sts:AssumeRole'
+                    }
+                ]
+            },
+            'ManagedPolicyArns': [
+                ('arn:aws:iam::aws:policy/service-role/'
+                 'AWSLambdaBasicExecutionRole')
+            ],
+            'Policies': []
+        }
+    }
+    res['Function'] = {
+        'Type': 'AWS::Lambda::Function',
+        'DependsOn': 'FunctionExecutionRole',
+        'Properties': {
+            'Code': {
+                'S3Bucket': {'Ref': 'LambdaS3Bucket'},
+                'S3Key': {'Ref': 'LambdaS3Key'}
+            },
+            'Role': {'Fn::GetAtt': ['FunctionExecutionRole', 'Arn']},
+            'Timeout': config['aws'].get('lambda_timeout', 10),
+            'MemorySize': config['aws'].get('lambda_memory', 128),
+            'Handler': 'handler.lambda_handler',
+            'Runtime': 'python2.7'
+        }
+    }
+    for stage in config['stage_environments'].keys():
+        res[stage.title() + 'FunctionAlias'] = {
+            'Type': 'AWS::Lambda::Alias',
+            'Properties': {
+                'Name': stage,
+                'FunctionName': {'Ref': 'Function'},
+                'FunctionVersion': {'Ref': stage.title() + 'Version'}
+            }
+        }
+    return res
+
+
+def _get_cfn_outputs(config):
+    outputs = collections.OrderedDict()
+    outputs['FunctionArn'] = {
+        'Value': {'Fn::GetAtt': ['Function', 'Arn']}
+    }
+    return outputs
+
+
+def get_cfn_template(config, pretty=False):
+    tpl = collections.OrderedDict(
+        [
+            ('AWSTemplateFormatVersion', '2010-09-09'),
+            ('Parameters', _get_cfn_parameters(config)),
+            ('Resources', _get_cfn_resources(config)),
+            ('Outputs', _get_cfn_outputs(config))
+        ]
+    )
+    for name, plugin in plugins.items():
+        if name in config and hasattr(plugin, 'cfn_template'):
+            tpl = plugin.cfn_template(config, tpl)
+    if pretty:
+        return json.dumps(tpl, indent=4, separators=(',', ': '))
+    return json.dumps(tpl)
