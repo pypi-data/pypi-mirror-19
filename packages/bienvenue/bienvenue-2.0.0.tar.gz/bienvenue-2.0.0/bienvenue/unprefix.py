3c4631e5775dbcf2a8994731c8b104747fe7745a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:36 +0000

    Add package bienvenue/2.0.0/bienvenue-2.0.0.tar.gz

---
+++ packages/bienvenue/bienvenue-2.0.0.tar.gz/bienvenue-2.0.0/bienvenue/unprefix.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import, print_function, unicode_literals
+
+
+def unprefix(prefix, d, all=False):
+    """
+    Returns a new dict by removing ``prefix`` from keys.
+    If ``all`` is ``False`` (default) then drops keys without the prefix,
+    otherwise keeping them.
+    """
+    d1 = dict(d) if all else {}
+    d1.update((k[len(prefix):], v) for k, v in d.items() if k.startswith(prefix))
+    return d1
