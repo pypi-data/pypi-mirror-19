ed5eb316996defd50d0b68867b51e89c80e7955f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:17 +0000

    Add package SocialCrawler/0.3.2.1/SocialCrawler-0.3.2.1-py3-none-any.whl

---
+++ packages/SocialCrawler/SocialCrawler-0.3.2.1-py3-none-any.whl/SocialCrawler/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""SocialCrawler.
+
+This modules import all classes from SocialCrawler package.
+
+ (c) J.Wirlino and A.Adativa
+ Date Sept 1th 2016
+ https://github.com/JosielWirlino/SocialCrawler
+
+"""
+
+
+from . import HistoricalCollector
+from . import ExtractorData
+from . import NewExtractorData
+from . import HeadLess
+from . import HackFoursquare
+from . import HTTPResponseError
+# from  . import ExtractorData
+
+__version__ = "0.3.2.1"
+
+__all__ = [	
+			"HistoricalCollector",
+			"ExtractorData",
+			"HackFoursquare",
+			"NewExtractorData",
+			"HeadLess",
+			"HTTPResponseError"
+		]
