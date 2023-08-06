a01c16c2957170ff5ca72c66b49fb0a37ed1b976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:46 +0000

    Add package getpassecho/1.1/getpassecho-1.1.tar.gz

---
+++ packages/getpassecho/getpassecho-1.1.tar.gz/getpassecho-1.1/getpassecho/__init__.py
@@ -0,0 +1,54 @@
+import sys
+import os
+import sys
+import termios
+import fcntl
+
+
+def getch():
+    fd = sys.stdin.fileno()
+
+    oldterm = termios.tcgetattr(fd)
+    newattr = termios.tcgetattr(fd)
+    newattr[3] = newattr[3] & ~termios.ICANON & ~termios.ECHO
+    termios.tcsetattr(fd, termios.TCSANOW, newattr)
+
+    oldflags = fcntl.fcntl(fd, fcntl.F_GETFL)
+    fcntl.fcntl(fd, fcntl.F_SETFL, oldflags | os.O_NONBLOCK)
+
+    try:
+        while 1:
+            try:
+                c = sys.stdin.read(1)
+                break
+            except IOError:
+                pass
+    finally:
+        termios.tcsetattr(fd, termios.TCSAFLUSH, oldterm)
+        fcntl.fcntl(fd, fcntl.F_SETFL, oldflags)
+    return c
+
+
+def getpass(prompt=None, stream=None, echo=True, echo_char='*'):
+    if stream is None:
+        stream = sys.stdout
+    if prompt is None:
+        prompt = 'Password: '
+
+    stream.write(prompt)
+    passwd = ''
+    c = None
+    while True:
+        c = getch()
+        if c == '\n':
+            break
+        elif c == '\x7f':
+            if passwd:
+                passwd = passwd[:-1]
+                if echo:
+                    sys.stdout.write('\b \b')
+        else:
+            passwd += c
+            if echo:
+                sys.stdout.write(echo_char)
+    return passwd
---
+++ packages/getpassecho/getpassecho-1.1.tar.gz/getpassecho-1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except ImportError:
+    long_description = open('README.md').read()
+
+setup(
+    name='getpassecho',
+    author='Kurt Spindler',
+    author_email='kespindler@gmail.com',
+    version='1.1',
+    description='getpass with an echo char',
+    long_description=long_description,
+    packages=['getpassecho'],
+)
+
