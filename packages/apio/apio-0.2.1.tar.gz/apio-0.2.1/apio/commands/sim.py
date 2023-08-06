b591599ba89743f6ca63d4ad8b756b654de009df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:44 +0000

    Add package apio/0.2.1/apio-0.2.1.tar.gz

---
+++ packages/apio/apio-0.2.1.tar.gz/apio-0.2.1/apio/commands/sim.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# -- This file is part of the Apio project
+# -- (C) 2016 FPGAwars
+# -- Author Jesús Arroyo
+# -- Licence GPLv2
+
+import click
+
+from apio.managers.scons import SCons
+
+# Python3 compat
+try:
+    unicode = str
+except NameError:  # pragma: no cover
+    pass
+
+
+@click.command('sim')
+@click.pass_context
+@click.option('-p', '--project-dir', type=unicode, metavar='path',
+              help='Set the target directory for the project.')
+def cli(ctx, project_dir):
+    """Launch the verilog simulation."""
+
+    exit_code = SCons(project_dir).sim()
+    ctx.exit(exit_code)
