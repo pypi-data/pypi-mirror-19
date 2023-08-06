bf6e9677fbca07bd4a85362957824b8b8e87499d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:36 +0000

    Add package resumable-urlretrieve/0.1.4/resumable-urlretrieve-0.1.4.tar.gz

---
+++ packages/resumable-urlretrieve/resumable-urlretrieve-0.1.4.tar.gz/resumable-urlretrieve-0.1.4/setup.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+from setuptools import setup # type: ignore
+
+setup(
+    name='resumable-urlretrieve',
+    version='0.1.4',
+    description='Small library to fetch files over HTTP and resuming their download',
+    author='Dario Bertini',
+    author_email='berdario+pypi@gmail.com',
+    url='https://github.com/berdario/resumable-urlretrieve',
+    license='MIT License',
+    packages=['resumable'],
+    install_requires=['requests'],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP']
+)
