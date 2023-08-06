b41243b5c9f444cf6c837f3164545b0dd3dac65b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:04 +0000

    Add package winCurses/0.1.2.1/winCurses-0.1.2.1.tar.gz

---
+++ packages/winCurses/winCurses-0.1.2.1.tar.gz/winCurses-0.1.2.1/winCurses/WTitle.py
@@ -0,0 +1,23 @@
+from .Win import *
+
+class WTitle(Win):
+    def __init__(self, *args, **kwargs):
+        Win.__init__(self, *args, **kwargs)
+        self.title = "Azaza"
+
+    def set_title(self, t, refresh=True):
+        self.title = t
+        if refresh:
+            self.refresh_title()
+
+    def resize_window(self):
+        Win.resize_window(self)
+        self.refresh_title()
+
+    def refresh_title(self, update=True):
+        self.clear()
+
+        y_offset = int(self._get_dim().y / 2)
+        x_offset = int((self._get_dim().x - len(self.title)) / 2) if len(self.title) < self.get_content_dim().x else 1
+        self.window.addstr(y_offset, x_offset, self.title[:self.get_content_dim().x])
+        Win.refresh(self)
