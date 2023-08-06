72266bba2de26d20c7f6e3491f905ad754aab77d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:38 +0000

    Add package webstoremgr/0.1/webstoremgr-0.1.zip

---
+++ packages/webstoremgr/webstoremgr-0.1.zip/webstoremgr-0.1/webstore_manager/logging_helper.py
@@ -0,0 +1,50 @@
+import logging
+import os
+
+import appdirs
+
+# log_formatter = logging.Formatter("%(asctime)s: %(levelname)s: %(message)s")
+log_formatter = logging.Formatter(
+    "%(asctime)s [%(filename)-15.15s] [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+
+loggers = []
+
+log_dir = appdirs.user_log_dir("webstore_manager", "melkamar")
+os.makedirs(log_dir, exist_ok=True)
+log_file = os.path.join(log_dir, "log")
+
+
+def init_logging():
+    logging.basicConfig(level=logging.DEBUG)
+
+    # Set logging format for requests
+    requests_log = logging.getLogger("requests.packages.urllib3")
+    requests_log.setLevel(logging.WARN)
+    requests_log.propagate = False
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(log_formatter)
+    requests_log.addHandler(console_handler)
+
+
+init_logging()
+
+
+def set_level(level):
+    for logger in loggers:
+        logger.setLevel(level)
+
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.propagate = False
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(log_formatter)
+    logger.addHandler(console_handler)
+
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setFormatter(log_formatter)
+    logger.addHandler(file_handler)
+
+    loggers.append(logger)
+    return logger
