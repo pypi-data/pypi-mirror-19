cd60b1762e4bb3277a7470c60a59c2d844734d7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:31 +0000

    Add package pa/0.0.2.dev0/pa-0.0.2.dev0.tar.gz

---
+++ packages/pa/pa-0.0.2.dev0.tar.gz/pa-0.0.2.dev0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='pa',
+    version='0.0.2.dev',
+    description='Paper Arxiv: A command line based academic paper management tool.',
+    url='',
+    author='',
+    author_email='',
+    license='',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+    ],
+    keywords='paper arxiv manage',
+    install_requires=[
+          'click',
+    ],
+    entry_points={
+          'console_scripts': [
+              'pa = paper_arxiv.pa:main'
+          ]
+    },
+)
