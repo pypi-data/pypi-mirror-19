1b955474a1ec2d6d81a35c134d7ccc16da668523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:22 +0000

    Add package gdsync/0.0.15/gdsync-0.0.15.tar.gz

---
+++ packages/gdsync/gdsync-0.0.15.tar.gz/gdsync-0.0.15/README.rst
@@ -0,0 +1,11 @@
+gdsync - Google Drive Synchronizer
+====================================
+
+gdsync synchronizes a Google Drive folder to another Google Drive folder.
+
+How to use
+===========
+
+::
+
+  gdsync SOURCE DESTINATION
---
+++ packages/gdsync/gdsync-0.0.15.tar.gz/gdsync-0.0.15/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+version = '0.0.15'
+
+setup(
+    name='gdsync',
+    version=version,
+    description='Google Drive Synchronizer',
+    long_description=open("README.rst").read(),
+    author='Masato Bito',
+    author_email='bito_m@uuum.jp',
+    url='https://github.com/uuum/gdsync',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    install_requires=['click', 'google-api-python-client', 'python-dateutil', 'simplejson'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': ['gdsync=gdsync.cli.cli:main']
+    },
+)
