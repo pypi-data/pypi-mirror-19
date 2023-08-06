c78a5e3883d792da8b10e222ffa7829d6f76fb3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:30 +0000

    Add package pincode_map/1.0/pincode_map-1.0.tar.gz

---
+++ packages/pincode_map/pincode_map-1.0.tar.gz/pincode_map-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pincode_map',
+      version='1.0',
+      description='Validate and Mapping Pincode to Locality, City and State ',
+      url='https://github.com/manti/pincode_map',
+      author='Manti HS',
+      author_email='manti.rvce@gmail.com',
+      license='MIT',
+      zip_safe=False,
+      packages=['pincode_map'],
+      package_dir={'pincode_map': 'pincode_map/'},
+    )
