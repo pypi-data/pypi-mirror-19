5c63d8ede1b1375139338a771b8ffa17b1d4dbac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:59 +0000

    Add package ska/1.6.6/ska-1.6.6.tar.gz

---
+++ packages/ska/ska-1.6.6.tar.gz/ska-1.6.6/src/ska/contrib/django/ska/apps.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+
+__title__ = 'ska.contrib.django.ska.apps'
+__author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
+__copyright__ = '2013-2016 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('Config',)
+
+try:
+    from django.apps import AppConfig
+
+    class Config(AppConfig):
+        """Config."""
+
+        name = 'ska.contrib.django.ska'
+        # label = 'ska_contrib_django_ska'
+        label = 'ska'
+
+except ImportError:
+    pass
