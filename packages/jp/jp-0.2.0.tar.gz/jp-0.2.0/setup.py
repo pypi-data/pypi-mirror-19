adc320ba8a4c0621747dad934c96fdbb7af17922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:24 +0000

    Add package jp/0.2.0/jp-0.2.0.tar.gz

---
+++ packages/jp/jp-0.2.0.tar.gz/jp-0.2.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+install_requires = ["click", "tabulate", "pygments"]
+
+def setup_package():
+    setup(
+        name='jp',
+        description='json print query filter',
+        long_description=long_description,
+        url='https://github.com/hoffmann/jp',
+        license='BSD',
+        classifiers=[
+          'Programming Language :: Python',
+        ],
+        keywords='',
+        packages=find_packages(exclude=['docs', 'tests*']),
+        setup_requires=['setuptools_scm'],
+        use_scm_version=True,
+        zip_safe=False,
+        include_package_data=True,
+        author='Peter Hoffmann',
+        install_requires=install_requires,
+        author_email='ph@peter-hoffmann.com',
+        entry_points={
+          'console_scripts': [
+              'jp = jp:cli'
+          ]
+       },
+    )
+
+if __name__ == "__main__":
+    setup_package()
