fc137e83b939bd1c72aafe780d7618db63556281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:07 +0000

    Add package tapioca-iugu/0.1.4/tapioca-iugu-0.1.4.tar.gz

---
+++ packages/tapioca-iugu/tapioca-iugu-0.1.4.tar.gz/tapioca-iugu-0.1.4/tapioca_iugu/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+__author__ = 'Olist Developers'
+__email__ = 'developers@olist.com'
+__version__ = '0.1.4'
+
+
+from .tapioca_iugu import Iugu  # noqa
---
+++ packages/tapioca-iugu/tapioca-iugu-0.1.4.tar.gz/tapioca-iugu-0.1.4/tapioca_iugu/tapioca_iugu.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+
+from tapioca import (
+    TapiocaAdapter, generate_wrapper_from_adapter, JSONAdapterMixin)
+
+from requests.auth import HTTPBasicAuth
+
+
+from .resource_mapping import RESOURCE_MAPPING
+
+
+class IuguClientAdapter(JSONAdapterMixin, TapiocaAdapter):
+    api_root = 'https://api.iugu.com/v1/'
+    resource_mapping = RESOURCE_MAPPING
+
+    def get_request_kwargs(self, api_params, *args, **kwargs):
+        params = super(IuguClientAdapter, self).get_request_kwargs(
+            api_params, *args, **kwargs)
+
+        params['auth'] = HTTPBasicAuth(api_params.get('user'), '')
+
+        return params
+
+    def get_iterator_list(self, response_data):
+        return response_data.get('items', response_data)
+
+    def get_iterator_next_request_kwargs(self, iterator_request_kwargs,
+                                         response_data, response):
+        if 'params' not in iterator_request_kwargs:
+            iterator_request_kwargs['params'] = {}
+
+        start = iterator_request_kwargs['params'].get('start', 0)
+        total_items = response_data.get('totalItems')
+        items = len(response_data.get('items'))
+        if total_items >= items + start:
+            iterator_request_kwargs['params']['start'] = start + items
+            return iterator_request_kwargs
+
+
+Iugu = generate_wrapper_from_adapter(IuguClientAdapter)
