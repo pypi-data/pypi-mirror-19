3ec08c5f90edd945bdd34683e55138486b4fa7dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:22 +0000

    Add package LibRadar/2.0b2/LibRadar-2.0b2-py2-none-any.whl

---
+++ packages/LibRadar/LibRadar-2.0b2-py2-none-any.whl/LibRadar/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+    LibRadar
+    ~~~~~
+
+    An automatically detecting tool for Android third-party libraries.
+    Ideas about this tool is written as a publisher, you could view the [PDF]
+    (http://dl.acm.org/citation.cfm?id=2889178) here.
+
+    :copyright: (c) 2016 by Zachary Marv.
+
+"""
+
+
+
+# Step 1 jar 反编译，得到api dict [tech: jad]
+# Step 2 hash feature Tree [tech: md5]
+# Step 3 [tech: Redis]
+# Step 3.1 [tech: 众数机制]
+# Step 3.2 [tech: 分布式、并行]
+# Step 3.3 Redis 导出
+# Step 4 Detector
+# Step 5 online [tech: node.js]
+
+# -*- coding: utf-8 -*-
+
+__version__ = '2.0b2'
+__author__ = 'Zachary Marv (马子昂)'
