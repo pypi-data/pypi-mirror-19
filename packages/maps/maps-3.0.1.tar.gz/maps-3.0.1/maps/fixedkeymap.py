bcace0d36fd2e4e53fb1d2f026e15dd7182e0ada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:47 +0000

    Add package maps/3.0.1/maps-3.0.1.tar.gz

---
+++ packages/maps/maps-3.0.1.tar.gz/maps-3.0.1/maps/fixedkeymap.py
@@ -0,0 +1,26 @@
+import collections
+import collections.abc
+
+class FixedKeyMap(collections.abc.MutableMapping):
+    def __init__(self, *args, **kwargs):
+        self._data = collections.OrderedDict(*args, **kwargs)
+
+    def __getitem__(self, name):
+        return self._data[name]
+
+    def __iter__(self):
+        return iter(self._data)
+
+    def __len__(self):
+        return len(self._data)
+
+    def __delitem__(self, name):
+        raise TypeError(
+            f"'{type(self).__name__}' object does not support item deletion")
+
+    def __setitem__(self, name, value):
+        if name not in self._data:
+            raise TypeError(
+                f"'{type(self).__name__}' object does not support new item assignment")
+        self._data[name] = value
+
---
+++ packages/maps/maps-3.0.1.tar.gz/maps-3.0.1/maps/namedfixedkeymap.py
@@ -0,0 +1,50 @@
+import abc
+import maps.utils as utils
+from maps.fixedkeymap import FixedKeyMap
+
+class NamedFixedKeyMapMeta(abc.ABCMeta):
+    def __new__(cls, typename, fields=[]):
+        fields = tuple(fields)
+        # validate names
+        for name in (typename,) + fields:
+            utils._validate_name(name)
+        utils._validate_fields(fields)
+
+        cls._fields = fields
+
+        def getattr__(self, name):
+            try:
+                return self._data[name]
+            except KeyError:
+                raise AttributeError(f"'{typename}' object has no attribute {name!r}")
+
+        def setattr__(self, name, value):
+            if name.startswith('_'):
+                super(type(self), self).__setattr__(name, value)
+            elif name in self._data:
+                self._data[name] = value
+            else:
+                raise TypeError(
+                    f"'{typename}' object does not support new attribute assignment")
+
+        def repr__(self): # pragma: no cover
+            kwargs = ', '.join(f'{key}={value!r}' for key, value in self.items())
+            return f'{typename}({kwargs})'
+
+        methods = {
+            '__getattr__': getattr__,
+            '__repr__': repr__,
+            '__setattr__': setattr__}
+
+        # handle custom __init__
+        template = '\n'.join([
+            'def __init__(self, {args}):',
+            '    super(type(self), self).__init__({kwargs})'])
+        args = ', '.join(fields)
+        kwargs = ', '.join([f'{i}={i}' for i in fields])
+        exec(template.format(args=args, kwargs=kwargs), methods)
+
+        return super().__new__(cls, typename, (FixedKeyMap,), methods)
+
+    def __init__(cls, name, fields=[]):
+        super().__init__(cls)
