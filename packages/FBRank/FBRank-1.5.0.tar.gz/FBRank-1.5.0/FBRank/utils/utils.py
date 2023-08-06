ab4f470554a87bf68349b0e61f042f3ae28f1898
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:38 +0000

    Add package FBRank/1.5.0/FBRank-1.5.0.tar.gz

---
+++ packages/FBRank/FBRank-1.5.0.tar.gz/FBRank-1.5.0/FBRank/utils/utils.py
@@ -0,0 +1,82 @@
+# -*- coding:utf-8 -*-
+import sys
+from functools import wraps
+
+# from .exceptions import NotSupprotedYetException
+# 如果在这里的话就会造成循环引入
+"""
+File "/home/hzcortex/FBRank/FBRank/parse/League.py", line 13, in <module>
+    from FBRank.utils.exceptions import IllegalArgumentException, NotSupprotedYetException
+  File "/home/hzcortex/FBRank/FBRank/utils/exceptions.py", line 2, in <module>
+    from .utils import github_url, connect_url
+  File "/home/hzcortex/FBRank/FBRank/utils/utils.py", line 5, in <module>
+    from .exceptions import NotSupprotedYetException
+ImportError: cannot import name 'NotSupprotedYetException'
+
+"""
+
+PY2 = sys.version[0] == '2'
+
+
+# decorator for check-name
+# For Club Check
+def check_before(attr='name'):
+    from .exceptions import NotSupprotedYetException
+    def wrapped_func(func):
+        @wraps(func)
+        def wrapped(self, *args, **kwargs):
+            # print(func.__class__.__name__)
+            if (getattr(self, attr).lower() not in club_transformat.keys()):
+                raise NotSupprotedYetException
+            return func(self, *args, **kwargs)
+
+        return wrapped
+
+    return wrapped_func
+
+
+# relationship
+
+
+league_transformat = {
+    ('英超', 'Premier League', 'Premiere League', '英国', '英格兰'): 'EPL',
+    ('德甲', 'Bundesliga', 'Bunde', '德国'): 'Bunde',
+    ('西甲', 'Liga BBva', '西班牙'): 'La Liga',
+    ('意甲', 'Serie A', '意大利'): 'Serie A'
+}
+
+league_configure = {
+    'EPL': {
+        'rank_url': 'http://soccer.hupu.com/table/England.html',
+        'news_url': 'http://sports.sina.com.cn/g/premierleague/'
+    },
+    'Bunde': {
+        'rank_url': 'http://soccer.hupu.com/table/Germany.html',
+        'news_url': 'http://sports.sina.com.cn/g/bundesliga/'
+    },
+    'La Liga': {
+        'rank_url': 'http://soccer.hupu.com/table/Spain.html',
+        'news_url': 'http://sports.sina.com.cn/g/laliga/'
+    },
+    'Serie A': {
+        'rank_url': 'http://soccer.hupu.com/table/Italy.html',
+        'news_url': 'http://sports.sina.com.cn/g/seriea/'
+    }
+}
+
+club_transformat = {
+    ('manchester united', '曼联', '曼彻斯特联'): 'manutd'
+}
+
+club_url = {
+
+}
+
+football_url = {
+
+}
+
+connect_url = 'iamwanghz@gmail.com'
+github_url = 'https://github.com/FBRank'
+
+league_news_pattern = r'<h2><a class="f074" href="(.*?)".*?>(.*?)</a>'
