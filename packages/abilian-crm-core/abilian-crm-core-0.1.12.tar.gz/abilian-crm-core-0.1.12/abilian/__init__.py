34589ac726ac17ac51d0aea85f2e9af0af519a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:03 +0000

    Add package abilian-crm-core/0.1.12/abilian-crm-core-0.1.12.tar.gz

---
+++ packages/abilian-crm-core/abilian-crm-core-0.1.12.tar.gz/abilian-crm-core-0.1.12/abilian/__init__.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+#noinspection PyUnresolvedReferences
+from pkgutil import extend_path
+
+#noinspection PyUnboundLocalVariable
+__path__ = extend_path(__path__, __name__)
