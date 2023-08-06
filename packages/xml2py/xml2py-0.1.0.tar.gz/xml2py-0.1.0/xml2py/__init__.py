92b893a9874e52bba4e26f05154055e7f5429a8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:32 +0000

    Add package xml2py/0.1.0/xml2py-0.1.0.tar.gz

---
+++ packages/xml2py/xml2py-0.1.0.tar.gz/xml2py-0.1.0/xml2py/__init__.py
@@ -0,0 +1,50 @@
+"""
+xml2py
+"""
+import xml.etree.cElementTree
+from .xmltodict import XmlToDict
+from .dotdict import DotDict
+
+__all__ = [
+        'DotDict',
+        'XmlToDict',
+        'dict_load', 'dict_loads',
+        'dotdict_load', 'dotdict_loads',
+        'to_dotdict',
+        ]
+
+def dict_load(xml_filename, **kwargs):
+    """ converts xml file to dict.
+    ``xml_filename`` -  the name of the xml file or the file object
+                        to convert.
+    ``kwargs`` - keyword arguments are passed to XmlToDict.__init__
+    """
+    tree = xml.etree.cElementTree.parse(xml_filename)
+    root = tree.getroot()
+    return XmlToDict(**kwargs).element_to_dict(root)
+
+def dict_loads(xml_string, **kwargs):
+    """ converts ``xml_string`` to dict.
+    ``kwargs`` - keyword arguments are passed to XmlToDict.__init__
+    """
+    root = xml.etree.cElementTree.fromstring(xml_string)
+    return XmlToDict(**kwargs).element_to_dict(root)
+
+def dotdict_load(xml_filename, **kwargs):
+    """ converts xml file to DotDict instance.
+    ``xml_filename`` -  the name of the xml file or the file object
+                        to convert.
+    ``kwargs`` - keyword arguments are passed to dict_load
+    """
+    d = dict_load(xml_filename, **kwargs)
+    return DotDict(d)
+
+def dotdict_loads(xml_string, **kwargs):
+    """ converts ``xml_string`` to DotDict instance.
+    ``kwargs`` - keyword arguments are passed to dict_load
+    """
+    d = dict_loads(xml_string, **kwargs)
+    return DotDict(d)
+
+to_dotdict = DotDict.to_dotdict # pylint: disable=invalid-name
+
