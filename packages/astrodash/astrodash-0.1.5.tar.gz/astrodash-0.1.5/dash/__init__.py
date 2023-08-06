e1aa6a8158068317ce67311adb8c89231800485e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:43 +0000

    Add package astrodash/0.1.5/astrodash-0.1.5.tar.gz

---
+++ packages/astrodash/astrodash-0.1.5.tar.gz/astrodash-0.1.5/dash/__init__.py
@@ -0,0 +1,32 @@
+import sys
+import os
+import pickle
+mainDirectory = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(mainDirectory, ".."))
+sys.path.insert(0, os.path.join(mainDirectory, "../GUI"))
+from restore_model import *
+from main import *
+from PyQt4 import QtGui
+
+from classify import Classify
+
+
+def main():
+    classification = Classify(filenames=['../test_spectrum_file.dat',
+                                         '../test_spectrum_file.dat'],
+                              redshifts=[0.34, 0.13])
+    print classification.list_best_matches()
+
+
+def run_gui():
+    app = QtGui.QApplication(sys.argv)
+    form = MainApp()
+    form.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
+    run_gui()
+
+
