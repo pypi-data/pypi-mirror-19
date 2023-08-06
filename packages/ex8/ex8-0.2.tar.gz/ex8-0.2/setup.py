1637a72cf4c8c4b072474c8416887e5f835de52a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:39 +0000

    Add package ex8/0.2/ex8-0.2.tar.gz

---
+++ packages/ex8/ex8-0.2.tar.gz/ex8-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ex8',
+      version='0.2',
+      description='ex8 is going to take over',
+      url='http://github.com/ex8',
+      author='Matt M',
+      author_email='matt@puroxy.org',
+      packages=['ex8'],
+      zip_safe=False)
