2151fe5a167c0afaeedafefbee7c4e6573a0e315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:20 +0000

    Add package DPP_Nester/1.0.0/DPP_Nester-1.0.0.zip

---
+++ packages/DPP_Nester/DPP_Nester-1.0.0.zip/DPP_Nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name = 'DPP_Nester',
+        version = '1.0.0',
+        py_modules = ['DPP_Nester'],
+        author = 'DPP',
+        author_email = 'hfpython@headfirstlabs.com',
+        url = 'http://www.headfirstlabs.com',
+        description = 'A simple printer of nested lists',
+    )
