02b41977c30d4bed24d8632ea9b79319d2dc4675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:51 +0000

    Add package prob140/0.1.5.1/prob140-0.1.5.1.tar.gz

---
+++ packages/prob140/prob140-0.1.5.1.tar.gz/prob140-0.1.5.1/prob140/multi_variable.py
@@ -0,0 +1,269 @@
+from datascience import *
+import numpy as np
+import pandas as pd
+
+def conditional(array):
+    return array/sum(array[0:-1])
+
+class JointDistribution(pd.DataFrame):
+
+    def marginal_of_Y(self):
+        """
+        Find the marginal distribution of the second variable
+
+        Examples
+        --------
+        >>> dist1 = Table().domain([0,1],[2,3]).probability([0.1, 0.2, 0.3, 0.4]).toJoint()
+        >>> dist1.marginal_of_Y()
+             X=0  X=1  Sum: Marginal of Y
+        Y=3  0.2  0.4                 0.6
+        Y=2  0.1  0.3                 0.4
+
+        """
+        copy = JointDistribution(self, copy=True)
+        copy['Sum: Marginal of {0}'.format(self._Y_column_label)] = copy.sum(axis=1)
+        return copy
+
+    def marginal_of_X(self):
+        """
+        Finds the marginal distribution of the first variable
+
+        Examples
+        --------
+        >>> dist1 = Table().domain([0,1],[2,3]).probability([0.1, 0.2, 0.3, 0.4]).toJoint()
+        >>> dist1.marginal_of_X()
+                            X=0  X=1
+        Y=3                 0.2  0.4
+        Y=2                 0.1  0.3
+        Sum: Marginal of X  0.3  0.7
+        """
+        copy = JointDistribution(self, copy=True)
+        copy.loc['Sum: Marginal of {0}'.format(self._X_column_label)] = copy.sum(axis=0)
+        return copy
+
+    def marginal(self, label):
+        """
+        Returns the marginal distribution of label
+
+        Parameters
+        ----------
+        label : String
+        The label of the variable of which we want to find the marginal distribution
+
+        Examples
+        --------
+        >>> dist2 = Table().domain("Coin1",['H','T'],"Coin2", ['H','T']).probability(np.array([0.24, 0.36, 0.16, 0.24])).toJoint()
+        >>> dist2.marginal("Coin1")
+                                Coin1=H  Coin1=T
+        Coin2=T                    0.36     0.24
+        Coin2=H                    0.24     0.16
+        Sum: Marginal of Coin1     0.60     0.40
+        >>> dist2.marginal("Coin2")
+                 Coin1=H  Coin1=T  Sum: Marginal of Coin2
+        Coin2=T     0.36     0.24                     0.6
+        Coin2=H     0.24     0.16                     0.4
+        """
+        copy = JointDistribution(self, copy=True)
+
+        if label == self._X_column_label:
+            copy.loc['Sum: Marginal of {0}'.format(self._X_column_label)] = copy.sum(axis=0)
+        elif label == self._Y_column_label:
+            copy['Sum: Marginal of {0}'.format(self._Y_column_label)] = copy.sum(axis=1)
+        else:
+            raise AssertionError("Label doesn't correspond with existing variable name")
+
+        return copy
+
+    def both_marginals(self):
+        """
+        Finds the marginal distribution of both variables
+
+        Examples
+        --------
+        >>> dist1 = Table().domain([0,1],[2,3]).probability([0.1, 0.2, 0.3, 0.4]).toJoint()
+        >>> dist1.both_marginals()
+                            X=0  X=1  Sum: Marginal of Y
+        Y=3                 0.2  0.4                 0.6
+        Y=2                 0.1  0.3                 0.4
+        Sum: Marginal of X  0.3  0.7                 1.0
+        """
+        copy = JointDistribution(self,copy=True)
+        copy['Sum: Marginal of {0}'.format(self._Y_column_label)] = copy.sum(axis=1)
+        copy.loc['Sum: Marginal of {0}'.format(self._X_column_label)] = copy.sum(axis=0)
+        return copy
+
+    def conditional_dist_X_given_Y(self):
+        """
+        Finds the conditional distribution of the first variable given the second variable
+
+        Examples
+        --------
+        >>> dist1 = Table().domain([0,1],[2,3]).probability([0.1, 0.2, 0.3, 0.4]).toJoint()
+        >>> dist1.conditional_dist_X_given_Y()
+                               X=0       X=1  Sum
+        Dist. of X | Y=3  0.333333  0.666667  1.0
+        Dist. of X | Y=2  0.250000  0.750000  1.0
+        Marginal of X     0.300000  0.700000  1.0
+
+        """
+        both = self.both_marginals()
+        
+        x = both.apply(conditional, axis=1)\
+            .rename(columns={'Sum: Marginal of {0}'.format(self._Y_column_label) :'Sum'})
+        
+        indices = both.index
+        new = make_array()
+        for i in np.arange(len(indices)-1):
+            new_index = 'Dist. of {0} | '.format(self._X_column_label)+indices[i]
+            new = np.append(new, new_index)
+        new = np.append(new, 'Marginal of {0}'.format(self._X_column_label))
+        
+        return x.set_index(new)
+
+    def conditional_dist_Y_given_X(self):
+        """
+        Finds the conditional distribution of the second variable given the first variable
+
+        Examples
+        --------
+        >>> dist1 = Table().domain([0,1],[2,3]).probability([0.1, 0.2, 0.3, 0.4]).toJoint()
+        >>> dist1.conditional_dist_Y_given_X()
+             Dist. of Y | X=0  Dist. of Y | X=1  Marginal of Y
+        Y=3          0.666667          0.571429            0.6
+        Y=2          0.333333          0.428571            0.4
+        Sum          1.000000          1.000000            1.0
+        """
+        both = self.both_marginals()
+        
+        indices = both.index
+        new = np.append(both.index[0:-1], 'Sum')
+        y = both.apply(conditional, axis=0).set_index(new)
+        
+        column_names = y.columns
+        new = make_array()
+        for i in np.arange(len(column_names)-1):
+            new_name = 'Dist. of {0} | '.format(self._Y_column_label)+column_names[i]
+            new = np.append(new, new_name)
+        new = np.append(new, 'Marginal of {0}'.format(self._Y_column_label))
+        
+        y.columns = new
+        return y
+
+    def conditional_dist_given(self, label):
+        """
+        Given the random variable label, finds the conditional distribution of the other variable
+
+        Parameters
+        ----------
+        label : String
+            given variable
+
+        Examples
+        --------
+        >>> dist2 = Table().domain("Coin1",['H','T'],"Coin2", ['H','T']).probability(np.array([0.24, 0.36, 0.16, 0.24])).toJoint()
+        >>> dist2.conditional_dist_given("Coin2")
+                                  Coin1=H  Coin1=T  Sum
+        Dist. of Coin1 | Coin2=T      0.6      0.4  1.0
+        Dist. of Coin1 | Coin2=H      0.6      0.4  1.0
+        Marginal of Coin1             0.6      0.4  1.0
+
+        """
+
+        if label == self._X_column_label:
+            return self.conditional_dist_Y_given_X()
+        elif label == self._Y_column_label:
+            return self.conditional_dist_X_given_Y()
+        else:
+            raise AssertionError("Label doesn't correspond with existing variable name")
+
+import itertools as it
+
+def multi_domain(table,*args):
+
+    if isinstance(args[0], str):
+        assert len(args) % 2 == 0, "Must alternate between name and values"
+        var_names = [args[2 * i] for i in range(len(args) // 2)]
+        values = [args[2 * i + 1] for i in range(len(args) // 2)]
+        var_values = list(zip(*it.product(*values)))
+    else:
+        var_names = [chr(ord('X')+i) for i in range(len(args))]
+        var_values = list(zip(*it.product(*args)))
+
+    new_table = table.copy()
+    for column_name,column_value in reversed(list(zip(var_names,var_values))):
+        new_table = new_table.with_column(column_name,column_value)
+        new_table.move_to_start(column_name)
+
+    return new_table
+
+def toJoint(table,X_column_label=None,Y_column_label=None,probability_column_label=None):
+    """
+    Converts a table of probabilities associated with two variables into a JointDistribution object
+
+    Parameters
+    ----------
+    table : Table
+        You can either call pass in a Table directly or call the toJoint() method of that Table. See examples
+    X_column_label (optional) : String
+        Label for the first variable. Defaults to the same label as that of first variable of Table
+    Y_column_label (optional) : String
+        Label for the second variable. Defaults to the same label as that of second variable of Table
+    probability_column_label (optional) : String
+        Label for probabilities
+
+    Returns
+    -------
+    JointDistribution
+        A JointDistribution object
+
+    Examples
+    --------
+    >>> dist1 = Table().domain([0,1],[2,3])
+    >>> dist1['Probability'] = make_array(0.1, 0.2, 0.3, 0.4)
+    >>> dist1.toJoint()
+         X=0  X=1
+    Y=3  0.2  0.4
+    Y=2  0.1  0.3
+    >>> dist2 = Table().domain("Coin1",['H','T'], "Coin2", ['H','T'])
+    >>> dist2['Probability'] = np.array([0.4*0.6, 0.6*0.6, 0.4*0.4, 0.6*0.4])
+    >>> dist2.toJoint()
+             Coin1=H  Coin1=T
+    Coin2=T     0.36     0.24
+    Coin2=H     0.24     0.16
+
+    """
+    assert table.num_columns >= 3, \
+        "You must have columns for your X variable, for your Y variable, and for your probabilities"
+    if X_column_label is None:
+        X_column_label = table.labels[0]
+    if Y_column_label is None:
+        Y_column_label = table.labels[1]
+    if probability_column_label is None:
+        probability_column_label = table.labels[table.num_columns-1]
+
+    assert np.allclose(sum(table[probability_column_label]),1), "Your probabilities don't sum to 1"
+    
+
+    x_possibilities = sorted(set(table[X_column_label]))
+    y_possibilities = sorted(set(table[Y_column_label]),reverse=True)
+
+
+    xInd = table.column_index(X_column_label)
+    yInd = table.column_index(Y_column_label)
+    pInd = table.column_index(probability_column_label)
+    
+    data = {poss: [0]*len(y_possibilities) for poss in x_possibilities}
+    
+    for row in table.rows:
+        data[row[xInd]][y_possibilities.index(row[yInd])] += row[pInd]
+
+
+    realData = {'%s=%s'%(X_column_label,str(poss)):value for poss,value in data.items()}
+    index = ['%s=%s'%(Y_column_label,poss) for poss in y_possibilities]
+    joint_dist = JointDistribution(realData,index=index)
+
+    joint_dist._X_column_label = X_column_label
+    joint_dist._Y_column_label = Y_column_label
+
+    return joint_dist
+    
---
+++ packages/prob140/prob140-0.1.5.1.tar.gz/prob140-0.1.5.1/prob140/single_variable.py
@@ -0,0 +1,451 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import collections
+import itertools
+import abc
+import sys
+import warnings
+
+import matplotlib
+
+from datascience import *
+
+inf = math.inf
+rgb = matplotlib.colors.colorConverter.to_rgb
+
+def check_valid_probability_table(table):
+    assert table.num_columns == 2, "In order to run a Prob140 function, your table must have 2 columns: (1) a Values column (2) a Probability column"
+    assert all(table.column(1)>=0), "Probabilities must be non-negative"
+
+
+def prob_event(self, x):
+    """
+    Finds the probability of an event x
+
+    Parameters
+    ----------
+    x : float or Iterable
+        An event represented either as a specific value in the domain or a
+        subset of the domain
+
+    Returns
+    -------
+    float
+        Probability of the event
+
+    Examples
+    --------
+
+    >>> dist = Table().domain([1,2,3,4]).probability([1/4,1/4,1/4,1/4])
+    >>> dist.prob_event(2)
+    0.25
+
+    >>> dist.prob_event([2,3])
+    0.5
+
+    >>> dist.prob_event(np.arange(1,5))
+    1.0
+
+    """
+    check_valid_probability_table(self)
+    if isinstance(x, collections.Iterable):
+        return sum(self.prob_event(k) for k in x)
+    else:
+        domain = self.column(0)
+        prob = self.column(1)
+        return sum(prob[np.where(domain == x)])
+
+
+def event(self, x):
+    """
+    Shows the probability that distribution takes on value x or list of
+    values x.
+
+    Parameters
+    ----------
+    x : float or Iterable
+        An event represented either as a specific value in the domain or a
+        subset of the domain
+
+    Returns
+    -------
+    Table
+        Shows the probabilities of each value in the event
+
+
+    Examples
+    --------
+    >>> dist = Table().domain([1,2,3,4]).probability([1/4,1/4,1/4,1/4])
+    >>> dist.event(2)
+    Domain | Probability
+    2      | 0.25
+
+    >>> dist.event([2,3])
+    Domain | Probability
+    2      | 0.25
+    3      | 0.25
+    """
+    check_valid_probability_table(self)
+
+    if not isinstance(x, collections.Iterable):
+        x = [x]    
+    probabilities = [self.prob_event(k) for k in x]
+    return Table().with_columns('Outcome',x,'Probability',probabilities)
+
+
+
+def _bin(dist, width=1, start=None):
+    """
+    Helper function that bins a distribution for plotting
+
+    Parameters
+    ----------
+    dist : Table
+        Distribution that needs to be binned
+    width (optional) : float
+        Width of each bin. Default is 1
+    start (optional) : float
+        Where to start first bin. Defaults to the minimum value of the domain
+
+    Returns
+    -------
+    (new_domain, new_prob)
+        Domain values of the new bins and the associated probabilities
+
+    Examples
+    --------
+    >>> x = Table().domain([0, 0.5, 1]).probability([1/3, 1/3, 1/3])
+    >>> _bin(x)
+    (array([ 0.,  1.]), array([ 0.66666667,  0.33333333]))
+    >>> _bin(x, width=0.5)
+    (array([ 0. ,  0.5,  1. ]), array([ 0.33333333,  0.33333333,  0.33333333]))
+
+    """
+    domain = dist.column(0)
+    prob = dist.column(1)
+
+    if start == None:
+        start = min(domain)
+
+    num_bins = math.ceil((max(domain) - start) / width) + 1
+
+    new_domain = np.arange(start, start + width * num_bins, width)
+
+    new_prob = np.zeros(num_bins)
+
+    for i in range(len(domain)):
+        index = math.ceil((domain[i] - start - width/2) / width)
+        new_prob[index] += prob[i]
+
+    return new_domain, new_prob
+
+def Plot(dist, width=1, mask=[], event=[], **vargs):
+    """
+    Plots the histogram for a single distribution
+
+    Parameters
+    ----------
+    dist : Table
+        A 2-column table representing a probability distribution
+    width (optional) : float
+        Width of the intervals (default: 1)
+    mask (optional) : boolean array or list of boolean arrays
+        Colors the parts of the histogram associated with each mask (
+        default: no mask)
+    vargs
+        See pyplot's additional optional arguments
+
+    """
+
+    options = {"width" : width, "lw" : 0, "alpha" : 0.7, "align" : "center"}
+    options.update(vargs)
+
+    check_valid_probability_table(dist)
+
+    domain_label = dist.labels[0]
+    dist = dist.sort(domain_label)
+    domain, prob = _bin(dist, width)
+
+    if len(event) != 0:
+
+        domain = set(dist.column(0))
+
+        def prob(x):
+            return np.array([dist.prob_event(a) for a in list(x)])
+
+        if isinstance(event[0], collections.Iterable):
+            # If event is a list of lists
+
+            colors = list(itertools.islice(itertools.cycle(dist.chart_colors), len(event) + 1))
+            for i in range(len(event)):
+                plt.bar(event[i], prob(event[i]) * 100, color=colors[i], **options)
+                domain -= set(event[i])
+
+            domain = np.array(list(domain))
+            plt.bar(domain, prob(domain) * 100, color=colors[-1], **options)
+
+        else:
+            # If event is just a list
+
+            plt.bar(event, prob(event) * 100, color="gold", **options)
+            domain = np.array(list(set(dist.column(0)) - set(event)))
+            plt.bar(domain, prob(domain) * 100, color="darkblue", **options)
+
+
+    elif len(mask) == 0:
+        # no mask or event
+        plt.bar(domain, prob, color="darkblue", **options)
+        #dist.hist(counts=domain_label, bins=np.arange(start - width / 2, end + width, width), **vargs)
+    else:
+        if isinstance(mask[0], collections.Iterable):
+            # If mask is a list of lists
+
+            colors = list(itertools.islice(itertools.cycle(dist.chart_colors), len(mask)))
+            for i in range(len(mask)):
+                plt.bar(domain[mask[i]], prob[mask[i]] * 100, color=colors[i], **options)
+
+        else:
+            # If mask is just a list
+
+            plt.bar(domain[mask], prob[mask] * 100, color="darkblue", **options)
+            plt.bar(domain[np.logical_not(mask)], prob[np.logical_not(mask)] * 100, color="gold", **options)
+
+
+    plt.xlabel(domain_label)
+    plt.ylabel("Percent per unit")
+
+    mindistance = 0.9 * max(min([dist.column(0)[i] - dist.column(0)[i - 1] for i in range(1, dist.num_rows)]),1)
+
+    plt.xlim((min(dist.column(0)) - mindistance - width / 2, max(dist.column(0))
+              + mindistance + width / 2))
+
+
+def Plots(*labels_and_dists, width=1, **vargs):
+    """
+    Overlays histograms for multiply probability distributions together.
+
+    Parameters
+    ----------
+    labels_and_dists : Even number of alternations between Strings and
+    Tables
+        Each distribution must have a label associated with it
+    width (optional) : float
+        Width of the intervals (default: 1)
+    vargs
+        See pyplot's documentation
+
+
+    Examples
+    --------
+    >>> dist1 = Table().domain([1,2,3,4]).probability([1/4,1/4,1/4,1/4])
+    >>> dist2 = Table().domain([3,4,5,6]).probability([1/2,1/8,1/8,1/4])
+    >>> Plots("Distribution1", dist1, "Distribution2", dist2)
+    <histogram with dist1 and dist2>
+
+    """
+
+    assert len(labels_and_dists) % 2 == 0, 'Even length sequence required'
+    options = Table.default_options.copy()
+    options.update(vargs)
+
+    i = 0
+
+    domain = set()
+    while i < len(labels_and_dists):
+        label = labels_and_dists[i]
+        dist = labels_and_dists[i + 1]
+        check_valid_probability_table(dist)
+
+        domain = domain.union(dist.column(0))
+        i += 2
+
+    domain = np.array(list(domain))
+
+    i = 0
+    distributions = ["Value", domain]
+    while i < len(labels_and_dists):
+        distributions.append(labels_and_dists[i])
+        dist = labels_and_dists[i + 1]
+        probability = np.vectorize(lambda x: prob_event(dist,x), otypes=[np.float])(domain)
+        distributions.append(probability)
+        i += 2
+
+    result = Table().with_columns(*distributions)
+
+    result.chart_colors = Table.chart_colors
+
+    start = min(domain)
+    end = max(domain)
+    end = (end // width + 1) * width
+    result.hist(counts="Value",
+                bins=np.arange(start - width / 2, end + width, width),
+                **vargs)
+    domain = np.sort(domain)
+
+    mindistance = 0.9 * max(min([domain[i] - domain[i - 1] for i in range(1, len(domain))]), 1)
+
+    plt.xlim((min(domain) - mindistance - width / 2, max(domain) + mindistance +
+              width / 2))
+
+    plt.xlabel("Value")
+
+
+def single_domain(self, values):
+    """
+    Assigns domain values to a single-variable distribution
+
+    Parameters
+    ----------
+    values : List or Array
+        Values to put into the domain
+
+    Returns
+    -------
+    Table
+        Table with those domain values in its first column
+
+    Examples
+    --------
+
+    >>> Table().domain([1,2,3])
+    Value
+    1
+    2
+    3
+    """
+    table = self.with_column('Value', values)
+    table.move_to_start('Value')
+    return table
+
+
+def probability_function(self, pfunc):
+    """
+    Assigns probabilities to a Distribution via a probability
+    function. The probability function is applied to each value of the
+    domain. Must have domain values in the first column first.
+
+    Parameters
+    ----------
+    pfunc : univariate function
+        Probability function of the distribution
+
+    Returns
+    -------
+    Table
+        Table with those probabilities in its second column
+
+    """
+    domain_names = self.labels
+    values = np.array(self.apply(pfunc, domain_names)).astype(float)
+    if any(values < 0):
+        warnings.warn("Probability cannot be negative")
+    return self.with_column('Probability', values)
+
+
+def probability(self, values):
+    """
+    Assigns probabilities to domain values.
+
+    Parameters
+    ----------
+    values : List or Array
+        Values that must correspond to the domain in the same order
+
+    Returns
+    -------
+    Table
+        A proability distribution with those probabilities
+    """
+    if any(np.array(values) < 0):
+        warnings.warn("Probability cannot be negative")
+    return self.with_column('Probability', values)
+
+
+def normalized(self):
+    """
+    Returns the distribution by making the proabilities sum to 1
+
+    Returns
+    -------
+    Table
+        A distribution with the probabilities normalized
+
+    Examples
+    --------
+    >>> Table().domain([1,2,3]).probability([1,1,1])
+    Value | Probability
+    1     | 1
+    2     | 1
+    3     | 1
+    >>> Table().domain([1,2,3]).probability([1,1,1]).normalized()
+    Value | Probability
+    1     | 0.333333
+    2     | 0.333333
+    3     | 0.333333
+    """
+    column_label = self.labels[-1]
+    return self.with_column(column_label,self.column(column_label)/sum(self.column(column_label)))
+
+
+def expected_value(self):
+    """
+    Finds expected value of distribution
+
+    Returns
+    -------
+    float
+        Expected value
+
+    """
+    check_valid_probability_table(self)
+    self = normalized(self)
+    ev = 0
+    for domain, probability in self.rows:
+        ev += domain * probability
+    return ev
+
+
+def variance(self):
+    """
+    Finds variance of distribution
+
+    Returns
+    -------
+    float
+        Variance
+    """
+    check_valid_probability_table(self)
+
+    self = normalized(self)
+    var = 0
+    ev = self.expected_value()
+    for domain, probability in self.rows:
+        var += (domain - ev) ** 2 * probability
+    return var
+
+
+def sd(self):
+    """
+    Finds standard deviation of Distribution
+
+    Returns
+    -------
+    float
+        Standard Deviation
+    """
+    return math.sqrt(self.variance())
+
+# Brighter colors than the default Table class
+chart_colors = (
+    rgb("darkblue"),
+    rgb("gold"),
+    (106/256, 166/256, 53/256), # vivid green
+    (234/256, 77/256, 108/256), # rose garden
+    rgb("brown"),
+    (240/256, 127/256, 80/256), # vivid orange
+    (53/256, 148/256, 216/256), # vivid blue
+    (122/256, 55/256, 139/256), # purple
+    rgb("black"),
+    rgb("red"),
+)
