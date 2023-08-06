3eebe5fe2a87b924042e765a7adee2e6d177223e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:45 +0000

    Add package smasherstats/1.3.6/smasherstats-1.3.6.tar.gz

---
+++ packages/smasherstats/smasherstats-1.3.6.tar.gz/smasherstats-1.3.6/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+    name = 'smasherstats',
+    packages = ['smasherstats'],
+    version = '1.3.6',
+    description = 'Library for stats gathering on smash players using web scrapers',
+    author = 'Michael Krasnitski',
+    author_email = 'michael.krasnitski@gmail.com',
+    license='MIT',
+    url = 'https://github.com/PolarBearITS/smasherstats',
+    download_url = 'https://github.com/PolarBearITS/smasherstats/tarball/1.3.6',
+    install_requires = [
+        'requests',
+        'beautifulsoup4',
+        'pysmash'
+    ]
+)
