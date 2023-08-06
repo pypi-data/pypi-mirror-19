ea9758cc6b16f62a357811c2039c7d845e7ab778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:51 +0000

    Add package GrowingIOSDK/1.0.1/GrowingIOSDK-1.0.1.tar.gz

---
+++ packages/GrowingIOSDK/GrowingIOSDK-1.0.1.tar.gz/GrowingIOSDK-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='GrowingIOSDK',
+    version='1.0.1',
+    author='Damon Lin',
+    author_email='linshengsheng@growingio.com',
+    url='https://github.com/growingio/growingio-python-sdk',
+    license='LICENSE.txt',
+    packages=['growingio'],
+    description='This is the official Python SDK for GrowingIO.',
+    long_description=open('README.txt').read(),
+)
