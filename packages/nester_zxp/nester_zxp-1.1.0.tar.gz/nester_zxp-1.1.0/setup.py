5332c8f773afb0a70f87ecf3b0cfccf49e3e9f97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:32 +0000

    Add package nester_zxp/1.1.0/nester_zxp-1.1.0.tar.gz

---
+++ packages/nester_zxp/nester_zxp-1.1.0.tar.gz/nester_zxp-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nester_zxp',
+    version = '1.1.0',
+    py_modules = ['nester'],
+    author = 'skyfox',
+    author_email = 'skyfox@sina.com',
+    url = 'http://skyfox.ha.cmcc',
+    description = 'A simple printer of nested lists',
+)
