809df5f559ec2212c0e76465c9ab0ebd27ee667e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:55 +0000

    Add package github-sectory/1.1.2/github-sectory-1.1.2.tar.gz

---
+++ packages/github-sectory/github-sectory-1.1.2.tar.gz/github-sectory-1.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name='github-sectory',
+	version='1.1.2',
+	description='CLI for downloading sub-directory of any Github repository!',
+	url='https://github.com/amarlearning/Github-Sectory',
+	author='Amar Prakash Pandey (@amarlearning)',
+    author_email='amar.om1994@gmail.com',
+	license='MIT',
+	keywords='github repository download sub-directory',
+	install_requires=['clint'],
+	scripts=['github-sectory']
+)
