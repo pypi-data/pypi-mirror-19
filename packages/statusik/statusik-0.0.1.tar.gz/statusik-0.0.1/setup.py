4a48f6a90ebd43ba5b2203f0fedfbc130d7f054e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:56 +0000

    Add package statusik/0.0.1/statusik-0.0.1.tar.gz

---
+++ packages/statusik/statusik-0.0.1.tar.gz/statusik-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='statusik',
+    version='0.0.1',
+    description='Yet another tool for feeding i3bar ...',
+    url='https://bitbucket.org/cepe/statusik',
+    author='Paweł Chwała',
+    author_email='pawel.chwala@gmail.com',
+    license='MIT',
+    packages=['statusik'],
+    zip_safe=False
+)
