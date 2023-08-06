3f6b96d66c50c6e40588a523023cc2ffe376385f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:34 +0000

    Add package KissAnime-Downloader/0.1/KissAnime%20Downloader-0.1.tar.gz

---
+++ packages/KissAnime-Downloader/KissAnime%20Downloader-0.1.tar.gz/KissAnime Downloader-0.1/setup.py
@@ -0,0 +1,17 @@
+"""
+Setup file for KissAnime_DL
+"""
+from setuptools import setup
+
+setup(
+    name='KissAnime Downloader',
+    version='0.1',
+    scripts=['kissanime_dl.py'],
+    description='Download anime episodes from KissAnime',
+    author='Abdullah Saleem',
+    author_email='a.saleem2993@gmail.com',
+    url='https://github.com/Abdullah2993/KissAnime-Downloader',
+    license='MIT',
+    keywords=['kissanime', 'download', 'downloader', 'anime', 'video'],
+    install_requires=['beautifulsoup4', 'cfscrape', 'wget']
+)
