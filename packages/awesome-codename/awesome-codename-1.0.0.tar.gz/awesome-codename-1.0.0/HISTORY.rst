419d1d3ba5c88f0d9da8e1cace8fbd5d3b1a413e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:38 +0000

    Add package awesome-codename/1.0.0/awesome-codename-1.0.0.tar.gz

---
+++ packages/awesome-codename/awesome-codename-1.0.0.tar.gz/awesome-codename-1.0.0/HISTORY.rst
@@ -0,0 +1,10 @@
+=======
+History
+=======
+
+.. comment:: bumpversion marker
+
+1.0.0 (2017-02-08)
+------------------
+
+* First release on PyPI.
---
+++ packages/awesome-codename/awesome-codename-1.0.0.tar.gz/awesome-codename-1.0.0/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from pkgutil import walk_packages
+from setuptools import setup, find_packages
+
+
+def read_file(filename):
+    with open(filename) as fp:
+        return fp.read().strip()
+
+
+def read_rst(filename):
+    # Ignore unsupported directives by pypi.
+    return '\n'.join(line for line in read_file(filename).splitlines()
+                     if not line.startswith('.. comment::'))
+
+
+def read_requirements(filename):
+    return [line.strip() for line in read_file(filename).splitlines()
+            if not line.startswith('#')]
+
+
+setup_attrs = dict(
+    name='awesome-codename',
+    version=read_file('VERSION'),
+    description="Awesome Codename Generator",
+    long_description=read_rst('README.rst') + '\n\n' + read_rst('HISTORY.rst'),
+    author="Rolando (Max) Espinoza",
+    author_email='rolando@rmax.io',
+    url='https://github.com/rolando/awesome-codename',
+    packages=list(find_packages('src')),
+    package_dir={'': 'src'},
+    package_data={'awesome_codename': ['data/*']},
+    entry_points={'console_scripts': ['awesome-codename = awesome_codename.__main__:main']},
+    install_requires=read_requirements('requirements/install.txt'),
+    include_package_data=True,
+    license="AGPL",
+    keywords='awesome-codename',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
+
+
+if __name__ == "__main__":
+    setup(**setup_attrs)
