6feb71731aa674c599589fe0c1fd8103fb998c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:53 +0000

    Add package asr_evaluation/0.2.5/asr_evaluation-0.2.5.tar.gz

---
+++ packages/asr_evaluation/asr_evaluation-0.2.5.tar.gz/asr_evaluation-0.2.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='asr_evaluation',
+    version='0.2.5',
+    author='Ben Lambert',
+    author_email='ben@benjaminlambert.com',
+    packages=['asr_evaluation'],
+    license='LICENSE.txt',
+    description='Evaluating ASR (automatic speech recognition) hypotheses, i.e. computing word error rate.',
+    install_requires=['edit_distance', 'termcolor', 'matplotlib'],
+    test_suite='test.test.TestASREvaluation',
+    entry_points={
+        'console_scripts': [
+            'evaluate.py = asr_evaluation.__main__:main'
+        ]
+    }
+)
