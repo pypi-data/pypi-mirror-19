5c28c97459735668bda164a7cba0f25e328cdaca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:02 +0000

    Add package pysharperratio/0.1.10/pysharperratio-0.1.10.tar.gz

---
+++ packages/pysharperratio/pysharperratio-0.1.10.tar.gz/pysharperratio-0.1.10/setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='pysharperratio',
+    version='0.01.10',
+    description='A moment-free estimator of the Sharpe (signal-to-noise) ratio.',
+    long_description=readme(),
+    author='Amir Sani',
+    url='http://www.amirsani.com',
+    author_email='reachme@amirsani.com',
+    download_url='https://github.com/amirsani/pySharpeRratio',
+    packages=['pysharperratio'],
+    zip_safe=False
+)
