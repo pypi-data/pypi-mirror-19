aa1e2d94dfed21b91d42e3e2a69aaadada021304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:09 +0000

    Add package thegame/1.5/thegame-1.5.tar.gz

---
+++ packages/thegame/thegame-1.5.tar.gz/thegame-1.5/thegame/__init__.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+__version__ = '1.5'
+
+__author__ = "Adrien Brunet"
+__contact__ = "plz-dont@thks.bye"
+__homepage__ = "https://github.com/adrienbrunet/thegame"
+__license__ = "AGPL"
+
+
+print("You just lost.")
