412a350be795e4f530477bd01520512ad0f8a5cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:58 +0000

    Add package astrodash/0.2.22/astrodash-0.2.22-py2.py3-none-any.whl

---
+++ packages/astrodash/astrodash-0.2.22-py2.py3-none-any.whl/dash/download_data_files.py
@@ -0,0 +1,29 @@
+import urllib
+import os
+
+scriptDirectory = os.path.dirname(os.path.abspath(__file__))
+print(scriptDirectory)
+
+
+def download_file(filename, urlpath, printStatus):
+    dataFilename = os.path.join(scriptDirectory, filename)
+    if not os.path.isfile(dataFilename):
+        print(printStatus)
+        dataFileDownload = urllib.URLopener()
+        dataFileDownload.retrieve(urlpath, dataFilename)
+        print(dataFilename)
+
+
+def download_all_files():
+    saveFilenames = ['type_age_atRedshiftZero.npz', 'model_trainedAtZeroZ.ckpt', 'training_params.pickle', 'templates.npz']
+
+    urlpaths = ["https://raw.githubusercontent.com/daniel-muthukrishna/DASH/master/dash/model_trainedAtZeroZ.ckpt",
+                "https://raw.githubusercontent.com/daniel-muthukrishna/DASH/master/dash/type_age_atRedshiftZero.npz",
+                "https://raw.githubusercontent.com/daniel-muthukrishna/DASH/master/dash/type_age_atRedshiftZero.npz",
+                "https://raw.githubusercontent.com/daniel-muthukrishna/DASH/master/dash/training_params.pickle"]
+
+    printStatuses = ["Downloading Trained Model...", "Downloading Training Data files...",
+                     "Downloading Model Parameters File...", "Downloading Template Data files..."]
+
+    for i in range(len(urlpaths)):
+        download_file(saveFilenames[i], urlpaths[i], printStatuses[i])
