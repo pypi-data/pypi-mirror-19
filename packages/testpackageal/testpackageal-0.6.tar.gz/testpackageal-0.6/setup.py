9fd2a584e41d10ac512ec6f2a3eacdafc5fdb693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:40 +0000

    Add package testpackageal/0.6/testpackageal-0.6.tar.gz

---
+++ packages/testpackageal/testpackageal-0.6.tar.gz/testpackageal-0.6/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+  name = 'testpackageal',
+  packages = find_packages(), # this must be the same as the name above
+  version = '0.6',
+  description = 'This is my first package.',
+  author = 'Angel Lagunas',
+  author_email = 'angel.david.lagunas@gmail.com',
+  url = 'https://github.com/angellagunas/testpackageal', # use the URL to the github repo
+  download_url = 'https://github.com/angellagunas/testpackageal/tarball/0.6', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
