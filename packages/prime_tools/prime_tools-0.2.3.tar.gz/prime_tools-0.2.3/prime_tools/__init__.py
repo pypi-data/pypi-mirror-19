1d6890f522506464045a31c218dddb050af84cc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:27 +0000

    Add package prime_tools/0.2.3/prime_tools-0.2.3.tar.gz

---
+++ packages/prime_tools/prime_tools-0.2.3.tar.gz/prime_tools-0.2.3/prime_tools/__init__.py
@@ -0,0 +1,6 @@
+from .prime_factorization import list_prime_factors
+from .prime_factorization import list_prime_factors_long
+from .prime_factorization import largest_prime_factor
+
+from .prime_generation import n_primes
+from .prime_generation import nth_prime
