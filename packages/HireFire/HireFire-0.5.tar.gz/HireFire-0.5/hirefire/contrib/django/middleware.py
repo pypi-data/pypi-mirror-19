1127aecbc3ff872a8f123e22dcb44b371bfa8125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:46 +0000

    Add package HireFire/0.5/HireFire-0.5.tar.gz

---
+++ packages/HireFire/HireFire-0.5.tar.gz/HireFire-0.5/hirefire/contrib/django/middleware.py
@@ -0,0 +1,65 @@
+from __future__ import absolute_import
+import os
+import re
+
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.http import HttpResponse
+
+try:
+    # Django >= 1.10
+    from django.utils.deprecation import MiddlewareMixin
+except ImportError:
+    # Not required for Django <= 1.9, see:
+    # https://docs.djangoproject.com/en/1.10/topics/http/middleware/#upgrading-pre-django-1-10-style-middleware
+    MiddlewareMixin = object
+
+
+from hirefire.procs import load_procs, dump_procs, HIREFIRE_FOUND
+
+
+def setting(name, default=None):
+    return os.environ.get(name, getattr(settings, name, None)) or default
+
+
+TOKEN = setting('HIREFIRE_TOKEN', 'development')
+PROCS = setting('HIREFIRE_PROCS', [])
+
+if not PROCS:
+    raise ImproperlyConfigured('The HireFire Django middleware '
+                               'requires at least one proc defined '
+                               'in the HIREFIRE_PROCS setting.')
+
+
+class HireFireMiddleware(MiddlewareMixin):
+    """
+    The Django middleware that is hardwired to the URL paths
+    HireFire requires. Implements the test response and the
+    json response that contains the procs data.
+    """
+    test_path = re.compile(r'^/hirefire/test/?$')
+    info_path = re.compile(r'^/hirefire/%s/info/?$' % re.escape(TOKEN))
+    loaded_procs = load_procs(*PROCS)
+
+    def test(self, request):
+        """
+        Doesn't do much except telling the HireFire bot it's installed.
+        """
+        return HttpResponse(HIREFIRE_FOUND)
+
+    def info(self, request):
+        """
+        The heart of the app, returning a JSON ecoded list
+        of proc results.
+        """
+        payload = dump_procs(self.loaded_procs)
+        return HttpResponse(payload, content_type='application/json')
+
+    def process_request(self, request):
+        path = request.path
+
+        if self.test_path.match(path):
+            return self.test(request)
+
+        elif self.info_path.match(path):
+            return self.info(request)
---
+++ packages/HireFire/HireFire-0.5.tar.gz/HireFire-0.5/hirefire/procs/__init__.py
@@ -0,0 +1,197 @@
+import json
+from collections import OrderedDict
+
+from ..utils import import_attribute, TimeAwareJSONEncoder
+
+import six
+
+
+__all__ = ('loaded_procs', 'Proc', 'load_proc', 'load_procs', 'dump_procs')
+
+
+HIREFIRE_FOUND = 'HireFire Middleware Found!'
+
+
+class Procs(OrderedDict):
+    pass
+
+loaded_procs = Procs()
+
+
+def load_proc(obj):
+    if isinstance(obj, Proc):
+        return obj
+    elif isinstance(obj, six.string_types):
+        try:
+            proc = import_attribute(obj)
+        except ImportError as e:
+            raise ValueError('The proc %r could not be imported: %s' %
+                             (obj, e))
+        except AttributeError as e:
+            raise ValueError('The proc %r could not be found: %s' %
+                             (obj, e))
+        if not isinstance(proc, Proc):
+            proc = proc()
+        return proc
+    raise ValueError('The proc %r could not be loaded' % obj)
+
+
+def load_procs(*procs):
+    """
+    Given a list of dotted import paths or Proc subclasses
+    populates the procs list.
+
+    Example::
+
+        load_procs('mysite.procs.WorkerCeleryProc',
+                   'mysite.proc.ThumbnailsRQProc')
+        load_procs(worker_rq_proc)
+        load_procs('mysite.proc.worker_rq_proc')
+
+    """
+    for obj in procs:
+        proc = load_proc(obj)
+        if proc.name in loaded_procs:
+            raise ValueError('Given proc %r overlaps with '
+                             'another already loaded proc (%r)' %
+                             (proc, loaded_procs[proc.name]))
+        loaded_procs[proc.name] = proc
+    return loaded_procs
+
+
+def native_dump_procs(procs):
+    """
+    Given a list of loaded procs, dump the data for them into
+    a list of dictionaries in the form expected by HireFire,
+    ready to be encoded into JSON.
+    """
+    data = []
+    cache = {}
+    for name, proc in procs.items():
+        try:
+            quantity = proc.quantity(cache=cache)
+        except TypeError:
+            quantity = proc.quantity()
+
+        data.append({
+            'name': name,
+            'quantity': quantity or 0,
+        })
+    return data
+
+
+def dump_procs(procs):
+    """
+    Given a list of loaded procs dumps the data for them in
+    JSON format.
+    """
+    data = native_dump_procs(procs)
+    return json.dumps(data, cls=TimeAwareJSONEncoder, ensure_ascii=False)
+
+
+class Proc(object):
+    """
+    The base proc class. Use this to implement custom queues or
+    other behaviours, e.g.::
+
+        import mysite.sekrit
+        from hirefire import procs
+
+        class MyCustomProc(procs.Proc):
+            name = 'worker'
+            queues = ['default']
+
+            def quantity(self):
+                return sum([mysite.sekrit.count(queue)
+                            for queue in self.queues])
+
+    :param name: the name of the proc (required)
+    :param queues: list of queue names to check (required)
+    :type name: str
+    :type queues: str or list of str
+
+    """
+    #: The name of the proc
+    name = None
+
+    #: The list of queues to check
+    queues = []
+
+    def __init__(self, name=None, queues=None):
+        if name is not None:
+            self.name = name
+        if self.name is None:
+            raise ValueError('The proc %r requires a name '
+                             'attribute' % self)
+        if queues is not None:
+            self.queues = queues
+        if not isinstance(self.queues, (list, tuple)):
+            self.queues = (queues,)
+        if not self.queues:
+            raise ValueError('The proc %r requires at least '
+                             'one queue to check' % self)
+
+    def __str__(self):
+        return self.name or 'unnamed'
+
+    def __repr__(self):
+        cls = self.__class__
+        return ("<Proc %s: '%s.%s'>" %
+                (self.name, cls.__module__, cls.__name__))
+
+    def quantity(self, **kwargs):
+        """
+        Returns the aggregated number of tasks of the proc queues.
+
+        Needs to be implemented in a subclass.
+
+        ``kwargs`` must be captured even when not used, to allow for
+        future extensions.
+
+        The only kwarg currently implemented is ``cache``, which is
+        a dictionary made available for cross-proc caching. It is
+        empty when the first proc is processed.
+        """
+        raise NotImplementedError
+
+
+class ClientProc(Proc):
+    """
+    A special subclass of the :class:`~hirefire.procs.Proc` class
+    that instantiates a list of clients for each given queue, e.g.::
+
+        import mysite.sekrit
+        from hirefire import procs
+
+        class MyCustomProc(procs.ClientProc):
+            name = 'worker'
+            queues = ['default']
+
+            def client(self, queue):
+                return mysite.sekrit.Client(queue)
+
+            def quantity(self):
+                return sum([client.count(queue)
+                            for client in self.clients])
+
+    See the implementation of the :class:`~hirefire.procs.rq.RQProc`
+    class for an example.
+
+    """
+    def __init__(self, *args, **kwargs):
+        super(ClientProc, self).__init__(*args, **kwargs)
+        self.clients = []
+        for queue in self.queues:
+            client = self.client(queue)
+            if client is None:
+                continue
+            self.clients.append(client)
+
+    def client(self, queue, *args, **kwargs):
+        """
+        Returns a client instance for the given queue to be used
+        in the ``quantity`` method.
+
+        Needs to be implemented in a subclass.
+        """
+        raise NotImplementedError
