8cc457ee78c3bbafc8647b2e3a861e5968c7f960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:18 +0000

    Add package spyder-unittest/0.1b2/spyder_unittest-0.1b2-py2.py3-none-any.whl

---
+++ packages/spyder-unittest/spyder_unittest-0.1b2-py2.py3-none-any.whl/spyder_unittest/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+# Copyright (c) Spyder Project Developers
+#
+# Licensed under the terms of the MIT License
+# (see LICENSE.txt for details)
+# -----------------------------------------------------------------------------
+"""Spyder unitest plugin."""
+
+# Local imports
+from .unittestplugin import UnitTestPlugin as PLUGIN_CLASS
+
+__version__ = '0.1.b2'
+PLUGIN_CLASS
