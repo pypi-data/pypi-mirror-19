a835519a70933351dca4571a7dd7a476003780ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:46 +0000

    Add package ares-util/0.1.12/ares_util-0.1.12-py2.py3-none-any.whl

---
+++ packages/ares-util/ares_util-0.1.12-py2.py3-none-any.whl/ares_util/exceptions.py
@@ -0,0 +1,16 @@
+# !/usr/bin/python
+# coding=utf-8
+
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+
+
+class InvalidCompanyIDError(Exception):
+    pass
+
+
+class AresNoResponseError(Exception):
+    pass
+
+
+class AresConnectionError(Exception):
+    pass
