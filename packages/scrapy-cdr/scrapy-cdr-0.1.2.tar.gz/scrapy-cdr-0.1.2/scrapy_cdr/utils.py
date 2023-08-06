648052f592e612086225bcb5155610cfb0dd5102
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:22 +0000

    Add package scrapy-cdr/0.1.2/scrapy-cdr-0.1.2.tar.gz

---
+++ packages/scrapy-cdr/scrapy-cdr-0.1.2.tar.gz/scrapy-cdr-0.1.2/scrapy_cdr/utils.py
@@ -0,0 +1,37 @@
+import hashlib
+from datetime import datetime
+
+from .items import CDRItem
+
+
+def text_cdr_item(response, *, crawler_name, team_name, metadata=None, item_cls=CDRItem):
+    content_type = response.headers.get('content-type', b'')
+    return cdr_item(
+        response.url,
+        metadata=metadata,
+        crawler_name=crawler_name,
+        team_name=team_name,
+        content_type=content_type.decode('ascii', 'ignore'),
+        extracted_text=extract_text(response),
+        raw_content=response.text,
+        item_cls=item_cls,
+    )
+
+
+def cdr_item(url, *, crawler_name, team_name, metadata=None, item_cls=CDRItem, **extra):
+    metadata = metadata or {}
+    timestamp = int(datetime.utcnow().timestamp() * 1000)
+    return item_cls(
+        _id=hashlib.sha256('{}-{}'.format(url, timestamp).encode('utf-8'))
+            .hexdigest().upper(),
+        crawler=crawler_name,
+        extracted_metadata=metadata,
+        team=team_name,
+        timestamp=timestamp,
+        url=url,
+        version=2.0,
+        **extra)
+
+
+def extract_text(response):
+    return '\n'.join(response.xpath('//body').xpath('string()').extract())
---
+++ packages/scrapy-cdr/scrapy-cdr-0.1.2.tar.gz/scrapy-cdr-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name='scrapy-cdr',
+    version='0.1.2',
+    packages=['scrapy_cdr'],
+    install_requires=[
+        'scrapy',
+    ],
+    license='MIT license',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
