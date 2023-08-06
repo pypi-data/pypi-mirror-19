89abfba53d80fefd830fd185c0d308fd23bf1242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:47 +0000

    Add package lightmatchingengine/0.1.2/LightMatchingEngine-0.1.2.tar.gz

---
+++ packages/lightmatchingengine/LightMatchingEngine-0.1.2.tar.gz/LightMatchingEngine-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='LightMatchingEngine',
+    version='0.1.2',
+    author='Gavin Chan',
+    author_email='gavincyi@gmail.com',
+    packages=['lightmatchingengine'],
+    url='http://pypi.python.org/pypi/LightMatchingEngine/',
+    license='LICENSE.txt',
+    description='A light matching engine.'
+    )
