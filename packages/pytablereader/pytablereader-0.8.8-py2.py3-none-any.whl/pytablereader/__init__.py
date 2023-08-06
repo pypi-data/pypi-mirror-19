15f0575ae9017165651162a5a40b9745e4c10883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:04 +0000

    Add package pytablereader/0.8.8/pytablereader-0.8.8-py2.py3-none-any.whl

---
+++ packages/pytablereader/pytablereader-0.8.8-py2.py3-none-any.whl/pytablereader/__init__.py
@@ -0,0 +1,61 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import absolute_import
+
+from .error import (
+    ValidationError,
+    InvalidTableNameError,
+    InvalidHeaderNameError,
+    InvalidPathError,
+    InvalidFilePathError,
+    InvalidUrlError,
+    InvalidDataError,
+    EmptyDataError,
+    OpenError,
+    LoaderNotFoundError,
+    HTTPError
+)
+from .tabledata import TableData
+from ._logger import logger
+
+from .csv.core import (
+    CsvTableFileLoader,
+    CsvTableTextLoader
+)
+from .html.core import (
+    HtmlTableFileLoader,
+    HtmlTableTextLoader
+)
+from .json.core import (
+    JsonTableFileLoader,
+    JsonTableTextLoader
+)
+from .ltsv.core import (
+    LtsvTableFileLoader,
+    LtsvTableTextLoader
+)
+from .markdown.core import (
+    MarkdownTableFileLoader,
+    MarkdownTableTextLoader
+)
+from .mediawiki.core import (
+    MediaWikiTableFileLoader,
+    MediaWikiTableTextLoader
+)
+from .spreadsheet.excelloader import ExcelTableFileLoader
+from .loadermanager import (
+    TableFileLoader,
+    TableUrlLoader
+)
+from ._tabledata_sanitizer import (
+    TableDataSanitizer,
+    SQLiteTableDataSanitizer
+)
+from .tsv.core import (
+    TsvTableFileLoader,
+    TsvTableTextLoader
+)
