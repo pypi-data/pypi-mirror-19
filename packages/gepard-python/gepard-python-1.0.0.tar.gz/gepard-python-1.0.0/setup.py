07469c80a80909d577b32e5f6917273332c1830b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:23 +0000

    Add package gepard-python/1.0.0/gepard-python-1.0.0.tar.gz

---
+++ packages/gepard-python/gepard-python-1.0.0.tar.gz/gepard-python-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name="gepard-python",
+    version="1.0.0",
+    install_requires=[
+        "python-dateutil"
+    ],
+    author = 'Hans-Juergen Gessinger',
+    author_email = 'gessinger.hj@gmail.com',
+    url = 'https://github.com/paulgessinger/gepard-python',
+    py_modules=['gepard'],
+    entry_points= {
+    },
+    keywords = [
+    "distributed-applications",
+    "events",
+    "semaphores",
+    "locks",
+    "messages",
+    "JavaScript",
+    "Java",
+    "Python",
+    "message"
+    ],
+    description = "Python client for gepard:General purpose communications for distributed applications / Microservices / events, semaphores and messages for JavaScript, Java, Python and PHP",
+)
