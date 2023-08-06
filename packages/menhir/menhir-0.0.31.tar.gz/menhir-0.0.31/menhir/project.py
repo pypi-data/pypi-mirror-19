021a4c471bf3536a0bd45f3d8d4103c340a25ecb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:31 +0000

    Add package menhir/0.0.31/menhir-0.0.31.tar.gz

---
+++ packages/menhir/menhir-0.0.31.tar.gz/menhir-0.0.31/menhir/project.py
@@ -0,0 +1,49 @@
+try:
+    from functools import lru_cache
+except:
+    from functools32 import lru_cache
+
+
+def project_name():
+    """Return the name of the project in the current directory."""
+    from os import getcwd
+    return project_name_for_dir(getcwd())
+
+
+@lru_cache()
+def project_name_for_dir(path):
+    """Return the name of the project in the current directory."""
+    from os.path import abspath, basename, exists, join
+    fname = join(path, '.project-name')
+    if exists(fname):
+        with open(fname, 'r') as f:
+            return f.read().strip()
+    return basename(abspath(path))
+
+
+@lru_cache()
+def sha():
+    from menhir.gitutils import head_commit, sha
+    return sha(head_commit(repo()))
+
+
+@lru_cache()
+def branch():
+    from menhir.gitutils import branch
+    return branch(repo())
+
+
+@lru_cache()
+def repo():
+    from menhir.gitutils import repo
+    return repo()
+
+
+def image(info, path):
+    repo = info.get('config', {}).get('docker', {}).get('repository')
+    if repo:
+        return '%s/%s:%s' % (
+            repo,
+            project_name_for_dir(path),
+            sha()
+        )
