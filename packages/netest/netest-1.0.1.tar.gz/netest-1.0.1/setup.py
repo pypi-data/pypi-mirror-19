729b9b91efba2044fd7abea94aa87382bb3d5fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:02 +0000

    Add package netest/1.0.1/netest-1.0.1.tar.gz

---
+++ packages/netest/netest-1.0.1.tar.gz/netest-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+       name        = 'netest',
+       version     = '1.0.1',
+       py_modules  = ['netest'],
+#      author      = 'gong',
+#       author_email = 'fishgolang@gmail.com',
+#      url         = 'http://www.baidu.com',
+#       description = 'a test modules',
+     )
