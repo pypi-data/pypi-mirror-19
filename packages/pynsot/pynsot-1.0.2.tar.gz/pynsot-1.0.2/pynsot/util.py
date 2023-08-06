28300ce8f1495df61194eef5a8dd1dff34aab480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:32 +0000

    Add package pynsot/1.0.2/pynsot-1.0.2.tar.gz

---
+++ packages/pynsot/pynsot-1.0.2.tar.gz/pynsot-1.0.2/pynsot/util.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+"""
+Utilities and stuff.
+"""
+
+from __future__ import unicode_literals
+
+from .vendor import netaddr
+
+
+def get_result(response):
+    """
+    Get the desired result from an API response.
+
+    :param response: Requests API response object
+    """
+    try:
+        payload = response.json()
+    except AttributeError:
+        payload = response
+
+    if 'results' in payload:
+        return payload['results']
+
+    # Or just return the payload... (next-gen)
+    return payload
+
+
+def validate_cidr(cidr):
+    """
+    Return whether ``cidr`` is valid.
+
+    :param cidr:
+        IPv4/IPv6 address
+    """
+    try:
+        netaddr.IPNetwork(cidr)
+    except (TypeError, netaddr.AddrFormatError):
+        return False
+    else:
+        return True
+
+
+def dict_to_cidr(obj):
+    """
+    Take an dict of a Network object and return a cidr-formatted string.
+
+    :param obj:
+        Dict of an Network object
+    """
+    return '%s/%s' % (obj['network_address'], obj['prefix_length'])
