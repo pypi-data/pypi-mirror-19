9c31712c4a0601a58361c39b1dd4099e4c266ff2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:31 +0000

    Add package solar-correlation-map/1.42.0/solar-correlation-map-1.42.0.tar.gz

---
+++ packages/solar-correlation-map/solar-correlation-map-1.42.0.tar.gz/solar-correlation-map-1.42.0/solar_correlation_map/__main__.py
@@ -0,0 +1,17 @@
+import sys
+import solar_corr
+
+import warnings
+
+warnings.filterwarnings("ignore")
+
+if __name__ == "__main__":
+    # This needs a lot of work
+    if len(sys.argv) < 3:
+        print("python -m solar_correlation_map CSV_FILE_PATH SUN_VARIABLE [IMAGE_FILE_NAME]")
+        print("example: python -m solar_correlation_map jedi.csv JEDI jedi.png")
+        exit(1)
+
+    image_path = sys.argv[3] if len(sys.argv) > 3 else "solar.png"
+    solar_corr.main(sys.argv[1], sys.argv[2], image_path)
+
