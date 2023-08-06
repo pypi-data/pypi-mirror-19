6b63d3628bb4856ec7771d4080c96d641808da24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:49 +0000

    Add package junkmodule/2017.1.16.1427/junkmodule-2017.1.16.1427.tar.gz

---
+++ packages/junkmodule/junkmodule-2017.1.16.1427.tar.gz/junkmodule-2017.1.16.1427/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import setuptools
+
+def main():
+
+    setuptools.setup(
+        name                 = "junkmodule",
+        version              = "2017.01.16.1427",
+        description          = "junk testing module",
+        long_description     = long_description(),
+        url                  = "https://github.com/wdbm/junkmodule",
+        author               = "Will Breaden Madden",
+        author_email         = "wbm@protonmail.ch",
+        license              = "GPLv3",
+        py_modules           = [
+                               "junkmodule"
+                               ],
+        install_requires     = [
+                               "numpy",
+                               "tonescale"
+                               ],
+        scripts              = [
+                               "junkmodule_script.py"
+                               ],
+        entry_points         = """
+            [console_scripts]
+            junkmodule = junkmodule:junkmodule
+        """
+    )
+
+def long_description(
+    filename = "README.md"
+    ):
+
+    try:
+        try:
+            import pypandoc
+            long_description = pypandoc.convert_file(filename, "rst")
+        except ImportError:
+            long_description = open(filename).read()
+    except IOError:
+        long_description = ""
+    return long_description
+
+if __name__ == "__main__":
+    main()
