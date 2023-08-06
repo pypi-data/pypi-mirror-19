22b8053155308d6f6b105c992bbd888413933932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:22 +0000

    Add package django-admin-extensions/0.10.0/django-admin-extensions-0.10.0.tar.gz

---
+++ packages/django-admin-extensions/django-admin-extensions-0.10.0.tar.gz/django-admin-extensions-0.10.0/tests/app/settings.py
@@ -0,0 +1,34 @@
+DEBUG = True
+
+INSTALLED_APPS = [
+    'adminextensions',
+    'tests.app',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'test.sql',
+    }
+}
+
+SECRET_KEY = 'Not very secret'
+
+ROOT_URLCONF = 'tests.app.urls'
+
+MIDDLEWARE_CLASSES = [
+     'django.middleware.common.CommonMiddleware',
+     'django.contrib.sessions.middleware.SessionMiddleware',
+     'django.contrib.auth.middleware.AuthenticationMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'APP_DIRS': True,
+    },
+]
