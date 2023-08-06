a3aabf3f2a78109c343d16ded49e6dc9e43a4192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:29 +0000

    Add package baseplate/0.16.0/baseplate-0.16.0.tar.gz

---
+++ packages/baseplate/baseplate-0.16.0.tar.gz/baseplate-0.16.0/baseplate/_compat.py
@@ -0,0 +1,29 @@
+# pylint: disable=import-error
+"""Python 3 compatibility."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import sys
+
+
+if sys.version_info.major == 3:  # pragma: nocover
+    import configparser
+    import queue
+    from io import BytesIO
+    range = range
+else:  # pragma: nocover
+    import ConfigParser as configparser
+    import Queue as queue
+    from cStringIO import StringIO as BytesIO
+    range = xrange
+
+
+__all__ = [
+    "configparser",
+    "queue",
+    "BytesIO",
+    "range",
+]
