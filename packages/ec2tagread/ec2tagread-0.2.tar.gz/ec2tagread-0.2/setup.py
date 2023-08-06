5fe33e453dae2606ff4ba538dd4e9fc1f5560eb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:52 +0000

    Add package ec2tagread/0.2/ec2tagread-0.2.tar.gz

---
+++ packages/ec2tagread/ec2tagread-0.2.tar.gz/ec2tagread-0.2/setup.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+from distutils.core import setup
+setup(
+  name='ec2tagread',
+  scripts=['ec2tagread'],
+  version='0.2',
+  description='Reads EC2 tags within an EC2 instance',
+  author='Carlos Peñas',
+  author_email='carlos.penas@the-cocktail.com',
+  url='https://github.com/theist/ec2tagread',  # use the URL to the github repo
+  download_url='https://github.com/theist/ec2tagread/tarball/0.2',
+  keywords=['ec2', 'aws', 'tags'],
+  classifiers=[],
+)
+
