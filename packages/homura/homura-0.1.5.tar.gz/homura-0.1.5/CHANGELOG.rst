bc8b9b94bc5d7ef32463544498f6118347c13ec9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:36 +0000

    Add package homura/0.1.5/homura-0.1.5.tar.gz

---
+++ packages/homura/homura-0.1.5.tar.gz/homura-0.1.5/CHANGELOG.rst
@@ -0,0 +1,34 @@
+Changelog
+=========
+
+0.1.5 (2017-01-21)
+------------------
+
+- Add HTTP basic auth support.
+
+0.1.4 (2017-01-20)
+------------------
+
+- Add argument ``user_agent``.
+
+0.1.3 (2015-09-26)
+------------------
+
+- Fixed `#5 <https://github.com/shichao-an/homura/issues/5>`_ and merged `#6 <https://github.com/shichao-an/homura/pull/6>`_.
+
+0.1.2 (2015-09-16)
+------------------
+
+- Added argument ``pass_through_opts``.
+
+0.1.1 (2015-04-14)
+------------------
+
+- Fixed unicode issues complained by ``urlparse`` and ``os.path.join`` in Python 2.x and 3.3.
+
+
+0.1.0 (2015-01-16)
+------------------
+
+- Initial release.
+
---
+++ packages/homura/homura-0.1.5.tar.gz/homura-0.1.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from codecs import open as codecs_open
+
+with codecs_open('README.md', encoding='utf-8') as f:
+    lng_desc = f.read()
+
+setup(
+    name='homura',
+    version='0.1.5',
+    description="Python downloader with progress",
+    long_description=lng_desc,
+    keywords='download progress',
+    author='Shichao An',
+    author_email='shichao.an@nyu.edu',
+    url='https://github.com/shichao-an/homura',
+    license='BSD',
+    install_requires=open('requirements.txt').read().splitlines(),
+    py_modules=['homura'],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+)
