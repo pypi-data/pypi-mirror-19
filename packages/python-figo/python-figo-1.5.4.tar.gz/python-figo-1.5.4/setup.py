ff06898077cfb009a97a380aa0da585a9d67b073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:24 +0000

    Add package python-figo/1.5.4/python-figo-1.5.4.tar.gz

---
+++ packages/python-figo/python-figo-1.5.4.tar.gz/python-figo-1.5.4/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='python-figo',
+      version='1.5.4',
+      description='Library to easily use the API of http://figo.io',
+      author='Stefan Richter',
+      author_email='stefan.richter@figo.me',
+      url='http://www.figo.me',
+      packages=['figo'],
+      license='BSD License',
+      classifiers=[
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Topic :: Office/Business :: Financial',
+          'Topic :: Internet'
+      ])
