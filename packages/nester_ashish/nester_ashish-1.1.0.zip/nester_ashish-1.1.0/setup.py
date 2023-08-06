a7624fa01f8115c10ca8a9f3b8a7f8eec466d7c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:57 +0000

    Add package nester_ashish/1.1.0/nester_ashish-1.1.0.zip

---
+++ packages/nester_ashish/nester_ashish-1.1.0.zip/nester_ashish-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='nester_ashish',
+    version='1.1.0',
+    py_module=['nester_ashish'],
+    author='ashishsachdev',
+    author_email='sachdevashish28@gmail.com',
+    url='http://www.headfirstlab.com',
+    descripton='test practise',
+    )
