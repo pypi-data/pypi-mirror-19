de9bce90decff255d5e897400c1fc38f4b03e29c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:01 +0000

    Add package ios2andres/0.1.1/ios2andres-0.1.1.tar.gz

---
+++ packages/ios2andres/ios2andres-0.1.1.tar.gz/ios2andres-0.1.1/setup.py
@@ -0,0 +1,31 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+version = '0.1.1'
+
+def read(f):
+    return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
+
+
+setup(name='ios2andres',
+      version=version,
+      description=('iOS to Android Resource Renamer'),
+      long_description='\n\n'.join((read('README.md'), read('CHANGELOG'))),
+      classifiers=[
+          'License :: OSI Approved :: BSD License',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python'],
+      keywords='android resources ios convert',
+      author='Jordan Jozwiak',
+      author_email='support@jozapps.com',
+      url='https://github.com/jordanjoz1/ios2androidres',
+      license='MIT',
+      py_modules=['ios2andres'],
+      namespace_packages=[],
+      install_requires = [],
+      entry_points={
+          'console_scripts': [
+              'ios2andres = ios2andres:main']
+      },
+      include_package_data = False)
