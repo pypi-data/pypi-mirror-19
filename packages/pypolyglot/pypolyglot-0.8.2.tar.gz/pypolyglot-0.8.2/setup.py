afe2561f7b64230ab0b05bb490f0b4fa44fdf870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:21 +0000

    Add package pypolyglot/0.8.2/pypolyglot-0.8.2.tar.gz

---
+++ packages/pypolyglot/pypolyglot-0.8.2.tar.gz/pypolyglot-0.8.2/setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup, find_packages
+import os
+
+moduleDirectory = os.path.dirname(os.path.realpath(__file__))
+exec(open(moduleDirectory + "/polyglot/__version__.py").read())
+
+
+def readme():
+    with open(moduleDirectory + '/README.rst') as f:
+        return f.read()
+
+install_requires = [
+    'pyyaml',
+    'pypolyglot',
+    'fundamentals',
+    'requests',
+    'pyopenssl',
+    'ndg-httpsclient',
+    'pyasn1'
+]
+
+# READ THE DOCS SERVERS
+exists = os.path.exists("/home/docs/")
+if exists:
+    c_exclude_list = ['healpy', 'astropy',
+                      'numpy', 'sherlock', 'wcsaxes', 'HMpTy', 'ligo-gracedb']
+    for e in c_exclude_list:
+        try:
+            install_requires.remove(e)
+        except:
+            pass
+
+setup(name="pypolyglot",
+      version=__version__,
+      description="Translate documents and webpages to various markup languages and document formats (html, epub, mobi ..)",
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Utilities',
+      ],
+      keywords=['pdf, html, parser'],
+      url='https://github.com/thespacedoctor/polyglot',
+      download_url='https://github.com/thespacedoctor/polyglot/archive/v%(__version__)s.zip' % locals(
+      ),
+      author='David Young',
+      author_email='davidrobertyoung@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=install_requires,
+      test_suite='nose.collector',
+      tests_require=['nose', 'nose-cover3'],
+      entry_points={
+          'console_scripts': ['polyglot=polyglot.cl_utils:main'],
+      },
+      zip_safe=False)
