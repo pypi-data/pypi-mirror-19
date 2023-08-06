5505339d564b29d1067aeeaa2bce38699ce7a0bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:53 +0000

    Add package ncc_paapi/0.0.3/ncc_paapi-0.0.3.tar.gz

---
+++ packages/ncc_paapi/ncc_paapi-0.0.3.tar.gz/ncc_paapi-0.0.3/paapi/__init__.py
@@ -0,0 +1,2 @@
+from paapi.paapi import PaAuth
+from paapi.paapi import PaApi
---
+++ packages/ncc_paapi/ncc_paapi-0.0.3.tar.gz/ncc_paapi-0.0.3/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""NCC Performance Analyser API to ElasticSearch coupler"""
+
+import os
+from setuptools import setup
+
+setup(
+    name='ncc_paapi',
+    version='0.0.3',
+    description='Abstraction classes to access the PA API',
+    author='NCC Group',
+    packages=['paapi'],
+    install_requires=['urllib3==1.19.1'],
+    url='https://github.com/ncc-tools/python-pa-api'
+)
