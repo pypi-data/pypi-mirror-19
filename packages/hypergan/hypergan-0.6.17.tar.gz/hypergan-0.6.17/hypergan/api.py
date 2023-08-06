92941507b8db87b5fbb214d5390d515f41972085
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:57 +0000

    Add package hypergan/0.6.17/hypergan-0.6.17.tar.gz

---
+++ packages/hypergan/hypergan-0.6.17.tar.gz/hypergan-0.6.17/hypergan/api.py
@@ -0,0 +1,11 @@
+# these are just facades for now
+class Discriminator:
+    @staticmethod
+    def load(name):
+        print("Would load", name)
+
+class Generator:
+    @staticmethod
+    def load(name):
+        print("Would load", name)
+
