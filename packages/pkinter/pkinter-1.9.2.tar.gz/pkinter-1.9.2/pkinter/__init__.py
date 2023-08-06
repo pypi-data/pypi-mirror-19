3ad489f4709992ead08d38962b79567ef1b7922f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:42 +0000

    Add package pkinter/1.9.2/pkinter-1.9.2.tar.gz

---
+++ packages/pkinter/pkinter-1.9.2.tar.gz/pkinter-1.9.2/pkinter/__init__.py
@@ -0,0 +1,16 @@
+from .toggledlabelframe import *
+from .labeledseparator import *
+from .roundingscale import *
+from .entrytext import *
+from .limitedentry import *
+from .colourpickerbutton import *
+from .editablelabel import *
+from .collapsiblepane import *
+from .hyperlink import *
+from .pageview import *
+from .toolbar import *
+from .statusbar import *
+
+__title__ = "Pkinter"
+__author__ = "DeflatedPickle"
+__version__ = "1.9.2"
