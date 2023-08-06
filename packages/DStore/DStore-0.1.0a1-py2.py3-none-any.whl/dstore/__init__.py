99877b402a2cd7fa179952c1ba3cfe9bbfa0d592
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:26 +0000

    Add package DStore/0.1.0a1/DStore-0.1.0a1-py2.py3-none-any.whl

---
+++ packages/DStore/DStore-0.1.0a1-py2.py3-none-any.whl/dstore/__init__.py
@@ -0,0 +1,8 @@
+from . import Event, var, mod
+from .Store import Store
+from .Model import Model
+from .MemoryStore import MemoryStore
+
+
+__all__ = [ "Event", "Interface", "Store", "Model", "Storage", "var", "mod", "MemoryStore" ]
+__version__ = '0.1.0a1'
