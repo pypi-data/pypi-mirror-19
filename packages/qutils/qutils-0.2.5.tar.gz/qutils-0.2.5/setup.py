d5ca03795e65ad3c7533d70cff04c8d93c88a261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:25 +0000

    Add package qutils/0.2.5/qutils-0.2.5.tar.gz

---
+++ packages/qutils/qutils-0.2.5.tar.gz/qutils-0.2.5/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+
+VERSION = '0.2.5'
+
+
+setup(
+    name='qutils',
+    packages=['qutils'],
+    version=VERSION,
+    description='utility functions / modules that may be frequently used in simple scripts',
+    author='Raychee Zhang',
+    author_email='raychee.zhang@gmail.com',
+    url='https://github.com/Raychee/qutils',
+    download_url='https://github.com/Raychee/qutils/tarball/' + VERSION,
+    keywords=['utils', 'common', 'dlmanager', 'json'],
+    install_requires=['pandas', 'pyyaml', 'requests']
+)
