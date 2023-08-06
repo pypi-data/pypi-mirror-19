a44e21e3dca676738e16f01c82f97b31b8b71e96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:50 +0000

    Add package pyglyph/0.2.1/pyglyph-0.2.1.tar.gz

---
+++ packages/pyglyph/pyglyph-0.2.1.tar.gz/pyglyph-0.2.1/glyph/utils/logging.py
@@ -0,0 +1,38 @@
+import os
+import sys
+import logging
+import logging.config
+import yaml
+
+# module name is in conflict with stdlib and can cause unwanted monkey patching
+
+
+def load_config(config_file, placeholders={}, default_level=logging.INFO):
+    """Load logging configuration from .yaml file."""
+    if not sys.warnoptions:
+        # Route warnings through python logging
+        logging.captureWarnings(True)
+    if os.path.exists(config_file):
+        with open(config_file, 'rt') as f:
+            content = f.read().format(**placeholders)
+        config = yaml.load(content)
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
+
+
+def log_level(verbosity):
+    """Convert numeric verbosity to logging log levels."""
+    level = [logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG]
+    verbosity = len(level) - 1 if verbosity >= len(level) else verbosity
+    return level[verbosity]
+
+
+def print_dict(p_func, d):
+    for k, v in sorted(d.items()):
+        p_func('{} = {}'.format(k, v))
+
+
+def print_params(p_func, gp_config):
+    print_dict(p_func, gp_config)
+    p_func('')
