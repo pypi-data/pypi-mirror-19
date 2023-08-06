df14543161b327f2f3e4824850fba708d535a209
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:57 +0000

    Add package nester_cjs/1.3.0/nester_cjs-1.3.0.tar.gz

---
+++ packages/nester_cjs/nester_cjs-1.3.0.tar.gz/nester_cjs-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name         = 'nester_cjs',
+        version      = '1.3.0',
+        py_modules   = ['nester_cjs'],
+        author       = 'cjs',
+        author_email = '912090789@qq.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of nested lists',       
+      )
