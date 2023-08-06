2e339f4787112326a376e079bc3a43884cd42e39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:40 +0000

    Add package macdict/0.1.1/macdict-0.1.1.tar.gz

---
+++ packages/macdict/macdict-0.1.1.tar.gz/macdict-0.1.1/macdict/dictionary.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+
+import ctypes
+
+from macdict.foundation import (
+    objc, sel_name, CFStringCreateWithBytes, CFStringEncodingUTF8, CFRange,
+    DCSCopyTextDefinition)
+
+
+def lookup_word(word):
+    word_bytes = word.encode('utf-8')
+    word_cfstring = CFStringCreateWithBytes(
+        None, word_bytes, len(word_bytes), CFStringEncodingUTF8, False)
+    definition_nsstring = DCSCopyTextDefinition(
+        None, word_cfstring, CFRange(0, len(word_bytes)))
+    definition = ctypes.c_char_p(objc.objc_msgSend(
+        definition_nsstring, sel_name('UTF8String')))
+    if definition.value:
+        return definition.value.decode('utf-8')
