5078ced68b8b49c9d3ab3636f43164715d17dc75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:25 +0000

    Add package mastercard-fraud-scoring/1.0.4/mastercard_fraud_scoring-1.0.4.tar.gz

---
+++ packages/mastercard-fraud-scoring/mastercard_fraud_scoring-1.0.4.tar.gz/mastercard_fraud_scoring-1.0.4/mastercardfraudscoring/__init__.py
@@ -0,0 +1,8 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardfraudscoring.resourceconfig import *
+
+
+# import apis into api package
+from mastercardfraudscoring.scorelookup import *
