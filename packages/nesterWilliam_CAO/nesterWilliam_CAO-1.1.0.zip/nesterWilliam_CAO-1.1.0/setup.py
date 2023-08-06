f0aecbe0a2b4b12fe0b588f8cd73bf927d67a016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:52 +0000

    Add package nesterWilliam_CAO/1.1.0/nesterWilliam_CAO-1.1.0.zip

---
+++ packages/nesterWilliam_CAO/nesterWilliam_CAO-1.1.0.zip/nesterWilliam_CAO-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+          name                      ='nesterWilliam_CAO',
+          version                   ='1.1.0',
+          py_modules                =['nesterWilliam_CAO'],
+          author                    ='hfpython',
+          author_email              ='chwei20002005@sina.com',
+          url                       ='http://www.headfirstlabs.com',
+          description               = 'A simple printer of nested lists',
+          )
