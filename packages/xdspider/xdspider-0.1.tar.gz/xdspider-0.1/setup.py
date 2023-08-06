a4a353b5d0d25fb7020da8d0b3bddf844fa7c3eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:58 +0000

    Add package xdspider/0.1/xdspider-0.1.tar.gz

---
+++ packages/xdspider/xdspider-0.1.tar.gz/xdspider-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='xdspider',
+      version='0.1',
+      description='Python Web Spider',
+      url='http://github.com/storborg/funniest',
+      author='Dean Van Greunen',
+      author_email='deanvg9000@gmail.com',
+      license='GPL v3',
+      packages=['xdspider'],
+      zip_safe=False)
