774327b123b0687fa1864e2104a7f45c741cd233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:51 +0000

    Add package djangorestframework-deep/0.1.1/djangorestframework-deep-0.1.1.tar.gz

---
+++ packages/djangorestframework-deep/djangorestframework-deep-0.1.1.tar.gz/djangorestframework-deep-0.1.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name="djangorestframework-deep",
+  packages=["rest_framework_deep"],
+  version="0.1.1",
+  description="Use DRF with embedded resources, both for reading and writing",
+  author="Stefan Schindler",
+  author_email="drfdeep@stschindler.io",
+  url="https://github.com/stschindler/djangorestframework-deep",
+  download_url="https://github.com/stschindler/djangorestframework-deep/tarball/0.1.1",
+  keywords=["Django", "Django REST Framework", "REST", "DRF"],
+  classifiers=[],
+)
