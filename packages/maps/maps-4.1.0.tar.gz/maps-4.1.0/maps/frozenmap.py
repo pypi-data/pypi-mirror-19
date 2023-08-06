3eed8e8ea517103b122b2f733c53bcb47d126be4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:36 +0000

    Add package maps/4.1.0/maps-4.1.0.tar.gz

---
+++ packages/maps/maps-4.1.0.tar.gz/maps-4.1.0/maps/frozenmap.py
@@ -0,0 +1,43 @@
+import collections.abc
+
+class FrozenMap(collections.abc.Mapping):
+    '''An immutable, hashable key-value mapping accessible via bracket-notation
+    (i.e. ``__getitem__``).
+
+    :param args: Position arguments in the same form as the :py:class:`dict` constructor.
+    :param kwargs: Keyword arguments in the same form as the :py:class:`dict` constructor.
+
+    Usage::
+
+       >>> import maps
+       >>> fm = maps.FrozenMap({'a': 1, 'b': 2})
+       >>> fm['a']
+       1
+       >>> list(fm.items())
+       [('a', 1), ('b', 2)]
+       >>> len(fm)
+       2
+       >>> hash(fm)
+       3212389899479848432
+    '''
+
+    def __init__(self, *args, **kwargs):
+        self._data = dict(*args, **kwargs)
+        self._hash = None
+
+    def __getitem__(self, key):
+        return self._data[key]
+
+    def __iter__(self):
+        return iter(self._data)
+
+    def __len__(self):
+        return len(self._data)
+
+    def __hash__(self):
+        if self._hash is None:
+            self._hash = hash(frozenset(self.items()))
+        return self._hash
+
+    def __repr__(self): # pragma: no cover
+        return f'{type(self).__name__}({self._data!r})'
