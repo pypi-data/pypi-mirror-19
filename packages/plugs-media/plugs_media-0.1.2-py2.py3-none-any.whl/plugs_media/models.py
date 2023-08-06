bade7a3d5ac8ecfcea253c9197cc5c8903245457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:28 +0000

    Add package plugs-media/0.1.2/plugs_media-0.1.2-py2.py3-none-any.whl

---
+++ packages/plugs-media/plugs_media-0.1.2-py2.py3-none-any.whl/plugs_media/models.py
@@ -0,0 +1,27 @@
+"""
+Plugs media models
+"""
+
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+
+from plugs_core import mixins
+
+class Media(mixins.Timestampable, models.Model):
+    """
+    Media Model
+    """
+    name = models.CharField(max_length=255, null=True, blank=True)
+    file = models.FileField(max_length=255, upload_to='%Y/%m/%d/')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True)
+    object_id = models.PositiveIntegerField(null=True)
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    # pylint: disable=R0903
+    class Meta:
+        """
+        Metaclass definition
+        """
+        verbose_name = 'media'
+        verbose_name_plural = 'media'
