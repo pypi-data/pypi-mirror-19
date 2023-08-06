dd8dfd04c3c71f55d4e44c5757d8453120076c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:01 +0000

    Add package serial_cog/0.1.1/serial_cog-0.1.1.tar.gz

---
+++ packages/serial_cog/serial_cog-0.1.1.tar.gz/serial_cog-0.1.1/setup.py
@@ -0,0 +1,18 @@
+import os
+
+import yaml
+from setuptools import setup
+from setuptools.command.install import install
+
+setup(
+    name='serial_cog',
+    version='0.1.1',
+    packages=['serial_cog.modules'],
+    url='https://github.com/Rashitko/serial_provider_cog',
+    download_url='https://github.com/Rashitko/serial_provider_cog/master/tarball/0.1',
+    license='MIT',
+    author='Michal Raska',
+    author_email='michal.raska@gmail.com',
+    description='',
+    install_requires=['up', 'pyyaml', 'pyserial'],
+)
