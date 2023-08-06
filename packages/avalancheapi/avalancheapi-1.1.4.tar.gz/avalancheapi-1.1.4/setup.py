6ad6e7021f0012ddc2ea1053f56e49e85e29783f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:38 +0000

    Add package avalancheapi/1.1.4/avalancheapi-1.1.4.tar.gz

---
+++ packages/avalancheapi/avalancheapi-1.1.4.tar.gz/avalancheapi-1.1.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'avalancheapi',
+  packages = ['avalancheapi'], # this must be the same as the name above
+  version = '1.1.4',
+  description = 'The Python API for Spirent Avalanche',
+  author = 'Matthew Jefferson',
+  author_email = 'matt.jefferson@spirent.com',
+  url = 'https://github.com/canadianjeans/avalanche.git', # use the URL to the github repo
+  download_url = 'https://github.com/canadianjeans/avalanche/archive/master.zip',
+  keywords = ['Spirent', 'avalanche', 'python'], 
+  classifiers = [],
+)
