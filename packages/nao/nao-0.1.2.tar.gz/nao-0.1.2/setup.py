772e5679580a8ac2db91eab4c4ad4de717e4152b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:43 +0000

    Add package nao/0.1.2/nao-0.1.2.tar.gz

---
+++ packages/nao/nao-0.1.2.tar.gz/nao-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+  name = 'nao',
+  packages = ['nao'],
+  version = '0.1.2',
+  description = 'Intelligent data manipulation tools',
+  author = 'Szabolcs Blága',
+  author_email = 'szabolcs.blaga@gmail.com',
+  url = 'https://github.com/blagasz/nao',
+  download_url = 'https://github.com/blagasz/nao/tarball/0.1',
+  license = 'GPL',
+  install_requires=[
+    'PyYAML',
+    'pandas',  # for datetime conversion and distinct
+  ],
+  keywords = ['data', 'yaml', 'multilingual', 'multivalue', 'config'],
+  classifiers = [],
+)
