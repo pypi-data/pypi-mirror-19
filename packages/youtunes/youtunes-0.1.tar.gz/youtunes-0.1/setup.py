efe46768617c3ff295ad547643364a2ba6ab03d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:58 +0000

    Add package youtunes/0.1/youtunes-0.1.tar.gz

---
+++ packages/youtunes/youtunes-0.1.tar.gz/youtunes-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'youtunes',
+  packages = ['youtunes'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Multithreaded direct download from youtube to mp3',
+  author = 'Andrew Chang',
+  author_email = 'achang97@stanford.edu',
+  url = 'https://github.com/achang97/YouTunes', # use the URL to the github repo
+  download_url = 'https://github.com/achang97/youtunes/tarball/0.1', # I'll explain this in a second
+  keywords = ['download', 'YouTube', 'iTunes'], # arbitrary keywords
+  classifiers = [],
+)
