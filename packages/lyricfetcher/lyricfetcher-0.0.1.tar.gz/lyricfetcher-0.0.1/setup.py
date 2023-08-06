8381f0c51b4ea1408d02b007fd4545ae411601d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:25 +0000

    Add package lyricfetcher/0.0.1/lyricfetcher-0.0.1.tar.gz

---
+++ packages/lyricfetcher/lyricfetcher-0.0.1.tar.gz/lyricfetcher-0.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='lyricfetcher',
+      version='0.0.1',
+      description='lyricfetcher',
+      url='http://github.com/bharatkalluri/lyricfetcher',
+      author='Bharat Kalluri',
+      author_email='bharatkalluri@protonmail.com',
+      license='MIT',
+      packages=['lyricfetcher'],
+      zip_safe=False,
+      install_requires=[
+          'requests',
+          'bs4',
+          'requests',
+          'beautifulsoup4',
+          'lxml'
+      ],
+      entry_points={
+          'console_scripts': ['lyricfetcher=lyricfetcher.console:main']
+      })
