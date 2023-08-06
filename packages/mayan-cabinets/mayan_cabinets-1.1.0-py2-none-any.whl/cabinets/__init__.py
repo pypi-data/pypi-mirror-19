b53bdb5c0a112de580fda782d77dfd5b56adb004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:49 +0000

    Add package mayan-cabinets/1.1.0/mayan_cabinets-1.1.0-py2-none-any.whl

---
+++ packages/mayan-cabinets/mayan_cabinets-1.1.0-py2-none-any.whl/cabinets/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+__author__ = 'Roberto Rosario'
+__build__ = 0x010100
+__copyright__ = 'Copyright 2017 Roberto Rosario'
+__license__ = 'Apache 2.0'
+__title__ = 'mayan-cabinets'
+__version__ = '1.1.0'
+
+default_app_config = 'cabinets.apps.CabinetsApp'
