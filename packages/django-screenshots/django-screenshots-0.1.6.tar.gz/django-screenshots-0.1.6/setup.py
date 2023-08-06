0acdd20886dd8b2e9af877770964eaaf891851da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:26 +0000

    Add package django-screenshots/0.1.6/django-screenshots-0.1.6.tar.gz

---
+++ packages/django-screenshots/django-screenshots-0.1.6.tar.gz/django-screenshots-0.1.6/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='django-screenshots',
+    version='0.1.6',
+    author='Jim Barcelona',
+    author_email='barce@me.com',
+    packages=['django_screenshots', 'django_screenshots.tests'],
+    install_requires=[
+      'future',
+    ],
+    scripts=[],
+    url='http://pypi.python.org/pypi/django-screenshots/',
+    license='LICENSE.txt',
+    description='Screen shot a URL.',
+    long_description=open('README.txt').read(),
+)
