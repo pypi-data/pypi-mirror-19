1627fe5bc1ec67fba39508b9b15ef092c7353d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:55 +0000

    Add package foamPy/0.0.4/foamPy-0.0.4.tar.gz

---
+++ packages/foamPy/foamPy-0.0.4.tar.gz/foamPy-0.0.4/foampy/__init__.py
@@ -0,0 +1,15 @@
+__version__ = "0.0.4"
+
+import os
+
+# Attempt to detect OpenFOAM version
+try:
+    foam_version = os.environ["WM_PROJECT_VERSION"]
+except KeyError:
+    foam_version = "x.x.x"
+
+from . import core
+from .core import *
+from . import dictionaries
+from . import foil
+from . import types
