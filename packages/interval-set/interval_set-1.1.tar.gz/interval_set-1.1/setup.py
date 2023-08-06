7bfb31a258225af396ec484dc8bd570630aa5369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:35 +0000

    Add package interval-set/1.1/interval_set-1.1.tar.gz

---
+++ packages/interval-set/interval_set-1.1.tar.gz/interval_set-1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='interval_set',
+      version='1.1',
+      description=(
+        'Closed interval set toolkit for resources representation in '
+        'scheduling'),
+      url='https://github.com/oar-team/interval_set',
+      author='Michael Mercier',
+      author_email='michael.mercier@inria.fr',
+      license='BSD',
+      packages=['interval_set'],
+      zip_safe=False)
