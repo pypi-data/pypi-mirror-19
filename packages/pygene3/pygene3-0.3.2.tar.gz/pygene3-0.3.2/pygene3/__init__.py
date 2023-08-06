e5bc364f2a3f4a93b22793acfd15ed7edc2d69a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:42 +0000

    Add package pygene3/0.3.2/pygene3-0.3.2.tar.gz

---
+++ packages/pygene3/pygene3-0.3.2.tar.gz/pygene3-0.3.2/pygene3/__init__.py
@@ -0,0 +1,14 @@
+"""
+pygene is a library for genetic algorithms in python
+
+It aims to be very simple to use, and suitable for people
+new to genetic algorithms.
+"""
+
+version = "0.3.2"
+
+__all__ = [
+    'gene', 'gamete', 'organism', 'population', 'xmlio', 'prog',
+    'config'
+    ]
+
