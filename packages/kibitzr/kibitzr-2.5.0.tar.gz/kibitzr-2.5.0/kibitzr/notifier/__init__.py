e6e18e3258dfdf96b85d78e3a5fd577899545407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:27 +0000

    Add package kibitzr/2.5.0/kibitzr-2.5.0.tar.gz

---
+++ packages/kibitzr/kibitzr-2.5.0.tar.gz/kibitzr-2.5.0/kibitzr/notifier/__init__.py
@@ -0,0 +1,5 @@
+from .mailgun import post_mailgun  # noqa
+from .custom import post_python  # noqa
+from .bash import post_bash  # noqa
+from .gitter import post_gitter  # noqa
+from .slack import SlackSession  # noqa
