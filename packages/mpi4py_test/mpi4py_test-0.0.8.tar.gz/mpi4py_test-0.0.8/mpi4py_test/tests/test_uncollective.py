cf60a63784dd79b89fe36d2fbee6a441b6de076a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:34 +0000

    Add package mpi4py_test/0.0.8/mpi4py_test-0.0.8.tar.gz

---
+++ packages/mpi4py_test/mpi4py_test-0.0.8.tar.gz/mpi4py_test-0.0.8/mpi4py_test/tests/test_uncollective.py
@@ -0,0 +1,6 @@
+from mpi4py_test import MPITest
+
+@MPITest(commsize=[1, 2])
+def test_uncollective_error(comm):
+    if comm.rank == 1:
+        raise ValueError("This is a dummy error. The test will fail. but shall not hang")
