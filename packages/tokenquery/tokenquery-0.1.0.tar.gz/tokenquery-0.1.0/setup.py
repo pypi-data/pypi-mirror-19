11c7b0b56a22fe3f4da5e1653589246f7653b8cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:12 +0000

    Add package tokenquery/0.1.0/tokenquery-0.1.0.tar.gz

---
+++ packages/tokenquery/tokenquery-0.1.0.tar.gz/tokenquery-0.1.0/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(
+    name='tokenquery',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Tokenquery - query language for tokens ',
+    author='Ramtin Seraj',
+    author_email='mehdizadeh.ramtin@gmail.com',
+    url='https://github.com/ramtinms/tokenquery',
+    download_url='https://github.com/ramtinms/tokenquery/tarball/0.1',
+    keywords=['natural language processing', 'nlp', 'regex', 'regular expressions', 'tokenizer', 'query'],
+    classifiers=['Intended Audience :: Information Technology',
+                 'Intended Audience :: Science/Research',
+                 'Topic :: Scientific/Engineering',
+                 'Topic :: Scientific/Engineering :: Artificial Intelligence',
+                 'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+                 'Topic :: Scientific/Engineering :: Information Analysis',
+                 'Topic :: Text Processing',
+                 'Topic :: Text Processing :: Filters',
+                 'Topic :: Text Processing :: General',
+                 'Topic :: Text Processing :: Indexing',
+                 'Topic :: Text Processing :: Linguistic',
+                 ],
+    install_requires=[
+        "requests",
+        "nltk",
+        "conllu",
+        "google-api-python-client",
+    ],
+
+)
