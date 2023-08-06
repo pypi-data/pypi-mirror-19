4700d168ce6e000f0771aeeade202d3d0f09cd62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:55 +0000

    Add package mxklabs/0.0.3/mxklabs-0.0.3.tar.gz

---
+++ packages/mxklabs/mxklabs-0.0.3.tar.gz/mxklabs-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'mxklabs',
+  packages = ['mxklabs'],
+  version = '0.0.3',
+  description = 'A python library for reading files in the DIMACS format',
+  author = 'Mark Kattenbelt',
+  author_email = 'mark.kattenbelt@gmail.com',
+  url = 'https://github.com/mxklabs/mxklabs-python',
+  download_url = 'https://github.com/mxklabs/mxklabs-python/tarball/0.0.3', # todo
+  keywords = ['DIMACS', 'SAT', 'satisfiability', 'CNF'],
+  classifiers = [],
+)
