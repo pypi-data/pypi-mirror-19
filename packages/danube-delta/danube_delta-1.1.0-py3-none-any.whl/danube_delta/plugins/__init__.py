246489bbdea14a79afa24bb2f9fe84c8cfc81104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:13 +0000

    Add package danube-delta/1.1.0/danube_delta-1.1.0-py3-none-any.whl

---
+++ packages/danube-delta/danube_delta-1.1.0-py3-none-any.whl/danube_delta/plugins/__init__.py
@@ -0,0 +1,11 @@
+
+from . import (
+    code_blocks, headings, plain_summary, featured_image, media, tables,
+    authors
+)
+
+
+PLUGINS = [
+    code_blocks, headings, plain_summary, featured_image, media, tables,
+    authors
+]
