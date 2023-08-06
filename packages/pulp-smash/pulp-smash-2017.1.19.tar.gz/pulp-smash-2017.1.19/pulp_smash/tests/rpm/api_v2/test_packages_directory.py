a90ea43c6c98e149cfebc8d68eb81d4fb8950082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:51 +0000

    Add package pulp-smash/2017.1.19/pulp-smash-2017.1.19.tar.gz

---
+++ packages/pulp-smash/pulp-smash-2017.1.19.tar.gz/pulp-smash-2017.1.19/pulp_smash/tests/rpm/api_v2/test_packages_directory.py
@@ -0,0 +1,182 @@
+# coding=utf-8
+"""Tests for the ``packages_directory`` distributor option.
+
+One can configure a distributor with a ``packages_directory`` configuration
+option. This option controls whether RPMs are published in the same directory
+as the ``repodata`` directory, or somewhere else. This module tests this
+feature. For more information, see `Pulp issue #1976`_.
+
+.. _Pulp issue #1976: https://pulp.plan.io/issues/1976
+"""
+import os
+import unittest
+from urllib.parse import urljoin
+
+from pulp_smash import api, selectors, utils
+from pulp_smash.constants import (
+    REPOSITORY_PATH,
+    RPM_NAMESPACES,
+    RPM_SIGNED_FEED_URL,
+)
+from pulp_smash.tests.rpm.api_v2.utils import (
+    gen_distributor,
+    gen_repo,
+    xml_handler,
+)
+from pulp_smash.tests.rpm.utils import check_issue_2277
+from pulp_smash.tests.rpm.utils import set_up_module as setUpModule  # noqa pylint:disable=unused-import
+
+
+def get_parse_repodata_xml(server_config, distributor, file_path):
+    """Fetch, parse and return an XML file from a ``repodata`` directory.
+
+    :param pulp_smash.config.ServerConfig server_config: Information about the
+        Pulp server being targeted.
+    :param distributor: Information about a distributor. It should be a dict
+        containing at least ``{'config': {'relative_url': …}}``.
+    :param file_path: The path to an XML file, relative to the distributor's
+        relative URL. For example: ``repodata/repomd.xml``.
+    :returns: The XML document, parsed as an ``xml.etree.ElementTree`` object.
+    """
+    path = urljoin('/pulp/repos/', distributor['config']['relative_url'])
+    path = urljoin(path, file_path)
+    return api.Client(server_config, xml_handler).get(path)
+
+
+def get_parse_repodata_primary_xml(cfg, distributor):
+    """Fetch, decompress, parse and return a ``repodata/…primary.xml.gz`` file.
+
+    :param pulp_smash.config.ServerConfig cfg: Information about the Pulp
+        server being targeted.
+    :param distributor: Information about a distributor. It should be a dict
+        containing at least ``{'config': {'relative_url': …}}``.
+    :returns: An ``xml.etree.ElementTree`` object.
+    """
+    repomd_xml = get_parse_repodata_xml(
+        cfg,
+        distributor,
+        'repodata/repomd.xml'
+    )
+    primary_xml_hrefs = [
+        href for href in get_file_hrefs(repomd_xml)
+        if href.endswith('primary.xml.gz')
+    ]
+    assert len(primary_xml_hrefs) == 1
+    return get_parse_repodata_xml(cfg, distributor, primary_xml_hrefs[0])
+
+
+def get_package_hrefs(primary_xml):
+    """Return the href of each package in a ``primary.xml`` file.
+
+    :param primary_xml: An ``xml.etree.ElementTree`` object representing the
+        root of a ``primary.xml`` document.
+    :returns: An iterable of hrefs, with each href as a string.
+    """
+    package_xpath = '{{{}}}package'.format(RPM_NAMESPACES['metadata/common'])
+    location_xpath = '{{{}}}location'.format(RPM_NAMESPACES['metadata/common'])
+    packages = primary_xml.findall(package_xpath)
+    locations = [package.find(location_xpath) for package in packages]
+    return tuple((location.get('href') for location in locations))
+
+
+def get_file_hrefs(repomd_xml):
+    """Return the href of each file in a ``repomd.xml`` file.
+
+    :param repomd_xml: An ``xml.etree.ElementTree`` object representing the
+        root of a ``repomd.xml`` document.
+    :returns: An iterable of hrefs, with each href as a string.
+    """
+    data_xpath = '{{{}}}data'.format(RPM_NAMESPACES['metadata/repo'])
+    location_xpath = '{{{}}}location'.format(RPM_NAMESPACES['metadata/repo'])
+    data = repomd_xml.findall(data_xpath)
+    locations = [datum.find(location_xpath) for datum in data]
+    return tuple((location.get('href') for location in locations))
+
+
+class PackagesDirectoryTestCase(utils.BaseAPITestCase):
+    """Test the distributor ``packages_directory`` option."""
+
+    @classmethod
+    def setUpClass(cls):
+        """Create a repository with a feed and sync it."""
+        super(PackagesDirectoryTestCase, cls).setUpClass()
+        if check_issue_2277(cls.cfg):
+            raise unittest.SkipTest('https://pulp.plan.io/issues/2277')
+        client = api.Client(cls.cfg, api.json_handler)
+        body = gen_repo()
+        body['importer_config']['feed'] = RPM_SIGNED_FEED_URL
+        cls.repo = client.post(REPOSITORY_PATH, body)
+        cls.repo = client.get(cls.repo['_href'], params={'details': True})
+        cls.resources.add(cls.repo['_href'])
+        utils.sync_repo(cls.cfg, cls.repo['_href'])
+
+    def test_default_behaviour(self):
+        """Do not use the ``packages_directory`` option.
+
+        Create a distributor with default options, and use it to publish the
+        repository. Verify packages end up in the current directory, relative
+        to the published repository's root. (This same directory contains the
+        ``repodata`` directory, and it may be changed by setting the
+        distributor's ``relative_url``.)
+        """
+        distributor = api.Client(self.cfg).post(
+            urljoin(self.repo['_href'], 'distributors/'),
+            gen_distributor(),
+        ).json()
+        utils.publish_repo(self.cfg, self.repo, {'id': distributor['id']})
+        primary_xml = get_parse_repodata_primary_xml(self.cfg, distributor)
+        package_hrefs = get_package_hrefs(primary_xml)
+        self.assertGreater(len(package_hrefs), 0)
+        for package_href in package_hrefs:
+            with self.subTest(package_href=package_href):
+                self.assertEqual(os.path.dirname(package_href), '')
+
+    def test_distributor_config(self):
+        """Use the ``packages_directory`` distributor option.
+
+        Create a distributor with the ``packages_directory`` option set, and
+        use it to publish the repository. Verify packages end up in the
+        specified directory, relative to the published repository's root.
+        """
+        if selectors.bug_is_untestable(1976, self.cfg.version):
+            self.skipTest('https://pulp.plan.io/issues/1976')
+        body = gen_distributor()
+        packages_dir = utils.uuid4()
+        body['distributor_config']['packages_directory'] = packages_dir
+        distributor = api.Client(self.cfg).post(
+            urljoin(self.repo['_href'], 'distributors/'),
+            body
+        ).json()
+        utils.publish_repo(self.cfg, self.repo, {'id': distributor['id']})
+        primary_xml = get_parse_repodata_primary_xml(self.cfg, distributor)
+        package_hrefs = get_package_hrefs(primary_xml)
+        self.assertGreater(len(package_hrefs), 0)
+        for package_href in package_hrefs:
+            with self.subTest(package_href=package_href):
+                self.assertEqual(os.path.dirname(package_href), packages_dir)
+
+    def test_publish_override_config(self):
+        """Use the ``packages_directory`` publish override option.
+
+        Create a distributor with default options, and use it to publish the
+        repository. Specify the ``packages_directory`` option during the
+        publish as an override option. Verify packages end up in the specified
+        directory, relative to the published repository's root.
+        """
+        if selectors.bug_is_untestable(1976, self.cfg.version):
+            self.skipTest('https://pulp.plan.io/issues/1976')
+        distributor = api.Client(self.cfg).post(
+            urljoin(self.repo['_href'], 'distributors/'),
+            gen_distributor(),
+        ).json()
+        packages_dir = utils.uuid4()
+        utils.publish_repo(self.cfg, self.repo, {
+            'id': distributor['id'],
+            'override_config': {'packages_directory': packages_dir},
+        })
+        primary_xml = get_parse_repodata_primary_xml(self.cfg, distributor)
+        package_hrefs = get_package_hrefs(primary_xml)
+        self.assertGreater(len(package_hrefs), 0)
+        for package_href in package_hrefs:
+            with self.subTest(package_href=package_href):
+                self.assertEqual(os.path.dirname(package_href), packages_dir)
---
+++ packages/pulp-smash/pulp-smash-2017.1.19.tar.gz/pulp-smash-2017.1.19/pulp_smash/tests/rpm/api_v2/test_sync_publish.py
@@ -0,0 +1,184 @@
+# coding=utf-8
+"""Tests that sync and publish RPM repositories.
+
+For information on repository sync and publish operations, see
+`Synchronization`_ and `Publication`_.
+
+.. _Publication:
+    http://docs.pulpproject.org/en/latest/dev-guide/integration/rest-api/repo/publish.html
+.. _Synchronization:
+    http://docs.pulpproject.org/en/latest/dev-guide/integration/rest-api/repo/sync.html
+"""
+import inspect
+import unittest
+from urllib.parse import urljoin
+
+from packaging.version import Version
+
+from pulp_smash import api, selectors, utils
+from pulp_smash.constants import (
+    DRPM_UNSIGNED_FEED_URL,
+    REPOSITORY_PATH,
+    RPM_SIGNED_FEED_COUNT,
+    RPM_SIGNED_FEED_URL,
+    SRPM_SIGNED_FEED_URL,
+)
+from pulp_smash.tests.rpm.api_v2.utils import gen_repo
+from pulp_smash.tests.rpm.utils import set_up_module as setUpModule  # noqa pylint:disable=unused-import
+
+
+_REPO_PUBLISH_PATH = '/pulp/repos/'  # + relative_url + unit_name.rpm.arch
+
+
+# This class is left public for documentation purposes.
+class SyncRepoBaseTestCase(utils.BaseAPITestCase):
+    """A parent class for repository syncronization test cases.
+
+    :meth:`get_feed_url` should be overridden by concrete child classes. This
+    method's response is used when setting the repository's importer feed URL.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """Create an RPM repository with a valid feed and sync it."""
+        if inspect.getmro(cls)[0] == SyncRepoBaseTestCase:
+            raise unittest.SkipTest('Abstract base class.')
+        super(SyncRepoBaseTestCase, cls).setUpClass()
+        client = api.Client(cls.cfg, api.json_handler)
+        body = gen_repo()
+        body['importer_config']['feed'] = cls.get_feed_url()
+        cls.repo_href = client.post(REPOSITORY_PATH, body)['_href']
+        cls.resources.add(cls.repo_href)
+        cls.report = utils.sync_repo(cls.cfg, cls.repo_href)
+
+    @staticmethod
+    def get_feed_url():
+        """Return an RPM repository feed URL. Should be overridden.
+
+        :raises: ``NotImplementedError`` if not overridden by a child class.
+        """
+        raise NotImplementedError()
+
+    def test_start_sync_code(self):
+        """Assert the call to sync a repository returns an HTTP 202."""
+        self.assertEqual(self.report.status_code, 202)
+
+    def test_task_progress_report(self):
+        """Assert no task's progress report contains error details.
+
+        Other assertions about the final state of each task are handled by the
+        client's response handler. (For more information, see the source of
+        :func:`pulp_smash.api.safe_handler`.)
+        """
+        tasks = tuple(api.poll_spawned_tasks(self.cfg, self.report.json()))
+        for i, task in enumerate(tasks):
+            with self.subTest(i=i):
+                error_details = task['progress_report']['yum_importer']['content']['error_details']  # noqa pylint:disable=line-too-long
+                self.assertEqual(error_details, [], task)
+
+
+class SyncRpmRepoTestCase(SyncRepoBaseTestCase):
+    """Test one can create and sync an RPM repository with an RPM feed."""
+
+    @staticmethod
+    def get_feed_url():
+        """Return an RPM repository feed URL."""
+        return RPM_SIGNED_FEED_URL
+
+    # This is specific to the RPM repo. Leave in this test case.
+    def test_unit_count_on_repo(self):
+        """Verify that the sync added the correct number of units to the repo.
+
+        Read the repository and examine its ``content_unit_counts`` attribute.
+        Compare these attributes to metadata from the remote repository.
+        Expected values are currently hard-coded into this test.
+        """
+        content_unit_counts = {
+            'rpm': RPM_SIGNED_FEED_COUNT,
+            'erratum': 4,
+            'package_group': 2,
+            'package_category': 1,
+        }
+        if self.cfg.version >= Version('2.9'):  # langpack support added in 2.9
+            content_unit_counts['package_langpacks'] = 1
+        repo = api.Client(self.cfg).get(self.repo_href).json()
+        self.assertEqual(repo['content_unit_counts'], content_unit_counts)
+
+    def test_no_change_in_second_sync(self):
+        """Verify that syncing a second time has no changes.
+
+        If the repository have not changed then Pulp must state that anything
+        was changed when doing a second sync.
+        """
+        report = utils.sync_repo(self.cfg, self.repo_href)
+        tasks = tuple(api.poll_spawned_tasks(self.cfg, report.json()))
+        with self.subTest(comment='spawned tasks'):
+            self.assertEqual(len(tasks), 1)
+        for count_type in ('added_count', 'removed_count', 'updated_count'):
+            with self.subTest(comment=count_type):
+                self.assertEqual(tasks[0]['result'][count_type], 0)
+
+
+class SyncDrpmRepoTestCase(SyncRepoBaseTestCase):
+    """Test one can create and sync an RPM repository with an DRPM feed."""
+
+    @staticmethod
+    def get_feed_url():
+        """Return an DRPM repository feed URL."""
+        return DRPM_UNSIGNED_FEED_URL
+
+
+class SyncSrpmRepoTestCase(SyncRepoBaseTestCase):
+    """Test one can create and sync an RPM repository with an SRPM feed."""
+
+    @staticmethod
+    def get_feed_url():
+        """Return an SRPM repository feed URL."""
+        return SRPM_SIGNED_FEED_URL
+
+
+class SyncInvalidFeedTestCase(utils.BaseAPITestCase):
+    """Create an RPM repository with an invalid feed and sync it.
+
+    The sync should complete with errors reported.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """Create an RPM repository with an invalid feed and sync it."""
+        super(SyncInvalidFeedTestCase, cls).setUpClass()
+        client = api.Client(cls.cfg, api.json_handler)
+        body = gen_repo()
+        body['importer_config']['feed'] = utils.uuid4()
+        repo = client.post(REPOSITORY_PATH, body)
+        client.response_handler = api.echo_handler
+        cls.report = client.post(urljoin(repo['_href'], 'actions/sync/'))
+        cls.tasks = tuple(api.poll_spawned_tasks(cls.cfg, cls.report.json()))
+        cls.resources.add(repo['_href'])
+
+    def test_start_sync_code(self):
+        """Assert the call to sync a repository returns an HTTP 202."""
+        self.assertEqual(self.report.status_code, 202)
+
+    def test_task_error_traceback(self):
+        """Assert each task's "error" and "traceback" fields are non-null."""
+        if selectors.bug_is_untestable(1455, self.cfg.version):
+            self.skipTest('https://pulp.plan.io/issues/1455')
+        for i, task in enumerate(self.tasks):
+            for key in {'error', 'traceback'}:
+                with self.subTest((i, key)):
+                    self.assertIsNotNone(task[key])
+
+    def test_task_progress_report(self):
+        """Assert each task's progress report contains error details."""
+        self.skipTest('https://pulp.plan.io/issues/1376')
+        for i, task in enumerate(self.tasks):
+            with self.subTest(i=i):
+                self.assertNotEqual(
+                    task['progress_report']['yum_importer']['content']['error_details'],  # noqa pylint:disable=line-too-long
+                    []
+                )
+
+    def test_number_tasks(self):
+        """Assert that only one task was spawned."""
+        self.assertEqual(len(self.tasks), 1)
