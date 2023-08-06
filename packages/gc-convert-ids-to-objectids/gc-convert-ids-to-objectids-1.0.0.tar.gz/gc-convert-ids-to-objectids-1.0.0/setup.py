1e8a507080a8169b3d5de22f0a8e51228118d3b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:49 +0000

    Add package gc-convert-ids-to-objectids/1.0.0/gc-convert-ids-to-objectids-1.0.0.tar.gz

---
+++ packages/gc-convert-ids-to-objectids/gc-convert-ids-to-objectids-1.0.0.tar.gz/gc-convert-ids-to-objectids-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, Extension
+
+setup(
+    name='gc-convert-ids-to-objectids',
+    version='1.0.0',
+    author='Alex Etling',
+    author_email='alex@gc.com',
+    packages=['convert_ids'],
+    package_dir={'': '.'},
+    zip_safe=False,
+    url='https://github.com/gamechanger/gc-convert-ids',
+    ext_modules = [Extension("convert_ids/convert_ids_to_objectids", ["convert_ids/convert_ids_to_objectids.c"])]
+)
