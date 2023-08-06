b766165e7ee5d5211f45bffcf2a8c9b843af38a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:28 +0000

    Add package envisage/4.6.0/envisage-4.6.0.tar.gz

---
+++ packages/envisage/envisage-4.6.0.tar.gz/envisage-4.6.0/envisage/_version.py
@@ -0,0 +1,8 @@
+# This file was automatically generated from the `setup.py` script.
+version = '4.6.0'
+full_version = '4.6.0'
+git_revision = '40fc473'
+is_released = True
+
+if not is_released:
+    version = full_version
