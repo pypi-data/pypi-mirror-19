41e9574f6cf395cd72ea4e8a2d1bd5f99e224e8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:17 +0000

    Add package cartoview/1.1.0/cartoview-1.1.0.tar.gz

---
+++ packages/cartoview/cartoview-1.1.0.tar.gz/cartoview-1.1.0/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name = 'cartoview',
+  packages = ['cartoview'],
+  version = '1.1.0',
+  description = 'Cartoview is a GIS web mapping application framework to easily share and deploy apps based on Geonode',
+  author = 'Cartologic',
+  author_email = 'info@cartologic.com',
+  url = 'https://github.com/cartologic/cartoview',
+  download_url = 'https://github.com/cartologic/cartoview/tarball/1.1.0', 
+  keywords = ['cartoview', 'gis', 'geonode', "django", "web mapping", "applications", "apps", "application management"],
+  classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Framework :: Django :: 1.8",
+    "Topic :: Scientific/Engineering :: GIS"
+  ],
+  license="BSD",
+  install_requires= [
+    "geonode==2.5.10",
+  ]
+)
