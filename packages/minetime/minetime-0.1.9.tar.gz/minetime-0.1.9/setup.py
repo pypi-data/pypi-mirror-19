b79d1b3cf28078f9e71a425d1124c0015822aa88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:48 +0000

    Add package minetime/0.1.9/minetime-0.1.9.tar.gz

---
+++ packages/minetime/minetime-0.1.9.tar.gz/minetime-0.1.9/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages, setup
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='minetime',
+    version='0.1.9',
+    install_requires=['python-redmine', 'click', 'pyyaml', 'tabulate'],
+    description='A simple Command line application helping you play with timelogs of configured redmine instance.',
+    long_description=readme,
+    author='yakoi',
+    author_email='jpgelinas@gmail.com',
+    url='https://gitlab.com/yakoi/minetime',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    entry_points={
+        'console_scripts': [
+            'minetime=minetime.core:mine_time',
+        ],
+    }
+)
