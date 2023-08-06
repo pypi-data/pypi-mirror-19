37efcb44aee2565d4ed37d00c64b62aca07f09fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:50 +0000

    Add package easy_pika/0.1/easy_pika-0.1.tar.gz

---
+++ packages/easy_pika/easy_pika-0.1.tar.gz/easy_pika-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name = 'easy_pika',
+	packages = ['easy_pika'],
+	version = '0.1',
+	description = 'Encapsulation of pika that make our life easier.',
+	author = 'fzuwwl',
+	author_email = 'fzuwwl@gmail.com',
+	url = 'https://github.com/kikimo/easy_pika',
+	download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1',
+	keywords = ['pika', 'rabbit', 'mq', 'RabbitMQ'],
+	classifiers = [],
+)
