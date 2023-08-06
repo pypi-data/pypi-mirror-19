4a7167ed362b71eda3fde0808f24859a34f2a4d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:07 +0000

    Add package JamesLi123/1.0.0/JamesLi123-1.0.0.zip

---
+++ packages/JamesLi123/JamesLi123-1.0.0.zip/JamesLi123-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'JamesLi123',
+    version = '1.0.0',
+    py_modules = ['JamesLi123'],
+    author = 'James Li',
+    author_email = 'jinkuli123@gmail.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+)
