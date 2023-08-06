47102bca0581366b3dd0a555860d2c32c8caf3f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:38 +0000

    Add package mdt/0.9.9/mdt-0.9.9.tar.gz

---
+++ packages/mdt/mdt-0.9.9.tar.gz/mdt-0.9.9/mdt/data/components/standard/compartment_models/ExpT1DecTM.py
@@ -0,0 +1,12 @@
+from mdt.models.compartments import CompartmentConfig
+
+__author__ = 'Francisco J. Fritz'
+__date__ = "2016-09-06"
+__maintainer__ = "Robbert Harms"
+__email__ = "robbert.harms@maastrichtuniversity.nl"
+
+
+class ExpT1DecTM(CompartmentConfig):
+
+    parameter_list = ('SEf', 'TM', 'flip_angle', 'Refoc_fa1', 'Refoc_fa2', 'T1')
+    cl_code = 'return pow(0.5, SEf) * sin(flip_angle) * sin(Refoc_fa1) * sin(Refoc_fa2) * exp(-TM / T1);'
