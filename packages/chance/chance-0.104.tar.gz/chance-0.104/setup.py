7647e56a7723d2b21ac26e0f67029484423d9482
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:54 +0000

    Add package chance/0.104/chance-0.104.tar.gz

---
+++ packages/chance/chance-0.104.tar.gz/chance-0.104/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='chance',
+    packages=['chance'],
+    version='0.104',
+    description='Minimalist generator of random strings, numbers, etc.',
+    author='Davit Vardanyan',
+    author_email='davoimail@gmail.com',
+    url='https://github.com/davitv/chance',
+    download_url='http://github.com/davitv/chance/tarball/0.104',
+    keywords=['random', 'generator', 'chancejs'],
+    classifiers=[],
+)
