bccc2c0f0b520ba296083a0c351dd92418bd4359
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:13 +0000

    Add package ftw.recipe.translations/1.2.6/ftw.recipe.translations-1.2.6.tar.gz

---
+++ packages/ftw.recipe.translations/ftw.recipe.translations-1.2.6.tar.gz/ftw.recipe.translations-1.2.6/ftw/recipe/translations/testing.py
@@ -0,0 +1,61 @@
+from pkg_resources import get_distribution
+from plone.testing import Layer
+import shutil
+import tempfile
+import zc.buildout.testing
+
+
+def resolve_dependencies(pkg_name, result=None, extras=()):
+    if result is None:
+        result = set()
+
+    if pkg_name in ('setuptools', 'zc.buildout'):
+        return result
+
+    result.add(pkg_name)
+    for pkg in get_distribution(pkg_name).requires(extras):
+        resolve_dependencies(pkg.project_name, result)
+
+    return result
+
+
+class RecipeLayer(Layer):
+    extras = ()
+
+    @property
+    def globs(self):
+        # globs is required for zc.buildout.testing setup / tear down
+        if 'buildout' not in self:
+            self['buildout'] = {}
+        return self['buildout']
+
+    def testSetUp(self):
+        zc.buildout.testing.buildoutSetUp(self)
+        for pkgname in resolve_dependencies('ftw.recipe.translations',
+                                            extras=self.extras):
+            zc.buildout.testing.install_develop(pkgname, self)
+
+    def testTearDown(self):
+        zc.buildout.testing.buildoutTearDown(self)
+
+
+RECIPE_FIXTURE = RecipeLayer()
+
+
+class MasstranslateRecipeLayer(RecipeLayer):
+    extras = ('masstranslate',)
+
+
+MASSTRANSLATE_RECIPE_FIXTURE = MasstranslateRecipeLayer()
+
+
+class TempDirectory(Layer):
+
+    def testSetUp(self):
+        self['tempdir'] = tempfile.mkdtemp('ftw.recipe.translations')
+
+    def testTearDown(self):
+        shutil.rmtree(self['tempdir'])
+
+
+TEMP_DIRECTORY_FIXTURE = TempDirectory()
---
+++ packages/ftw.recipe.translations/ftw.recipe.translations-1.2.6.tar.gz/ftw.recipe.translations-1.2.6/setup.py
@@ -0,0 +1,68 @@
+import os
+from setuptools import setup, find_packages
+
+
+version = '1.2.6'
+
+extras = {
+    'tests': [
+        'mocker',
+        'plone.testing',
+        'unittest2',
+    ],
+
+    'masstranslate': [
+        'gspread',
+        'keyring',
+        'oauth2client < 2a',
+    ]
+}
+
+extras['tests'] += extras['masstranslate']
+
+
+setup(name='ftw.recipe.translations',
+      version=version,
+      description='Mass export / import of translations into' + \
+      ' Google Docs Spreadsheets',
+
+      long_description=open('README.rst').read() + '\n' + \
+      open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      classifiers=[
+          'Framework :: Plone',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+
+      keywords='ftw recipe translations import export google docs',
+      author='4teamwork AG',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/ftw.recipe.translations',
+
+      license='GPL2',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw', 'ftw.recipe'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+          'argparse',
+          'i18ndude',
+          'path.py',
+          'setuptools',
+          'zc.buildout',
+          'zc.recipe.egg',
+      ],
+
+      tests_require=extras['tests'],
+      extras_require=extras,
+
+      entry_points = {
+          'zc.buildout': [
+              'default = ftw.recipe.translations.masstranslate.recipe:Recipe',
+              'package = ftw.recipe.translations.i18nbuild.recipe:Recipe'],
+          'console_scripts': [
+              'masstranslate = ftw.recipe.translations.masstranslate.command:main',
+              'i18n-build = ftw.recipe.translations.i18nbuild.command:main']
+      })
