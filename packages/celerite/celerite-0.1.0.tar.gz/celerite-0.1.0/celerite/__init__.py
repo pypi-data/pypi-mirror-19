acf631e7a2009f3785fe17760067bd36c15d8319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:24 +0000

    Add package celerite/0.1.0/celerite-0.1.0.tar.gz

---
+++ packages/celerite/celerite-0.1.0.tar.gz/celerite-0.1.0/celerite/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "0.1.0"
+
+try:
+    __CELERITE_SETUP__
+except NameError:
+    __CELERITE_SETUP__ = False
+
+if not __CELERITE_SETUP__:
+    __all__ = ["Solver", "GP", "terms"]
+
+    from . import terms
+    from .celerite import GP
+    from .solver import Solver, get_library_version
+
+    __library_version__ = get_library_version()
