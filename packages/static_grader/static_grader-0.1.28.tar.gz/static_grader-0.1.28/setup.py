4252e5eb3153d03612e04d22724defd9d88e50d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:50 +0000

    Add package static_grader/0.1.28/static_grader-0.1.28.tar.gz

---
+++ packages/static_grader/static_grader-0.1.28.tar.gz/static_grader-0.1.28/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from static_grader import __version__
+
+setup(name='static_grader',
+  version=__version__,
+  description='A Static grader',
+  author='The Data Incubator',
+  packages=['static_grader'],
+  install_requires=[
+    'requests',
+    'numpy>=1.6.1'
+  ]
+)
