1c9cade903c5c72ff9bd57ebf966f9b5a4a5d815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:22 +0000

    Add package bayesloop/1.1.3/bayesloop-1.1.3.zip

---
+++ packages/bayesloop/bayesloop-1.1.3.zip/bayesloop-1.1.3/bayesloop/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# import study types
+from .core import *
+
+# observation models and transition models need to be distinguishable
+from . import observationModels
+from . import observationModels as om  # short form
+from . import transitionModels
+from . import transitionModels as tm  # short form
+
+# misc
+from helper import cint, oint
+from .jeffreys import *
+from .fileIO import *
---
+++ packages/bayesloop/bayesloop-1.1.3.zip/bayesloop-1.1.3/bayesloop/fileIO.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+"""
+This file includes functions for saving and loading instances of Study objects using dill.
+"""
+
+from __future__ import division, print_function
+
+
+def save(filename, study):
+    """
+    Save an instance of a bayesloop study class to file.
+
+    Args:
+        filename(str): Path + filename to store bayesloop study
+        study: Instance of study class (Study, RasterStudy, etc.)
+    """
+    try:
+        import dill
+    except ImportError:
+        raise ImportError('No module named dill. This module represents an optional dependency of bayesloop and is '
+                          'therefore not installed alongside bayesloop.')
+
+    with open(filename, 'wb') as f:
+        dill.dump(study, f, protocol=dill.HIGHEST_PROTOCOL)
+    print('+ Successfully saved current study.')
+
+
+def load(filename):
+    """
+    Load a instance of a bayesloop study class that was saved using the bayesloop.save() function.
+
+    Args:
+        filename(str): Path + filename to stored bayesloop study
+
+    Returns:
+        Study instance
+    """
+    try:
+        import dill
+    except ImportError:
+        raise ImportError('No module named dill. This module represents an optional dependency of bayesloop and is '
+                          'therefore not installed alongside bayesloop.')
+
+    with open(filename, 'rb') as f:
+        S = dill.load(f)
+    print('+ Successfully loaded study.')
+
+    return S
