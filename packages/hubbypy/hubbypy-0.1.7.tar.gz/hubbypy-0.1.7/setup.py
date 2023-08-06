c85c73aee458e01c161d7d7ca2f31d2bdea6092c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:30 +0000

    Add package hubbypy/0.1.7/hubbypy-0.1.7.tar.gz

---
+++ packages/hubbypy/hubbypy-0.1.7.tar.gz/hubbypy-0.1.7/setup.py
@@ -0,0 +1,40 @@
+import os.path
+
+import setuptools
+
+root_dir = os.path.abspath(os.path.dirname(__file__))
+
+description = "A Python wrapper for the HubSpot contacts and contact properties API"
+
+with open(os.path.join(root_dir, 'README.md')) as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='hubbypy',
+    version='0.1.7',
+    description=description,
+    long_description=long_description,
+    url='https://github.com/gojefferson/hubbypy',
+    author='Jeff Kerr',
+    author_email='jeff@casefleet.com',
+    license='BSD',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    py_modules=[
+        'hubbypy.contact_properties',
+        'hubbypy.hub_api',
+    ],
+    install_requires=[
+        'requests',
+    ],
+)
