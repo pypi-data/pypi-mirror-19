89bd13dec72c42a397e347bc232b9bcafc081577
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:01 +0000

    Add package todoist-tracker/0.1.3/todoist-tracker-0.1.3.tar.gz

---
+++ packages/todoist-tracker/todoist-tracker-0.1.3.tar.gz/todoist-tracker-0.1.3/todoist_tracker/cli/__init__.py
@@ -0,0 +1,21 @@
+import argparse
+
+from . import overdue
+from .utils import get_command_line_parser
+
+COMMAND_MODULES = (overdue, )
+
+
+def get_cli_parser():
+    command_line_parser = argparse.ArgumentParser(
+        description="calculate and log various todoist statistics",
+    )
+    return get_command_line_parser(command_line_parser, COMMAND_MODULES)
+
+
+def run_subcommand(args):
+    """This function runs the command that is selected by this particular
+    subcommand parser.
+    """
+    command = args.__dict__.pop("command")
+    command.execute(**args.__dict__)
---
+++ packages/todoist-tracker/todoist-tracker-0.1.3.tar.gz/todoist-tracker-0.1.3/todoist_tracker/cli/overdue/priority.py
@@ -0,0 +1,42 @@
+import datetime
+
+from ..base import BaseCommand
+from ...overdue import get_overdue_items
+
+
+class Command(BaseCommand):
+    help_text = (
+        "Count the number of overdue tasks by priority"
+    )
+
+    def add_command_line_options(self):
+        super(Command, self).add_command_line_options()
+
+    def execute(self, **kwargs):
+        super(Command, self).execute(**kwargs)
+
+        overdue_items = get_overdue_items(self.todoist_api)
+
+        counter = {'p1': 0, 'p2': 0, 'p3': 0, 'p4': 0}
+        for item in overdue_items:
+            priority = 'p' + str(4 - item['priority'] + 1)
+            counter[priority] += 1
+
+        if kwargs['debug']:
+            for priority, count in sorted(counter.iteritems()):
+                print '%4d overdue %s priority items' % (count, priority)
+            print '-' * 30
+            print '%4d overdue items TOTAL' % len(overdue_items)
+        else:
+            worksheet = self.get_or_create_worksheet(
+                'overdue priority',
+                ['date', 'n', 'p1', 'p2', 'p3', 'p4'],
+            )
+            worksheet.insert_row([
+                datetime.datetime.now().date(),
+                len(overdue_items),
+                counter['p1'],
+                counter['p2'],
+                counter['p3'],
+                counter['p4'],
+            ], 2)
