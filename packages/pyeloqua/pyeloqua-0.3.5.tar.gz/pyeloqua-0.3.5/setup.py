16319752aebf16bda32d7cc74bc1554f6852b40c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:16 +0000

    Add package pyeloqua/0.3.5/pyeloqua-0.3.5.tar.gz

---
+++ packages/pyeloqua/pyeloqua-0.3.5.tar.gz/pyeloqua-0.3.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+__version__ = '0.3.5'
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='pyeloqua',
+    version=__version__,
+    description='Python wrapper functions for Eloqua APIs',
+    long_description=readme(),
+    url='https://github.com/colemanja91/pyeloqua',
+    author='Jeremiah Coleman',
+    author_email='colemanja91@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'pbr',
+        'six'
+    ],
+    zip_safe=False,
+    keywords = 'eloqua marketing automation api bulk'
+)
