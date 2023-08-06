e44915c1c5b299e8ceb8f0e8370c0ecf293f0b15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:42 +0000

    Add package tcconfig/0.7.0a3/tcconfig-0.7.0a3-py2.py3-none-any.whl

---
+++ packages/tcconfig/tcconfig-0.7.0a3-py2.py3-none-any.whl/tcconfig/_common.py
@@ -0,0 +1,93 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
+
+from __future__ import absolute_import
+import contextlib
+
+import dataproperty
+import logbook
+import six
+
+from ._error import NetworkInterfaceNotFoundError
+from ._logger import logger
+
+
+ANYWHERE_NETWORK = "0.0.0.0/0"
+
+
+@contextlib.contextmanager
+def logging_context(name):
+    logger.debug("|---- {:s}: {:s} -----".format("start", name))
+    try:
+        yield
+    finally:
+        logger.debug("----- {:s}: {:s} ----|".format("complete", name))
+
+
+def verify_network_interface(device):
+    try:
+        import netifaces
+    except ImportError:
+        return
+
+    if device not in netifaces.interfaces():
+        raise NetworkInterfaceNotFoundError(
+            "network interface not found: {}".format(device))
+
+
+def sanitize_network(network):
+    """
+    :return: Network string
+    :rtype: str
+    :raises ValueError: if the network string is invalid.
+    """
+
+    import ipaddress
+
+    if dataproperty.is_empty_string(network):
+        return ""
+
+    if network.lower() == "anywhere":
+        return ANYWHERE_NETWORK
+
+    try:
+        ipaddress.IPv4Address(six.u(network))
+        return network + "/32"
+    except ipaddress.AddressValueError:
+        pass
+
+    ipaddress.IPv4Network(six.u(network))  # validate network str
+
+    return network
+
+
+def run_command_helper(command, error_regexp, message, exception=None):
+    import subprocrunner as spr
+
+    if logger.level != logbook.DEBUG:
+        spr.set_logger(is_enable=False)
+
+    proc = spr.SubprocessRunner(command)
+    proc.run()
+
+    if logger.level != logbook.DEBUG:
+        spr.set_logger(is_enable=True)
+
+    if proc.returncode == 0:
+        return 0
+
+    match = error_regexp.search(proc.stderr)
+    if match is None:
+        logger.error(proc.stderr)
+        return proc.returncode
+
+    if dataproperty.is_not_empty_string(message):
+        logger.notice(message)
+
+    if exception is not None:
+        raise exception(command)
+
+    return proc.returncode
