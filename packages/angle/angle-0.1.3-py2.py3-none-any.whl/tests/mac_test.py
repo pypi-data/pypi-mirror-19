40196f351b56ca92096758eabbd934b78abd0a01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:13 +0000

    Add package angle/0.1.3/angle-0.1.3-py2.py3-none-any.whl

---
+++ packages/angle/angle-0.1.3-py2.py3-none-any.whl/tests/mac_test.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import angle
+
+from tests.parser_test_helper import *
+
+
+class MacTest(ParserBaseTest,unittest.TestCase):
+    
+
+    def test_mail(self):
+        pass
+
+
+    def test_bash2(self):
+        parse('exec ls')
+        parse('bash ls')
+        parse('ls "."')
+        parse('ls')
+        parse('x=ls')
+
+    def test_bash(self):
+        assert_result_is("½*2",1)
+
+    # parse('x=ls\na=x[1]')
+        # parse('x=ls;a=x[1]')#// fucked up a=
+        # parse('`ls`')
+
+
+    def test_applescript(self):
+        if (ENV['APPLE']): skip()
+        parse('Tell application "Finder" to open home')
+
+    def test_files(self):
+        if (ENV['APPLE']): skip()
+        variables['x'] = ['/Users/me', ]
+        variables['my home folder'] = ['/Users/me', ]
+        assert('/Users/me == x')
+        assert('my home folder == /Users/me')
+        assert('my home folder == x')
+
+    def test_files3(self):
+        if (ENV['APPLE'], ):
+            skip()
+        skip()
+        init('my home folder = Dir.home')
+        self.parser.setter()
+        init('my home folder == /Users/me')
+        self.parser.condition()
+        init('/Users/me/.bashrc ok')
+        p = self.parser.linuxPath()
+        init('Dir.home')
+        r = self.parser.rubyThing()
+        parse('x := /Users/me ')
+        assert('my home folder == /Users/me')
+        assert('/Users/me == x')
+        assert('my home folder == x')
+
+    def test_variable_transitivity(self):
+        if (ENV['APPLE'], ):
+            skip()
+        parse('my home folder = Dir.home ')
+        parse('xs= my home folder ')
+        assert('xs = /Users/me')
+
+    def test_contains_file(self):
+        if (ENV['APPLE'], ):
+            skip()
+        parse('xs= all files in Dir.home')
+        p(variables['xs'])
+        assert('xs contains .bashrc')
+        parse('xs= Dir.home')
+        assert('xs contains .bashrc')
+        parse('xs=/Users/me')
+        assert('xs contains .bashrc')
+        parse('my home folder = Dir.home')
+        parse('my home folder is Dir.home')
+        p(variables())
+        p(variableValues())
+        assert()
+
+    def test_contains_file2(self):
+        if (ENV['APPLE'], ):
+            skip()
+        parse('my home folder = Dir.home')
+        parse('xs = my home folder ')
+        parse('xs = files in my home folder ')
+        assert('xs contains .bashrc')
+        skip()
+        parse('xs = all files in my home folder ')
+        parse('xs shall be all files in my home folder ')
---
+++ packages/angle/angle-0.1.3-py2.py3-none-any.whl/tests/observer_test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import angle
+from tests.parser_test_helper import *
+
+class ObserverTest(ParserBaseTest,unittest.TestCase):
+    
+
+    def _test_every_date1(self):
+        parse('every 1 seconds { say "Ja!" }')
+        parse('every 2 seconds do say "OK"')
+        sleep(10000)
+
+    def _test_every_date(self):
+        parse('every 1 seconds do say "OK"')
+        parse('beep every three seconds')
+        parse('every two seconds puts "YAY"')
+        parse('every minute puts "HURRAY"')
+        parse('every five seconds do say "OK"')
+        sleep(10000)
+
+    def test_whenever(self):
+        parse('var x;beep whenever x is 5')
+        parse('beep once x is 5')
+        parse('once x is 5 do beep')
+        parse('once x is 5 beep ')
+        parse('x is 5')
+
+    def test_whenever_2(self):
+        skip('test this later')
+        parse('whenever the clock shows five seconds do beep')
+        assert(self.result.equals('1/3'))
