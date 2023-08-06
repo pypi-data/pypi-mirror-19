82cb48544fdec4fd8bec27a70a56b21e642f1cfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:06 +0000

    Add package wl_api/0.1.3/wl_api-0.1.3.tar.gz

---
+++ packages/wl_api/wl_api-0.1.3.tar.gz/wl_api-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'wl_api',
+  packages = ['wl_api'],
+  version = '0.1.3',
+  description = 'a wrapper library for the Warlight API',
+  author = 'knyte',
+  author_email = 'galactaknyte@gmail.com',
+  url = 'https://github.com/knyte/wl_api',
+  download_url = 'https://github.com/knyte/wl_api/tarball/0.1.3',
+  keywords = ['warlight', 'wrapper', 'api'],
+  classifiers = [],
+)
---
+++ packages/wl_api/wl_api-0.1.3.tar.gz/wl_api-0.1.3/wl_api/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+wl_api
+~~~~~~
+
+A wrapper library for the Warlight public API
+
+"""
+
+__version__ = '0.1.3'
+__author__ = 'knyte'
+
+from .wl_api import *
