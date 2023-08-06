f5700d01e5e56de3622eed98154da597e420f7b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:22 +0000

    Add package Marina-decide/0.13/Marina-decide-0.13.tar.gz

---
+++ packages/Marina-decide/Marina-decide-0.13.tar.gz/Marina-decide-0.13/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'Marina-decide',
+  packages = ['Marina-decide'], # this must be the same as the name above
+  version = '0.13',
+  description = 'Decision making tool',
+  package_dir = {'Marina-decide': 'lib'},
+  author = 'Stefan Nožinić',
+  author_email = 'stefan@lugons.org',
+  url = 'https://github.com/fantastic001/Marina', # use the URL to the github repo
+  download_url = 'https://github.com/fantastic001/Marina/tarball/0.13', 
+  keywords = ['decision-making', 'scoring', 'organization', 'lifehack'], # arbitrary keywords
+  classifiers = [],
+  scripts = ["marina-build", "marina-decide"],
+)
