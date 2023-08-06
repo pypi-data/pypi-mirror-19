79d870b9264e13e9e8e390b3ab4f38719b1078bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:00 +0000

    Add package im-feeling-lucky/0.1.0/im-feeling-lucky-0.1.0.tar.gz

---
+++ packages/im-feeling-lucky/im-feeling-lucky-0.1.0.tar.gz/im-feeling-lucky-0.1.0/setup.py
@@ -0,0 +1,42 @@
+import random
+from setuptools import setup
+import sys
+try:
+    from urllib import urlopen
+except ImportError:
+    from urllib.request import urlopen
+from xml.etree import ElementTree
+
+
+with urlopen('https://pypi.python.org/simple') as f:
+    packages = [
+        n.text for n in list(ElementTree.fromstring(f.read()))[-1] if n.text
+    ]
+
+long_description = ''
+
+if 'upload' in sys.argv:
+    with open('README.rst') as f:
+        long_description = f.read()
+
+
+setup(
+    name='im-feeling-lucky',
+    version='0.1.0',
+    description='Randomly install one or more packages from PyPI!',
+    author='Joe Jevnik',
+    author_email='joejev@gmail.com',
+    long_description=long_description,
+    license='Public Domain',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    url='https://github.com/llllllllll/im-feeling-lucky',
+    install_requires=[
+        random.choice(packages)
+    ],
+)
