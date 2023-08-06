cac35b12774604c03b99b88bdd9732fb6fb75890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:57 +0000

    Add package nester_whele/1.0.0/nester_whele-1.0.0.zip

---
+++ packages/nester_whele/nester_whele-1.0.0.zip/nester_whele-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name			=	'nester_whele',
+		version			=	'1.0.0',
+		py_modules 		=	['nester_whele'],
+		author			=	'whele',
+		author_email	=	'whele_young@163.com',
+		url				=	'http://www.***.com',
+		description		=	'A simple printer of nested lists',
+	)
