c2ed5cdf8173cceb7bc25c201a618e55353512ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:44 +0000

    Add package ares-util/0.1.11/ares_util-0.1.11.tar.gz

---
+++ packages/ares-util/ares_util-0.1.11.tar.gz/ares_util-0.1.11/setup.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+
+import io
+
+from setuptools import setup
+
+setup(
+    name='ares_util',
+    version='0.1.11',
+    description='A tool for information system allowing a retrieval '
+                'of information on economic entities registered in '
+                'the Czech Republic (ARES - Access to Registers of Economic Subjects / Entities).',
+    long_description=io.open("README.rst", 'r', encoding="utf-8").read(),
+    url='https://github.com/illagrenan/ares_util',
+    license='MIT',
+    author='Vasek Dohnal',
+    author_email='vaclav.dohnal@gmail.com',
+    packages=['ares_util'],
+    install_requires=['xmltodict', 'requests'],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Environment :: Console',
+        'Intended Audience :: Developers'
+    ],
+    test_suite='tests',
+    tests_require=[
+        'responses'
+    ]
+)
