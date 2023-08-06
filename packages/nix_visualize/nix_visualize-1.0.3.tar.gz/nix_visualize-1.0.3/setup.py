7b0027ccdf16ef480ac6f3dca88467123facdd47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:30 +0000

    Add package nix_visualize/1.0.3/nix_visualize-1.0.3.tar.gz

---
+++ packages/nix_visualize/nix_visualize-1.0.3.tar.gz/nix_visualize-1.0.3/setup.py
@@ -0,0 +1,27 @@
+import os
+import setuptools
+import sys
+
+PACKAGE_NAME = "nix_visualize"
+VERSION = "1.0.3"
+setuptools.setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    packages=[PACKAGE_NAME],
+    description="CLI to automate generation of pretty Nix dependency trees",
+    author="Craig Booth",
+    author_email="craigmbooth@gmail.com",
+    url="https://github.com/craigmbooth/nix-dependency-visualizer",
+    download_url = "https://github.com/craigmbooth/nix-dependency-visualizer/tarball/"+VERSION,
+    keywords=["nix", "matplotlib"],
+    classifiers=[],
+    install_requires=[
+        "matplotlib>=1.5",
+        "networkx>=1.11",
+        "pygraphviz>=1.3"
+    ],
+    data_files = [],
+    entry_points={"console_scripts": [
+        "nix-visualize=nix_visualize.visualize_tree:main"
+    ]}
+)
