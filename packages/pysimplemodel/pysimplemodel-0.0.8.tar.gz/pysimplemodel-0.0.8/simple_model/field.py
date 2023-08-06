028c33cf8472eac35291b9be74bc15ad034698a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:32 +0000

    Add package pysimplemodel/0.0.8/pysimplemodel-0.0.8.tar.gz

---
+++ packages/pysimplemodel/pysimplemodel-0.0.8.tar.gz/pysimplemodel-0.0.8/simple_model/field.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2017 Luiz Menezes
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+from typing import List
+
+from .exceptions import EmptyField
+
+
+class ModelField:
+    def __init__(self, model, name, value, allow_empty):
+        self._model = model
+        self.name = name
+        self.value = value
+        self.allow_empty = allow_empty
+
+        try:
+            self._validate = getattr(model, 'validate_{}'.format(name))
+        except AttributeError:
+            self._validate = None
+
+    def validate(self):
+        if not self.allow_empty and not self._model.is_empty(self.value):
+            raise EmptyField(self.name)
+
+        if not self._validate:
+            return
+
+        self._validate(self.value)
+
+    def serialize(self):
+        try:
+            serialized = self.value.serialize()
+        except AttributeError:
+            serialized = None
+
+        if isinstance(self.value, (List, tuple)):
+            serialized = [field.serialize() for field in self.value]
+
+        return serialized or self.value
