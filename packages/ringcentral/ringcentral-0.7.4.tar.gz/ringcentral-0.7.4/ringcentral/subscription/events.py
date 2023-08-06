6ab0915347f411954ff921553e054ead47720706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:35 +0000

    Add package ringcentral/0.7.4/ringcentral-0.7.4.tar.gz

---
+++ packages/ringcentral/ringcentral-0.7.4.tar.gz/ringcentral-0.7.4/ringcentral/subscription/events.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+class Events:
+    connectionError = 'connectionError'
+    notification = 'notification'
+    subscribeSuccess = 'subscribeSuccess'
+    subscribeError = 'subscribeError'
+    renewSuccess = 'renewSuccess'
+    renewError = 'renewError'
+    removeSuccess = 'removeSuccess'
+    removeError = 'removeError'
+
+    def __init__(self):
+        pass
