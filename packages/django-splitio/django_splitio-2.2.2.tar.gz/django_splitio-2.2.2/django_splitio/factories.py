14897439fee79026bfe9e79e7873896f90a4bfb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:24 +0000

    Add package django-splitio/2.2.2/django_splitio-2.2.2.tar.gz

---
+++ packages/django-splitio/django_splitio-2.2.2.tar.gz/django_splitio-2.2.2/django_splitio/factories.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from django.conf import settings
+
+from splitio.factories import get_factory as get_splitio_factory
+
+from .settings import splitio_settings
+
+
+def django_split_factory():
+    pass
+
+def get_factory():
+    """Returns a split.io client/manager factory implementation using the configuration given
+        in the SPLITIO section of the Django settings.
+        :return: A split.io client/manager factory implementation
+        :rtype: splitio.factories.SplitFactory
+        """
+    # Get config from Django settings
+    sdk_config = settings.SPLITIO
+
+    api_key = ''
+    if 'apiKey' in sdk_config:
+      api_key = sdk_config['apiKey']
+
+    return get_splitio_factory(api_key, config=sdk_config)
