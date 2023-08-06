553f0d83d7380608043eb0357de7d1d9698ad671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:37 +0000

    Add package undict/0.1.0/undict-0.1.0.tar.gz

---
+++ packages/undict/undict-0.1.0.tar.gz/undict-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='undict',
+      version='0.1.0',
+      packages = ['undict'],
+      # find_packages(),
+      author='firstprayer',
+      author_email='zhangty10@gmail.com',
+      description='Undict - Easier API for using Dictionary, especially with json',
+      url='https://github.com/firstprayer/undict.git',
+      install_requires=[],
+)
