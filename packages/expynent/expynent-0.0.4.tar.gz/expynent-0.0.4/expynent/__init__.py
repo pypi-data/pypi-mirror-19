e7c5f088be40b32cc590ed2d8a801aad9a03df0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:20 +0000

    Add package expynent/0.0.4/expynent-0.0.4.tar.gz

---
+++ packages/expynent/expynent-0.0.4.tar.gz/expynent-0.0.4/expynent/__init__.py
@@ -0,0 +1,10 @@
+"""
+:copyright: (c) 2016 by Isaak Uchakaev <likid.geimfari@gmail.com>.
+:software_license: BSD 3-Clause, see LICENSES for more details.
+:repository: https://github.com/lk-geimfari/expynent
+"""
+
+from .patterns import *
+
+__version__ = '0.0.4'
+__author__ = 'Likid Geimfari'
