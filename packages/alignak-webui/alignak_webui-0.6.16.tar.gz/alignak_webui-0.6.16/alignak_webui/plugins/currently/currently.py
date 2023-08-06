79adb0d549c9dc0a48b4edaa435926fde0cfbf0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:49 +0000

    Add package alignak-webui/0.6.16/alignak_webui-0.6.16.tar.gz

---
+++ packages/alignak-webui/alignak_webui-0.6.16.tar.gz/alignak_webui-0.6.16/alignak_webui/plugins/currently/currently.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2015-2017:
+#   Frederic Mohier, frederic.mohier@alignak.net
+#
+# This file is part of (WebUI).
+#
+# (WebUI) is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# (WebUI) is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with (WebUI).  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+    Plugin Currently
+"""
+
+from logging import getLogger
+
+from bottle import request
+
+from alignak_webui import _
+from alignak_webui.utils.plugin import Plugin
+
+# pylint: disable=invalid-name
+logger = getLogger(__name__)
+
+
+class PluginCurrently(Plugin):
+    """ Currently plugin """
+
+    def __init__(self, app, cfg_filenames=None):
+        """
+        Currently plugin
+        """
+        self.name = 'Currently'
+        self.backend_endpoint = None
+
+        self.pages = {
+            'get_currently': {
+                'name': 'Currently',
+                'route': '/currently',
+                'view': 'currently'
+            }
+        }
+
+        super(PluginCurrently, self).__init__(app, cfg_filenames)
+
+    def get_currently(self):  # pylint:disable=no-self-use
+        """
+        Display currently page
+        """
+        user = request.environ['beaker.session']['current_user']
+        datamgr = request.app.datamgr
+
+        # Get the stored panels
+        currently_panels = datamgr.get_user_preferences(user, 'currently_panels', None)
+
+        # Get the stored graphs
+        currently_graphs = datamgr.get_user_preferences(user, 'currently_graphs', None)
+
+        # Live state stored queuers
+        # hosts_states_queue = datamgr.get_user_preferences(user, 'hosts_states_queue', [])
+        # services_states_queue = datamgr.get_user_preferences(user, 'services_states_queue', [])
+        lv = datamgr.get_livesynthesis({'sort': '_created'})
+        hosts_states_queue = lv['hosts_synthesis']
+        logger.warning("Hosts state queue: %s", hosts_states_queue)
+        services_states_queue = lv['services_synthesis']
+
+        return {
+            'currently_panels': currently_panels,
+            'currently_graphs': currently_graphs,
+            'hosts_states_queue': hosts_states_queue,
+            'services_states_queue': services_states_queue,
+            'title': request.query.get('title', _('Keep an eye'))
+        }
