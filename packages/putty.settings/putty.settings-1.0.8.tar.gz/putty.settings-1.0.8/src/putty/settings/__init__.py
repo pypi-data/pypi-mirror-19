f2c5aa56fc050783e0f21ebd244c96be80b10ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:19 +0000

    Add package putty.settings/1.0.8/putty.settings-1.0.8.tar.gz

---
+++ packages/putty.settings/putty.settings-1.0.8.tar.gz/putty.settings-1.0.8/src/putty/settings/__init__.py
@@ -0,0 +1,1 @@
+from .ssh_host_keys import SshHostKeys, HostKeyEntry, putty_to_known_host, known_host_to_putty, FileFormat
---
+++ packages/putty.settings/putty.settings-1.0.8.tar.gz/putty.settings-1.0.8/src/putty/settings/ssh_host_keys/ssh_host_keys_nix.py
@@ -0,0 +1,10 @@
+class SshHostKeys:
+    pass
+
+
+def putty_to_known_host(*args, **kwargs):
+    pass
+
+
+def known_host_to_putty(*args, **kwargs):
+    pass
