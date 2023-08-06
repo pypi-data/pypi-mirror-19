bef79fb84ee20f5f752e8f54aad9cfe7aa465486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:00 +0000

    Add package fabutils/0.0.19/fabutils-0.0.19.tar.gz

---
+++ packages/fabutils/fabutils-0.0.19.tar.gz/fabutils-0.0.19/src/fabutils/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+This is a helper package that contains all the code used by the project
+fabfile. This package was created to keep the fabfile clean.
+"""
+from __future__ import absolute_import
