d6a0cd1aaa7d8be2d919413fef10b475406a0dbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:26 +0000

    Add package aetros/0.5.0/aetros-0.5.0.tar.gz

---
+++ packages/aetros/aetros-0.5.0.tar.gz/aetros-0.5.0/aetros/__main__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+import sys
+import aetros
+
+if __name__ == '__main__':
+    sys.exit(aetros.main())
---
+++ packages/aetros/aetros-0.5.0.tar.gz/aetros-0.5.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from setuptools import find_packages
+import aetros
+
+setup(name='aetros',
+      version=aetros.const.__version__,
+      description='Python SDK for aetros.com',
+      author='Marc J. Schmidt',
+      author_email='marc@marcjschmidt.de',
+      url='https://github.com/aetros/aetros-cli',
+      download_url='https://github.com/aetros/aetros-cli/tarball/' + aetros.const.__version__,
+      license='MIT',
+      entry_points={
+          'console_scripts': ['aetros = aetros:main'],
+      },
+      install_requires=('keras>=1.0.6',
+                        'requests',
+                        'numpy',
+                        'scipy',
+                        'h5py',
+                        'psutil',
+                        'image',
+                        'cherrypy>=7.1.0',
+                        'py-cpuinfo==0.2.3'
+                        ),
+      packages=['aetros'])
