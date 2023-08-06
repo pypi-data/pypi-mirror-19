bad6c558311da39978df35e292ddaa32c934c658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:48 +0000

    Add package toil-nanopore/0.2.13/toil-nanopore-0.2.13.tar.gz

---
+++ packages/toil-nanopore/toil-nanopore-0.2.13.tar.gz/toil-nanopore-0.2.13/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="toil-nanopore",
+      version="0.2.13",
+      description="Toil-script for performing MinION sequence analysis",
+      author="Benedict Paten and Art Rand",
+      author_email="benedict@soe.ucsc.edu, arand@soe.ucsc.edu",
+      url="https://github.com/ArtRand/toil-marginAlign",
+      package_dir={"": "src"},
+      packages=find_packages("src"),
+      install_requires=["PyYAML==3.12",
+                        "marginAlign==1.1.3",
+                        "toil==3.5.0a1.dev325",
+                        "toil-lib==1.2.0a1.dev139"],
+      entry_points={
+          "console_scripts": ["toil-nanopore = toil_nanopore.toil_nanopore_pipeline:main"]})
