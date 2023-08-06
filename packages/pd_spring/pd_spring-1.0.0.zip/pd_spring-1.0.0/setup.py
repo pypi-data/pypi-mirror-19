5643af322bf86a0c1a7f1aa25d8579818b1949d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:35 +0000

    Add package pd_spring/1.0.0/pd_spring-1.0.0.zip

---
+++ packages/pd_spring/pd_spring-1.0.0.zip/pd_spring-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'pd_spring',
+    version = '1.0.0',
+    py_modules = ['pd_spring'],
+    author = 'zheyang',
+    author_email = '1059708676@qq.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of list',
+    )
