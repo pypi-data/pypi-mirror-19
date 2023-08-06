4af66357e4a79d4d92e8c6ae9560f0cd3c7291e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:35 +0000

    Add package pymeboot/0.0.1.post1/pymeboot-0.0.1.post1.tar.gz

---
+++ packages/pymeboot/pymeboot-0.0.1.post1.tar.gz/pymeboot-0.0.1.post1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name = 'pymeboot',
+    packages = ['pymeboot'],
+    version = '0.0.1-1',
+    description = "Maximum entropy bootstrap for time series",
+    author = 'Kira Huselius Gylling',
+    author_email = 'kira.gylling@gmail.com',
+    url = 'https://github.com/kirahg/pymeboot',
+    download_url = 'https://github.com/kirahg/pymeboot/tarball/0.0.1-1',
+    install_requires = [],
+    license = 'MIT',
+    keywords = ['data', 'statistics', 'time series', 'bootstrap'],
+    classifiers = [],
+)
