7c74b9605e6a60e4f926c31f5dd051456222eb42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:48 +0000

    Add package menhir/0.0.25/menhir-0.0.25.tar.gz

---
+++ packages/menhir/menhir-0.0.25.tar.gz/menhir-0.0.25/menhir/tools/pytest.py
@@ -0,0 +1,58 @@
+"""
+The pytest tool invokes pytest based tests.
+"""
+import logging
+
+from menhir.tool import Tool
+from menhir.tool_utils import (
+    OK,
+    FAIL,
+    NOTHING_TO_DO,
+    package_script,
+    tool_env,
+    working_dir,
+)
+
+log = logging.getLogger(__name__)
+
+
+def tool():
+    return PyTest()
+
+
+class PyTest(Tool):
+
+    def is_using_tool(tool, path, info):
+        from os.path import exists, join
+        path = join(path, 'setup.cfg')
+        if exists(path):
+            with open(path, "r") as file:
+                data = file.read()
+                return 'pytest' in data
+
+    def dependencies(tool, path):
+        return []
+
+    def add_arg_parser(tool, parser):
+        parser.description = "Invoke pytest based tests"
+        parser.add_argument('args', nargs='*', help='pytest arguments')
+
+    def execute_build_phase(tool, path, info, args,):
+        """Execute a build phase."""
+        import subprocess
+        from menhir.project import project_name_for_dir
+
+        log.info('pytest: %s %s %s', path, info, args)
+        if info.get('changed') or info.get('changed_dependents'):
+            log.info('Running pytest in %s', path)
+            project_name = project_name_for_dir(path)
+            env = tool_env()
+            env['MENHIR_PROJECT'] = project_name
+            with package_script("/tools/pytest/test.sh") as f:
+                with working_dir(path):
+                    res = subprocess.call([f.name] + args.args, env=env)
+                    if res:
+                        return FAIL
+                    return OK
+        else:
+            return NOTHING_TO_DO
