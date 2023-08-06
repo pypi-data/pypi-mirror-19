6bc228f5470357d35abfeb85460cbd244f451be0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:44 +0000

    Add package pypi-duplicate-file-test/1.0/pypi-duplicate-file-test-1.0.tar.gz

---
+++ packages/pypi-duplicate-file-test/pypi-duplicate-file-test-1.0.tar.gz/pypi-duplicate-file-test-1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    url='http://example.com/',
+    author='ero-sennin',
+    author_email='ag+services@sologoc.com',
+    name='pypi-duplicate-file-test',
+    version='1.0',
+    packages=['pypi_duplicate_file_test'],
+    description='A test package for duplicate release files. See https://sourceforge.net/p/pypi/support-requests/704/'
+)
