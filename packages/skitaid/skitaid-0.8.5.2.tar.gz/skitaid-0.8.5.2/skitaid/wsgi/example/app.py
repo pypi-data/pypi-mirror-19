007ece820be55c5a47fedd57378ace8a02a6beea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:54 +0000

    Add package skitaid/0.8.5.2/skitaid-0.8.5.2.tar.gz

---
+++ packages/skitaid/skitaid-0.8.5.2.tar.gz/skitaid-0.8.5.2/skitaid/wsgi/example/app.py
@@ -0,0 +1,67 @@
+from skitai.saddle import Saddle
+import skitai
+
+app = Saddle (__name__)
+
+app.debug = True
+app.use_reloader = True
+app.jinja_overlay ()
+
+@app.route ("/")
+def index (was):	
+	return was.render ("index.html")
+
+@app.route ("/documentation")
+def documentation (was):
+	req = was.get ("https://pypi.python.org/pypi/skitai")
+	pypi_content = (
+			"<h4>"
+			"<p>It seems some problem at <a href='https://pypi.python.org/pypi/skitai'>PyPi</a>.</p>"
+			"</h4>"	
+			"<p>Please visit <a href='https://pypi.python.org/pypi/skitai'> https://pypi.python.org/pypi/skitai</a></p>"
+		)
+	
+	rs = req.getwait (10)
+	if rs.data	:
+		content = rs.data.decode ("utf8")
+		s = content.find ('<div class="section">')
+		if s != -1:		
+			e = content.find ('<a name="downloads">', s)
+			if e != -1:						
+				pypi_content = "<h4>This contents retrieved right now using skitai was service from <a href='https://pypi.python.org/pypi/skitai'> https://pypi.python.org/pypi/skitai</a></h4>" + content [s:e]
+	
+	return was.render ("documentation.html", content = pypi_content)
+
+@app.route ("/hello")
+def hello (was, num = 1):
+	was.response ["Content-Type"] = "text/plain"
+	return "\n".join (["hello" for i in range (int(num))])
+
+@app.route ("/redirect1")
+def redirect1 (was):
+	return was.response ("301 Object Moved", "", headers = [("Location", "/redirect2")])
+
+@app.route ("/redirect2")
+def redirect2 (was):
+	return was.response ("301 Object Moved", "", headers = [("Location", "/")])
+
+@app.route ("/upload")
+def upload (was, **karg):
+	return was.response ("200 OK", str (karg), headers = [("Content-Type", "text/plain")])
+
+@app.route ("/post")
+def post (was, username):	
+		return 'USER: %s' % username
+
+	
+if __name__ == "__main__":
+	import skitai
+	skitai.run (
+		address = "0.0.0.0",
+		port = 5000,
+		mount = [
+			("/", app),
+			("/", 'static')
+		]	
+	)
+	
