20a27c169bb21c67eb40e1ac5f9f84053146329f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:15:56 +0000

    Add package astrodash/0.2.11/astrodash-0.2.11-py2.py3-none-any.whl

---
+++ packages/astrodash/astrodash-0.2.11-py2.py3-none-any.whl/dash/__init__.py
@@ -0,0 +1,30 @@
+import sys
+import os
+import pickle
+mainDirectory = os.path.dirname(os.path.abspath(__file__))
+#from main import *
+from PyQt4 import QtGui
+
+from classify import Classify
+
+
+def main():
+    classification = Classify(filenames=['../test_spectrum_file.dat',
+                                         '../test_spectrum_file.dat'],
+                              redshifts=[0.34, 0.13])
+    print classification.list_best_matches()
+    classification.plot_with_gui(indexToPlot=1)
+
+
+def run_gui():
+    app = QtGui.QApplication(sys.argv)
+    form = MainApp()
+    form.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
+    run_gui()
+
+
