0f06bfbcbf646974c825ee3634da4499c9b029ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:03 +0000

    Add package npimp/0.1/npimp-0.1.tar.gz

---
+++ packages/npimp/npimp-0.1.tar.gz/npimp-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open('README') as f:
+    long_description = ''.join(f.readlines())
+
+
+setup(
+    name='npimp',
+    version='0.1',
+    description='Notebook regulation daemon',
+    long_description=long_description,
+    author='Tomáš Kukrál',
+    author_email='tomas.kukral@6shore.net',
+    license='MIT',
+    url='https://github.com/tomkukral/ntbpimp/',
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'npimp = npimp:main',
+        ],
+    },
+)
