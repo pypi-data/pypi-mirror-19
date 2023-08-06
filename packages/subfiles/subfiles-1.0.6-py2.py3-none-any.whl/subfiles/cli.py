c5c04471d26ba5a189a5514131a176e3fa7fe5d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:50 +0000

    Add package subfiles/1.0.6/subfiles-1.0.6-py2.py3-none-any.whl

---
+++ packages/subfiles/subfiles-1.0.6-py2.py3-none-any.whl/subfiles/cli.py
@@ -0,0 +1,48 @@
+"""
+subtypes
+
+Usage:
+  subtypes -s | --schema
+  subtypes -l | --list
+  subtypes -h | --help
+  subtypes --version
+
+Options:
+  -h --help                         Show this screen.
+  --version                         Show version.
+
+Examples:
+  subtypes -s
+  subtypes -l
+
+Help:
+  For help using this tool, please open an issue on the Github repository:
+  https://github.com/mindey/subfiles
+"""
+
+
+from inspect import getmembers, isclass
+
+from docopt import docopt
+
+from . import __version__ as VERSION
+
+
+def main():
+    """Main CLI entrypoint."""
+    import subfiles.commands
+    options = docopt(__doc__, version=VERSION)
+
+    # Here we'll try to dynamically match the command the user is trying to run
+    # with a pre-defined command class we've already created.
+    for (k, v) in options.items(): 
+        if k in ['-l', '--list']:
+            k = 'list'
+        if k in ['-s', '--schema']:
+            k = 'schema'
+        if hasattr(subfiles.commands, k) and v:
+            module = getattr(subfiles.commands, k)
+            subfiles.commands = getmembers(module, isclass)
+            command = [command[1] for command in subfiles.commands if command[0] != 'Base'][0]
+            command = command(options)
+            command.run()
