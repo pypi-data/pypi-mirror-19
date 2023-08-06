29f9d3d37c05fe912f9a112f6bff60d063f74ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:11 +0000

    Add package django-cspreport/0.1.10/django-cspreport-0.1.10.tar.gz

---
+++ packages/django-cspreport/django-cspreport-0.1.10.tar.gz/django-cspreport-0.1.10/csp_report/admin.py
@@ -0,0 +1,40 @@
+from django.contrib import admin
+
+from csp_report.models import CSPReport, CleanedCSPReport, CSPReportFilter
+from django.core import urlresolvers
+
+
+class CSPReportAdmin(admin.ModelAdmin):
+    list_display = ('id', 'host', 'document_uri', 'date', 'short_blocked_uri', )
+    ordering = ['-id', ]
+    list_filter = ('host', 'date', )
+
+admin.site.register(CSPReport, CSPReportAdmin)
+
+
+class CleanedCSPReportAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'link_to_report',
+        'document_domain',
+        'document_path',
+        'short_document_query',
+        'blocked_domain',
+        'short_blocked_url',
+    )
+    ordering = ['-id', ]
+    list_filter = ('document_domain', 'blocked_domain', 'document_path')
+
+    def link_to_report(self, obj):
+        link = urlresolvers.reverse('admin:csp_report_cspreport_change', args=[obj.report.id])
+        return '<a href="{}">{}</a>'.format(link, obj.report.id)
+
+    link_to_report.allow_tags = True
+
+admin.site.register(CleanedCSPReport, CleanedCSPReportAdmin)
+
+
+class CSPReportFilterAdmin(admin.ModelAdmin):
+    ordering = ['-id', ]
+
+admin.site.register(CSPReportFilter, CSPReportFilterAdmin)
---
+++ packages/django-cspreport/django-cspreport-0.1.10.tar.gz/django-cspreport-0.1.10/csp_report/models.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from django.contrib.postgres.fields import JSONField
+from django.db import models
+from django.template.defaultfilters import truncatechars
+
+
+class CSPReport(models.Model):
+    host = models.TextField(blank=True, null=True, default='')
+    document_uri = models.TextField()
+    blocked_uri = models.TextField()
+    referrer = models.TextField(blank=True, null=True, default='')
+
+    body = JSONField(default={}, blank=True, null=True)
+    request_meta = JSONField(default={}, blank=True, null=True)
+
+    date = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def short_blocked_uri(self):
+        return truncatechars(self.blocked_uri, 100)
+
+    def __str__(self):
+        return 'CSPReport {}'.format(self.id)
+
+
+class CleanedCSPReport(models.Model):
+    report = models.ForeignKey(CSPReport)
+
+    document_scheme = models.TextField(blank=True, null=True, default='')
+    document_domain = models.TextField(blank=True, null=True, default='')
+    document_path = models.TextField(blank=True, null=True, default='')
+    document_query = models.TextField(blank=True, null=True, default='')
+
+    blocked_domain = models.TextField(blank=True, null=True, default='')
+    blocked_url = models.TextField(blank=True, null=True, default='')
+
+    @property
+    def short_blocked_uri(self):
+        return truncatechars(self.blocked_url, 100)
+
+    @property
+    def short_document_query(self):
+        return truncatechars(self.document_query, 100)
+
+class CSPReportFilter(models.Model):
+    filter_type = models.TextField()
+    value = models.TextField()
+    is_enabled = models.BooleanField(default=True)
+
+    def __str__(self):
+        return '{}: {}'.format(self.filter_type, self.value)
