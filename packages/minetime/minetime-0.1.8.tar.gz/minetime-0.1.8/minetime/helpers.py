d8112bfdc0883e5bf8fd2a1a1407a81cd17f378c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:25 +0000

    Add package minetime/0.1.8/minetime-0.1.8.tar.gz

---
+++ packages/minetime/minetime-0.1.8.tar.gz/minetime-0.1.8/minetime/helpers.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+import logging
+import os
+from datetime import datetime
+
+import click
+import yaml
+
+
+def get_config():
+    """read config from file and return python dictionnary"""
+
+    config = False
+    for loc in os.curdir, click.get_app_dir('minetime'), os.environ.get("MINETIME_CONF"):
+        try:
+            with open(os.path.join(loc, "config.yml")) as source:
+                config = yaml.safe_load(source)
+                if config:
+                    if 'XXXXXX' in config['user']['api_key']:
+                        continue
+                    else:
+                        return config
+        except (IOError, AttributeError):
+            pass
+
+    # No config
+    return False
+
+
+def get_logger(debug=False):
+    logger = logging.getLogger()
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    if debug:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.ERROR)
+
+    logger.debug('minetime ready : %s', 'time\'s on!')
+    return logger
+
+
+def convert_date_string(value):
+        return datetime.strptime(value, '%Y-%m-%d').date()
+
+
+def convert_time_string(value):
+        return datetime.strptime(value, '%H:%M').time()
+
+
+def get_activity_name(activity_id, activities):
+    if not activities:
+        activities = {}
+        activities[9] = u"Development"
+
+    try:
+        return activities[activity_id]
+    except:
+        return str(activity_id)
