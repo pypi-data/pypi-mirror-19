b80fd9da687e774e4ce91653fe5e778f56ed5049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:40 +0000

    Add package pdfplumber/0.4.6/pdfplumber-0.4.6.tar.gz

---
+++ packages/pdfplumber/pdfplumber-0.4.6.tar.gz/pdfplumber-0.4.6/setup.py
@@ -0,0 +1,24 @@
+import sys, os
+from setuptools import setup, find_packages
+import subprocess
+
+version = "0.4.6"
+
+base_reqs = [
+    "chardet",
+    "pycrypto",
+    "unicodecsv>=0.14.1",
+    "pdfminer.six>=20151013"
+]
+
+setup(
+    name="pdfplumber",
+    description="Plumb a PDF for detailed information about each char, rectangle, and line -- and easily extract text and tables.",
+    version=version,
+    packages=find_packages(exclude=["test",]),
+    tests_require=[ "nose", "pandas" ] + base_reqs,
+    install_requires=base_reqs,
+    entry_points={
+        "console_scripts": [ "pdfplumber = pdfplumber.cli:main" ] 
+    }
+)
