47a61474e5ca0a22c35b2d0925a9bd2af0db3262
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:34 +0000

    Add package nameko-neo4j/0.0.1/nameko_neo4j-0.0.1-py3-none-any.whl

---
+++ packages/nameko-neo4j/nameko_neo4j-0.0.1-py3-none-any.whl/nameko_neo4j/graph_session.py
@@ -0,0 +1,19 @@
+from py2neo import Graph, authenticate
+from nameko.extensions import DependencyProvider
+
+
+class GraphSession(DependencyProvider):
+
+    def setup(self):
+        self.db_username = self.container.config['DATABASE']['USERNAME']
+        self.db_password = self.container.config['DATABASE']['PASSWORD']
+        self.db_url = self.container.config['DATABASE']['URL']
+        self.db_endpoint = self.container.config['DATABASE']['ENDPOINT']
+
+    def get_dependency(self):
+        authenticate(
+            self.db_url,
+            user=self.db_username,
+            password=self.db_password
+        )
+        self.graph = Graph(self.db_endpoint)
