a72a8e70b6f39fefda52563142134598a65e3152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:24 +0000

    Add package ccw_sanitize/1.0.0/ccw_sanitize-1.0.0.zip

---
+++ packages/ccw_sanitize/ccw_sanitize-1.0.0.zip/ccw_sanitize-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+#from astroid.__pkginfo__ import author, author_email, description
+
+setup(
+      name = 'ccw_sanitize',
+      version = '1.0.0',
+      py_modules = ['ccw_sanitize'],
+      author = 'ccw',
+      author_email = 'ccw3435@naver.com',
+      description = 'a simple printer of sanitize'
+      )
