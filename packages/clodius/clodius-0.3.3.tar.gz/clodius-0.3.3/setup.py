5c7dff768eaf7e1b429692f96474467c42b3c4de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:27 +0000

    Add package clodius/0.3.3/clodius-0.3.3.tar.gz

---
+++ packages/clodius/clodius-0.3.3.tar.gz/clodius-0.3.3/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+from distutils.extension import Extension
+
+from Cython.Build import cythonize
+
+
+import numpy
+
+setup(
+    name='clodius',
+    include_dirs=[numpy.get_include()],
+    ext_modules = cythonize([Extension(
+        "clodius.fast", ["clodius/fast.pyx"], include_dirs=[
+            numpy.get_include()])]),
+    version='0.3.3',
+    description='Tile generation of big data',
+    author='Peter Kerpedjiev',
+    author_email='pkerpedjiev@gmail.com',
+    url='',
+    packages=['clodius'],
+    scripts=['scripts/make_single_threaded_tiles.py', 'scripts/tile_bigWig.py']
+)
