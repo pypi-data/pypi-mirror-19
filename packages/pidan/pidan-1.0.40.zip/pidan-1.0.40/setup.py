c8de37b3486ffbe50855fbb104f35e71ddbee6ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:55 +0000

    Add package pidan/1.0.40/pidan-1.0.40.zip

---
+++ packages/pidan/pidan-1.0.40.zip/pidan-1.0.40/setup.py
@@ -0,0 +1,16 @@
+# install:python setup.py sdist upload
+# -*- coding:utf-8 -*-
+from distutils.core import setup
+ 
+setup (
+    name = 'pidan',
+    version = '1.0.40',
+    packages=["pidan"],
+    platforms=['any'],
+    author = 'chengang',
+    author_email = 'chengang.net@gmail.com',
+    description = u'修正获取文件扩展名的函数',
+    package_data = {
+        '': ['*.data'],
+    },
+)
