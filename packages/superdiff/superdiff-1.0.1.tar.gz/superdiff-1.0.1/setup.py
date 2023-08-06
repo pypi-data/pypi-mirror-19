1ae959cf2a59b46edf1a909aa3ddd7cfb01850bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:34 +0000

    Add package superdiff/1.0.1/superdiff-1.0.1.tar.gz

---
+++ packages/superdiff/superdiff-1.0.1.tar.gz/superdiff-1.0.1/setup.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+
+from setuptools import setup
+
+setup(name='superdiff',
+      version='1.0.1',
+      description='A library for performing flexible diff checks.',
+      author='James Perretta',
+      author_email='jameslp@umich.edu',
+      license='GNU Lesser General Public License v3',
+      url='https://github.com/james-perretta/superdiff',
+      packages=['superdiff'],
+      install_requires=[],
+      classifiers=[
+          'Programming Language :: Python :: 3.5',
+      ])
