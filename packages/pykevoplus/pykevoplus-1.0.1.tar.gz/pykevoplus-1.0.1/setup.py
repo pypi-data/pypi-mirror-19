72868e30a04634534da05edf310e121c958398b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:21 +0000

    Add package pykevoplus/1.0.1/pykevoplus-1.0.1.tar.gz

---
+++ packages/pykevoplus/pykevoplus-1.0.1.tar.gz/pykevoplus-1.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+import os
+
+setup(
+    name = "pykevoplus",
+    version = "1.0.1",
+    author = "Carl Seelye",
+    author_email = "cseelye@gmail.com",
+    description = "Control Kwikset Kevo locks",
+    license = "MIT",
+    keywords = "kevo kwikset",
+    packages = ["pykevoplus"],
+    url = "https://github.com/cseelye/pykevoplus",
+    long_description = open(os.path.join(os.path.dirname(__file__), "README.rst")).read(),
+    install_requires = [
+        "requests",
+        "beautifulsoup4"
+    ]
+)
