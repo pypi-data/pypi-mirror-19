cc6f7e2c4e01608e4832fad8e1130138ce0375b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:46 +0000

    Add package hurriyetlog/0.3/hurriyetlog-0.3.tar.gz

---
+++ packages/hurriyetlog/hurriyetlog-0.3.tar.gz/hurriyetlog-0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="hurriyetlog",                  # This is the name of your PyPI-package.
+    description="hurriyet python log",   # Description
+    version="0.3",                       # Update the version number for new releases
+    author="hurriyet.com.tr",
+    author_email="destek@hurriyet.com.tr",
+    scripts=["hurriyetlog"],             # The name of your scipt, and also the command you'll be
+    license="MIT",
+    url="http://www.hurriyet.com.tr",
+    install_requires=["json", "logging", "socket", "datetime"]
+)
