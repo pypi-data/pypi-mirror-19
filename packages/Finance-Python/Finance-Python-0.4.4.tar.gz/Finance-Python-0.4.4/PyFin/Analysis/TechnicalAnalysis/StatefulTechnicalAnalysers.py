b7d15ece327a416c88132b298fb1bbf4c98b466b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:04 +0000

    Add package Finance-Python/0.4.4/Finance-Python-0.4.4.tar.gz

---
+++ packages/Finance-Python/Finance-Python-0.4.4.tar.gz/Finance-Python-0.4.4/PyFin/Analysis/TechnicalAnalysis/StatefulTechnicalAnalysers.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+u"""
+Created on 2015-8-8
+
+@author: cheng.li
+"""
+
+import copy
+import numpy as np
+from PyFin.Analysis.SecurityValueHolders import SecurityValueHolder
+from PyFin.Analysis.SecurityValueHolders import SecuritiesValues
+from PyFin.Math.Accumulators.StatefulAccumulators import MovingAverage
+from PyFin.Math.Accumulators.StatefulAccumulators import MovingMax
+from PyFin.Math.Accumulators.StatefulAccumulators import MovingMinimum
+from PyFin.Math.Accumulators.StatefulAccumulators import MovingSum
+from PyFin.Math.Accumulators.StatefulAccumulators import MovingVariance
+from PyFin.Math.Accumulators.StatefulAccumulators import MovingCountedPositive
+from PyFin.Math.Accumulators.StatefulAccumulators import MovingPositiveAverage
+from PyFin.Math.Accumulators.StatefulAccumulators import MovingCountedNegative
+from PyFin.Math.Accumulators.StatefulAccumulators import MovingNegativeAverage
+from PyFin.Math.Accumulators.StatefulAccumulators import MovingPositiveDifferenceAverage
+from PyFin.Math.Accumulators.StatefulAccumulators import MovingNegativeDifferenceAverage
+from PyFin.Math.Accumulators.StatefulAccumulators import MovingRSI
+from PyFin.Math.Accumulators.StatefulAccumulators import MovingHistoricalWindow
+from PyFin.Math.Accumulators.Performancers import MovingLogReturn
+
+
+class SecuritySingleValueHolder(SecurityValueHolder):
+    def __init__(self, window, HolderType, dependency='x'):
+        super(SecuritySingleValueHolder, self).__init__(dependency)
+        self._window += window
+        if self._compHolder:
+            self._holderTemplate = HolderType(window=window, dependency='x')
+            self._innerHolders = {
+                name: copy.deepcopy(self._holderTemplate) for name in self._compHolder.symbolList
+                }
+        else:
+            self._holderTemplate = HolderType(window=window, dependency=self._dependency)
+
+
+class SecurityMovingAverage(SecuritySingleValueHolder):
+    def __init__(self, window, dependency='x'):
+        super(SecurityMovingAverage, self).__init__(window, MovingAverage, dependency)
+
+
+class SecurityMovingMax(SecuritySingleValueHolder):
+    def __init__(self, window, dependency='x'):
+        super(SecurityMovingMax, self).__init__(window, MovingMax, dependency)
+
+
+class SecurityMovingMinimum(SecuritySingleValueHolder):
+    def __init__(self, window, dependency='x'):
+        super(SecurityMovingMinimum, self).__init__(window, MovingMinimum, dependency)
+
+
+class SecurityMovingSum(SecuritySingleValueHolder):
+    def __init__(self, window, dependency='x'):
+        super(SecurityMovingSum, self).__init__(window, MovingSum, dependency)
+
+
+class SecurityMovingVariance(SecuritySingleValueHolder):
+    def __init__(self, window, dependency='x'):
+        super(SecurityMovingVariance, self).__init__(window, MovingVariance, dependency)
+
+
+class SecurityMovingCountedPositive(SecuritySingleValueHolder):
+    def __init__(self, window, dependency='x'):
+        super(SecurityMovingCountedPositive, self).__init__(window, MovingCountedPositive, dependency)
+
+
+class SecurityMovingPositiveAverage(SecuritySingleValueHolder):
+    def __init__(self, window, dependency='x'):
+        super(SecurityMovingPositiveAverage, self).__init__(window, MovingPositiveAverage, dependency)
+
+
+class SecurityMovingCountedNegative(SecuritySingleValueHolder):
+    def __init__(self, window, dependency='x'):
+        super(SecurityMovingCountedNegative, self).__init__(window, MovingCountedNegative, dependency)
+
+
+class SecurityMovingNegativeAverage(SecuritySingleValueHolder):
+    def __init__(self, window, dependency='x'):
+        super(SecurityMovingNegativeAverage, self).__init__(window, MovingNegativeAverage, dependency)
+
+
+class SecurityMovingPositiveDifferenceAverage(SecuritySingleValueHolder):
+    def __init__(self, window, dependency='x'):
+        super(SecurityMovingPositiveDifferenceAverage, self).__init__(window, MovingPositiveDifferenceAverage, dependency)
+
+
+class SecurityMovingNegativeDifferenceAverage(SecuritySingleValueHolder):
+    def __init__(self, window, dependency='x'):
+        super(SecurityMovingNegativeDifferenceAverage, self).__init__(window, MovingNegativeDifferenceAverage, dependency)
+
+
+class SecurityMovingRSI(SecuritySingleValueHolder):
+    def __init__(self, window, dependency='x'):
+        super(SecurityMovingRSI, self).__init__(window, MovingRSI, dependency)
+
+
+class SecurityMovingLogReturn(SecuritySingleValueHolder):
+    def __init__(self, window, dependency='x'):
+        super(SecurityMovingLogReturn, self).__init__(window, MovingLogReturn, dependency)
+
+
+class SecurityMovingHistoricalWindow(SecuritySingleValueHolder):
+    def __init__(self, window, dependency='x'):
+        super(SecurityMovingHistoricalWindow, self).__init__(window, MovingHistoricalWindow, dependency)
+
+    def __getitem__(self, item):
+        if isinstance(item, str):
+            return super(SecurityMovingHistoricalWindow, self).__getitem__(item)
+        elif isinstance(item, int):
+            res = {}
+            for name in self._innerHolders:
+                try:
+                    res[name] = self._innerHolders[name].value[item]
+                except ArithmeticError:
+                    res[name] = np.nan
+            return SecuritiesValues(res)
+        else:
+            raise ValueError("{0} is not recognized as valid int or string".format(item))
+
+
+if __name__ == '__main__':
+
+    from PyFin.api import MA
+
+    t = MA(1, 'x') < MA(1, 'y')
+
+    data = {'aapl': {'x': 4, 'y': 3},
+            'goog': {'x': 1, 'y': 4}
+            }
+
+    t.push(data)
+
+    print(t.value)
+    print(t._dependency)
+
+    for k, v in t.value.iteritems():
+        print(k, v)
---
+++ packages/Finance-Python/Finance-Python-0.4.4.tar.gz/Finance-Python-0.4.4/PyFin/Math/Accumulators/impl.pyx
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+u"""
+Created on 2017-1-1
+
+@author: cheng.li
+"""
+
+import copy
+import numpy as np
+cimport numpy as np
+import cython
+
+
+cdef class Deque:
+
+    cdef public int window
+    cdef public int is_full
+    cdef public list con
+    cdef public int start
+
+    def __init__(self,
+                 int window,
+                 int is_full=0,
+                 con=None,
+                 int start=0):
+        self.window = window
+        self.is_full = is_full
+        if con:
+            self.con = copy.deepcopy(con)
+        else:
+            self.con = []
+        self.start = start
+
+    @cython.boundscheck(False)
+    def dump(self, value):
+        cdef int n
+        n = self.start
+
+        if self.is_full:
+            popout = self.con[n]
+            self.con[n] = value
+            self.start = (n + 1) % self.window
+        else:
+            if hasattr(value, '__len__'):
+                popout = np.array([np.nan] * len(value))
+            else:
+                popout = np.nan
+
+            self.con.append(value)
+            if len(self.con) == self.window:
+                self.is_full = 1
+        return popout
+
+    def size(self):
+        return len(self.con)
+
+    def isFull(self):
+        return self.is_full == 1
+
+    def as_array(self):
+        return np.array(self.as_list())
+
+    def as_list(self):
+        return [self.con[(self.start + item) % self.window] for item in range(len(self.con))]
+
+    def __getitem__(self, item):
+        return self.con[(self.start + item) % self.window]
+
+    def __deepcopy__(self, memo):
+        return Deque(self.window,
+                     self.is_full,
+                     self.con,
+                     self.start)
+
+    def __richcmp__(Deque self, Deque other, int op):
+        if op == 2:
+            return self.window == other.window \
+                   and self.is_full == other.is_full \
+                   and self.con == other.con \
+                   and self.start == other.start
+        elif op == 3:
+            return self.window != other.window \
+                   or self.is_full != other.is_full \
+                   or self.con != other.con \
+                   or self.start != other.start
+
+    def __reduce__(self):
+        d = {
+            'window': self.window,
+            'is_full': self.is_full,
+            'con': self.con,
+            'start': self.start
+        }
+        return Deque, (0,), d
+
+    def __setstate__(self, state):
+        self.window = state['window']
+        self.is_full = state['is_full']
+        self.con = state['con']
+        self.start = state['start']
