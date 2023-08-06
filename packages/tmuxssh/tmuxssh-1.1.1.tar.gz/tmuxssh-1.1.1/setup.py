4c3f04308f8eb7adf17c46fb03a2e992a6cac39f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:14 +0000

    Add package tmuxssh/1.1.1/tmuxssh-1.1.1.tar.gz

---
+++ packages/tmuxssh/tmuxssh-1.1.1.tar.gz/tmuxssh-1.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+setup(
+    name='tmuxssh',
+    author='Santiago Pappier',
+    author_email='spappier@gmail.com',
+    url='http://github.com/spappier/tmuxssh',
+    version='1.1.1',
+    description='ssh into several hosts at once using tmux',
+    long_description=readme,
+    license='MIT',
+    entry_points=dict(console_scripts=['tmuxssh = tmuxssh:main']),
+    py_modules=['tmuxssh'],
+    scripts=['tmuxssh.py'],
+    install_requires=['docopt'],
+    keywords='tmux ssh',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+    ]
+)
