e7ec726172b25e0714d2d85187abd6c22bfa976b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:50 +0000

    Add package lubdub/0.0.1.3/lubdub-0.0.1.3.tar.gz

---
+++ packages/lubdub/lubdub-0.0.1.3.tar.gz/lubdub-0.0.1.3/lubdub/__init__.py
@@ -0,0 +1,33 @@
+""" HRV: Submodule providing standalone calculations, graphing utilities, etc. """
+
+from .calc import nn_to_rolling_hrv, rmssd, normalize_rr, sdnn
+from .exceptions import LubDubError
+from .load import load_plaintext_rrs
+
+
+def PlotHRV(rr_intervals=None, nn_intervals=None, window=12):
+    """ Takes a list of un-normalized rr_intervals OR normalized nn_intervals, and 
+    returns a dictionary containing 'y_values' (array) and 'y_range' (tuple) plotting HRV over time.
+
+    Specify window param to change length of values to use for each successive RMSSD 
+    in the rolling HRV calculation.
+
+    :param rr_intervals: (list) [default: None]
+    :param nn_intervals: (list) [default: None]
+    :param window: (int) [default: 12]
+    :return graph: (dict)
+    """
+    if rr_intervals:
+        rolling_hrv = nn_to_rolling_hrv(normalize_rr(rr_intervals), window=window)
+    elif nn_intervals:
+        rolling_hrv = normalize_rr(rr_intervals, window=window)
+    else:
+        raise LubDubError('Missing argument: supply either rr_intervals or nn_intervals to PlotHRV.')
+
+    return {'y_values': rolling_hrv,
+            'y_range': (min(rolling_hrv), max(rolling_hrv))
+           }
+
+
+# https://en.wikipedia.org/wiki/Iambic_pentameter
+
---
+++ packages/lubdub/lubdub-0.0.1.3.tar.gz/lubdub-0.0.1.3/tests/test_calc.py
@@ -0,0 +1,12 @@
+import unittest
+import random
+
+class TestCalc(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
