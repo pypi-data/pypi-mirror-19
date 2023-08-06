90c77c3b02d86aa550aa294c3231e2c34bb4748c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:25 +0000

    Add package mastercard-places/1.0.5/mastercard_places-1.0.5.tar.gz

---
+++ packages/mastercard-places/mastercard_places-1.0.5.tar.gz/mastercard_places-1.0.5/mastercardplaces/__init__.py
@@ -0,0 +1,11 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardplaces.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardplaces.geocodelocation import *
+from mastercardplaces.merchantcategorycodes import *
+from mastercardplaces.merchantindustries import *
+from mastercardplaces.merchantpointofinterest import *
