c766042bad87c71648961f60eed508762d4cfdf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:49 +0000

    Add package signal_unkillable/0.0.2/signal_unkillable-0.0.2.tar.gz

---
+++ packages/signal_unkillable/signal_unkillable-0.0.2.tar.gz/signal_unkillable-0.0.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os, sys, subprocess
+from distutils.core import setup
+
+
+if 'install' in sys.argv:
+    subprocess.check_call(
+        ['make'],
+        cwd='./signal_unkillable',
+        env=dict(os.environ, PWD=os.environ['PWD'] + '/signal_unkillable'),
+    )
+
+
+setup(
+    name='signal_unkillable',
+    version='0.0.2',
+    description=(
+        'A python interface to the SIGNAL_UNKILLABLE flag in linux.'
+    ),
+    packages=['signal_unkillable'],
+    package_data={'signal_unkillable': ['*.c', '*.ko', 'Makefile']},
+)
