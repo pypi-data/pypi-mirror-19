a0472a3a8b30251298975404526c6e39046fca0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:45 +0000

    Add package faeriebot/0.1/faeriebot-0.1.tar.gz

---
+++ packages/faeriebot/faeriebot-0.1.tar.gz/faeriebot-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='faeriebot',
+	version='0.1',
+	description='Flowing Accounts for Electronically Recording Income & Expenses Bot',
+	url='http://github.com/ivanwangsa/faeriebot',
+	author='Ivan Wangsa',
+	author_email='ivanwangsa@gmail.com',
+	license='MIT',
+	packages=['faeriebot'],
+	zip_safe=False
+)
