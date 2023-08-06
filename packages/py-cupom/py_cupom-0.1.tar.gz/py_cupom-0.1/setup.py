b0d1e36bec69578405b4c0c8d9652a97723902e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:02 +0000

    Add package py-cupom/0.1/py_cupom-0.1.tar.gz

---
+++ packages/py-cupom/py_cupom-0.1.tar.gz/py_cupom-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+  name = 'py_cupom',
+  packages = ['py_cupom'],
+  version = '0.1',
+  description = 'A python script to generate temporary codes with low collision rate.',
+  author = 'AlexVanAxe',
+  author_email = 'alexvanaxe@gmail.com',
+  url = 'https://github.com/simplificateit/py-cupom',
+  download_url = 'https://github.com/simplificateit/py-cupom/tree/0.1',
+  keywords = ['cupom', 'generator', 'random'],
+  classifiers = [],
+)
