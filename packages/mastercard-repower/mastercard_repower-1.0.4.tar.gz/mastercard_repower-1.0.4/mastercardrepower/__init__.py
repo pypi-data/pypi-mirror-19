afe11bf533c39a254e5b67d87ba82473795b8f42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:25 +0000

    Add package mastercard-repower/1.0.4/mastercard_repower-1.0.4.tar.gz

---
+++ packages/mastercard-repower/mastercard_repower-1.0.4.tar.gz/mastercard_repower-1.0.4/mastercardrepower/__init__.py
@@ -0,0 +1,9 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardrepower.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardrepower.repowertransfer import *
+from mastercardrepower.repowertransferreversal import *
