ae0b86c8f61bad6770051504a93bee5d6e5ed290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:53 +0000

    Add package graphysio/0.5/graphysio-0.5.zip

---
+++ packages/graphysio/graphysio-0.5.zip/graphysio-0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name = 'graphysio',
+      version = '0.5',
+      description = 'Graphical visualization of physiologic time series',
+      url = 'https://github.com/jaj42/graphysio',
+      author = 'Jona JOACHIM',
+      author_email = 'jona@joachim.cc',
+      license = 'ISC',
+      install_requires = ['pyqtgraph', 'pandas'],
+      scripts = ['scripts/graphysio.py'],
+      packages = ['graphysio', 'graphysio.ui'],
+)
