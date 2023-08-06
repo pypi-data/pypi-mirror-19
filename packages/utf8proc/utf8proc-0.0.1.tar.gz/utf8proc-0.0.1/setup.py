3d02d2c8c3374087ed417a58215c041363311572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:03 +0000

    Add package utf8proc/0.0.1/utf8proc-0.0.1.tar.gz

---
+++ packages/utf8proc/utf8proc-0.0.1.tar.gz/utf8proc-0.0.1/setup.py
@@ -0,0 +1,73 @@
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+import sys
+import setuptools
+
+__version__ = '0.0.1'
+
+
+class get_pybind_include(object):
+    """Helper class to determine the pybind11 include path
+
+    The purpose of this class is to postpone importing pybind11
+    until it is actually installed, so that the ``get_include()``
+    method can be invoked. """
+
+    def __init__(self, user=False):
+        self.user = user
+
+    def __str__(self):
+        import pybind11
+        return pybind11.get_include(self.user)
+
+
+ext_modules = [
+    Extension(
+        'utf8proc',
+        ['src/utf8tool.cpp', 'src/utf8proc.cpp', 'src/main.cpp'],
+        include_dirs=[
+            # Path to pybind11 headers
+            get_pybind_include(),
+            get_pybind_include(user=True)
+        ],
+        language='c++'
+    ),
+]
+
+
+class BuildExt(build_ext):
+    """A custom build extension for adding compiler-specific options."""
+    c_opts = {
+        'msvc': ['/EHsc'],
+        'unix': [],
+    }
+
+    if sys.platform == 'darwin':
+        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+
+    def build_extensions(self):
+        ct = self.compiler.compiler_type
+        opts = self.c_opts.get(ct, [])
+        if ct == 'unix':
+            opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
+            opts.append('-std=c++14')
+            opts.append('-fvisibility=hidden')
+        elif ct == 'msvc':
+            opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
+        for ext in self.extensions:
+            ext.extra_compile_args = opts
+        build_ext.build_extensions(self)
+
+setup(
+    name='utf8proc',
+    version=__version__,
+    author='Guillaume Campagna',
+    author_email='guillaume@transitapp.com',
+    url='https://github.com/TransitApp/utf8proc',
+    description='Python wrapper on libutf8proc',
+    long_description='',
+    ext_modules=ext_modules,
+    install_requires=['pybind11>=1.7'],
+    cmdclass={'build_ext': BuildExt},
+    zip_safe=False,
+)
