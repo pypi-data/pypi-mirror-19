a42bf5222a899b54b0e71d0fbad774d6bd1dd4e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:07 +0000

    Add package instabot/0.2.3b/instabot-0.2.3b.tar.gz

---
+++ packages/instabot/instabot-0.2.3b.tar.gz/instabot-0.2.3b/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'instabot',
+  packages = ['instabot'],
+  version = '0.2.3b',
+  description = 'Cool Instagram bot scripts and API python wrapper.',
+  author = 'Daniil Okhlopkov',
+  author_email = 'ohld@icloud.com',
+  url = 'https://github.com/ohld/instabot',
+  download_url = 'https://github.com/ohld/instabot/tarball/0.2.3b',
+  keywords = ['instagram', 'bot', 'api'],
+  classifiers = [],
+  install_requires=['tqdm', 'requests-toolbelt', 'requests'],
+)
