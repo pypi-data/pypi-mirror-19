aba28456dfb3407f93a283fd6cd707abe823dbee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:51 +0000

    Add package coreschema/0.0.1/coreschema-0.0.1-py2-none-any.whl

---
+++ packages/coreschema/coreschema-0.0.1-py2-none-any.whl/coreschema/__init__.py
@@ -0,0 +1,12 @@
+from coreschema.schemas import (
+    Object, Array, Integer, Number, String, Boolean, Null,
+    Enum, Anything, Ref, RefSpace, Union
+)
+
+
+__version__ = '0.0.1'
+
+__all__ = [
+    Object, Array, Integer, Number, String, Boolean, Null,
+    Enum, Anything, Ref, RefSpace, Union
+]
