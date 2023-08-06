3536a144700cfbfd2dd2343aef01beaac55e5b29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:47 +0000

    Add package SnappEmailApiClient/0.3.15/SnappEmailApiClient-0.3.15.zip

---
+++ packages/SnappEmailApiClient/SnappEmailApiClient-0.3.15.zip/SnappEmailApiClient-0.3.15/snapp_email/endpoints/navigation/__init__.py
@@ -0,0 +1,46 @@
+from .MenuItem_21Endpoint import MenuItem_21Endpoint
+from .MenuItem_20Endpoint import MenuItem_20Endpoint
+from .Menu_22Endpoint import Menu_22Endpoint
+from .Menu_21Endpoint import Menu_21Endpoint
+from .Menu_20Endpoint import Menu_20Endpoint
+
+
+class NavigationEndpoint:
+    def __init__(self, api_client):
+        self._api_client = api_client
+
+    @property
+    def MenuItem_21(self):
+        """
+        :return: MenuItem_21Endpoint
+        """
+        return MenuItem_21Endpoint(self._api_client)
+        
+    @property
+    def MenuItem_20(self):
+        """
+        :return: MenuItem_20Endpoint
+        """
+        return MenuItem_20Endpoint(self._api_client)
+        
+    @property
+    def Menu_22(self):
+        """
+        :return: Menu_22Endpoint
+        """
+        return Menu_22Endpoint(self._api_client)
+        
+    @property
+    def Menu_21(self):
+        """
+        :return: Menu_21Endpoint
+        """
+        return Menu_21Endpoint(self._api_client)
+        
+    @property
+    def Menu_20(self):
+        """
+        :return: Menu_20Endpoint
+        """
+        return Menu_20Endpoint(self._api_client)
+        
