d0ba731c3665bd904b23c956a7614d51adbe22e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:43 +0000

    Add package nvmdeploy/0.6/nvmdeploy-0.6-py2-none-any.whl

---
+++ packages/nvmdeploy/nvmdeploy-0.6-py2-none-any.whl/nvmdeploysrc/nevermore.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+import argparse
+import sys
+from fastinit import need_init, startInit, reInit, addTask, deleteTask, listTask, doTask
+
+def entry():
+    nvm_deploy = """
+     \033[1;32;40m (/≧▽≦)/ *** 快速发布 pod的命令行工具 *** ヾ(^▽^ヾ)
+     \033[1;33;40m BETA 版本 0.6 \033[0m
+    """
+
+    print nvm_deploy
+
+    if len(sys.argv) == 1:
+        sys.argv.append('--help')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--init', help="快速初始化命令行工具", action="store_true")
+    parser.add_argument('-a', '--add', help="添加新的 task", action="store_true")
+    parser.add_argument('-d', '--delete', help="删除指定名称的 task")
+    parser.add_argument('-t', '--task', help="需要发布的 task 名称")
+    parser.add_argument('-l', '--list', help="查看所有 task", action="store_true")
+
+    args = parser.parse_args()
+
+    # print args
+
+    try:
+        if need_init():
+            startInit()
+        else:
+            if args.task:
+                doTask(args.task)
+            elif args.add:
+                addTask()
+            elif args.init:
+                reInit()
+            elif args.delete:
+                deleteTask(args.delete)
+            elif args.list:
+                listTask()
+
+    except:
+        print("\n \033[1;31;40m 异常退出\n")
