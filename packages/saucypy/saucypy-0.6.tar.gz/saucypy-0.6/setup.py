bc0b9e2683ec39ebdefad843e4bde11b07d8dc49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:53 +0000

    Add package saucypy/0.6/saucypy-0.6.tar.gz

---
+++ packages/saucypy/saucypy-0.6.tar.gz/saucypy-0.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='saucypy',
+    packages=['saucypy'],
+    install_requires=[
+        'requests'
+    ],
+    version='0.6',
+    description='SauceLabs API Python Wrapper',
+    author='Ewen McCahon',
+    author_email='ewen.m.mccahon@student.uts.edu.au',
+    url='https://github.com/Neko-Design/saucypy',
+    download_url='https://github.com/Neko-Design/saucypy/tarball/0.6',
+    keywords=['saucelabs', 'api', 'wrapper', 'interface', 'sauce'],
+    classifiers=[]
+)
