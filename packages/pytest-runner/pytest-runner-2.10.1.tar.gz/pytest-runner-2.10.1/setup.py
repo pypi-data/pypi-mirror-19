f19fee6d555d59715dc1d82648dca1bb9de91da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:14 +0000

    Add package pytest-runner/2.10.1/pytest-runner-2.10.1.tar.gz

---
+++ packages/pytest-runner/pytest-runner-2.10.1.tar.gz/pytest-runner-2.10.1/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+# Project skeleton maintained at https://github.com/jaraco/skeleton
+
+import io
+import sys
+
+import setuptools
+
+with io.open('README.rst', encoding='utf-8') as readme:
+	long_description = readme.read()
+
+needs_wheel = set(['release', 'bdist_wheel', 'dists']).intersection(sys.argv)
+wheel = ['wheel'] if needs_wheel else []
+
+name = 'pytest-runner'
+description = 'Invoke py.test as distutils command with dependency resolution'
+
+setup_params = dict(
+	name=name,
+	use_scm_version=True,
+	author="Jason R. Coombs",
+	author_email="jaraco@jaraco.com",
+	description=description or name,
+	long_description=long_description,
+	url="https://github.com/pytest-dev/" + name,
+	namespace_packages=name.split('.')[:-1],
+	py_modules=['ptr'],
+	install_requires=[
+	],
+	extras_require={
+	},
+	setup_requires=[
+		'setuptools_scm>=1.9,!=1.13.1,!=1.14.0',
+	] + wheel,
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"Intended Audience :: Developers",
+		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 2.6",
+		"Programming Language :: Python :: 2.7",
+		"Programming Language :: Python :: 3",
+		"Framework :: Pytest",
+	],
+	entry_points = {
+		'distutils.commands': [
+			'ptr = ptr:PyTest',
+			'pytest = ptr:PyTest',
+		],
+	},
+)
+if __name__ == '__main__':
+	setuptools.setup(**setup_params)
