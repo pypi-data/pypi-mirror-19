064220d81ccf1a88bec44e624edb23caa20dd4f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:02 +0000

    Add package pyeloqua/0.3.3/pyeloqua-0.3.3.tar.gz

---
+++ packages/pyeloqua/pyeloqua-0.3.3.tar.gz/pyeloqua-0.3.3/pyeloqua/__init__.py
@@ -0,0 +1,6 @@
+""" pyeloqua is a package of wrappers functions for interacting with Eloqua over Bulk API 2.0.
+    Right now, they cover Contacts, Accounts, and Custom Data Objects."""
+
+from .pyeloqua import Eloqua
+
+__version__ = '0.3.3'
