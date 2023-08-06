4ba9243bfab9e60ac35b80bf4e8ef07024362fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:18 +0000

    Add package Juboor_Toolbox/0.1/Juboor_Toolbox-0.1.zip

---
+++ packages/Juboor_Toolbox/Juboor_Toolbox-0.1.zip/Juboor_Toolbox-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Juboor_Toolbox',
+      version='0.1',
+      description='This is a compilation of the tools that I use from day-to-day',
+      url='http://github.com/Djuboor/Toolbox',
+      author='David Juboor',
+      author_email='dnjuboor@gmail.com',
+      license='MIT',
+      packages=['Juboor_Toolbox'],
+      zip_safe=False)
