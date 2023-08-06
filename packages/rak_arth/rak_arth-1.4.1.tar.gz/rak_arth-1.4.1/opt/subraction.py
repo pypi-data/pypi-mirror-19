201389b660c52ce264e93e263755ab8d7aa17b16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:43 +0000

    Add package rak_arth/1.4.1/rak_arth-1.4.1.tar.gz

---
+++ packages/rak_arth/rak_arth-1.4.1.tar.gz/rak_arth-1.4.1/opt/subraction.py
@@ -0,0 +1,17 @@
+def sub(a,*b):
+	s=a
+	for i in b:
+		s=s-i
+	print("the subraction of given numbers is-->",s)
+
+# obj=kar()
+# obj.sub(10,5,4)
+
+# def sub(*a):
+# 	# print(a[0])
+# 	s=a[0]
+# 	for i in a[1:]:
+# 		s=s-i
+# 	print("the subraction of given numbers is-->",s)
+#
+# sub(10,-7,5)
