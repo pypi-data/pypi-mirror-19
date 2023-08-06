7674956e97b50ba1693f1b1070f42226b0317f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:04 +0000

    Add package PyQt4Enhanced/0.1.1/PyQt4Enhanced-0.1.1.tar.gz

---
+++ packages/PyQt4Enhanced/PyQt4Enhanced-0.1.1.tar.gz/PyQt4Enhanced-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PyQt4Enhanced',
+    version='0.1.1',
+    packages=['PyQt4Enhanced'],
+    url='https://github.com/finn-christiansen/PyQt4Enhanced',
+    license='to set',
+    author='Finn Christiansen',
+    author_email='mail@finnchristiansen.com',
+    description='Enhanced PyQt4 module'
+)
