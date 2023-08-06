39233ab3a5b3438e933f389c6869d9a9a1e77e46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:54 +0000

    Add package sqlwrapper/0.1/sqlwrapper-0.1.tar.gz

---
+++ packages/sqlwrapper/sqlwrapper-0.1.tar.gz/sqlwrapper-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'sqlwrapper',
+  packages = ['sqlwrapper'],
+  version = '0.1',
+  description = ' This module is a wrapper aroung SQL query language, i.e an interface between the database and python.',
+  author = 'Ankitesh Gupta, Saif Vazir',
+  author_email = 'ankiteshguptas@gmail.com,ssv.saif@gmail.com',
+  url = 'https://github.com/ankitesh97/sqlwrapper',
+  download_url = 'https://github.com/ankitesh97/sqlwrapper/tarball/0.1',
+  keywords = ['sql', 'sqlite', 'database', 'queries', 'sqlwrapper','sql', 'mysql', 'postgresql'],
+  classifiers = [],
+)
