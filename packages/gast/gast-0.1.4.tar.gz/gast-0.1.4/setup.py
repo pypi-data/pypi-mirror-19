3c1d1bfc259beca6bd343067bb61c3feae55848e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:13 +0000

    Add package gast/0.1.4/gast-0.1.4.tar.gz

---
+++ packages/gast/gast-0.1.4.tar.gz/gast-0.1.4/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='gast',  # gast, daou naer!
+      version='0.1.4',
+      packages=['gast'],
+      description='Python AST that abstracts the underlying Python version',
+      author='serge-sans-paille',
+      author_email='serge.guelton@telecom-bretagne.eu',
+      license="BSD 3-Clause",
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Natural Language :: English',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 3'],
+      )
