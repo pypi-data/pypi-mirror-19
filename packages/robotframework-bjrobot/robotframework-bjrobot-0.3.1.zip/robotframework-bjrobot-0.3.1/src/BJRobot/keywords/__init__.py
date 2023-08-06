db6e81cb578b5662cf7f6d74ab5eb3b2458d0523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:56 +0000

    Add package robotframework-bjrobot/0.3.1/robotframework-bjrobot-0.3.1.zip

---
+++ packages/robotframework-bjrobot/robotframework-bjrobot-0.3.1.zip/robotframework-bjrobot-0.3.1/src/BJRobot/keywords/__init__.py
@@ -0,0 +1,21 @@
+from browsermanager import BrowserManager
+from element import Element
+from screenshot import Screenshot
+from logging import Logging
+from runonfailure import RunOnFailure
+from mobile_keyevent import Keyevent
+from mobile_element import Mobile_Element
+from android_utils import AndroidUtils
+from mobile_touch import Mobile_Touch
+from applicationmanager import ApplicationManagemer
+
+__all__ = ['BrowserManager',
+           'Element',
+           'ApplicationManagemer',
+           'Mobile_Element',
+           'AndroidUtils',
+           'Mobile_Touch',
+           'Screenshot',
+           'Keyevent',
+           'Logging',
+           'RunOnFailure']
