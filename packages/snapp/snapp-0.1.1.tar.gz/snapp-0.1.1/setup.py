2fd6d43baa90387d8e07323102ab85011e9bb0b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:09 +0000

    Add package snapp/0.1.1/snapp-0.1.1.tar.gz

---
+++ packages/snapp/snapp-0.1.1.tar.gz/snapp-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+from distutils.core import setup
+
+setup(name='snapp',
+      version='0.1.1',
+      description='Python framework for building command line applications.',
+      author='Matthew Corner',
+      author_email='matt.corner@gmail.com',
+      url='https://www.signiant.com',
+      packages=find_packages(),
+      license='MIT',
+      install_requires=['']
+     )
