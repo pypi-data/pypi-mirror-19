9b705602e09b682b28285c639f8aa18d71c65be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:15 +0000

    Add package mambo/1.0.0b19/Mambo-1.0.0b19.tar.gz

---
+++ packages/mambo/Mambo-1.0.0b19.tar.gz/Mambo-1.0.0b19/mambo/__init__.py
@@ -0,0 +1,19 @@
+"""
+Mambo
+"""
+
+from .core import *
+from .ext import *
+from . import (decorators,
+               utils,
+               exceptions)
+from mambo.contrib import base
+
+# deprecated
+from .decorators import *
+from .utils import *
+
+
+
+
+
---
+++ packages/mambo/Mambo-1.0.0b19.tar.gz/Mambo-1.0.0b19/mambo/contrib/base/__init__.py
@@ -0,0 +1,9 @@
+
+
+from mambo import register_package
+
+__version__ = "1.0.0"
+
+register_package(__package__)
+
+
