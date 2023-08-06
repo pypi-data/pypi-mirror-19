64a492a094e5e23afc7acfe96f2426ae8895e235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:10 +0000

    Add package logging-test-case/1.0/logging-test-case-1.0.zip

---
+++ packages/logging-test-case/logging-test-case-1.0.zip/logging-test-case-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='logging-test-case',
+      version='1.0',
+      description='Provides class LoggingTestCase to help test log files.',
+      author='Chad Rosenquist',
+      author_email='ChadRosenquist@hotmail.com',
+      url='https://github.com/chadrosenquist/logging-test-case',
+      packages=['loggingtestcase'],
+      license='MIT'
+)
