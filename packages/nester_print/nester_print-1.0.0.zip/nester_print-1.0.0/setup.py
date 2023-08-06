e6d2867c4f795c617f269af8cb6b3098fc9d18c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:33 +0000

    Add package nester_print/1.0.0/nester_print-1.0.0.zip

---
+++ packages/nester_print/nester_print-1.0.0.zip/nester_print-1.0.0/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  6 21:43:52 2017
+
+@author: Notebook
+"""
+
+from distutils.core import setup
+
+setup(
+      name         = 'nester_print',
+      version      = '1.0.0',
+      py_modules   = ['nester_print'],
+      author       = 'upython',
+      author_email = 'rizki113070213@gmail.com',
+      url          = 'http://www.an-najm.com',
+      description  = 'A simple printer of nested lists')
