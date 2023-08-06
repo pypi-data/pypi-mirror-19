1e84ad018aba8a4615250e21655cdc1744f8c6d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:05 +0000

    Add package earl/1.0.dev0/earl-1.0.dev0.tar.gz

---
+++ packages/earl/earl-1.0.dev0.tar.gz/earl-1.0.dev0/earl/__init__.py
@@ -0,0 +1,35 @@
+from collections import OrderedDict
+from urlparse import urlparse, urlunparse, parse_qsl
+from urllib import urlencode
+from json import dumps
+
+class OD(OrderedDict):
+    def json(self):
+        return dumps(self)
+
+class Url(object):
+    def __init__(self, url):
+        self.original_url = url
+        self.parsed_url = urlparse(self.original_url)
+        self.host = self.parsed_url.netloc
+        self.query = OD(parse_qsl(self.parsed_url.query))
+
+    def unparse(self):
+        l = list(self.parsed_url)
+        l[4] = urlencode(self.query)
+        return urlunparse(l)
+
+    def set(self, *args, **kwargs):
+        for name, value in list(args) + kwargs.items():
+            self.query[name] = value
+        return self
+
+    def __getattr__(self, item):
+        if item in object.__dict__:
+            object.__getattribute__(self, item)
+        else:
+            return self.parsed_url.__getattribute__(item)
+
+    def __repr__(self):
+        return self.unparse()
+
