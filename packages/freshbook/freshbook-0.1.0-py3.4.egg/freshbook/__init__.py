5cb70dd1507a65116d20ba4039cb031cdd344c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:04 +0000

    Add package freshbook/0.1.0/freshbook-0.1.0-py3.4.egg

---
+++ packages/freshbook/freshbook-0.1.0-py3.4.egg/freshbook/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """agustin"""
+__email__ = 'agustinbv@gmail.com'
+__version__ = '0.1.0'
---
+++ packages/freshbook/freshbook-0.1.0-py3.4.egg/freshbook/cli.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+"""
+Freshbooks logger tool.
+
+Usage:
+  freshbook init
+  freshbook commit [-d DATE] [--hours HOURS] [-m MESSAGE]
+  freshbook -h | --help
+
+Options:
+  -h --help                     Show this screen.
+  --version                     Show version.
+  --hours HOURS                 Hours spend on the task (defaults to config task hours)
+  -d DATE --date DATE           Date in ISO format, i.e. 2015-09-10 (defaults today)
+  -m MESSAGE --message=MESSAGE  Message to attach on the time entry.
+"""
+
+from __future__ import print_function
+from docopt import docopt
+
+import os
+import sys
+import datetime
+import json
+from six.moves import input
+
+from .freshbook import Freshbook
+from .utils import SetupConfig
+
+
+def main():
+    args = docopt(__doc__)
+    print(args)
+
+    if args['init']:
+        SetupConfig().run()
+        sys.exit(0)
+
+    if not os.path.exists(SetupConfig.PATH):
+        sys.exit("Run `freshbook init` first.")
+
+    with open(SetupConfig.PATH) as f:
+        config = json.load(f)
+
+    fb = Freshbook(config['account']['url'], config['account']['token'])
+
+    if args['commit']:
+
+        fb.commit(
+            project_id=config['project']['id'],
+            task_id=config['project']['task']['id'],
+            hours=args['--hours'] or config['project']['task']['hours'],
+            date=args['--date'] or datetime.date.today().isoformat(),
+            notes=args['--message'],
+        )
+
+
+if __name__ == '__main__':
+    main()
