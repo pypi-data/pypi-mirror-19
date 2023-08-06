37f7b1822e54b705a75278adb263386578623239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:28 +0000

    Add package mastercard-remittance/1.0.3/mastercard_remittance-1.0.3.tar.gz

---
+++ packages/mastercard-remittance/mastercard_remittance-1.0.3.tar.gz/mastercard_remittance-1.0.3/mastercardremittance/__init__.py
@@ -0,0 +1,13 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardremittance.resourceconfig import *
+
+
+# import apis into api package
+from mastercardremittance.cancelremittance import *
+from mastercardremittance.quotes import *
+from mastercardremittance.remittancefundingandpayment import *
+from mastercardremittance.remittancepayment import *
+from mastercardremittance.retrieveremittance import *
+from mastercardremittance.sanctionscreening import *
