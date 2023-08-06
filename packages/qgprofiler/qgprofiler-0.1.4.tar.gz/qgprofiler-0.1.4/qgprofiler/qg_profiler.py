dbacbd205539134542df36fda9b9d123cf5e6527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:05 +0000

    Add package qgprofiler/0.1.4/qgprofiler-0.1.4.tar.gz

---
+++ packages/qgprofiler/qgprofiler-0.1.4.tar.gz/qgprofiler-0.1.4/qgprofiler/qg_profiler.py
@@ -0,0 +1,61 @@
+from .node import Node, NodeList
+from .helper import make_folder_and_get_file_type
+import json
+
+class QGProfiler(object):
+    def __init__(self, root_name, file_path):
+        self.root_node = Node(root_name, None)
+        self.current_node = self.root_node
+        self.file_path = file_path
+        self.file_type = make_folder_and_get_file_type(file_path)
+
+    def push(self, name):
+        index = self.current_node.is_child_in_children(name)
+        if index == -1:
+            new_node = Node(name, self.current_node)
+            self.current_node.add_child(new_node)
+            self.current_node = new_node
+        else:
+            self.current_node = self.current_node.get_child(index)
+            self.current_node.modify_time()
+
+    def pop(self):
+        if self.root_node != self.current_node:
+            self.current_node.increment_value()
+            self.current_node.modify_time()
+            self.current_node = self.current_node.get_parent()
+        else:
+            raise ValueError('You have reached root node, try end')
+
+    def end(self):
+        if self.root_node == self.current_node:
+            self.root_node.increment_value()
+            self.root_node.modify_time()
+        else:
+            raise ValueError('You are not at the root node')
+
+    def generate_file(self):
+        def recursive_json_generator(node):
+            _dict = {}
+            _dict['name'] = node.get_name()
+            _dict['value'] = node.get_value()
+            _dict['children'] = [recursive_json_generator(child_node) for child_node in node.get_children()]
+            return _dict
+
+        def recursive_xml_generator(node):
+            _xml = '<' + node.get_name() + ' value="' + str(node.get_value()) + '">'
+            _xml += ''.join([recursive_xml_generator(child_node) for child_node in node.get_children()]) 
+            _xml += '</' + node.get_name() + '>'
+            return _xml
+
+        if self.file_type == 'json':
+            _json = recursive_json_generator(self.root_node)
+            text = json.dumps(_json)
+            self.write_file(text)
+        elif self.file_type == 'xml':
+            text = recursive_xml_generator(self.root_node)
+            self.write_file(text)
+
+    def write_file(self, text):
+        with open(self.file_path, 'w') as f:
+            f.write(text)
---
+++ packages/qgprofiler/qgprofiler-0.1.4.tar.gz/qgprofiler-0.1.4/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='qgprofiler',
+    version='0.1.4',
+    description='Profiler with time deterministic profiling of functions',
+    url='https://github.com/quantumgraph/qgprofiler',
+    author='QuantumGraph',
+    author_email='contact@quantumgraph.com',
+    license='MIT',
+    packages=['qgprofiler'],
+    long_description=read('README.md'),
+    keywords = ['profiler', 'monitoring', 'logging', 'utility'],
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
+        'License :: Freeware',
+        'Programming Language :: Python',
+    ],
+)
