e6d5a6020187829f6e515462c4fed80f218ca304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:44 +0000

    Add package launchpadtools/0.3.0/launchpadtools-0.3.0.tar.gz

---
+++ packages/launchpadtools/launchpadtools-0.3.0.tar.gz/launchpadtools-0.3.0/launchpadtools/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+__name__ = 'launchpadtools'
+__version__ = '0.3.0'
+__author__ = 'Nico Schlömer'
+__author_email__ = 'nico.schloemer@gmail.com'
+
+from . import clone
+from . import submit
