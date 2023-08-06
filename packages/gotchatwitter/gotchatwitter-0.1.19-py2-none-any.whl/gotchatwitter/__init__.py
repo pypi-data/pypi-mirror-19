1c58ebcd90e345477afba4a1a843b1b2db8cab8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:19 +0000

    Add package gotchatwitter/0.1.19/gotchatwitter-0.1.19-py2-none-any.whl

---
+++ packages/gotchatwitter/gotchatwitter-0.1.19-py2-none-any.whl/gotchatwitter/__init__.py
@@ -0,0 +1,4 @@
+from .gotchatwitter import GotchaTwitter
+from .json2csv import json2csv
+
+__all__ = ['GotchaTwitter', 'json2csv']
---
+++ packages/gotchatwitter/gotchatwitter-0.1.19-py2-none-any.whl/gotchatwitter/json2csv.py
@@ -0,0 +1,28 @@
+import json
+import csv
+
+
+def json2csv(type, fp_in, fp_out, output_mode='w'):
+    with open(fp_in) as i, open(fp_out, output_mode) as o:
+        csvwriter = csv.writer(o)
+        for line in i:
+            if type == 'user':
+                csvwriter.writerow(json2csv_user(line))
+
+
+def json2csv_user(line):
+    jline = json.loads(line)
+
+    uid = jline.get('id_str')
+    scn = jline.get('screen_name')
+    is_verified = jline.get('verified')
+    created_at = jline.get('created_at')
+    location = jline.get('location')
+
+    n_tweets = jline.get('statuses_count')
+    n_following = jline.get('friends_count')
+    n_followers = jline.get('followers_count')
+    n_likes = jline.get('favourites_count')
+
+    return [uid, scn, is_verified, created_at, location,
+            n_tweets, n_following, n_followers, n_likes]
