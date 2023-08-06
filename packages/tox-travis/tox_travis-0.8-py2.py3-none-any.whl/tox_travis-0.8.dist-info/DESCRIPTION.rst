e7e0c722385616aa640fa66afb397e4aaa24d141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:05 +0000

    Add package tox-travis/0.8/tox_travis-0.8-py2.py3-none-any.whl

---
+++ packages/tox-travis/tox_travis-0.8-py2.py3-none-any.whl/tox_travis-0.8.dist-info/DESCRIPTION.rst
@@ -0,0 +1,45 @@
+====================================
+Tox-Travis: Integrate Tox and Travis
+====================================
+
+.. image:: https://img.shields.io/pypi/v/tox-travis.svg
+   :target: https://pypi.python.org/pypi/tox-travis
+   :alt: Latest Version
+
+.. image:: https://readthedocs.org/projects/tox-travis/badge/?version=stable
+   :target: http://tox-travis.readthedocs.io/en/stable/?badge=stable
+   :alt: Documentation Status
+
+.. image:: https://travis-ci.org/ryanhiebert/tox-travis.svg?branch=master
+   :target: https://travis-ci.org/ryanhiebert/tox-travis
+
+.. image:: https://badges.gitter.im/ryanhiebert/tox-travis.svg
+   :alt: Join the chat at https://gitter.im/ryanhiebert/tox-travis
+   :target: https://gitter.im/ryanhiebert/tox-travis?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge
+
+Tox-Travis is a plugin for Tox that simplifies the setup
+between Tox and Travis.
+
+
+Usage
+=====
+
+Configure the Python versions to test with in ``.travis.yml``,
+and install ``tox-travis`` with pip:
+
+.. code-block:: yaml
+
+    sudo: false
+    language: python
+    python:
+      - "2.7"
+      - "3.4"
+    install: pip install tox-travis
+    script: tox
+
+Tox will only run the ``py27`` or ``py34`` env
+(or envs that have a factor that matches)
+as appropriate for the version of Python
+that is being run by each Travis job.
+
+
---
+++ packages/tox-travis/tox_travis-0.8-py2.py3-none-any.whl/tox_travis/toxenv.py
@@ -0,0 +1,230 @@
+"""Default TOXENV based on the Travis environment."""
+from __future__ import print_function
+from itertools import product
+import os
+import sys
+import re
+import py
+import tox.config
+
+from tox.config import _split_env as split_env
+try:
+    from tox.config import default_factors
+except ImportError:
+    default_factors = None
+
+from .utils import TRAVIS_FACTORS, parse_dict
+
+
+def default_toxenv():
+    """Default TOXENV automatically based on the Travis environment."""
+    if 'TRAVIS' not in os.environ:
+        return
+
+    if 'TOXENV' in os.environ:
+        return  # Skip any processing if already set
+
+    config = py.iniconfig.IniConfig('tox.ini')
+
+    # Find the envs that tox knows about
+    declared_envs = get_declared_envs(config)
+
+    # Find all the envs for all the desired factors given
+    desired_factors = get_desired_factors(config)
+
+    # Reduce desired factors
+    desired_envs = ['-'.join(env) for env in product(*desired_factors)]
+
+    # Find matching envs
+    matched = match_envs(declared_envs, desired_envs,
+                         passthru=len(desired_factors) == 1)
+    os.environ.setdefault('TOXENV', ','.join(matched))
+
+    # Travis virtualenv do not provide `pypy3`, which tox tries to execute.
+    # This doesnt affect Travis python version `pypy3`, as the pyenv pypy3
+    # is in the PATH.
+    # https://github.com/travis-ci/travis-ci/issues/6304
+    # Force use of the virtualenv `python`.
+    version = os.environ.get('TRAVIS_PYTHON_VERSION')
+    if version and default_factors and version.startswith('pypy3.3-5.2-'):
+        default_factors['pypy3'] = 'python'
+
+
+def get_declared_envs(config):
+    """Get the full list of envs from the tox config.
+
+    This notably also includes envs that aren't in the envlist,
+    but are declared by having their own testenv:envname section.
+
+    The envs are expected in a particular order. First the ones
+    declared in the envlist, then the other testenvs in order.
+    """
+    tox_section = config.sections.get('tox', {})
+    envlist = split_env(tox_section.get('envlist', []))
+
+    # Add additional envs that are declared as sections in the config
+    section_envs = [
+        section[8:] for section in sorted(config.sections, key=config.lineof)
+        if section.startswith('testenv:')
+    ]
+
+    return envlist + [env for env in section_envs if env not in envlist]
+
+
+def get_version_info():
+    """Get version info from the sys module.
+
+    Override from environment for testing.
+    """
+    overrides = os.environ.get('__TOX_TRAVIS_SYS_VERSION')
+    if overrides:
+        version, major, minor = overrides.split(',')[:3]
+        major, minor = int(major), int(minor)
+    else:
+        version, (major, minor) = sys.version, sys.version_info[:2]
+    return version, major, minor
+
+
+def guess_python_env():
+    """Guess the default python env to use."""
+    version, major, minor = get_version_info()
+    if 'PyPy' in version:
+        return 'pypy3' if major == 3 else 'pypy'
+    return 'py{major}{minor}'.format(major=major, minor=minor)
+
+
+def get_default_envlist(version):
+    """Parse a default tox env based on the version.
+
+    The version comes from the ``TRAVIS_PYTHON_VERSION`` environment
+    variable. If that isn't set or is invalid, then use
+    sys.version_info to come up with a reasonable default.
+    """
+    if version in ['pypy', 'pypy3']:
+        return version
+
+    # Assume single digit major and minor versions
+    match = re.match(r'^(\d)\.(\d)(?:\.\d+)?$', version or '')
+    if match:
+        major, minor = match.groups()
+        return 'py{major}{minor}'.format(major=major, minor=minor)
+
+    return guess_python_env()
+
+
+def get_desired_factors(config):
+    """Get the list of desired envs per declared factor.
+
+    Look at all the accepted configuration locations, and give a list
+    of envlists, one for each Travis factor found.
+
+    Look in the ``[travis]`` section for the known Travis factors,
+    which are backed by environment variable checking behind the
+    scenes, but provide a cleaner interface.
+
+    Also look for the ``[tox:travis]`` section, which is deprecated,
+    and treat it as an additional ``python`` key from the ``[travis]``
+    section.
+
+    Finally, look for factors based directly on environment variables,
+    listed in the ``[travis:env]`` section. Configuration found in the
+    ``[travis]`` and ``[tox:travis]`` sections are converted to this
+    form under the hood, and are considered in the same way.
+
+    Special consideration is given to the ``python`` factor. If this
+    factor is set in the environment, then an appropriate configuration
+    will be provided automatically if no manual configuration is
+    provided.
+
+    To allow for the most flexible processing, the envlists provided
+    by each factor are not combined after they are selected, but
+    instead returned as a list of envlists, and expected to be
+    combined as and when appropriate by the caller. This allows for
+    special handling based on the number of factors that were found
+    to apply to this environment.
+    """
+    # Find configuration based on known travis factors
+    travis_section = config.sections.get('travis', {})
+    found_factors = [
+        (factor, parse_dict(travis_section[factor]))
+        for factor in TRAVIS_FACTORS
+        if factor in travis_section
+    ]
+
+    # Backward compatibility with the old tox:travis section
+    if 'tox:travis' in config.sections:
+        print('The [tox:travis] section is deprecated in favor of'
+              ' the "python" key of the [travis] section.', file=sys.stderr)
+        found_factors.append(('python', config.sections['tox:travis']))
+
+    # Inject any needed autoenv
+    version = os.environ.get('TRAVIS_PYTHON_VERSION')
+    if version:
+        default_envlist = get_default_envlist(version)
+        if not any(factor == 'python' for factor, _ in found_factors):
+            found_factors.insert(0, ('python', {version: default_envlist}))
+        python_factors = [(factor, mapping)
+                          for factor, mapping in found_factors
+                          if version and factor == 'python']
+        for _, mapping in python_factors:
+            mapping.setdefault(version, default_envlist)
+
+    # Convert known travis factors to env factors,
+    # and combine with declared env factors.
+    env_factors = [
+        (TRAVIS_FACTORS[factor], mapping)
+        for factor, mapping in found_factors
+    ] + [
+        (name, parse_dict(value))
+        for name, value in config.sections.get('travis:env', {}).items()
+    ]
+
+    # Choose the correct envlists based on the factor values
+    return [
+        split_env(mapping[os.environ[name]])
+        for name, mapping in env_factors
+        if name in os.environ and os.environ[name] in mapping
+    ]
+
+
+def match_envs(declared_envs, desired_envs, passthru):
+    """Determine the envs that match the desired_envs.
+
+    If ``passthru` is True, and none of the declared envs match the
+    desired envs, then the desired envs will be used verbatim.
+
+    :param declared_envs: The envs that are declared in the tox config.
+    :param desired_envs: The envs desired from the tox-travis config.
+    :param bool passthru: Whether to used the ``desired_envs`` as a
+                          fallback if no declared envs match.
+    """
+    matched = [
+        declared for declared in declared_envs
+        if any(env_matches(declared, desired) for desired in desired_envs)
+    ]
+    return desired_envs if not matched and passthru else matched
+
+
+def env_matches(declared, desired):
+    """Determine if a declared env matches a desired env.
+
+    Rather than simply using the name of the env verbatim, take a
+    closer look to see if all the desired factors are fulfilled. If
+    the desired factors are fulfilled, but there are other factors,
+    it should still match the env.
+    """
+    desired_factors = desired.split('-')
+    declared_factors = declared.split('-')
+    return all(factor in declared_factors for factor in desired_factors)
+
+
+def override_ignore_outcome(config):
+    """Override ignore_outcome if unignore_outcomes is set to True."""
+    if 'TRAVIS' not in os.environ:
+        return
+
+    tox_config = py.iniconfig.IniConfig('tox.ini')
+    travis_reader = tox.config.SectionReader("travis", tox_config)
+    if travis_reader.getbool('unignore_outcomes', False):
+        for env in config.envlist:
+            config.envconfigs[env].ignore_outcome = False
