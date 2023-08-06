7d0d2b6819ff57950ca3c60f626cff4fadc57501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:40 +0000

    Add package sentry-telegram/0.1.2/sentry_telegram-0.1.2.tar.gz

---
+++ packages/sentry-telegram/sentry_telegram-0.1.2.tar.gz/sentry_telegram-0.1.2/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup
+
+import sentry_telegram
+
+tests_require = [
+    'sentry==8.12.0',
+]
+
+
+setup(
+    name='sentry_telegram',
+    version=sentry_telegram.__version__,
+    packages=['sentry_telegram'],
+    url='https://github.com/butorov/sentry-telegram',
+    author='Viacheslav Butorov',
+    author_email='butorovv@gmail.com',
+    description='Plugin for Sentry which allows sending notification via Telegram messenger.',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    include_package_data=True,
+    tests_require=tests_require,
+)
