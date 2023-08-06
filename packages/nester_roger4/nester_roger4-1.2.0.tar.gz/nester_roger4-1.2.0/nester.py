1f310b5e2f8a3cf4b2af85f36a2b10ba626505af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:56 +0000

    Add package nester_roger4/1.2.0/nester_roger4-1.2.0.tar.gz

---
+++ packages/nester_roger4/nester_roger4-1.2.0.tar.gz/nester_roger4-1.2.0/nester.py
@@ -0,0 +1,13 @@
+""" Este é o modulo "nester.py", e fornece uma função chamada print_lol()
+que imprime lista que podem ou nao incluir listas aninhadas"""
+
+def print_lol(the_list):
+    """Esta função requer um argumento posicional chamado "the_list", que é
+    qualquer lista Python(de possíveis listas aninhadas). Cada item de dados na
+    lista fornecida é (recursivamente) impresso na tela em sua própria  linha"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item)
+        else:
+            print(each_item)
+            
