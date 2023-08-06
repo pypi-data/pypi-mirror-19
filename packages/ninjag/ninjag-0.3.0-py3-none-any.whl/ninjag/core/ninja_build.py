e9d67bd76c1fa37b7d5f5079440aab31f252779b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:49 +0000

    Add package ninjag/0.3.0/ninjag-0.3.0-py3-none-any.whl

---
+++ packages/ninjag/ninjag-0.3.0-py3-none-any.whl/ninjag/core/ninja_build.py
@@ -0,0 +1,30 @@
+from .ninja_def import ninja_def
+from .ninja_io import ninja_io
+from .ninja_build_dep import ninja_build_dep
+
+
+def ninja_build(tasks):
+    """Return the Ninja build section
+
+    Args:
+        tasks (list): a list of task dictionaries
+    Returns:
+        a string
+    """
+    output = []
+    for t in tasks:
+        item = "build {0}: {1} {2}{3}{4}".format(
+                    ninja_io(t['out']),
+                    t['rule'],
+                    ninja_io(t['in']),
+                    ninja_build_dep(t, "dep|"),
+                    ninja_build_dep(t, "dep||")
+                )
+        if 'def' in t:
+            output.append("{}\n{}".format(
+                    item,
+                    ninja_def(t['def'], "  "))
+                )
+        else:
+            output.append(item)
+    return "\n\n".join(output)
