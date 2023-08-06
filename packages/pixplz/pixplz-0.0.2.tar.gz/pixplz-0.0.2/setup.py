8313695bcd7dc665c8061ce2eeb66d2869c2b44f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:07 +0000

    Add package pixplz/0.0.2/pixplz-0.0.2.tar.gz

---
+++ packages/pixplz/pixplz-0.0.2.tar.gz/pixplz-0.0.2/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+install_requires = [
+    "grequests",
+    "Pillow",
+    "requests",
+    "six",
+    "tqdm"
+]
+
+setup(name="pixplz",
+      version="0.0.2",
+      author="Paul Fitzpatrick",
+      author_email="paulfitz@alum.mit.edu",
+      description="Fetch some images to use as casual training data",
+      packages=['pixplz'],
+      entry_points={
+          "console_scripts": [
+              "pixplz=pixplz.fetch:main"
+          ]
+      },
+      install_requires=install_requires,
+      url="https://github.com/paulfitz/pixplz"
+)
