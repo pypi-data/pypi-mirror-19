3fe2a1b58a563ca2928e7cc1eed4fa4c26286c36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:06 +0000

    Add package specpie/1.1.3/specpie-1.1.3.post1-py3-none-any.whl

---
+++ packages/specpie/specpie-1.1.3.post1-py3-none-any.whl/specpie/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from colorama.initialise import init
+
+from specpie.spec import should, spec
+
+from specpie.descriptors import describe, context, it
+
+init()
+
+should = should
+describe = describe
+context = context
+it = it
+spec = spec
