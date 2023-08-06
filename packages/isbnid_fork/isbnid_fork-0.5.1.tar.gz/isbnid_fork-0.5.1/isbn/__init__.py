5ad9a2913e70e6438732a6fffebb9439add95946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:21 +0000

    Add package isbnid_fork/0.5.1/isbnid_fork-0.5.1.tar.gz

---
+++ packages/isbnid_fork/isbnid_fork-0.5.1.tar.gz/isbnid_fork-0.5.1/isbn/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Hypatia: Module ISBN [__init__]
+#
+
+__author__ = "Neko"
+__license__ = 'LGPL http://www.gnu.org/licenses/lgpl.txt'
+
+from . isbn import ISBN, ISBNError
+
+__all__ = ['ISBN', 'ISBNError']
---
+++ packages/isbnid_fork/isbnid_fork-0.5.1.tar.gz/isbnid_fork-0.5.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name = 'isbnid_fork',
+    author = 'ISBNid GitHub',
+    author_email = 'admin@inspirehep.net',
+    description = "Python ISBN ids",
+    license = "GPL",
+    url = 'https://github.com/inspirehep/isbnid',
+    keywords = 'ISBN',
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: Indexing",
+        ],
+    packages = ['isbn'],
+    package_dir = {'isbn': 'isbn'},
+    install_requires=['autosemver~=0.2'],
+    setup_requires=['autosemver~=0.2'],
+    autosemver={
+        'bugtracker_url': 'https://github.com/inspirehep/isbnid/issues'
+    },
+)
