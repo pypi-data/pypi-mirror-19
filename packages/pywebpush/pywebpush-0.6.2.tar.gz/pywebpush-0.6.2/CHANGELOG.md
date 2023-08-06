9642fee9bd45fca0e783ca326f5885e13363a11c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:33 +0000

    Add package pywebpush/0.6.2/pywebpush-0.6.2.tar.gz

---
+++ packages/pywebpush/pywebpush-0.6.2.tar.gz/pywebpush-0.6.2/CHANGELOG.md
@@ -0,0 +1,23 @@
+## 0.4.0 (2016-06-05)
+feat: make python 2.7 / 3.5 polyglot
+
+## 0.3.4 (2016-05-17)
+bug: make header keys case insenstive
+
+## 0.3.3 (2016-05-17)
+bug: force key string encoding to utf8
+
+## 0.3.2 (2016-04-28)
+bug: fix setup.py issues
+
+## 0.3 (2016-04-27)
+feat: added travis, normalized directories
+
+
+## 0.2 (2016-04-27)
+feat: Added tests, restructured code
+
+
+## 0.1 (2016-04-25)
+
+Initial release
---
+++ packages/pywebpush/pywebpush-0.6.2.tar.gz/pywebpush-0.6.2/setup.py
@@ -0,0 +1,48 @@
+import io
+import os
+
+from setuptools import find_packages, setup
+
+__version__ = "0.6.2"
+
+
+def read_from(file):
+    reply = []
+    with io.open(os.path.join(here, file), encoding='utf8') as f:
+        for l in f:
+            l = l.strip()
+            if not l:
+                break
+            if l[0] != '#' or l[:2] != '//':
+                reply.append(l)
+    return reply
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+with io.open(os.path.join(here, 'README.rst'), encoding='utf8') as f:
+    README = f.read()
+with io.open(os.path.join(here, 'CHANGELOG.md'), encoding='utf8') as f:
+    CHANGES = f.read()
+
+setup(name="pywebpush",
+      version=__version__,
+      packages=find_packages(),
+      description='WebPush publication library',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=["Topic :: Internet :: WWW/HTTP",
+                   "Programming Language :: Python :: Implementation :: PyPy",
+                   'Programming Language :: Python',
+                   "Programming Language :: Python :: 2",
+                   "Programming Language :: Python :: 2.7"
+                   ],
+      keywords='push webpush publication',
+      author="jr conlin",
+      author_email="src+webpusher@jrconlin.com",
+      url='https://github.com/web-push-libs/pywebpush',
+      license="MPL2",
+      test_suite="nose.collector",
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=read_from('requirements.txt'),
+      tests_require=read_from('test-requirements.txt')
+      )
