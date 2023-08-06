b0537419ba87b96ec4e991c8ab1c417583dc5c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:51 +0000

    Add package oss4blog/0.0.2/oss4blog-0.0.2.zip

---
+++ packages/oss4blog/oss4blog-0.0.2.zip/oss4blog-0.0.2/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/2/5 0005 上午 11:00
+# @Author  : Trojx
+# @File    : setup.py
+from distutils.core import setup
+
+setup(
+    name='oss4blog',
+    version='0.0.2',
+    author='Trojx',
+    author_email='raojianxun@126.com',
+    packages=['oss4blog'],
+    scripts=['oss4blog/oss4blog.py'],
+    url='http://pypi.python.org/pypi/oss4blog/',
+    license='The MIT License',
+    description='Useful util to use Aliyun OSS to build image hosting for static blog.',
+    long_description='Useful util to use Aliyun OSS to build image hosting for static blog.',
+    install_requires=[
+        'pyperclip', 'oss2', 'watchdog',],
+)
