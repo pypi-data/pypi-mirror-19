5cfbc02ea31885542c65999d3e38bd2cbcf1ba6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:54 +0000

    Add package gea/0.2.8/gea-0.2.8.tar.gz

---
+++ packages/gea/gea-0.2.8.tar.gz/gea-0.2.8/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='gea',
+    version='0.2.8',
+    description='Gestión de Expedientes de Agrimensores para Django.',
+    author='Santiago Pestarini',
+    author_email='santiago@pestarini.com.ar',
+    url='http://pypi.python.org/pypi/gea/',
+    packages=['gea'],
+    package_data={
+        'gea': ['templates/*','templates/*/*', 'templatetags/*', 'static/*/*'],
+    },
+    license='LICENSE',
+    long_description=open('README.rst', 'r').read(),
+    install_requires=[
+        'Django == 1.8.9',
+        'django-grappelli == 2.7.3',
+        'django-filebrowser == 3.6.4',
+        'django-nested-admin == 2.1.8',
+        'django-extensions == 1.7.6',
+    ],   
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities'
+    ],
+)
+
