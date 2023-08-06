dc568ad328eab9009c58335fa0de86b334599f4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:25 +0000

    Add package mastercard-retail-location-insights/1.0.5/mastercard_retail_location_insights-1.0.5.tar.gz

---
+++ packages/mastercard-retail-location-insights/mastercard_retail_location_insights-1.0.5.tar.gz/mastercard_retail_location_insights-1.0.5/mastercardretaillocationinsights/__init__.py
@@ -0,0 +1,12 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardretaillocationinsights.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardretaillocationinsights.merchantindustries import *
+from mastercardretaillocationinsights.retailunitmerchants import *
+from mastercardretaillocationinsights.retailunits import *
+from mastercardretaillocationinsights.retailunitsmetrics import *
+from mastercardretaillocationinsights.subscriptions import *
