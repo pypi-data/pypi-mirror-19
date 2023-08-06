44c6241740218582d46790d23766e401965ff40c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:58 +0000

    Add package jira-history/0.8.1/jira-history-0.8.1.tar.gz

---
+++ packages/jira-history/jira-history-0.8.1.tar.gz/jira-history-0.8.1/setup.py
@@ -0,0 +1,35 @@
+from distutils.core import setup
+import os
+
+
+def read(path):
+    root = os.path.dirname(os.path.abspath(__file__))
+    return open(os.path.join(root, path)).read()
+
+
+setup(
+    name='jira-history',
+    description='Makes working with Jira issue changelogs a breeze!',
+    long_description=read('README.rst'),
+    url='https://github.com/redtoad/jira-history',
+    version='0.8.1',
+    author='Sebastian Rahlf',
+    author_email='basti@redtoad.de',
+    packages=['jira_history'],
+    requires=['jira'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Other Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='jira changelog ticket history'
+)
