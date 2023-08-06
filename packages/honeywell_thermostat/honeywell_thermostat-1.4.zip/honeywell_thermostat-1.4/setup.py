bd53d251bfd2280db5484894ed05852f665e44af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:28 +0000

    Add package honeywell_thermostat/1.4/honeywell_thermostat-1.4.zip

---
+++ packages/honeywell_thermostat/honeywell_thermostat-1.4.zip/honeywell_thermostat-1.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+# add to pypi with the following commands:
+'''
+python setup.py register -r pypi
+python setup.py sdist upload -r pypi
+pip install honeywell_thermostat --upgrade
+pip show honeywell_thermostat
+'''
+
+setup(
+    name='honeywell_thermostat',
+    packages=['honeywell_thermostat'],
+    version='1.4',
+    description='Python API for Honeywell thermostats',
+    author='Jordan Thompson',
+    author_email='Jordan@ThompCo.com',
+    download_url='https://github.com/jorythompson/thermostat',
+    url='https://github.com/jorythompson/thermostat',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ],
+    keywords='develoment tools',
+    install_requires=[],
+
+)
