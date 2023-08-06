348976dc12b7ecc51a4c48e5891b714ae726172e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:12 +0000

    Add package ifv/0.1.3/ifv-0.1.3.tar.gz

---
+++ packages/ifv/ifv-0.1.3.tar.gz/ifv-0.1.3/setup.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name="ifv",
+    packages=["ifv"],
+    version="0.1.3",
+    description="a simple common api client framework",
+    url="https://github.com/MrLYC/IFV",
+    author="MrLYC",
+    author_email="imyikong@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+
+        "License :: OSI Approved :: MIT License",
+
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+    keywords=["api", "client", "framework"],
+    extras_require={
+        "extend": ["requests>=2.7.0"],
+    },
+)
