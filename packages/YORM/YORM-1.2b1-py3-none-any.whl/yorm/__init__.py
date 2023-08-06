7f8f856cf11d93544cc9a1deaa4b64506164cc70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:02 +0000

    Add package YORM/1.2b1/YORM-1.2b1-py3-none-any.whl

---
+++ packages/YORM/YORM-1.2b1-py3-none-any.whl/yorm/__init__.py
@@ -0,0 +1,10 @@
+"""Package for YORM."""
+
+from . import bases, types
+from .common import UUID
+from .decorators import sync, sync_object, sync_instances, attr
+from .utilities import create, find, match, load, save, delete
+from .bases import Container, Converter, Mappable
+
+__project__ = 'YORM'
+__version__ = '1.2b1'
