8be788574e105a738531f8815527de69b66a7f46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:40 +0000

    Add package arith/1.0.2/arith-1.0.2.tar.gz

---
+++ packages/arith/arith-1.0.2.tar.gz/arith-1.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    # Application name:
+    name="arith",
+
+    # Version number (initial):
+    version="1.0.2",
+
+    # Application author details:
+    author="ramya",
+    author_email="munja.ramya@gmail.com",
+
+    # Packages
+    packages=["arith"],
+
+    # Include additional files into the package
+    #include_package_data=True,
+
+    # Details
+    url="http://pypi.python.org/pypi/MyApplication_v010/",
+
+    #
+    # license="LICENSE.txt",
+    description = "Useful towel-related stuff"
+
+    # long_description=open("README.txt").read(),
+
+    # install_requires = ["flask" , ],
+)
