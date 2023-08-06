e359e00bdf37d6a4ac48d35f43a77be455bdde01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:49 +0000

    Add package globe/1.0.52/globe-1.0.52.tar.gz

---
+++ packages/globe/globe-1.0.52.tar.gz/globe-1.0.52/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+import setuptools
+setup(
+  name = 'globe',
+  packages = ['globe', 'globe/connect', 'globe/connect/voiceLib'], # this must be the same as the name above
+  version = '1.0.52',
+  description = 'Globe Api ',
+  author = 'clark galgo',
+  author_email = 'clark21@dev-engine.net',
+  url = 'https://github.com/globelabs/globe-connect-python', # use the URL to the github repo
+  download_url = 'https://github.com/globelabs/globe-connect-python/archive/master.zip', # I'll explain this in a second
+  keywords = ['Globe', 'Globe Api', 'Globe Labs'], # arbitrary keywords
+  classifiers = [],
+  install_requires = ['pycurl']
+)
