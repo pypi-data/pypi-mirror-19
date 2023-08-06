c5e8ed246f7ab24b61a6f82ee5e5ddc7f99804c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:52 +0000

    Add package irs/4.7.21/irs-4.7.21-py2.py3-none-any.whl

---
+++ packages/irs/irs-4.7.21-py2.py3-none-any.whl/irs/config.py
@@ -0,0 +1,19 @@
+CONFIG = dict(
+
+    # To autostart rhythmbox with a new song:
+    # default_flags = '-c rhythmbox %(loc)s',
+    # To make choosing of links default:
+    # default_flags = '-l',
+    default_flags = '',
+
+
+    # These are necessary to download Spotify playlists
+    client_id = 'e4198f6a3f7b48029366f22528b5dc66',
+    client_secret = 'fa96b67d223547e88c931a978b2fcecc',
+
+    # For a custom directory. Note that `~` will not work as a shortcut.
+    directory = '',
+
+    # If you want numbered file names
+    numbered_file_names = True,
+)
