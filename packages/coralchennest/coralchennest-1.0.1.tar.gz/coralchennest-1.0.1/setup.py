726c92f852fa1ad972d1706e766a106841c243b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:22 +0000

    Add package coralchennest/1.0.1/coralchennest-1.0.1.tar.gz

---
+++ packages/coralchennest/coralchennest-1.0.1.tar.gz/coralchennest-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+name = 'coralchennest',
+version = '1.0.1',
+py_modules = ['nester'],
+author = 'coralchen1105',
+url = 'https://coralchen1105.wordpress.com/',
+author_email = 'coralchen1105@gmail.com',
+description = 'A simple printer of nested lists',
+)
