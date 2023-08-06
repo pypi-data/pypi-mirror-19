aaa8e6f914e89aafa461ada983428eb1e2b1473b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:34 +0000

    Add package tellduslive/0.2.2/tellduslive-0.2.2.tar.gz

---
+++ packages/tellduslive/tellduslive-0.2.2.tar.gz/tellduslive-0.2.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+from setuptools import setup
+from tellduslive import __version__
+
+setup(name="tellduslive",
+      version=__version__,
+      description="Communicate with Telldus Live",
+      url="https://github.com/molobrakos/tellduslive",
+      license="",
+      author="Erik",
+      author_email="Erik",
+      install_requires=['requests_oauthlib'],
+      py_modules=["tellduslive"],
+      provides=["tellduslive"],)
