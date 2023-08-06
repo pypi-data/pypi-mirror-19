0c417e275d6710caf8c50c56ac42673748aa5e4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:24 +0000

    Add package mastercard-lost-stolen/1.0.5/mastercard_lost_stolen-1.0.5.tar.gz

---
+++ packages/mastercard-lost-stolen/mastercard_lost_stolen-1.0.5.tar.gz/mastercard_lost_stolen-1.0.5/mastercardloststolen/__init__.py
@@ -0,0 +1,8 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardloststolen.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardloststolen.accountinquiry import *
