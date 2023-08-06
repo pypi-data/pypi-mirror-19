3d4203a766a1fb9a15e45992df9721ade2663f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:11 +0000

    Add package sciurus/0.1.0/sciurus-0.1.0.tar.gz

---
+++ packages/sciurus/sciurus-0.1.0.tar.gz/sciurus-0.1.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='sciurus',
+    packages=['sciurus'],
+    version='0.1.0',
+    description='An minimalist function scheduler',
+    author='elric-dev',
+    author_email='hsalee101@gmail.com',
+    url='https://github.com/elric-dev/sciurus',
+    keywords=['scheduler'],
+    license='MIT',
+    classifiers=[],
+)
