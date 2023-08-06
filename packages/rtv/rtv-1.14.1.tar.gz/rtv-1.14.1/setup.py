7e16557b37c07d8e2401ffbf3fbb9ca089c305ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:14 +0000

    Add package rtv/1.14.1/rtv-1.14.1.tar.gz

---
+++ packages/rtv/rtv-1.14.1.tar.gz/rtv-1.14.1/setup.py
@@ -0,0 +1,41 @@
+import sys
+import setuptools
+
+from version import __version__ as version
+
+
+setuptools.setup(
+    name='rtv',
+    version=version,
+    description='A simple terminal viewer for Reddit (Reddit Terminal Viewer)',
+    long_description=open('README.rst').read(),
+    url='https://github.com/michael-lazar/rtv',
+    author='Michael Lazar',
+    author_email='lazar.michael22@gmail.com',
+    license='MIT',
+    keywords='reddit terminal praw curses',
+    packages=['rtv'],
+    package_data={'rtv': ['templates/*']},
+    data_files=[("share/man/man1", ["rtv.1"])],
+    install_requires=[
+        'praw >=3.5, <4',
+        'six',
+        'requests',
+        'kitchen',
+        'beautifulsoup4',
+        'mailcap-fix',
+        ],
+    entry_points={'console_scripts': ['rtv=rtv.__main__:main']},
+    classifiers=[
+        'Intended Audience :: End Users/Desktop',
+        'Environment :: Console :: Curses',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Terminals',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Message Boards',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: News/Diary',
+        ],
+)
