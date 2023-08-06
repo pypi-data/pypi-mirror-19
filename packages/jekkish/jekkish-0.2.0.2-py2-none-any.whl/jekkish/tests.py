cf7158580121eeaf99b788bec564f4d400734f2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:21 +0000

    Add package jekkish/0.2.0.2/jekkish-0.2.0.2-py2-none-any.whl

---
+++ packages/jekkish/jekkish-0.2.0.2-py2-none-any.whl/jekkish/tests.py
@@ -0,0 +1,15 @@
+import unittest
+
+# Tests for main
+# - Arg: filename
+# - Arg: outputfile
+# - Arg: watch
+# - Render or watch
+
+# Tests for Jekkish
+# Init
+# - These all get set properly, spliced properly, etc.
+# load_variables
+# - Prevents "content" from being in the YAML header
+# make_file
+# - command gets set properly. Maybe I should just handle this in init, too
