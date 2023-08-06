204c5bd7ee527639e4ea667f36bead74dd858fd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:29 +0000

    Add package cfconfigurator/0.2.2/cfconfigurator-0.2.2.tar.gz

---
+++ packages/cfconfigurator/cfconfigurator-0.2.2.tar.gz/cfconfigurator-0.2.2/cfconfigurator/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+python-cfconfigurator
+"""
+
+__program__ = "cfconfigurator"
+__version__ = "0.2.2"
+__author__ = "Jose Riguera"
+__year__ = "2016"
+__email__ = "<jose.riguera@springer.com>"
+__license__ = "MIT"
