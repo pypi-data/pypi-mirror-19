cb18983cbcf310abb28a2103c3d9e36ea0d4fee7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:27 +0000

    Add package PyWebTools/1.1.3/PyWebTools-1.1.3.tar.gz

---
+++ packages/PyWebTools/PyWebTools-1.1.3.tar.gz/PyWebTools-1.1.3/README.RST
@@ -0,0 +1,16 @@
+PyWebTools README
+=================
+
+The PyWebTools package provides a number of helpers for use with Pyramid_, Kajiki_,
+Formencode_, and SQLAlchemy_ packages.
+
+Full documentation can be found at http://pywebtools.readthedocs.io.
+
+.. image:: https://readthedocs.org/projects/pywebtools/badge/?version=latest
+   :target: http://pywebtools.readthedocs.io/en/latest/?badge=latest
+   :alt: Documentation Status
+
+.. _Pyramid: https://trypyramid.com/
+.. _Kajiki: http://pythonhosted.org/Kajiki/
+.. _Formencode: http://www.formencode.org
+.. _SQLAlchemy: http://www.sqlalchemy.org/
---
+++ packages/PyWebTools/PyWebTools-1.1.3.tar.gz/PyWebTools-1.1.3/setup.py
@@ -0,0 +1,55 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.RST')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.RST')).read()
+
+requires = [
+    'kajiki',
+    'sqlalchemy',
+    'formencode',
+    'pyramid',
+    'decorator',
+    'transaction',
+    'zope.sqlalchemy'
+    ]
+
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'pytest',  # includes virtualenv
+    'pytest-cov',
+    ]
+
+setup(name='PyWebTools',
+      version='1.1.3',
+      description='A collection of helpers for use with Pyramid, Kajiki, Formencode, and SQLAlchemy',
+      long_description=README + '\n\n' +  CHANGES,
+      classifiers=[
+                   'Development Status :: 4 - Beta',
+                   'Programming Language :: Python :: 3',
+                   'Topic :: Internet :: WWW/HTTP',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+                   'Operating System :: OS Independent'
+        ],
+      author='Mark Hall',
+      author_email='Mark.Hall@work.room3b.eu',
+      url='http://bitbucket.org/mhall/pywebtools/overview',
+      keywords='web pyramid kajiki formencode SQLAlchemy',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='nose.collector',
+      install_requires = requires,
+      extras_require={
+          'testing': tests_require,
+      },
+      entry_points="""
+      [pytest11]
+      pywebtools_testing = pywebtools.testing
+      """
+      )
