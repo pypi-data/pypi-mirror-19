68525b3202eeb1440d62f1cc178e6f942197b8fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:35 +0000

    Add package everest-pipeline/2.0/everest_pipeline-2.0-py2.py3-none-any.whl

---
+++ packages/everest-pipeline/everest_pipeline-2.0-py2.py3-none-any.whl/everest/__init__.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function, absolute_import, unicode_literals
+
+# Version number
+__version__ = "2.0"
+
+# Was everest imported from setup.py?
+try:
+  __EVEREST_SETUP__
+except NameError:
+  __EVEREST_SETUP__ = False
+
+if __EVEREST_SETUP__:
+  # Set up the individual missions
+  from . import missions
+  for mission in missions.Missions:
+    getattr(missions, mission).Setup()
+else:
+  # This is a regular everest run
+  
+  # Import all modules
+  from . import config
+  from . import utils
+  from . import math
+  from . import transit
+  from . import pool
+  from . import fits
+  from . import dvs
+  from . import gp
+  from . import missions
+  from . import basecamp
+  from . import detrender
+  from . import inject
+  from . import user
+  
+  # Import the good stuff
+  from .detrender import *
+  from .inject import *
+  from .missions import *
+  from .transit import Transit
+  from .user import Everest, DVS
