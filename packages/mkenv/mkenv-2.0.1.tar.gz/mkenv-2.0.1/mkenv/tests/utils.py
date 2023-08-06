3ce9f9ceca56deb6889e6eefa38a2a92ae973e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:14 +0000

    Add package mkenv/2.0.1/mkenv-2.0.1.tar.gz

---
+++ packages/mkenv/mkenv-2.0.1.tar.gz/mkenv-2.0.1/mkenv/tests/utils.py
@@ -0,0 +1,88 @@
+from StringIO import StringIO
+import os
+import sys
+
+from filesystems import Path
+import click.testing
+import filesystems.memory
+
+from mkenv.common import Locator, VirtualEnv
+
+
+class CLIMixin(object):
+    def setUp(self):
+        super(CLIMixin, self).setUp()
+
+        self.stdin = StringIO()
+        self.stdout = StringIO()
+        self.stderr = StringIO()
+
+        self.filesystem = filesystems.memory.FS()
+        self.link_dir = Path("bin")
+        self.filesystem.create_directory(self.link_dir)
+
+        self.locator = Locator(
+            root=Path.root(),
+            make_virtualenv=lambda **kwargs: VirtualEnv(
+                create=self.fake_create,
+                install=self.fake_install,
+                **kwargs
+            ),
+        )
+        self.installed = {}
+
+    def fake_create(self, virtualenv, **kwargs):
+        self.filesystem.create_directory(path=virtualenv.path)
+
+    def fake_install(self, virtualenv, packages, requirements, **kwargs):
+        self.installed.setdefault(virtualenv, []).append(
+            (packages, requirements),
+        )
+
+    def run_cli(self, argv=(), exit_status=os.EX_OK):
+        runner = click.testing.CliRunner()
+        result = runner.invoke(
+            self._fix_click(self.cli.main),
+            args=argv,
+            default_map=dict(
+                link_dir=self.link_dir,
+                locator=self.locator,
+                filesystem=self.filesystem,
+            ),
+        )
+        if result.exception and not isinstance(result.exception, SystemExit):
+            cls, exc, tb = result.exc_info
+            raise cls, exc, tb
+
+        self.assertEqual(
+            result.exit_code,
+            exit_status,
+            msg="Different exit code, {} != {}\n\nstderr:\n\n{!r}".format(
+                result.exit_code, exit_status, self.stderr.getvalue(),
+            ),
+        )
+        return (
+            self.stdin.getvalue(),
+            self.stdout.getvalue(),
+            self.stderr.getvalue(),
+        )
+
+    def _fix_click(self, real_main):
+        """
+        Click is really really really annoying.
+
+        It patches sys.stdout and sys.stderr to the same exact StringIO.
+
+        """
+
+        class Fixed(object):
+            def __getattr__(self, attr):
+                return getattr(real_main, attr)
+
+            def main(this, *args, **kwargs):
+                stdout, sys.stdout = sys.stdout, self.stdout
+                stderr, sys.stderr = sys.stderr, self.stderr
+                real_main.main(*args, **kwargs)
+                sys.stdout = stdout
+                sys.stderr = stderr
+        return Fixed()
