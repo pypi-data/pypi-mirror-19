affdc0112b77a43dfad355cb3396b1815c681a4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:19 +0000

    Add package kozinaki/0.1.7/kozinaki-0.1.7.tar.gz

---
+++ packages/kozinaki/kozinaki-0.1.7.tar.gz/kozinaki-0.1.7/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name='kozinaki',
+    description='OpenStack multi-cloud driver for AWS, Azure',
+    url='https://github.com/compunova/kozinaki.git',
+    author='Compunova',
+    author_email='kozinaki@compu-nova.com',
+    version='0.1.7',
+    long_description=readme(),
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'haikunator',
+        'requests==2.11',
+        'azure',
+        'boto3',
+        'google-cloud',
+        'cryptography',
+        'Fabric3',
+        'Jinja2',
+        'PyYAML',
+        'terminaltables',
+        'apache-libcloud',
+    ],
+    entry_points={
+        'console_scripts': [
+            'kozinaki-manage=kozinaki.manage.__main__:main',
+        ],
+    }
+)
