c5be2ebfc5f615f21d8cdc48dc203e63004ec379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:58 +0000

    Add package pywsd/1.0.2/pywsd-1.0.2.tar.gz

---
+++ packages/pywsd/pywsd-1.0.2.tar.gz/pywsd-1.0.2/pywsd/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python -*- coding: utf-8 -*-
+#
+# Python Word Sense Disambiguation (pyWSD)
+#
+# Copyright (C) 2014-2017 alvations
+# URL:
+# For license information, see LICENSE.md
+
+import pywsd.lesk
+import pywsd.baseline
+import pywsd.similarity
+
+#import semcor
+#import semeval
+
+from pywsd.allwords_wsd import disambiguate
