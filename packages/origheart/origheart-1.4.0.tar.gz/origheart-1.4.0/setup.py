f81380db748d01fb53f40add0f3391ac938c47f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:19 +0000

    Add package origheart/1.4.0/origheart-1.4.0.tar.gz

---
+++ packages/origheart/origheart-1.4.0.tar.gz/origheart-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'origheart',
+    version         = '1.4.0',
+    py_modules      = ['origheart'],
+    author          = 'origheart',
+    author_email    = 'origheart@gmail.com',
+    url             = 'http://www.origheart.com',
+    description     = 'A simple printer of nested lists.',
+)
