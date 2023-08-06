de91feb3031b70182a9fcc0803bcf9a53c677dfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:06 +0000

    Add package ebclient.py/0.1.16/ebclient.py-0.1.16.tar.gz

---
+++ packages/ebclient.py/ebclient.py-0.1.16.tar.gz/ebclient.py-0.1.16/ebclient/create_uo/TemplateFields/__init__.py
@@ -0,0 +1,13 @@
+"""Field names in UO template"""
+type = "type"
+generation = "generation"
+commkey = "commkey"
+billingkey = "billingkey"
+appkey = "appkey"
+environment = "environment"
+
+yes = "yes"
+no = "no"
+
+FLAG_APP_GEN = 21
+FLAG_COMM_GEN = 20
