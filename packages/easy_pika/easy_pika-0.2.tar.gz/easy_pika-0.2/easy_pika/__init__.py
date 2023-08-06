26cedca93e4fe578147027d29f51ef3da2d34a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:53 +0000

    Add package easy_pika/0.2/easy_pika-0.2.tar.gz

---
+++ packages/easy_pika/easy_pika-0.2.tar.gz/easy_pika-0.2/easy_pika/__init__.py
@@ -0,0 +1,5 @@
+def hello():
+	print "Hello world!"
+
+__all__ = [ hello ]
+
---
+++ packages/easy_pika/easy_pika-0.2.tar.gz/easy_pika-0.2/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+	name = 'easy_pika',
+	packages = ['easy_pika'],
+	version = '0.2',
+	description = 'Encapsulation of pika that make our life easier.',
+	author = 'fzuwwl',
+	author_email = 'fzuwwl@gmail.com',
+	url = 'https://github.com/kikimo/easy_pika',
+	# download_url = '',
+	keywords = ['pika', 'rabbit', 'mq', 'RabbitMQ'],
+	classifiers = [],
+)
