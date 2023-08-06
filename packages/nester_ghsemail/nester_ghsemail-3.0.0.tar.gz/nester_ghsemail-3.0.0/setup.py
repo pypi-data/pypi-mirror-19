a6c040d50b9450651b18154643bb009db200348f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:06 +0000

    Add package nester_ghsemail/3.0.0/nester_ghsemail-3.0.0.tar.gz

---
+++ packages/nester_ghsemail/nester_ghsemail-3.0.0.tar.gz/nester_ghsemail-3.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'nester_ghsemail',
+    version='3.0.0',
+    py_modules = ['nester'],
+    author='ghsemail',
+    authoremail = 'ghsemail@163.com',
+    url = 'http://www.hadfirstlabs.com',
+    description='A simple athlete program',
+)
