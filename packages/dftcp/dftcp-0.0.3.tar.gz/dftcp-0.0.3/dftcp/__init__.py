16ae0d4c61c58bd0999002d8ef4e9f23b62bec4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:55 +0000

    Add package dftcp/0.0.3/dftcp-0.0.3.tar.gz

---
+++ packages/dftcp/dftcp-0.0.3.tar.gz/dftcp-0.0.3/dftcp/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 26 16:06:08 2016
+
+@author: chernov
+"""
+
+from .tcp_protocol import DataforgeEnvelopeProtocol
+from .tcp_protocol import DataforgeEnvelopeEchoClient
