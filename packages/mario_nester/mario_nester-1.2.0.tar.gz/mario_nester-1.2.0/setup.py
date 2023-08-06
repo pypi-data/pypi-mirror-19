39dbb0f9a5ac8d30487308bdeaac07bfb8f109bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:12 +0000

    Add package mario_nester/1.2.0/mario_nester-1.2.0.tar.gz

---
+++ packages/mario_nester/mario_nester-1.2.0.tar.gz/mario_nester-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = "mario_nester",
+	version = "1.2.0",
+	py_modules = ["nester"],
+	author = "Mario",
+	author_email = "frankxuanhui@aliyun.com",
+	description = "A simple printer of nested lists"
+)
