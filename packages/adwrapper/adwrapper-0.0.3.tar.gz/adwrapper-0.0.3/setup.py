a1b5d9196f6fe6e7b94f960e5039e497083e0f6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:18 +0000

    Add package adwrapper/0.0.3/adwrapper-0.0.3.tar.gz

---
+++ packages/adwrapper/adwrapper-0.0.3.tar.gz/adwrapper-0.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+        name='adwrapper',
+        version='0.0.3',
+        packages=find_packages(exclude=['tests', 'test.*']),
+        url='https://github.com/incognitjoe/adwrapper',
+        author='Joe Butler',
+        author_email='incognitjoeb@gmail.com',
+        description='Simple wrapper for common Active Directory actions.',
+        install_requires=[
+            'python-ldap',
+        ]
+)
