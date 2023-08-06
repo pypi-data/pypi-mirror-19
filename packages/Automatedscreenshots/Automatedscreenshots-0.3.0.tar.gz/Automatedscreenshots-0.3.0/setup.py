f3330a1433c2ffbd2d785d539a24815eeca3d116
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:51 +0000

    Add package Automatedscreenshots/0.3.0/Automatedscreenshots-0.3.0.tar.gz

---
+++ packages/Automatedscreenshots/Automatedscreenshots-0.3.0.tar.gz/Automatedscreenshots-0.3.0/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='Automatedscreenshots',
+    version='0.3.0',
+    author='Jim Barcelona',
+    author_email='barce@me.com',
+    packages=['automatedscreenshots', 'automatedscreenshots.tests'],
+    scripts=[],
+    url='http://pypi.python.org/pypi/Automatedscreenshots/',
+    license='LICENSE.txt',
+    description='Useful screenshots stuff.',
+    long_description=open('README.txt').read(),
+)
