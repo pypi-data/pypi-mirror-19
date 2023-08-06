3bd6de54950965193e3ce05affd65d30fcde4046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:34 +0000

    Add package django-webpack-helper/0.2.1/django-webpack-helper-0.2.1.tar.gz

---
+++ packages/django-webpack-helper/django-webpack-helper-0.2.1.tar.gz/django-webpack-helper-0.2.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-webpack-helper",
+    version="0.2.1",
+    url="http://github.com/chriscz/django-webpack-helper",
+    description="A django app that helps integrate webpack",
+    author="Chris Coetzee",
+    license='Mozilla Public License 2.0 (MPL 2.0)',
+    author_email='chriscz93@gmail.com',
+    packages=find_packages(exclude=['exampleapp']),
+    install_requires=[
+        'django-webpack-loader'
+    ],
+    include_package_data=True,
+
+    classifiers = [
+        "Programming Language :: Python :: 2.7",            
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+    ]
+)
