35c08ad5059e6daef346df3ef4a6550e937a1185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:58 +0000

    Add package Py-TabWriter/0.1.2/Py_TabWriter-0.1.2.tar.gz

---
+++ packages/Py-TabWriter/Py_TabWriter-0.1.2.tar.gz/Py_TabWriter-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+desc = '\n'.join(['A module for writing formatted columns of text.',
+                  'See https://github.com/qazbnm456/py_tabwriter for details'])
+
+setup(
+    name='Py_TabWriter',
+    version='0.1.2',
+    packages=['py_tabwriter'],
+    license='BSD 3-clause license',
+    long_description=desc,
+    author='Boik Su',
+    author_email='boik@tdohacker.org',
+    url='https://github.com/qazbnm456/py_tabwriter'
+)
