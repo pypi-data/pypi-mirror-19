c52110e0136a8599d7c0405fc5e5ea2250c8b6c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:50 +0000

    Add package simpil/1.0.1/simpil-1.0.1-py2.py3-none-any.whl

---
+++ packages/simpil/simpil-1.0.1-py2.py3-none-any.whl/simpil/__init__.py
@@ -0,0 +1,13 @@
+__version__ = u'1.0.1'
+
+from PIL import ImageFont
+
+RGB = u"RGB"
+JPEG = u"JPEG"
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255,)
+WHITE = (255, 255, 255)
+
+DEFAULT_FONT = ImageFont.load_default()
