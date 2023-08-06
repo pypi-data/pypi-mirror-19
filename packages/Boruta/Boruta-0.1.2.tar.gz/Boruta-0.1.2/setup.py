5378446ea05f5fb1491677d19b15c5a14562d932
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:12 +0000

    Add package Boruta/0.1.2/Boruta-0.1.2.tar.gz

---
+++ packages/Boruta/Boruta-0.1.2.tar.gz/Boruta-0.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='Boruta',
+      version='0.1.2',
+      description='Python Implementation of Boruta Feature Selection',
+      url='https://github.com/danielhomola/boruta_py',
+      download_url='https://github.com/danielhomola/boruta_py/tarball/0.1.2',
+      author='Daniel Homola',
+      author_email='dani.homola@gmail.com',
+      license='BSD 3 clause',
+      packages=['boruta'],
+      keywords=['feature selection', 'machine learning', 'random forest'],
+      install_requires=['numpy>=1.10.4',
+                        'scikit-learn>=0.17.1',
+                        'scipy>=0.17.0'
+                        ],
+      setup_requires=['setuptools-markdown'],
+      long_description_markdown_filename='README.md')
