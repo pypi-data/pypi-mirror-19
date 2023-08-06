d21413a03736fc6adb6809a65a75646a7e917d9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:09 +0000

    Add package nesterCAO/1.0.0/nesterCAO-1.0.0.zip

---
+++ packages/nesterCAO/nesterCAO-1.0.0.zip/nesterCAO-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+          name                      ='nesterCAO',
+          version                   ='1.0.0',
+          py_modules                =['nesterCAO'],
+          author                    ='hfpython',
+          author_email              ='chwei20002005@sina.com',
+          url                       ='http://www.headfirstlabs.com',
+          description               = 'A simple printer of nested lists',
+          )
