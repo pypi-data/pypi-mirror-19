f73e142cdb00a16ada9f35d6dae989af10aa15ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:10 +0000

    Add package mosaic-nist/1.3.2/mosaic-nist-1.3.2.tar.gz

---
+++ packages/mosaic-nist/mosaic-nist-1.3.2.tar.gz/mosaic-nist-1.3.2/mosaic/utilities/sqlQuery.py
@@ -0,0 +1,17 @@
+"""
+	Wrapper function for sqliteMDIO
+"""
+import mosaic.mdio.sqlite3MDIO as sql
+
+__all__=["query"]
+
+def query(dbname, query_str):
+	"""
+		Simple wrapper to perform a query on a MOSAIC database.
+	"""
+	db=sql.sqlite3MDIO()
+	db.openDB(dbname)
+	q=db.queryDB(query_str)
+	db.closeDB()
+
+	return q
