7c083f4bdc0a4092c2ae1dc27ba2301396a39175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:39 +0000

    Add package pex/1.2.0/pex-1.2.0-py2.py3-none-any.whl

---
+++ packages/pex/pex-1.2.0-py2.py3-none-any.whl/pex/version.py
@@ -0,0 +1,8 @@
+# Copyright 2015 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+__version__ = '1.2.0'
+
+PACKAGING_REQUIREMENT = 'packaging>=16.8,<17.0'
+SETUPTOOLS_REQUIREMENT = 'setuptools>=20.0,<34.0'
+WHEEL_REQUIREMENT = 'wheel>=0.26.0,<0.30.0'
