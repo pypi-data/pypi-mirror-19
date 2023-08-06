e316c4d1312094c96906d0f57b2f6d3bf317bc50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:12 +0000

    Add package conda-autoenv/0.1/conda-autoenv-0.1.tar.gz

---
+++ packages/conda-autoenv/conda-autoenv-0.1.tar.gz/conda-autoenv-0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+	long_description = f.read()
+
+setup(
+	name='conda-autoenv',
+	version='0.1',
+	description='Automatic Conda Environment',
+	long_description=long_description,
+	url='https://github.com/sharonzhou/conda-autoenv',
+	download_url='https://github.com/sharonzhou/conda-autoenv/tarball/0.1',
+	author='Sharon Zhou',
+	author_email='sharonzpost@gmail.com',
+	license='MIT',
+	classifiers=[
+		'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+	],
+	keywords=['conda', 'auto', 'env', 'automatic', 'environment', 'directory', 'pip requirements', 'environment.yml', 'yaml'],
+	packages=find_packages(),
+	install_requires=['conda'],
+	scripts=['conda_autoenv.sh']
+)
