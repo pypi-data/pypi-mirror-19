2245058b657267a1cc62cf780fd07a60ea41cdbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:58 +0000

    Add package JamesLi123/1.4.0/JamesLi123-1.4.0.zip

---
+++ packages/JamesLi123/JamesLi123-1.4.0.zip/JamesLi123-1.4.0/JamesLi123.py
@@ -0,0 +1,18 @@
+"""This is "nester.py" module, which provides a function named print_lol(). This function is uesd
+to print the list, including(or not) nested list."""
+
+
+def print_lol(the_list, indent=False, level=0):
+    """This function has a location parameter named "the_list", and it is any Python list(maybe a
+    list including nested list). Each data item of the specified list will be printed to the screen
+    (recursively), and each item will occupy a line space respectively. The second argument(named
+    "indent")controls whether indent or not. The third argument(named "level") is used to insert
+    TAB when it meets nested lists."""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t", end='')
+            print(each_item)
