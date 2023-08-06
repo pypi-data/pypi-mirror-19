fe63420c566e599f3127746656c6c5cb242077c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:46 +0000

    Add package wolf_nester/1.0.0/wolf_nester-1.0.0.tar.gz

---
+++ packages/wolf_nester/wolf_nester-1.0.0.tar.gz/wolf_nester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='wolf_nester',
+    version='1.0.0',
+    py_modules=['wolf_nester'],
+    author='wolf',
+    author_email='sunboy_liaobin20@163.com',
+    description='A simple printer of nested lists'
+)
