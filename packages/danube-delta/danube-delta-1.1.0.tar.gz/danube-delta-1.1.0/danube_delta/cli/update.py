d4c2a09f964c7f97757b5778ccae0bef448f89e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:13 +0000

    Add package danube-delta/1.1.0/danube-delta-1.1.0.tar.gz

---
+++ packages/danube-delta/danube-delta-1.1.0.tar.gz/danube-delta-1.1.0/danube_delta/cli/update.py
@@ -0,0 +1,11 @@
+
+from . import blog
+from .helpers import run
+
+
+@blog.command()
+def update():
+    """Gets other people's changes from GitHub"""
+
+    run('git pull --rebase origin master')
+    run('pip install -r requirements.txt')
