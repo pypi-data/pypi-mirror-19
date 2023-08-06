1cb78b2c6780ec44c30641ad43d17bfde3bf50c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:22 +0000

    Add package solcast/0.1.0/solcast-0.1.0-py2.py3-none-any.whl

---
+++ packages/solcast/solcast-0.1.0-py2.py3-none-any.whl/solcast/__init__.py
@@ -0,0 +1,9 @@
+from .pv_power_forecasts import PvPowerForecasts as get_pv_power_forecasts
+from .pv_power_estimated_actuals import PvPowerEstimatedActuals as get_pv_power_estimated_actuals
+from .radiation_forecasts import RadiationForecasts as get_radiation_forecasts
+from .radiation_estimated_actuals import RadiationEstimatedActuals as get_radiation_estimated_actuals
+
+from .pv_power_forecasts import PvPowerForecasts
+from .pv_power_estimated_actuals import PvPowerEstimatedActuals
+from .radiation_forecasts import RadiationForecasts
+from .radiation_estimated_actuals import RadiationEstimatedActuals
