da58ca3d276847fa2974be02467c411c368b58d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:26 +0000

    Add package jconf/0.1/jconf-0.1.zip

---
+++ packages/jconf/jconf-0.1.zip/jconf-0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='jconf',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Violentshell',
+    author_email='violentshell@gmail.com',
+    description='Simple module to write, load and update JSON config files.',
+    classifiers=['Development Status :: 3 - Alpha',
+                'Programming Language :: Python :: 3',
+                'Programming Language :: Python :: 3.4']
+)
