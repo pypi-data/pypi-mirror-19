972a92cfeb4fed31e8be0cec9b9ccf37184196cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:58 +0000

    Add package mv-logger/0.0.2/mv-logger-0.0.2.tar.gz

---
+++ packages/mv-logger/mv-logger-0.0.2.tar.gz/mv-logger-0.0.2/mv_logger/logstash.py
@@ -0,0 +1,28 @@
+from logstash.formatter import LogstashFormatterVersion1
+
+
+class Formatter(LogstashFormatterVersion1):
+
+    def format(self, record):
+        # Create message dict
+        message = {
+            '@timestamp': self.format_timestamp(record.created),
+            '@version': '1',
+            'host': self.host,
+            'pathname': record.pathname,
+            'tags': self.tags,
+            'msg': record.getMessage(),
+
+            # Extra Fields
+            'level': record.levelname,
+            'logger_name': record.name,
+        }
+
+        # Add extra fields
+        message.update(self.get_extra_fields(record))
+
+        # If exception, add debug info
+        if record.exc_info:
+            message.update(self.get_debug_fields(record))
+
+        return self.serialize(message)
