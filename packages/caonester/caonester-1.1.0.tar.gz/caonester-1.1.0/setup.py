e468374b7331a9963990b52d6bb320c54c3b3dc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:12 +0000

    Add package caonester/1.1.0/caonester-1.1.0.tar.gz

---
+++ packages/caonester/caonester-1.1.0.tar.gz/caonester-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='caonester',
+    version='1.1.0',
+    py_modules=['printf_fun'],
+    author='jinn',
+    author_email='jinnsblog@gmail.com',
+    url='http://www.jinnsblog.com',
+    description='simple demo'
+)
