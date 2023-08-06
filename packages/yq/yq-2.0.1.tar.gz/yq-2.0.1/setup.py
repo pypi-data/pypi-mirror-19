fc4af1cee9bfa2c764ef925d1860440f554243f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:49 +0000

    Add package yq/2.0.1/yq-2.0.1.tar.gz

---
+++ packages/yq/yq-2.0.1.tar.gz/yq-2.0.1/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os, glob
+from setuptools import setup, find_packages
+
+setup(
+    name="yq",
+    version="2.0.1",
+    url="https://github.com/kislyuk/yq",
+    license="Apache Software License",
+    author="Andrey Kislyuk",
+    author_email="kislyuk@gmail.com",
+    description="yq: Command-line YAML processor - jq wrapper for YAML documents",
+    long_description=open("README.rst").read(),
+    install_requires=[
+        "setuptools",
+        "PyYAML >= 3.11"
+    ],
+    packages=find_packages(exclude=["test"]),
+    scripts=glob.glob("scripts/*"),
+    include_package_data=True,
+    platforms=["MacOS X", "Posix"],
+    test_suite="test",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ]
+)
