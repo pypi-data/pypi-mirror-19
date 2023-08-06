01b87937bfe0ebbd9a7ca74379ba062441e16637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:53 +0000

    Add package poppy-torso/1.1.5/poppy-torso-1.1.5.tar.gz

---
+++ packages/poppy-torso/poppy-torso-1.1.5.tar.gz/poppy-torso-1.1.5/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import re
+import sys
+
+from setuptools import setup, find_packages
+
+
+def version():
+    with open('poppy_torso/_version.py') as f:
+        return re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", f.read()).group(1)
+
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+
+setup(name='poppy-torso',
+      version=version(),
+      packages=find_packages(),
+
+      install_requires=['poppy-creature >= 1.8',
+                        'pypot >= 2.11'],
+
+      include_package_data=True,
+      exclude_package_data={'': ['README', '.gitignore']},
+
+      zip_safe=False,
+
+      author='Pierre Rouanet, Matthieu Lapeyre',
+      author_email='pierre.rouanet@gmail.com',
+      description='Poppy Torso Software Library',
+      url='https://github.com/poppy-project/poppy-creature',
+      license='GNU GENERAL PUBLIC LICENSE Version 3',
+
+      **extra
+      )
