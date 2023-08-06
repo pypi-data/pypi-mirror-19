65b851551fc1a7fd55be61a46292555fbc415839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:48 +0000

    Add package excel-export/0.4.2/excel-export-0.4.2.zip

---
+++ packages/excel-export/excel-export-0.4.2.zip/excel-export-0.4.2/excel_export/cli.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import argparse
+import extract, export
+from excel_export import __version__
+
+def main(argv):
+	parser = argparse.ArgumentParser('excel-extract_tables', 'Export sql(sqlite, mysql) or db(sqlite) from specifically formatted excel file.')
+	
+	parser.add_argument('-I', '--input-files', nargs='+', type=str, help='Excel file path to export scripts. -I file1.xlsx file2.xlsx')
+	
+	group_sqlite = parser.add_argument_group('[For Sqlite]', 'Options for sqlite exporting.')
+	group_sqlite.add_argument('--sqlite-output', default=None, type=str, help='Output FILE NAME for sqlite script.')
+	group_sqlite.add_argument('--sqlite-add-create-table', default=False, action='store_true', help='Add DROP and CREATE TABLE statement in script file.')
+	group_sqlite.add_argument('--sqlite-with-db-file', default=False, action='store_true', help='Enable Exporting DB file.')
+	group_sqlite.add_argument('--sqlite-includeonly', default=None, type=str, help='tables and columns to only include exporting file. --sqlite-includeonly "table1,table2(col1,col2)"')
+	group_sqlite.add_argument('--sqlite-exclude', default=None, type=str, help='tables and columns to exclude exporting file. --sqlite-exclude "table1,table2(col1,col2)"')
+	
+	group_mysql = parser.add_argument_group('[For MySql]', 'Options for mysql exporting.')
+	group_mysql.add_argument('--mysql-output', default=None, type=str, help='Output FILE NAME for mysql script.')
+	group_mysql.add_argument('--mysql-add-create-table', default=False, action='store_true', help='Add DROP and CREATE TABLE statement in script file.')
+	group_mysql.add_argument('--mysql-add-truncate', default=False, action='store_true', help='Add TRUNCATE TABLE statement in script file.')
+	group_mysql.add_argument('--mysql-extended-insert', default=False, action='store_true', help='Write INSERT statements using multiple-row syntax that includes several VALUES lists')
+	group_mysql.add_argument('--mysql-includeonly', default=None, type=str, help='tables and columns to only include exporting file. --mysql-includeonly "table1,table2(col1,col2)"')
+	group_mysql.add_argument('--mysql-exclude', default=None, type=str, help='tables and columns to exclude exporting file. --sqlite-exclude "table1,table2(col1,col2)"')
+	
+	parser.add_argument('--version', default=False, action='store_true', help='Print the current version')
+	
+	args = parser.parse_args()
+	
+	if args.version:
+		print('excel-export version {}'.format(__version__))
+		exit(0)
+	
+	if args.input_files and (args.sqlite_output or args.mysql_output):
+		all_tables = {}
+		
+		for excel_file in args.input_files:
+			excel_file = unicode(excel_file, sys.getfilesystemencoding())
+			print excel_file
+			
+			tables = extract.extract_tables_from_excel(excel_file)
+			all_tables.update(tables)
+		
+		if args.sqlite_output:
+			includeonly_info = parse_filter_info(args.sqlite_includeonly) if args.sqlite_includeonly else None
+			exclude_info = parse_filter_info(args.sqlite_exclude) if args.sqlite_exclude else None
+			sqls = export.convert_to_sqls_for_sqlite(all_tables, exclude_info=exclude_info, includeonly_info=includeonly_info, add_create_table=args.sqlite_add_create_table)
+			
+			export.export_to_sqlfile(sqls, args.sqlite_output)
+			
+			if args.sqlite_with_db_file and args.sqlite_add_create_table:
+				export.export_to_sqlite3(sqls, args.sqlite_output + ".db")
+			
+			pass
+		
+		if args.mysql_output:
+			includeonly_info = parse_filter_info(args.mysql_includeonly) if args.mysql_includeonly else None
+			exclude_info = parse_filter_info(args.mysql_exclude) if args.mysql_exclude else None
+			sqls = export.convert_to_sqls_for_mysql(all_tables, exclude_info=exclude_info, includeonly_info=includeonly_info, add_create_table=args.mysql_add_create_table, add_truncate=args.mysql_add_truncate, extended_insert=args.mysql_extended_insert)
+			
+			export.export_to_sqlfile(sqls, args.mysql_output)
+			
+			pass
+	
+	else:
+		parser.print_help()
+	
+	pass
+
+#추출시 제외할 정보 파서
+#return (제외한 테이블, 제외할 컬럼) ( set(except_table), { table : set(except_cols) } )
+def parse_filter_info(filter_info):
+	filter_tables = set([])
+	filter_cols = {}
+	
+	last_elem = []
+	in_bracket_tab = None
+	
+	for ch in filter_info:
+		if ch == '(':
+			in_bracket_tab = ''.join(last_elem).strip()
+			if in_bracket_tab:
+				filter_cols[in_bracket_tab] = set([])
+			last_elem = []
+		elif ch == ')':
+			if in_bracket_tab:
+				v = ''.join(last_elem).strip()
+				if v:
+					filter_cols[in_bracket_tab].add(v)
+				
+			last_elem = []
+			in_bracket_tab = None
+		elif ch == ',':
+			v = ''.join(last_elem).strip()
+			if v:
+				if in_bracket_tab:
+					filter_cols[in_bracket_tab].add(v)
+				else:
+					filter_tables.add(v)
+				
+			last_elem = []
+		else:
+			last_elem.append(ch)
+	
+	v = ''.join(last_elem).strip()
+	if v:
+		filter_tables.add(v)
+	
+	
+	return (filter_tables, filter_cols)
+
+def entry_point():
+	main(sys.argv[1:])
+
+if __name__ == "__main__":
+	entry_point()
+	
---
+++ packages/excel-export/excel-export-0.4.2.zip/excel-export-0.4.2/excel_export/export.py
@@ -0,0 +1,211 @@
+# -*- coding: utf-8 -*-
+
+import sqlite3
+import os
+import codecs
+import sys
+
+def export_to_sqlfile(sqls, sql_file):
+	with codecs.open(sql_file, "w", 'utf-8') as file_fd:
+		for sql in sqls:
+			file_fd.write(sql + "\n")
+	
+def export_to_sqlite3(sqls, db_file):
+	if os.path.exists(db_file):
+		os.remove(db_file)
+		
+	with sqlite3.connect(db_file) as con:
+		for sql in sqls:
+			try:
+				con.execute(sql)
+			except:
+				print "execute sqlite: ", sql.encode('utf8')
+				exc_type, exc_value, exc_traceback  = sys.exc_info()
+				exc_value = "%s; %s" % (exc_value, sql.encode('utf8'))
+				raise exc_type, exc_value, exc_traceback
+
+def convert_to_sqls_for_sqlite(tables, **kwargs):
+	includeonly_info = kwargs.get("includeonly_info", None)
+	exclude_info = kwargs.get("exclude_info", None)
+	add_create_table = kwargs.get("add_create_table", False)
+	
+	type_map = {'T': 'TEXT', 'I': 'INTEGER', 'N': 'NUMERIC', 'D': 'REAL'}
+	sqls = []
+	
+	try:
+		for table_name in sorted(tables):
+			if includeonly_info and table_name not in includeonly_info[0] and table_name not in includeonly_info[1]:
+				continue
+			if exclude_info and table_name in exclude_info[0]:
+				continue
+			
+			val = tables[table_name]
+			
+			if includeonly_info and includeonly_info[1].has_key(table_name):
+				val = _includeonly_columns(val, includeonly_info[1][table_name])
+			if exclude_info and exclude_info[1].has_key(table_name):
+				val = _exclude_columns(val, exclude_info[1][table_name])
+			
+			col_infos, values = val
+			
+			if len(col_infos) > 0:
+				create_cols = []
+				insert_cols = []
+				
+				for col_info in col_infos:
+					col_type = type_map[col_info[1][0]]
+					create_cols.append("`%s` %s" % (col_info[0], col_type))
+					insert_cols.append("`%s`" % col_info[0])
+					
+				pks = [ x[0] for x in col_infos if x[3] ]
+				if pks:
+					create_cols.append('PRIMARY KEY (%s)' % ", ".join('`' + x + '`' for x in pks))
+				
+				if add_create_table:
+					sqls.append("DROP TABLE IF EXISTS `%s`;" % table_name)
+					sqls.append("CREATE TABLE `%s` (%s);" % (table_name, ",".join(create_cols)))
+				
+				for row in values:
+					row_val = _convert_values(col_infos, row)
+					sqls.append("INSERT INTO `%s`(%s) VALUES (%s);" % (table_name, ",".join(insert_cols), ",".join(row_val)))
+					
+	except:
+		exc_type, exc_value, exc_traceback  = sys.exc_info()
+		exc_value = "%s; error at '%s'" % (exc_value, table_name)
+		raise exc_type, exc_value, exc_traceback
+	
+	return sqls
+	
+
+def convert_to_sqls_for_mysql(tables, **kwargs):
+	includeonly_info = kwargs.get("includeonly_info", None)
+	exclude_info = kwargs.get("exclude_info", None)
+	add_create_table = kwargs.get("add_create_table", False)
+	add_truncate = kwargs.get("add_truncate", False)
+	extended_insert = kwargs.get("extended_insert", False)
+
+	type_map = {'T': 'VARCHAR', 'I': 'INT', 'N': 'DECIMAL', 'D': 'DATETIME'}
+	sqls = []
+
+	try:
+		for table_name in sorted(tables):
+			if includeonly_info and table_name not in includeonly_info[0] and table_name not in includeonly_info[1]:
+				continue
+			if exclude_info and table_name in exclude_info[0]:
+				continue
+			
+			val = tables[table_name]
+			
+			if includeonly_info and includeonly_info[1].has_key(table_name):
+				val = _includeonly_columns(val, includeonly_info[1][table_name])
+			if exclude_info and exclude_info[1].has_key(table_name):
+				val = _exclude_columns(val, exclude_info[1][table_name])
+			
+			col_infos, values = val
+			
+			if len(col_infos) > 0:
+				create_cols = []
+				insert_cols = []
+				
+				for col_info in col_infos:
+					col_type = type_map[col_info[1][0]]
+					
+					if col_type == 'VARCHAR':
+						col_type = "VARCHAR(%d)" % col_info[1][1]
+					elif col_type == 'DECIMAL':
+						col_type = "DECIMAL(%d,%d)" % (col_info[1][1] + col_info[1][2], col_info[1][2])
+						
+					create_cols.append("`%s` %s" % (col_info[0], col_type))
+					insert_cols.append("`%s`" % col_info[0])
+					
+				pks = [ x[0] for x in col_infos if x[3] ]
+				if pks:
+					create_cols.append('PRIMARY KEY (%s)' % ", ".join('`' + x + '`' for x in pks))
+				
+				if add_create_table:
+					sqls.append("DROP TABLE IF EXISTS `%s`;" % table_name)
+					sqls.append("CREATE TABLE `%s` (%s);" % (table_name, ",".join(create_cols)))
+				elif add_truncate:
+					sqls.append("TRUNCATE `%s`;" % table_name)
+				
+				if extended_insert:
+					for row in values:
+						row_val = _convert_values(col_infos, row)
+						sqls.append("INSERT INTO `%s`(%s) VALUES (%s);" % (table_name, ",".join(insert_cols), ",".join(row_val)))
+				else:
+					sql_row_vals = []
+					for row in values:
+						sql_row_vals.append("(%s)" % ",".join(_convert_values(col_infos, row)))
+					
+					sqls.append("INSERT INTO `%s`(%s) VALUES \n%s;" % (table_name, ",".join(insert_cols), ",\n".join(sql_row_vals)))
+	except:
+		exc_type, exc_value, exc_traceback  = sys.exc_info()
+		exc_value = "%s; error at '%s'" % (exc_value, table_name)
+		raise exc_type, exc_value, exc_traceback
+	
+	return sqls
+
+	pass
+
+def _convert_values(col_infos, row):
+	values = []
+	
+	for i in xrange(len(col_infos)):
+		try:
+			val = None
+			if row[i] == None:
+				val = 'null'
+			else:
+				if col_infos[i][1][0] == 'T' or col_infos[i][1][0] == 'D':
+					val = "'%s'" % row[i]
+				else: # I or N
+					val = str(row[i])
+				
+			values.append(val)
+		except:
+			exc_type, exc_value, exc_traceback  = sys.exc_info()
+			exc_value = "%s; error at '%s'" % (exc_value, row[i])
+			raise exc_type, exc_value, exc_traceback
+	
+	return values
+
+def _includeonly_columns(val, includeonly_cols):
+	includeonly_indices = []
+	filtered_head = []
+	filtered_rows = []
+	
+	for idx, item in enumerate(val[0]):
+		if item[0] in includeonly_cols:
+			includeonly_indices.append(idx)
+			filtered_head.append(item)
+	
+	for row in val[1]:
+		filtered_row = []
+		for idx, v in enumerate(row):
+			if idx in includeonly_indices:
+				filtered_row.append(v)
+		
+		filtered_rows.append(tuple(filtered_row))
+	
+	return (filtered_head, filtered_rows)
+
+def _exclude_columns(val, exclude_cols):
+	exclude_indices = []
+	filtered_head = []
+	filtered_rows = []
+	
+	for idx, item in enumerate(val[0]):
+		if item[0] in exclude_cols:
+			exclude_indices.append(idx)
+		else:
+			filtered_head.append(item)
+	
+	for row in val[1]:
+		filtered_row = []
+		for idx, v in enumerate(row):
+			if idx not in exclude_indices:
+				filtered_row.append(v)
+		
+		filtered_rows.append(tuple(filtered_row))
+		
+	return (filtered_head, filtered_rows)
