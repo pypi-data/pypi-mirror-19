315dbebd0355e7f2dcf64d156be81bb28dea3b8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:38 +0000

    Add package rolabesti/0.2.0/rolabesti-0.2.0.tar.gz

---
+++ packages/rolabesti/rolabesti-0.2.0.tar.gz/rolabesti-0.2.0/rolabesti/copier.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from .utils import execute
+
+
+def copy(tracks, destiny):
+    print('[copier] copying {} track{} to {}'.format(len(tracks), 's'[len(tracks) == 1:], destiny))
+
+    command = ['cp'] + [track['path'] for track in tracks] + [destiny]
+    execute(command, background=False)
+
+    print('[copier] {} track{} copied'.format(len(tracks), 's'[len(tracks) == 1:]))
