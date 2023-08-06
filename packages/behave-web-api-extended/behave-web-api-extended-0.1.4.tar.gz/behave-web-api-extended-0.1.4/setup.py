0380434ba88a1b641d614f8193781a516e3e02e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:51 +0000

    Add package behave-web-api-extended/0.1.4/behave-web-api-extended-0.1.4.tar.gz

---
+++ packages/behave-web-api-extended/behave-web-api-extended-0.1.4.tar.gz/behave-web-api-extended-0.1.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+import behave_web_api_extended
+
+install_requires = [
+    'behave>=1.2.4',
+    'behave-web-api==1.0.6',
+    'bottle==0.12.9',
+    'freezegun>=0.3.8',
+    'ordereddict==1.1',
+    'nose==1.3.7',
+    'requests>=2.0.0'
+]
+
+
+setup(
+    name='behave-web-api-extended',
+    version=behave_web_api_extended.__version__,
+    packages=['behave_web_api_extended', 'behave_web_api_extended.steps'],
+    setup_requires=['wheel'],
+    install_requires=install_requires,
+    description="Provides additional testing for JSON APIs with Behave",
+    author='Victor Anjos',
+    author_email='va@deeplearni.ng',
+    license='MIT',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Testing',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7'
+    ]
+)
