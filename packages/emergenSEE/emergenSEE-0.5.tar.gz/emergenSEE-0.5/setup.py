2f78730529259722ebd2e85aa2dcd14514cce101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:46 +0000

    Add package emergenSEE/0.5/emergenSEE-0.5.tar.gz

---
+++ packages/emergenSEE/emergenSEE-0.5.tar.gz/emergenSEE-0.5/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'emergenSEE',
+  packages = ['emergenSEE'], # this must be the same as the name above
+  version = '0.5',
+  description = 'Visualize NYC EMS Emergencies',
+  author = 'Max Mattioli',
+  author_email = 'm.max@columbia.edu',
+  url = 'https://github.com/m-a-x/emergenSEE', # use the URL to the github repo
+  download_url = 'https://github.com/m-a-x/emergenSEE/tarball/0.5',
+  license='MIT',
+  classifiers = []
+)
