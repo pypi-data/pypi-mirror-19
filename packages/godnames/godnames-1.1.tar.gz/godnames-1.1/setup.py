38cd3721dd82b00d22a024696926696317d936b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:24 +0000

    Add package godnames/1.1/godnames-1.1.tar.gz

---
+++ packages/godnames/godnames-1.1.tar.gz/godnames-1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name = 'godnames',
+  packages = ['godnames'],
+  version = '1.1',
+  description = 'A library to generate randomly names',
+  author = 'Raphael Stefanini',
+  author_email = 'rphl@rphl.net',
+  url = 'https://github.com/rphlo/py-godnames',
+  download_url = 'https://github.com/rphlo/py-godnames/tarball/1.1',
+  keywords = ['random', 'naming', 'name'],
+  classifiers = [],
+)
