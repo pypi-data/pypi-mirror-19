fd85d4ff7b9b4b99cc32f5d708e61b1ccd8e2f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:18 +0000

    Add package pkinter/1.12.2/pkinter-1.12.2-py3-none-any.whl

---
+++ packages/pkinter/pkinter-1.12.2-py3-none-any.whl/pkinter/__init__.py
@@ -0,0 +1,18 @@
+from .toggledlabelframe import *
+from .labeledseparator import *
+from .roundingscale import *
+from .entrytext import *
+from .limitedentry import *
+from .colourpickerbutton import *
+from .editablelabel import *
+from .collapsiblepane import *
+from .hyperlink import *
+from .pageview import *
+from .toolbar import *
+from .statusbar import *
+from .linenumbers import *
+from .boundbutton import *
+
+__title__ = "Pkinter"
+__author__ = "DeflatedPickle"
+__version__ = "1.12.2"
