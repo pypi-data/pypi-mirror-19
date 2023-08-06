87dfb8a6ad8edba675702f4455c1e4cae192ac04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:55 +0000

    Add package PyConfigure/0.5.4/PyConfigure-0.5.4.tar.gz

---
+++ packages/PyConfigure/PyConfigure-0.5.4.tar.gz/PyConfigure-0.5.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+version = "0.5.4"
+
+setup(
+    name="PyConfigure",
+    version=version,
+    description="configuration toolkit based on YAML",
+    author="Andrey Popp",
+    author_email="8mayday@gmail.com",
+    url='https://github.com/alfred82santa/configure',
+    py_modules=["configure"],
+    test_suite="tests",
+    install_requires=["pyyaml"],
+    zip_safe=False)
