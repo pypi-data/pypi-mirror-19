38e1fafb030b5d0a792c241a38f4fc1475d8be3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:26 +0000

    Add package mastercard-match/1.0.4/mastercard_match-1.0.4-py2-none-any.whl

---
+++ packages/mastercard-match/mastercard_match-1.0.4-py2-none-any.whl/mastercardmatch/__init__.py
@@ -0,0 +1,13 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardmatch.resourceconfig import *
+
+
+# import apis into api package
+from mastercardmatch.acquirercontactrequest import *
+from mastercardmatch.addterminatedmerchant import *
+from mastercardmatch.retroactiveinquirydetailsrequest import *
+from mastercardmatch.retroactiveinquiryrequest import *
+from mastercardmatch.terminationinquiryhistoryrequest import *
+from mastercardmatch.terminationinquiryrequest import *
