3a977416f38fa3373fe0e5d488d88a87f769d930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:04 +0000

    Add package flametree/0.1.4/flametree-0.1.4.tar.gz

---
+++ packages/flametree/flametree-0.1.4.tar.gz/flametree-0.1.4/flametree/DiskFileManager.py
@@ -0,0 +1,64 @@
+import os
+import shutil
+
+
+class DiskFileManager:
+
+    @staticmethod
+    def list_files(directory):
+        path = directory._path
+        return [] if not os.path.exists(path) else [
+            name for name in os.listdir(path)
+            if os.path.isfile(os.path.join(path, name))
+        ]
+    @staticmethod
+    def list_dirs(directory):
+        path = directory._path
+        return [] if not os.path.exists(path) else [
+            name for name in os.listdir(path)
+            if os.path.isdir(os.path.join(path, name))
+        ]
+
+    @staticmethod
+    def read(fileobject, mode="r"):
+        with open(fileobject._path, mode=mode) as f:
+            result = f.read()
+        return result
+
+    @staticmethod
+    def write(fileobject, content, mode="a"):
+        with open(fileobject._path, mode=mode) as f:
+            f.write(content)
+
+    @staticmethod
+    def delete(target):
+        if target._is_dir:
+            shutil.rmtree(target._path)
+        else:
+            os.remove(target._path)
+
+    def create(self, target, replace=True):
+        path = target._path
+        if replace and os.path.exists(path):
+            self.delete(target)
+        if replace or (not os.path.exists(path)):
+            if target._is_dir:
+                os.mkdir(path)
+            else:
+                with open(path, "w") as f:
+                    pass
+
+    @staticmethod
+    def join_paths(*paths):
+        return os.path.join(*paths)
+
+    @staticmethod
+    def close():
+        pass
+
+    def tell(self, fileobject):
+        with open(fileobject._path, "a") as f:
+             return f.tell()
+
+    def flush(self, fileobject):
+        pass
