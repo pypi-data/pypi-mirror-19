b748a8e7a14572e35754009ad5f67e06624ca213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:25 +0000

    Add package mastercard-mcaid-analytics/1.0.1/mastercard_mcaid_analytics-1.0.1.tar.gz

---
+++ packages/mastercard-mcaid-analytics/mastercard_mcaid_analytics-1.0.1.tar.gz/mastercard_mcaid_analytics-1.0.1/mastercardmcaidanalytics/__init__.py
@@ -0,0 +1,13 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardmcaidanalytics.resourceconfig import *
+
+
+# import apis into api package
+from mastercardmcaidanalytics.cards import *
+from mastercardmcaidanalytics.devices import *
+from mastercardmcaidanalytics.merchants import *
+from mastercardmcaidanalytics.products import *
+from mastercardmcaidanalytics.programs import *
+from mastercardmcaidanalytics.transactions import *
