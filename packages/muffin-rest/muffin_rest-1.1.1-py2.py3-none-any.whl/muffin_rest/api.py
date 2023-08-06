63e3d8f2ed676ff86ab5096364cbeec0004e1bd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:03 +0000

    Add package muffin-rest/1.1.1/muffin_rest-1.1.1-py2.py3-none-any.whl

---
+++ packages/muffin-rest/muffin_rest-1.1.1-py2.py3-none-any.whl/muffin_rest/api.py
@@ -0,0 +1,104 @@
+"""!!! STILL NOT IMPLEMENTED. WORK IN PROGRESS !!!."""
+import asyncio
+import re
+from types import FunctionType
+
+import muffin
+from aiohttp import web
+from muffin.urls import ParentResource
+
+
+PREFIX_RE = re.compile('(/|\s)')
+
+
+class ApiRoute(web.Route):
+
+    """Support multiple routers."""
+
+    def __init__(self, api):
+        """Initialize the route."""
+        self.api = api
+        self.router = web.UrlDispatcher()
+        super(ApiRoute, self).__init__('*', None, api.prefix_name)
+
+    def match(self, path):
+        """Check path for API."""
+        if path.startswith(self.api.prefix):
+            path = path[len(self.api.prefix):]
+            for name, route in self.router.items():
+                match_info = route.match(path)
+                if match_info is not None:
+                    match_info['api-name'] = name
+                    return match_info
+        return None
+
+    @asyncio.coroutine
+    def handler(self, request):
+        """Handle request."""
+        route = self.router[request.match_info['api-name']]
+        response = yield from route._handler(request)
+        return response
+
+    def url(self, **kwargs):
+        """Do nothing for now."""
+        return self.api.prefix
+
+
+class Api():
+
+    """Bind group of resources together."""
+
+    def __init__(self, app, prefix='/api', scheme=False):
+        """Initialize the API."""
+        self.app = app
+        self.prefix = prefix.rstrip('/')
+        self.prefix_name = PREFIX_RE.sub('.', prefix.strip('/'))
+        self.handlers = {}
+        self.resource = ParentResource(self.prefix, name=self.prefix_name)
+        self.app.router._reg_resource(self.resource)
+        if scheme:
+            path = '/'.join((self.prefix, scheme.strip('/')))
+            handler = muffin.Handler.from_view(self.render_scheme, 'GET')
+            app.register(path)(handler)
+
+    def register(self, *paths, methods=None, name=None):
+        """Register handler to the API."""
+        if isinstance(methods, str):
+            methods = [methods]
+
+        def wrapper(handler):
+
+            if isinstance(handler, FunctionType):
+                handler = muffin.Handler.from_view(handler, *(methods or ['GET']))
+
+            if handler.name in self.handlers:
+                raise muffin.MuffinException('Handler is already registered: %s' % handler.name)
+
+            self.handlers[handler.name] = handler
+
+            handler.connect(
+                self.app, *paths, methods=methods, name=name or handler.name,
+                router=self.resource.router)
+
+            return handler
+
+        # Support for @app.register(func)
+        if len(paths) == 1 and callable(paths[0]):
+            view = paths[0]
+            paths = []
+            return wrapper(view)
+
+        return wrapper
+
+    def render_scheme(self, request):
+        """Render API Scheme."""
+        response = {}
+
+        for name, handler in self.handlers.items():
+            scheme = {'methods': handler.methods, 'desc': handler.__doc__}
+            scheme.update(getattr(handler, 'scheme', lambda: {})())
+            response[name] = scheme
+
+        return response
+
+#  pylama:ignore=W1401,W0212
---
+++ packages/muffin-rest/muffin_rest-1.1.1-py2.py3-none-any.whl/muffin_rest/filters.py
@@ -0,0 +1,107 @@
+"""Support API filters."""
+import ujson
+import operator
+
+from cached_property import cached_property
+from marshmallow import fields, missing
+
+
+class Filter:
+
+    """Base filter class."""
+
+    operators = {
+        '$lt': operator.lt,
+        '$le': operator.le,
+        '$gt': operator.gt,
+        '$ge': operator.ge,
+        '$eq': operator.eq,
+        '$ne': operator.ne,
+        '$in': lambda v, c: v in c,
+    }
+
+    list_ops = '$in',
+
+    field_cls = fields.Raw
+
+    def __init__(self, name, fname=None, field=None):
+        """Initialize filter.
+
+        :param name: Column/Property name.
+        :param fname: Filter name
+        :param fields: Marshmallow Field instance
+
+        """
+        self.field = field or self.field_cls()
+        self.fname = fname or name
+        self.name = name
+
+    def __repr__(self):
+        """String representation."""
+        return '<Filter %s>' % self.name
+
+    def parse(self, data):
+        """Parse operator and value from filter's data."""
+        val = data.get(self.fname, missing)
+        if not isinstance(val, dict):
+            return (self.operators['$eq'], self.field.deserialize(val)),
+
+        return tuple(
+            (
+                self.operators[op],
+                (self.field.deserialize(val)) if op not in self.list_ops else [
+                    self.field.deserialize(v) for v in val])
+            for (op, val) in val.items() if op in self.operators
+        )
+
+    def filter(self, collection, data, resource=None, **kwargs):
+        """Filter given collection."""
+        ops = self.parse(data)
+        validator = lambda obj: all(op(obj, val) for (op, val) in ops)  # noqa
+        return [o for o in collection if validator(o)]
+
+
+class Filters:
+
+    """Build filters for given handler."""
+
+    FILTER_CLASS = Filter
+
+    def __init__(self, filters, Handler):
+        """Initialize object."""
+        self._filters = filters
+        self.Handler = Handler
+
+    @cached_property
+    def filters(self):
+        """Build filters."""
+        if not self._filters:
+            return None
+        return list(f if isinstance(f, Filter) else self.convert(f) for f in self._filters)
+
+    def convert(self, args):
+        """Prepare filters."""
+        name = args
+        field = fname = None
+        if isinstance(args, (list, tuple)):
+            name, params = args
+            field = params.get('field')
+            fname = params.get('fname')
+
+        if not self.Handler or  not self.Handler.Schema or \
+                name not in self.Handler.Schema._declared_fields:
+            return self.FILTER_CLASS(name, fname=fname, field=field)
+
+        field = field or self.Handler.Schema._declared_fields[name]
+        return self.FILTER_CLASS(name, fname=fname, field=field)
+
+    def filter(self, data, collection, **kwargs):
+        """Filter given collection."""
+        if not data or self.filters is None:
+            return collection
+
+        for f in self.filters:
+            if f.fname not in data:
+                continue
+            collection = f.filter(collection, data, resource=self.Handler, **kwargs)
+        return collection
