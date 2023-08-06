0ec8ba1dce71e3fc28a7133be5fe15e188f7a751
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:22 +0000

    Add package chiwassu/0.0.1/chiwassu-0.0.1.tar.gz

---
+++ packages/chiwassu/chiwassu-0.0.1.tar.gz/chiwassu-0.0.1/chiwassu/say.py
@@ -0,0 +1,5 @@
+def say_chiwassu():
+    print('chiwassu')
+
+def say_oissu():
+    print('oissu')
---
+++ packages/chiwassu/chiwassu-0.0.1.tar.gz/chiwassu-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = "chiwassu",
+    version = "0.0.1",
+    author = "sotaro takeshtia",
+    author_email = "oh.sore.sore.soutarou@gmail.com",
+    description = ("for pypi practice"),
+    license = "MIT",
+    keywords = "japanese",
+    url = "",
+    packages=['chiwassu'],
+)
