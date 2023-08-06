cb8542507b52d400c373cb53a583b9fce9865be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:41 +0000

    Add package pysimplemodel/0.0.4/pysimplemodel-0.0.4.tar.gz

---
+++ packages/pysimplemodel/pysimplemodel-0.0.4.tar.gz/pysimplemodel-0.0.4/simple_model/model.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2017 Luiz Menezes
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+from .exceptions import EmptyField, ValidationError
+
+
+class Model:
+    fields = ()
+    allow_empty = ()
+
+    def __init__(self, **kwargs):
+        for field_name in self.fields:
+            field_value = kwargs.get(field_name, None)
+            setattr(self, field_name, field_value)
+
+    def is_empty(self, value):
+        return bool(value)
+
+    def validate(self, raise_exception=True):
+        for field_name in self.fields:
+            value = getattr(self, field_name)
+
+            allow_empty = field_name in self.allow_empty
+            if not allow_empty and not self.is_empty(value):
+                if raise_exception:
+                    raise EmptyField(field_name)
+                return False
+
+            try:
+                validate_field = getattr(self, 'validate_{}'.format(field_name))
+            except AttributeError:
+                continue
+
+            try:
+                validate_field(value)
+            except ValidationError:
+                if raise_exception:
+                    raise
+                return False
+
+        return True
+
+    def serialize(self):
+        self.validate()
+        data = {}
+        for field_name in self.fields:
+            field_value = getattr(self, field_name)
+            if isinstance(field_value, Model):
+                field_value = field_value.serialize()
+
+            data[field_name] = field_value
+
+        return data
