3d366d6ee134288390ba126db47fbfb3895fbf62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:05 +0000

    Add package findip/1.2/findip-1.2.zip

---
+++ packages/findip/findip-1.2.zip/findip-1.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'findip',
+  packages = ['findip'], # this must be the same as the name above
+  version = '1.2',
+  description = 'Finding Available IP Addresses Script',
+  long_description='',
+  author = 'Sepand Haghighi & Mohammadreza Rezaie',
+  author_email = 'sepand.haghighi@yahoo.com',
+  url = 'http://www.shaghighi.ir/pyIP', # use the URL to the github repo
+  download_url = 'https://github.com/sepandhaghighi/pyIP/archive/v1.2.zip', # I'll explain this in a second
+  keywords = ['free', 'ipfinder', 'ip','python','script','python3','pyIP','findi'], # arbitrary keywords
+  classifiers = [],
+  license='MIT',
+)
