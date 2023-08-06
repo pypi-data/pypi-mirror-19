fe3449734db084a5b3b3f679b00a313dc3243424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:00 +0000

    Add package jsTUX/0.1/jsTUX-0.1.tar.gz

---
+++ packages/jsTUX/jsTUX-0.1.tar.gz/jsTUX-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+     name='jsTUX',           # This is the name of our PyPI-package.
+     version='0.1',           # Update the version number for new releases
+     scripts=['jsTUX']   # The name of our scipt, and also the command we'll be using for calling it
+)
