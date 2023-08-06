6f44e6935ebc259257cc5750bf02d0f00290c6ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:23 +0000

    Add package python-html-assert/0.1.3/python-html-assert-0.1.3.tar.gz

---
+++ packages/python-html-assert/python-html-assert-0.1.3.tar.gz/python-html-assert-0.1.3/pha/__init__.py
@@ -0,0 +1,26 @@
+from matchers import (
+    linear_match as html_match,
+    prune_unmatched_elements
+)
+
+from spec import (
+    a,
+    accordion,
+    acc_body,
+    acc_group,
+    acc_heading,
+    div,
+    elem,
+    heading,
+    html,
+    img,
+    input,
+    option,
+    select,
+    text,
+)
+
+from formatters import (
+    pretty_html,
+    pretty_spec
+)
