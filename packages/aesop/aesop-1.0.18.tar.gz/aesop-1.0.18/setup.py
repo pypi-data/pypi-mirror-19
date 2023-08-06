0ce59745add0df66ba562a45ccad11b50d7a58b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:56 +0000

    Add package aesop/1.0.18/aesop-1.0.18.tar.gz

---
+++ packages/aesop/aesop-1.0.18.tar.gz/aesop-1.0.18/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='aesop',
+    version='1.0.18',
+    description='Module for analyzing electrostatics with protein structures',
+    long_description=readme,
+    author='Reed Harrison, Rohith Mohan',
+    author_email='reed.harrison@email.ucr.edu, rohith.mohan@email.ucr.edu',
+    url='https://github.com/rohithmohan/aesop-python',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs', 'data')),
+    install_requires=['prody', 'scipy', 'numpy', 'python-dateutil'], # Commented out for ReadTheDocs
+    include_package_data=True,
+#    package_data={
+#        'aesop-python':
+#            ['README.rst',
+#            'LICENSE',
+#            ],
+#    },
+    zip_safe=False	
+)
+
