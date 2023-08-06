8df44f10bce747cd7d6f3b897a757d2ae210dd51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:15 +0000

    Add package mario_nester/1.3.0/mario_nester-1.3.0.tar.gz

---
+++ packages/mario_nester/mario_nester-1.3.0.tar.gz/mario_nester-1.3.0/mario_nester.py
@@ -0,0 +1,14 @@
+"""This is the "mario_nester.py" module"""
+def print_lol(theList,level=0,indent=False):
+        """This function takes one positional argument called "theList",which
+            is any Python list.Each data item in the provided list is recursively
+            printed to the screen on it's own line."""
+        for item in theList:
+                if isinstance(item,list):
+                    print_lol(item,level+1,indent)
+                else:
+                    if indent:
+                        for tab in range(level):
+                                print("\t",end='')
+                    print(item)
+                    
