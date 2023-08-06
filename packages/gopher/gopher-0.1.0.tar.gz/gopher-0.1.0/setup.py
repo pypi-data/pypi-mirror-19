6e9fc3bf546c173f3db28aacc31b17b2448d3c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:03 +0000

    Add package gopher/0.1.0/gopher-0.1.0.tar.gz

---
+++ packages/gopher/gopher-0.1.0.tar.gz/gopher-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from setuptools import find_packages
+
+import gopher
+
+
+setup(
+    name=gopher.__name__,
+    packages=find_packages(exclude=['tests*', 'example']),
+    version=gopher.__version__,
+    author=gopher.__author__,
+    author_email=gopher.__email__,
+    description="Download randomized gopher image (png) from gopherize,me",
+    url=gopher.__url__,
+    license=gopher.__license__,
+    scripts=['gopher.py'],
+    install_requires=[
+        'click==6.7',
+        'requests==2.13.0'
+    ],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Topic :: Multimedia :: Graphics",
+        "Topic :: Utilities",
+        "Environment :: Console"
+    ]
+)
