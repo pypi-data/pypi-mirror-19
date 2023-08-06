e7a5d1943e3ce73233e720a750db71fe9eb662ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:18 +0000

    Add package logging-mv-integrations/0.1.0/logging-mv-integrations-0.1.0.zip

---
+++ packages/logging-mv-integrations/logging-mv-integrations-0.1.0.zip/logging-mv-integrations-0.1.0/logging_mv_integrations/support/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#  @copyright 2016 TUNE, Inc. (http://www.tune.com)
+#  @namespace logging_mv_integrations
+
+from .singleton import (Singleton)
+from .utils import (safe_str, safe_int, safe_float)
