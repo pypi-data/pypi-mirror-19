6487cd3113bb2ecdc425af3f1d6315fc3712d290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:53 +0000

    Add package tinamit/1.2.2/tinamit-1.2.2.tar.gz

---
+++ packages/tinamit/tinamit-1.2.2.tar.gz/tinamit-1.2.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+with open('tinamit/versión.txt') as archivo_versión:
+    versión = archivo_versión.read().strip()
+
+
+setup(
+    name='tinamit',
+    version=versión,
+    packages=find_packages(),
+    url='https://github.com/julienmalard/Tinamit',
+    license='GNU GPL 3',
+    author='Julien Jean Malard',
+    author_email='julien.malard@mail.mcgill.ca',
+    description='Conexión de modelos socioeconómicos (dinámicas de los sistemas) con modelos biofísicos.',
+    requires=['numpy', 'matplotlib', 'scipy'],
+    package_data={
+        # Incluir estos documentos de los paquetes:
+        '': ['*.txt', '*.vpm', '*.json', '*.png', '*.jpg', '*.gif'],
+    },
+)
