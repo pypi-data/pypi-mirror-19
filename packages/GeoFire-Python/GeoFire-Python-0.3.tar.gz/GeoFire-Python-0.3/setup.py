4bef63050bd5d72c38aa8530db01ff5df0c4d336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:02 +0000

    Add package GeoFire-Python/0.3/GeoFire-Python-0.3.tar.gz

---
+++ packages/GeoFire-Python/GeoFire-Python-0.3.tar.gz/GeoFire-Python-0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'GeoFire-Python',
+  packages = ['GeoFire'],
+  version = '0.3',
+  description = 'A Firebase geo-query library for Python',
+  author = 'Guanjiu Zhang',
+  author_email = 'ininex@umich.edu',
+  url = 'https://github.com/ininex/geofire-python',
+  download_url = 'https://github.com/ininex/geofire-python/tarball/0.3',
+  keywords = ['firebase', 'geohash', 'geofire'],
+  classifiers = [],
+)
