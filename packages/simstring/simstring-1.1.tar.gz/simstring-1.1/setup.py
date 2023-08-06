606961c1008ae4e86ffe2de3a540732b26e928aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:18 +0000

    Add package simstring/1.1/simstring-1.1.tar.gz

---
+++ packages/simstring/simstring-1.1.tar.gz/simstring-1.1/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""
+setup.py file for SWIG example
+"""
+
+import sys
+import os.path
+
+def get_rootdir():
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), '../..'))
+def get_includedir():
+    return '.'
+
+def get_swigdir():
+    return os.path.join(get_rootdir(), 'swig')
+
+#import os; os.environ['CC'] = 'g++'; os.environ['CXX'] = 'g++';
+#os.environ['CPP'] = 'g++'; os.environ['LDSHARED'] = 'g++'
+
+from distutils.core import setup, Extension
+
+if sys.platform.startswith("darwin"):
+    libs = ['-liconv']
+else:
+    # need iconv too but without proper -L adding -liconv here won't always work
+    libs = []
+
+simstring_module = Extension(
+    '_simstring',
+    sources = [
+        'export.cpp',
+        'export_wrap.cpp',
+        ],
+    include_dirs=[get_includedir(),],
+    extra_link_args=libs,
+    language='c++',
+    )
+
+setup(
+    name = 'simstring',
+    version = '1.1',
+    author = 'Naoaki Okazaki',
+    maintainer = 'Dillon Hicks',
+    maintainer_email = 'dillon@dillonhicks.io',
+    description = """SimString Python module""",
+    ext_modules = [simstring_module],
+    py_modules = ["simstring"],
+    )
