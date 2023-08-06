81ae906323ed449029b06f3efb86569be04ac462
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:39 +0000

    Add package web3/3.5.3/web3-3.5.3.tar.gz

---
+++ packages/web3/web3-3.5.3.tar.gz/web3-3.5.3/web3/utils/compat/__init__.py
@@ -0,0 +1,58 @@
+import sys
+import os
+
+
+if sys.version_info.major == 2:
+    from .compat_py2 import (
+        urlparse,
+        urlunparse,
+        Generator,
+    )
+else:
+    from .compat_py3 import (  # noqa: #401
+        urlparse,
+        urlunparse,
+        Generator,
+    )
+
+
+def get_threading_backend():
+    if 'WEB3_THREADING_BACKEND' in os.environ:
+        return os.environ['WEB3_THREADING_BACKEND']
+    elif 'THREADING_BACKEND' in os.environ:
+        return os.environ['THREADING_BACKEND']
+    else:
+        return 'stdlib'
+
+
+THREADING_BACKEND = get_threading_backend()
+
+
+if THREADING_BACKEND == 'stdlib':
+    from .compat_stdlib import (
+        Timeout,
+        sleep,
+        socket,
+        threading,
+        make_server,
+        GreenletThread,
+        spawn,
+        subprocess,
+    )
+    from .compat_requests import (
+        make_post_request,
+    )
+elif THREADING_BACKEND == 'gevent':
+    from .compat_gevent import (  # noqa: F401
+        Timeout,
+        sleep,
+        socket,
+        threading,
+        make_server,
+        GreenletThread,
+        spawn,
+        subprocess,
+        make_post_request,
+    )
+else:
+    raise ValueError("Unsupported threading backend.  Must be one of 'gevent' or 'stdlib'")
