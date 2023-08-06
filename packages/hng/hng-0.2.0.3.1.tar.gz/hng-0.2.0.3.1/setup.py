15a088199f4b563252280508eb19d91c898cf890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:15 +0000

    Add package hng/0.2.0.3.1/hng-0.2.0.3.1.tar.gz

---
+++ packages/hng/hng-0.2.0.3.1.tar.gz/hng-0.2.0.3.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+def readme():
+  with open("README.rst") as f:
+    return f.read()
+setup(name='hng',
+      version='0.2.0.3.1',
+      description='Python package for open-source healthcare machine learning @hngml',
+      long_description = readme(),
+      classifiers=['Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      url='https://bitbucket.org/HNGML/hng',
+      author='hngml',
+      author_email='nilesh.chaudhari@healthnextgen.com',
+      license='MIT',
+      packages=['hng'],
+      install_requires=[
+          'markdown',
+      ],
+      scripts=['bin/hng'],#write a separate script file for a command line call
+      #entry_points = {'console_scripts':['test-me-again=test_package.terminal_functions:main'],},
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      include_package_data=True,
+      zip_safe=False)
