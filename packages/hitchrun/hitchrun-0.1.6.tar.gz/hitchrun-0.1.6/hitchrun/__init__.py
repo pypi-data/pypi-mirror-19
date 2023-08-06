c33be8429bc33b9153ba3b723fcb781b4ba1bb34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:47 +0000

    Add package hitchrun/0.1.6/hitchrun-0.1.6.tar.gz

---
+++ packages/hitchrun/hitchrun-0.1.6.tar.gz/hitchrun-0.1.6/hitchrun/__init__.py
@@ -0,0 +1,20 @@
+from hitchrun import commandline
+from path import Path
+from hitchrun.maintenance import hitch_maintenance
+import os
+import sys
+
+
+cwd = Path(os.getcwd())
+
+
+def this_dir(filename):
+    """
+    Use like so:
+
+      keypath = this_dir(__file__)
+    """
+    return Path(filename).abspath().dirname()
+
+
+genpath = Path(sys.executable).parent.parent.abspath()
