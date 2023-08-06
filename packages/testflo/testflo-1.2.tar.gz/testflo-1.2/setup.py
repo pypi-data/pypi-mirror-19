61f205a85f6ab51ddf1baabbe5f2de66bf7aa77a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:36 +0000

    Add package testflo/1.2/testflo-1.2.tar.gz

---
+++ packages/testflo/testflo-1.2.tar.gz/testflo-1.2/setup.py
@@ -0,0 +1,16 @@
+
+from distutils.core import setup
+
+setup(name='testflo',
+      version='1.2',
+      description="A simple flow based testing framework",
+      license='Apache 2.0',
+      install_requires=[
+        'six',
+      ],
+      packages=['testflo'],
+      entry_points="""
+          [console_scripts]
+          testflo=testflo.main:main
+      """
+      )
