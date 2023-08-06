10c4a1004ce3f272bbfc3596ce6bda2840d25d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:57 +0000

    Add package py-environ/0.2.2/py-environ-0.2.2.tar.gz

---
+++ packages/py-environ/py-environ-0.2.2.tar.gz/py-environ-0.2.2/py_environ.py
@@ -0,0 +1,40 @@
+import logging
+import os
+
+from six.moves.configparser import SafeConfigParser
+
+log = logging.getLogger(__name__)
+
+
+TRUE_EQUIVALENT_STRINGS = ["1", "yes", "true", "on"]
+FALSE_EQUIVALENT_STRINGS = ["0", "no", "false", "off"]
+
+def to_environ_key(k):
+    return k.upper() \
+            .replace('-', '_') \
+            .replace('.', '_DOT_') \
+            .replace(' ', '_') \
+            .replace(':', '_') \
+
+
+class EnvironmentConfigWrapper(SafeConfigParser, object):
+    def has_option(self, section, option):
+        env_prefix = to_environ_key(section)
+        env_option = to_environ_key(option)
+        env_name = "%s_%s" % (env_prefix, env_option)
+
+        if env_name in os.environ:
+            return True
+        else:
+            return super(EnvironmentConfigWrapper, self).has_option(section, option)
+
+    def get(self, section, option, **kwargs):
+        env_prefix = to_environ_key(section)
+        env_option = to_environ_key(option)
+        env_name = "%s_%s" % (env_prefix, env_option)
+
+        if env_name in os.environ:
+            return os.environ[env_name]
+        else:
+            return super(EnvironmentConfigWrapper, self).get(section, option, **kwargs)
+        
