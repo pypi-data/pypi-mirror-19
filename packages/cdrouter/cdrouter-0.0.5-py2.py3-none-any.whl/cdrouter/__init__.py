b3dd96de9e941ff67852ee5853a3e6a8e54f901d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:11 +0000

    Add package cdrouter/0.0.5/cdrouter-0.0.5-py2.py3-none-any.whl

---
+++ packages/cdrouter/cdrouter-0.0.5-py2.py3-none-any.whl/cdrouter/__init__.py
@@ -0,0 +1,28 @@
+#
+# Copyright (c) 2017 by QA Cafe.
+# All Rights Reserved.
+#
+
+"""Python client for the CDRouter Web API."""
+
+__version__ = '0.0.5'
+
+from .cdrouter import Service
+
+from .configs import ConfigsService
+from .devices import DevicesService
+from .attachments import AttachmentsService
+from .jobs import JobsService
+from .packages import PackagesService
+from .results import ResultsService
+from .testresults import TestResultsService
+from .annotations import AnnotationsService
+from .captures import CapturesService
+from .highlights import HighlightsService
+from .imports import ImportsService
+from .exports import ExportsService
+from .history import HistoryService
+from .system import SystemService
+from .tags import TagsService
+from .testsuites import TestsuitesService
+from .users import UsersService
