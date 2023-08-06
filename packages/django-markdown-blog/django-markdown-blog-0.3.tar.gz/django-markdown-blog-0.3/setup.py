e7ef3c1348fcf6d0ec578802df40439a6728f7f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:23 +0000

    Add package django-markdown-blog/0.3/django-markdown-blog-0.3.tar.gz

---
+++ packages/django-markdown-blog/django-markdown-blog-0.3.tar.gz/django-markdown-blog-0.3/setup.py
@@ -0,0 +1,43 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-markdown-blog',
+    version='0.3',
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='A simple Django app to provide markdown blog functionality.',
+    long_description=README,
+    author='Kevin Mann',
+    author_email='kevinrmann@gmail.com',
+    install_requires=[
+        'Django>=1.9',
+        'django-markdown-app>=0.9.0',
+        'django-imagekit>=3.3',
+        'awesome-slugify>=1.6.5',
+        'django-autoslug>=1.9.3'
+    ],
+    classifiers=[ 
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',  # replace "X.Y" as appropriate
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
