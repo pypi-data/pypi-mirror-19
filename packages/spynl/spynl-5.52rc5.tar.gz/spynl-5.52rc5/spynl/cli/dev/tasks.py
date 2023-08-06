5637d40675b6223c1749a2c95dba0c4e01f3f318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:17 +0000

    Add package spynl/5.52rc5/spynl-5.52rc5.tar.gz

---
+++ packages/spynl/spynl-5.52rc5.tar.gz/spynl-5.52rc5/spynl/cli/dev/tasks.py
@@ -0,0 +1,184 @@
+import os
+from invoke import task
+from invoke.exceptions import Exit
+import pip
+
+from spynl.main.version import __version__ as spynl_version
+from spynl.main.pkg_utils import  get_config_package, get_dev_config 
+from spynl.cli.utils import resolve_repo_names, repo, chdir
+
+
+repos_help = "Affected repos, e.g. spynl,spynl.auth. "\
+             "Defaults to all installed."
+
+
+@task(help={'repos': repos_help,
+            'developing': 'If True (default), python setup.py develop is '
+                          'used, otherwise install',
+            'revision': 'Which revision to update to (default: do not update)',
+            'fallbackrevision': 'Use this revision if the repository does not '
+                                ' have the target revision.',
+            'install-deps': 'If True (default), pre-install dependencies '
+                            '(eg. via apt-get).',
+            'src_path': 'Desired installation path. Defaults to '
+                        '$VIRTUAL_ENV/src directory.'})
+def install(ctx, repos='_all', developing=True, revision=None,
+            fallbackrevision=None, install_deps=True, src_path=None):
+    '''Get code and install repos in the current virtual environment.'''
+    ctx.run('pip install --upgrade setuptools')  # make sure we have the latest
+    src_path = src_path or os.environ['VIRTUAL_ENV'] + '/src'
+    config_package = get_config_package()
+    if config_package is None:
+        raise Exit("No config package with ini files found.")
+    config = get_dev_config() 
+    if fallbackrevision is None:
+        fallbackrevision = 'master' 
+        if config.get('spynl.ops.scm_type', '') == 'hg':
+            fallbackrevision = 'default' 
+    if not os.path.exists(src_path):
+        os.mkdir(src_path)
+    for repo_name in resolve_repo_names(repos, complain_not_installed=False):
+        repo_path = src_path + '/' + repo_name
+        if not os.path.exists(repo_path):
+            clone_url = 'ssh://%s@%s/%s' % (config.get('spynl.ops.scm_type'),
+                                            config.get('spynl.ops.scm_team'),
+                                            repo_name)
+            with chdir(src_path):
+                print("[spynl dev.install] CLONING from %s" % clone_url)
+                ctx.run('hg clone %s' % clone_url)
+        else:
+            print("[spynl dev.install] UPDATING {}".format(repo_name))
+            with chdir(repo_path):
+                ctx.run('hg pull --update')
+        with chdir(repo_path):
+            if revision is not None:
+                print('[spynl dev.install] Updating to revision %s ...' % revision)
+                if not ctx.run('hg update {}'.format(revision), warn=True):
+                    print('[spynl dev.install] Updating to fallback revision %s ...'
+                          % fallbackrevision)
+                    ctx.run('hg update {}'.format(fallbackrevision))
+            if os.path.isfile('./setup.sh'):
+                pre_cmd = './setup.sh --pre-install '\
+                          '--virtualenv=%s' % os.environ['VIRTUAL_ENV']
+                if install_deps:
+                    pre_cmd += ' --install-dependencies'
+                ctx.run(pre_cmd)
+            if developing:
+                print("[spynl dev.install] DEVELOPING {}".format(repo_name))
+                ctx.run('python setup.py develop')
+            else:
+                print("[spynl dev.install] INSTALLING {}".format(repo_name))
+                ctx.run('python setup.py install')
+            translations(ctx, repos=repo_name, action='compile')
+            if os.path.isfile('./setup.sh'):
+                ctx.run('./setup.sh --post-install')
+
+
+@task
+def serve(ctx):
+    '''Run local dev server'''
+    config_package = get_config_package()
+    if config_package is None:
+        raise Exit("No spynl-plugin found with .ini files. Exiting ...")
+    with repo('spynl'):
+        ctx.run('pserve %s/development.ini --reload'
+                % config_package.location, pty=True)
+
+
+@task(aliases=('tests',),
+      help={'repos': repos_help,
+            'called-standalone': 'If False, this task is a pre-task and the '
+                                 'user gets to cancel the task flow when '
+                                 'tests fail.',
+            'reports': 'If True, Junit and Coverage reports will be made '
+                       '(requires a few extra packages, '
+                       'e.g. for pycoverage).'})
+def test(ctx, repos='_all', called_standalone=True, reports=False):
+    '''
+    Perfom tests in one or more spynl plugins.
+    '''
+    for repo_name in resolve_repo_names(repos):
+        print("[spynl dev.test] Testing repo: {}".format(repo_name))
+        with repo(repo_name):
+            if not reports:
+                result = ctx.run('py.test', warn=True)
+            else:
+                result = ctx.run('py.test --junit-xml=pytests.xml --cov %s '
+                                 ' --cov-report xml --cov-append' % repo_name)
+            if not result.ok and called_standalone is False\
+               and input("[spynl dev.test] Tests failed. Continue anyway? Y/n") not in ('y', 'Y'):
+                raise Exit("[spynl dev.test] Aborting at user request.")
+
+
+@task(help={'repos': repos_help,
+            'languages': 'An iterable of language codes. Defaults to ("nl",).',
+            'action': 'Either "compile" (compile selected languages) or '
+                      '"refresh" (extract messages from the source code '
+                      'and update the .po files for the selected languages). '
+                      'Defaults to compile.'})
+def translations(ctx, repos='_all', languages=('nl',), action='compile'):
+    '''
+    Ensure that translations files are up-to-date w.r.t. to the code. If action
+    is set to compile (default), this will compile the catalogs for all selected
+    languages. If action is set to refresh, this will extract messages from the
+    source code and update the .po files for the selected languages (will
+    initialize if necessary).
+    '''
+    if action == 'compile':
+        refresh = False
+    elif action == 'refresh':
+        refresh = True
+    else:
+        raise Exit("[spynl dev.translations] action should be 'compile' or "
+                   "'refresh'.")
+
+    for repo_name in resolve_repo_names(repos):
+        with repo(repo_name):
+            if repo_name == 'spynl':
+                repo_name = 'spynl.main'
+            print("[spynl dev.translations] Repo: %s ..." % repo_name)
+            repo_path = repo_name.replace('.', '/')
+            # make locale folder if it doesn't exist already:
+            if not os.path.exists('%s/locale' % repo_path):
+                os.mkdir('./%s/locale' % repo_path)
+            if refresh:
+                # Extract messages from source:
+                ctx.run('python setup.py extract_messages '
+                        '--output-file {rp}/locale/messages.pot --no-wrap '
+                        '--sort-by-file --input-dirs {rp} --project {project} '
+                        '--copyright-holder "Softwear BV" --version {v}'
+                        .format(rp=repo_path, project=repo_name,
+                                v=spynl_version))
+            for lang in languages:
+                path2po = '%s/locale/%s/LC_MESSAGES/%s.po' % (repo_path, lang,
+                                                              repo_name)
+                # update if refresh
+                if refresh:
+                    # init if needed:
+                    if not os.path.exists(path2po):
+                        print('[spynl dev.translations] File %s does not exist.'
+                              ' Initializing.' % path2po)
+                        ctx.run('python setup.py init_catalog -l {lang} '
+                                '-i {rp}/locale/messages.pot '
+                                '-d {rp}/locale -D {rn}'
+                                .format(rp=repo_path, rn=repo_name, lang=lang))
+                    # update if not init
+                    else:
+                        print('[spynl dev.translations] update the %s catalog'
+                              % lang)
+                        ctx.run('python setup.py update_catalog -N --no-wrap '
+                                '-l {lang} -i {rp}/locale/messages.pot '
+                                '-d {rp}/locale -D {rn}'
+                                .format(rp=repo_path, rn=repo_name, lang=lang))
+                # if not refresh, compile
+                elif os.path.exists(path2po):
+                    ctx.run('python setup.py compile_catalog --domain {rn} '
+                            '--directory {rp}/locale --domain {rn} '
+                            '--locale {lang}'
+                            .format(rp=repo_path, rn=repo_name, lang=lang))
+                else:
+                    print('[spynl dev.translations] File %s does not exist.'
+                          ' Update the repo first.' % path2po)
+
+                print("[spynl dev.translations] Done with language %s." % lang)
+                print("--------------------------------------------------")
---
+++ packages/spynl/spynl-5.52rc5.tar.gz/spynl-5.52rc5/spynl/main/docs/settings.py
@@ -0,0 +1,222 @@
+"""
+Set-up for ini documentation list of dicts
+
+In the ini_doc list, only use 'yes' or 'no' for the required field,
+as this is used in the code to check if all required settings are set.
+"""
+
+from spynl.main.exceptions import SpynlException
+
+
+ini_description = '''
+These are the settings that can be added to the ini file under the heading
+[app:main]. This table only lists settings specific to Spynl. Pyramid settings
+and Pyramid Mail configuration should also be put in [app:main], but are not
+listed here.  Spynl will not start if one of the required settings is not
+present in the ini file. <br>
+The pyramid asbool function returns the boolean value True if the case-lowered
+value of string input s is any of t, true, y, on, or 1, otherwise return the
+boolean value False. If s is the value None, return False. If s is already one
+of the boolean values True or False, return it. <br>
+N.B. It is not possible to add comments after the setting,
+comments should be on their own line.
+'''
+
+ini_doc = [{'name': 'spynl.date_systemtz',
+            'plugin': '',
+            'required': 'no',
+            'default': 'UTC',
+            'info': 'Timezone that is used if no time zone is explicitly '
+                    'given and also no time zone is found in or to be '
+                    'used from the user object.'},
+
+           {'name': 'spynl.date_format',
+            'plugin': '',
+            'required': 'no',
+            'default': '%Y-%m-%dT%H:%M:%S%z',
+            'info': 'Format for dates.'},
+
+           {'name': 'spynl.languages',
+            'plugin': '',
+            'required': 'no',
+            'default': 'en',
+            'info': 'A csv string describing the languages supported by the '
+                    'application. Spynl has no dialect support, so only '
+                    'two-letter language codes should be provided. '
+                    'The first language is considered to be the preferred '
+                    'language.'},
+
+           {'name': 'spynl.domain',
+            'plugin': '',
+            'required': 'no',
+            'default': 'localhost',
+            'info': 'Used for setting the cookie-domain, and for a default '
+                    'no-reply emailaddress.'},
+
+           {'name': 'spynl.languages',
+            'plugin': '',
+            'required': 'no',
+            'default': 'nl',
+            'info': 'Languages (e.g. de,nl) which Spynl should translate '
+                    'to (other than english). This affects how the task '
+                    '<spynl dev.translations> works. Use a comma-separated '
+                    ' list for more than one.'},
+ 
+           {'name': 'spynl.tld_origin_whitelist',
+            'plugin': '',
+            'required': 'no',
+            'default': '',
+            'info': 'Comma-separated whitelist for allowed origins. '
+                    'It is expected to hold only the top-level domains, '
+                    ' e.g. "google.com".'},
+
+           {'name': 'spynl.dev_origin_whitelist',
+            'plugin': '',
+            'required': 'no',
+            'default': '',
+            'info': 'Comma separated whitelist for allowed origins which is '
+                    'usable for a development context. It is expected to hold'
+                    ' either complete domains or mere protocols, e.g. '
+                    '"chrome-extension://"..'},
+
+           {'name': 'spynl.pretty',
+            'plugin': '',
+            'required': 'no',
+            'default': 'false',
+            'info': 'Pretty printing for development. Is read with Pyramid '
+                    'asbool function.'},
+
+           {'name': 'spynl.session',
+            'plugin': '',
+            'required': 'no',
+            'default': 'Pyramid',
+            'info': 'Sets the name of the session class to use. Spynl will '
+                    'look this class up under settings[<name>]. '
+                    'The class should conform to the interface '
+                    'pyramid.interfaces.ISession. The value "Pyramid" or '
+                    'omitting this setting will result in the use of the '
+                    'Session class from beaker.session'},
+ 
+           {'name': 'spynl.ops.scm_type',
+            'plugin': '',
+            'required': 'no',
+            'default': 'git',
+            'info': 'The source control management system used by your Spynl '
+                    'plugin repositories use. One of "git" or "hg", defaults '
+                    ' to "git".'},
+ 
+           {'name': 'spynl.ops.scm_team',
+            'plugin': '',
+            'required': 'no',
+            'default': '',
+            'info': 'The source control management provider+team where '
+                    'your Spynl plugin repositories can be found, e.g. '
+                    '"github.com/myteam" or "bitbucket.org/myteam".'},
+           
+           {'name': 'spynl.ops.environment',
+            'plugin': '',
+            'required': 'no',
+            'default': '',
+            'info': 'The environment Spynl is running in. This is quite '
+                    'useful for things like error log aggregation.'},
+
+           {'name': 'spynl.ops.function',
+            'plugin': '',
+            'required': 'no',
+            'default': '',
+            'info': 'The function this Spynl node is performing. This '
+                    'can be useful for error log aggregation if you '
+                    ' employ more than one Spynl applications.'},
+
+           {'name': 'spynl.ops.jenkins_url',
+            'plugin': '',
+            'required': 'no',
+            'default': '',
+            'info': 'The URL of your Jenkins server.'},
+
+           {'name': 'spynl.ops.dev_url',
+            'plugin': '',
+            'required': 'no',
+            'default': '',
+            'info': 'The URL where your development app can be '
+                    'accessed. Needed to run smoke tests after deploys.'},
+
+           {'name': 'spynl.ops.dev_domain',
+            'plugin': '',
+            'required': 'no',
+            'default': '',
+            'info': 'The top level domain on which your development app '
+                    'resides. This is used to get the cookie domain '
+                    'correct.'},
+
+           {'name': 'spynl.ops.ecr.dev_tasks',
+            'plugin': '',
+            'required': 'no',
+            'default': 'dev',
+            'info': 'The tasks supported in your development AWS ECR.'},
+
+           {'name': 'spynl.ops.ecr.dev_url',
+            'plugin': '',
+            'required': 'no',
+            'default': '',
+            'info': 'The URL of your AWS ECR used for development. Include '
+                    'AWS profile name like this: profile@ecr-url.'},
+
+           {'name': 'spynl.ops.ecr.prod_url',
+            'plugin': '',
+            'required': 'no',
+            'default': '',
+            'info': 'The URL of your AWS ECR used for production. Include '
+                    'AWS profile name like this: profile@ecr-url.'},
+
+           {'name': 'spynl.sentry.key',
+            'plugin': '',
+            'required': 'no',
+            'default': '',
+            'info': 'The key used to connect to Sentry. Leave empty if '
+                    'you are not using Sentry.'},
+
+           {'name': 'spynl.sentry.project',
+            'plugin': '',
+            'required': 'no',
+            'default': '',
+            'info': 'The project identifier Spynl reports to in Sentry.'},
+
+           {'name': 'spynl.newrelic.key',
+            'plugin': '',
+            'required': 'no',
+            'default': '',
+            'info': 'The key used to connect to NewRelic. Leave empty if '
+                    'you are not usig NewRelic.'}
+
+          ]
+def get_ini_doc_setting(name):
+    """
+    Returns the documentation entry of a specific setting or None
+
+    Because ini_doc is a list, this function is needed to get the documentation
+    of a specific setting.
+    """
+    for setting in ini_doc:
+        if setting['name'] == name:
+            return setting
+
+    return None
+
+
+def check_required_settings(config):
+    """check whether all required ini settings are set"""
+    for setting in ini_doc:
+        if (setting['required'] == 'yes' and
+                setting['name'] not in config.registry.settings):
+            msg = 'Please set {} in the Pyramid ini file'.format(
+                setting['name'])
+            raise SpynlException(msg)
+        # setting['required'] can only be yes or no (no accidental true's)
+        if setting['required'] not in ('no', 'yes'):
+            msg = ('The value for required in ini_doc can only be yes or no, '
+                   'please change this for setting: {}'
+                   .format(setting['name']))
+            raise SpynlException(msg)
+
+
