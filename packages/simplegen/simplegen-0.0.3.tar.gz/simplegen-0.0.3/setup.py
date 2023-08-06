04ac5e7906747c422c2308ac6a7d074d6e9babf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:29 +0000

    Add package simplegen/0.0.3/simplegen-0.0.3.tar.gz

---
+++ packages/simplegen/simplegen-0.0.3.tar.gz/simplegen-0.0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt', session='hey')
+reqs = [str(ir.req) for ir in install_reqs]
+
+
+setup(
+    name='simplegen',
+    version='0.0.3',
+    install_requires=reqs,
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    url='https://github.com/mohamed-aziz/simplegen',
+    license='GPLV3',
+    author='Mohamed Aziz Knani',
+    author_email='medazizknani@gmail.com',
+    description='Simple site generator for my site.',
+    packages=['simplegen'],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'makesite=simplegen.cli:makesite',
+            'initsite=simplegen.cli:initsite'
+        ]
+    },
+    package_date={
+        '': ['requirements.txt', 'readme.md', 'requirements_dev.txt']
+    },
+)
