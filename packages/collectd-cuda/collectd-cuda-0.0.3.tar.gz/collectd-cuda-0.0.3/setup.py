9374d9b376df8977e4c6c9452f39264dea3872aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:17 +0000

    Add package collectd-cuda/0.0.3/collectd-cuda-0.0.3.tar.gz

---
+++ packages/collectd-cuda/collectd-cuda-0.0.3.tar.gz/collectd-cuda-0.0.3/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+setup(name='collectd-cuda',
+      version='0.0.3',
+      description='CollectD plugin for Nvidia CUDA statistics',
+      author='Jon Skarpeteig',
+      author_email='jon.skarpeteig@gmail.com',
+      url='https://github.com/Yuav/collectd-cuda',
+      packages=[
+        'collectd_cuda',
+      ],
+      package_dir={'collectd_cuda': 'collectd_cuda'},
+      keywords='collectd-cuda',
+      include_package_data=True,
+      install_requires=[
+          'collectd'
+      ],
+      )
