c0696f1b6a50858b6e1d4eebec8b7f3c895e17af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:57 +0000

    Add package zz_nester/1.1.0/zz_nester-1.1.0.tar.gz

---
+++ packages/zz_nester/zz_nester-1.1.0.tar.gz/zz_nester-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup (
+    name = "zz_nester",
+    version = "1.1.0",
+    py_modules = ["zz_nester"],
+    author = "adai041",
+    author_url = "zhangzhiyong0819@126.com",
+    url = "",
+    description = "My first Python File",
+)
