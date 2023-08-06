06ad5f0591c83e0123ef2f1f4c5a4622729f7189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:47 +0000

    Add package permutation_test/0.17/permutation_test-0.17.tar.gz

---
+++ packages/permutation_test/permutation_test-0.17.tar.gz/permutation_test-0.17/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='permutation_test',
+      version='0.17',
+      description='Implementation of Fishers permutation test',
+      long_description=readme(),
+      url='https://github.com/cmohl2013/permutation_test',
+      author='Christoph Moehl',
+      author_email='christoph.moehl@dzne.de',
+      license='MIT',
+      packages=['permutation_test'],
+      install_requires=['numpy', 'pandas'],
+      entry_points = {
+        'console_scripts': [\
+              'permtest=permutation_test.permtest:main']
+        },
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
