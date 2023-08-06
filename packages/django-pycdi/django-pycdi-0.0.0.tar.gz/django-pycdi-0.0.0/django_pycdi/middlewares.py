b49d5d65f7157391586f76bfd50a32ac195552c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:11 +0000

    Add package django-pycdi/0.0.0/django-pycdi-0.0.0.tar.gz

---
+++ packages/django-pycdi/django-pycdi-0.0.0.tar.gz/django-pycdi-0.0.0/django_pycdi/middlewares.py
@@ -0,0 +1,21 @@
+from pycdi.core import DEFAULT_CONTAINER
+
+
+class CDIMiddleware(object):
+    def __init__(self, get_response=None):
+        self.get_response = get_response
+        self.container = DEFAULT_CONTAINER
+
+    def __call__(self, request):
+        return self.get_response(request)
+
+    def process_view(self, request, view_func, view_args, view_kwargs):
+        inject_kwargs = getattr(view_func, '_inject_kwargs', False)
+        if not inject_kwargs:
+            return None
+        inject_args = getattr(view_func, '_inject_args', False)
+        sub_container = self.container.sub_container(request, **view_kwargs)
+        if len(inject_args) > 0 or 'request' in inject_kwargs:
+            return sub_container.call(view_func, *view_args, **view_kwargs)
+        else:
+            return sub_container.call(view_func, request, *view_args, **view_kwargs)
