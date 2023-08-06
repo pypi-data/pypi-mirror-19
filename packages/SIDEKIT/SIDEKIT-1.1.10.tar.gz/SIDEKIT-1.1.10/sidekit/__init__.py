a61e92448eb403b07d71fb372eb7ff8b3794ed48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:59 +0000

    Add package SIDEKIT/1.1.10/SIDEKIT-1.1.10.tar.gz

---
+++ packages/SIDEKIT/SIDEKIT-1.1.10.tar.gz/SIDEKIT-1.1.10/sidekit/__init__.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of SIDEKIT.
+#
+# SIDEKIT is a python package for speaker verification.
+# Home page: http://www-lium.univ-lemans.fr/sidekit/
+#
+# SIDEKIT is a python package for speaker verification.
+# Home page: http://www-lium.univ-lemans.fr/sidekit/
+#    
+# SIDEKIT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU LLesser General Public License as 
+# published by the Free Software Foundation, either version 3 of the License, 
+# or (at your option) any later version.
+#
+# SIDEKIT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with SIDEKIT.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Copyright 2014-2016 Anthony Larcher and Sylvain Meignier
+"""
+
+from ctypes import *
+from ctypes.util import find_library
+import logging
+import numpy
+import os
+import sys
+
+PARALLEL_MODULE = 'multiprocessing'  # can be , threading, multiprocessing MPI is planned in the future
+PARAM_TYPE = numpy.float32
+STAT_TYPE = numpy.float64
+THEANO_CONFIG = "cpu"  # can be gpu or cpu
+
+
+# Import bosaris-like classes
+from sidekit.bosaris import IdMap
+from sidekit.bosaris import Ndx
+from sidekit.bosaris import Key
+from sidekit.bosaris import Scores
+from sidekit.bosaris import DetPlot
+from sidekit.bosaris import effective_prior
+from sidekit.bosaris import logit_effective_prior
+from sidekit.bosaris import fast_minDCF
+
+# Import classes
+from sidekit.features_extractor import FeaturesExtractor
+from sidekit.features_server import FeaturesServer
+from sidekit.mixture import Mixture
+from sidekit.statserver import StatServer
+
+from sidekit.frontend.io import write_pcm
+from sidekit.frontend.io import read_pcm
+from sidekit.frontend.io import pcmu2lin
+from sidekit.frontend.io import read_sph
+from sidekit.frontend.io import write_label
+from sidekit.frontend.io import read_label
+from sidekit.frontend.io import read_spro4
+from sidekit.frontend.io import read_audio
+from sidekit.frontend.io import write_spro4
+from sidekit.frontend.io import read_htk
+from sidekit.frontend.io import write_htk
+
+from sidekit.frontend.vad import vad_energy
+from sidekit.frontend.vad import vad_snr
+from sidekit.frontend.vad import label_fusion
+from sidekit.frontend.vad import speech_enhancement
+
+
+from sidekit.frontend.normfeat import cms
+from sidekit.frontend.normfeat import cmvn
+from sidekit.frontend.normfeat import stg
+from sidekit.frontend.normfeat import rasta_filt
+
+
+from sidekit.frontend.features import compute_delta
+from sidekit.frontend.features import framing
+from sidekit.frontend.features import pre_emphasis
+from sidekit.frontend.features import trfbank
+from sidekit.frontend.features import mel_filter_bank
+from sidekit.frontend.features import mfcc
+from sidekit.frontend.features import pca_dct
+from sidekit.frontend.features import shifted_delta_cepstral
+
+from sidekit.iv_scoring import cosine_scoring
+from sidekit.iv_scoring import mahalanobis_scoring
+from sidekit.iv_scoring import two_covariance_scoring
+from sidekit.iv_scoring import PLDA_scoring
+
+from sidekit.gmm_scoring import gmm_scoring 
+
+from sidekit.jfa_scoring import jfa_scoring
+
+# Import NNET classes and functions
+theano_imported = False
+try:
+    if THEANO_CONFIG == "gpu":
+        os.environ['THEANO_FLAGS'] = 'mode=FAST_RUN,device=gpu,floatX=float32'
+    else:
+        os.environ['THEANO_FLAGS'] = 'mode=FAST_RUN,device=cpu,floatX=float32'
+
+    theano_imported = True
+except ImportError:
+    print("Cannot import Theano")
+
+if theano_imported:
+    print("Import theano")
+    from sidekit.nnet.feed_forward import FForwardNetwork
+
+
+from sidekit.sv_utils import clean_stat_server
+
+libsvm_loaded = False
+try:
+    dirname = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'libsvm')
+    if sys.platform == 'win32':
+        libsvm = CDLL(os.path.join(dirname, r'libsvm.dll'))
+        libsvm_loaded = True
+    else:
+        libsvm = CDLL(os.path.join(dirname, 'libsvm.so.2'))
+        libsvm_loaded = True
+except:
+    # For unix the prefix 'lib' is not considered.
+    if find_library('svm'):
+        libsvm = CDLL(find_library('svm'))
+        libsvm_loaded = True
+    elif find_library('libsvm'):
+        libsvm = CDLL(find_library('libsvm'))
+        libsvm_loaded = True
+    else:
+        libsvm_loaded = False
+        logging.warning('WARNNG: libsvm is not installed, please refer to the' +
+                        ' documentation if you intend to use SVM classifiers')
+
+if libsvm_loaded:
+    from sidekit.libsvm import *
+    from sidekit.svm_scoring import *
+    from sidekit.svm_training import *
+
+__author__ = "Anthony Larcher and Sylvain Meignier"
+__copyright__ = "Copyright 2014-2016 Anthony Larcher and Sylvain Meignier"
+__license__ = "LGPL"
+__maintainer__ = "Anthony Larcher"
+__email__ = "anthony.larcher@univ-lemans.fr"
+__status__ = "Production"
+__docformat__ = 'reStructuredText'
+__version__="1.1.10"
+
+# __all__ = ["io",
+#            "vad",
+#            "normfeat",
+#            "features"
+#            ]
---
+++ packages/SIDEKIT/SIDEKIT-1.1.10.tar.gz/SIDEKIT-1.1.10/sidekit/frontend/features.py
@@ -0,0 +1,431 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of SIDEKIT.
+#
+# SIDEKIT is a python package for speaker verification.
+# Home page: http://www-lium.univ-lemans.fr/sidekit/
+#
+# SIDEKIT is a python package for speaker verification.
+# Home page: http://www-lium.univ-lemans.fr/sidekit/
+#    
+# SIDEKIT is free software: you can redistribute it and/or modify
+# it under the terms of the GNU LLesser General Public License as 
+# published by the Free Software Foundation, either version 3 of the License, 
+# or (at your option) any later version.
+#
+# SIDEKIT is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with SIDEKIT.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Copyright 2014-2016 Anthony Larcher and Sylvain Meignier
+
+:mod:`frontend` provides methods to process an audio signal in order to extract
+useful parameters for speaker verification.
+"""
+
+import numpy
+import scipy
+from scipy.fftpack.realtransforms import dct
+from sidekit.frontend.vad import pre_emphasis
+from sidekit.frontend.io import *
+from sidekit.frontend.normfeat import *
+from sidekit.frontend.features import *
+
+PARAM_TYPE = numpy.float32
+
+__author__ = "Anthony Larcher and Sylvain Meignier"
+__copyright__ = "Copyright 2014-2016 Anthony Larcher and Sylvain Meignier"
+__license__ = "LGPL"
+__maintainer__ = "Anthony Larcher"
+__email__ = "anthony.larcher@univ-lemans.fr"
+__status__ = "Production"
+__docformat__ = 'reStructuredText'
+
+
+def hz2mel(f):
+    """Convert an array of frequency in Hz into mel.
+    
+    :param f: frequency to convert
+    
+    :return: the equivalence on the mel scale.
+    """
+    return 1127.01048 * numpy.log(f / 700.0 + 1)
+
+
+def mel2hz(m):
+    """Convert an array of mel values in Hz.
+    
+    :param m: ndarray of frequencies to convert in Hz.
+    
+    :return: the equivalent values in Hertz.
+    """
+    return (numpy.exp(m / 1127.01048) - 1) * 700.0
+
+
+def compute_delta(features,
+                  win=3,
+                  method='filter',
+                  filt=numpy.array([.25, .5, .25, 0, -.25, -.5, -.25])):
+    """features is a 2D-ndarray  each row of features is a a frame
+    
+    :param features: the feature frames to compute the delta coefficients
+    :param win: parameter that set the length of the computation window.
+            The size of the window is (win x 2) + 1
+    :param method: method used to compute the delta coefficients
+        can be diff or filter
+    :param filt: definition of the filter to use in "filter" mode, default one
+        is similar to SPRO4:  filt=numpy.array([.2, .1, 0, -.1, -.2])
+        
+    :return: the delta coefficients computed on the original features.
+    """
+    # First and last features are appended to the begining and the end of the 
+    # stream to avoid border effect
+    x = numpy.zeros((features.shape[0] + 2 * win, features.shape[1]), dtype=PARAM_TYPE)
+    x[:win, :] = features[0, :]
+    x[win:-win, :] = features
+    x[-win:, :] = features[-1, :]
+
+    delta = numpy.zeros(x.shape, dtype=PARAM_TYPE)
+
+    if method == 'diff':
+        filt = numpy.zeros(2 * win + 1, dtype=PARAM_TYPE)
+        filt[0] = -1
+        filt[-1] = 1
+
+    for i in range(features.shape[1]):
+        delta[:, i] = numpy.convolve(features[:, i], filt)
+
+    return delta[win:-win, :]
+
+
+def pca_dct(cep, left_ctx=12, right_ctx=12, p=None):
+    """Apply DCT PCA as in [McLaren 2015] paper:
+    Mitchell McLaren and Yun Lei, 'Improved Speaker Recognition 
+    Using DCT coefficients as features' in ICASSP, 2015
+    
+    A 1D-dct is applied to the cepstral coefficients on a temporal
+    sliding window.
+    The resulting matrix is then flatten and reduced by using a Principal
+    Component Analysis.
+    
+    :param cep: a matrix of cepstral cefficients, 1 line per feature vector
+    :param left_ctx: number of frames to consider for left context
+    :param right_ctx: number of frames to consider for right context
+    :param p: a PCA matrix trained on a developpment set to reduce the
+       dimension of the features. P is a portait matrix
+    """
+    y = numpy.r_[numpy.resize(cep[0, :], (left_ctx, cep.shape[1])),
+                 cep,
+                 numpy.resize(cep[-1, :], (right_ctx, cep.shape[1]))]
+
+    ceps = framing(y, win_size=left_ctx + 1 + right_ctx).transpose(0, 2, 1)
+    dct_temp = (dct_basis(left_ctx + 1 + right_ctx, left_ctx + 1 + right_ctx)).T
+    if p is None:
+        p = numpy.eye(dct_temp.shape[0] * cep.shape[1], dtype=PARAM_TYPE)
+    return (numpy.dot(ceps.reshape(-1, dct_temp.shape[0]),
+                      dct_temp).reshape(ceps.shape[0], -1)).dot(p)
+
+
+def shifted_delta_cepstral(cep, d=1, p=3, k=7):
+    """
+    Compute the Shifted-Delta-Cepstral features for language identification
+    
+    :param cep: matrix of feature, 1 vector per line
+    :param d: represents the time advance and delay for the delta computation
+    :param k: number of delta-cepstral blocks whose delta-cepstral 
+       coefficients are stacked to form the final feature vector
+    :param p: time shift between consecutive blocks.
+    
+    return: cepstral coefficient concatenated with shifted deltas
+    """
+
+    y = numpy.r_[numpy.resize(cep[0, :], (d, cep.shape[1])),
+                 cep,
+                 numpy.resize(cep[-1, :], (k * 3 + d, cep.shape[1]))]
+
+    delta = compute_delta(y, win=d, method='diff')
+
+    sdc = numpy.empty((cep.shape[0], cep.shape[1] * k))
+
+    idx = numpy.zeros(len(sdc), dtype='bool')
+    for ii in range(k):
+        idx[d + ii * p] = True
+    for ff in range(len(cep)):
+        sdc[ff, :] = delta[idx, :].reshape(1, -1)
+        idx = numpy.roll(idx, 1)
+    return numpy.hstack((cep, sdc))
+
+
+def trfbank(fs, nfft, lowfreq, maxfreq, nlinfilt, nlogfilt, midfreq=1000):
+    """Compute triangular filterbank for cepstral coefficient computation.
+
+    :param fs: sampling frequency of the original signal.
+    :param nfft: number of points for the Fourier Transform
+    :param lowfreq: lower limit of the frequency band filtered
+    :param maxfreq: higher limit of the frequency band filtered
+    :param nlinfilt: number of linear filters to use in low frequencies
+    :param  nlogfilt: number of log-linear filters to use in high frequencies
+    :param midfreq: frequency boundary between linear and log-linear filters
+
+    :return: the filter bank and the central frequencies of each filter
+    """
+    # Total number of filters
+    nfilt = nlinfilt + nlogfilt
+
+    # ------------------------
+    # Compute the filter bank
+    # ------------------------
+    # Compute start/middle/end points of the triangular filters in spectral
+    # domain
+    frequences = numpy.zeros(nfilt + 2, dtype=PARAM_TYPE)
+    if nlogfilt == 0:
+        linsc = (maxfreq - lowfreq) / (nlinfilt + 1)
+        frequences[:nlinfilt + 2] = lowfreq + numpy.arange(nlinfilt + 2) * linsc
+    elif nlinfilt == 0:
+        low_mel = hz2mel(lowfreq)
+        max_mel = hz2mel(maxfreq)
+        mels = numpy.zeros(nlogfilt + 2)
+        # mels[nlinfilt:]
+        melsc = (max_mel - low_mel) / (nfilt + 1)
+        mels[:nlogfilt + 2] = low_mel + numpy.arange(nlogfilt + 2) * melsc
+        # Back to the frequency domain
+        frequences = mel2hz(mels)
+    else:
+        # Compute linear filters on [0;1000Hz]
+        linsc = (min([midfreq, maxfreq]) - lowfreq) / (nlinfilt + 1)
+        frequences[:nlinfilt] = lowfreq + numpy.arange(nlinfilt) * linsc
+        # Compute log-linear filters on [1000;maxfreq]
+        low_mel = hz2mel(min([1000, maxfreq]))
+        max_mel = hz2mel(maxfreq)
+        mels = numpy.zeros(nlogfilt + 2, dtype=PARAM_TYPE)
+        melsc = (max_mel - low_mel) / (nlogfilt + 1)
+
+        # Verify that mel2hz(melsc)>linsc
+        while mel2hz(melsc) < linsc:
+            # in this case, we add a linear filter
+            nlinfilt += 1
+            nlogfilt -= 1
+            frequences[:nlinfilt] = lowfreq + numpy.arange(nlinfilt) * linsc
+            low_mel = hz2mel(frequences[nlinfilt - 1] + 2 * linsc)
+            max_mel = hz2mel(maxfreq)
+            mels = numpy.zeros(nlogfilt + 2, dtype=PARAM_TYPE)
+            melsc = (max_mel - low_mel) / (nlogfilt + 1)
+
+        mels[:nlogfilt + 2] = low_mel + numpy.arange(nlogfilt + 2) * melsc
+        # Back to the frequency domain
+        frequences[nlinfilt:] = mel2hz(mels)
+
+    heights = 2. / (frequences[2:] - frequences[0:-2])
+
+    # Compute filterbank coeff (in fft domain, in bins)
+    fbank = numpy.zeros((nfilt, int(numpy.floor(nfft / 2)) + 1), dtype=PARAM_TYPE)
+    # FFT bins (in Hz)
+    n_frequences = numpy.arange(nfft) / (1. * nfft) * fs
+
+    for i in range(nfilt):
+        low = frequences[i]
+        cen = frequences[i + 1]
+        hi = frequences[i + 2]
+
+        lid = numpy.arange(numpy.floor(low * nfft / fs) + 1, numpy.floor(cen * nfft / fs) + 1, dtype=numpy.int)
+        left_slope = heights[i] / (cen - low)
+        rid = numpy.arange(numpy.floor(cen * nfft / fs) + 1,
+                           min(numpy.floor(hi * nfft / fs) + 1, nfft), dtype=numpy.int)
+        right_slope = heights[i] / (hi - cen)
+        fbank[i][lid] = left_slope * (n_frequences[lid] - low)
+        fbank[i][rid[:-1]] = right_slope * (hi - n_frequences[rid[:-1]])
+
+    return fbank, frequences
+
+
+def mel_filter_bank(fs, nfft, lowfreq, maxfreq, widest_nlogfilt, widest_lowfreq, widest_maxfreq,):
+    """Compute triangular filterbank for cepstral coefficient computation.
+
+    :param fs: sampling frequency of the original signal.
+    :param nfft: number of points for the Fourier Transform
+    :param lowfreq: lower limit of the frequency band filtered
+    :param maxfreq: higher limit of the frequency band filtered
+    :param widest_nlogfilt: number of log filters
+    :param widest_lowfreq: lower frequency of the filter bank
+    :param widest_maxfreq: higher frequency of the filter bank
+    :param widest_maxfreq: higher frequency of the filter bank
+
+    :return: the filter bank and the central frequencies of each filter
+    """
+
+    # ------------------------
+    # Compute the filter bank
+    # ------------------------
+    # Compute start/middle/end points of the triangular filters in spectral
+    # domain
+    widest_freqs = numpy.zeros(widest_nlogfilt + 2, dtype=PARAM_TYPE)
+    low_mel = hz2mel(widest_lowfreq)
+    max_mel = hz2mel(widest_maxfreq)
+    mels = numpy.zeros(widest_nlogfilt+2)
+    melsc = (max_mel - low_mel) / (widest_nlogfilt + 1)
+    mels[:widest_nlogfilt + 2] = low_mel + numpy.arange(widest_nlogfilt + 2) * melsc
+    # Back to the frequency domain
+    widest_freqs = mel2hz(mels)
+
+    # Select filters in the narrow band
+    sub_band_freqs = numpy.array([fr for fr in widest_freqs if lowfreq <= fr <= maxfreq], dtype=PARAM_TYPE)
+
+    heights = 2./(sub_band_freqs[2:] - sub_band_freqs[0:-2])
+    nfilt = sub_band_freqs.shape[0] - 2
+
+    # Compute filterbank coeff (in fft domain, in bins)
+    fbank = numpy.zeros((nfilt, numpy.floor(nfft/2)+1), dtype=PARAM_TYPE)
+    # FFT bins (in Hz)
+    nfreqs = numpy.arange(nfft) / (1. * nfft) * fs
+
+    for i in range(nfilt):
+        low = sub_band_freqs[i]
+        cen = sub_band_freqs[i+1]
+        hi = sub_band_freqs[i+2]
+        lid = numpy.arange(numpy.floor(low * nfft / fs) + 1, numpy.floor(cen * nfft / fs) + 1, dtype=numpy.int)
+        left_slope = heights[i] / (cen - low)
+        rid = numpy.arange(numpy.floor(cen * nfft / fs) + 1, min(numpy.floor(hi * nfft / fs) + 1,
+                                                                 nfft), dtype=numpy.int)
+        right_slope = heights[i] / (hi - cen)
+        fbank[i][lid] = left_slope * (nfreqs[lid] - low)
+        fbank[i][rid[:-1]] = right_slope * (hi - nfreqs[rid[:-1]])
+
+    return fbank, sub_band_freqs
+
+
+def mfcc(input_sig,
+         lowfreq=100, maxfreq=8000,
+         nlinfilt=0, nlogfilt=24,
+         nwin=0.025,
+         fs=16000,
+         nceps=13,
+         shift=0.01,
+         get_spec=False,
+         get_mspec=False,
+         prefac=0.97):
+    """Compute Mel Frequency Cepstral Coefficients.
+
+    :param input_sig: input signal from which the coefficients are computed.
+            Input audio is supposed to be RAW PCM 16bits
+    :param lowfreq: lower limit of the frequency band filtered. 
+            Default is 100Hz.
+    :param maxfreq: higher limit of the frequency band filtered.
+            Default is 8000Hz.
+    :param nlinfilt: number of linear filters to use in low frequencies.
+            Default is 0.
+    :param nlogfilt: number of log-linear filters to use in high frequencies.
+            Default is 24.
+    :param nwin: length of the sliding window in seconds
+            Default is 0.025.
+    :param fs: sampling frequency of the original signal. Default is 16000Hz.
+    :param nceps: number of cepstral coefficients to extract. 
+            Default is 13.
+    :param shift: shift between two analyses. Default is 0.01 (10ms).
+    :param get_spec: boolean, if true returns the spectrogram
+    :param get_mspec:  boolean, if true returns the output of the filter banks
+    :param prefac: pre-emphasis filter value
+
+    :return: the cepstral coefficients in a ndaray as well as 
+            the Log-spectrum in the mel-domain in a ndarray.
+
+    .. note:: MFCC are computed as follows:
+        
+            - Pre-processing in time-domain (pre-emphasizing)
+            - Compute the spectrum amplitude by windowing with a Hamming window
+            - Filter the signal in the spectral domain with a triangular filter-bank, whose filters are approximatively
+               linearly spaced on the mel scale, and have equal bandwith in the mel scale
+            - Compute the DCT of the log-spectrom
+            - Log-energy is returned as first coefficient of the feature vector.
+    
+    For more details, refer to [Davis80]_.
+    """
+
+    # Prepare the signal to be processed on a sliding window.
+    # We first compute the overlap of frames and cut the signal in frames of length nwin
+    # overlaping by "overlap" samples
+    window_length = int(round(nwin * fs))
+    overlap = window_length - int(shift * fs)
+    framed = framing(input_sig, window_length, win_shift=window_length-overlap).copy()
+
+    # Pre-emphasis filtering is applied after framing to be consistent with stream processing
+    framed = pre_emphasis(framed, prefac)
+
+    l = framed.shape[0]
+    n_fft = 2 ** int(numpy.ceil(numpy.log2(window_length)))
+    ham = numpy.hamming(window_length)
+    spec = numpy.ones((l, int(n_fft / 2) + 1), dtype=PARAM_TYPE)
+    log_energy = numpy.log((framed**2).sum(axis=1))
+    dec = 500000
+    start = 0
+    stop = min(dec, l)
+    while start < l:
+        aham = framed[start:stop, :] * ham
+        mag = numpy.fft.rfft(aham, n_fft, axis=-1)
+        spec[start:stop, :] = mag.real**2 + mag.imag**2
+        start = stop
+        stop = min(stop + dec, l)
+    del framed
+
+    # Filter the spectrum through the triangle filter-bank
+    fbank = trfbank(fs, n_fft, lowfreq, maxfreq, nlinfilt, nlogfilt)[0]
+
+    mspec = numpy.log(numpy.dot(spec, fbank.T))   # A tester avec log10 et log
+
+    # Use the DCT to 'compress' the coefficients (spectrum -> cepstrum domain)
+    # The C0 term is removed as it is the constant term
+    ceps = dct(mspec, type=2, norm='ortho', axis=-1)[:, 1:nceps + 1]
+    lst = list()
+    lst.append(ceps)
+    lst.append(log_energy)
+    if get_spec:
+        lst.append(spec)
+    else:
+        lst.append(None)
+        del spec
+    if get_mspec:
+        lst.append(mspec)
+    else:
+        lst.append(None)
+        del mspec
+
+    return lst
+
+
+def framing(sig, win_size, win_shift=1, context=(0, 0), pad='zeros'):
+    """
+    :param sig: input signal, can be mono or multi dimensional
+    :param win_size: size of the window in term of samples
+    :param win_shift: shift of the sliding window in terme of samples
+    :param context: tuple of left and right context
+    :param pad: can be zeros or edge
+    """
+    dsize = sig.dtype.itemsize
+    if sig.ndim == 1:
+        sig = sig[:, numpy.newaxis]
+    # Manage padding
+    c = (context, ) + (sig.ndim - 1) * ((0, 0), )
+    _win_size = win_size + sum(context)
+    shape = (int((sig.shape[0] - win_size) / win_shift) + 1, 1, _win_size, sig.shape[1])
+    strides = tuple(map(lambda x: x * dsize, [win_shift * sig.shape[1], 1, sig.shape[1], 1]))
+    if pad == 'zeros':
+        return numpy.lib.stride_tricks.as_strided(numpy.lib.pad(sig, c, 'constant', constant_values=(0,)),
+                                                  shape=shape,
+                                                  strides=strides).squeeze()
+    elif pad == 'edge':
+        return numpy.lib.stride_tricks.as_strided(numpy.lib.pad(sig, c, 'edge'),
+                                                  shape=shape,
+                                                  strides=strides).squeeze()
+
+
+def dct_basis(nbasis, length):
+    """
+    :param nbasis: number of CT coefficients to keep
+    :param length: length of the matrix to process
+    :return: a basis of DCT coefficients
+    """
+    return scipy.fftpack.idct(numpy.eye(nbasis, length), norm='ortho')
