7a648c15d9ef54f041417b4e5ac1ea2ed8c0c9e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:17 +0000

    Add package lma/0.1/lma-0.1.zip

---
+++ packages/lma/lma-0.1.zip/lma-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'lma',
+  packages = ['lma'],
+  version = '0.1',
+  license='MIT',
+  description = 'ftir spectra library to analyse md simulations',
+  author = 'Matthias Massarczyk',
+  author_email = 'matthias.massarczyk@bph.rub.de',
+  url = 'https://github.com/MatthiasMa/Local-Mode-Analysis',
+  download_url = 'https://github.com/MatthiasMa/Local-Mode-Analysis/tarball/0.1', # I'll explain this in a second
+  keywords = ['ftir', 'spectra', 'spectrum', 'md', 'molecular', 'simulation'],
+  classifiers = [],
+)
