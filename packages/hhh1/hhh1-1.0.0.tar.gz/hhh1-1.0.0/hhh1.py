8299e9414147640047fec035b7345296b99cbe0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:34 +0000

    Add package hhh1/1.0.0/hhh1-1.0.0.tar.gz

---
+++ packages/hhh1/hhh1-1.0.0.tar.gz/hhh1-1.0.0/hhh1.py
@@ -0,0 +1,8 @@
+
+#This func is to count items in a cell or a str.
+
+def count_num(the_nums):
+    num=0
+    for each_words in the_nums:
+        num=num+1
+    return num
---
+++ packages/hhh1/hhh1-1.0.0.tar.gz/hhh1-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'hhh1',
+    version = '1.0.0',
+    py_modules= ['hhh1'],
+    author = 'huag',
+    author_email = 'liuhhgg@163.com',
+    url = 'http://www.baidu.com',
+    description = 'A simple counter for str & cell',
+
+    )
