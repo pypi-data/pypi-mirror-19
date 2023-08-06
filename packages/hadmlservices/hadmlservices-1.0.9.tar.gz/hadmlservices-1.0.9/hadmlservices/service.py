83ffe87aed658bf21de4200f89a7626e7c903fc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:18 +0000

    Add package hadmlservices/1.0.9/hadmlservices-1.0.9.tar.gz

---
+++ packages/hadmlservices/hadmlservices-1.0.9.tar.gz/hadmlservices-1.0.9/hadmlservices/service.py
@@ -0,0 +1,66 @@
+import os
+import sys
+import urllib
+from subprocess import call
+import time
+from hadmlservices import callbacks
+
+AWS_REGION = "us-east-1"
+
+def update(cb, key, message):
+    print message
+    cb.update_job(key, message)
+
+def user_data(root_dir,
+              input_s3,
+              output_s3,
+              model_params):
+    
+    cb = callbacks.CB();
+    update(cb, "job_state", "creating input and output dirs")
+    for x in [root_dir + "/input", root_dir + '/output']:
+        if not os.path.exists(x):
+            os.mkdir(x, 0777)
+        os.chmod(x, 0777)    
+    
+    update(cb, "job_state", "downloading log agent")
+    agent = root_dir + '/awslogs-agent-setup.py'
+    url = ("https://s3.amazonaws.com/aws-cloudwatch/" +
+           "downloads/latest/awslogs-agent-setup.py")
+    f = urllib.URLopener()
+    f.retrieve(url, agent)
+
+    update(cb, "job_state", "launching log agent")
+    os.chmod(agent, 0700)
+    path = os.path.dirname (os.path.abspath (__file__))
+    call([agent, "-n", "-r", AWS_REGION, "-c", path + "/awslogs.conf"])
+
+    update(cb, "job_state", "copying input data")
+    update(cb, "started", time.strftime('%Y-%m-%d %H:%M:%S'))
+    call(["aws", "s3", "sync", input_s3, root_dir + "/input"])
+    
+    update(cb, "job_state", "executing run_script")
+    call([root_dir + "/run_script",
+         root_dir + "/input",
+         root_dir + "/output",
+         model_params])
+
+    update(cb, "job_state", "copying output data")
+    call(["aws", "s3", "sync", root_dir + "/output", output_s3])
+
+    update(cb, "job_state", "finished")
+    update(cb, "finished", time.strftime('%Y-%m-%d %H:%M:%S'))
+
+    cb.terminate_instance()
+    return
+    '''
+    time.sleep(10)
+    # get instance id from aws
+    url = "http://169.254.169.254/latest/meta-data/instance-id"
+    instance_id = urllib.urlopen(url).read()
+    call(["aws",
+          "ec2",
+          "terminate-instances",
+           "--instance-ids", instance_id, 
+           "--region", AWS_REGION])
+    '''
