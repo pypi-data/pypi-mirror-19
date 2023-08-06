6e000ffe6ed2bfae4e0ddf6b667f86455ffa1d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:12 +0000

    Add package MusicRaft/0.3.0/MusicRaft-0.3.0.tar.gz

---
+++ packages/MusicRaft/MusicRaft-0.3.0.tar.gz/MusicRaft-0.3.0/setup.py
@@ -0,0 +1,24 @@
+"""
+MusicRaft
+"""
+
+#from distutils.core import setup
+from setuptools import setup
+
+setup(name = 'MusicRaft',
+    version = '0.3.0',
+    author = "Larry Myerscough",
+    author_email='hippostech@gmail.com',
+    packages=['musicraft'],
+    scripts=['bin/musicraft_local.py'],
+    url='http://pypi.python.org/pypi/MusicRaft/',
+    license='LICENSE.txt',
+    description='GUI for abcplus music notation.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "numpy >= 1.1.1",
+        "mido >= 1.1.14",
+        "PySide >= 1.2.1",
+
+    ],
+)
