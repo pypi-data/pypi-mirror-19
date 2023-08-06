464f26d9dee4a9dce71b4204b7b5811db276bde9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:02 +0000

    Add package keras-diagram/1.0.3/keras_diagram-1.0.3.tar.gz

---
+++ packages/keras-diagram/keras_diagram-1.0.3.tar.gz/keras_diagram-1.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+import pypandoc
+
+setup(name='keras_diagram',
+      version='1.0.3',
+      description='Keras models as ASCII diagrams',
+      long_description=pypandoc.convert('README.md','rst',format='markdown'),
+      url='http://github.com/brianlow/keras_diagram',
+      author='Brian Low',
+      author_email='brian.low22@gmail.com',
+      license='MIT',
+      packages=['keras_diagram'],
+      keywords='keras ascii diagram model',
+      zip_safe=False)
