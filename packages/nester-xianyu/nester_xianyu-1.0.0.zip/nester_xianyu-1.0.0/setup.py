b2efaea340fc44e2da2844e21c93ae9808252c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:17 +0000

    Add package nester-xianyu/1.0.0/nester_xianyu-1.0.0.zip

---
+++ packages/nester-xianyu/nester_xianyu-1.0.0.zip/nester_xianyu-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name              = 'nester_xianyu',
+    version           = '1.0.0',
+    py_modules        = ['nester'],
+    author            = 'xianyu',
+    author_email      = 'hypython@headfirstlabs.com',
+    url               = 'http://www.headfirstlabs.com',
+    description       = 'A simple printer of nested lists',
+)
