968c79403c292ba5097ec27f621abde5c3cd1584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:07 +0000

    Add package athleteatghsemail/1.0.0/athleteatghsemail-1.0.0.tar.gz

---
+++ packages/athleteatghsemail/athleteatghsemail-1.0.0.tar.gz/athleteatghsemail-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'athleteatghsemail',
+    version='1.0.0',
+    py_modules = ['coachData2'],
+    author='ghsemail',
+    authoremail = 'ghsemail@163.com',
+    url = 'http://www.hadfirstlabs.com',
+    description='A simple athlete program',
+)
