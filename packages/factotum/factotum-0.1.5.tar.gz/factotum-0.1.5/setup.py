1cb0f5cbc29b4ae154d8d11d2cc6eb4bdcc50b9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:28 +0000

    Add package factotum/0.1.5/factotum-0.1.5.tar.gz

---
+++ packages/factotum/factotum-0.1.5.tar.gz/factotum-0.1.5/setup.py
@@ -0,0 +1,37 @@
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "factotum",
+    version = "0.1.5",
+    author = "Brian Ballsun-Stanton",
+    author_email = "factorio@drbbs.org",
+    description = ("A tool to control a headless factorio server."),
+    license = "GPLv3",
+    keywords = "factorio rcon headless",
+    url = "https://github.com/Denubis/factotum",
+    py_modules=['factotum'],
+    packages = find_packages(),
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Games/Entertainment :: Simulation",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    ],
+    install_requires=["click>=5, <6", "ptyprocess", "clint", "circus", "daemonocle", "requests", "python-valve", "factoirc", "pyfakefs"],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'factotum=factotum.factotum:cli'
+        ]
+    }
+
+)
+
