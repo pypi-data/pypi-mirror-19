ddba2c9c6a6e70dc364f27c0e47664fca55b7524
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:30 +0000

    Add package aiothrift/0.0.2/aiothrift-0.0.2.tar.gz

---
+++ packages/aiothrift/aiothrift-0.0.2.tar.gz/aiothrift-0.0.2/aiothrift/__init__.py
@@ -0,0 +1,8 @@
+from .connection import create_connection, ThriftConnection
+from .errors import (
+    ConnectionClosedError
+)
+
+
+__version__ = '0.0.2'
+
---
+++ packages/aiothrift/aiothrift-0.0.2.tar.gz/aiothrift-0.0.2/aiothrift/server.py
@@ -0,0 +1,39 @@
+import asyncio
+
+from thriftpy.transport import TTransportException
+
+from aiothrift.processor import TProcessor
+from aiothrift.protocol import TBinaryProtocol
+
+
+class Server:
+    def __init__(self, processor, protocol_cls=TBinaryProtocol):
+        self.processor = processor
+        self.protocol_cls = protocol_cls
+
+    @asyncio.coroutine
+    def __call__(self, reader, writer):
+        iproto = self.protocol_cls(reader)
+        oproto = self.protocol_cls(writer)
+        while not reader.at_eof():
+            try:
+                yield from self.processor.process(iproto, oproto)
+            except TTransportException:
+                pass
+            except Exception:
+                pass
+        writer.close()
+
+
+@asyncio.coroutine
+def make_server(service, handler,
+                host="localhost", port=9090,
+                loop=None,
+                protocol_cls=TBinaryProtocol,
+                ):
+    processor = TProcessor(service, handler)
+    if loop is None:
+        loop = asyncio.get_event_loop()
+    server = yield from asyncio.start_server(
+        Server(processor, protocol_cls), host, port, loop=loop)
+    return server
