b9869f1e00e39684652c01ccd04fe397332661c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:05 +0000

    Add package property/2.2/property-2.2.tar.gz

---
+++ packages/property/property-2.2.tar.gz/property-2.2/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='property',
+      version='2.2',
+      packages=['properties'],
+      description='A python module to read a property file.Similar to PropertyPlaceholderConfigurer in spring (java).Example : http://anandjoshi.me/pythonpropertyfileloader/',
+      author='Anand Joshi',
+      author_email='anandhjoshi@outlook.com',
+      )
