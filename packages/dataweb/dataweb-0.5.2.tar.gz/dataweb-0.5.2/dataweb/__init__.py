531c62d74cabe98d578619088dfc91c9d515ead3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:01 +0000

    Add package dataweb/0.5.2/dataweb-0.5.2.tar.gz

---
+++ packages/dataweb/dataweb-0.5.2.tar.gz/dataweb-0.5.2/dataweb/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+DATAWEB: Gestión de datos recogidos en web de forma periódica.
+"""
+from dataweb.classdataweb import DataWeb
+from dataweb.requestweb.__init__ import get_data_en_intervalo, request_data_url
+
+
+__version__ = '0.5.2'
