795e18c0c64ecf2a4bbb807442374091d4cc19b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:47 +0000

    Add package maps/3.0.0/maps-3.0.0.tar.gz

---
+++ packages/maps/maps-3.0.0.tar.gz/maps-3.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='maps',
+    version='3.0.0',
+    description='Maps: flavors of Python dictionaries',
+    url='https://github.com/pcattori/maps',
+    author='Pedro Cattori',
+    author_email='pcattori@gmail.com',
+    license='MIT',
+    packages=['maps'],
+    test_suite='tests',
+    extras_require={'test': ['codecov==2.0.5']},
+    keywords = ['maps', 'namddict', 'frozendict', 'fixedkey', 'fixeddict'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6']
+)
