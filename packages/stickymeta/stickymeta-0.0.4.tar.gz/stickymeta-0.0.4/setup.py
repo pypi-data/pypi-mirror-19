ddd192b719b67da88b5f97a1700aed75c085cb52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:24 +0000

    Add package stickymeta/0.0.4/stickymeta-0.0.4.tar.gz

---
+++ packages/stickymeta/stickymeta-0.0.4.tar.gz/stickymeta-0.0.4/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='stickymeta',
+    version='0.0.4',
+    packages=['stickymeta'],
+    url='http://github.com/mizhgun/scrapy_stickymeta',
+    license='MIT',
+    author='Mikhail Kolganov',
+    author_email='mikhail.kolganov@gmail.com',
+    description='Handy tools to maintain persistent meta values between requests in Scrapy spiders',
+    keywords=['scrapy'],
+)
