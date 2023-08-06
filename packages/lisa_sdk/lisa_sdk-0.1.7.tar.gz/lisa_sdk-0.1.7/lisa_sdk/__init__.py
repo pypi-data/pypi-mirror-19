df01d88cc02c0e39662e868b9ece55c1333c8a8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:05 +0000

    Add package lisa_sdk/0.1.7/lisa_sdk-0.1.7.tar.gz

---
+++ packages/lisa_sdk/lisa_sdk-0.1.7.tar.gz/lisa_sdk-0.1.7/lisa_sdk/__init__.py
@@ -0,0 +1,33 @@
+class LisaPlugin(object):
+    def get_name(self):
+        raise NotImplementedError
+
+    def get_description(self):
+        raise NotImplementedError
+
+    def get_call_keywords(self):
+        raise NotImplementedError
+
+    def on_enable(self):
+        raise NotImplementedError
+
+    def on_disable(self):
+        raise NotImplementedError
+
+    def process_speech(self, speech, language):
+        raise NotImplementedError
+
+    def get_listening_language(self):
+        raise NotImplementedError
+
+
+class UserSpeech(object):
+    def __init__(self, speeches, language):
+        self.speeches = speeches
+        self.language = language
+
+
+class SpeechResponse(object):
+    def __init__(self, speech, language=None):
+        self.speech = speech
+        self.language = language
