c4442756704fdfe37f94514a4ec204427f8fc52f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:18 +0000

    Add package rolabesti/0.1.1/rolabesti-0.1.1.tar.gz

---
+++ packages/rolabesti/rolabesti-0.1.1.tar.gz/rolabesti-0.1.1/rolabesti/settings.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import getpass
+import os
+
+# directories
+BASE_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)))
+MUSIC_DIR = '/home/{}/Music'.format(getpass.getuser())
+
+# default arguments
+MAX_TRACKLIST_LENGTH = 60
+MAX_TRACK_LENGTH = 10
+MIN_TRACK_LENGTH = 0
+PLAYER = 'vlc'
+SORTING = 'random'
+
+# player settings
+OVERLAP_LENGTH = 3
+
+# mongo settings
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+MONGO_DBNAME = 'rolabesti'
+MONGO_COLNAME = 'tracks'
