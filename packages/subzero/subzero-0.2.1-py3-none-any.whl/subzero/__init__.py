79cf9b0b0b3a4e8312fe05e38fbb8018f7e8ff40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:14 +0000

    Add package subzero/0.2.1/subzero-0.2.1-py3-none-any.whl

---
+++ packages/subzero/subzero-0.2.1-py3-none-any.whl/subzero/__init__.py
@@ -0,0 +1,56 @@
+import sys
+
+import subzero.dist
+
+try:
+    from setuptools import setup as distutils_setup
+except ImportError:
+    from distutils.core import setup as distutils_setup
+
+from subzero.dist import build_exe, Executable
+from subzero.windist import bdist_msi
+
+# This file was originally taken from cx_Freeze by Anthony Tuininga, and is licensed under the  PSF license.
+
+version = "5.0"
+__version__ = version
+
+
+def _AddCommandClass(commandClasses, name, cls):
+    if name not in commandClasses:
+        commandClasses[name] = cls
+
+
+def setup(**attrs):
+    commandClasses = attrs.setdefault("cmdclass", {})
+    if sys.platform == "win32":
+        if sys.version_info[:2] >= (2, 5):
+            _AddCommandClass(commandClasses, "bdist_msi", bdist_msi)
+    _AddCommandClass(commandClasses, "build_exe", build_exe)
+    if 'install_requires' in attrs:
+        # Simply set the install_requires to setup_requires and setuptools will take care of the rest.
+        attrs.setdefault('setup_requires', []).extend(attrs['install_requires'])
+
+    attrs.setdefault('scripts', [])
+    attrs.setdefault('entry_points', {}).setdefault('console_scripts', [])
+    attrs.setdefault('options', {}).setdefault('build_exe', {}).setdefault('executables', [])
+
+    for script in attrs['scripts'] + attrs['entry_points']['console_scripts']:
+        if type(script) is Executable:
+            attrs['options']['build_exe']['executables'].append(script)
+        else:
+            attrs['options']['build_exe']['executables'].append(None)
+
+    attrs['scripts'] = [str(script) for script in attrs['scripts']]
+    attrs['entry_points']['console_scripts'] = \
+        [str(entry_point) for entry_point in attrs['entry_points']['console_scripts']]
+
+    distutils_setup(**attrs)
+
+
+subzero.dist.setup = setup  # Backwards compatibility
+
+from ._version import get_versions
+
+__version__ = get_versions()['version']
+del get_versions
