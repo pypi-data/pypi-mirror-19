2fe2a6713134823ef193fde5698ae77d43c3b3c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:35 +0000

    Add package twistyRL/0.1.6/twistyRL-0.1.6.tar.gz

---
+++ packages/twistyRL/twistyRL-0.1.6.tar.gz/twistyRL-0.1.6/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+from os import path
+
+setup_requires = [ ]
+
+install_requires = [
+    'numpy==1.12.0'
+]
+
+
+dependency_links = [
+]
+
+setup(
+        name='twistyRL',
+        version='0.1.06',
+        url='https://github.com/python-study-ko/twistyRL',
+        license='MIT License',
+        description='twisty cube game for ML',
+        author='wesky93',
+        author_email='wesky93@gmail.com',
+        packages=[ "twistyRL" ],
+        include_package_data=True,
+        install_requires=install_requires,
+        setup_requires=setup_requires,
+        dependency_links=dependency_links,
+        keywords=[ 'cube', 'rubiks', 'twistycube', 'ML', 'RL' ],
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+            'License :: OSI Approved :: MIT License',
+        ]
+
+)
