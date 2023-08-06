0f3eb8412eb3c8f5a901d44345fd3cbca3d46804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:18 +0000

    Add package afdata/0.0.7/afdata-0.0.7.tar.gz

---
+++ packages/afdata/afdata-0.0.7.tar.gz/afdata-0.0.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='afdata',
+    description='A simple data science library',
+    version='0.0.7',
+    url='https://github.com/aaronfalloon/afdata',
+    author='Aaron Falloon',
+    author_email='contact@aaronfalloon.com',
+    license='MIT',
+    packages=['afdata']
+)
