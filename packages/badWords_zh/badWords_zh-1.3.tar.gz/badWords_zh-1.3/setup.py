c9b4da70d10e7dadafc6c92e6e60822afb3e6e3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:32 +0000

    Add package badWords_zh/1.3/badWords_zh-1.3.tar.gz

---
+++ packages/badWords_zh/badWords_zh-1.3.tar.gz/badWords_zh-1.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'badWords_zh',
+  packages = ['badWords_zh'],
+  version = '1.3',
+  description = 'Chinese Bad Word Counter',
+  author = 'chhsu',
+  author_email = 'dev@botimize.io',
+  url = 'http://gitlab.botimize.io/chhsu/badWord_counter_zh',
+  download_url = 'https://github.com/botimize/badWord_counter_zh/tarball/0.1',
+  keywords = ['botimize', 'python', 'badWords', 'chinese', 'data_science'],
+  classifiers = [],
+)
