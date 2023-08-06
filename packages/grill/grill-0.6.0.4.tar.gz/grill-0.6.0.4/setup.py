2624fe2ae7c73b76cec7808ff0d0132db55df446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:17 +0000

    Add package grill/0.6.0.4/grill-0.6.0.4.tar.gz

---
+++ packages/grill/grill-0.6.0.4.tar.gz/grill-0.6.0.4/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='grill',
+    version='0.6.0.4',
+    packages=find_packages(),
+    description='Pipeline and data flow tools for (but not limited to) audiovisual projects, with DCC support.',
+    author='Christian Lopez Barron',
+    author_email='christianlb.vfx@outlook.com',
+    url='https://github.com/thegrill/grill',
+    download_url='https://github.com/thegrill/grill/releases/tag/0.6.0.4',
+    classifiers=['Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',]
+)
