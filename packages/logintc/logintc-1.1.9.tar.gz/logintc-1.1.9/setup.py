2cd9400cf324bf3e0b1d4149ce67e938cd078669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:35 +0000

    Add package logintc/1.1.9/logintc-1.1.9.tar.gz

---
+++ packages/logintc/logintc-1.1.9.tar.gz/logintc-1.1.9/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+import logintc
+
+setup(
+    name='logintc',
+    version='.'.join(logintc.__version_info__),
+    author='Cyphercor Inc.',
+    author_email='support@cyphercor.com',
+    url='https://github.com/logintc/logintc-python',
+    packages=find_packages(),
+    package_data={'': ['*.pem']},
+    license='LICENSE',
+    description='API client for LoginTC two-factor authentication.',
+    long_description=open('README.rst', 'rt').read(),
+    keywords=['logintc', 'two-factor', 'authentication', 'security'],
+    install_requires=['httplib2 >= 0.9.2'],
+    classifiers=['Topic :: Security',
+                 'License :: OSI Approved :: BSD License']
+)
