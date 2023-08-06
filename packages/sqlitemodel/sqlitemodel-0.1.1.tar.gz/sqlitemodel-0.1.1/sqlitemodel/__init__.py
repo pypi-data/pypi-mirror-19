2948de600031a745a427da924c253a19e8c72aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:21 +0000

    Add package sqlitemodel/0.1.1/sqlitemodel-0.1.1.tar.gz

---
+++ packages/sqlitemodel/sqlitemodel-0.1.1.tar.gz/sqlitemodel-0.1.1/sqlitemodel/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) 2016-2017, René Tanczos <gravmatt@gmail.com> (Twitter @gravmatt)
+The MIT License (MIT)
+
+sqlitemodel is a wrapper for the sqlite3 database that enables you to
+create models you can easily query, save and update.
+
+Project on github https://github.com/gravmatt/sqlitemodel
+"""
+
+__author__ = 'Rene Tanczos'
+__version__ = '0.1.1'
+__license__ = 'MIT'
+
+from SQL import SQL
+from Database import Database
+from Model import Model
