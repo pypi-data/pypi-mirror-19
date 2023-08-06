0e06fac8a24ce666d59e9f0faed4b34874908e42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:30 +0000

    Add package toil/3.5.2a1.dev12/toil-3.5.2a1.dev12-py2.7.egg

---
+++ packages/toil/toil-3.5.2a1.dev12-py2.7.egg/toil/version.py
@@ -0,0 +1,13 @@
+dockerShortTag = '3.5.2a1.dev12-6d4f0b9'
+baseVersion = '3.5.2a1'
+dockerTag = '3.5.2a1.dev12-6d4f0b9d41c4598dd7555ea5b1e817b18595b87a'
+dockerName = 'toil'
+buildNumber = '12'
+cgcloudVersion = '1.6.0a1.dev393'
+version = '3.5.2a1.dev12-6d4f0b9d41c4598dd7555ea5b1e817b18595b87a'
+dirty = False
+shortVersion = '3.5.2a1.dev12-6d4f0b9'
+currentCommit = '6d4f0b9d41c4598dd7555ea5b1e817b18595b87a'
+dockerMinimalTag = '3.5.2a1.dev12'
+distVersion = '3.5.2a1.dev12'
+dockerRegistry = 'quay.io/ucsc_cgl'
