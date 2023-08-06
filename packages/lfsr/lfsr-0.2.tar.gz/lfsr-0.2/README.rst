edb43b428f70f8880e3f9ce75226625c6220036a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:37 +0000

    Add package lfsr/0.2/lfsr-0.2.tar.gz

---
+++ packages/lfsr/lfsr-0.2.tar.gz/lfsr-0.2/README.rst
@@ -0,0 +1,13 @@
+lfsr
+====
+
+Linear Feedback Shift Register toolkit.
+
+* **db** - Database of precalculated values.
+
+`Documentation <http://pylfsr.readthedocs.io/>`_
+
+`Source code <https://github.com/gergelyk/pylfsr/>`_
+
+`Package <https://pypi.python.org/pypi/lfsr/>`_
+
---
+++ packages/lfsr/lfsr-0.2.tar.gz/lfsr-0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import os
+
+top_dir, _ = os.path.split(os.path.abspath(__file__))
+
+with open(os.path.join(top_dir, 'VERSION')) as f:
+    version = f.readline().strip()
+
+setup(
+    name = 'lfsr',
+    version = version,
+    description = 'Linear Feedback Shift Register toolkit.',
+    author = 'Grzegorz Krason',
+    author_email = 'grzegorz@krason.me',
+    url = 'https://github.com/gergelyk/pylfsr',
+    download_url = 'https://github.com/gergelyk/pylfsr/tarball/' + version,
+    license = 'MIT',
+    packages = ['lfsr'],
+    package_data = {'': ['../VERSION']},
+    keywords = 'lfsr prng random generator'.split(),
+    classifiers = [],
+)
+
