22fd817164a5cb425ce5f36e929c38d1acbaa8c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:52 +0000

    Add package artagger/0.1.0.2/artagger-0.1.0.2.tar.gz

---
+++ packages/artagger/artagger-0.1.0.2.tar.gz/artagger-0.1.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='artagger',
+    version='0.1.0.2',
+    description='RDRPOSTagger Implementation.',
+    author='Frans Huang',
+    author_email='franssiswanto@gmail.com',
+    url='https://github.com/franziz/artagger/',
+    packages=["artagger", "artagger.InitialTagger", "artagger.SCRDRlearner", "artagger.Utility"],
+    include_package_data=True,
+    license='MIT',
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+    ],
+)
