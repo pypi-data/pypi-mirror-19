3260310a3221cccd9d70bcf4f7d3720be6a4caea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:49 +0000

    Add package custos/0.0.7/custos-0.0.7.tar.gz

---
+++ packages/custos/custos-0.0.7.tar.gz/custos-0.0.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='custos',
+    version='0.0.7',
+    description='A framework for monitoring and notifying',
+    url='https://github.com/fact-project/pycustos',
+    author='Dominik Neise, Maximilian Noethe, Sebastian Mueller',
+    author_email='maximilian.noethe@tu-dortmund.de',
+    license='MIT',
+    packages=[
+        'custos',
+        'custos.checks',
+        'custos.notify',
+        ],
+    install_requires=[
+        'requests',
+        'apscheduler',
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest>=3.0.0'],
+    zip_safe=False,
+)
