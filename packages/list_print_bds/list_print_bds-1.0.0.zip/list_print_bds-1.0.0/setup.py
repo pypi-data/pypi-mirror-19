14aec2dcc1657178c2b4c787878a4cf10533c2f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:31 +0000

    Add package list_print_bds/1.0.0/list_print_bds-1.0.0.zip

---
+++ packages/list_print_bds/list_print_bds-1.0.0.zip/list_print_bds-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'list_print_bds',
+    version = '1.0.0',
+    py_modules = ['list_print'],
+    author = 'DaddyBA',
+    author_email = 'hills143+python@gmail.com',
+    url = 'http://linkedin.com/in/bryan-shannon-09a85014',
+    description = 'A simple printer of nested lists'
+    )
