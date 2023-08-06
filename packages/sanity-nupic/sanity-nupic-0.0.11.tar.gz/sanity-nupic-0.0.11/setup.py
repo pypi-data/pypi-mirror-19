63612e7b9c282039987747301612781a5b02a046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:44 +0000

    Add package sanity-nupic/0.0.11/sanity-nupic-0.0.11.tar.gz

---
+++ packages/sanity-nupic/sanity-nupic-0.0.11.tar.gz/sanity-nupic-0.0.11/setup.py
@@ -0,0 +1,17 @@
+import platform
+import sys
+
+from setuptools import find_packages, setup
+
+setup(name="sanity-nupic",
+      version="0.0.11",
+      description="NuPIC server for Sanity client",
+      author="Marcus Lewis",
+      author_email="mrcslws@gmail.com",
+      url="https://github.com/nupic-community/sanity-nupic/",
+      packages=find_packages(),
+      package_data={'htmsanity': ['htmsanity/nupic/sanity/public/*',]},
+      include_package_data=True,
+      install_requires=['Twisted', 'autobahn', 'transit-python'],
+      zip_safe=False,
+     )
