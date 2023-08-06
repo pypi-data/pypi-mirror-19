8ce2778c310024bac864661b12857cbe711c8dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:33 +0000

    Add package nester_print/1.2.0/nester_print-1.2.0.zip

---
+++ packages/nester_print/nester_print-1.2.0.zip/nester_print-1.2.0/nester_print.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  7 16:24:27 2017
+
+@author: Notebook
+"""
+
+"""
+This is the "nester.py" module, and it provides one function called print_lol()
+which prints list that may or may not include nested lists.
+"""
+
+def print_lol(the_list, level=0):
+    """
+    This function takes a positional argument called "the_list" which is any Python
+    list (of, possibly, nested lists). Each data item in the provided list is
+    (recursively) printed to the screen on its own line.
+    A second argument called "level" is an optional argument and used to insert tab-stops when a nested
+    list is encountered
+    """
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level+1)
+        else:
+            for tab_stop in range(level):
+                print("\t",end='')
+            print(each_item)
