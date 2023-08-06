34d3b840c13c4e0a42bd5aa8eb71b4c504c21cac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:13 +0000

    Add package Flask-Datadog/0.1.4/Flask-Datadog-0.1.4.tar.gz

---
+++ packages/Flask-Datadog/Flask-Datadog-0.1.4.tar.gz/Flask-Datadog-0.1.4/README.rst
@@ -0,0 +1,29 @@
+Flask-Datadog
+=============
+
+This is a simple Flask extension that allows to access DogStatsd in your Flask application. It has an API
+compatible with Flask-StatsD
+
+
+Installation
+------------
+
+To install it, simply: ::
+
+    pip install Flask-Datadog
+
+
+Usage
+-----
+
+You only need to import and initialize your app ::
+
+    from flask import Flask
+    from flask.ext.datadog import API, StatsD
+
+    app = Flask(__name__)
+    app.config['STATSD_HOST'] = 'localhost'
+    app.config['DATADOG_API_KEY']  = 'api_key'
+    app.config['DATADOG_APP_KEY']  = 'app_key'
+    statsd = StatsD(app)
+    dogapi = API(app)
---
+++ packages/Flask-Datadog/Flask-Datadog-0.1.4.tar.gz/Flask-Datadog-0.1.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='Flask-Datadog',
+    version='0.1.4',
+    url='https://github.com/50onRed/flask-datadog.git',
+    license='BSD',
+    author='50onRed',
+    author_email='marcus.mccurdy@gmail.com',
+    description='Access to dogstatsd in your app.',
+    py_modules=['flask_datadog'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=['Flask', 'datadog'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
