376290c90bdcf8f50b0f9bf851664e64b9d1ecb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:25 +0000

    Add package pytorch-extras/0.1.1/pytorch-extras-0.1.1.tar.gz

---
+++ packages/pytorch-extras/pytorch-extras-0.1.1.tar.gz/pytorch-extras-0.1.1/torch_extras/extras.py
@@ -0,0 +1,25 @@
+import six
+import torch
+from torch.autograd import Variable
+
+
+def expand_along(var, mask):
+    indexes = torch.range(0, var.size(0) - 1).view(-1, 1).repeat(1, mask.size(1))
+    _mask = indexes[mask].long()
+    if isinstance(var, Variable):
+        _mask = Variable(_mask, volatile=var.volatile)
+    return torch.index_select(var, 0, _mask)
+
+
+def expand_dims(var, dim=0):
+    sizes = list(var.size())
+    sizes.insert(dim, 1)
+    return var.view(*sizes)
+
+
+def select_item(var, index):
+    index_mask = index.view(-1, 1).repeat(1, var.size(1))
+    mask = torch.range(0, var.size(1) - 1).long()
+    mask = mask.repeat(var.size(0), 1)
+    mask = mask.eq(index_mask)
+    return var[mask]
