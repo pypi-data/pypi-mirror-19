794268a300db1f0f8fa22c50f6907a21911dc781
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:38 +0000

    Add package toil/3.5.0a1.dev326/toil-3.5.0a1.dev326.tar.gz

---
+++ packages/toil/toil-3.5.0a1.dev326.tar.gz/toil-3.5.0a1.dev326/src/toil/version.py
@@ -0,0 +1,13 @@
+dockerShortTag = '3.5.0a1.dev326-8c445e1'
+baseVersion = '3.5.0a1'
+dockerTag = '3.5.0a1.dev326-8c445e1fbcd6ecf5062da59be32b5e2095be8f08'
+dockerName = 'toil'
+buildNumber = '326'
+cgcloudVersion = '1.6.0a1.dev393'
+version = '3.5.0a1.dev326-8c445e1fbcd6ecf5062da59be32b5e2095be8f08'
+dirty = False
+shortVersion = '3.5.0a1.dev326-8c445e1'
+currentCommit = '8c445e1fbcd6ecf5062da59be32b5e2095be8f08'
+dockerMinimalTag = '3.5.0a1.dev326'
+distVersion = '3.5.0a1.dev326'
+dockerRegistry = 'quay.io/ucsc_cgl'
