e826d9bfe441e0b6841f26876e24e751a5e32548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:25 +0000

    Add package wellrng/0.2/wellrng-0.2.tar.gz

---
+++ packages/wellrng/wellrng-0.2.tar.gz/wellrng-0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name = 'wellrng',
+  packages = ['wellrng'],
+  version = '0.2',
+  description = 'A replacment for the default random lib using WELL1024a RNG',
+  author = 'Raphael Stefanini',
+  author_email = 'rphl@rphl.net',
+  url = 'https://github.com/rphlo/py-wellrng',
+  download_url = 'https://github.com/rphlo/py-wellrng/tarball/0.2',
+  keywords = ['random', 'well1024a', 'PRNG', 'RNG'],
+  classifiers = [],
+)
