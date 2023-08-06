468d6afdbe54d87006b9c87a48fa2dc1d99b62a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:31 +0000

    Add package requestor/0.1.1/requestor-0.1.1-py2.py3-none-any.whl

---
+++ packages/requestor/requestor-0.1.1-py2.py3-none-any.whl/requestor-0.1.1.dist-info/DESCRIPTION.rst
@@ -0,0 +1,5 @@
+Requestor
+=========
+Use this package to make HTTP post calls to django APIs with csrf support, and return response in json.
+
+
---
+++ packages/requestor/requestor-0.1.1-py2.py3-none-any.whl/requestor.py
@@ -0,0 +1,12 @@
+import requests
+
+def requestor(url, data):
+    get_response = requests.get(url)
+    post_response = requests.post(
+          url,
+          data=data,
+          headers={"X-CSRFToken": get_response.cookies['csrftoken']},
+          cookies=get_response.cookies
+        )
+
+    return post_response
