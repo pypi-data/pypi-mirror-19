112b07d375c70e96383198fbbc727dc0e5275879
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:48 +0000

    Add package online-judge-tools/0.1.1/online-judge-tools-0.1.1.tar.gz

---
+++ packages/online-judge-tools/online-judge-tools-0.1.1.tar.gz/online-judge-tools-0.1.1/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name='online-judge-tools',
+    version='0.1.1',
+    description='Tools for online-judge services',
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'colorama',
+    ],
+    author='Kimiyuki Onaka',
+    author_email='kimiyuki95@gmail.com',
+    url='https://github.com/kmyk/online-judge-tools',
+    license='MIT License',
+    packages=find_packages(exclude=( 'tests', 'docs' )),
+    scripts=[ 'oj' ],
+)
