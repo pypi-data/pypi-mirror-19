561894fd031ced683a7f38c160a605a9928c8e63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:03 +0000

    Add package ccw_get_forecast/1.0.0/ccw_get_forecast-1.0.0.zip

---
+++ packages/ccw_get_forecast/ccw_get_forecast-1.0.0.zip/ccw_get_forecast-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+#from astroid.__pkginfo__ import author, author_email, description
+
+setup(
+      name = 'ccw_get_forecast',
+      version = '1.0.0',
+      py_modules = ['ccw_get_forecast'],
+      author = 'ccw',
+      author_email = 'ccw3435@naver.com',
+      description = 'a simple facade pattern test! return city weather.'
+      )
