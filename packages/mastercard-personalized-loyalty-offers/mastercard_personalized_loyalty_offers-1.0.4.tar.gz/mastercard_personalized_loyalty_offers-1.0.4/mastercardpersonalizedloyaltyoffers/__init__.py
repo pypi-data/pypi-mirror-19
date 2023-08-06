5cfcbc2820a75349efc31de62d5de4b7a1666f5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:26 +0000

    Add package mastercard-personalized-loyalty-offers/1.0.4/mastercard_personalized_loyalty_offers-1.0.4.tar.gz

---
+++ packages/mastercard-personalized-loyalty-offers/mastercard_personalized_loyalty_offers-1.0.4.tar.gz/mastercard_personalized_loyalty_offers-1.0.4/mastercardpersonalizedloyaltyoffers/__init__.py
@@ -0,0 +1,15 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardpersonalizedloyaltyoffers.resourceconfig import *
+
+
+# import apis into api package
+from mastercardpersonalizedloyaltyoffers.activatestatementcreditoffer import *
+from mastercardpersonalizedloyaltyoffers.matchedoffers import *
+from mastercardpersonalizedloyaltyoffers.offerdetails import *
+from mastercardpersonalizedloyaltyoffers.redeemedoffers import *
+from mastercardpersonalizedloyaltyoffers.statementcreditactivationdetail import *
+from mastercardpersonalizedloyaltyoffers.userfeedback import *
+from mastercardpersonalizedloyaltyoffers.usersaving import *
+from mastercardpersonalizedloyaltyoffers.usertoken import *
