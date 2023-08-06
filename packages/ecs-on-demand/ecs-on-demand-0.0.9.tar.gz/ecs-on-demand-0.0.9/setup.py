d58712a32e486e230b5f8ef92366c7b3ca05f02c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:56 +0000

    Add package ecs-on-demand/0.0.9/ecs-on-demand-0.0.9.tar.gz

---
+++ packages/ecs-on-demand/ecs-on-demand-0.0.9.tar.gz/ecs-on-demand-0.0.9/setup.py
@@ -0,0 +1,20 @@
+import os
+from setuptools import setup, find_packages
+import warnings
+
+setup(
+    name='ecs-on-demand',
+    version='0.0.9',
+    packages=find_packages(),
+    include_package_data=True,
+    # namespace_packages = ['ecs_on_demand'],
+    author="Jason Bartz",
+    author_email="jason@jasonbartz.com",
+    url="https://github.com/jasonbartz/ecs-on-demand",
+    # download_url = "https://github.com/jasonbartz/ecs-on-demand/tarball/v0.0.1",
+    keywords = ['amazon', 'ecs', 'aws', 'container', 'docker'],
+    classifiers = [],
+    extras_require={
+        'not-lambda': ['boto3>=1.4.4'],
+    }
+)
