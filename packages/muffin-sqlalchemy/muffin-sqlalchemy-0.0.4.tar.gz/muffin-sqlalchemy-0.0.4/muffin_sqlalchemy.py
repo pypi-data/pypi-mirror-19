43a6bee77372359b7bd93c54119e236f4b278f53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:52:37 +0000

    Add package muffin-sqlalchemy/0.0.4/muffin-sqlalchemy-0.0.4.tar.gz

---
+++ packages/muffin-sqlalchemy/muffin-sqlalchemy-0.0.4.tar.gz/muffin-sqlalchemy-0.0.4/muffin_sqlalchemy.py
@@ -0,0 +1,66 @@
+""" Support SqlAlchemy in Muffin framework. """
+import asyncio
+
+from muffin.plugins import BasePlugin
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+__version__ = "0.0.4"
+__project__ = "muffin-sqlalchemy"
+__author__ = "Diego Garcia <drgarcia1986@gmail.com>"
+__license__ = "MIT"
+
+
+SqlAlchemyDeclarativeBase = declarative_base()
+
+
+class Plugin(BasePlugin):
+
+    """ SqlAlchemy Plugin. """
+
+    name = 'sqlalchemy'
+    defaults = {
+        'database_uri': 'sqlite:///muffin.db',
+    }
+
+    def __init__(self, *args, **kwargs):
+        """ Initialize the Plugin. """
+        super().__init__(*args, **kwargs)
+
+    def setup(self, app):
+        """ Setup self. """
+        super().setup(app)
+
+        self.sqlalchemy_engine = create_engine(self.cfg.database_uri)
+        self.session_builder = sessionmaker(bind=self.sqlalchemy_engine)
+
+        @app.manage.command
+        def create_database():
+            SqlAlchemyDeclarativeBase.metadata.create_all(
+                self.sqlalchemy_engine
+            )
+            print('OK')
+
+    @asyncio.coroutine
+    def sqlalchemy_middleware(self, app, handler):
+        """Set session from request."""
+
+        @asyncio.coroutine
+        def middleware(request):
+            request.sqlalchemy_session = self.session_builder()
+            response = yield from handler(request)
+            request.sqlalchemy_session.close()
+            return response
+
+        return middleware
+
+    def start(self, app):
+        """ Start plugin. """
+        app.middlewares.insert(0, self.sqlalchemy_middleware)
+
+    def finish(self, app):
+        """ Finish plugin. """
+        pass
