682d813669b6b697fe5aabac68f6b24a21452fd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:36 +0000

    Add package culqipy/0.2.0/culqipy-0.2.0.tar.gz

---
+++ packages/culqipy/culqipy-0.2.0.tar.gz/culqipy-0.2.0/culqipy/__init__.py
@@ -0,0 +1,15 @@
+# Resource
+
+from culqipy.resource import (
+    Token,
+    Charge,
+    Plan,
+    Subscription,
+    Refund
+)
+
+# Configuration variables
+
+API_URL = "https://api.culqi.com/v2"
+COD_COMMERCE = None
+API_KEY = None
