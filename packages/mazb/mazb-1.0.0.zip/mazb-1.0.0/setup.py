f96f98d107b3a0a583476296e2bac4dfcfc5b1c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:46 +0000

    Add package mazb/1.0.0/mazb-1.0.0.zip

---
+++ packages/mazb/mazb-1.0.0.zip/mazb-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='mazb',
+    version='1.0.0',
+    py_modules=['mazb'],
+    author='mazb',
+    author_email='mzb1113@163.com',
+    description='A simple lists',
+    )
