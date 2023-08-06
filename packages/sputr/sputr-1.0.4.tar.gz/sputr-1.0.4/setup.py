39ed38cc14f6fd876b038c03d1563e03d7afa101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:20 +0000

    Add package sputr/1.0.4/sputr-1.0.4.tar.gz

---
+++ packages/sputr/sputr-1.0.4.tar.gz/sputr-1.0.4/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import importlib
+
+version = importlib.import_module('sputr.config').version
+
+setup(
+    name='sputr',
+    version=version,
+    description='Simple Python Unit Test Runner',
+    author='Matt Wisniewski',
+    author_email='sputr@mattw.us',
+    license='MIT',
+    url='https://github.com/polishmatt/sputr',
+    keywords=['testing'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+    packages=[
+        'sputr'
+    ],
+    install_requires=[
+        'click==6.6'
+    ],
+    entry_points={
+        'console_scripts': [
+            'sputr = sputr.cli:cli'
+        ],
+    },
+)
