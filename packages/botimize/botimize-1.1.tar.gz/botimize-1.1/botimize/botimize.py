8de1064e04577d893715ebd704abbc8bb24e9c58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:33 +0000

    Add package botimize/1.1/botimize-1.1.tar.gz

---
+++ packages/botimize/botimize-1.1.tar.gz/botimize-1.1/botimize/botimize.py
@@ -0,0 +1,48 @@
+import requests
+
+API_URL = 'https://api.botimize.io'
+
+class Botimize:
+    def __init__(self, apiKey, platform):
+
+        self.apiKey = apiKey
+        self.platform = platform
+        self.apiUrl = API_URL
+
+    def log_incoming(self, data):
+        uri = self.apiUrl + '/messages'
+        auth = {
+            'apikey': self.apiKey
+        }
+        options = {
+          'tag': 'unknown',
+          'platform': self.platform,
+          'direction': 'incoming',
+          'raw': data
+        }
+        response = requests.post(
+            uri,
+            params=auth,
+            json=options
+        )
+        result = response.json()
+        return result
+
+    def log_outgoing(self, data):
+        uri = self.apiUrl + '/messages'
+        auth = {
+            'apikey': self.apiKey
+        }
+        options = {
+          'tag': 'unknown',
+          'platform': self.platform,
+          'direction': 'outgoing',
+          'raw': data
+        }
+        response = requests.post(
+            uri,
+            params=auth,
+            json=options
+        )
+        result = response.json()
+        return result
