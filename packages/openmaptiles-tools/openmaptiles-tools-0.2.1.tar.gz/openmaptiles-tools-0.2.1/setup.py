4a7cb00673c5146d11ac1ff9936d62a8c488ff18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:10 +0000

    Add package openmaptiles-tools/0.2.1/openmaptiles-tools-0.2.1.tar.gz

---
+++ packages/openmaptiles-tools/openmaptiles-tools-0.2.1.tar.gz/openmaptiles-tools-0.2.1/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name='openmaptiles-tools',
+    version='0.2.1',
+    packages=['openmaptiles'],
+    license='MIT',
+    long_description=open('README.md').read(),
+    scripts=[
+        'bin/generate-doc',
+        'bin/generate-etlgraph',       
+        'bin/generate-imposm3',
+        'bin/generate-mapping-graph',       
+        'bin/generate-metadata',        
+        'bin/generate-sql',
+        'bin/generate-sqlquery',        
+        'bin/generate-tm2source',
+    ],
+    install_requires=[
+      'docopt',
+      'pyyaml',
+      'graphviz',
+    ],
+)
