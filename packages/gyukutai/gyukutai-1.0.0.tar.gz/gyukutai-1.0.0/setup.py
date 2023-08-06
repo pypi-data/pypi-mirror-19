4cdcab85b61ba15dddf01dd5c2a085ffd84e03ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:13 +0000

    Add package gyukutai/1.0.0/gyukutai-1.0.0.tar.gz

---
+++ packages/gyukutai/gyukutai-1.0.0.tar.gz/gyukutai-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gyukutai',
+    version='1.0.0',
+    packages=['gyukutai'],
+    url='https://github.com/SunDwarf/gyukutai.git',
+    license='MIT',
+    author='Laura Dickinson',
+    author_email='l@veriny.tf',
+    description='Hurting CPython deeply',
+    install_requires=["forbiddenfruit"]
+)
