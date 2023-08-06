7cf0a57ef568a9a9edc2d7ad21d9b823d7218282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:18 +0000

    Add package iati.validate/0.0.1.dev0/iati.validate-0.0.1.dev0.tar.gz

---
+++ packages/iati.validate/iati.validate-0.0.1.dev0.tar.gz/iati.validate-0.0.1.dev0/iati/__init__.py
@@ -0,0 +1,2 @@
+def validate():
+    return NotImplemented
---
+++ packages/iati.validate/iati.validate-0.0.1.dev0.tar.gz/iati.validate-0.0.1.dev0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'iati.validate',
+    version = '0.0.1dev',
+    description = 'A placeholder package for the upcoming iati.* software libraries',
+    author = 'IATI Technical Team and other contributors',
+    author_email = 'code@iatistandard.org',
+    licence = 'MIT',
+    packages = find_packages(exclude='tests')
+)
