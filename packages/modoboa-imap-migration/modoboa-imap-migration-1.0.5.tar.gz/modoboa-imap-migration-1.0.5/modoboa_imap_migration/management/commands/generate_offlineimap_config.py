0b878639b21bfab3a1bc289c9c651d3dae68c341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:08 +0000

    Add package modoboa-imap-migration/1.0.5/modoboa-imap-migration-1.0.5.tar.gz

---
+++ packages/modoboa-imap-migration/modoboa-imap-migration-1.0.5.tar.gz/modoboa-imap-migration-1.0.5/modoboa_imap_migration/management/commands/generate_offlineimap_config.py
@@ -0,0 +1,43 @@
+"""
+A management command to create an offlineimap configuration file.
+"""
+
+from optparse import make_option
+
+from django.core.management.base import BaseCommand
+from django.template.loader import render_to_string
+
+from modoboa.admin.app_settings import load_admin_settings
+from modoboa.lib import parameters
+
+
+from ...modo_extension import ImapMigration
+from ...models import Migration
+
+
+class Command(BaseCommand):
+
+    """Command definition."""
+
+    help = "Generate an offlineimap configuration file."
+
+    option_list = BaseCommand.option_list + (
+        make_option("--output", default="/tmp/offlineimap.conf",
+                    help="Path of the generated file"),
+    )
+
+    def handle(self, *args, **options):
+        """Entry point."""
+        load_admin_settings()
+        ImapMigration().load()
+
+        context = {
+            "imap_server_address": parameters.get_admin("SERVER_ADDRESS"),
+            "imap_server_port": parameters.get_admin("SERVER_PORT"),
+            "imap_server_secured": parameters.get_admin("SECURED"),
+            "migrations": Migration.objects.select_related().all(),
+        }
+        with open(options["output"], "w") as fpo:
+            content = render_to_string(
+                "modoboa_imap_migration/offlineimap.conf", context)
+            fpo.write(content)
