3c8337fb85d420f7cd941b756b16730a896a3c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:48 +0000

    Add package britney/0.5.1/britney-0.5.1.tar.gz

---
+++ packages/britney/britney-0.5.1.tar.gz/britney-0.5.1/britney/middleware/format.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+"""
+britney.middleware.format
+~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2013 - Arnaud Grausem
+:licence: BSD see LICENSE for details
+"""
+
+__all__ = ['Json']
+
+
+import abc
+import json
+
+from . import base
+
+
+class Format(base.Middleware):
+    """
+    """
+
+    __metaclass__ = abc.ABCMeta
+
+    content_type = ''
+    accept = ''
+
+    @abc.abstractmethod
+    def dump(self, data):
+        """
+        """
+        pass
+
+    @abc.abstractmethod
+    def load(self, data):
+        """
+        """
+        pass
+
+    def content_length(self, content):
+        """
+        :param content:
+        :return: content length header information
+        """
+        return str(len(content))
+
+    def process_request(self, environ):
+        base.add_header(environ, 'Accept', self.accept)
+        if environ['spore.payload'] and not environ['spore.payload_format']:
+            self.process_payload(environ)
+
+    def process_payload(self, environ):
+        """
+        """
+        payload = self.dump(environ['spore.payload'])
+        environ['spore.payload'] = payload
+        base.add_header(environ, 'Content-Length', self.content_length(payload))
+        base.add_header(environ, 'Content-Type', self.content_type)
+
+    def process_response(self, response):
+        response.data = self.load(response.text) if response.text else {}
+        return response
+
+
+class Json(Format):
+    """
+    """
+
+    content_type = 'application/json'
+    accept = 'application/json'
+
+    def dump(self, data):
+        return json.dumps(data)
+
+    def load(self, data):
+        return json.loads(data)
