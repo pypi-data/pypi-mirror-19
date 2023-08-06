f864e69de8923e21a143613b5d1dca7c1909fe31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:22 +0000

    Add package imblearn/0.0/imblearn-0.0.tar.gz

---
+++ packages/imblearn/imblearn-0.0.tar.gz/imblearn-0.0/setup.py
@@ -0,0 +1,20 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='UNKNOWN',
+    author_email='UNKNOWN',
+    description='Toolbox for imbalanced dataset in machine learning.',
+    install_requires=['imbalanced-learn'],
+    long_description='''Use `imbalanced-learn <https://pypi.python.org/pypi/imbalanced-learn/>`_ instead.''',
+    maintainer='UNKNOWN',
+    maintainer_email='UNKNOWN',
+    name='imblearn',
+    platforms=['all'],
+    py_modules=['wheel-platform-tag-is-broken-on-empty-wheels-see-issue-141'],
+    url='https://pypi.python.org/pypi/imbalanced-learn/',
+    version="0.0",
+    zip_safe=False,
+)
