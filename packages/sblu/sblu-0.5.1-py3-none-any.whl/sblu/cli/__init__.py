47f0a8d3d8bac4d2cafc5e202549d8b1ea6da5ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:42 +0000

    Add package sblu/0.5.1/sblu-0.5.1-py3-none-any.whl

---
+++ packages/sblu/sblu-0.5.1-py3-none-any.whl/sblu/cli/__init__.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import click
+
+CONTEXT_SETTINGS = dict(auto_envvar_prefix='COMPLEX')
+
+
+def make_cli_class(package):
+    cmd_folder = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                              package))
+
+    class ComplexCLI(click.MultiCommand):
+
+        def list_commands(self, ctx):
+            rv = []
+            for filename in os.listdir(cmd_folder):
+                if filename.endswith('.py') and \
+                   filename.startswith('cmd_'):
+                    rv.append(filename[4:-3])
+            rv.sort()
+            return rv
+
+        def get_command(self, ctx, name):
+            try:
+                if sys.version_info[0] == 2:
+                    name = name.encode('ascii', 'replace')
+                mod = __import__('sblu.cli.{0}.cmd_{1}'.format(package, name),
+                                 None, None, ['cli'])
+            except ImportError:
+                return
+            return mod.cli
+
+    return ComplexCLI
