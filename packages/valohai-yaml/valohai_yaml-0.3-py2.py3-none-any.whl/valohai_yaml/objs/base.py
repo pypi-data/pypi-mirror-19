c9779af1f916be968dddd1d9d4c5f6ed236a8b66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:01 +0000

    Add package valohai-yaml/0.3/valohai_yaml-0.3-py2.py3-none-any.whl

---
+++ packages/valohai-yaml/valohai_yaml-0.3-py2.py3-none-any.whl/valohai_yaml/objs/base.py
@@ -0,0 +1,13 @@
+class _SimpleObject(object):
+    def get_data(self):
+        """
+        Get data for serialization.
+        """
+        return vars(self)
+
+    def serialize(self):
+        return {key.replace('_', '-'): value for (key, value) in self.get_data().items() if value is not None}
+
+    @classmethod
+    def parse(cls, data):
+        return cls(**{key.replace('-', '_'): value for (key, value) in data.items()})
