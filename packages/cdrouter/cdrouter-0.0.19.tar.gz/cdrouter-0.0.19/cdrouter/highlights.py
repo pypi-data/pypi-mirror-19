50529582220d05d42baae0feaf9b0c0a6eef50bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:51 +0000

    Add package cdrouter/0.0.19/cdrouter-0.0.19.tar.gz

---
+++ packages/cdrouter/cdrouter-0.0.19.tar.gz/cdrouter-0.0.19/cdrouter/highlights.py
@@ -0,0 +1,68 @@
+#
+# Copyright (c) 2017 by QA Cafe.
+# All Rights Reserved.
+#
+
+"""Module for accessing CDRouter Highlights."""
+
+from marshmallow import Schema, fields, post_load
+
+class Highlight(object):
+    def __init__(self, **kwargs):
+        self.id = kwargs.get('id', None)
+        self.seq = kwargs.get('seq', None)
+        self.line = kwargs.get('line', None)
+        self.color = kwargs.get('color', None)
+
+class HighlightSchema(Schema):
+    id = fields.Str()
+    seq = fields.Str()
+    line = fields.Str()
+    color = fields.Str()
+
+    @post_load
+    def post_load(self, data):
+        return Highlight(**data)
+
+class HighlightsService(object):
+    """Service for accessing CDRouter Highlights."""
+
+    RESOURCE = 'highlights'
+    BASE = RESOURCE + '/'
+
+    def __init__(self, service):
+        self.service = service
+
+    def _base(self, id, seq): # pylint: disable=invalid-name,redefined-builtin
+        return 'results/'+str(id)+'/tests/'+str(seq)+self.BASE
+
+    def list(self, id, seq): # pylint: disable=invalid-name,redefined-builtin
+        """Get a list of highlights."""
+        schema = HighlightSchema(exclude=('id', 'seq'))
+        resp = self.service.list(self._base(id, seq))
+        return self.service.decode(schema, resp, many=True)
+
+    def get(self, id, seq, line): # pylint: disable=invalid-name,redefined-builtin
+        """Get a highlight."""
+        schema = HighlightSchema()
+        resp = self.service.get_id(self._base(id, seq), line)
+        return self.service.decode(schema, resp)
+
+    def create_or_edit(self, id, seq, resource): # pylint: disable=invalid-name,redefined-builtin
+        """Create or edit a highlight."""
+        schema = HighlightSchema(exclude=('id', 'seq'))
+        json = self.service.encode(schema, resource)
+
+        schema = HighlightSchema()
+        resp = self.service.edit(self._base(id, seq), resource.line, json)
+        return self.service.decode(schema, resp)
+
+    def create(self, id, seq, resource): # pylint: disable=invalid-name,redefined-builtin
+        return self.create_or_edit(id, seq, resource)
+
+    def edit(self, id, seq, resource): # pylint: disable=invalid-name,redefined-builtin
+        return self.create_or_edit(id, seq, resource)
+
+    def delete(self, id, seq, line): # pylint: disable=invalid-name,redefined-builtin
+        """Get a highlight."""
+        return self.service.delete_id(self._base(id, seq), line)
---
+++ packages/cdrouter/cdrouter-0.0.19.tar.gz/cdrouter-0.0.19/cdrouter/history.py
@@ -0,0 +1,48 @@
+#
+# Copyright (c) 2017 by QA Cafe.
+# All Rights Reserved.
+#
+
+"""Module for accessing CDRouter History."""
+
+from marshmallow import Schema, fields, post_load
+from .cdr_datetime import DateTime
+
+class History(object):
+    def __init__(self, **kwargs):
+        self.user_id = kwargs.get('user_id', None)
+        self.created = kwargs.get('created', None)
+        self.resource = kwargs.get('resource', None)
+        self.id = kwargs.get('id', None)
+        self.name = kwargs.get('name', None)
+        self.action = kwargs.get('action', None)
+        self.description = kwargs.get('description', None)
+
+class HistorySchema(Schema):
+    user_id = fields.Str()
+    created = DateTime()
+    resource = fields.Str()
+    id = fields.Str()
+    name = fields.Str()
+    action = fields.Str()
+    description = fields.Str()
+
+    @post_load
+    def post_load(self, data):
+        return History(**data)
+
+class HistoryService(object):
+    """Service for accessing CDRouter History."""
+
+    RESOURCE = 'history'
+    BASE = RESOURCE + '/'
+
+    def __init__(self, service):
+        self.service = service
+        self.base = self.BASE
+
+    def list(self, filter=None, type=None, sort=None, limit=None, page=None): # pylint: disable=redefined-builtin
+        """Get a list of history entries."""
+        schema = HistorySchema()
+        resp = self.service.list(self.base, filter, type, sort, limit, page)
+        return self.service.decode(schema, resp, many=True)
