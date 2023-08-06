cfce163acd852a8b62cf891729d3bd321457f8a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:22 +0000

    Add package demographer-popgen/0.0.0.dev2/demographer_popgen-0.0.0.dev2.tar.gz

---
+++ packages/demographer-popgen/demographer_popgen-0.0.0.dev2.tar.gz/demographer_popgen-0.0.0.dev2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='demographer_popgen',
+      version='0.0.0.dev2',
+      description='DemoGrapher: Software for graphically studying population histories.',
+      url='https://github.com/ejewett/demographer',
+      author='Ethan Jewett',
+      author_email='ejewett@gmail.com',
+      license='GPL3.0',
+      packages=['demographer_popgen'],
+      install_requires=['division','mpmath','numpy','scipy','matplotlib','ast','json','PIL'],
+      classifiers=['Development Status :: 3 - Alpha','Intended Audience :: End Users/Desktop','License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)','Programming Language :: Python :: 2.7','Topic :: Scientific/Engineering :: Bio-Informatics',],
+      zip_safe=False)
