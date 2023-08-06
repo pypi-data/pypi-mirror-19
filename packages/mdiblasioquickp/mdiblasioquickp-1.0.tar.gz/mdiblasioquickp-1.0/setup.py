f53bb7e2c03c9daa9e4253a4f5dc932308a78068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:38 +0000

    Add package mdiblasioquickp/1.0/mdiblasioquickp-1.0.tar.gz

---
+++ packages/mdiblasioquickp/mdiblasioquickp-1.0.tar.gz/mdiblasioquickp-1.0/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(name="mdiblasioquickp",
+	version="1.0",
+	description="Test quickpackage",
+	url="https://github.com/mdiblasio/quickp",
+	author="mdiblasio",
+	author_email="mdiblasio72@gmail.com",
+	license='MIT',
+	packages=["mdiblasioquickp"],
+	scripts=["bin/mdiblasioquickp"],
+	zip_safe=False)
