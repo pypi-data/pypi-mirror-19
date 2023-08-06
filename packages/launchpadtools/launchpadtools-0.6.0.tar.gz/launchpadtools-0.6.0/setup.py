b1381537a93b14b2ec49944275805f50fa4d9b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:17 +0000

    Add package launchpadtools/0.6.0/launchpadtools-0.6.0.tar.gz

---
+++ packages/launchpadtools/launchpadtools-0.6.0.tar.gz/launchpadtools-0.6.0/setup.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+#
+import os
+from distutils.core import setup
+import codecs
+
+# https://packaging.python.org/single_source_version/
+base_dir = os.path.abspath(os.path.dirname(__file__))
+about = {}
+with open(os.path.join(base_dir, 'launchpadtools', '__about__.py')) as f:
+    exec(f.read(), about)
+
+
+def read(fname):
+    try:
+        content = codecs.open(
+            os.path.join(os.path.dirname(__file__), fname),
+            encoding='utf-8'
+            ).read()
+    except Exception:
+        content = ''
+    return content
+
+setup(
+    name='launchpadtools',
+    version=about['__version__'],
+    author=about['__author__'],
+    author_email=about['__author_email__'],
+    packages=['launchpadtools'],
+    description='Tools for Debian/Ubuntu Launchpad',
+    long_description=read('README.rst'),
+    url='https://github.com/nschloe/launchpadtools',
+    download_url='https://pypi.python.org/pypi/launchpadtools',
+    license='License :: OSI Approved :: MIT License',
+    platforms='any',
+    install_requires=[
+        'GitPython',
+        'launchpadlib',
+        ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: System :: Operating System'
+        ],
+    scripts=[
+        'tools/launchpad-submit',
+        ]
+    )
