25048059137695824f8456e345a319dcca3a4645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:41 +0000

    Add package youtunes/0.6/youtunes-0.6.tar.gz

---
+++ packages/youtunes/youtunes-0.6.tar.gz/youtunes-0.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+setup(
+  name = 'youtunes',
+  packages = ['main'],
+  version = '0.6',
+  description = 'Multithreaded direct download from youtube to mp3',
+  author = 'Andrew Chang',
+  author_email = 'achang97@stanford.edu',
+  license = 'MIT',
+  url = 'https://github.com/achang97/YouTunes',
+  download_url = 'https://github.com/achang97/youtunes/tarball/0.6',
+  keywords = ['download', 'YouTube', 'iTunes'],
+  classifiers = [
+    'Operating System :: MacOS',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2.7'
+  ],
+  py_modules=['main.py'],
+  scripts=['youtunes'],
+  install_requires = [
+        'youtube-dl',
+        'beautifulsoup4==4.4.1',
+        'eyeD3==0.7.10',
+        'requests==2.9.1',
+        'ffprobe==0.5'
+  ]
+)
