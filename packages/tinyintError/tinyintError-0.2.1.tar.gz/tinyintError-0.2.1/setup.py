e8f64910d59595f266583ad313d1862a94cfb551
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:18 +0000

    Add package tinyintError/0.2.1/tinyintError-0.2.1.tar.gz

---
+++ packages/tinyintError/tinyintError-0.2.1.tar.gz/tinyintError-0.2.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+	name = "tinyintError",
+	packages = ["package"],
+	version = "0.2.1",
+	description = "Este paquete nos permite validar errores de tipo tinyInt",
+	author = "Eduardo Ismael",
+	author_email = "eduardo@codigofacilito.com",
+	url = "",
+	keyword = ["error", "int", "tinyInt"]
+
+)
