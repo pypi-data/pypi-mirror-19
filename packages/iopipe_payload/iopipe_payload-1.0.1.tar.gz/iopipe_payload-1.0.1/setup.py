ff5caa69cd08646708a6f4b7fea3d4005926d761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:32 +0000

    Add package iopipe_payload/1.0.1/iopipe_payload-1.0.1.tar.gz

---
+++ packages/iopipe_payload/iopipe_payload-1.0.1.tar.gz/iopipe_payload-1.0.1/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from pip.download import PipSession
+from setuptools import setup, find_packages
+
+setup(name='iopipe_payload',
+      version='1.0.1',
+      description='IOpipe payload schema & normalization library',
+      author='IOpipe',
+      author_email='support@iopipe.com',
+      url='https://github.com/iopipe/iopipe-payload',
+      packages=find_packages(),
+      setup_requires=[
+          "flake8"
+      ],
+      extras_require={
+          'dev': [
+              'flake8'
+          ]
+      }
+     )
