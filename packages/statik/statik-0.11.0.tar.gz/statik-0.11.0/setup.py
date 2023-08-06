0bdd6b230b41a3c23724dfb268df703fd109735f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:26 +0000

    Add package statik/0.11.0/statik-0.11.0.tar.gz

---
+++ packages/statik/statik-0.11.0.tar.gz/statik-0.11.0/setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+Setup script for Statik, the static web site generator.
+"""
+
+from setuptools import setup
+from statik import __version__
+
+INSTALL_REQUIREMENTS = [
+    "future>=0.16.0",
+    "jinja2>=2.8",
+    "PyYAML>=3.11",
+    "SQLAlchemy>=1.0.14",
+    "markdown>=2.6.6",
+    "livereload>=2.4.1",
+]
+
+setup(
+    name="statik",
+    version=__version__,
+    description="General-purpose static web site generator",
+    author="Thane Thomson",
+    author_email="connect@thanethomson.com",
+    url="https://getstatik.com",
+    install_requires=INSTALL_REQUIREMENTS,
+    entry_points={
+        'console_scripts': [
+            'statik = statik.cmdline:main',
+        ]
+    },
+    packages=["statik"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Utilities",
+    ]
+)
