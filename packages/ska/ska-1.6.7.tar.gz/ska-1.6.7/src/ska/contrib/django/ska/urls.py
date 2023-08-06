8919f0f587e2fff4c1fa737877dfaf86854184f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:58:59 +0000

    Add package ska/1.6.7/ska-1.6.7.tar.gz

---
+++ packages/ska/ska-1.6.7.tar.gz/ska-1.6.7/src/ska/contrib/django/ska/urls.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+
+from django.conf.urls import url
+
+from .views import login
+
+__title__ = 'ska.contrib.django.ska.urls'
+__author__ = 'Artur Barseghyan'
+__copyright__ = 'Copyright (c) 2013-2017 Artur Barseghyan'
+__license__ = 'GPL 2.0/LGPL 2.1'
+__all__ = ('urlpatterns',)
+
+
+urlpatterns = [
+    url(r'^login/', login, name="ska.login"),
+]
