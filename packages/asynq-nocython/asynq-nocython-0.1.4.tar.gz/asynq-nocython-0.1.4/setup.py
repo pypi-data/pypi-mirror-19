5fded012c84a3391f765bd99dfbfa0c8cdadb3d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:32 +0000

    Add package asynq-nocython/0.1.4/asynq-nocython-0.1.4.tar.gz

---
+++ packages/asynq-nocython/asynq-nocython-0.1.4.tar.gz/asynq-nocython-0.1.4/setup.py
@@ -0,0 +1,49 @@
+# Copyright 2016 Quora, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import setup
+
+import codecs
+
+
+VERSION = '0.1.4'
+
+if __name__ == '__main__':
+    with codecs.open('./README.rst', encoding='utf-8') as f:
+        long_description = f.read()
+
+    setup(
+        name='asynq-nocython',
+        version=VERSION,
+        author='Quora, Inc.',
+        author_email='asynq@quora.com',
+        description='Quora\'s asynq library',
+        long_description=long_description,
+        url='https://github.com/quora/asynq',
+        license='Apache Software License',
+        classifiers=[
+            'License :: OSI Approved :: Apache Software License',
+
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+        ],
+        keywords='quora asynq common utility',
+        packages=['asynq', 'asynq.tests'],
+        install_requires=['qcore-nocython'],
+    )
+
