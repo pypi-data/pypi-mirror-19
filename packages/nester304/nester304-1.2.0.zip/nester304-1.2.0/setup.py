9d1cf8293e0a972bf238c84a952df9681e394a3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:59 +0000

    Add package nester304/1.2.0/nester304-1.2.0.zip

---
+++ packages/nester304/nester304-1.2.0.zip/nester304-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name        ='nester304',
+    version     ='1.2.0',
+    py_modules  =['nester304'],
+    author      ='hyjpython',
+    author_email='yuanjun_study@outlook.com',
+    url         ='http://www.headfirstlabs.com',
+    description ='A simple printer of nested lists',
+)
