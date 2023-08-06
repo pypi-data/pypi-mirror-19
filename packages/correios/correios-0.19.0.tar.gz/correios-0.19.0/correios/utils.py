bc4a98c160418cc223fe8cdecf476f825009ff91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:47 +0000

    Add package correios/0.19.0/correios-0.19.0.tar.gz

---
+++ packages/correios/correios-0.19.0.tar.gz/correios-0.19.0/correios/utils.py
@@ -0,0 +1,34 @@
+from itertools import chain
+from typing import Container, Iterable, Sized
+
+
+class RangeSet(Sized, Iterable, Container):
+    def __init__(self, *ranges):
+        self.ranges = []
+
+        for r in ranges:
+            if isinstance(r, range):
+                self.ranges.append(r)
+                continue
+
+            try:
+                element = list(r.ranges)
+            except AttributeError:
+                element = None
+
+            try:
+                element = element or [range(*r)]
+            except:
+                msg = "RangeSet argument must be a range, RangeSet or an Iterable, not {}"
+                raise ValueError(msg.format(type(r)))
+
+            self.ranges.extend(element)
+
+    def __iter__(self):
+        return chain.from_iterable(r for r in self.ranges)
+
+    def __contains__(self, elem):
+        return any(elem in r for r in self.ranges)
+
+    def __len__(self):
+        return sum(len(r) for r in self.ranges)
