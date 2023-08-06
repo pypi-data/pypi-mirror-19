0687a3dad90bad8e108913d4b2b336d99c935872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:43 +0000

    Add package emergenSEE/0.9.4/emergenSEE-0.9.4.tar.gz

---
+++ packages/emergenSEE/emergenSEE-0.9.4.tar.gz/emergenSEE-0.9.4/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+  name = 'emergenSEE',
+  packages = ['emergenSEE'],
+  version = '0.9.4',
+  description = 'Visualize NYC EMS Emergencies',
+  author = 'Max Mattioli',
+  author_email = 'm.max@columbia.edu',
+  url = 'https://github.com/m-a-x/emergenSEE',
+  download_url = 'https://github.com/m-a-x/emergenSEE/tarball/0.9.4',
+  license='MIT',
+  install_requires=['pandas>=0.19.1', 'numpy>=1.11.3', 'matplotlib>=1.5.3', 'descartes>=1.0.2', 'scikit-learn>=0.18.1', 'imageio==1.6'],
+  package_data={'emergenSEE': ['data/*.pkl']},
+  classifiers = []
+)
