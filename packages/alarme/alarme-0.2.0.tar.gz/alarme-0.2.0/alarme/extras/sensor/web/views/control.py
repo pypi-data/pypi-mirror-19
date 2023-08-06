ee3f06c029ca6549dc7d7003fc95e6c732e8f425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:23 +0000

    Add package alarme/0.2.0/alarme-0.2.0.tar.gz

---
+++ packages/alarme/alarme-0.2.0.tar.gz/alarme-0.2.0/alarme/extras/sensor/web/views/control.py
@@ -0,0 +1,25 @@
+from aiohttp_jinja2 import template
+
+from .core import CoreView, http_found
+from ..util import login_required, handle_exception
+
+
+class Control(CoreView):
+
+    @handle_exception
+    @login_required
+    @template('control.html')
+    async def get(self):
+        self.logger.debug('control_page_view')
+        return {'current_state': self.sensor.app.state,
+                'buttons': sorted(self.sensor.buttons, key=lambda x: x['order'])}
+
+    @handle_exception
+    @login_required
+    @http_found
+    async def post(self):
+        data = await self.request.post()
+        behaviour = data['behaviour']
+        futures = await self.sensor.notify(behaviour)
+        for future in (futures or []):
+            await future
