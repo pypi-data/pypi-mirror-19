8e6d58c3a77168f367c7f91831cb489b9b2487a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:08 +0000

    Add package Flask-Hooker/1.0/Flask-Hooker-1.0.tar.gz

---
+++ packages/Flask-Hooker/Flask-Hooker-1.0.tar.gz/Flask-Hooker-1.0/setup.py
@@ -0,0 +1,36 @@
+"""
+Flask-Hooker
+--------------
+"""
+
+from setuptools import setup
+
+setup(
+    name='Flask-Hooker',
+    version=1.0,
+    url='http://github.com/doblel/Flask-Hooker/',
+    license='MIT',
+    author='Lisandro Lucatti (doblel)',
+    author_email='lucattilisandro@gmail.com',
+    description='Receive and manage webhooks of several services at the same time',
+    long_description='',
+    packages=['flask_hooker'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    entry_points={},
+    install_requires=['Flask'],
+    tests_require=[],
+    test_suite='t',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
