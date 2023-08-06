cfed780501a870401e534328f49ecd912cc87801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:29:47 +0000

    Add package marginAlign/1.1.3/marginAlign-1.1.3.tar.gz

---
+++ packages/marginAlign/marginAlign-1.1.3.tar.gz/marginAlign-1.1.3/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name="marginAlign",
+      version="1.1.3",
+      description="Toil-based functions for performing MinION sequence analysis",
+      author="Benedict Paten and Art Rand",
+      author_email="benedict@soe.ucsc.edu, arand@soe.ucsc.edu",
+      url="https://github.com/ArtRand/marginAlign",
+      package_dir={"": "src"},
+      packages=find_packages("src"),
+      install_requires=["PyVCF==0.6.7",
+                        "numpy==1.9.2",
+                        "pysam==0.8.2.1",
+                        "pandas==0.18.1"]
+      )
