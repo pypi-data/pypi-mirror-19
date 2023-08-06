5c68cd5b8e3a815947247041384449319591d2d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:59 +0000

    Add package kinto-emailer/0.1.0/kinto-emailer-0.1.0.tar.gz

---
+++ packages/kinto-emailer/kinto-emailer-0.1.0.tar.gz/kinto-emailer-0.1.0/CHANGELOG.rst
@@ -0,0 +1,12 @@
+Changelog
+=========
+
+This document describes changes between each past release.
+
+0.1.0 (2017-01-25)
+------------------
+
+**Initial version**
+
+- Use a list of hooks to configure emails bound to notifications (fixes #11)
+- Support *kinto-signer* events (fixes #14)
---
+++ packages/kinto-emailer/kinto-emailer-0.1.0.tar.gz/kinto-emailer-0.1.0/setup.py
@@ -0,0 +1,42 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read_file(filename):
+    """Open a related file and return its content."""
+    with codecs.open(os.path.join(here, filename), encoding='utf-8') as f:
+        content = f.read()
+    return content
+
+README = read_file('README.rst')
+CHANGELOG = read_file('CHANGELOG.rst')
+CONTRIBUTORS = read_file('CONTRIBUTORS.rst')
+
+
+REQUIREMENTS = [
+    'kinto>5',
+    'pyramid<1.8',
+    'pyramid_mailer',
+]
+
+setup(name='kinto-emailer',
+      version='0.1.0',
+      description='Kinto emailer plugin',
+      long_description=README + "\n\n" + CHANGELOG + "\n\n" + CONTRIBUTORS,
+      license='Apache License (2.0)',
+      classifiers=[
+          "Programming Language :: Python",
+          "Topic :: Internet :: WWW/HTTP",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+          "License :: OSI Approved :: Apache Software License"
+      ],
+      keywords="web services",
+      author='Mozilla Services',
+      author_email='developers@kinto-storage.org',
+      url='https://github.com/Kinto/kinto-emailer',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=REQUIREMENTS)
