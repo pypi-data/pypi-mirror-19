008e0d340890f3b541ff6ea3a2fe7d9eb13d1954
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:04 +0000

    Add package nester_lxp/1.3.0/nester_lxp-1.3.0.zip

---
+++ packages/nester_lxp/nester_lxp-1.3.0.zip/nester_lxp-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+		name	='nester_lxp',
+		version	='1.3.0',
+		py_modules	=['nester_lxp'],
+		author	='Sampson',
+		author_email	='li.xin.peng@qq.com',
+		url		=' ',
+		description	='A simple printer of nested list',
+		)
