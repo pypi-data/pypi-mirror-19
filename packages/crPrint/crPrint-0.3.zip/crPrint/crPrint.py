d54b4cae018848c067932b8be86ca08908025f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:45 +0000

    Add package crPrint/0.3/crPrint-0.3.zip

---
+++ packages/crPrint/crPrint-0.3.zip/crPrint/crPrint.py
@@ -0,0 +1,58 @@
+#-*- coding: utf-8 -*-
+""" 
+    Colors:
+        Green
+        Pink
+        Red
+        Blue
+        Cyan
+        Yellow
+        Gray
+
+
+    Example:
+    --------
+
+        >>> from crPrint import cprint
+        >>>  cprint('OLA MUNDO','Green')
+        OLA MUNDO
+
+   © Copyright 2017 João Mário
+
+"""
+
+__author__ = 'João Mário'
+__mail__ = 'joaok63@outlook.com'
+
+__copyright__ = '© 2017 João Mário'
+__license__ = 'GPL'
+
+
+def cprint(texto='',cor=''):
+
+    if cor == 'Red':
+        print(('\033[91m%s\033[0m')% (texto))
+
+    elif cor == 'Yellow':
+        print(('\033[93m%s\033[0m')% (texto))
+
+    elif cor == 'Green':
+        print(('\033[92m%s\033[0m')% (texto))
+
+    elif cor == 'Pink':
+        print(('\033[95m%s\033[0m')% (texto))
+
+    elif cor == 'Blue':
+        print(('\033[94m%s\033[0m')% (texto))
+
+    elif cor == 'Cyan':
+        print(('\033[96m%s\033[0m')% (texto))
+
+    elif cor == 'Gray':
+        print(('\033[90m%s\033[0m')% (texto))
+
+    else:
+        print(texto)
+
+if __name__ == '__main__':
+  exit() 
---
+++ packages/crPrint/crPrint-0.3.zip/crPrint/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import crPrint
+
+setup(
+        name='crPrint',
+        version='0.3',
+        description='Print text with color',
+        long_description = 'crPrint',
+        author='João Mário',
+        author_email='joaok63@outlook.com',
+        url = 'https://twitter.com/joaok63',
+        license = 'GPLv3',
+        py_modules = ['crPrint'],
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Console',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            ],
+        keywords='color colour print console',
+
+
+)
