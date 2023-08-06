c426775ee846be4a769251e54dadfb8866c96373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:21 +0000

    Add package ComicThief/0.1.9/ComicThief-0.1.9.tar.gz

---
+++ packages/ComicThief/ComicThief-0.1.9.tar.gz/ComicThief-0.1.9/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+
+setup(
+    name='ComicThief',
+    version='0.1.9',
+    description='Comic Books Scraper. Easy download comics and make .cbz files.',
+    long_description=readme,
+    author='Nuncjo',
+    author_email='zoreander@gmail.com',
+    url='https://github.com/nuncjo/comicthief',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    package_data={'': ['*.*']},
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: Unix",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+    platforms=["Windows", "MacOS", "Unix"],
+    install_requires=['requests', 'tqdm', 'lxml']
+)
