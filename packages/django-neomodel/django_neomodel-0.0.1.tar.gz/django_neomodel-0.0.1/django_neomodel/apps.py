34f1170ccc53c98f1abd1d8d3d58b157d5df9c17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:33 +0000

    Add package django-neomodel/0.0.1/django_neomodel-0.0.1.tar.gz

---
+++ packages/django-neomodel/django_neomodel-0.0.1.tar.gz/django_neomodel-0.0.1/django_neomodel/apps.py
@@ -0,0 +1,18 @@
+from django.apps import AppConfig
+from django.conf import settings
+from neomodel import config
+
+
+config.AUTO_INSTALL_LABELS = False
+
+
+class NeomodelConfig(AppConfig):
+    name = 'django_neomodel'
+    verbose_name = 'Django neomodel'
+
+    def read_settings(self):
+        config.DATABASE_URL = getattr(settings, 'NEOMODEL_NEO4J_BOLT_URL', config.DATABASE_URL)
+        config.FORCE_TIMEZONE = getattr(settings, 'NEOMODEL_FORCE_TIMEZONE', False)
+
+    def ready(self):
+        self.read_settings()
