0fe1eea3da9631b1d9711dd4870653783c84eca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:38 +0000

    Add package backup-all-my-flickr-photos/0.5/backup_all_my_flickr_photos-0.5.tar.gz

---
+++ packages/backup-all-my-flickr-photos/backup_all_my_flickr_photos-0.5.tar.gz/backup_all_my_flickr_photos-0.5/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+version='0.5'
+
+setup(
+    name='backup_all_my_flickr_photos',
+    version=version,
+    description="A script to download all photos and videos in your Flickr account",
+    author="David D Lowe",
+    author_email="daviddlowe.flimm@gmail.com",
+    url="https://github.com/Flimm/backup-all-my-flickr-photos",
+    download_url="https://github.com/Flimm/backup-all-my-flickr-photos/tarball/v%s" % (version,),
+    scripts=['bin/backup-all-my-flickr-photos'],
+    install_requires=[
+        'flickr_api',
+        'humanfriendly',
+    ],
+    license='bsd',
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Environment :: Console',
+    ],
+)
