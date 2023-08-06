cdc87add10b0c4c5c2068d29ce330566c48f7e65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:40 +0000

    Add package pyapp/0.1.0/pyApp-0.1.0.tar.gz

---
+++ packages/pyapp/pyApp-0.1.0.tar.gz/pyApp-0.1.0/pyapp/checks/__init__.py
@@ -0,0 +1,22 @@
+"""
+*Provides an interface and reports of simple pre-flight sanity checks for
+ your application.*
+
+"""
+from __future__ import absolute_import, unicode_literals
+
+from .messages import (
+    CheckMessage,
+    DEBUG, INFO, WARNING, ERROR, CRITICAL,
+    Debug, Info, Warn, Error, Critical,
+)
+from .registry import register, run_checks, Tags
+from .built_in import security  # NOQA isort:skip
+
+
+__all__ = [
+    'CheckMessage',
+    'DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL',
+    'Debug', 'Info', 'Warn', 'Error', 'Critical',
+    'register', 'run_checks', 'Tags'
+]
