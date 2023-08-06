62cd585c70d8d523277abe6ce2de1618b99c8d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:42 +0000

    Add package nodeconductor-aws/0.2.0/nodeconductor-aws-0.2.0.tar.gz

---
+++ packages/nodeconductor-aws/nodeconductor-aws-0.2.0.tar.gz/nodeconductor-aws-0.2.0/src/nodeconductor_aws/filters.py
@@ -0,0 +1,23 @@
+import django_filters
+
+from nodeconductor.structure import filters as structure_filters
+
+from . import models
+
+
+class ImageFilter(structure_filters.BaseServicePropertyFilter):
+
+    class Meta:
+        model = models.Image
+        fields = structure_filters.BaseServicePropertyFilter.Meta.fields + ('region',)
+
+    region = django_filters.UUIDFilter(name='region__uuid')
+
+
+class SizeFilter(structure_filters.BaseServicePropertyFilter):
+
+    class Meta:
+        model = models.Size
+        fields = structure_filters.BaseServicePropertyFilter.Meta.fields + ('region',)
+
+    region = django_filters.UUIDFilter(name='regions__uuid')
