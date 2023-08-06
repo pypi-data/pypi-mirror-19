d5b0a0fec22952dd0a1e687cf4044dd228be70bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:53 +0000

    Add package draft-kings/0.6/draft_kings-0.6.tar.gz

---
+++ packages/draft-kings/draft_kings-0.6.tar.gz/draft_kings-0.6/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup, find_packages
+setup(
+  name = 'draft_kings',
+  packages = find_packages(exclude=['tests*']),
+  install_requires=['requests', 'enum34'],
+  version = '0.6',
+  description = 'A DraftKings client',
+  author = 'Jae Bradley',
+  author_email = 'jae.b.bradley@gmail.com',
+  url = 'https://github.com/jaebradley/draftkings_client', # use the URL to the github repo
+  download_url = 'https://github.com/jaebradley/draftkings_client/tarball/0.2', # I'll explain this in a second
+  keywords = ['sports', 'dfs', 'draftkings'], # arbitrary keywords
+  classifiers = [],
+)
