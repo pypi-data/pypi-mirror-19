f60d3221a22f9f5a1b448f4b6c1b51f227f0a301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:25 +0000

    Add package gdtesttask/1.0.0/gdtesttask-1.0.0.tar.gz

---
+++ packages/gdtesttask/gdtesttask-1.0.0.tar.gz/gdtesttask-1.0.0/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from os import path
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
+setup(
+    name="gdtesttask",
+    version="1.0.0",
+    packages=['gdtesttask'],
+    description="Simple python project for jenkins task",
+    long_description=long_description,
+    url="https://github.com/shev2dev/jenkins-test",
+    author="Shevchenko Artem",
+    author_email="shevchenkoaa@sgu.ru",
+    license="MIT",
+    keywords="test jenkins easy"
+)
