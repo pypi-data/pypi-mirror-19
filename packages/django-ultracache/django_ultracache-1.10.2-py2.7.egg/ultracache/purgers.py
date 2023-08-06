0d9c0d7047c75e0976acc910d9567fd84b2066bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:35 +0000

    Add package django-ultracache/1.10.2/django_ultracache-1.10.2-py2.7.egg

---
+++ packages/django-ultracache/django_ultracache-1.10.2-py2.7.egg/ultracache/purgers.py
@@ -0,0 +1,50 @@
+import requests
+
+from django.conf import settings
+
+
+def varnish(path):
+    # See https://www.varnish-software.com/static/book/Cache_invalidation.html
+    loc = settings.ULTRACACHE["purge"]["method"]["url"].rstrip("/") + "/" \
+        + path.lstrip("/")
+    try:
+        r = requests.request("PURGE", loc, timeout=1)
+    except requests.exceptions.RequestException:
+        pass
+
+
+def nginx(path):
+    # See https://github.com/FRiCKLE/ngx_cache_purge
+
+    # Simplest case - one node
+    loc = settings.ULTRACACHE["purge"]["method"]["url"].rstrip("/") + "/" \
+        + path.lstrip("/")
+    try:
+        r = requests.request("PURGE", loc, timeout=1)
+    except requests.exceptions.RequestException:
+        pass
+
+    # More nodes
+    for host in ("196.1.2.3", "196.1.2.4"):
+        loc = "http://%s%s" % (host, path)
+        try:
+            r = requests.request("PURGE", loc, timeout=1)
+        except requests.exceptions.RequestException:
+            pass
+
+    # Using RabbitMQ to avoid needing knowledge of the nodes. See
+    # twisted/monitor.py on how to handle the messages. You will need pika
+    # installed.
+    rabbit_host = "192.1.2.5"
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=rabbit_host)
+    )
+    channel = connection.channel()
+    channel.exchange_declare(exchange="purgatory", type="fanout")
+    channel.basic_publish(
+        exchange="purgatory",
+        routing_key="",
+        body=path
+    )
+    connection.close()
+
---
+++ packages/django-ultracache/django_ultracache-1.10.2-py2.7.egg/ultracache/tests/settings/16.py
@@ -0,0 +1,46 @@
+import os
+
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+    }
+}
+
+ROOT_URLCONF = "ultracache.tests.urls"
+
+INSTALLED_APPS = (
+    "ultracache",
+    # Include tests because it declares models
+    "ultracache.tests",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "rest_framework"
+
+)
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+    }
+}
+
+SITE_ID = 1
+
+SECRET_KEY = "SECRET_KEY"
+
+TEMPLATE_DIRS = (os.path.realpath(os.path.dirname(__file__)) + "/ultracache/tests/templates/",)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.request",
+)
+
+ULTRACACHE = {
+    "purge": {"method": "ultracache.tests.utils.dummy_purger"},
+    "drf": {"viewsets": {"*": {}}}
+}
