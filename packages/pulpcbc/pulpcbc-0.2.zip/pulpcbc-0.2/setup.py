792c050166f0327e88c024fc12583d3e8ada5a82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:40 +0000

    Add package pulpcbc/0.2/pulpcbc-0.2.zip

---
+++ packages/pulpcbc/pulpcbc-0.2.zip/pulpcbc-0.2/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+
+ext_modules = [Extension('pulpcbc',
+                         ['pulpcbc.pyx', 'CyEventHandler.cpp'],
+                         libraries=['CbcSolver', 'Cbc', 'Cgl', 'Osi', 'OsiClp',
+                                    'Clp', 'CoinUtils', 'rt', 'bz2', 'z',
+                                    'blas', 'lapack'],
+                         extra_compile_args=['-fpermissive'],
+                         extra_link_args=['-Wl,--no-as-needed'],
+                         language="c++",
+                        )]
+
+
+setup(
+    name='pulpcbc',
+    version='0.2',
+    url='https://github.com/opensoft/pulpcbc',
+    cmdclass={'build_ext': build_ext},
+    ext_modules=ext_modules,
+    requires=[
+        'Cython',
+        'PuLP',
+    ],
+)
