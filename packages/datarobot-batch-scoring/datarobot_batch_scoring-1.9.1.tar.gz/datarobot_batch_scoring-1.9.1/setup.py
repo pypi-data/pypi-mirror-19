49e6aeb1f49a6bd14f3bbef7d1ec53b7a20ea3c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:46 +0000

    Add package datarobot-batch-scoring/1.9.1/datarobot_batch_scoring-1.9.1.tar.gz

---
+++ packages/datarobot-batch-scoring/datarobot_batch_scoring-1.9.1.tar.gz/datarobot_batch_scoring-1.9.1/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import codecs
+import os
+import os.path
+import re
+import sys
+
+extra = {}
+
+if 'py2exe' in sys.argv:
+    import py2exe
+    from distutils.core import setup
+    extra['console'] = ['batch_scoring.py']
+else:
+    py2exe = None
+    from setuptools import setup
+
+    fname = os.path.join(os.path.abspath(os.path.dirname(
+        __file__)), 'requirements.txt')
+
+    install_requires = open(fname, 'r').readlines()
+
+    extra['entry_points'] = {
+        'console_scripts': [
+            'batch_scoring = datarobot_batch_scoring.main:main']}
+    extra['install_requires'] = install_requires
+
+
+fname = os.path.join(os.path.abspath(os.path.dirname(
+    __file__)), 'datarobot_batch_scoring', '__init__.py')
+
+
+with codecs.open(fname, 'r', 'latin1') as fp:
+    try:
+        version = re.findall(r"^__version__ = '([^']+)'\r?$",
+                             fp.read(), re.M)[0]
+    except IndexError:
+        raise RuntimeError('Unable to determine version.')
+
+
+setup(
+    name='datarobot_batch_scoring',
+    version=version,
+    description=("A script to support start/resume batch scoring "
+                 "via Datarobot API."),
+    author='DataRobot',
+    author_email='support@datarobot.com',
+    maintainer='DataRobot',
+    maintainer_email='support@datarobot.com',
+    license='BSD',
+    url='http://www.datarobot.com/',
+    packages=['datarobot_batch_scoring'],
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    **extra
+)
