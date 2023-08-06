825b33e483e10e3658afad67607fc35fc98b16b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:22 +0000

    Add package pydatalearn/0.1.2/pydatalearn-0.1.2.zip

---
+++ packages/pydatalearn/pydatalearn-0.1.2.zip/pydatalearn-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pydatalearn',
+    version='0.1.2',
+    packages=['pydatalearn', 'pydatalearn.naive_bayes_classifier'],
+    url='https://github.com/ParadoxZero/pydatalearn',
+    license="MIT License",
+    author='Sidhin S Thomas',
+    author_email='sidhin.thomas@gmail.com',
+    description="Collection of data science models for machine learning"
+)
