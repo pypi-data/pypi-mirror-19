d8203cc0188265b42819731baf19e96499e051ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:01 +0000

    Add package gemstone/0.5.0/gemstone-0.5.0-py3-none-any.whl

---
+++ packages/gemstone/gemstone-0.5.0-py3-none-any.whl/tests/test_service_registry_integration.py
@@ -0,0 +1,42 @@
+from unittest import TestCase, skipIf
+from unittest.mock import patch
+from io import BytesIO
+import sys
+
+from gemstone import MicroService
+
+IS_WINDOWS = sys.platform.startswith("win32")
+
+
+class TestServiceNoServiceRegistries(MicroService):
+    name = "test.service.1"
+
+
+class TestServiceOneServiceRegistry(MicroService):
+    name = "test.service.1"
+    service_registry_urls = ["http://localhost:9999/api"]
+
+
+class ServiceRegistryIntegrationTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    @patch("gemstone.core.microservice.PeriodicCallback")
+    @patch("urllib.request.urlopen")
+    @skipIf(IS_WINDOWS, "different default callbacks for windows")
+    def test_periodic_callback_init(self, urlopen, PeriodicCallback):
+        callbacks = list(TestServiceNoServiceRegistries()._periodic_task_iter())
+        self.assertEqual(len(callbacks), 0)
+        urlopen.assert_not_called()
+
+        urlopen.return_value = BytesIO(b'{"result":{"name":"registry", "methods":'
+                                       b'{"ping": "", "locate_service": ""}}}')
+
+        callbacks = list(TestServiceOneServiceRegistry()._periodic_task_iter())
+        self.assertEqual(len(callbacks), 1)
+        self.assertEqual(urlopen.call_count, 2)  # get_service_specs and initial ping
