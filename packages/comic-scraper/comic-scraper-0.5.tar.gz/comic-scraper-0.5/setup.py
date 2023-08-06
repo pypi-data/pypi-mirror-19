34446892c5bd545ab59f85dbc0b08d6c9b649552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:55 +0000

    Add package comic-scraper/0.5/comic-scraper-0.5.tar.gz

---
+++ packages/comic-scraper/comic-scraper-0.5.tar.gz/comic-scraper-0.5/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(name='comic-scraper',
+      version='0.5',
+      description='Scraps comics and creates cbz files',
+      url='https://github.com/AbstractGeek/comic-scraper',
+      download_url='https://github.com/AbstractGeek/comic-scraper/tarball/0.5',
+      author='Dinesh Natesan',
+      author_email='abstractgeek@outlook.com',
+      license='MIT',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Games/Entertainment',
+      ],
+      keywords='comics manga scraper',
+      packages=['comic_scraper'],
+      install_requires=[
+          'beautifulsoup4',
+          'futures',
+          'requests==2.9.1'
+      ],
+      entry_points={
+        'console_scripts':
+        ['comic-scraper=comic_scraper.comic_scraper:main'],
+        },
+      include_package_data=True,
+      zip_safe=False)
