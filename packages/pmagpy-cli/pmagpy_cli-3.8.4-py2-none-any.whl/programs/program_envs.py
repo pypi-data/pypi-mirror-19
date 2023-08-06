34a530421fd11ba49e75c2915726f378914d598d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:30 +0000

    Add package pmagpy-cli/3.8.4/pmagpy_cli-3.8.4-py2-none-any.whl

---
+++ packages/pmagpy-cli/pmagpy_cli-3.8.4-py2-none-any.whl/programs/program_envs.py
@@ -0,0 +1,10 @@
+# programs that require a non-TKAgg backend
+
+prog_env = {'core_depthplot': 'WXAgg',
+            'ani_depthplot': 'WXAgg',
+            'demag_gui': 'WXAgg',
+            'magic_gui': 'WXAgg',
+            'magic_gui3': 'WXAgg',
+            'pmag_gui': 'WXAgg',
+            'pmag_gui3': 'WXAgg',
+            'thellier_gui': 'WXAgg'}
