e5f3b2e519b3302265028d81a3e5d2531c2daa9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:14 +0000

    Add package tracker17/0.2/tracker17-0.2.tar.gz

---
+++ packages/tracker17/tracker17-0.2.tar.gz/tracker17-0.2/setup.py
@@ -0,0 +1,22 @@
+import os
+from distutils.core import setup
+
+long_description = 'This small tool can be used to track mail packages from about anywhere on the world thanks to 17track.net service.'
+
+if os.path.exists('README.md'):
+    long_description = open('README.md', 'r').read()
+
+
+setup(
+  name = 'tracker17',
+  packages = ['tracker17'], # this must be the same as the name above
+  version = '0.2',
+  description = '17Track.net Python API (Unnoficial)',
+  long_description = long_description,
+  author = 'Kamus Hadenes',
+  author_email = 'kamushadenes@hyadesinc.com',
+  url = 'https://github.com/kamushadenes/tracker17', # use the URL to the github repo
+  download_url = 'https://github.com/kamushadenes/tracker17/tarball/0.1', # I'll explain this in a second
+  keywords = ['tracking', 'mail', 'api'], # arbitrary keywords
+)
+
