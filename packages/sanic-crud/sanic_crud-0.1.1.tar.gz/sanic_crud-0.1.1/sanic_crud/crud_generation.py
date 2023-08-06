dfc52c834da1b32100c342f2fb62ed174bd46d40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:24 +0000

    Add package sanic-crud/0.1.1/sanic_crud-0.1.1.tar.gz

---
+++ packages/sanic-crud/sanic_crud-0.1.1.tar.gz/sanic_crud-0.1.1/sanic_crud/crud_generation.py
@@ -0,0 +1,18 @@
+from .crud_config import CrudConfig, CrudShortcuts
+from .single_resource import BaseResource
+from .collection_resource import BaseCollectionResource
+
+
+def generate_crud(app, model_array):
+    for model in model_array:
+        if not hasattr(model, 'crud_config'):
+            model.crud_config = CrudConfig
+
+        shortcuts = CrudShortcuts(model)
+        model.shortcuts = shortcuts
+
+        SingleResource = type('SingleResource', (BaseResource,), {'model': model})
+        CollectionResource = type('CollectionResource', (BaseCollectionResource,), {'model': model})
+
+        app.add_route(SingleResource.as_view(), shortcuts.base_uri + '/<{}:{}>'.format(shortcuts.primary_key, shortcuts.primary_key_type))
+        app.add_route(CollectionResource.as_view(), shortcuts.base_uri)
