67c470fba793a3238097ddf4920d1a3ae303d922
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:09 +0000

    Add package django-performance-testing/0.6.0/django_performance_testing-0.6.0-py2.py3-none-any.whl

---
+++ packages/django-performance-testing/django_performance_testing-0.6.0-py2.py3-none-any.whl/django_performance_testing/apps.py
@@ -0,0 +1,20 @@
+from django.apps import AppConfig
+
+
+class DjangoPerformanceTestingAppConfig(AppConfig):
+
+    name = 'django_performance_testing'
+
+    def ready(self):
+        from django_performance_testing.registry import \
+            SettingsOrDefaultBasedRegistry
+        from django_performance_testing import core
+        core.limits_registry = SettingsOrDefaultBasedRegistry()
+        from .test_client import integrate_into_test_client
+        integrate_into_test_client()
+        from .test_runner import integrate_into_django_test_runner
+        integrate_into_django_test_runner()
+        from .queries import setup_sending_before_clearing_queries_log_signal
+        setup_sending_before_clearing_queries_log_signal()
+        from .templates import integrate_into_django_templates
+        integrate_into_django_templates()
