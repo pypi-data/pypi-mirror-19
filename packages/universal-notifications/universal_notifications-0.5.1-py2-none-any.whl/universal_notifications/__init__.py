e185c5f5ef757b8bf0487072d1171d4510e756e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:24 +0000

    Add package universal-notifications/0.5.1/universal_notifications-0.5.1-py2-none-any.whl

---
+++ packages/universal-notifications/universal_notifications-0.5.1-py2-none-any.whl/universal_notifications/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+__title__ = 'Universal Notification'
+__version__ = '0.5.1'
+__author__ = 'Pawel Krzyzaniak'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016 Arabella'
+
+# Version synonym
+VERSION = __version__
