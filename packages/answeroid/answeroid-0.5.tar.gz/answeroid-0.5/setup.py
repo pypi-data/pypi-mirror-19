679ebbea79bcd0728e7af8954258b47166a30531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:03 +0000

    Add package answeroid/0.5/answeroid-0.5.tar.gz

---
+++ packages/answeroid/answeroid-0.5.tar.gz/answeroid-0.5/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='answeroid',
+    packages=['answeroid'],
+    version='0.5',
+    description='A simple bot for helping on Q/A sites',
+    author='Bahrom Matyakubov',
+    author_email='bahrom.matyakubov@gmail.com',
+    url='https://github.com/bahrom-matyakubov/answeroid',
+    download_url='https://github.com/bahrom-matyakubov/answeroid/tarball/0.5',
+    keywords=['Q&A', 'answer', 'question', 'help', 'bot'],
+    classifiers=[],
+)
