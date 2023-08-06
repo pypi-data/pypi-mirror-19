ba32a21455691f5203db846c501b68ec214e70f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:21 +0000

    Add package simuvex/6.7.1.31/simuvex-6.7.1.31-py2-none-manylinux1_i686.whl

---
+++ packages/simuvex/simuvex-6.7.1.31-py2-none-manylinux1_i686.whl/simuvex/plugins/procedure_data.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from .plugin import SimStatePlugin
+class SimProcedureData(SimStatePlugin):
+    def __init__(self):
+        SimStatePlugin.__init__(self)
+
+        self.hook_addr = 0
+        self.callstack = []
+
+    def copy(self):
+        out = SimProcedureData()
+        out.hook_addr = self.hook_addr
+        out.callstack = list(self.callstack)
+        return out
+
+    def merge(self, others, merge_conditions, common_ancestor=None):
+        return False
+
+    def widen(self, others):
+        return False
+
+    def clear(self):
+        s = self.state
+        self.__init__()
+        self.state = s
+
+SimProcedureData.register_default('procedure_data', SimProcedureData)
