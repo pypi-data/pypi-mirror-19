0fe2bdea4f64a66fda0f145b8ce988150de84132
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:57 +0000

    Add package eudplib/0.55.1/eudplib-0.55.1.zip

---
+++ packages/eudplib/eudplib-0.55.1.zip/eudplib-0.55.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from Cython.Build import cythonize
+
+__version__ = '0.55.1'
+
+
+setup(
+    name="eudplib",
+    version=__version__,
+    packages=find_packages(),
+    package_data={
+        '': ['*.c', '*.pxd', '*.pyx', '*.dll', '*.lst'],
+    },
+    install_requires=[
+        'Cython',
+    ],
+    ext_modules=cythonize([
+        "eudplib/core/allocator/*.pyx",
+        "eudplib/utils/*.pyx",
+    ]),
+
+    # metadata for upload to PyPI
+    author="Trgk",
+    author_email="whyask37@naver.com",
+    description="EUD Trigger generator",
+    license="MIT license",
+    keywords="starcraft rawtrigger eud",
+    url="http://blog.naver.com/whyask37/",  # project home page, if any
+)
