f8acd325fc41a1afda378e152a1248a8538951cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:49 +0000

    Add package restea/0.3.2/restea-0.3.2.tar.gz

---
+++ packages/restea/restea-0.3.2.tar.gz/restea-0.3.2/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+
+setup(
+    name='restea',
+    packages=['restea', 'restea.adapters'],
+    version='0.3.2',
+    description='Simple RESTful server toolkit',
+    author='Walery Jadlowski',
+    author_email='bodb.digr@gmail.com',
+    url='https://github.com/bodbdigr/restea',
+    download_url='https://github.com/bodbdigr/restea/archive/0.3.2.tar.gz',
+    keywords=['rest', 'restful', 'restea'],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
