2e9b0385781bea2cc97882f81efa0623097e4b75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:24 +0000

    Add package trabBuild/0.1.1/trabBuild-0.1.1.tar.gz

---
+++ packages/trabBuild/trabBuild-0.1.1.tar.gz/trabBuild-0.1.1/setup.py
@@ -0,0 +1,38 @@
+from os import path
+from setuptools import setup
+
+
+__version__ = '0.1.1'
+
+SETUP_DIR = path.abspath(path.dirname(__file__))
+
+
+# Get the long description from the README file.
+with open(path.join(SETUP_DIR, 'README.rst')) as f:
+    long_description = f.read()
+
+
+# get the dependencies and installs
+with open(path.join(SETUP_DIR, 'requirements.txt')) as f:
+    all_reqs = f.read().split('\n')
+
+
+install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+
+
+setup(
+    name='trabBuild',
+    version=__version__,
+    packages=['trabbuild'],
+    license='Apache Software License',
+    author='Scott Doucet',
+    author_email='duroktar@gmail.com',
+    description='A script for updating and building python dists',
+    long_description=long_description,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'trabbuild = trabbuild.__init__:main'
+        ]
+    }
+)
