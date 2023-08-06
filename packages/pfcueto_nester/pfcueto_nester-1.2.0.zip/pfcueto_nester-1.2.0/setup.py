93838ea68ed2b95b1281c6dbc4f78826781774c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:45 +0000

    Add package pfcueto_nester/1.2.0/pfcueto_nester-1.2.0.zip

---
+++ packages/pfcueto_nester/pfcueto_nester-1.2.0.zip/pfcueto_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'pfcueto_nester',
+    version = '1.2.0',
+    py_modules = {'nester'},
+    author = 'pfcq',
+    author_email = 'pfcq@gmail.com',
+    url = 'http://www.pfcq.com',
+    description = 'A simple printer of nested lists'
+    )
