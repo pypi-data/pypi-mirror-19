140ced0353fa31d61d836dd7653fbb803ee4fd3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:43 +0000

    Add package django-donations/0.5.0/django_donations-0.5.0-py2.py3-none-any.whl

---
+++ packages/django-donations/django_donations-0.5.0-py2.py3-none-any.whl/donations/admin.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8
+from __future__ import unicode_literals, absolute_import
+
+from django.contrib import admin
+
+from donations.models import Donation, Frequency, DonationProvider
+
+
+class DonationAdmin(admin.ModelAdmin):
+    list_display = ('amount', 'provider', 'frequency', 'datetime', 'donor', 'is_verified')
+
+
+class FrequencyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'interval')
+
+
+class DonationProviderAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'klass')
+
+
+admin.site.register(Donation, DonationAdmin)
+admin.site.register(Frequency, FrequencyAdmin)
+admin.site.register(DonationProvider, DonationProviderAdmin)
