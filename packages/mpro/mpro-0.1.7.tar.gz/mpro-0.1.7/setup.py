e5e5b829f477478036361bf386a953717589fb5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:45 +0000

    Add package mpro/0.1.7/mpro-0.1.7.tar.gz

---
+++ packages/mpro/mpro-0.1.7.tar.gz/mpro-0.1.7/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(	# Application name:
+	name='mpro',
+	# Version number:
+	version='0.1.7',	
+	package='mpro',
+	# Application author details:
+	author='reddyreddymanohar',
+	author_email='reddyreddymanohar@gmail.com',
+
+)
+
