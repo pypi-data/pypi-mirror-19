8801cee5787ea1ccc43178fd65a4cb3004ee019b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:08 +0000

    Add package easyRedisCache/0.13/easyRedisCache-0.13.zip

---
+++ packages/easyRedisCache/easyRedisCache-0.13.zip/easyRedisCache-0.13/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+setup(
+    name='easyRedisCache',
+    version='0.13',
+    author=u'David Ziegler',
+    author_email='webmaster@SpreadPost.de',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/InfinityMod/easyRedisCache',
+    license='BSD',
+    description='Easy lock secured cache for redis',
+    zip_safe=False,
+    keywords=['cache', 'lock', 'memcached', 'redis'],
+    dependency_links=[
+        "git+ssh:git@github.com:InfinityMod/easyRedisCache.git#egg=redisSherlock-0.3.0"
+    ],
+    install_requires=[
+        'redisSherlock',
+        'redis'
+    ],
+)
