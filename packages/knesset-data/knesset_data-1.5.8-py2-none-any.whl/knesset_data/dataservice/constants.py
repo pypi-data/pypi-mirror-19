9de5a15eb8ea3f9864d974eea29cb4b14a6bc279
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:06 +0000

    Add package knesset-data/1.5.8/knesset_data-1.5.8-py2-none-any.whl

---
+++ packages/knesset-data/knesset_data-1.5.8-py2-none-any.whl/knesset_data/dataservice/constants.py
@@ -0,0 +1,14 @@
+
+# this list is based on the list at the following url under ODATA:
+# http://main.knesset.gov.il/Activity/Info/Pages/Databases.aspx
+SERVICE_URLS = {
+    'laws': "http://knesset.gov.il/KnessetOdataService/LawsData.svc",
+    'bills': "http://knesset.gov.il/KnessetOdataService/BillsData.svc",
+    'final_laws': "http://knesset.gov.il/KnessetOdataService/FinalLawsData.svc",
+    'members': "http://knesset.gov.il/KnessetOdataService/KnessetMembersData.svc",
+    'votes': "http://knesset.gov.il/KnessetOdataService/VotesData.svc",
+    'committees': "http://knesset.gov.il/KnessetOdataService/CommitteeScheduleData.svc",
+    'messages': "http://knesset.gov.il/KnessetOdataService/KnessetMessagesData.svc",
+    'mmm': "http://knesset.gov.il/KnessetOdataService/MMMData.svc",
+    'lobbyists': "http://knesset.gov.il/KnessetOdataService/LobbyistData.svc",
+}
