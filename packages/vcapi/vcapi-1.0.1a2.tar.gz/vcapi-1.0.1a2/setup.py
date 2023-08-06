957a38cce31a407257062597a97e9a9fddfd226b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:06 +0000

    Add package vcapi/1.0.1a2/vcapi-1.0.1a2.tar.gz

---
+++ packages/vcapi/vcapi-1.0.1a2.tar.gz/vcapi-1.0.1a2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='vcapi',
+    packages=['vcapi'],
+    version='1.0.1.a2',
+    description="Python API wrapper for Veracode's API",
+    author="Anthony Lozano",
+    author_email='amlozano1@gmail.com',
+    keywords=['veracode', 'api', 'wrapper'],
+    install_requires=['requests>=2.11.1',
+                      'click>=6.6'],
+    url='https://github.com/AnthonyLozano/vcapi',
+    entry_points={
+        'console_scripts': [
+            'vcapi=vcapi.vcapi:main',
+        ],
+    },
+)
