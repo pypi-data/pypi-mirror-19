4b4562f99548ba4443d1235b8bee75380c3a1df8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:27 +0000

    Add package pystreamdl/0.0.1/pystreamdl-0.0.1.tar.gz

---
+++ packages/pystreamdl/pystreamdl-0.0.1.tar.gz/pystreamdl-0.0.1/setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(name='pystreamdl',
+	        version='0.0.1',
+	        description='Downloader library for large files based on pycurl',
+	        author='Fina Wilke',
+	        author_email='fina@felinira.net',
+	        url='https://github.com/Farthen/pystreamdl',
+	        py_modules=['pystreamdl'],
+	        license='MIT',
+	        
+	        keywords='pycurl downloader library',
+	        install_requires=['pycurl'],
+	        
+	        classifiers=[
+            # How mature is this project? Common values are
+            #   3 - Alpha
+            #   4 - Beta
+            #   5 - Production/Stable
+                'Development Status :: 3 - Alpha',
+
+                # Indicate who your project is intended for
+                'Intended Audience :: Developers',
+                'Topic :: Software Development :: Build Tools',
+
+                # Pick your license as you wish (should match "license" above)
+                'License :: OSI Approved :: MIT License',
+
+                # Specify the Python versions you support here. In particular, ensure
+                # that you indicate whether you support Python 2, Python 3 or both.
+                'Programming Language :: Python :: 2',
+                'Programming Language :: Python :: 2.6',
+                'Programming Language :: Python :: 2.7',
+        ],
+)
