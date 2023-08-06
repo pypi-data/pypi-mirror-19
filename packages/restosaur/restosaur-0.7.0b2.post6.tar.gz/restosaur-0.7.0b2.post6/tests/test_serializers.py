887b2e9f104ba19dff9449e76d30539239c973ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:40 +0000

    Add package restosaur/0.7.0b2.post6/restosaur-0.7.0b2.post6.tar.gz

---
+++ packages/restosaur/restosaur-0.7.0b2.post6.tar.gz/restosaur-0.7.0b2.post6/tests/test_serializers.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+from restosaur import API
+from restosaur.contrib.django.dispatch import resource_dispatcher_factory
+
+import unittest
+import json
+
+
+class MultiPartFormSerializerTestCase(unittest.TestCase):
+    def setUp(self):
+        super(MultiPartFormSerializerTestCase, self).setUp()
+
+        from django.test import RequestFactory
+        self.rqfactory = RequestFactory()
+
+        self.api = API('foo')
+        self.view = self.api.resource('/')
+
+        @self.view.post('multipart/form-data')
+        def view_post_json(ctx):
+            return ctx.OK(ctx.body)
+
+        @self.view.representation(media='application/json')
+        def view_repr(obj, ctx):
+            return dict(obj.items())
+
+    def call(self, resource, method, *args, **kw):
+        rq = getattr(self.rqfactory, method)(resource.path, *args, **kw)
+        return resource_dispatcher_factory(self.api, resource)(rq)
+
+    def test_decoding_multipart_form(self):
+        resp = self.call(
+                self.view, 'post', data={'foo': 'bar'},
+                HTTP_ACCEPT='application/json')
+        self.assertEqual(resp.status_code, 200)
+        data = json.loads(resp.content)
+        self.assertEqual(data['foo'], 'bar')
