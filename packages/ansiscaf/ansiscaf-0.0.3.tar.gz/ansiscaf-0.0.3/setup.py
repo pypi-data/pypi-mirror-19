32ac22434cf92290e84988d00944249e0052a420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:38 +0000

    Add package ansiscaf/0.0.3/ansiscaf-0.0.3.tar.gz

---
+++ packages/ansiscaf/ansiscaf-0.0.3.tar.gz/ansiscaf-0.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys, os
+
+
+install_requires=[
+    # -*- Extra requirements: -*-
+
+],
+
+long_desc="""help create file tree for ansible"""
+
+setup(name='ansiscaf',
+      version="0.0.3",
+      description="ansible scaffold",
+      long_description=long_desc,
+      keywords='ansible scaffold',
+      author='terry.shi',
+      author_email='terryshi96@yahoo.com',
+      url='https://github.com/terryshi96/ansiscaf',
+      license='MIT License',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      install_requires=install_requires,
+      entry_points={
+            "console_scripts": [
+                  "ansiscaf = ansiscaf:main",
+            ],
+      },
+      classifiers=[],
+      )
+#python setup.py sdist --formats=zip,gztar  && python setup.py sdist upload
