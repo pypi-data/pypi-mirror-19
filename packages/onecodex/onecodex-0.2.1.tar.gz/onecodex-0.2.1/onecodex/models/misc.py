6e5b49556f0e39bc212eaba4ee03421850a78b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:39 +0000

    Add package onecodex/0.2.1/onecodex-0.2.1.tar.gz

---
+++ packages/onecodex/onecodex-0.2.1.tar.gz/onecodex-0.2.1/onecodex/models/misc.py
@@ -0,0 +1,28 @@
+from onecodex.models import OneCodexBase
+from onecodex.models.helpers import truncate_string
+
+
+class Tags(OneCodexBase):
+    _resource_path = '/api/v1/tags'
+
+    def __repr__(self):
+        return '<{} {}: "{}">'.format(self.__class__.__name__, self.id,
+                                      truncate_string(self.name, 24))
+
+
+class Users(OneCodexBase):
+    _resource_path = '/api/v1/users'
+
+
+class Projects(OneCodexBase):
+    _resource_path = '/api/v1/projects'
+
+    @classmethod
+    def search_public(cls, *filters, **keyword_filters):
+        keyword_filters['_instances'] = 'instances_public'
+        keyword_filters['limit'] = 100
+        return cls.where(*filters, **keyword_filters)
+
+
+class Jobs(OneCodexBase):
+    _resource_path = '/api/v1/jobs'
