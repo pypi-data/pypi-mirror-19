6c3b51bea25a7245cb80def309511a82481d4c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:25 +0000

    Add package mastercard-spendalerts/1.0.2/mastercard_spendalerts-1.0.2-py2-none-any.whl

---
+++ packages/mastercard-spendalerts/mastercard_spendalerts-1.0.2-py2-none-any.whl/mastercardspendalerts/__init__.py
@@ -0,0 +1,16 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardspendalerts.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardspendalerts.alerts import *
+from mastercardspendalerts.amount import *
+from mastercardspendalerts.card import *
+from mastercardspendalerts.carddisable import *
+from mastercardspendalerts.channel import *
+from mastercardspendalerts.controls import *
+from mastercardspendalerts.crossborder import *
+from mastercardspendalerts.declines import *
+from mastercardspendalerts.verify import *
