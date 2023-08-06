1b952aeabab0384fcb3330c4bf5bc16b00522b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:20 +0000

    Add package knmt/0.9.9/knmt-0.9.9.tar.gz

---
+++ packages/knmt/knmt-0.9.9.tar.gz/knmt-0.9.9/nmt_chainer/_build.py
@@ -0,0 +1,6 @@
+# file created by setup.py
+__build__ = "779b1982420baf016e20b3aa290a31e249f84632"
+
+__git_diff__ = """
+"diff --git a/nmt_chainer/_version.py b/nmt_chainer/_version.py\nindex 725c309..5fd292c 100644\n--- a/nmt_chainer/_version.py\n+++ b/nmt_chainer/_version.py\n@@ -1 +1 @@\n-__version__ = \"0.9.8\"\n\\ No newline at end of file\n+__version__ = \"0.9.9\"\n\\ No newline at end of file\n"
+"""
