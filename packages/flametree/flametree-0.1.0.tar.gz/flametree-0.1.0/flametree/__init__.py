44cecee7f17477caf21299a8d9285adbbdd1ed9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:19 +0000

    Add package flametree/0.1.0/flametree-0.1.0.tar.gz

---
+++ packages/flametree/flametree-0.1.0.tar.gz/flametree-0.1.0/flametree/__init__.py
@@ -0,0 +1,7 @@
+""" flametree/__init__.py """
+
+# __all__ = []
+
+from .DirectoryFilesTree import DirectoryFilesTree
+from .ZipFilesTree import ZipFilesTree, ZipFilesManager
+from .utils import files_tree
