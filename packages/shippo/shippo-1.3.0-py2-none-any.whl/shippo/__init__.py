721df0f2b82244d341382822fdb928ab58713313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:37 +0000

    Add package shippo/1.3.0/shippo-1.3.0-py2-none-any.whl

---
+++ packages/shippo/shippo-1.3.0-py2-none-any.whl/shippo/__init__.py
@@ -0,0 +1,21 @@
+# Configurable variables
+api_key = None
+api_base = 'https://api.goshippo.com/'
+api_version = None
+verify_ssl_certs = False
+rates_req_timeout = 20.0  # seconds
+
+from shippo.resource import (
+    Address,
+    CarrierAccount,
+    CustomsDeclaration,
+    CustomsItem,
+    Manifest,
+    Parcel,
+    Rate,
+    Refund,
+    Shipment,
+    Track,
+    Transaction,
+    Batch,
+)
