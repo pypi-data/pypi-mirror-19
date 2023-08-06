a5f19fd17c1f1389b880fea19947fc69ec3f140b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:03 +0000

    Add package ez-grid/0.1/ez_grid-0.1.zip

---
+++ packages/ez-grid/ez_grid-0.1.zip/ez_grid-0.1/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ez_grid',
+      packages=['ez_grid'],
+      version='0.1',
+      description='A lightweight convenient grid class',
+      author='0Hughman',
+      author_email="rammers2@hotmail.co.uk",
+      url='https://github.com/0Hughman0/ez_grid',
+      download_url='https://github.com/0Hughman0/ez_grid/tarball/0.1',
+      keywords=["grid", "config", "lightweight", "no dependencies", "configuration", "csv"],
+      classifiers=[]
+     )
