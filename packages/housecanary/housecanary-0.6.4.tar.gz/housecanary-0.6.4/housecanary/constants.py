964d9cd381397aa7a1f045e7e4d0fd732cad47b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:47 +0000

    Add package housecanary/0.6.4/housecanary-0.6.4.tar.gz

---
+++ packages/housecanary/housecanary-0.6.4.tar.gz/housecanary-0.6.4/housecanary/constants.py
@@ -0,0 +1,9 @@
+HTTP_CODE_OK = 200
+HTTP_FORBIDDEN = 403
+HTTP_TOO_MANY_REQUESTS = 429
+
+BIZ_CODE_OK = 0
+
+URL_PREFIX = "https://api-dev.housecanary.net"
+# URL_PREFIX = "http://localhost:9123"
+DEFAULT_VERSION = "v2"
