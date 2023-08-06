f3460363631ab6cf95a146b6f178588f3efd590a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:28 +0000

    Add package sqliteJson/0.1/sqliteJson-0.1.tar.gz

---
+++ packages/sqliteJson/sqliteJson-0.1.tar.gz/sqliteJson-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'sqliteJson',
+    packages=['sqliteJson'],
+    version='0.1',
+    description= 'Convert sqlite query results into json consumable dicts',
+    author = 'Khubo',
+    author_email = 'aswinrajv@gmail.com',
+    url = 'https://github.com/khubo/sqlitejson',
+    download_url='https://github.com/khubo/sqliteJson/tarball/0.1',
+    keywords = ['sqliteJson', 'bottle', 'json', 'sqlite3'],
+    classifiers = [],
+)
