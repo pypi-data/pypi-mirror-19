19fd0958453606d112eda9e6fd81ef1929930966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:17 +0000

    Add package cms7/0.1a19/cms7-0.1a19.tar.gz

---
+++ packages/cms7/cms7-0.1a19.tar.gz/cms7-0.1a19/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='cms7',
+    version='0.1a19',
+    description='Simple static site generator',
+    author='Ed Kellett',
+    author_email='e@kellett.im',
+    url='https://github.com/edk0/cms7',
+    packages=['cms7', 'cms7.modules'],
+    install_requires=[
+        'colorlog',
+        'clize',
+        'feedgenerator',
+        'html5lib',
+        'jinja2',
+        'markdown',
+        'requests',
+        'pathlib2',
+        'python-dateutil',
+        'pyyaml',
+    ],
+    entry_points={
+        'console_scripts': [
+            'cms7 = cms7.cli:main'
+        ]
+    }
+)
