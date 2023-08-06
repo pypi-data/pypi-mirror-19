c00f24119ee675e6ff1bebbba092e547f1d3a0b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:24 +0000

    Add package pkinter/1.13.15/pkinter-1.13.15.tar.gz

---
+++ packages/pkinter/pkinter-1.13.15.tar.gz/pkinter-1.13.15/pkinter/__init__.py
@@ -0,0 +1,19 @@
+from .toggledlabelframe import *
+from .labeledseparator import *
+from .roundingscale import *
+from .entrytext import *
+from .limitedentry import *
+from .colourpickerbutton import *
+from .editablelabel import *
+from .collapsiblepane import *
+from .hyperlink import *
+from .pageview import *
+from .toolbar import *
+from .statusbar import *
+from .linenumbers import *
+from .boundbutton import *
+from .validentry import *
+
+__title__ = "Pkinter"
+__author__ = "DeflatedPickle"
+__version__ = "1.13.15"
