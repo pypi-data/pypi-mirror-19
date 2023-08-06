d0bd5ea4930e4f836a9684efa2ed67a1d846bd02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:59 +0000

    Add package cwltool/1.0.20170119182607/cwltool-1.0.20170119182607.tar.gz

---
+++ packages/cwltool/cwltool-1.0.20170119182607.tar.gz/cwltool-1.0.20170119182607/cwltool/factory.py
@@ -0,0 +1,46 @@
+from . import main
+from . import load_tool
+from . import workflow
+import os
+from .process import Process
+from typing import Any, Text, Union, Tuple
+from typing import Callable as tCallable
+import argparse
+
+class WorkflowStatus(Exception):
+    def __init__(self, out, status):
+        # type: (Dict[Text,Any], Text) -> None
+        super(WorkflowStatus, self).__init__("Completed %s" % status)
+        self.out = out
+        self.status = status
+
+class Callable(object):
+    def __init__(self, t, factory):  # type: (Process, Factory) -> None
+        self.t = t
+        self.factory = factory
+
+    def __call__(self, **kwargs):
+        # type: (**Any) -> Union[Text, Dict[Text, Text]]
+        execkwargs = self.factory.execkwargs.copy()
+        execkwargs["basedir"] = os.getcwd()
+        out, status = self.factory.executor(self.t, kwargs, **execkwargs)
+        if status != "success":
+            raise WorkflowStatus(out, status)
+        else:
+            return out
+
+class Factory(object):
+    def __init__(self, makeTool=workflow.defaultMakeTool,
+                 executor=main.single_job_executor,
+                 **execkwargs):
+        # type: (tCallable[[Dict[Text, Any], Any], Process],tCallable[...,Tuple[Dict[Text,Any], Text]], **Any) -> None
+        self.makeTool = makeTool
+        self.executor = executor
+        self.execkwargs = execkwargs
+
+    def make(self, cwl):
+        """Instantiate a CWL object from a CWl document."""
+        load = load_tool.load_tool(cwl, self.makeTool)
+        if isinstance(load, int):
+            raise Exception("Error loading tool")
+        return Callable(load, self)
