32fd6eb52d1499e1627ad83c51a7f31f7e577ed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:52 +0000

    Add package ChenONNE_nester/1.1.0/ChenONNE_nester-1.1.0.tar.gz

---
+++ packages/ChenONNE_nester/ChenONNE_nester-1.1.0.tar.gz/ChenONNE_nester-1.1.0/nester.py
@@ -0,0 +1,13 @@
+"""This is "nester.py" module, which provides a function named print_lol() that print a list, while the list may (or not) contain a nested list."""
+ 
+def print_lol(the_list,level):
+	"""This function get a parameter called "the_list", which can be any Python List(also can be a list contain list). All data in the list will be printed on the screen in a line"""
+	for each_item in the_list:
+		if isinstance(each_item,list):
+			print_lol(each_item,level+1)
+		else:
+			for num in range(level):
+				print("\t",end='')
+			print(each_item)
+
+
---
+++ packages/ChenONNE_nester/ChenONNE_nester-1.1.0.tar.gz/ChenONNE_nester-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'ChenONNE_nester',
+	version = '1.1.0',
+	py_modules = ['nester'],
+	author = 'SunshineChen',
+	author_email = '1012868589@qq.com',
+	url = 'http://www.headfirstlabs.com',
+	description = 'A simple printer of nested lists',
+	)
+
