986fecf45f0b282eabecf7852267d627d97eb1ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:30 +0000

    Add package mindsensors_i2c/1.1/mindsensors_i2c-1.1.tar.gz

---
+++ packages/mindsensors_i2c/mindsensors_i2c-1.1.tar.gz/mindsensors_i2c-1.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='mindsensors_i2c',
+    version='1.1',
+    description='mindsensors_i2c library',
+    author='mindsensors.com',
+    author_email='contact@mindsensors.com',
+    url='http://www.mindsensors.com',
+    py_modules=['mindsensors_i2c', 'ms_explorerlib'],
+    )
