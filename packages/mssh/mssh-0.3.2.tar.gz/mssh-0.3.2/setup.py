48a18f6a319094e7d70ec7c8390e40e66615701b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:34 +0000

    Add package mssh/0.3.2/mssh-0.3.2.tar.gz

---
+++ packages/mssh/mssh-0.3.2.tar.gz/mssh-0.3.2/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+	name='mssh',
+	version='0.3.2',
+	packages=find_packages(),
+	url='https://github.com/foofilers/mssh',
+	license='GPLv3',
+	author='Igor Maculan',
+	author_email='n3wtron@gmail.com',
+	description='ssh manager',
+	keywords=['ssh','manager'],
+	install_requires=['PyYAML'],
+	scripts=['bin/mssh'],
+	include_package_data=True,
+	package_data={
+		'mssh':['completions/zsh/_mssh','completions/bash/mssh'],
+		'mssh.config':[]
+	}
+)
