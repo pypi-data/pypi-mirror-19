e2dc2361a8ca2a762b0985836e9bcf052a3b2ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:25 +0000

    Add package slap/3.0.0/slap-3.0.0-py2-none-any.whl

---
+++ packages/slap/slap-3.0.0-py2-none-any.whl/slap/git.py
@@ -0,0 +1,10 @@
+import sys
+from subprocess import check_output
+
+
+def get_changed_files(sha='HEAD~1'):
+    return check_output(['git', 'diff', '--name-only', 'HEAD', sha]).splitlines()
+
+
+def get_changed_mxds(sha='HEAD~1'):
+    return [f for (f) in get_changed_files(sha) if str(f).lower().endswith('mxd')]
