96af0897ca19ebc68e04e2c6796a64f88663f52f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:41 +0000

    Add package eudplib/0.55.2/eudplib-0.55.2-cp36-cp36m-win32.whl

---
+++ packages/eudplib/eudplib-0.55.2-cp36-cp36m-win32.whl/eudplib/core/variable/vbuf.py
@@ -0,0 +1,82 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+Copyright (c) 2014 trgk
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+
+from ..eudobj import EUDObject
+from ..allocator import RegisterCreatePayloadCallback
+
+
+class EUDVarBuffer(EUDObject):
+    """Variable buffer
+
+    72 bytes per variable.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        self._initvals = []
+
+    def DynamicConstructed(self):
+        return True
+
+    def CreateVarTrigger(self, initval):
+        ret = self + (72 * len(self._initvals))
+        self._initvals.append(initval)
+        return ret
+
+    def GetDataSize(self):
+        return 2408 + 72 * (len(self._initvals) - 1)
+
+    def WritePayload(self, emitbuffer):
+        output = bytearray(2408 + 72 * (len(self._initvals) - 1))
+
+        for i in range(len(self._initvals)):
+            # 'preserve rawtrigger'
+            output[72 * i + 2376:72 * i + 2380] = b'\x04\0\0\0'
+
+        for i, initval in enumerate(self._initvals):
+            heads = 72 * (i - 1) + 352 if i > 0 else 0
+            heade = 72 * i + 348
+            emitbuffer.WriteBytes(output[heads:heade])
+            emitbuffer.WriteDword(initval)
+            # output[72 * i + 320 + 20:72 * i + 320 + 24] = ut.i2b4(initval)
+
+        tails = 72 * (len(self._initvals) - 1) + 352
+        emitbuffer.WriteBytes(output[tails:])
+
+
+_evb = None
+
+
+def RegisterNewVariableBuffer():
+    global _evb
+    _evb = EUDVarBuffer()
+
+
+def GetCurrentVariableBuffer():
+    return _evb
+
+
+RegisterCreatePayloadCallback(RegisterNewVariableBuffer)
