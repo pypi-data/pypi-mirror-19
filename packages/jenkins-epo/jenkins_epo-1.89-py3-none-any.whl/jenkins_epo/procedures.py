ddad7bd28bc390688da0e995e5322564b500ac1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:46 +0000

    Add package jenkins-epo/1.89/jenkins_epo-1.89-py3-none-any.whl

---
+++ packages/jenkins-epo/jenkins_epo-1.89-py3-none-any.whl/jenkins_epo/procedures.py
@@ -0,0 +1,94 @@
+# This file is part of jenkins-epo
+#
+# jenkins-epo is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or any later version.
+#
+# jenkins-epo is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# jenkins-epo.  If not, see <http://www.gnu.org/licenses/>.
+
+import asyncio
+import itertools
+import logging
+
+from .github import GITHUB, cached_arequest
+from .repository import Repository
+from .settings import SETTINGS
+
+
+logger = logging.getLogger(__name__)
+
+
+def iter_heads():
+    queues = []
+
+    for repository in list_repositories():
+        branches = repository.fetch_protected_branches()
+        pulls = repository.fetch_pull_requests()
+        heads = reversed(sorted(itertools.chain(
+            repository.process_protected_branches(branches),
+            repository.process_pull_requests(pulls),
+        ), key=lambda head: head.sort_key()))
+
+        # Yield first head of the repository before loading next repositories.
+        queue = iter(heads)
+        try:
+            yield next(heads)
+        except StopIteration:
+            continue
+        except GeneratorExit:
+            return
+        else:
+            queues.append(queue)
+
+    while queues:
+        for queue in queues[:]:
+            try:
+                yield next(queue)
+            except StopIteration:
+                queues.remove(queue)
+            except GeneratorExit:
+                return
+
+
+def list_repositories(with_settings=False):
+    repositories = set()
+
+    env_repos = filter(
+        None,
+        SETTINGS.REPOSITORIES.replace(' ', ',').split(',')
+    )
+    for repository in env_repos:
+        if repository in repositories:
+            continue
+        owner, name = repository.split('/')
+        try:
+            repository = Repository.from_name(owner, name)
+        except Exception as e:
+            logger.warn("Failed to fetch repo %s: %s", repository, e)
+            if SETTINGS.DEBUG:
+                raise
+            else:
+                continue
+
+        if repository in repositories:
+            continue
+
+        repositories.add(repository)
+        logger.debug("Managing %s.", repository)
+        yield repository
+
+
+@asyncio.coroutine
+def whoami():
+    user = yield from cached_arequest(GITHUB.user)
+    logger.info(
+        "I'm @%s on GitHub. %s remaining API calls.",
+        user['login'], GITHUB.x_ratelimit_remaining,
+    )
+    return user['login']
