3e8d14b57589c17c0800eab3c719e346570f2666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:22 +0000

    Add package nester_dchiu/1.2.0/nester_dchiu-1.2.0.tar.gz

---
+++ packages/nester_dchiu/nester_dchiu-1.2.0.tar.gz/nester_dchiu-1.2.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+
+    name = 'nester_dchiu',
+    version = '1.2.0',
+    py_modules = ['nester_dchiu'],
+    author = 'dchiu',
+    author_email = 'dean.qiu73@gmail.com',
+    url = '',
+    description = 'A simple printer of nested lists',
+
+    )
