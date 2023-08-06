649bf54622c19c646bef21a34a3934cb1653ab6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:57 +0000

    Add package pyfoscam/1.0/pyfoscam-1.0.tar.gz

---
+++ packages/pyfoscam/pyfoscam-1.0.tar.gz/pyfoscam-1.0/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+import os
+
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Customer Service',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: Telecommunications Industry',
+    'Natural Language :: English',
+    'Operating System :: POSIX',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Multimedia :: Sound/Audio',
+    'Topic :: Utilities',
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.6",
+    "Programming Language :: Python :: 2.7",
+]
+
+setup(
+    name='pyfoscam',
+    version='1.0',
+    description='Foscam Python Library for H.264 IP Cameras (FI9821W/P/HD816W/P)',
+    long_description=open('README', 'r').read(),
+    author='Cherish Chen',
+    author_email='sinchb128@gmail.com',
+    url='https://github.com/quatanium/foscam-python-lib',
+    include_package_data=True,
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    keywords=['foscam', 'Camera', 'IPC'],
+)
