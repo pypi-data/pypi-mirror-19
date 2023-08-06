f5ebeae974a71ac89d209bb266944b031f8c9942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:29 +0000

    Add package rak_arth/1.0.3/rak_arth-1.0.3.tar.gz

---
+++ packages/rak_arth/rak_arth-1.0.3.tar.gz/rak_arth-1.0.3/apps/Addition.py
@@ -0,0 +1,12 @@
+def add(*a):
+	sum = 0
+	for i in a:
+		sum = sum+i
+	print("the addition of given numbers is-->",sum)
+
+
+def mul(*a):
+	ml = 1
+	for i in a:
+		ml = ml*i
+	print("the multiplication of given numbers is-->",ml)
---
+++ packages/rak_arth/rak_arth-1.0.3.tar.gz/rak_arth-1.0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(	name="rak_arth",
+	packages = ["apps"],
+	version="1.0.3",
+	author="sripelli",
+	author_email="sripelli.rakesh@gmail.com"
+)
