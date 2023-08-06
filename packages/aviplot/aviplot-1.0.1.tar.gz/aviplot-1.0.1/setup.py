086cc8634fe63007c632baf2ac52f2d08a3b7c28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:50 +0000

    Add package aviplot/1.0.1/aviplot-1.0.1.tar.gz

---
+++ packages/aviplot/aviplot-1.0.1.tar.gz/aviplot-1.0.1/setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+__version__ = '1.0.1'
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = 'aviplot',
+    version = __version__,
+    author = 'Avijit Ghosh',
+    author_email = 'avijitg22@gmail.com',
+    url = 'https://github.com/AvijitGhosh82/aviplot',
+    description = 'Provide a matplotlib like interface to plotting data with Google Maps',
+    long_description=read('README.rst'),
+    license='MIT',
+    keywords='python wrapper google maps',
+    packages = find_packages(),
+    include_package_data=True,
+    package_data = {
+        'aviplot': ['markers/*.png'],
+    },
+    install_requires=['requests'],
+)
