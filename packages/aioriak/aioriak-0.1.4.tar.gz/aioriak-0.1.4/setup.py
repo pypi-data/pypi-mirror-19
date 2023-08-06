b3a4b3a006ded11ee607cf72a0ba81498da29b60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:09 +0000

    Add package aioriak/0.1.4/aioriak-0.1.4.tar.gz

---
+++ packages/aioriak/aioriak-0.1.4.tar.gz/aioriak-0.1.4/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from commands import (docker_build, docker_start, docker_stop, setup_riak,
+                      create_bucket_types, Test)
+
+setup(
+    name='aioriak',
+    version='0.1.4',
+    description='Async implementation of Riak DB python client',
+    author='Makc Belousov',
+    author_email='m.belousov@rambler-co.ru',
+    url='https://github.com/rambler-digital-solutions/aioriak',
+    keywords='riak asyncio client',
+    packages=find_packages(exclude=('*.tests',)),
+    include_package_data=True,
+    zip_safe=False,
+    license='MIT',
+    install_requires=[
+        'python3-riak-pb==2.1.0.6',
+        'riak==2.5.5',
+    ],
+    tests_require=['nose==1.3.7',
+                   'coverage==4.0.3'],
+    cmdclass={
+        'test': Test,
+        'docker_build': docker_build,
+        'docker_start': docker_start,
+        'docker_stop': docker_stop,
+        'setup_riak': setup_riak,
+        'create_bucket_types': create_bucket_types,
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
