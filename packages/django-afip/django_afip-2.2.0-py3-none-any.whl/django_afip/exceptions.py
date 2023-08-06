e3b7cd23a3bc28e11b13c91db6f63cc9a4bfe672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:57 +0000

    Add package django-afip/2.2.0/django_afip-2.2.0-py3-none-any.whl

---
+++ packages/django-afip/django_afip-2.2.0-py3-none-any.whl/django_afip/exceptions.py
@@ -0,0 +1,13 @@
+from . import parsers
+
+
+class AfipException(Exception):
+    """
+    Wraps around errors returned by AFIP's WS.
+    """
+
+    def __init__(self, response):
+        Exception.__init__(self, 'Error {}: {}'.format(
+            response.Errors.Err[0].Code,
+            parsers.parse_string(response.Errors.Err[0].Msg),
+        ))
