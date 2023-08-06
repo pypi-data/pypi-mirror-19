e3619400f4d3341f2a3929240373f5c0166fc27b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:58 +0000

    Add package wqwqq/1.1.0/wqwqq-1.1.0.zip

---
+++ packages/wqwqq/wqwqq-1.1.0.zip/wqwqq-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name='wqwqq',
+	version='1.1.0',
+	py_modeles=['wqwqq'],
+	author='hfpython',
+	author_email='hfpython@headfirstlabs.com',
+	url='http://www.headfirstlabs.com',
+	description='A simple printer of nested lists',
+)
