61f4563fa666b9302782408b10e1429f233d80ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:57 +0000

    Add package csirtg-indicator/0.0.0a28/csirtg_indicator-0.0.0a28.tar.gz

---
+++ packages/csirtg-indicator/csirtg_indicator-0.0.0a28.tar.gz/csirtg_indicator-0.0.0a28/csirtg_indicator/constants.py
@@ -0,0 +1,10 @@
+import sys
+
+PYVERSION = 2
+if sys.version_info > (3,):
+    PYVERSION = 3
+
+COLUMNS = ['tlp', 'lasttime', 'reporttime', 'itype', 'indicator', 'count', 'cc', 'asn', 'asn_desc', 'confidence',
+           'description', 'tags', 'rdata', 'provider']
+
+MAX_FIELD_SIZE = 30
