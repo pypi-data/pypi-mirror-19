498cea7e8873f2a1d48bc926bf7fde5705ed8e8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:30 +0000

    Add package PrintDebug/0.3.0/PrintDebug-0.3.0.tar.gz

---
+++ packages/PrintDebug/PrintDebug-0.3.0.tar.gz/PrintDebug-0.3.0/printdebug/__init__.py
@@ -0,0 +1,43 @@
+""" PrintDebug
+    ...small module that helps with debug printing.
+    -Christopher Welborn 08-21-2014
+"""
+
+from .tools import (
+    __version__,
+    DebugColrPrinter,
+    DebugNotEnabled,
+    DebugPrinter,
+    debug,
+    debug_enable,
+    default_format,
+    get_frame,
+    get_lineinfo,
+    printobject,
+    LineInfo,
+    suppress,
+)
+
+from .catchers import (
+    StdErrCatcher,
+    StdOutCatcher,
+)
+
+__all__ = [
+    '__version__',
+    # Exported tools
+    'DebugColrPrinter',
+    'DebugNotEnabled',
+    'DebugPrinter',
+    'debug',
+    'debug_enable',
+    'default_format',
+    'get_frame',
+    'get_lineinfo',
+    'printobject',
+    'LineInfo',
+    # Exported extras
+    'suppress',
+    'StdErrCatcher',
+    'StdOutCatcher',
+]
