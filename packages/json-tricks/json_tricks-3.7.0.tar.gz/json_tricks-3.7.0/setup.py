9dd60246d2a55f4bc462e9ff3671cb87e10333d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:07 +0000

    Add package json-tricks/3.7.0/json_tricks-3.7.0.tar.gz

---
+++ packages/json-tricks/json_tricks-3.7.0.tar.gz/json_tricks-3.7.0/setup.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+with open('README.rst', 'r') as fh:
+	readme = fh.read()
+
+
+setup(
+	name='json_tricks',
+	description='Add features to json: en/decoding of numpy arrays, ' +
+		'preservation of ordering and ignoring of comments in input',
+	long_description=readme,
+	url='https://github.com/mverleg/pyjson_tricks',
+	author='Mark V',
+	maintainer='(the author)',
+	author_email='mdilligaf@gmail.com',
+	license='Revised BSD License (LICENSE.txt)',
+	keywords=['json', 'numpy', 'OrderedDict', 'comments',],
+	version='3.7.0',
+	packages=['json_tricks'],
+	include_package_data=True,
+	zip_safe=False,
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'License :: OSI Approved :: BSD License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.6',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Topic :: Software Development :: Libraries :: Python Modules',
+	],
+	install_requires=[
+		# pytz for timezones (and tests)
+		# pytest for tests
+		# sphinx for documentation
+		# numpy for numpy functionality
+	],
+)
+
+
