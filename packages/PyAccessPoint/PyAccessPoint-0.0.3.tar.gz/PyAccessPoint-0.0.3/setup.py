51e0fad73dcf815026d08496db1fec55ff13b054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:49 +0000

    Add package PyAccessPoint/0.0.3/PyAccessPoint-0.0.3.tar.gz

---
+++ packages/PyAccessPoint/PyAccessPoint-0.0.3.tar.gz/PyAccessPoint-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+s = setup(
+    install_requires=[
+        'wireless',
+        'netifaces',
+        'psutil'
+    ],
+    name='PyAccessPoint',
+    version='0.0.3',
+    description='Package to manage wifi hotspot on linux',
+    license='GNU GPLv3',
+    author='Anton Bautkin',
+    author_email='antonbautkin@gmail.com',
+    url='https://github.com/Goblenus/pyaccesspoint',
+    packages=['PyAccessPoint'],
+    entry_points={
+        'console_scripts': [
+            'pyaccesspoint = PyAccessPoint.main:main',
+        ],
+    }
+)
