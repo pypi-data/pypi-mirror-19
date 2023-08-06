c8355e78511546dceadfa34f414c7daaa65a90e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:03 +0000

    Add package telegraphapi/0.2/telegraphapi-0.2.zip

---
+++ packages/telegraphapi/telegraphapi-0.2.zip/telegraphapi-0.2/telegraphapi/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: MonsterDeveloper
+@contact: https://github.com/MonsterDeveloper
+@license: MIT License, see LICENSE file
+
+Copyright (C) 2017
+"""
+
+__author__ = 'Andrew Developer'
+__version__ = '0.2'
+
+from .api import Telegraph, TelegraphAPIException
---
+++ packages/telegraphapi/telegraphapi-0.2.zip/telegraphapi-0.2/telegraphapi/exceptions.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: MonsterDeveloper
+@contact: https://github.com/MonsterDeveloper
+@license: MIT License, see LICENSE file
+
+Copyright (C) 2017
+"""
+
+class TelegraphAPIException(Exception):
+    pass
+
+
+class ParsingException(Exception):
+    pass
+
+
+class NotAllowedTag(ParsingException):
+    pass
+
+
+class InvalidHTML(ParsingException):
+    pass
