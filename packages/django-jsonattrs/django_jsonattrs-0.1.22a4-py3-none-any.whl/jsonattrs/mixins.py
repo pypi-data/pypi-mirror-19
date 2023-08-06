b0827004895d40d56403306315a3e6974040df4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:09:53 +0000

    Add package django-jsonattrs/0.1.22a4/django_jsonattrs-0.1.22a4-py3-none-any.whl

---
+++ packages/django-jsonattrs/django_jsonattrs-0.1.22a4-py3-none-any.whl/jsonattrs/mixins.py
@@ -0,0 +1,27 @@
+from jsonattrs.models import Schema
+
+
+def template_xlang_labels(attr):
+    try:
+        labels = ['data-label-{}="{}"'.format(k, v)
+                  for k, v in attr.long_name_xlat.items()]
+        return ' '.join(labels)
+    except AttributeError:
+        return None
+
+
+class JsonAttrsMixin:
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+
+        obj = self.object
+        field = self.attributes_field
+        obj_attrs = getattr(obj, field)
+
+        schemas = Schema.objects.from_instance(obj)
+        attrs = [a for s in schemas for a in s.attributes.all()]
+        context[field] = [(a.long_name,
+                           a.render(obj_attrs.get(a.name, '—')),
+                           template_xlang_labels(a))
+                          for a in attrs if not a.omit]
+        return context
