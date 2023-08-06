f65e2a710a79bdefff9ec6c98cf5506363f9a2bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:44 +0000

    Add package knightswhosay/0.1/knightswhosay-0.1.tar.gz

---
+++ packages/knightswhosay/knightswhosay-0.1.tar.gz/knightswhosay-0.1/knightswhosay/knights.py
@@ -0,0 +1,4 @@
+class KnightWhoSayNi(object):
+
+    def say(self):
+        print('Ni!')
---
+++ packages/knightswhosay/knightswhosay-0.1.tar.gz/knightswhosay-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='knightswhosay',
+      version='0.1',
+      description='Knights who say stugg',
+      url='',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='BoNoBo',
+      packages=['knightswhosay'],
+      install_requires=[
+          'flask',
+      ],
+      zip_safe=False)
+
