0364636aef47fe767f840a8ea92b9f8005d1f641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:43 +0000

    Add package cieloApi3/0.1.1/cieloApi3-0.1.1.tar.gz

---
+++ packages/cieloApi3/cieloApi3-0.1.1.tar.gz/cieloApi3-0.1.1/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='cieloApi3',
+    version='0.1.1',
+    url='https://github.com/thiago-Malaca/API-3.0-Python',
+    license='MIT License',
+    author='Thiago Malaquias',
+    author_email='thiago.malaca@gmail.com',
+    keywords='api3.0 cielo python sdk ecommerce',
+    description=u'SDK API-3.0 Python Cielo',
+    packages=['cielo.api30'],
+    install_requires=['requests'],
+)
