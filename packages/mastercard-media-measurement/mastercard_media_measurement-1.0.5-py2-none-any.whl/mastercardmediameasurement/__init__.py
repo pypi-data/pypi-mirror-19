1d84d90c1227a7066e254ceb6e5e07e39bc6b71f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:25 +0000

    Add package mastercard-media-measurement/1.0.5/mastercard_media_measurement-1.0.5-py2-none-any.whl

---
+++ packages/mastercard-media-measurement/mastercard_media_measurement-1.0.5-py2-none-any.whl/mastercardmediameasurement/__init__.py
@@ -0,0 +1,8 @@
+from mastercardapicore import *
+
+# import apis resourceconfig
+from mastercardmediameasurement.resourceconfig import ResourceConfig
+
+
+# import apis into api package
+from mastercardmediameasurement.mediameasurements import *
