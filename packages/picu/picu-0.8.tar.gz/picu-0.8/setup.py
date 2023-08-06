fca0b40756d08c38fd13318c1f57d0bc9273b184
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:17:24 +0000

    Add package picu/0.8/picu-0.8.tar.gz

---
+++ packages/picu/picu-0.8.tar.gz/picu-0.8/setup.py
@@ -0,0 +1,13 @@
+#!/bin/env python2
+from setuptools import setup
+
+setup(
+    name="picu",
+    version='0.8',
+    author="Wil Tan",
+    author_email="wil@cloudregistry.net",
+    description="Python ctypes-based ICU Wrapper",
+    license="MIT/X",
+    install_requires=[],
+    packages=['picu']
+)
