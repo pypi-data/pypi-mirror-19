837edbb963a4cd506db09d0be1d34c3a0ca851a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:19 +0000

    Add package galaxy-sdk-python3/0.5/galaxy-sdk-python3-0.5.tar.gz

---
+++ packages/galaxy-sdk-python3/galaxy-sdk-python3-0.5.tar.gz/galaxy-sdk-python3-0.5/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+def load_file(name):
+    with open(name) as fs:
+        content = fs.read().strip().strip('\n')
+    return content
+
+
+README = load_file('README.md')
+
+setup(
+    name='galaxy-sdk-python3',
+    version="0.5",
+    author='http://xiangyang.li',
+    author_email='wo@xiangyang.li',
+    url='http://xiangyang.li/project/galaxy-sdk-python',
+    description='Xiaomi Galaxy SDK for Python3',
+    long_description=README,
+    packages=find_packages('lib'),
+    package_dir={'': 'lib'},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: Apache Software License',
+        'Intended Audience :: Developers',
+    ],
+    license='Apache License (2.0)',
+)
