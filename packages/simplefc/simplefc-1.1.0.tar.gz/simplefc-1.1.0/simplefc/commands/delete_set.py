1b09220ab672dc6abbf19d13b1d5bca85aeff2fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:12 +0000

    Add package simplefc/1.1.0/simplefc-1.1.0.tar.gz

---
+++ packages/simplefc/simplefc-1.1.0.tar.gz/simplefc-1.1.0/simplefc/commands/delete_set.py
@@ -0,0 +1,38 @@
+"""delete a flashcard set"""
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+from __future__ import absolute_import
+
+from builtins import input
+from builtins import str
+from future import standard_library
+standard_library.install_aliases()
+from .base import Base
+import sqlite3
+import os
+
+class Delete_set(Base):
+
+    def run(self):
+        name = str(self.options.get('<setname>'))
+        yn = input("Are you sure you want to delete flashcard set '"
+                 + name + "'?\n(y/n)\n> ")
+        if yn == 'y':
+            path = os.path.dirname(os.path.realpath(__file__))
+            dbpfp = path + '/../dbpath.txt'
+            dbpathfile = open(dbpfp, 'r')
+            dbpath = dbpathfile.read()
+            dbpathfile.close()
+            conn = sqlite3.connect(dbpath)
+            cur = conn.cursor()
+            cur.execute('drop table ' + name + ';')
+            conn.commit()
+            conn.close()
+            print("deleted set '" + name + "'")
+
+        elif yn == 'n':
+            print("took no action")
+
+        else:
+            print("required input either 'y' or 'n'.")
---
+++ packages/simplefc/simplefc-1.1.0.tar.gz/simplefc-1.1.0/simplefc/commands/reset_data.py
@@ -0,0 +1,32 @@
+"""Set all 'correct' and 'incorrect' records back to 0."""
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+from __future__ import absolute_import
+
+from builtins import str
+from future import standard_library
+standard_library.install_aliases()
+from .base import Base
+import sqlite3
+import os
+
+class Reset_data(Base):
+
+    def run(self):
+        path = os.path.dirname(os.path.realpath(__file__))
+        dbpfp = path + '/../dbpath.txt'
+        dbpathfile = open(dbpfp, 'r')
+        dbpath = dbpathfile.read()
+        dbpathfile.close()
+        conn = sqlite3.connect(dbpath)
+        cur = conn.cursor()
+        name = str(self.options.get('<setname>'))
+        cur.execute("update " +name+ " set correct=0, "
+                    "incorrect=0;")
+        
+        print("Correct/incorrect count has been reset "
+              "for flashcard set '" +name+ "'.")
+
+        conn.commit()
+        conn.close()
