d1e92031cd4888cf689f8bb0f7b7c8ea0d0c6a8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:05 +0000

    Add package rp_arith/1.1.0/rp_arith-1.1.0.tar.gz

---
+++ packages/rp_arith/rp_arith-1.1.0.tar.gz/rp_arith-1.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    # Application name:
+    name="rp_arith",
+
+    # Version number (initial):
+    version="1.1.0",
+
+    # Application author details:
+    author="name surname",
+    author_email="name@addr.ess",
+
+    # Packages
+    packages=["rp_add","rp_sub"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="http://pypi.python.org/pypi/MyApplication_v010/",
+
+    #
+    # license="LICENSE.txt",
+    description="Useful towel-related stuff.",
+
+    # long_description=open("README.txt").read(),
+)
