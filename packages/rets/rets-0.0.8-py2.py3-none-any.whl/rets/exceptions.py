b5b1b05e93388cdf9d11ccb6b13d56bd3a18d536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:16:57 +0000

    Add package rets/0.0.8/rets-0.0.8-py2.py3-none-any.whl

---
+++ packages/rets/rets-0.0.8-py2.py3-none-any.whl/rets/exceptions.py
@@ -0,0 +1,18 @@
+class InvalidFormat(Exception):
+    pass
+
+
+class RETSException(Exception):
+    pass
+
+
+class NotLoggedIn(Exception):
+    pass
+
+
+class ParseError(Exception):
+    pass
+
+
+class MissingVersion(Exception):
+    pass
