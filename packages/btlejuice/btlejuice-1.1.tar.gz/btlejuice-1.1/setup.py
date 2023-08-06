7d58b3af8199ff7fa828034706dd1b70ac9ed48d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:16 +0000

    Add package btlejuice/1.1/btlejuice-1.1.tar.gz

---
+++ packages/btlejuice/btlejuice-1.1.tar.gz/btlejuice-1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='btlejuice',
+    version='1.1',
+    description='BtleJuice MitM framework Python bindings',
+    author='Damien Cauquil',
+    author_email='damien.cauquil@digitalsecurity.fr',
+    license='MIT',
+    packages=['btlejuice','btlejuice.socketIO_client'],
+    url='https://github.com/DigitalSecurity/btlejuice-python-bindings',
+    install_requires=[
+        'six',
+        'websocket'
+    ]
+)
