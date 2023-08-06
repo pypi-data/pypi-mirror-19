44fd39ab62b1c65d7be587d9f5fe9a3359a4416a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:45 +0000

    Add package Orange3/3.3.10/Orange3-3.3.10-cp35-none-win32.whl

---
+++ packages/Orange3/Orange3-3.3.10-cp35-none-win32.whl/Orange/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM ORANGE SETUP.PY
+short_version = '3.3.10'
+version = '3.3.10'
+full_version = '3.3.10'
+git_revision = '80be73507d3a4cffb4550984d42565cc75574e55'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
