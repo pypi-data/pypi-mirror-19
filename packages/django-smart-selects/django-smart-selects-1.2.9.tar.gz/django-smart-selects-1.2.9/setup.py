68d230cf59626d13ad73bf44005e4aba1b2d85f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:10 +0000

    Add package django-smart-selects/1.2.9/django-smart-selects-1.2.9.tar.gz

---
+++ packages/django-smart-selects/django-smart-selects-1.2.9.tar.gz/django-smart-selects-1.2.9/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(name="django-smart-selects",
+      version="1.2.9",
+      description="Django application to handle chained model fields.",
+      author="Patrick Lauber",
+      author_email="digi@treepy.com",
+      url="https://github.com/digi604/django-smart-selects",
+      packages=find_packages(),
+      include_package_data=True,
+      )
