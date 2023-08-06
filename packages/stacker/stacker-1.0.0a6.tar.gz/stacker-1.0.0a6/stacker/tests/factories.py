e0c9dc3dc1b3f11eac4a629bf11f061126f90530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:45 +0000

    Add package stacker/1.0.0a6/stacker-1.0.0a6.tar.gz

---
+++ packages/stacker/stacker-1.0.0a6.tar.gz/stacker-1.0.0a6/stacker/tests/factories.py
@@ -0,0 +1,34 @@
+from mock import MagicMock
+
+from stacker.context import Context
+from stacker.lookups import Lookup
+
+
+def mock_provider(**kwargs):
+    return MagicMock(**kwargs)
+
+
+def mock_context(namespace=None, **kwargs):
+    environment = kwargs.get("environment", {})
+    if namespace is not None:
+        environment["namespace"] = namespace
+
+    return Context(environment, **kwargs)
+
+
+def generate_definition(base_name, stack_id, **overrides):
+    definition = {
+        "name": "%s.%d" % (base_name, stack_id),
+        "class_path": "stacker.tests.fixtures.mock_blueprints.%s" % (
+            base_name.upper()),
+        "namespace": "example-com",
+        "requires": []
+    }
+    definition.update(overrides)
+    return definition
+
+
+def mock_lookup(lookup_input, lookup_type='output', raw=None):
+    if raw is None:
+        raw = lookup_input
+    return Lookup(type=lookup_type, input=lookup_input, raw=raw)
