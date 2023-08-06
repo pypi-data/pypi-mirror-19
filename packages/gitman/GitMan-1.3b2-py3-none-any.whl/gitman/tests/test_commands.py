0838e513fc5bddaeab4baf1719ff090f8db7c57b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:40 +0000

    Add package gitman/1.3b2/GitMan-1.3b2-py3-none-any.whl

---
+++ packages/gitman/GitMan-1.3b2-py3-none-any.whl/gitman/tests/test_commands.py
@@ -0,0 +1,37 @@
+# pylint: disable=no-self-use
+
+import os
+
+from gitman.commands import _find_root, install, update, display, delete
+
+from .conftest import ROOT, FILES
+
+PROJECT_ROOT = os.path.dirname(os.path.dirname(ROOT))
+PROJECT_PARENT = os.path.dirname(PROJECT_ROOT)
+
+
+class TestCommands:
+
+    def test_commands_can_be_run_without_project(self, tmpdir):
+        tmpdir.chdir()
+
+        assert not install()
+        assert not update()
+        assert not display()
+        assert not delete()
+
+
+class TestFindRoot:
+
+    def test_specified(self):
+        os.chdir(PROJECT_PARENT)
+        assert FILES == _find_root(FILES)
+
+    def test_none(self):
+        assert PROJECT_ROOT == _find_root(None, cwd=ROOT)
+
+    def test_current(self):
+        assert PROJECT_ROOT == _find_root(PROJECT_ROOT, cwd=ROOT)
+
+    def test_missing(self):
+        assert PROJECT_PARENT == _find_root(None, cwd=PROJECT_PARENT)
