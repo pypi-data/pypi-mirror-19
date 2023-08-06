6293b6e8ec7dabb858a6a1bba40449f9e2e31549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:20 +0000

    Add package syntax-sugar/0.0.4/syntax_sugar-0.0.4.tar.gz

---
+++ packages/syntax-sugar/syntax_sugar-0.0.4.tar.gz/syntax_sugar-0.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="syntax_sugar",
+    version="0.0.4",
+    url='https://github.com/czheo/syntax_sugar_python',
+    description="add syntactic sugar to Python",
+    author="czheo",
+    license="MIT",
+    keywords="syntax, functional",
+    packages=find_packages(),
+    install_requires=[
+        'multiprocess',
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
