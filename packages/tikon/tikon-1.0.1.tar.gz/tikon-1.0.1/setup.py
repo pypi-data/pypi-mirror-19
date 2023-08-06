fc9bb95574981f497d5a91e433df1f2af5b44edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:18 +0000

    Add package tikon/1.0.1/tikon-1.0.1.tar.gz

---
+++ packages/tikon/tikon-1.0.1.tar.gz/tikon-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+with open('tikon/versión.txt') as archivo_versión:
+    versión = archivo_versión.read().strip()
+
+setup(
+    name='tikon',
+    version=versión,
+    packages=find_packages(),
+    url='https://github.com/julienmalard/Tikon',
+    license='GNU 3',
+    author='Julien Jean Malard',
+    author_email='julien.malard@mail.mcgill.ca',
+    description='',
+    requires=['pymc', 'numpy', 'matplotlib', 'scipy'],
+    package_data={
+        # Incluir estos documentos de los paquetes:
+        '': ['*.csv', 'versión.txt', 'Interfaz/*.json', 'Interfaz/*.png', 'Interfaz/*.jpg', 'Interfaz/*.png'],
+    },
+)
