b3a4684d23cd84a0ee96acef0cfa304e35d728d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:20 +0000

    Add package django-cspreport/0.1.5/django-cspreport-0.1.5.tar.gz

---
+++ packages/django-cspreport/django-cspreport-0.1.5.tar.gz/django-cspreport-0.1.5/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+  name='django-cspreport',
+  packages=find_packages(),
+  version='0.1.5',
+  description='Django app for handling reports from web browsers of violations of content security policy.',
+  author='Anton Tuchak',
+  author_email='anton.tuchak@gmail.com',
+  url='https://github.com/atuchak/django-cspreport',
+  # download_url='https://github.com/atuchak/django-cspreport/archive/0.1.0.tar.gz',
+  keywords=['Content-Security-Policy', 'csp', 'Django'],
+  classifiers=[],
+)
