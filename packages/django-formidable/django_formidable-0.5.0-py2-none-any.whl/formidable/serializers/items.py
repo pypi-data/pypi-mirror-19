9a6a38fac974c86dcb8c9e6346cbf65ec07affe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:10 +0000

    Add package django-formidable/0.5.0/django_formidable-0.5.0-py2-none-any.whl

---
+++ packages/django-formidable/django_formidable-0.5.0-py2-none-any.whl/formidable/serializers/items.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from rest_framework import serializers
+
+from formidable.models import Item
+from formidable.serializers.list import NestedListSerializer
+
+
+class ItemListSerializer(NestedListSerializer):
+
+    field_id = 'value'
+    parent_name = 'field_id'
+
+    def validate(self, data):
+        data = super(ItemListSerializer, self).validate(data)
+        for index, item in enumerate(data):
+            item['order'] = index
+
+        return data
+
+    def to_representation(self, items):
+        return super(ItemListSerializer, self).to_representation(
+            items.order_by('order')
+        )
+
+
+class ItemSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Item
+        list_serializer_class = ItemListSerializer
+        fields = ('value', 'label', 'help_text')
