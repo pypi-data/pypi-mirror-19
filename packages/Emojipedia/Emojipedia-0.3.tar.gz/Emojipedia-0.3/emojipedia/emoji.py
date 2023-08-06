43b0afe29ae9d4a57c2bc4cc4a938ef19b9f2e7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:10 +0000

    Add package Emojipedia/0.3/Emojipedia-0.3.tar.gz

---
+++ packages/Emojipedia/Emojipedia-0.3.tar.gz/Emojipedia-0.3/emojipedia/emoji.py
@@ -0,0 +1,91 @@
+class Emoji:
+    def __init__(self, content):
+        self.soup = content
+
+        self._aliases = None
+        self._character = None
+        self._codepoints = None
+        self._description = None
+        self._platforms = None
+        self._shortcodes = None
+        self._title = None
+
+    @property
+    def description(self):
+        if not self._description:
+            text = self.soup.find('section', {'class': 'description'}).text
+            if text:
+                self._description = text.strip()
+        return self._description
+
+    @property
+    def codepoints(self):
+        if not self._codepoints:
+            code_list = self.soup.find(text='Codepoints').findNext('ul')
+            if code_list:
+                nonunique = [child.text.split()[1]
+                             for child in code_list.findChildren()]
+                self._codepoints = list(set(nonunique))
+        return self._codepoints
+
+    @property
+    def platforms(self):
+        if not self._platforms:
+            self._platforms = list()
+            platform_section = self.soup.find('section',
+                                              {'class': 'vendor-list'})
+            for vendor in platform_section.findAll('div', {'class': 'vendor-rollout-target'}):
+                vendor_title = vendor.findNext('a')
+                vendor_img = vendor.find('div', {'class': 'vendor-image'})
+
+                platform = {
+                    'title': vendor_title.text
+                }
+
+                if vendor_img:
+                    #platform['platform_url'] = vendor_img.find('a')['href']
+                    platform['platform_image'] = vendor_img.find('img')['src']
+                self._platforms.append(platform)
+        return self._platforms
+
+    @property
+    def shortcodes(self):
+        if not self._shortcodes:
+            codelist = self.soup.find(text='Shortcodes')
+            if codelist:
+                self._shortcodes = codelist.findNext('ul').text.strip()
+        return self._shortcodes
+
+    @property
+    def aliases(self):
+        if not self._aliases:
+            alias_section = self.soup.find('section', {'class': 'aliases'})
+            if alias_section:
+                self._aliases = list()
+                for alias in alias_section.findAll('li'):
+                    # Remove initial emoji + whitespace
+                    self._aliases.append(" ".join(alias.text.split()[1:]))
+        return self._aliases
+
+    @property
+    def title(self):
+        if not self._title:
+            # Remove initial emoji + whitespace
+            self._title = " ".join(self.soup.find('h1').text.split()[1:])
+        return self._title
+
+    @property
+    def character(self):
+        if not self._character:
+            self._character = self.soup.find('h1').text.split()[0]
+        return self._character
+
+    def __str__(self):
+        string = u"<Emoji - '{0}' - character: {2}, description: {1}>"
+        string = string.format(self.title,
+                               self.description[:20] + "...",
+                               self.character)
+        return string
+
+    def __repr__(self):
+        return self.__str__()
---
+++ packages/Emojipedia/Emojipedia-0.3.tar.gz/Emojipedia-0.3/emojipedia/emojipedia.py
@@ -0,0 +1,35 @@
+from __future__ import unicode_literals
+from bs4 import BeautifulSoup
+import requests
+from codecs import decode
+from .emoji import Emoji
+
+
+class Emojipedia:
+    @staticmethod
+    def search(query):
+        return Emoji(Emojipedia.get_emoji_page(query))
+
+    @staticmethod
+    def random():
+        return Emoji(Emojipedia.get_emoji_page('random'))
+
+    @staticmethod
+    def valid_emoji_page(soup):
+        """
+        (soup) -> bool
+        """
+        _type = soup.find('meta', {'property': 'og:type'})
+        return (_type and _type['content'] == 'article')
+
+    @staticmethod
+    def get_emoji_page(query):
+        response = requests.get('http://emojipedia.org/' + query)
+        if response.status_code != 200:
+            raise UserWarning('Could not get emojipedia page for \'{0}\''
+                              .format(query))
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        if not Emojipedia.valid_emoji_page(soup):
+            raise UserWarning('Query did not yield a emoji entry')
+        return soup
