692b7186bd2d08729b1276068916f27fc5952176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:31 +0000

    Add package qwer/1.0.0/qwer-1.0.0.tar.gz

---
+++ packages/qwer/qwer-1.0.0.tar.gz/qwer-1.0.0/qw/Sort Without Sort.py
@@ -0,0 +1,16 @@
+# l=[3,1,6,4,5]
+# c=len(l)
+# m=[]
+# for i in range(c):
+#
+#     m.append(min(l))
+#     l.remove(min(l))
+# print m
+
+l=[3,1,6,4,5]
+m=[]
+for i in l:
+    m.append()
+    # m.append(len(l))
+
+
---
+++ packages/qwer/qwer-1.0.0.tar.gz/qwer-1.0.0/qw/__init__.py
@@ -0,0 +1,1 @@
+from Sort Without Sort import *
