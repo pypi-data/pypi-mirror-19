0747cac8296208fdaf46dad61ba31378784e3502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:35:38 +0000

    Add package xknx/0.4.0/xknx-0.4.0.tar.gz

---
+++ packages/xknx/xknx-0.4.0.tar.gz/xknx-0.4.0/xknx/stateupdater.py
@@ -0,0 +1,34 @@
+import threading
+import time
+
+class StateUpdater(threading.Thread):
+
+    def __init__(self,
+                 xknx,
+                 timeout,
+                 start_timeout=15,
+                 sleep_during_devices=0.5):
+        self.xknx = xknx
+        self.timeout = timeout
+        self.start_timeout = start_timeout
+        self.sleep_during_devices = sleep_during_devices
+        threading.Thread.__init__(self)
+
+
+    def run(self):
+        time.sleep(self.start_timeout)
+        print("Starting Update Thread")
+        while True:
+            self.sync_states()
+            time.sleep(self.timeout)
+
+    def sync_states(self):
+        for device in self.xknx.devices.devices:
+            device.sync_state()
+            time.sleep(self.sleep_during_devices)
+
+    @staticmethod
+    def start_thread(xknx, timeout=60):
+        stateupdater = StateUpdater(xknx, timeout)
+        stateupdater.setDaemon(True)
+        stateupdater.start()
---
+++ packages/xknx/xknx-0.4.0.tar.gz/xknx-0.4.0/xknx/time.py
@@ -0,0 +1,54 @@
+from .device import Device
+from .address import Address
+from .dpt_time import DPTTime
+from .telegram import Telegram
+from .dpt import DPTArray
+
+
+class Time(Device):
+
+    def __init__(self,
+                 xknx,
+                 name,
+                 group_address=None):
+
+        Device.__init__(self, xknx, name)
+
+        if isinstance(group_address, (str, int)):
+            group_address = Address(group_address)
+
+        self.group_address = group_address
+
+
+    @classmethod
+    def from_config(cls, xknx, name, config):
+        group_address = \
+            config.get('group_address')
+
+        return cls(xknx,
+                   name,
+                   group_address=group_address)
+
+
+    def has_group_address(self, group_address):
+        return self.group_address == group_address
+
+
+    def broadcast_time(self):
+        telegram = Telegram()
+        telegram.group_address = self.group_address
+        telegram.payload = DPTArray(DPTTime.current_time_as_knx())
+        self.xknx.telegrams.put(telegram)
+
+
+    def sync_state(self):
+        self.broadcast_time()
+
+
+    def __str__(self):
+        return "<Time name={0}, group_address={1}>" \
+            .format(self.name, self.group_address)
+
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
