659d4c86675d687674387e9ba30b86407cae0a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:17:06 +0000

    Add package Products.LinguaPlone/4.1.8/Products.LinguaPlone-4.1.8.tar.gz

---
+++ packages/Products.LinguaPlone/Products.LinguaPlone-4.1.8.tar.gz/Products.LinguaPlone-4.1.8/Products/LinguaPlone/setuphandlers.py
@@ -0,0 +1,39 @@
+from Products.CMFCore.utils import getToolByName
+
+
+def importReindexLanguageIndex(context):
+    if context.readDataFile("linguaplone-reindex.txt") is None:
+        return
+    site = context.getSite()
+    logger = context.getLogger('LinguaPlone')
+    catalog = getToolByName(site, 'portal_catalog')
+
+    if 'Language' in catalog.indexes():
+        index = catalog._catalog.getIndex('Language')
+        # We do not want a LanguageIndex but a FieldIndex
+        if index.meta_type != 'FieldIndex':
+            catalog.delIndex('Language')
+
+    # Check again.
+    if 'Language' not in catalog.indexes():
+        extra = {'indexed_attrs': ['Language']}
+        catalog.addIndex('Language', 'FieldIndex', extra)
+        logger.info("Added FieldIndex for field Language.")
+
+    # Reindex when there are no objects.
+    index = catalog._catalog.getIndex('Language')
+    if index.numObjects() == 0:
+        catalog.reindexIndex('Language', None)
+
+
+def uninstall(context):
+    if context.readDataFile("linguaplone-uninstall.txt") is None:
+        return
+    site = context.getSite()
+    logger = context.getLogger('LinguaPlone')
+    qi = getToolByName(site, 'portal_quickinstaller')
+    qi.uninstallProducts(products=['LinguaPlone'])
+    o = qi._getOb('LinguaPlone', None)
+    if o:
+        assert(not o.isInstalled())
+    logger.info('Uninstalled LinguaPlone from portal_quickinstaller.')
