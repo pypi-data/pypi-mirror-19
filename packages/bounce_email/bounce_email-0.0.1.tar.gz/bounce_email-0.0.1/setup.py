5ebcf6440cd765857e066620babdf0bbac0b097a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:45 +0000

    Add package bounce_email/0.0.1/bounce_email-0.0.1.tar.gz

---
+++ packages/bounce_email/bounce_email-0.0.1.tar.gz/bounce_email-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='bounce_email',
+    version='0.0.1',
+    description=open('README.md').read(),
+    url='http://github.com/ronbeltran/bounce_email',
+    author='Ronnie Beltran',
+    author_email='rbbeltran.09@gmail.com',
+    license='MIT',
+    packages=['bounce_email'],
+    zip_safe=False,
+)
