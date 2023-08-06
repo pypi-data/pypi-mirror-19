5fa5969492f1ecd31bb34fd8f537c0304a6496be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:20 +0000

    Add package django-splitio/2.2.3/django_splitio-2.2.3.tar.gz

---
+++ packages/django-splitio/django_splitio-2.2.3.tar.gz/django_splitio-2.2.3/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from os import path
+from setuptools import setup
+
+install_requires = ['splitio_client[redis]>=2.1']
+
+with open(path.join(path.abspath(path.dirname(__file__)), 'django_splitio', 'version.py')) as f:
+    exec (f.read())
+
+setup(name='django_splitio',
+      version=__version__,
+      description='Split.io Django Application',
+      author='Patricio Echague, Sebastian Arrubia',
+      author_email='pato@split.io, sebastian@split.io',
+      url='https://github.com/splitio/django-splitio',
+      license='Apache License 2.0',
+      install_requires=install_requires,
+      setup_requires=['flake8', 'coverage'],
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development :: Libraries'
+      ],
+      packages=['django_splitio'])
