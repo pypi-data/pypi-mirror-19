0214d030ed11820ce91a756a354f630067ac34f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:16 +0000

    Add package ncp/1.2/ncp-1.2.tar.gz

---
+++ packages/ncp/ncp-1.2.tar.gz/ncp-1.2/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(name='ncp',
+      version='1.2',
+      packages=find_packages(),
+      include_package_data=True,  # 启用清单文件MANIFEST.in
+      exclude_package_data={'': ['.gitignore']},
+      zip_safe=False,
+      install_requires=['jinja2>=2.9'],
+      entry_points='''
+        [console_scripts]
+        ncp=ncp.app:main
+    ''')
