9f74aceba3ccf3dac4a9d60e0686b4f229a04309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:40:34 +0000

    Add package lisa_translation_plugin/0.1.6/lisa_translation_plugin-0.1.6.tar.gz

---
+++ packages/lisa_translation_plugin/lisa_translation_plugin-0.1.6.tar.gz/lisa_translation_plugin-0.1.6/lisa_translation_plugin/__init__.py
@@ -0,0 +1,36 @@
+import goslate
+from lisa_sdk import LisaPlugin
+
+
+class LisaTranslationPlugin(LisaPlugin):
+    def __init__(self):
+        super().__init__()
+        self.name = "lisa-translation-plugin"
+        self.description = "module de traduction linguistique"
+        self.listening_language = "default"
+
+    def get_name(self):
+        return self.name
+
+    def get_description(self):
+        return self.description
+
+    def process_speech(self, speech, language):
+        pass
+
+    def get_listening_language(self):
+        return self.listening_language
+
+    def get_call_keywords(self):
+        return {
+            "fr-FR": ["traduction"]
+        }
+
+
+def main():
+    gs = goslate.Goslate()
+    print(gs.translate("hello world", "fr"))
+
+
+if __name__ == "__main__":
+    main()
