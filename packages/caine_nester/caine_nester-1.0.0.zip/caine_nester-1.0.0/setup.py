7091f2aec054fea5efc756ca4d496607f21aba9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:58 +0000

    Add package caine_nester/1.0.0/caine_nester-1.0.0.zip

---
+++ packages/caine_nester/caine_nester-1.0.0.zip/caine_nester-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name		= 'caine_nester',
+	version 	= '1.0.0',
+	py_modules	= ['nester'],
+	author		= 'caine',
+	author_email= '34665969@qq.com',
+	url			= 'http://www.heeadfirstlabs.com',
+	description = 'A simple printer of nested lists',
+)
