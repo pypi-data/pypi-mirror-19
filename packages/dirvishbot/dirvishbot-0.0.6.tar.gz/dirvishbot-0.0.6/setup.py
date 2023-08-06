4c178b371b8818cd491d4dac01771b3ff598df4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:52 +0000

    Add package dirvishbot/0.0.6/dirvishbot-0.0.6.tar.gz

---
+++ packages/dirvishbot/dirvishbot-0.0.6.tar.gz/dirvishbot-0.0.6/setup.py
@@ -0,0 +1,40 @@
+import re
+from setuptools import setup, find_packages
+from subprocess import check_output
+
+DATA_FILES = [
+    ('/etc/dirvishbot', ['resources/config.json.example']),
+    ('/var/log/dirvishbot', [])
+]
+
+
+def install_startscript():
+    try:
+        systemd_pid = re.match('([0-9].*)', check_output(["pidof", 'systemd']).decode('utf-8')).group(1)
+        if int(systemd_pid) > 0:
+            DATA_FILES.append(tuple(('/etc/systemd/system/', ['resources/systemd/dirvishbot.service'])))
+    except Exception as e:
+        print("No systemd detected. You have to write your own start-script or run dirvishbot by hand.")
+
+    return DATA_FILES
+
+
+setup(
+    name='dirvishbot',
+    version='0.0.6',
+    description='A simple telegram bot for dirvish',
+    url='https://github.com/jkuettner/dirvishbot',
+    download_url='https://github.com/jkuettner/dirvishbot',
+    author='jkuettner',
+    license='GPL3',
+    packages=find_packages(exclude=['resources']),
+    zip_safe=False,
+    install_requires=['python-telegram-bot'],
+    keywords='dirvish telegram bot notify notification',
+    data_files=install_startscript(),
+    entry_points={
+        'console_scripts': [
+            'dirvishbot = dirvishbot.__main__:main'
+        ]
+    }
+)
